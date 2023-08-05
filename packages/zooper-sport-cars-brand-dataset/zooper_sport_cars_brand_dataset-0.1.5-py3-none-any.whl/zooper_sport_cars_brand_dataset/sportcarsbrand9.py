dfaab730fc1c34575e861e92d87ae51756ace9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:22 +0000

    Add package zooper-sport-cars-brand-dataset/0.1.5/zooper_sport_cars_brand_dataset-0.1.5-py3-none-any.whl

---
+++ packages/zooper-sport-cars-brand-dataset/zooper_sport_cars_brand_dataset-0.1.5-py3-none-any.whl/zooper_sport_cars_brand_dataset/sportcarsbrand9.py
@@ -0,0 +1,36 @@
+"""SPORTCARSBRAND9 small images classification dataset.
+"""
+import os
+import shutil
+import tarfile
+import time
+
+from keras.utils.data_utils import get_file
+
+
+def load_data(target_dir='sport_cars_brand_dataset'):
+    """Loads SPORTCARSBRAND9 dataset.
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    filename = '/tmp/sport-cars-brand-9'
+    origin = 'https://sport-cars.s3-us-west-2.amazonaws.com/sport-cars-brand-9.tar.gz'
+
+    st = time.time()
+    path = get_file(filename, origin=origin, untar=True)
+
+    def tar_member(members):
+        for tarinfo in members:
+            yield tarinfo
+
+    tar = tarfile.open(path + '.tar.gz')
+    tar.extractall(members=tar_member(tar))
+    tar.close()
+
+    if os.path.isdir(target_dir):
+        print('Deleting {} ...'.format(target_dir))
+        shutil.rmtree(target_dir)
+    if target_dir != 'sport_cars_brand_dataset':
+        shutil.move('sport_cars_brand_dataset', target_dir)
+    et = time.time()
+    print('`sport_cars_brand` dataset downloaded to {}. Elapsed time: {} secs'.format(target_dir, (et - st)))
