73389e61342a34773fddc6ec0b59a45433ee7019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:17 +0000

    Add package serpentine/0.1.3/serpentine-0.1.3-py3-none-any.whl

---
+++ packages/serpentine/serpentine-0.1.3-py3-none-any.whl/serpentine/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Serpentine binning for Hi-C contact maps
+
+Provides:
+    -An implementation of the serpentine binning procedure described in
+    Baudry et al., usable on single or multiple contact maps
+    -A set of functions for quickly visualizing the effects of serpentine
+    binning in terms of contact distribution (median absolute deviation, etc.)
+
+"""
+
+from .serpentine import (
+    dshow,
+    fltmatr,
+    fromupdiag,
+    mad,
+    MDafter,
+    MDbefore,
+    mshow,
+    outstanding_filter,
+    serpentin_binning,
+    serpentin_iteration,
+    barycenter,
+    all_barycenters,
+    extract_serpentines,
+)
+from .benchmarks import *
+
+from .version import __version__ as version
+
+__author__ = "Cluster Buster (scovit, a.k.a. Vittore F. Scolari), \
+              Lyamovich (baudrly, a.k.a. Lyam Baudry)"
+__copyright__ = "Copyright © 2017-2018, Institut Pasteur, Paris, France"
+__credits__ = ["Cluster Buster", "Lyamovich"]
+__license__ = "Artistic"
+__maintainer__ = "Cluster Buster"
+__email__ = "vittore.scolari@pasteur.fr"
+__status__ = "Pre-Alpha"
+__version__ = version
