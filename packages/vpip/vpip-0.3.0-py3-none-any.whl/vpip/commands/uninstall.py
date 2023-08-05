9b440443c64ab66e320baa6deda7c32a3601541c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:09 +0000

    Add package vpip/0.3.0/vpip-0.3.0-py3-none-any.whl

---
+++ packages/vpip/vpip-0.3.0-py3-none-any.whl/vpip/commands/uninstall.py
@@ -0,0 +1,44 @@
+help = "Uninstall packages and delete from dependencies"
+options = [
+    {
+        "name": ["-g", "--global"],
+        "dest": "global_",
+        "action": "store_true",
+        "help": "Remove packages from the global folder"
+    },
+    {
+        "name": "PACKAGE",
+        "nargs": "+",
+        "help": "Package name"
+    }
+]
+
+def run(ns):
+    if ns.global_:
+        uninstall_global(ns.PACKAGE)
+    else:
+        uninstall_local(ns.PACKAGE)
+    
+def uninstall_global(packages):
+    """Uninstall global packages.
+    
+    :arg list[str] packages: Package names.
+    """
+    from .. import venv
+
+    for pkg in packages:
+        print("removing {}...".format(pkg))
+        venv.get_global_pkg_venv(pkg).destroy()
+    
+def uninstall_local(packages):
+    """Uninstall packages and remove from dependencies.
+    
+    :arg list[str] packages: Package names.
+    """
+    from .. import venv, pip_api, dependency
+
+    vv = venv.get_current_venv()
+    with vv.activate():
+        for pkg in packages:
+            pip_api.uninstall(pkg)
+        dependency.delete(packages)
