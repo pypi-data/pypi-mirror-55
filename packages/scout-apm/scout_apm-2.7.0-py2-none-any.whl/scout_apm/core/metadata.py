4dc9d3b7d0dc8f75c35827f6e53ecd27abb9a5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:44 +0000

    Add package scout-apm/2.7.0/scout_apm-2.7.0-py2-none-any.whl

---
+++ packages/scout-apm/scout_apm-2.7.0-py2-none-any.whl/scout_apm/core/metadata.py
@@ -0,0 +1,68 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import datetime as dt
+import logging
+import sys
+from os import getpid
+
+from scout_apm.core.commands import ApplicationEvent
+from scout_apm.core.config import scout_config
+from scout_apm.core.socket import CoreAgentSocket
+
+logger = logging.getLogger(__name__)
+
+
+class AppMetadata(object):
+    @classmethod
+    def report(cls):
+        event = ApplicationEvent(
+            event_type="scout.metadata",
+            event_value=cls.data(),
+            source="Pid: " + str(getpid()),
+            timestamp=dt.datetime.utcnow(),
+        )
+        CoreAgentSocket.instance().send(event)
+
+    @classmethod
+    def data(cls):
+        try:
+            data = {
+                "language": "python",
+                "language_version": "{}.{}.{}".format(*sys.version_info[:3]),
+                # Deprecated: (see #327)
+                "version": "{}.{}.{}".format(*sys.version_info[:3]),
+                "server_time": dt.datetime.utcnow().isoformat() + "Z",
+                "framework": scout_config.value("framework"),
+                "framework_version": scout_config.value("framework_version"),
+                "environment": "",
+                "app_server": scout_config.value("app_server"),
+                "hostname": scout_config.value("hostname"),
+                "database_engine": "",  # Detected
+                "database_adapter": "",  # Raw
+                "application_name": "",  # Environment.application_name,
+                "libraries": cls.get_python_packages_versions(),
+                "paas": "",
+                "application_root": scout_config.value("application_root"),
+                "scm_subdirectory": scout_config.value("scm_subdirectory"),
+                "git_sha": scout_config.value("revision_sha"),
+            }
+        except Exception as e:
+            logger.debug("Exception in AppMetadata: %r", e)
+            data = {}
+
+        return data
+
+    @classmethod
+    def get_python_packages_versions(cls):
+        try:
+            import pkg_resources
+        except ImportError:
+            return []
+
+        return list(
+            sorted(
+                (distribution.project_name, distribution.version)
+                for distribution in pkg_resources.working_set
+            )
+        )
---
+++ packages/scout-apm/scout_apm-2.7.0-py2-none-any.whl/scout_apm/instruments/elasticsearch.py
@@ -0,0 +1,194 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+from scout_apm.core.monkey import monkeypatch_method, unpatch_method
+from scout_apm.core.tracked_request import TrackedRequest
+
+logger = logging.getLogger(__name__)
+
+
+class Instrument(object):
+    installed = False
+
+    CLIENT_METHODS = [
+        "bulk",
+        "count",
+        "create",
+        "delete",
+        "delete_by_query",
+        "exists",
+        "exists_source",
+        "explain",
+        "field_caps",
+        "get",
+        "get_source",
+        "index",
+        "mget",
+        "msearch",
+        "msearch_template",
+        "mtermvectors",
+        "reindex",
+        "reindex_rethrottle",
+        "search",
+        "search_shards",
+        "search_template",
+        "termvectors",
+        "update",
+        "update_by_query",
+    ]
+
+    def installable(self):
+        try:
+            from elasticsearch import Elasticsearch  # noqa: F401
+            from elasticsearch import Transport  # noqa: F401
+        except ImportError:
+            logger.info("Unable to import for Elasticsearch instruments")
+            return False
+        if self.installed:
+            logger.warning("Elasticsearch Instruments are already installed.")
+            return False
+        return True
+
+    def install(self):
+        if not self.installable():
+            logger.info("Elasticsearch instruments are not installable. Skipping.")
+            return False
+
+        self.__class__.installed = True
+
+        self.instrument_client()
+        self.instrument_transport()
+
+    def uninstall(self):
+        if not self.installed:
+            logger.info("Elasticsearch instruments are not installed. Skipping.")
+            return False
+
+        self.__class__.installed = False
+
+        self.uninstrument_client()
+        self.uninstrument_transport()
+
+    def instrument_client(self):
+        try:
+            from elasticsearch import Elasticsearch  # noqa: F401
+        except ImportError:
+            logger.info(
+                "Unable to import for Elasticsearch Client instruments. "
+                "Instrument install failed."
+            )
+            return False
+
+        for method_str in self.__class__.CLIENT_METHODS:
+            try:
+                code_str = """\
+@monkeypatch_method(Elasticsearch)
+def {method_str}(original, self, *args, **kwargs):
+    tracked_request = TrackedRequest.instance()
+    index = kwargs.get('index', 'Unknown')
+    if isinstance(index, (list, tuple)):
+        index = ','.join(index)
+    index = index.title()
+    name = '/'.join(['Elasticsearch', index, '{camel_name}'])
+    tracked_request.start_span(operation=name, ignore_children=True)
+
+
+    try:
+        return original(*args, **kwargs)
+    finally:
+        tracked_request.stop_span()
+""".format(
+                    method_str=method_str,
+                    camel_name="".join(c.title() for c in method_str.split("_")),
+                )
+
+                exec(code_str)
+                logger.info("Instrumented Elasticsearch Elasticsearch.%s", method_str)
+
+            except Exception as e:
+                logger.warning(
+                    "Unable to instrument for Elasticsearch Elasticsearch.%s: %r",
+                    method_str,
+                    e,
+                )
+                return False
+        return True
+
+    def uninstrument_client(self):
+        from elasticsearch import Elasticsearch
+
+        for method_str in self.__class__.CLIENT_METHODS:
+            unpatch_method(Elasticsearch, method_str)
+
+    def instrument_transport(self):
+        try:
+            from elasticsearch import Transport
+
+            def _sanitize_name(name):
+                try:
+                    op = name.split("/")[-1]
+                    op = op[1:]  # chop leading '_' from op
+                    allowed_names = [
+                        "bench",
+                        "bulk",
+                        "count",
+                        "exists",
+                        "explain",
+                        "field_stats",
+                        "health",
+                        "mget",
+                        "mlt",
+                        "mpercolate",
+                        "msearch",
+                        "mtermvectors",
+                        "percolate",
+                        "query",
+                        "scroll",
+                        "search_shards",
+                        "source",
+                        "suggest",
+                        "template",
+                        "termvectors",
+                        "update",
+                        "search",
+                    ]
+
+                    if op in allowed_names:
+                        return op.title()
+                    return "Unknown"
+                except Exception:
+                    return "Unknown"
+
+            @monkeypatch_method(Transport)
+            def perform_request(original, self, *args, **kwargs):
+                try:
+                    op = _sanitize_name(args[1])
+                except IndexError:
+                    op = "Unknown"
+
+                tracked_request = TrackedRequest.instance()
+                tracked_request.start_span(
+                    operation="Elasticsearch/{}".format(op), ignore_children=True
+                )
+
+                try:
+                    return original(*args, **kwargs)
+                finally:
+                    tracked_request.stop_span()
+
+            logger.info("Instrumented Elasticsearch Transport")
+
+        except Exception as e:
+            logger.warning(
+                "Unable to instrument for Elasticsearch Transport.perform_request: %r",
+                e,
+            )
+            return False
+        return True
+
+    def uninstrument_transport(self):
+        from elasticsearch import Transport
+
+        unpatch_method(Transport, "perform_request")
