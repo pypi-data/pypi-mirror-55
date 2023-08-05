1a4903119ca7ceef04cb891443f84e5178f0e69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:47 +0000

    Add package gwemopt/0.0.59/gwemopt-0.0.59.tar.gz

---
+++ packages/gwemopt/gwemopt-0.0.59.tar.gz/gwemopt-0.0.59/gwemopt/catalog.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import copy
+import h5py
+import numpy as np
+import healpy as hp
+
+from scipy.stats import norm
+from scipy.special import gammaincinv
+
+from astroquery.vizier import Vizier
+
+from astropy.table import Table
+from astropy.io import ascii
+from astropy.cosmology import WMAP9 as cosmo
+from astropy.table import Column
+import astropy.units as u
+
+# Unset row limits when querying Vizier
+Vizier.ROW_LIMIT = -1
+
+
+def get_catalog(params, map_struct):
+    if not os.path.isdir(params["catalogDir"]):
+        os.makedirs(params["catalogDir"])
+
+    catalogFile = os.path.join(params["catalogDir"],
+                               "%s.hdf5" % params["galaxy_catalog"])
+
+    """AB Magnitude zero point."""
+    MAB0 = -2.5 * np.log10(3631.e-23)
+    pc_cm = 3.08568025e18
+    const = 4. * np.pi * (10. * pc_cm)**2.
+
+    if params["galaxy_catalog"] == "2MRS":
+        if not os.path.isfile(catalogFile):
+            import astropy.constants as c
+
+            cat, = Vizier.get_catalogs('J/ApJS/199/26/table3')
+
+            ra, dec = cat["RAJ2000"], cat["DEJ2000"]
+            cz = cat["cz"]
+            magk = cat["Ktmag"]
+
+            z = (u.Quantity(cat['cz']) / c.c).to(u.dimensionless_unscaled)
+
+            completeness = 0.5
+            alpha = -1.0
+            MK_star = -23.55
+            MK_max = MK_star + 2.5 * np.log10(gammaincinv(alpha + 2,
+                                                          completeness))
+            MK = magk - cosmo.distmod(z)
+            idx = (z > 0) & (MK < MK_max)
+
+            ra, dec = ra[idx], dec[idx]
+            z = z[idx]
+            magk = magk[idx]
+
+            distmpc = cosmo.luminosity_distance(z).to('Mpc').value
+
+            with h5py.File(catalogFile, 'w') as f:
+                f.create_dataset('ra', data=ra)
+                f.create_dataset('dec', data=dec)
+                f.create_dataset('z', data=z)
+                f.create_dataset('magk', data=magk)
+                f.create_dataset('distmpc', data=distmpc)
+
+        else:
+            with h5py.File(catalogFile, 'r') as f:
+                ra, dec = f['ra'][:], f['dec'][:]
+                z = f['z'][:]
+                magk = f['magk'][:]
+                distmpc = f['distmpc'][:]
+        r = distmpc * 1.0
+        mag = magk * 1.0
+
+    elif params["galaxy_catalog"] == "GLADE":
+        if not os.path.isfile(catalogFile):
+            cat, = Vizier.get_catalogs('VII/281/glade2')
+
+            ra, dec = cat["RAJ2000"], cat["DEJ2000"]
+            distmpc, z = cat["Dist"], cat["z"]
+            magb, magk = cat["Bmag"], cat["Kmag"]
+            # Keep track of galaxy identifier
+            GWGC, PGC, HyperLEDA = cat["GWGC"], cat["PGC"], cat["HyperLEDA"]
+            _2MASS, SDSS = cat["_2MASS"], cat["SDSS-DR12"]
+
+            idx = np.where(distmpc >= 0)[0]
+            ra, dec = ra[idx], dec[idx]
+            distmpc, z = distmpc[idx], z[idx]
+            magb, magk = magb[idx], magk[idx]
+            GWGC, PGC, HyperLEDA = GWGC[idx], PGC[idx], HyperLEDA[idx]
+            _2MASS, SDSS = _2MASS[idx], SDSS[idx]
+
+            with h5py.File(catalogFile, 'w') as f:
+                f.create_dataset('ra', data=ra)
+                f.create_dataset('dec', data=dec)
+                f.create_dataset('distmpc', data=distmpc)
+                f.create_dataset('magb', data=magb)
+                f.create_dataset('magk', data=magk)
+                f.create_dataset('z', data=z)
+                # Add galaxy identifier
+                f.create_dataset('GWGC', data=GWGC)
+                f.create_dataset('PGC', data=PGC)
+                f.create_dataset('HyperLEDA', data=HyperLEDA)
+                f.create_dataset('2MASS', data=_2MASS)
+                f.create_dataset('SDSS', data=SDSS)
+
+        else:
+            with h5py.File(catalogFile, 'r') as f:
+                ra, dec = f['ra'][:], f['dec'][:]
+                distmpc, z = f['distmpc'][:], f['z'][:]
+                magb, magk = f['magb'][:], f['magk'][:]
+                GWGC, PGC, _2MASS = f['GWGC'][:], f['PGC'][:], f['2MASS'][:]
+                HyperLEDA, SDSS = f['HyperLEDA'][:], f['SDSS'][:]
+                # Convert bytestring to unicode
+                GWGC = GWGC.astype('U')
+                PGC = PGC.astype('U')
+                HyperLEDA = HyperLEDA.astype('U')
+                _2MASS = _2MASS.astype('U')
+                SDSS = SDSS.astype('U')
+
+        # Keep only galaxies with finite B mag when using it in the grade
+        if params["galaxy_grade"] == "S":
+            idx = np.where(~np.isnan(magb))[0]
+            ra, dec, distmpc = ra[idx], dec[idx], distmpc[idx]
+            magb, magk = magb[idx], magk[idx]
+            GWGC, PGC, HyperLEDA = GWGC[idx], PGC[idx], HyperLEDA[idx]
+            _2MASS, SDSS = _2MASS[idx], SDSS[idx]
+
+        r = distmpc * 1.0
+        mag = magb * 1.0
+
+    elif params["galaxy_catalog"] == "CLU":
+        if not os.path.isfile(catalogFile):
+            raise ValueError("Please add %s." % catalogFile)
+
+        t = Table.read(catalogFile)
+        name = t["name"]
+        ra, dec = t["ra"], t["dec"]
+        sfr_fuv, mstar = t["sfr_fuv"], t["mstar"]
+        distmpc, magb = t["distmpc"], t["magb"]
+        a, b2a, pa = t["a"], t["b2a"], t["pa"]
+        btc = t["btc"]
+
+        idx = np.where(distmpc >= 0)[0]
+        ra, dec = ra[idx], dec[idx]
+        sfr_fuv, mstar = sfr_fuv[idx], mstar[idx]
+        distmpc, magb = distmpc[idx], magb[idx]
+        a, b2a, pa = a[idx], b2a[idx], pa[idx]
+        btc = btc[idx]
+
+        idx = np.where(~np.isnan(magb))[0]
+        ra, dec = ra[idx], dec[idx]
+        sfr_fuv, mstar = sfr_fuv[idx], mstar[idx]
+        distmpc, magb = distmpc[idx], magb[idx]
+        a, b2a, pa = a[idx], b2a[idx], pa[idx]
+        btc = btc[idx]
+
+        z = -1*np.ones(distmpc.shape)
+        r = distmpc * 1.0
+        mag = magb * 1.0
+
+    n, cl = params["powerlaw_n"], params["powerlaw_cl"]
+    dist_exp = params["powerlaw_dist_exp"]
+
+    prob_scaled = copy.deepcopy(map_struct["prob"])
+    prob_sorted = np.sort(prob_scaled)[::-1]
+    prob_indexes = np.argsort(prob_scaled)[::-1]
+    prob_cumsum = np.cumsum(prob_sorted)
+    index = np.argmin(np.abs(prob_cumsum - cl)) + 1
+    prob_scaled[prob_indexes[index:]] = 0.0
+    prob_scaled = prob_scaled**n
+
+    theta = 0.5 * np.pi - dec * 2 * np.pi / 360.0
+    phi = ra * 2 * np.pi / 360.0
+    ipix = hp.ang2pix(map_struct["nside"], ra, dec, lonlat=True)
+
+    if "distnorm" in map_struct:
+        if map_struct["distnorm"] is not None:
+            
+            #creat an mask to cut at 3 sigma in distance
+            mask = np.zeros(len(r))
+
+            condition_indexer = np.where( (r < (map_struct["distmu"][ipix] + (3*map_struct["distsigma"][ipix]))) & (r > (map_struct["distmu"][ipix] - (3*map_struct["distsigma"][ipix])) )) 
+            mask[condition_indexer] = 1
+
+            Sloc = prob_scaled[ipix] * (map_struct["distnorm"][ipix] *
+                                        norm(map_struct["distmu"][ipix],
+                                        map_struct["distsigma"][ipix]).pdf(r))**params["powerlaw_dist_exp"] / map_struct["pixarea"]
+    
+            #multiplie the Sloc by 1 or 0 according to the 3 sigma condistion
+            Sloc = Sloc*mask
+            idx = np.where(condition_indexer)[0]
+        else:
+            Sloc = copy.copy(prob_scaled[ipix])
+            idx = np.arange(len(r)).astype(int)
+    else:
+        Sloc = copy.copy(prob_scaled[ipix])
+        idx = np.arange(len(r)).astype(int)
+
+    # this happens when we are using a tiny catalog...
+    if len(idx) == 0:
+        idx = np.arange(len(r)).astype(int)
+    if np.all(Sloc == 0.0):
+        Sloc[:] = 1.0
+
+    L_nu = const * 10.**((mag + MAB0)/(-2.5))
+    idx2 = np.where(~np.isfinite(L_nu))[0]
+    L_nu[idx2] = 0.0
+    L_nu = L_nu / np.nanmax(L_nu[idx])
+    L_nu = L_nu**params["catalog_n"]
+    L_nu[L_nu < 0.001] = 0.001
+    L_nu[L_nu > 1.0] = 1.0
+    Slum = L_nu / np.sum(L_nu)
+
+    mlim, M_KNmin, M_KNmax = 22, -17, -12
+    L_KNmin = const * 10.**((M_KNmin + MAB0)/(-2.5))
+    L_KNmax = const * 10.**((M_KNmax + MAB0)/(-2.5))
+
+    Llim = 4. * np.pi * (r * 1e6 * pc_cm)**2. * 10.**((mlim + MAB0)/(-2.5))
+    Sdet = (L_KNmax-Llim)/(L_KNmax-L_KNmin)
+    Sdet[Sdet < 0.01] = 0.01
+    Sdet[Sdet > 1.0] = 1.0
+
+    # Set nan values to zero
+    Sloc[np.isnan(Sloc)] = 0
+    Slum[np.isnan(Slum)] = 0
+
+    S = Sloc*Slum*Sdet
+
+    prob = np.zeros(map_struct["prob"].shape)
+    if params["galaxy_grade"] == "Sloc":     
+        for j in range(len(ipix)):
+            prob[ipix[j]] += Sloc[j]
+        grade = Sloc
+    elif params["galaxy_grade"] == "S":
+        for j in range(len(ipix)):
+            prob[ipix[j]] += S[j]
+        grade = S
+
+    #prob[prob==0] = 1e-10
+    prob = prob / np.sum(prob)
+
+    map_struct['prob_catalog'] = prob
+    if params["doUseCatalog"]:
+        map_struct['prob'] = prob
+
+    idx = np.where(~np.isnan(grade))[0]
+    grade = grade[idx]
+    ra, dec, Sloc, S = ra[idx], dec[idx], Sloc[idx], S[idx]
+    distmpc, z = distmpc[idx], z[idx]
+    if params["galaxy_catalog"] == "GLADE":
+        GWGC, PGC, HyperLEDA = GWGC[idx], PGC[idx], HyperLEDA[idx]
+        _2MASS, SDSS = _2MASS[idx], SDSS[idx]
+    
+    """
+    Sthresh = np.max(grade)*0.01
+    idx = np.where(grade >= Sthresh)[0]
+    grade = grade[idx]
+    ra, dec, Sloc, S = ra[idx], dec[idx], Sloc[idx], S[idx]
+    distmpc, z = distmpc[idx], z[idx]
+    if params["galaxy_catalog"] == "GLADE":
+        GWGC, PGC, HyperLEDA = GWGC[idx], PGC[idx], HyperLEDA[idx]
+        _2MASS, SDSS = _2MASS[idx], SDSS[idx]
+    """
+    
+    idx = np.argsort(grade)[::-1]
+    grade = grade[idx]
+
+    ra, dec, Sloc, S = ra[idx], dec[idx], Sloc[idx], S[idx]
+    distmpc, z = distmpc[idx], z[idx]
+    if params["galaxy_catalog"] == "GLADE":
+        GWGC, PGC, HyperLEDA = GWGC[idx], PGC[idx], HyperLEDA[idx]
+        _2MASS, SDSS = _2MASS[idx], SDSS[idx]
+
+    # Keep only galaxies within 3sigma in distance
+    mask = Sloc > 0
+    ra, dec, Sloc, S = ra[mask], dec[mask], Sloc[mask], S[mask]
+    distmpc, z = distmpc[mask], z[mask]
+    if params["galaxy_catalog"] == "GLADE":
+        GWGC, PGC, HyperLEDA = GWGC[mask], PGC[mask], HyperLEDA[mask]
+        _2MASS, SDSS = _2MASS[mask], SDSS[mask]
+
+    if len(ra) > 2000:
+        print('Cutting catalog to top 2000 galaxies...')
+        idx = np.arange(2000).astype(int)
+        ra, dec, Sloc, S = ra[idx], dec[idx], Sloc[idx], S[idx]
+        distmpc, z = distmpc[idx], z[idx]
+        if params["galaxy_catalog"] == "GLADE":
+            GWGC, PGC, HyperLEDA = GWGC[idx], PGC[idx], HyperLEDA[idx]
+            _2MASS, SDSS = _2MASS[idx], SDSS[idx]
+
+    # now normalize the distributions
+    S = S / np.sum(S)
+    Sloc = Sloc / np.sum(Sloc)
+    
+    catalog_struct = {}
+    catalog_struct["ra"] = ra
+    catalog_struct["dec"] = dec
+    catalog_struct["Sloc"] = Sloc
+    catalog_struct["S"] = S
+
+    if params["writeCatalog"]:
+        catalogfile = os.path.join(params["outputDir"], 'catalog.csv')
+        fid = open(catalogfile, 'w')
+        cnt = 1
+        if params["galaxy_catalog"] == "GLADE":
+            fid.write("id, RAJ2000, DEJ2000, Sloc, S, Dist, z, GWGC, PGC, HyperLEDA, 2MASS, SDSS\n")
+            for a, b, c, d, e, f, g, h, i, j, k in zip(ra, dec, Sloc, S, distmpc, z, GWGC, PGC, HyperLEDA, _2MASS, SDSS):
+                fid.write("%d, %.5f, %.5f, %.5e, %.5e, %.4f, %.4f, %s, %s, %s, %s, %s\n" % (cnt, a, b, c, d, e, f, g, h, i, j, k))
+                cnt = cnt + 1
+        else:
+            fid.write("id, RAJ2000, DEJ2000, Sloc, S, Dist, z\n")
+            for a, b, c, d, e, f in zip(ra, dec, Sloc, S, distmpc, z):
+                fid.write("%d, %.5f, %.5f, %.5e, %.5e, %.4f, %.4f\n" % (cnt, a, b, c, d, e, f))
+                cnt = cnt + 1
+
+        fid.close()
+
+    return map_struct, catalog_struct
---
+++ packages/gwemopt/gwemopt-0.0.59.tar.gz/gwemopt-0.0.59/gwemopt/efficiency.py
@@ -0,0 +1,132 @@
+
+import os, sys
+import numpy as np
+import healpy as hp
+from astropy.time import Time
+import matplotlib
+#matplotlib.rc('text', usetex=True)
+matplotlib.use('Agg')
+matplotlib.rcParams.update({'font.size': 16})
+matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
+import matplotlib.pyplot as plt
+
+import gwemopt.utils
+
+def compute_efficiency(params, map_struct, lightcurve_struct, coverage_struct):
+
+    nside = params["nside"]
+    Ninj = params["Ninj"]
+    Ndet = params["Ndet"]
+    gpstime = params["gpstime"]
+    mjd_inj = Time(gpstime, format='gps', scale='utc').mjd
+    #FOV_r = np.sqrt(float(params['FOV'])/np.pi)
+
+    if params["doCatalog"]:
+        prob_data_sorted = np.sort(map_struct["prob_catalog"])[::-1]
+        prob_data_indexes = np.argsort(map_struct["prob_catalog"])[::-1]
+    else:
+        prob_data_sorted = np.sort(map_struct["prob"])[::-1]
+        prob_data_indexes = np.argsort(map_struct["prob"])[::-1]
+    prob_data_cumsum = np.cumsum(prob_data_sorted)
+    rand_values = np.random.rand(Ninj,)
+    dists = np.logspace(-1,3,1000)
+    #dists = np.logspace(-1,np.log10(300),100)
+    ndetections = np.zeros((len(dists),))
+
+    ras = []
+    decs = []
+
+    for ii in range(Ninj):
+        # this returns the index of the value in cumsum of probablity closest to a random value
+        ipix = np.argmin(np.abs(prob_data_cumsum-rand_values[ii])) 
+        # the point corresponding to that
+        ra_inj = map_struct["ra"][prob_data_indexes][ipix]
+        dec_inj = map_struct["dec"][prob_data_indexes][ipix]
+
+        ras.append(ra_inj)
+        decs.append(dec_inj)
+
+        # THE SKY-LOCATION AS A HEALPIXEL ID
+        pinpoint = hp.ang2pix(nside, theta=ra_inj, phi=dec_inj, lonlat=True)
+
+        #idxs = np.where(np.sqrt((coverage_struct["data"][:,0]-ra_inj)**2 + (coverage_struct["data"][:,1]-dec_inj)**2) <= FOV_r)[0]
+        #idxs_ra = np.where(np.abs(coverage_struct["data"][:,0]-ra_inj)<=coverage_struct["FOV"]/2.0)[0]
+        #idxs_dec = np.where(np.abs(coverage_struct["data"][:,1]-dec_inj)<=coverage_struct["FOV"]/2.0)[0]
+        #idxs = np.intersect1d(idxs_ra,idxs_dec)
+
+        idxs = []
+        for jj in range(len(coverage_struct["ipix"])):
+            expPixels = coverage_struct["ipix"][jj]
+            if pinpoint in expPixels:
+                idxs.append(jj)
+        if len(idxs) == 0:
+            continue
+
+        mjds = coverage_struct["data"][idxs,2]
+        mags = coverage_struct["data"][idxs,3]
+        filts = coverage_struct["filters"][idxs]
+
+        detections = np.zeros((len(dists),))
+
+        for mjd, mag, filt in zip(mjds,mags,filts):
+            lightcurve_t = lightcurve_struct["t"] + mjd_inj
+            lightcurve_mag = lightcurve_struct[filt]
+
+            lightcurve_mag_interp = np.interp(mjd,lightcurve_t,lightcurve_mag)
+            dist_threshold = (10**(((mag-lightcurve_mag_interp)/5.0)+1.0))/1e6
+
+            idxs_detections = np.where(dists <= dist_threshold)[0]
+            detections[idxs_detections] = detections[idxs_detections] + 1
+        idxs_detections = np.where(detections >= Ndet)[0]
+        ndetections[idxs_detections] = ndetections[idxs_detections] + 1
+    efficiency = ndetections / Ninj
+    efficiency_struct = {}
+    efficiency_struct["ra"] = np.array(ras)
+    efficiency_struct["dec"] = np.array(decs)
+    efficiency_struct["efficiency"] = efficiency
+    efficiency_struct["distances"] = dists
+    save_efficiency_data(params, efficiency_struct, lightcurve_struct)
+    efficiency_metric = calculate_efficiency_metric(params, efficiency_struct)
+    save_efficiency_metric(params, os.path.join(params["outputDir"],"efficiency.txt"), efficiency_metric, lightcurve_struct)
+    return efficiency_struct
+
+
+def save_efficiency_data(params, efficiency_struct, lightcurve_struct):
+    for i in range(0, len(efficiency_struct["distances"])):
+        dist = efficiency_struct["distances"][i]
+        eff = efficiency_struct["efficiency"][i]
+        filename = os.path.join(params["outputDir"],'efficiency_' + lightcurve_struct['name'] + '.txt')
+        if os.path.exists(filename):
+            append_write = 'a'
+            efficiency_file = open(filename, append_write)
+        else:
+            append_write = 'w'
+            efficiency_file = open(filename, append_write)
+            efficiency_file.write("Distance" + "\t" + "efficiency\n")
+        efficiency_file.write(str(dist) + "\t" + str(eff) + "\n")
+    efficiency_file.close()
+
+
+def calculate_efficiency_metric(params, efficiency_struct):
+    dist_sum = 0
+    weighted_sum = 0
+    for i in range(0, len(efficiency_struct["distances"])):
+        dist = efficiency_struct["distances"][i]
+        eff = efficiency_struct["efficiency"][i]
+        dist_sum += dist * dist
+        weighted_sum += dist * dist * eff
+    metric = weighted_sum / dist_sum
+    uncertainty = np.sqrt(metric * (1 - metric) / params["Ninj"])
+    return (metric, uncertainty)
+
+
+def save_efficiency_metric(params, efficiency_filename, efficiency_metric, lightcurve_struct):
+    if os.path.exists(efficiency_filename):
+        append_write = 'a'
+        efficiency_file = open(efficiency_filename, append_write)
+    else:
+        append_write = 'w'
+        efficiency_file = open(efficiency_filename, append_write)
+        efficiency_file.write("tilesType\t" + "timeallocationType\t" + "scheduleType\t" + "Ntiles\t" + "efficiencyMetric\t" + "efficiencyMetric_err\t" + "injection\n")
+    efficiency_file.write(params["tilesType"] + "\t" + params["timeallocationType"] + "\t" +  params["scheduleType"] + "\t" + str(params["Ntiles"]) + "\t" + str(efficiency_metric[0]) + "\t" + str(efficiency_metric[1]) + "\t" + lightcurve_struct["name"] + "\n")
+    efficiency_file.close()
