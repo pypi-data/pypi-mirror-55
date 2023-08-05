ff8f3685ab0dbffccffdf7c1c3f3ea8e66ee9048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package scout-apm/2.7.0/scout_apm-2.7.0-cp34-cp34m-manylinux1_x86_64.whl

---
+++ packages/scout-apm/scout_apm-2.7.0-cp34-cp34m-manylinux1_x86_64.whl/scout_apm/core/stacktracer.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from scout_apm.core.monkey import CallableProxy, monkeypatch_method
+from scout_apm.core.tracked_request import TrackedRequest
+
+
+def trace_method(cls, method_name=None):
+    def decorator(info_func):
+        method_to_patch = method_name or info_func.__name__
+
+        @monkeypatch_method(cls, method_to_patch)
+        def tracing_method(original, self, *args, **kwargs):
+            entry_type, detail = info_func(self, *args, **kwargs)
+
+            operation = entry_type
+            if detail["name"] is not None:
+                operation = operation + "/" + detail["name"]
+
+            tracked_request = TrackedRequest.instance()
+            span = tracked_request.start_span(operation=operation)
+            for key, value in detail.items():
+                span.tag(key, value)
+
+            try:
+                return original(*args, **kwargs)
+            finally:
+                tracked_request.stop_span()
+
+        return tracing_method
+
+    return decorator
+
+
+def trace_function(func, info):
+    try:
+
+        def tracing_function(original, *args, **kwargs):
+            if callable(info):
+                entry_type, detail = info(*args, **kwargs)
+            else:
+                entry_type, detail = info
+
+            operation = entry_type
+            if detail["name"] is not None:
+                operation = operation + "/" + detail["name"]
+
+            tracked_request = TrackedRequest.instance()
+            span = tracked_request.start_span(operation=operation)
+
+            for key in detail:
+                span.tag(key, detail[key])
+
+            try:
+                return original(*args, **kwargs)
+            finally:
+                TrackedRequest.instance().stop_span()
+
+        return CallableProxy(func, tracing_function)
+    except Exception:
+        # If we can't wrap for any reason, just return the original
+        return func
---
+++ packages/scout-apm/scout_apm-2.7.0-cp34-cp34m-manylinux1_x86_64.whl/scout_apm/flask/sqlalchemy.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from flask_sqlalchemy import SQLAlchemy
+
+import scout_apm.sqlalchemy
+from scout_apm.core.monkey import monkeypatch_method
+
+
+def instrument_sqlalchemy(db):
+    @monkeypatch_method(SQLAlchemy)
+    def get_engine(original, self, *args, **kwargs):
+        engine = original(*args, **kwargs)
+        scout_apm.sqlalchemy.instrument_sqlalchemy(engine)
+        return engine
