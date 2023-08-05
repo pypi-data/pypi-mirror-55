8e5df29392a854f5639cc0865cd638d53b6a760e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package xsnumpy/0.1/xsnumpy-0.1.tar.gz

---
+++ packages/xsnumpy/xsnumpy-0.1.tar.gz/xsnumpy-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='xsnumpy',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Sagar gupta',
+      license='MIT',
+      packages=['xsnumpy'],
+      zip_safe=False)
