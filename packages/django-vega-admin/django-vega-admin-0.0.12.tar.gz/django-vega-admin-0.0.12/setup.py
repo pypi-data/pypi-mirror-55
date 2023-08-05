ec43dc163e71392fd8f24b1686759b65a99e22ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:12 +0000

    Add package django-vega-admin/0.0.12/django-vega-admin-0.0.12.tar.gz

---
+++ packages/django-vega-admin/django-vega-admin-0.0.12.tar.gz/django-vega-admin-0.0.12/setup.py
@@ -0,0 +1,39 @@
+"""
+Setup.py for django-vega-admin
+"""
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
+setup(
+    name="django-vega-admin",
+    version=__import__("vega_admin").__version__,
+    description="Simple and fast automated CRUD for any Django model",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="Kelvin Jayanoris",
+    author_email="kelvin@jayanoris.com",
+    url="https://github.com/moshthepitt/django-vega-admin",
+    packages=find_packages(exclude=["docs", "tests"]),
+    install_requires=[
+        "Django >=2.1.10",
+        "django-crispy-forms",
+        "django-braces",
+        "django-filter",
+        "django-tables2",
+        "tablib",
+        "pyyaml>=4.2b1",  # fixes security vulnerability
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Framework :: Django",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+    ],
+)
