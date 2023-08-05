8d21756171f1848adbc5e4f5b1cea33e51b1c40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:00 +0000

    Add package parsevasp/0.4.3/parsevasp-0.4.3.tar.gz

---
+++ packages/parsevasp/parsevasp-0.4.3.tar.gz/parsevasp-0.4.3/README.md
@@ -0,0 +1,5 @@
+A parser for VASP using lxml or fallback elementTree.
+
+TODO:
+
+- test the extraction of dielectric functions for qp and bse etc.
---
+++ packages/parsevasp/parsevasp-0.4.3.tar.gz/parsevasp-0.4.3/parsevasp/__init__.py
@@ -0,0 +1,8 @@
+"""A parser for VASP."""
+
+__version__ = '0.4.2'
+
+import parsevasp.vasprun
+import parsevasp.poscar
+import parsevasp.incar
+import parsevasp.kpoints
