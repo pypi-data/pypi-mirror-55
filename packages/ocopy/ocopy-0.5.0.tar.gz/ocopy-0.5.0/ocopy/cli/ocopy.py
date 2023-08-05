ba253277065f63bb0211a9cc07e1e40bfdacc432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:10 +0000

    Add package ocopy/0.5.0/ocopy-0.5.0.tar.gz

---
+++ packages/ocopy/ocopy-0.5.0.tar.gz/ocopy-0.5.0/ocopy/cli/ocopy.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import shutil
+import sys
+import time
+from pathlib import Path
+from typing import List
+
+import click
+
+from ocopy.copy import CopyJob
+from ocopy.utils import folder_size, get_mount
+
+
+@click.command()
+@click.version_option(prog_name="o/COPY")
+@click.option(
+    "--overwrite/--dont-overwrite",
+    help="Allow overwriting of destination files (defaults to --dont-overwrite)",
+    default=False,
+)
+@click.option(
+    "--verify/--dont-verify",
+    help="Verify copy by re-calculating the xxHash of the source and all destinations (defaults to --verify)",
+    default=True,
+)
+@click.option(
+    "--skip-existing/--dont-skip",
+    help="Skip existing files if they have the same size and modification time as the source (defaults to --dont-skip)",
+    default=False,
+)
+@click.argument("source", nargs=1, type=click.Path(exists=True, readable=True, file_okay=False, dir_okay=True))
+@click.argument(
+    "destinations", nargs=-1, type=click.Path(exists=True, readable=True, writable=True, file_okay=False, dir_okay=True)
+)
+def cli(overwrite: bool, verify: bool, skip_existing: bool, source: str, destinations: List[str]):
+    """
+    o/COPY by OTTOMATIC
+
+    Copy SOURCE directory to DESTINATIONS
+    """
+    size = folder_size(source)
+    for destination in destinations:
+        if shutil.disk_usage(destination).free < size:
+            click.secho(f"{destination} does not have enough free space.", fg="red")
+            sys.exit(1)
+    click.secho(f"Copying {source} to {', '.join(destinations)}", fg="green")
+
+    destinations = [Path(d) for d in destinations]
+    if len(destinations) != len({get_mount(d) for d in destinations}):
+        click.secho(f"Destinations should all be on different drives.", fg="yellow")
+
+    start = time.time()
+    job = CopyJob(Path(source), destinations, overwrite=overwrite, verify=verify, skip_existing=skip_existing)
+
+    with click.progressbar(job.progress, length=100, item_show_func=lambda name: name) as progress:
+        for _ in progress:
+            pass
+
+    stop = time.time()
+    click.echo(f"\n{size / 1000 / 1000 / (stop - start):.2f} MB/s")
+
+
+if __name__ == "__main__":
+    cli()  # pragma: no cover
---
+++ packages/ocopy/ocopy-0.5.0.tar.gz/ocopy-0.5.0/ocopy/copy.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python3
+import datetime
+from concurrent.futures import as_completed
+from concurrent.futures.thread import ThreadPoolExecutor
+from dataclasses import dataclass
+from pathlib import Path
+from queue import Queue
+from shutil import copystat
+from threading import Thread, Event, currentThread
+from time import sleep
+from typing import List
+
+import xxhash
+
+from ocopy.file_info import FileInfo
+from ocopy.hash import multi_xxhash_check, write_xxhash_summary
+from ocopy.mhl import write_mhl
+from ocopy.progress import get_progress_queue
+from ocopy.utils import threaded, folder_size
+
+
+class CopyTreeError(OSError):
+    """
+    Raised by recursive copytree
+    """
+    pass
+
+
+class VerificationError(OSError):
+    """
+    Raised if checksums for copied files do not match
+    """
+    pass
+
+
+@dataclass
+class ErrorListEntry:
+    """
+    Used to store errors while continuing the recursive copytree
+    """
+    source: Path
+    destinations: List[Path]
+    error_message: str
+
+
+def copy(src_file: Path, destinations: List[Path], chunk_size: int = 1024 * 1024) -> str:
+    """
+    Copies one file to multiple destinations chunk by chunk while calculating the checksum
+    """
+    queues = [Queue(maxsize=10) for _ in destinations]
+
+    def writer(queue: Queue, file_path: Path):
+        with open(file_path, "wb") as dest_f:
+            while True:
+                write_chunk = queue.get()
+                if not write_chunk:
+                    queue.task_done()
+                    break
+                dest_f.write(write_chunk)
+                queue.task_done()
+
+    with ThreadPoolExecutor(max_workers=len(destinations)) as executor:
+        futures = [executor.submit(writer, queues[i], d) for i, d in enumerate(destinations)]
+
+        x = xxhash.xxh64()
+        progress_queue = get_progress_queue()
+
+        with open(src_file, "rb") as f:
+            while True:
+                chunk = f.read(chunk_size)
+                for q in queues:
+                    q.put(chunk)
+
+                if not chunk:
+                    break
+
+                x.update(chunk)
+                if progress_queue:
+                    progress_queue.put((src_file, len(chunk)))
+
+        for future in as_completed(futures):
+            future.result()
+
+    for q in queues:
+        q.join()
+
+    for d in destinations:
+        copystat(src_file, d)
+
+    return x.hexdigest()
+
+
+def copytree(
+    source: Path, destinations: List[Path], overwrite=False, verify=True, skip_existing=False
+) -> List[FileInfo]:
+    """Recursively copy a source directory to multiple destinations"""
+
+    for d in destinations:
+        d.mkdir(parents=True, exist_ok=True)
+
+    ignored_files = [".DS_Store"]
+
+    file_infos = []
+    errors = []
+
+    for src_path in source.glob("*"):
+        if _is_cancelled():
+            break
+
+        if src_path.name in ignored_files:
+            continue
+        dst_paths = [d / src_path.name for d in destinations]
+        try:
+            if src_path.is_dir():
+                file_infos += copytree(src_path, dst_paths, overwrite, verify, skip_existing)
+            else:
+                file_hash = verified_copy(src_path, dst_paths, overwrite, verify, skip_existing)
+                stat = src_path.stat()
+                file_infos.append(FileInfo(src_path, file_hash, stat.st_size, stat.st_mtime))
+
+        # catch the Error from the recursive copytree so that we can continue with other files
+        except CopyTreeError as err:
+            errors.extend(err.args[0])
+        except OSError as why:
+            errors.append(ErrorListEntry(src_path, dst_paths, str(why)))
+
+    for d in destinations:
+        copystat(source, d)
+
+    if errors:
+        raise CopyTreeError(errors)
+
+    return file_infos
+
+
+def verified_copy(src_file: Path, destinations: List[Path], overwrite=False, verify=True, skip_existing=False) -> str:
+    """
+    Copies one file to multiple destinations.
+
+    - Creates temporary file on destinations
+    - Calculates checksum of source during copy
+    - Re-Reads source and all destinations and make sure all checksums match
+    """
+    to_do_destinations = destinations
+
+    for d in destinations:
+        if d.exists():
+            if (
+                skip_existing
+                and src_file.stat().st_size == d.stat().st_size
+                and round(src_file.stat().st_mtime) == round(d.stat().st_mtime)
+            ):
+                to_do_destinations.remove(d)
+            elif overwrite:
+                d.unlink()
+            else:
+                raise FileExistsError(f"{d.as_posix()} exists!")
+
+    if to_do_destinations:
+        tmp_destinations = [d.with_name(d.name + ".copy_in_progress") for d in to_do_destinations]
+        try:
+            file_hash = copy(src_file, tmp_destinations)
+
+            # Verify source and destinations
+            if not verify or file_hash == multi_xxhash_check(tmp_destinations + [src_file]):
+                for tmp in tmp_destinations:
+                    tmp.rename(tmp.with_name(tmp.name.replace(".copy_in_progress", "")))
+                return file_hash
+            else:
+                raise VerificationError(f"Verification failed for {src_file}")
+        finally:
+            for tmp in tmp_destinations:
+                try:
+                    tmp.unlink()
+                except FileNotFoundError:
+                    pass
+    else:
+        return "skipped"
+
+
+def copy_and_seal(source: Path, destinations: List[Path], overwrite=False, verify=True, skip_existing=False):
+    destinations = [d / source.name for d in destinations]
+
+    start = datetime.datetime.utcnow()
+    file_infos = copytree(source, destinations, overwrite=overwrite, verify=verify, skip_existing=skip_existing)
+
+    if not skip_existing:
+        write_mhl(destinations, file_infos, source, start)
+        write_xxhash_summary(destinations, file_infos)
+
+
+def _is_cancelled() -> bool:
+    try:
+        return currentThread().cancelled
+    except AttributeError:
+        return False
+
+
+class CopyJob(Thread):
+    total_size: int
+    total_done: int
+    finished: bool
+    errors: List[ErrorListEntry]
+
+    def __init__(
+        self, source: Path, destinations: List[Path], overwrite=False, verify=True, skip_existing=False, auto_start=True
+    ):
+        super().__init__()
+        self.daemon = True
+        self.errors = []
+        self._progress_queue = Queue()
+        self._cancel = Event()
+
+        self.source = source
+        self.destinations = destinations
+        self.overwrite = overwrite
+        self.verify = verify
+        self.skip_existing = skip_existing
+
+        self.total_size = folder_size(source)
+        self.todo_size = self.total_size * (2 if self.verify else 1)
+        self.total_done = 0
+        self.current_item = None
+        self.finished = False
+
+        if auto_start:
+            self.start()
+
+    def cancel(self):
+        self._cancel.set()
+
+    @property
+    def cancelled(self) -> bool:
+        return self._cancel.isSet()
+
+    @threaded
+    def _progress_reader(self):
+        while not (self.finished or self.cancelled):
+            file_path, done = self._progress_queue.get()
+            self.current_item = Path(file_path).name
+            self.total_done += done
+
+    @property
+    def percent_done(self) -> int:
+        return round(100 / self.todo_size * self.total_done)
+
+    @property
+    def progress(self) -> str:
+        for i in range(1, 101):
+            while self.percent_done < i:
+                sleep(0.1)
+            yield self.current_item
+
+    def run(self):
+        if self.cancelled:
+            self.finished = True
+            return
+
+        self._progress_reader()
+
+        try:
+            copy_and_seal(
+                source=self.source,
+                destinations=self.destinations,
+                overwrite=self.overwrite,
+                verify=self.verify,
+                skip_existing=self.skip_existing,
+            )
+
+        except CopyTreeError as e:
+            self.errors = e.args[0]
+
+        self.finished = True
