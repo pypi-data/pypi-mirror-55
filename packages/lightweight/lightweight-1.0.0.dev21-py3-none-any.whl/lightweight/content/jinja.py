aa473a3c88f621174d9b56b6c8c660356896c165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:21 +0000

    Add package lightweight/1.0.0.dev21/lightweight-1.0.0.dev21-py3-none-any.whl

---
+++ packages/lightweight/lightweight-1.0.0.dev21-py3-none-any.whl/lightweight/content/jinja.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional, Dict, Any, Union, TYPE_CHECKING
+
+from jinja2 import Template
+
+from lightweight.files import FileName
+from lightweight.template import template
+from .content import Content
+
+if TYPE_CHECKING:
+    from lightweight import SitePath
+
+
+@dataclass
+class JinjaSource(Content):
+    filename: FileName
+    source_path: Optional[Path]
+    params: Dict[str, Any]
+    template: Template
+
+    def write(self, path: SitePath):
+        path.create(self.template.render(
+            site=path.site, source=self, **self.params
+        ))
+
+
+def render(template_path: Union[str, Path], **params) -> JinjaSource:
+    """Renders the page at path with provided parameters.
+
+    Templates are resolved from current directory (NOT `./templates/`)."""
+    path = Path(template_path)
+    return JinjaSource(
+        filename=FileName(path.name),
+        source_path=path,
+        params=params,
+        template=template(path, base_dir='.')
+    )
---
+++ packages/lightweight/lightweight-1.0.0.dev21-py3-none-any.whl/lightweight/content/lwmd.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+
+from collections import namedtuple
+from typing import List, Dict
+
+import mistune  # type: ignore # no typings
+from slugify import slugify  # type: ignore # no typings
+
+TocEntry = namedtuple('TocEntry', ['slug', 'text', 'level'])
+
+
+class TocMixin(object):
+    toc_entries: List[TocEntry]
+
+    def reset(self):
+        self.toc_entries = []
+
+    def header(self, text, level, raw=None):
+        slug = slugify(text)
+        rv = f'<h{level:d} id="{slug}">{text}</h{level:d}>\n'
+        self.toc_entries.append(TocEntry(slug, text, level))
+        return rv
+
+    def render_toc(self, level=3):
+        toc_items = list(self._iter_toc(level))
+        if len(toc_items) < 5:
+            return ''
+        return ''.join(self._iter_toc(level))
+
+    def _iter_toc(self, level):
+        # TODO:mdrachuk:9/26/19: return structured model of ToC instead of HTML. Let users define how to render it.
+        first_level = 0
+        last_level = 0
+
+        yield '<ul id="table-of-content">\n'
+
+        for entry in self.toc_entries:
+            slug, text, cur_level = entry
+
+            if cur_level > level:
+                # ignore this level
+                continue
+
+            if first_level == 0:
+                # based on first level
+                first_level = cur_level
+                last_level = cur_level
+                yield f'<li><a href="#{slug}">{text}</a>'
+            elif last_level == cur_level:
+                yield f'</li>\n<li><a href="#{slug}">{text}</a>'
+            elif last_level == cur_level - 1:
+                last_level = cur_level
+                yield f'\n<ul>\n<li><a href="#{slug}">{text}</a>'
+            elif last_level > cur_level:
+                # close indention
+                yield '</li>'
+                while last_level > cur_level:
+                    yield '</ul>\n</li>\n'
+                    last_level -= 1
+                yield f'<li><a href="#{slug}">{text}</a>'
+
+        # close tags
+        yield '</li>\n'
+        while last_level > first_level:
+            yield '</ul>\n</li>\n'
+            last_level -= 1
+
+        yield '</ul>\n'
+
+
+class LwRenderer(TocMixin, mistune.Renderer):
+
+    def __init__(self, link_mapping: Dict[str, str]):
+        super().__init__()
+        self.url_mapping = link_mapping
+
+    def reset(self):
+        super(LwRenderer, self).reset()
+
+    def link(self, link, title, text):
+        if link.startswith('/'):
+            without_slash = link[1:]
+            if without_slash in self.url_mapping:
+                link = self.url_mapping[without_slash]
+        elif link in self.url_mapping:
+            link = self.url_mapping[link]
+        return super().link(link, title, text)
+
+
+class LwMarkdown(mistune.Markdown):
+    def __init__(self, link_mapping: Dict[str, str]):
+        super().__init__(renderer=LwRenderer(link_mapping))
+
+    def render(self, text):
+        self.renderer.reset()
+        html = super().render(text)
+        toc_html = self.renderer.render_toc(level=3)
+        return html, toc_html
