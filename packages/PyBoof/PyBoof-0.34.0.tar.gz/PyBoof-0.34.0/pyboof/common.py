e84498e7c7def3aed04b101c5b79f79e494b13af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:33 +0000

    Add package PyBoof/0.34.0/PyBoof-0.34.0.tar.gz

---
+++ packages/PyBoof/PyBoof-0.34.0.tar.gz/PyBoof-0.34.0/pyboof/common.py
@@ -0,0 +1,111 @@
+from py4j import java_gateway
+from pyboof import gateway
+from six import string_types
+
+
+def exception_use_mmap():
+    raise Exception("Need to turn on mmap. Add pb.init_memmap() to your code before any other calls to PyBoof")
+
+def is_java_class(java_class, string_path):
+    return gateway.jvm.pyboof.PyBoofEntryPoint.isClass(java_class,string_path)
+
+
+def is_java_list(object):
+    object.getClass()
+    return True
+
+
+def ejml_matrix_d_to_f( D ):
+    F = gateway.jvm.org.ejml.data.FMatrixRMaj( D.getNumRows(), D.getNumCols() )
+    gateway.jvm.org.ejml.ops.ConvertMatrixData.convert(D, F)
+    return F
+
+def boof_fixed_length( length ):
+    return gateway.jvm.boofcv.struct.ConfigLength(float(length),float(-1))
+
+class JavaWrapper:
+    def __init__(self, java_object=None):
+        self.java_obj = java_object
+        self.java_fields = [x for x in gateway.jvm.pyboof.PyBoofEntryPoint.getPublicFields(self.java_obj.getClass())]
+
+    def __getattr__(self, item):
+        if "java_fields" in self.__dict__ and item in self.__dict__["java_fields"]:
+            return java_gateway.get_field(self.java_obj, item)
+        else:
+            return object.__getattribute__(self, item)
+
+    def __setattr__(self, key, value):
+        if "java_fields" in self.__dict__ and key in self.__dict__["java_fields"]:
+            java_gateway.set_field(self.java_obj, key, value)
+        else:
+            self.__dict__[key] = value
+
+    def __dir__(self):
+        return sorted(set( self.__dict__.keys() + self.java_fields))
+
+    def set_java_object(self, obj):
+        self.java_obj = obj
+
+    def get_java_object(self):
+        return self.java_obj
+
+    def __str__(self):
+        return "Wrapped Java:\n"+self.java_obj.toString()
+
+
+class JavaConfig(JavaWrapper):
+    """
+    Provides a nice python wrapper around Java classes.  Public variables are automatically turned into Python
+    attributes
+    """
+    # TODO variables which are java classes are a little messed up
+    def __init__(self, java_class_path):
+        if isinstance(java_class_path, string_types):
+            self.java_class_path = java_class_path
+
+            words = java_class_path.replace('$',".").split(".")
+
+            a = gateway.jvm.__getattr__(words[0])
+            for i in range(1, len(words)):
+                a = a.__getattr__(words[i])
+
+            self.java_obj = a.__call__()
+        else:
+            self.java_obj = java_class_path
+        JavaWrapper.__init__(self, self.java_obj)
+
+    def __getattr__(self, item):
+        if "java_fields" in self.__dict__ and item in self.__dict__["java_fields"]:
+            a = java_gateway.get_field(self.java_obj,item)
+            if gateway.jvm.pyboof.PyBoofEntryPoint.isConfigClass(a):
+                return JavaConfig(a)
+            else:
+                return a
+        else:
+            return object.__getattribute__(self, item)
+
+    def __setattr__(self, key, value):
+        if "java_fields" in self.__dict__ and key in self.__dict__["java_fields"]:
+            if isinstance(value, JavaConfig):
+                java_gateway.set_field(self.java_obj, key, value.java_obj)
+            else:
+                java_gateway.set_field(self.java_obj, key, value)
+        else:
+            self.__dict__[key] = value
+
+
+class JavaList(JavaWrapper):
+    def __init__(self, java_list, java_type):
+        JavaWrapper.__init__(self,java_list)
+        self.java_type = java_type
+
+    def size(self):
+        return self.java_obj.size()
+
+    def save_to_disk(self, file_name ):
+        gateway.jvm.pyboof.FileIO.saveList(self.java_obj,self.java_type,file_name)
+
+
+def JavaList_to_fastqueue(java_list, java_class_type , queue_declare=False):
+    return gateway.jvm.pyboof.PyBoofEntryPoint.listToFastQueue(java_list, java_class_type, queue_declare)
+
---
+++ packages/PyBoof/PyBoof-0.34.0.tar.gz/PyBoof-0.34.0/pyboof/swing.py
@@ -0,0 +1,109 @@
+import math
+import py4j.java_gateway as jg
+from pyboof import gateway
+import pyboof
+import numpy as np
+
+def show( boof_image , title="Image"):
+    gateway.jvm.boofcv.gui.image.ShowImages.showWindow(boof_image,title)
+
+def show_grid( images , columns=-1, title="Image Grid"):
+    if type(images) is not tuple or not list:
+        images = (images)
+
+    array = gateway.new_array(gateway.jvm.java.awt.image.BufferedImage,len(images))
+    for idx,image in enumerate(images):
+        if jg.is_instance_of(gateway, image, gateway.jvm.java.awt.image.BufferedImage ):
+            array[idx] = image
+        else:
+            array[idx] = gateway.jvm.boofcv.io.image.ConvertBufferedImage.convertTo(image,None,True)
+
+    # If no grid is specified try to make it square
+    if columns <= 0:
+        columns = int(math.sqrt(len(images)))
+    gateway.jvm.boofcv.gui.image.ShowImages.showGrid(columns, title, array)
+
+
+def show_list(image_name_pairs, title="Image List"):
+    if type(image_name_pairs) is not tuple or not list:
+        image_name_pairs = (image_name_pairs)
+
+    names = []
+    buffered = []
+    for pair in image_name_pairs:
+        if jg.is_instance_of(gateway, pair[0], gateway.jvm.java.awt.image.BufferedImage ):
+            buffered.append(pair[0])
+        else:
+            buffered.append( gateway.jvm.boofcv.io.image.ConvertBufferedImage.convertTo(pair[0],None,True) )
+        names.append(pair[1])
+
+    panel = gateway.jvm.boofcv.gui.ListDisplayPanel()
+    for i in range(len(names)):
+        panel.addImage(buffered[i],names[i])
+    gateway.jvm.boofcv.gui.image.ShowImages.showWindow(panel, title)
+
+
+def colorize_gradient( derivX , derivY ):
+    return gateway.jvm.boofcv.gui.image.VisualizeImageData.colorizeGradient(derivX,derivY,-1,None)
+
+
+def render_binary( binary , invert=False):
+    return gateway.jvm.boofcv.gui.binary.VisualizeBinaryData.renderBinary(binary,invert,None)
+
+
+def visualize_matches(image_left , image_right , points_src , points_dst , match_indexes,
+                      title = "Associated Features"):
+
+    if type(points_src) is list:
+        points_src = pyboof.p2b_list_point2D(points_src,np.double)
+
+    if type(points_dst) is list:
+        points_dst = pyboof.p2b_list_point2D(points_dst,np.double)
+
+    if type(match_indexes) is list:
+        raise Exception("Haven't bothered to implement python to java conversion for match_indexes yet")
+
+    # If possible, convert images into a BufferedImage data type
+    if jg.is_instance_of(gateway, image_left, gateway.jvm.boofcv.struct.image.ImageBase):
+        image_left = gateway.jvm.boofcv.io.image.ConvertBufferedImage.convertTo(image_left, None, True)
+
+    if jg.is_instance_of(gateway, image_right, gateway.jvm.boofcv.struct.image.ImageBase):
+        image_right = gateway.jvm.boofcv.io.image.ConvertBufferedImage.convertTo(image_right, None, True)
+
+    panel = gateway.jvm.boofcv.gui.feature.AssociationPanel(20)
+    panel.setImages( image_left, image_right)
+    panel.setAssociation(points_src, points_dst, match_indexes)
+    gateway.jvm.boofcv.gui.image.ShowImages.showWindow(panel, title)
+
+
+def visualize_lines(image, lines, title="Lines"):
+    # If possible, convert images into a BufferedImage data type
+    if jg.is_instance_of(gateway, image, gateway.jvm.boofcv.struct.image.ImageBase):
+        image = gateway.jvm.boofcv.io.image.ConvertBufferedImage.convertTo(image, None, True)
+
+    d = gateway.jvm.java.awt.Dimension(image.getWidth(), image.getHeight())
+
+    if type(lines) is list:
+        if len(lines) > 0 and type(lines[0]) is tuple: # See if it's a list of line and names
+            panel = gateway.jvm.boofcv.gui.ListDisplayPanel()
+            for o in lines:
+                panel_lines = gateway.jvm.boofcv.gui.feature.ImageLinePanel()
+                panel_lines.setImage(image)
+                panel_lines.setLines(pyboof.p2b_list_LineParametric(o[1],np.float))
+                panel_lines.setPreferredSize(d)
+                panel.addItem(panel_lines, o[0])
+
+
+            gateway.jvm.boofcv.gui.image.ShowImages.showWindow(panel, title)
+            return
+        else:
+            lines = pyboof.p2b_list_LineParametric(lines, np.float)
+
+    print("lines {}".format(len(lines)))
+    print("foo {}".format(lines[0]))
+
+    panel = gateway.jvm.boofcv.gui.feature.ImageLinePanel()
+    panel.setImage(image)
+    panel.setLines(lines)
+    panel.setPreferredSize(d)
+    gateway.jvm.boofcv.gui.image.ShowImages.showWindow(panel, title)
