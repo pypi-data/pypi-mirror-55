7d02530248beb424e607e4d1cc0c9c2cc672086b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:33 +0000

    Add package PackageStudy/1.0/PackageStudy-1.0-py3-none-any.whl

---
+++ packages/PackageStudy/PackageStudy-1.0-py3-none-any.whl/fhx/fhx.py
@@ -0,0 +1,10 @@
+#coding = utf -8
+
+def fhx_name():
+	print('my name is fhx, thanks!')
+
+def fhx_age():
+	print('I am four years old, thanks!')
+
+if __name__ == '__main__':
+	fhx_name()
---
+++ packages/PackageStudy/PackageStudy-1.0-py3-none-any.whl/fjz/fjz.py
@@ -0,0 +1,7 @@
+#coding = utf -8
+
+def fjz_name():
+	print('my name is fhz, thanks!')
+	
+if __name__ == '__main__':
+	fjz_name()
