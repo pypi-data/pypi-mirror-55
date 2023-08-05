50e4a29a0f445917aa416d827b4bd0e4427527eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:55 +0000

    Add package pyesg/0.1.1/pyesg-0.1.1.tar.gz

---
+++ packages/pyesg/pyesg-0.1.1.tar.gz/pyesg-0.1.1/setup.py
@@ -0,0 +1,22 @@
+"""Package setup"""
+from setuptools import setup, find_packages
+
+setup(
+    name="pyesg",
+    version="0.1.1",
+    description="Economic Scenario Generator in Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jason-ash/pyesg",
+    author="Jason Ash",
+    author_email="jason@ashanalytics.com",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas", "scipy"],
+    extras_require={"dev": ["pre-commit", "pylint", "mypy"]},
+    include_package_data=True,
+    package_data={
+        "pyesg": ["../README.md", "../LICENSE.md", "../MANIFEST.in", "datasets/*"]
+    },
+    test_suite="tests",
+    zip_safe=False,
+)
