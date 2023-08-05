574f572c5827205ba6723daeb990927827ec09a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package remember-forever/0.1/remember_forever-0.1.tar.gz

---
+++ packages/remember-forever/remember_forever-0.1.tar.gz/remember_forever-0.1/remember_forever/Love.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+class Love():
+    def get_birthday(self):
+        print('冬儿，祝你生日快乐')
---
+++ packages/remember-forever/remember_forever-0.1.tar.gz/remember_forever-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='remember_forever',
+      version='0.1',
+      description='weixiao & yandong love story',
+      packages=['remember_forever'],
+      zip_safe=False)
