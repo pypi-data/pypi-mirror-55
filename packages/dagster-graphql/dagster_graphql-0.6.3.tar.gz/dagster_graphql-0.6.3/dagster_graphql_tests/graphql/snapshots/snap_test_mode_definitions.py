83d43a9c2d95e927a35f39ae3fbdc96f08d2bf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:58 +0000

    Add package dagster-graphql/0.6.3/dagster_graphql-0.6.3.tar.gz

---
+++ packages/dagster-graphql/dagster_graphql-0.6.3.tar.gz/dagster_graphql-0.6.3/dagster_graphql_tests/graphql/snapshots/snap_test_mode_definitions.py
@@ -0,0 +1,349 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+snapshots = Snapshot()
+
+snapshots['test_query_multi_mode 1'] = {
+    'pipeline': {
+        '__typename': 'Pipeline',
+        'configTypes': [
+            {
+                'name': 'Any'
+            },
+            {
+                'name': 'Any.InputHydrationConfig'
+            },
+            {
+                'name': 'Any.MaterializationSchema'
+            },
+            {
+                'name': 'Bool'
+            },
+            {
+                'name': 'Bool.InputHydrationConfig'
+            },
+            {
+                'name': 'Bool.MaterializationSchema'
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': None
+            },
+            {
+                'name': 'Float'
+            },
+            {
+                'name': 'Float.InputHydrationConfig'
+            },
+            {
+                'name': 'Float.MaterializationSchema'
+            },
+            {
+                'name': 'Int'
+            },
+            {
+                'name': 'Int.InputHydrationConfig'
+            },
+            {
+                'name': 'Int.MaterializationSchema'
+            },
+            {
+                'name': 'MultiModeWithResources.AddMode.ExecutionConfig'
+            },
+            {
+                'name': 'MultiModeWithResources.AddMode.ExecutionConfig.InProcess'
+            },
+            {
+                'name': 'MultiModeWithResources.AddMode.ExecutionConfig.Multiprocess'
+            },
+            {
+                'name': 'MultiModeWithResources.AddMode.StorageConfig'
+            },
+            {
+                'name': 'MultiModeWithResources.AddMode.StorageConfig.Filesystem'
+            },
+            {
+                'name': 'MultiModeWithResources.AddMode.StorageConfig.InMemory'
+            },
+            {
+                'name': 'MultiModeWithResources.ApplyToThree.Outputs'
+            },
+            {
+                'name': 'MultiModeWithResources.LoggerConfig'
+            },
+            {
+                'name': 'MultiModeWithResources.LoggerConfig.Console'
+            },
+            {
+                'name': 'MultiModeWithResources.Mode.AddMode.Environment'
+            },
+            {
+                'name': 'MultiModeWithResources.Mode.AddMode.Resources'
+            },
+            {
+                'name': 'MultiModeWithResources.Mode.AddMode.Resources.Op'
+            },
+            {
+                'name': 'MultiModeWithResources.SolidConfig.ApplyToThree'
+            },
+            {
+                'name': 'MultiModeWithResources.SolidsConfigDictionary'
+            },
+            {
+                'name': 'Path'
+            },
+            {
+                'name': 'Path.MaterializationSchema'
+            },
+            {
+                'name': 'String'
+            },
+            {
+                'name': 'String.InputHydrationConfig'
+            },
+            {
+                'name': 'String.MaterializationSchema'
+            }
+        ],
+        'environmentType': {
+            'fields': [
+                {
+                    'configType': {
+                        'name': 'MultiModeWithResources.AddMode.ExecutionConfig'
+                    }
+                },
+                {
+                    'configType': {
+                        'name': 'MultiModeWithResources.LoggerConfig'
+                    }
+                },
+                {
+                    'configType': {
+                        'name': 'MultiModeWithResources.Mode.AddMode.Resources'
+                    }
+                },
+                {
+                    'configType': {
+                        'name': 'MultiModeWithResources.SolidsConfigDictionary'
+                    }
+                },
+                {
+                    'configType': {
+                        'name': 'MultiModeWithResources.AddMode.StorageConfig'
+                    }
+                }
+            ],
+            'name': 'MultiModeWithResources.Mode.AddMode.Environment'
+        },
+        'modes': [
+            {
+                'description': 'Mode that adds things',
+                'loggers': [
+                    {
+                        'configField': {
+                            'configType': {
+                                'fields': [
+                                    {
+                                        'configType': {
+                                            'name': 'String'
+                                        },
+                                        'name': 'log_level'
+                                    },
+                                    {
+                                        'configType': {
+                                            'name': 'String'
+                                        },
+                                        'name': 'name'
+                                    }
+                                ],
+                                'name': None
+                            }
+                        },
+                        'name': 'console'
+                    }
+                ],
+                'name': 'add_mode',
+                'resources': [
+                    {
+                        'configField': {
+                            'configType': {
+                                'name': 'Int'
+                            }
+                        },
+                        'name': 'op'
+                    }
+                ]
+            },
+            {
+                'description': 'Mode that adds two numbers to thing',
+                'loggers': [
+                    {
+                        'configField': {
+                            'configType': {
+                                'fields': [
+                                    {
+                                        'configType': {
+                                            'name': 'String'
+                                        },
+                                        'name': 'log_level'
+                                    },
+                                    {
+                                        'configType': {
+                                            'name': 'String'
+                                        },
+                                        'name': 'name'
+                                    }
+                                ],
+                                'name': None
+                            }
+                        },
+                        'name': 'console'
+                    }
+                ],
+                'name': 'double_adder',
+                'resources': [
+                    {
+                        'configField': {
+                            'configType': {
+                                'fields': [
+                                    {
+                                        'configType': {
+                                            'name': 'Int'
+                                        },
+                                        'name': 'num_one'
+                                    },
+                                    {
+                                        'configType': {
+                                            'name': 'Int'
+                                        },
+                                        'name': 'num_two'
+                                    }
+                                ],
+                                'name': None
+                            }
+                        },
+                        'name': 'op'
+                    }
+                ]
+            },
+            {
+                'description': 'Mode that multiplies things',
+                'loggers': [
+                    {
+                        'configField': {
+                            'configType': {
+                                'fields': [
+                                    {
+                                        'configType': {
+                                            'name': 'String'
+                                        },
+                                        'name': 'log_level'
+                                    },
+                                    {
+                                        'configType': {
+                                            'name': 'String'
+                                        },
+                                        'name': 'name'
+                                    }
+                                ],
+                                'name': None
+                            }
+                        },
+                        'name': 'console'
+                    }
+                ],
+                'name': 'mult_mode',
+                'resources': [
+                    {
+                        'configField': {
+                            'configType': {
+                                'name': 'Int'
+                            }
+                        },
+                        'name': 'op'
+                    }
+                ]
+            }
+        ],
+        'presets': [
+            {
+                'mode': 'add_mode',
+                'name': 'add'
+            }
+        ]
+    }
+}
