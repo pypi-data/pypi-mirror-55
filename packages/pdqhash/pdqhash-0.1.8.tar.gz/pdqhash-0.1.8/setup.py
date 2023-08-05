a17b36ebb0523294226db0697760e33f9326099e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:20 +0000

    Add package pdqhash/0.1.8/pdqhash-0.1.8.tar.gz

---
+++ packages/pdqhash/pdqhash-0.1.8.tar.gz/pdqhash-0.1.8/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools.extension import Extension
+import numpy
+
+EXTENSIONS = [
+    Extension(
+        'pdqhash.bindings', ['pdqhash/bindings.pyx'],
+        include_dirs=['ThreatExchange/hashing',
+                      numpy.get_include()],
+        language='c++',
+        extra_compile_args=['--std=c++11'])
+]
+
+setup(ext_modules=EXTENSIONS)
