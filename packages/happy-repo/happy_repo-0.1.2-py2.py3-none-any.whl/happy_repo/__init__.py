4a609e38f45222c8c58a09fe6db3595d62623d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:31 +0000

    Add package happy-repo/0.1.2/happy_repo-0.1.2-py2.py3-none-any.whl

---
+++ packages/happy-repo/happy_repo-0.1.2-py2.py3-none-any.whl/happy_repo/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .happy_repo import simple_function
+"""Top-level package for happy_repo."""
+
+__all__ = ['simple_function', ]
+
+__author__ = """Oren Kot"""
+__email__ = 'przemyslaw.kot@gmail.com'
+__version__ = '0.1.0'
