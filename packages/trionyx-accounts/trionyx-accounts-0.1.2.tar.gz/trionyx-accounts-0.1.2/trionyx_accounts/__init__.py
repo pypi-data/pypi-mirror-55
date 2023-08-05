93e2549cdf53d05a98c82ab707c4672e960c69a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package trionyx-accounts/0.1.2/trionyx-accounts-0.1.2.tar.gz

---
+++ packages/trionyx-accounts/trionyx-accounts-0.1.2.tar.gz/trionyx-accounts-0.1.2/trionyx_accounts/__init__.py
@@ -0,0 +1,10 @@
+"""
+trionyx_accounts
+~~~~~~~~~~~~~~~~
+
+:copyright: 2019 by Maikel Martens
+:license: GPLv3
+"""
+__version__ = '0.1.2'
+
+default_app_config = 'trionyx_accounts.apps.AccountsConfig'
