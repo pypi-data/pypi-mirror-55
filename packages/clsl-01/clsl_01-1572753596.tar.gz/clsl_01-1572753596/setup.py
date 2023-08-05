93c6713645a2adf55586b975430d630fcb8fae24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package clsl-01/1572753596/clsl_01-1572753596.tar.gz

---
+++ packages/clsl-01/clsl_01-1572753596.tar.gz/clsl_01-1572753596/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="clsl_01",
+    version="1572753596",
+    author="Luzi Leung",
+    author_email="luzi82@gmail.com",
+    description="Test lib of https://github.com/luzi82/codelog.serverless",
+    long_description="Test lib of https://github.com/luzi82/codelog.serverless",
+    long_description_content_type="text/plain",
+    url="https://github.com/luzi82/codelog.serverless",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
