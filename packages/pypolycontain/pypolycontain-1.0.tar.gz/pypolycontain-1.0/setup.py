9a737c4dc396a48b179695d6f1ebdaae7a83650f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package pypolycontain/1.0/pypolycontain-1.0.tar.gz

---
+++ packages/pypolycontain/pypolycontain-1.0.tar.gz/pypolycontain-1.0/setup.py
@@ -0,0 +1,10 @@
+#from distutils.core import setup
+from setuptools import setup, Extension
+
+setup(
+    name='pypolycontain',
+    version='1.0',
+    packages=[],
+    license='MIT',
+    url="https://github.com/sadraddini/pypolycontain",
+)
