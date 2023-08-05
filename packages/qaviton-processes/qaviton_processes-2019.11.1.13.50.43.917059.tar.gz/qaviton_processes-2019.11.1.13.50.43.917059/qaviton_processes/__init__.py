7629ac9c45a1c3e733dff5826bfb2733db5c8b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package qaviton-processes/2019.11.1.13.50.43.917059/qaviton_processes-2019.11.1.13.50.43.917059.tar.gz

---
+++ packages/qaviton-processes/qaviton_processes-2019.11.1.13.50.43.917059.tar.gz/qaviton_processes-2019.11.1.13.50.43.917059/qaviton_processes/__init__.py
@@ -0,0 +1,15 @@
+from qaviton_processes.multi_processing import Task
+from qaviton_processes.system import (
+    bs,
+    run,
+    pip,
+    git,
+    escape,
+    python,
+    python_code,
+    pytest,
+    run_async,
+    pytest_async,
+    python_async,
+    python_code_async,
+)
