ed3ca6862c24de9d9edfcc73ba334737b975c033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:02 +0000

    Add package mosaik/2.5.2/mosaik-2.5.2.tar.gz

---
+++ packages/mosaik/mosaik-2.5.2.tar.gz/mosaik-2.5.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mosaik',
+    version='2.5.2',
+    author='Stefan Scherfke',
+    author_email='mosaik@offis.de',
+    description='Mosaik is a flexible Smart-Grid co-simulation framework.',
+    long_description=(open('README.rst', encoding='utf-8').read() + '\n\n' +
+                      open('CHANGES.txt', encoding='utf-8').read() + '\n\n' +
+                      open('AUTHORS.txt', encoding='utf-8').read()),
+    url='https://mosaik.offis.de',
+    install_requires=[
+        'networkx>=2.0',
+        'mosaik-api>=2.3',
+        'simpy>=3.0.10',
+        'simpy.io>=0.2.3',
+    ],
+
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
