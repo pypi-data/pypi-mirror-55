e261dc539536b08006f8b0bd754966d52544d511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:27 +0000

    Add package nds2utils/0.0.1/nds2utils-0.0.1.tar.gz

---
+++ packages/nds2utils/nds2utils-0.0.1.tar.gz/nds2utils-0.0.1/README.md
@@ -0,0 +1,23 @@
+# nds2utils
+
+nds2utils is a user interface for the python nds2 client.  
+nds2 error messages and data structures can be hard to parse. 
+nds2utils provides helper functions, defaults, warnings, and error messages for users trying to get ligo data with python. 
+
+# Installation
+
+You can install using `pip` at https://pypi.org/project/nds2utils/ 
+```
+pip install nds2utils
+```
+then in a python scipy or ipython terminal, import the dataUtils library by running
+```python
+import nds2utils
+from nds2utils.dataUtils import *
+```
+
+
+# Quick Start
+
+# Features
+ 
---
+++ packages/nds2utils/nds2utils-0.0.1.tar.gz/nds2utils-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nds2utils",
+    version="0.0.1",
+    author="Craig Cahillane",
+    author_email="ccahilla@caltech.edu",
+    description="nds2 utilities for reading LIGO data quickly and easily",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.ligo.org/craig-cahillane/nds2utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
