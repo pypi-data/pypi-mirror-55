62ae02b5cd54a4cae77f7021612c8d9934bed960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package weather.com-web-crawling-application/0.1.dev0/weather.com%20web-crawling-application-0.1.dev0.tar.gz

---
+++ packages/weather.com-web-crawling-application/weather.com%20web-crawling-application-0.1.dev0.tar.gz/weather.com web-crawling-application-0.1.dev0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'weather.com web-crawling-application',
+    version = '0.1dev',
+    scripts = ['app.py'],
+    author = "Keerthi Rangan",
+    author_email = "keerthi9.5.95@gmail.com",
+)
