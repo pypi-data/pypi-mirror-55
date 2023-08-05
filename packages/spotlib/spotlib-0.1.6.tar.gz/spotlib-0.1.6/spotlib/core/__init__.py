b3801252bb5fc666ee7c5f5e22a741af83a21500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package spotlib/0.1.6/spotlib-0.1.6.tar.gz

---
+++ packages/spotlib/spotlib-0.1.6.tar.gz/spotlib-0.1.6/spotlib/core/__init__.py
@@ -0,0 +1,11 @@
+from spotlib._version import __version__ as version
+
+
+__author__ = 'Blake Huber'
+__version__ = version
+__email__ = "blakeca00@gmail.com"
+
+
+from spotlib.core.endpoints import DurationEndpoints
+from spotlib.core.spotcore import EC2SpotPrices
+from spotlib.core.utc import UtcConversion, utc_conversion
