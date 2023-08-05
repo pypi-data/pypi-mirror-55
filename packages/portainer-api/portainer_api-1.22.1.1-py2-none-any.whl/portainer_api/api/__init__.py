f9513d0456cedd4ed0bc4e03ea7ca636824e68bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:44 +0000

    Add package portainer-api/1.22.1.1/portainer_api-1.22.1.1-py2-none-any.whl

---
+++ packages/portainer-api/portainer_api-1.22.1.1-py2-none-any.whl/portainer_api/api/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from portainer_api.api.auth_api import AuthApi
+from portainer_api.api.dockerhub_api import DockerhubApi
+from portainer_api.api.endpoint_groups_api import EndpointGroupsApi
+from portainer_api.api.endpoints_api import EndpointsApi
+from portainer_api.api.extensions_api import ExtensionsApi
+from portainer_api.api.registries_api import RegistriesApi
+from portainer_api.api.resource_controls_api import ResourceControlsApi
+from portainer_api.api.roles_api import RolesApi
+from portainer_api.api.settings_api import SettingsApi
+from portainer_api.api.stacks_api import StacksApi
+from portainer_api.api.status_api import StatusApi
+from portainer_api.api.tags_api import TagsApi
+from portainer_api.api.team_memberships_api import TeamMembershipsApi
+from portainer_api.api.teams_api import TeamsApi
+from portainer_api.api.templates_api import TemplatesApi
+from portainer_api.api.upload_api import UploadApi
+from portainer_api.api.users_api import UsersApi
