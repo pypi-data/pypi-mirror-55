832d87378c4ea33a54abd1d47eb1ad7037d36866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package pyexecutors/0.0.7/pyexecutors-0.0.7.tar.gz

---
+++ packages/pyexecutors/pyexecutors-0.0.7.tar.gz/pyexecutors-0.0.7/pyexecutors/__init__.py
@@ -0,0 +1,36 @@
+from . import executors
+from . import holders
+
+"""
+    pyexecutors
+    ~~~~~~~~~~~
+    
+    a lite-weight library to efficiently run series of 
+    asynchronous and synchronous tasks concurrently
+    without worrying about managing different threads
+    in your own.
+    
+"""
+
+
+def executor():
+    return executors.Executors()
+
+
+def async_tasks(f, args=None, kwargs=None):
+    if args is None:
+        args = []
+    if kwargs is None:
+        kwargs = {}
+    return holders.AsyncTasks(f, args, kwargs)
+
+
+def sync_tasks(f, args=None, kwargs=None):
+    if kwargs is None:
+        kwargs = {}
+    if args is None:
+        args = []
+    return holders.SyncTasks(f, args, kwargs)
+
+
+__version__ = "0.0.3.dev"
