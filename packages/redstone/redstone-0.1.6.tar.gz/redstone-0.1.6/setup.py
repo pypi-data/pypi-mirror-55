83e8c5d1e4a9c56f5c61eed8296e1fd99c492224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:02 +0000

    Add package redstone/0.1.6/redstone-0.1.6.tar.gz

---
+++ packages/redstone/redstone-0.1.6.tar.gz/redstone-0.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="redstone",
+    version="0.1.6",
+    author="Mathew Odden",
+    author_email="mathewrodden@gmail.com",
+    url="https://github.com/locke105/redstone",
+    packages=find_packages(),
+    install_requires=[
+        "requests[security]"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
