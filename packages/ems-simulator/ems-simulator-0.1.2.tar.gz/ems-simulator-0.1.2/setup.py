91cf06f422e969a5fe19067e8a85e74f31dbadce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:01 +0000

    Add package ems-simulator/0.1.2/ems-simulator-0.1.2.tar.gz

---
+++ packages/ems-simulator/ems-simulator-0.1.2.tar.gz/ems-simulator-0.1.2/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+setup(
+    name="ems-simulator",
+    version="0.1.2",
+    packages=['ems'],
+    python_requires='>=3.4',
+
+    # metadata
+    author="Mauricio C. de Oliveira, Timothy Lam, Hans Yuan",
+    author_email="mauricio@ucsd.edu",
+
+    description="Python library for EMS simulations",
+    license="MIT",
+
+    keywords=["EMS", "Simulation"],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'geopy',
+        'pandas',
+        'pyyaml',
+        'shapely'
+    ],
+    url="https://github.com/EMSTrack/EMS-Simulator",
+    download_url="https://github.com/EMSTrack/EMS-Simulator/archive/v0.1.tar.gz",
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Other Audience',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
