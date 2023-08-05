7a1b7fdd0e1949ecd4725ffd340a71a4a8c49968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:39 +0000

    Add package distributions-udacity-kh/0.1/distributions_udacity_kh-0.1.tar.gz

---
+++ packages/distributions-udacity-kh/distributions_udacity_kh-0.1.tar.gz/distributions_udacity_kh-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_udacity_kh',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udacity_kh'],
+      zip_safe=False)
