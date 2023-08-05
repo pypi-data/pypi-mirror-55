5f752f96d76ff23f5065b8132f350c9287987ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:54 +0000

    Add package peek-core-user/2.1.1/peek-core-user-2.1.1.tar.gz

---
+++ packages/peek-core-user/peek-core-user-2.1.1.tar.gz/peek-core-user-2.1.1/peek_core_user/plugin-module/services/logged-in.guard.ts
@@ -0,0 +1,29 @@
+import {Injectable} from "@angular/core";
+import {CanActivate, Router} from "@angular/router";
+import {UserService} from "./user.service";
+
+@Injectable()
+export class LoggedInGuard implements CanActivate {
+    constructor(private user: UserService, private router:Router) {
+    }
+
+    canActivate() {
+        if (!this.user.hasLoaded()) {
+            return new Promise<boolean>((resolve, reject) => {
+                this.user.loadingFinishedObservable()
+                    .first()
+                    .subscribe(() => {
+                        resolve(this.canActivate());
+                    });
+            });
+        }
+
+        if (this.user.isLoggedIn())
+            return true;
+
+        this.router.navigate(['peek_core_user', 'login']);
+        return false;
+    }
+}
+
+
