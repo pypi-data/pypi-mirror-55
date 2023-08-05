2406d1be0213bb97db4937672fee68cf8439c781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:30 +0000

    Add package sevenbridges-python/0.24.2/sevenbridges-python-0.24.2.tar.gz

---
+++ packages/sevenbridges-python/sevenbridges-python-0.24.2.tar.gz/sevenbridges-python-0.24.2/setup.py
@@ -0,0 +1,42 @@
+import io
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+version = '0.0.1+local-build'
+if os.path.isfile('VERSION'):
+    with io.open('VERSION', 'r', encoding='utf-8') as f:
+        version = f.read()
+
+install_requires = ["requests>=2.20.0", "six>=1.10.0"]
+if sys.version_info < (3,):
+    install_requires.append("futures>=3.0.4")
+
+setup(
+    name='sevenbridges-python',
+    version=version,
+    description='SBG API python client bindings',
+    install_requires=install_requires,
+    long_description=io.open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    platforms=['Windows', 'POSIX', 'MacOS'],
+    maintainer='Seven Bridges Genomics Inc.',
+    maintainer_email='dejan.knezevic@sbgenomics.com',
+    url='https://github.com/sbg/sevenbridges-python',
+    license='Apache Software License 2.0',
+    include_package_data=True,
+    packages=find_packages(exclude=["*.tests"]),
+    keywords=['sevenbridges', 'sbg', 'api', 'cgc', 'cancer', 'genomics',
+              'cloud'],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
---
+++ packages/sevenbridges-python/sevenbridges-python-0.24.2.tar.gz/sevenbridges-python-0.24.2/sevenbridges/__init__.py
@@ -0,0 +1,85 @@
+"""
+sevenbridges-python
+~~~~~~~~~~~~~~~~~~~
+:copyright: 2018 Seven Bridges Genomics Inc.
+:license: Apache 2.0
+"""
+import io
+import os
+import ssl
+import logging
+
+
+__version__ = '0.0.1+local-build'
+if os.path.isfile('VERSION'):
+    with io.open('VERSION', 'r', encoding='utf-8') as f:
+        __version__ = f.read()
+
+
+from sevenbridges.api import Api
+from sevenbridges.config import Config
+
+from sevenbridges.models.invoice import Invoice
+from sevenbridges.models.billing_group import (
+    BillingGroup, BillingGroupBreakdown
+)
+from sevenbridges.models.user import User
+from sevenbridges.models.endpoints import Endpoints
+from sevenbridges.models.project import Project
+from sevenbridges.models.task import Task
+from sevenbridges.models.app import App
+from sevenbridges.models.dataset import Dataset
+from sevenbridges.models.bulk import BulkRecord
+from sevenbridges.models.team import Team, TeamMember
+from sevenbridges.models.member import Member, Permissions
+from sevenbridges.models.file import File
+from sevenbridges.models.storage_export import Export
+from sevenbridges.models.storage_import import Import
+from sevenbridges.models.volume import Volume
+from sevenbridges.models.marker import Marker
+from sevenbridges.models.division import Division
+from sevenbridges.models.automation import (
+    Automation, AutomationRun, AutomationPackage, AutomationMember
+)
+from sevenbridges.models.async_jobs import AsyncJob
+
+from sevenbridges.models.enums import (
+    AppCopyStrategy, AppRawFormat, AsyncFileOperations, AsyncJobStates,
+    AutomationRunActions, DivisionRole, FileStorageType, ImportExportState,
+    TaskStatus, TransferState, VolumeAccessMode, VolumeType,
+)
+from sevenbridges.errors import (
+    SbgError, ResourceNotModified, ReadOnlyPropertyError, ValidationError,
+    TaskValidationError, PaginationError, BadRequest, Unauthorized, Forbidden,
+    NotFound, Conflict, TooManyRequests, ServerError, ServiceUnavailable,
+    MethodNotAllowed, RequestTimeout, LocalFileAlreadyExists,
+    ExecutionDetailsInvalidTaskType
+)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = [
+    'Api', 'AsyncJob', 'Automation', 'AutomationRun', 'AutomationMember',
+    'AutomationPackage',  'Config', 'Invoice', 'BillingGroup',
+    'BillingGroupBreakdown', 'User', 'Endpoints', 'Project', 'Task', 'App',
+    'Member', 'Permissions', 'File', 'Export', 'Import', 'Volume', 'Marker',
+    'Division', 'Team', 'TeamMember', 'Dataset', 'BulkRecord',
+    # Enums
+    'AppCopyStrategy', 'AppRawFormat', 'AppCopyStrategy',
+    'AsyncFileOperations', 'AsyncJobStates', 'AutomationRunActions',
+    'DivisionRole', 'FileStorageType', 'ImportExportState', 'TaskStatus',
+    'TransferState', 'VolumeAccessMode', 'VolumeType',
+    # Errors
+    'SbgError', 'ResourceNotModified', 'ReadOnlyPropertyError',
+    'ValidationError', 'TaskValidationError', 'PaginationError', 'BadRequest',
+    'Unauthorized', 'Forbidden', 'NotFound', 'Conflict', 'TooManyRequests',
+    'ServerError', 'ServiceUnavailable', 'MethodNotAllowed', 'RequestTimeout',
+    'LocalFileAlreadyExists', 'ExecutionDetailsInvalidTaskType'
+]
+
+required_ssl_version = (1, 0, 1)
+if ssl.OPENSSL_VERSION_INFO < required_ssl_version:
+    raise SbgError(
+        'OpenSSL version included in this python version must be '
+        'at least 1.0.1 or greater. Please update your environment build.'
+    )
