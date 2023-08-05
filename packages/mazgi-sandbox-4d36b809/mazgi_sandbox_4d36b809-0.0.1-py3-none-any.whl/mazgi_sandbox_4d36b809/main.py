b312f4e9dd7cb3dee69b6d78171c6609896c97b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package mazgi-sandbox-4d36b809/0.0.1/mazgi_sandbox_4d36b809-0.0.1-py3-none-any.whl

---
+++ packages/mazgi-sandbox-4d36b809/mazgi_sandbox_4d36b809-0.0.1-py3-none-any.whl/mazgi_sandbox_4d36b809/main.py
@@ -0,0 +1,6 @@
+from .one import one_class
+
+
+def main():
+    obj = one_class.OneClass()
+    obj.use_two()
---
+++ packages/mazgi-sandbox-4d36b809/mazgi_sandbox_4d36b809-0.0.1-py3-none-any.whl/mazgi_sandbox_4d36b809/two/two_class.py
@@ -0,0 +1,5 @@
+class TwoClass:
+    def func(self):
+        msg = "func() in two"
+        print(msg)
+        return msg
