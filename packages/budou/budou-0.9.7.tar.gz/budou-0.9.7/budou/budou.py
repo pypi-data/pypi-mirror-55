9bf7273f937be295ef25d05cf57921e7f849ba02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:58 +0000

    Add package budou/0.9.7/budou-0.9.7.tar.gz

---
+++ packages/budou/budou-0.9.7.tar.gz/budou-0.9.7/budou/budou.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Budou: an automatic organizer tool for beautiful line breaking in CJK
+
+Usage:
+  budou [--segmenter=<seg>] [--language=<lang>] [--classname=<class>] [--inlinestyle] <source>
+  budou -h | --help
+  budou -v | --version
+
+
+Options:
+  -h --help                   Show this screen.
+
+  -v --version                Show version.
+
+  --segmenter=<segmenter>     Segmenter to use [default: nlapi].
+
+  --language=<language>       Language the source in.
+
+  --classname=<classname>     Class name for output SPAN tags. Use
+                              comma-separated value to specify multiple classes.
+
+  --inlinestyle               Add :code:`display:inline-block` as inline style
+                              attribute.
+"""
+
+from __future__ import print_function
+
+import sys
+import warnings
+from docopt import docopt
+from .parser import get_parser
+from .__version__ import __version__
+
+AVAILABLE_SEGMENTERS = {'nlapi', 'mecab'}
+
+def main():
+  """Budou main method for the command line tool.
+  """
+  args = docopt(__doc__)
+  if args['--version']:
+    print(__version__)
+    sys.exit()
+
+  result = parse(
+      args['<source>'],
+      segmenter=args['--segmenter'],
+      language=args['--language'],
+      classname=args['--classname'],
+      inlinestyle=args['--inlinestyle'],
+      )
+  print(result['html_code'])
+  sys.exit()
+
+def parse(source, segmenter='nlapi', language=None, max_length=None,
+          classname=None, attributes=None, inlinestyle=False, **kwargs):
+  """Parses input source.
+
+  Args:
+    source (str): Input source to process.
+    segmenter (str, optional): Segmenter to use [default: nlapi].
+    language (str, optional): Language code.
+    max_length (int, optional): Maximum length of a chunk.
+    classname (str, optional): Class name of output SPAN tags.
+    attributes (dict, optional): Attributes for output SPAN tags.
+    inlinestyle (bool, optional): Add :code:`display:inline-block` as inline
+                                  style attribute.
+
+  Returns:
+    Results in a dict. :code:`chunks` holds a list of chunks
+    (:obj:`budou.chunk.ChunkList`) and :code:`html_code` holds the output HTML
+    code.
+  """
+  parser = get_parser(segmenter, **kwargs)
+  return parser.parse(
+      source, language=language, max_length=max_length, classname=classname,
+      attributes=attributes, inlinestyle=inlinestyle)
+
+def authenticate(json_path=None):
+  """Gets a Natural Language API parser by authenticating the API.
+
+  **This method is deprecated.** Please use :obj:`budou.parser.get_parser` to
+  obtain a parser instead.
+
+  Args:
+    json_path (str, optional): The file path to the service account's
+        credentials.
+
+  Returns:
+    Parser. (:obj:`budou.parser.NLAPIParser`)
+
+  """
+  msg = ('budou.authentication() is deprecated. '
+         'Please use budou.parser.get_parser() to obtain a parser instead.')
+  warnings.warn(msg, DeprecationWarning)
+  parser = get_parser('nlapi', credentials_path=json_path)
+  return parser
+
+if __name__ == '__main__':
+  main()
---
+++ packages/budou/budou-0.9.7.tar.gz/budou-0.9.7/budou/cachefactory.py
@@ -0,0 +1,157 @@
+# Copyright 2017 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Budou cache factory class."""
+from abc import ABCMeta, abstractmethod
+import os
+import pickle
+import six
+
+
+def load_cache(filename=None):
+  """Returns a cache service.
+
+  If Google App Engine Standard Environment's memcache is available, this uses
+  memcache as the backend. Otherwise, this uses :obj:`pickle` to cache
+  the outputs in the local file system.
+
+  Args:
+    filename (str, optional): The file path to the cache file. This is
+        used only when :obj:`pickle` is used as the backend.
+
+  Returns:
+    A cache system (:obj:`budou.cachefactory.BudouCache`)
+  """
+  try:
+    return AppEngineMemcache()
+  except ImportError:
+    return PickleCache(filename)
+
+
+@six.add_metaclass(ABCMeta)
+class BudouCache:
+  """Base class for cache system.
+  """
+
+  @abstractmethod
+  def get(self, key):
+    """Abstract method: Gets a value by a key.
+
+    Args:
+      key (str): Key to retrieve the value.
+
+    Returns:
+      Retrieved value (str or None).
+
+    Raises:
+      NotImplementedError: If it's not implemented.
+    """
+    raise NotImplementedError()
+
+  @abstractmethod
+  def set(self, key, val):
+    """Abstract method: Sets a value in a key.
+
+    Args:
+      key (str): Key for the value.
+      val (str): Value to set.
+
+    Raises:
+      NotImplementedError: If it's not implemented.
+    """
+    raise NotImplementedError()
+
+
+class PickleCache(BudouCache):
+  """Cache system with :obj:`pickle` backend.
+
+  Args:
+    filename (str): The file path to the cache file.
+
+  Attributes:
+    filename (str): The file path to the cache file.
+  """
+
+  DEFAULT_FILE_NAME = '/tmp/budou-cache.pickle'
+  """ The default path to the cache file.
+  """
+
+  def __init__(self, filename):
+    self.filename = filename if filename else self.DEFAULT_FILE_NAME
+
+  def get(self, key):
+    """Gets a value by a key.
+
+    Args:
+      key (str): Key to retrieve the value.
+
+    Returns: Retrieved value (str or None).
+    """
+    self._create_file_if_none_exists()
+    with open(self.filename, 'rb') as file_object:
+      cache_pickle = pickle.load(file_object)
+      val = cache_pickle.get(key, None)
+      return val
+
+  def set(self, key, val):
+    """Sets a value in a key.
+
+    Args:
+      key (str): Key for the value.
+      val (str): Value to set.
+    """
+    self._create_file_if_none_exists()
+    with open(self.filename, 'r+b') as file_object:
+      cache_pickle = pickle.load(file_object)
+      cache_pickle[key] = val
+      file_object.seek(0)
+      pickle.dump(cache_pickle, file_object)
+
+  def _create_file_if_none_exists(self):
+    if os.path.exists(self.filename):
+      return
+    with open(self.filename, 'wb') as file_object:
+      pickle.dump({}, file_object)
+
+
+class AppEngineMemcache(BudouCache):
+  """Cache system with :obj:`google.appengine.api.memcache` backend.
+
+  Attributes:
+    memcache (:obj:`google.appengine.api.memcache`): Memcache service.
+  """
+
+  def __init__(self):
+    from google.appengine.api import memcache
+    self.memcache = memcache
+
+  def get(self, key):
+    """Gets a value by a key.
+
+    Args:
+      key (str): Key to retrieve the value.
+
+    Returns:
+      Retrieved value (str or None).
+    """
+    return self.memcache.get(key, None)
+
+  def set(self, key, val):
+    """Sets a value in a key.
+
+    Args:
+      key (str): Key for the value.
+      val (str): Value to set.
+    """
+    self.memcache.set(key, val)
