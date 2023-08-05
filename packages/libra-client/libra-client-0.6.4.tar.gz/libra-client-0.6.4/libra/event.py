8b44737ecbc869a0a1af0d1a5a518c159a351b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package libra-client/0.6.4/libra-client-0.6.4.tar.gz

---
+++ packages/libra-client/libra-client-0.6.4.tar.gz/libra-client-0.6.4/libra/event.py
@@ -0,0 +1,39 @@
+from canoser import *
+from libra.hasher import gen_hasher
+from libra.language_storage import TypeTag
+
+
+EVENT_KEY_LENGTH = 32
+
+class EventKey(DelegateT):
+    delegate_type = [Uint8, EVENT_KEY_LENGTH]
+
+
+class EventHandle(Struct):
+    _fields = [
+        ('count', Uint64),
+        ('key', EventKey)
+    ]
+
+
+class ContractEvent(Struct):
+    _fields = [
+        ('key', EventKey),
+        ('sequence_number', Uint64),
+        ('type_tag', TypeTag),
+        ('event_data', [Uint8])
+    ]
+
+    @classmethod
+    def from_proto(cls, proto):
+        ret = cls()
+        ret.key = bytes_to_int_list(proto.key)
+        ret.sequence_number = proto.sequence_number
+        ret.type_tag = TypeTag.deserialize(proto.type_tag)
+        ret.event_data = bytes_to_int_list(proto.event_data)
+        return ret
+
+    def hash(self):
+        shazer = gen_hasher(b"ContractEvent")
+        shazer.update(self.serialize())
+        return shazer.digest()
