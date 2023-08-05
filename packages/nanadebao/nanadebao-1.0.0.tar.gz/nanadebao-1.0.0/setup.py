3cd67d8fb652cc1c4794e5ffd6266d69f61c160f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package nanadebao/1.0.0/nanadebao-1.0.0.tar.gz

---
+++ packages/nanadebao/nanadebao-1.0.0.tar.gz/nanadebao-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name              =      "nanadebao",
+    version           =      "1.0.0",
+    py_modules        =      ["nanadebao"],
+    author            =      "hfpython",
+    author_email      =      "hfpython@headfirstlabs.com",
+    url               =      "http://www.headfirstlabs.com",
+    description       =      "A simple printer of nested lists",
+    )
