c6e7c25c79a9d0af297f00e0c37783405ca44677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:14 +0000

    Add package deploy-task-definitions/0.0.6/deploy_task_definitions-0.0.6-py3.7.egg

---
+++ packages/deploy-task-definitions/deploy_task_definitions-0.0.6-py3.7.egg/src/EventsClient.py
@@ -0,0 +1,29 @@
+import boto3
+
+class EventsClient:
+
+    def __init__(self, region):
+        self.client = boto3.client('events',region_name=region)
+
+    def list_targets_by_rule(self,rule):
+        response = self.client.list_targets_by_rule(
+            Rule=rule
+        )
+        return response
+
+    def putTargets(self,id,cluster_arn, cloudwatch_rule, task_definition_arn, roleArn):
+        response = self.client.put_targets(
+            Rule=cloudwatch_rule,
+            Targets=[
+                {
+                    'Id': "{}-event-target".format(id),
+                    "Arn": cluster_arn,
+                    "RoleArn": roleArn,
+                    'EcsParameters': {
+                         'TaskDefinitionArn': task_definition_arn,
+                         'TaskCount': 1
+                    }
+                }
+            ]
+        )
+        return response
