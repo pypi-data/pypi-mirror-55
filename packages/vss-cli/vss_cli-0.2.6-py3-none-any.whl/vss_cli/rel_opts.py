cf4897ee9db0694152990c95ce65dc2a77e820a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:17 +0000

    Add package vss-cli/0.2.6/vss_cli-0.2.6-py3-none-any.whl

---
+++ packages/vss-cli/vss_cli-0.2.6-py3-none-any.whl/vss_cli/rel_opts.py
@@ -0,0 +1,33 @@
+import click
+
+filter_opt = click.option(
+    '-f',
+    '--filter-by',
+    type=click.Tuple([click.STRING, click.STRING]),
+    default=(None, None),
+    help='filter list by <field_name> <operator>,<value>',
+)
+sort_opt = click.option(
+    '-s',
+    '--sort',
+    type=click.Tuple([click.STRING, click.STRING]),
+    default=(None, None),
+    help='sort by <field_name> <asc|desc>',
+)
+
+all_opt = click.option(
+    '-a',
+    '--show-all',
+    is_flag=True,
+    help='show all results',
+    show_default=True,
+)
+count_opt = click.option(
+    '-c', '--count', type=click.INT, help='size of results', default=50
+)
+page_opt = click.option(
+    '-p', '--page', is_flag=True, help='page results in a less-like format'
+)
+wait_opt = click.option(
+    '--wait', is_flag=True, help='wait for request to complete'
+)
