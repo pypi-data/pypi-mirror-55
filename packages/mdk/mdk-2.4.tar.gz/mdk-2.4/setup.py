b17fbc961b85c5072785d7da3278b0e75a25f3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package mdk/2.4/mdk-2.4.tar.gz

---
+++ packages/mdk/mdk-2.4.tar.gz/mdk-2.4/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+"""build/set up mdk"""
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author='Michael Darr',
+    author_email='mdarr@matician.com',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    description='a docker-compose helper',
+    entry_points={
+        'console_scripts': ['mdk=mdk.main:mdk'],
+    },
+    install_requires=[
+        'Click>=7',
+        'docker-compose>=1.22',
+        'pyyaml',
+    ],
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name='mdk',
+    packages=find_packages(),
+    python_requires='>=3',
+    url='https://matician.com/',
+    version='2.4',
+)
