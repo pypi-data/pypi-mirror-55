ca0f1fa71068c39c9d23ba04d668c111bcad3b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:44 +0000

    Add package aipi/0.1.0/aipi-0.1.0.tar.gz

---
+++ packages/aipi/aipi-0.1.0.tar.gz/aipi-0.1.0/README.md
@@ -0,0 +1,2 @@
+# aipi
+python 开源命令行 AI P图工具。
---
+++ packages/aipi/aipi-0.1.0.tar.gz/aipi-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aipi",
+    version="0.1.0",
+    author="kenblikylee",
+    author_email="kenblikylee@126.com",
+    description="python 开源命令行 AI P图工具。",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kenblikylee/aipi.git",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'aipi = aipi.cli:main'
+        ]
+    },
+    python_requires='>=3.6',
+)
