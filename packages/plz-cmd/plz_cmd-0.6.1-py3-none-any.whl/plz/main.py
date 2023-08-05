2efe48656f10e073c602035d837d4a8b1371884b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:01 +0000

    Add package plz-cmd/0.6.1/plz_cmd-0.6.1-py3-none-any.whl

---
+++ packages/plz-cmd/plz_cmd-0.6.1-py3-none-any.whl/plz/main.py
@@ -0,0 +1,67 @@
+import argparse
+import sys
+
+from colorama import Fore
+from colorama import Style
+
+from .config import plz_config
+from .runner import gather_and_run_commands
+
+
+def usage():
+    print(Fore.BLUE + "Usage:\nplz <command> [<additional arguments> ...]")
+    print(Style.RESET_ALL)
+
+
+def invalid_directory():
+    print(
+        "plz must be run from a directory that has a plz.yaml file or from "
+        "within a git repo that contains a plz.yaml in the repo root path."
+    )
+    print()
+    print("For more information, visit https://github.com/m3brown/plz")
+
+
+def list_options(config):
+    options = sorted([task["id"] for task in config])
+    print("Available commands from config:")
+    for cmd in options:
+        print(" - {cmd}".format(cmd=cmd))
+
+
+def execute_from_config(cmd, args):
+    (config, cwd) = plz_config()
+
+    for task in config:
+        if "id" in task and task["id"] == cmd:
+            if "cmd" in task:
+                rc = gather_and_run_commands(task["cmd"], cwd=cwd, args=args)
+                sys.exit(rc)
+    if cmd and cmd.lower() == "help":
+        usage()
+        list_options(config)
+    else:
+        print(Fore.RED + "Could not find command with id '{}'".format(cmd))
+        print(Style.RESET_ALL)
+        list_options(config)
+    sys.exit(1)
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("cmd", nargs="?", default="help")
+    parser.add_argument("passthrough_args", nargs=argparse.REMAINDER)
+
+    if len(args) < 1:
+        (config, cwd) = plz_config()
+        print()
+        usage()
+        list_options(config)
+        sys.exit(1)
+
+    args = parser.parse_args(args)
+
+    execute_from_config(args.cmd, args.passthrough_args)
