527e4edf67b49a7aed06312b0434103cd0d068e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:28 +0000

    Add package deal/3.2.0/deal-3.2.0.tar.gz

---
+++ packages/deal/deal-3.2.0.tar.gz/deal-3.2.0/deal/_decorators/pre.py
@@ -0,0 +1,19 @@
+# app
+from .._exceptions import PreContractError
+from .._types import ExceptionType
+from .base import Base
+
+
+class Pre(Base):
+    """
+    Check contract (validator) before function processing.
+    Validate input arguments.
+    """
+    exception: ExceptionType = PreContractError
+
+    def patched_function(self, *args, **kwargs):
+        """
+        Step 3. Wrapped function calling.
+        """
+        self.validate(*args, **kwargs)
+        return self.function(*args, **kwargs)
