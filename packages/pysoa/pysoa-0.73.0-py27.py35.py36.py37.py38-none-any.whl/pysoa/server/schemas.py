ca5716a308879421d3ae9500c9602ddffff5834d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:50 +0000

    Add package pysoa/0.73.0/pysoa-0.73.0-py27.py35.py36.py37.py38-none-any.whl

---
+++ packages/pysoa/pysoa-0.73.0-py27.py35.py36.py37.py38-none-any.whl/pysoa/server/schemas.py
@@ -0,0 +1,47 @@
+from __future__ import (
+    absolute_import,
+    unicode_literals,
+)
+
+from conformity.fields import (
+    Boolean,
+    Dictionary,
+    Integer,
+    List,
+    SchemalessDictionary,
+    UnicodeString,
+)
+
+
+ActionRequestSchema = Dictionary(
+    {
+        'action': UnicodeString(),
+        'body': SchemalessDictionary(key_type=UnicodeString()),
+    },
+    optional_keys=('body', ),
+)
+
+# TODO Update this
+ControlHeaderSchema = Dictionary(
+    {
+        'continue_on_error': Boolean(),
+    },
+    allow_extra_keys=True,
+)
+
+# TODO Update this
+ContextHeaderSchema = Dictionary(
+    {
+        'switches': List(Integer()),
+        'correlation_id': UnicodeString(),
+    },
+    allow_extra_keys=True,
+)
+
+JobRequestSchema = Dictionary(
+    {
+        'control': ControlHeaderSchema,
+        'context': ContextHeaderSchema,
+        'actions': List(ActionRequestSchema, min_length=1),
+    },
+)
