53d7b286ad1ade10cea300212c0769119a74f913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:59 +0000

    Add package volux/0.9.6/volux-0.9.6-py3-none-any.whl

---
+++ packages/volux/volux-0.9.6-py3-none-any.whl/volux/__init__.py
@@ -0,0 +1,11 @@
+from .operator import *
+from .module import *
+from .core import *
+from .demo import *
+
+__version__     = '0.0.1'
+__description__ = 'Core for Volux media tools.'
+__url__         = 'http://github.com/drtexx/volux'
+__author__      = 'Denver P'
+# __authoremail__ = ''
+__license__     = 'GPLv3+'
