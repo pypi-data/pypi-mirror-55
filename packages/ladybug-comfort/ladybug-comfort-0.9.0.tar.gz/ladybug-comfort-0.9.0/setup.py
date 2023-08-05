8d11e48e59ab8c8a1416ddaf73e02806b53da6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:11 +0000

    Add package ladybug-comfort/0.9.0/ladybug-comfort-0.9.0.tar.gz

---
+++ packages/ladybug-comfort/ladybug-comfort-0.9.0.tar.gz/ladybug-comfort-0.9.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ladybug-comfort",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="Ladybug comfort is a Python library that adds thermal comfort functionalities to Ladybug.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/ladybug-comfort",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'lbt-ladybug>=0.1.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
+)
