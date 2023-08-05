d437f18295c274251f1d35d75b85a681e6cb77ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:55 +0000

    Add package kcet-pyseba/0.0.18/kcet-pyseba-0.0.18.tar.gz

---
+++ packages/kcet-pyseba/kcet-pyseba-0.0.18.tar.gz/kcet-pyseba-0.0.18/pykcet/__init__.py
@@ -0,0 +1,9 @@
+class pythonmsg:
+    def __init__(self):
+        self.text=["Good Luck"]
+    def kprint(self):
+        return "KAMARAJ COLLEGE OF ENGINEERING AND TECHNOLOGY        An Autonomous Institution           "
+    def intern(self):
+        return "Interning is done for performance optimization"
+       
+
