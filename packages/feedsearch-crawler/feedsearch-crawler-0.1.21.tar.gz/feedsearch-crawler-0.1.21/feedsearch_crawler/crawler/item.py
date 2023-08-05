f9d2622ab1f725db873627ea93586f74bafb0dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package feedsearch-crawler/0.1.21/feedsearch-crawler-0.1.21.tar.gz

---
+++ packages/feedsearch-crawler/feedsearch-crawler-0.1.21.tar.gz/feedsearch-crawler-0.1.21/feedsearch_crawler/crawler/item.py
@@ -0,0 +1,11 @@
+import logging
+from abc import ABC
+
+
+class Item(ABC):
+    ignore_item = False
+
+    def __init__(self, **kwargs):
+        for k in kwargs.keys():
+            if hasattr(self, k):
+                self.__setattr__(k, kwargs[k])
