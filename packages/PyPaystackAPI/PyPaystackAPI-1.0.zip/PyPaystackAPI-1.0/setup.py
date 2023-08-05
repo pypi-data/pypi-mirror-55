8bc0757552b6e12cfa64ffc16ccac9c87a76ac43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package PyPaystackAPI/1.0/PyPaystackAPI-1.0.zip

---
+++ packages/PyPaystackAPI/PyPaystackAPI-1.0.zip/PyPaystackAPI-1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name = 'PyPaystackAPI',  
+  packages = ['PyPaystackAPI'],   
+  version = '1.0',
+  license='MIT',    
+  description = 'A python API to easily access important paystack services.',
+  author = 'Mobolaji Abdulsalam',                   
+  author_email = 'ibraheemabdulsalam@gmail.com',
+  url = 'https://github.com/moriire/PyPaystackAPI',  
+  download_url = 'https://github.com/moriire/PyPaystackAPI/archive/v_10.tar.gz',
+  keywords = ['python payment wrapper','paystack payment API', 'python paystack library', 'python payment library'],
+  install_requires=['requests'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],)
