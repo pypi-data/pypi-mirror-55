4baa7a82aa54990e02277165d66f7dd7f5134424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package PyABridge/1.0.4/PyABridge-1.0.4.tar.gz

---
+++ packages/PyABridge/PyABridge-1.0.4.tar.gz/PyABridge-1.0.4/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+setup(
+    name='PyABridge',
+    packages=['PyABridge'],
+    version='1.0.4',
+    license='MIT',
+    description='',
+    author='Arpado Software',
+    author_email='support@arpado.site',
+    url='https://github.com/ArpadoSoftware/PyABridge',
+    download_url='https://github.com/ArpadoSoftware/PyABridge/dist/PyABridge-1.0.tar.gz',
+    keywords=['VST', 'MIDI', 'AUDIO', 'ABRIDGE', 'ARPADO'],
+    install_requires=[
+        'PyDispatcher'
+    ],
+    package_dir={'PyABridge': 'PyABridge'},
+    data_files=[('.', [
+        'PyABridge/_ABridgeAdapter.pyd',
+        'PyABridge/_ABridgeAdapter.so',
+        'PyABridge/libwinpthread-1.dll',
+    ])],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    include_package_data=True
+)
