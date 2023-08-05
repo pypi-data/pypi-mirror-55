03683bbff5d6f37a4b618c2a4dfcbea9b14f0f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:21 +0000

    Add package broccoli-server/0.1.4/broccoli_server-0.1.4.tar.gz

---
+++ packages/broccoli-server/broccoli_server-0.1.4.tar.gz/broccoli_server-0.1.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'jinja2==2.10.1',
+    'flask==1.0.3',
+    'pika==1.0.1',
+    'pymongo==3.8.0',
+    'flask-cors==3.0.8',
+    'flask-jwt-extended==3.18.2',
+    'dnspython==1.16.0',
+    'jsonschema==3.0.1',
+    'apscheduler==3.6.0'
+]
+
+tests_require = [
+    'mongomock==3.17.0',
+    'freezegun==0.3.12'
+]
+
+setup(
+    name='broccoli_server',
+    version='0.1.4',
+    description='The server component of a web content crawling and sorting framework',
+    url='http://github.com/KTachibanaM/broccoli-platform',
+    author='KTachibanaM',
+    author_email='whj19931115@gmail.com',
+    license='WTFPL',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite="broccoli_server.tests",
+)
