0e0e3d7342728afcac6fd8ab75b45a40e5c8d804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package sqlu/0.1.0/sqlu-0.1.0-py3-none-any.whl

---
+++ packages/sqlu/sqlu-0.1.0-py3-none-any.whl/sqlu/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+import re
+from flatten_json import flatten
+from moz_sql_parser import parse
+
+def remove_comments(sql):
+    ## remove the /* */ comments
+    sql2 = re.sub(r"/\*[^*]*\*+(?:[^*/][^*]*\*+)*/", "", sql)
+
+    # remove whole line -- and # comments
+    lines = [line for line in sql2.splitlines() if not re.match("^\s*(--|#)", line)]
+    # remove trailing -- and # comments
+    return " ".join([re.split("--|#", line)[0] for line in lines])
+    
+def extract_tables_from_select(sql):
+    """extract tables and views from a select statement"""
+    pattern = "from(_\d+)?(.*join)?$"
+    sql_dict = flatten(parse(sql))
+    return [value for key, value in sql_dict.items() if re.search(pattern, key)]
