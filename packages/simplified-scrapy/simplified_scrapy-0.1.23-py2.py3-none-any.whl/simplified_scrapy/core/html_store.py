70c7f2d577919c9f498bf58b27175813c1b75a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:33 +0000

    Add package simplified-scrapy/0.1.23/simplified_scrapy-0.1.23-py2.py3-none-any.whl

---
+++ packages/simplified-scrapy/simplified_scrapy-0.1.23-py2.py3-none-any.whl/simplified_scrapy/core/html_store.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#coding=utf-8
+import os,io
+import sys
+if sys.version_info.major == 2:
+  from utils import printInfo,getTimeNow,md5
+else:
+  from .utils import printInfo,getTimeNow,md5
+class HtmlStore:
+  _htmls=[]
+  _htmlPath='htmls/{}/'
+  def __init__(self, name):
+    self._htmlPath=self._htmlPath.format(name)
+    if(not os.path.exists('htmls/')):
+      os.mkdir('htmls/')
+    if(not os.path.exists(self._htmlPath)):
+      os.mkdir(self._htmlPath)
+  def saveHtml(self,url,html):
+    self._htmls.append({"url":url,"html":html})
+    if(isinstance(url,str)):
+      self._saveHtml(url,html)
+    else:
+      self._saveHtml(url["url"],html)
+
+  def popHtml(self,state=0):
+    if(len(self._htmls)>0):
+      return self._htmls.pop()
+
+  def _saveHtml(self,url,html):
+    filename = md5(url)+'.htm'   
+    file = io.open(self._htmlPath+filename, "w",encoding="utf-8")
+    file.write(html)
+    file.close()
+
+  def updateState(self,id,state):
+    pass
