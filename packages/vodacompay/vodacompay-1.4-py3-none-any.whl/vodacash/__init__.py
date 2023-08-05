d60702c8044787436d4b02295fc8cf93bd009260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package vodacompay/1.4/vodacompay-1.4-py3-none-any.whl

---
+++ packages/vodacompay/vodacompay-1.4-py3-none-any.whl/vodacash/__init__.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+import requests
+import json
+from lxml import etree
+
+CallbackResponse = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<response>
+<dataItem>
+<name>ResponseCode<</name>
+<type>String</type>
+<value>200</value>
+</dataItem>
+</response>
+"""
+
+
+def strdate(dt):
+    ax = str(dt)
+    return ax.split('.')[0].replace('-', '').replace(' ', '').replace(':', '')
+
+
+def parse_async_result(content):
+    root = etree.from_string(content)
+    js_out = {}
+    keys = []
+    for dataItem in root.xpath("//dataItem"):
+        name, t, value = dataItem.getchildren()
+        keys.append({name.text: value.text})
+    [js_out.update(ks) for ks in keys]
+    return js_out
+
+
+class Vodacash(object):
+    def __init__(self, username, password, callback_url):
+        self.LOGIN_URL = "http://167.71.65.114/api/v1/login"
+        self.C2B_URL = "http://167.71.65.114/api/v1/c2b"
+        self.Username = username
+        self.Password = password
+        self.token = None
+        self.shortcode = None
+        self.callback_channel = 2
+        self.callback_url = callback_url
+        self.authenticate()
+
+    def authenticate(self):
+        result = requests.post(
+            self.LOGIN_URL, json={"Username": self.Username, "Password": self.Password}).content
+        result = json.loads(result)
+        self.token = result["token"]
+
+    def c2b(self, customer_msisdn, amount):
+        result = requests.post(
+            self.C2B_URL,
+            json={
+                "Amount": amount,
+                "CustomerMSISDN": customer_msisdn,
+                "Date": strdate(datetime.utcnow()),
+                "token": str(self.token),
+            }
+        ).content
+        result = json.loads(result)
+        return result
