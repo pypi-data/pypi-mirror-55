1ac3fc77676215f15c5a15c2f0fcabb76fb98fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:38 +0000

    Add package humannotator/0.0.2/humannotator-0.0.2.tar.gz

---
+++ packages/humannotator/humannotator-0.0.2.tar.gz/humannotator-0.0.2/humannotator/utils.py
@@ -0,0 +1,17 @@
+class Base(object):
+    @staticmethod
+    def _check_input(name, x, cls):
+        if not isinstance(x, cls):
+            raise TypeError(
+                f"`{name}` must be of type {cls.__name__}."
+                )
+
+    def __repr__(self):
+        items = (f"{i.strip('_')}={vars(self)[i]!r}" for i in vars(self))
+        return f"{self.__class__.__name__}({', '.join(items)})"
+
+
+def option(character, instruction, newline=True):
+    if newline:
+        return f"[{character}] {instruction}  \n"
+    return f"[{character}] {instruction}"
