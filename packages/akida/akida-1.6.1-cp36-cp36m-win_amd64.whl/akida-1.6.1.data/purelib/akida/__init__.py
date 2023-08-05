46425c6882d46060fbe27c5931f5859e47cbf558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:32 +0000

    Add package akida/1.6.1/akida-1.6.1-cp36-cp36m-win_amd64.whl

---
+++ packages/akida/akida-1.6.1-cp36-cp36m-win_amd64.whl/akida-1.6.1.data/purelib/akida/__init__.py
@@ -0,0 +1,21 @@
+from .core import (Sparse, Tensor, BackendType, ConvolutionMode,
+                   PoolingType, LearningType)
+
+from .layer import *
+from .input_data import InputData
+from .fully_connected import FullyConnected
+from .convolutional import Convolutional
+from .separable_convolutional import SeparableConvolutional
+from .input_convolutional import InputConvolutional
+from .input_bcspike import InputBCSpike
+from .model import Model
+
+Layer.set_variable = set_variable
+Layer.get_variable = get_variable
+Layer.get_variable_names = get_variable_names
+Layer.get_observer = get_observer
+Layer.get_statistics = get_statistics
+Layer.get_learning_histogram = get_learning_histogram
+
+from .inputs import coords_to_sparse, dense_to_sparse
+from .observer import Observer
