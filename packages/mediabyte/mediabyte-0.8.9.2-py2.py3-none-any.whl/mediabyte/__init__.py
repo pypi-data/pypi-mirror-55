63c154d30e5f1df19f6e2eb14a502f6253c5e64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:20 +0000

    Add package mediabyte/0.8.9.2/mediabyte-0.8.9.2-py2.py3-none-any.whl

---
+++ packages/mediabyte/mediabyte-0.8.9.2-py2.py3-none-any.whl/mediabyte/__init__.py
@@ -0,0 +1,19 @@
+from .files.yota import Yota, Cue, Sample, Mixtape
+from .files.bit import Link, Mp3
+from .files.lib import Convert
+#from .files.yota_fuzz import main as yota_fuzz
+#from .files.bit_fuzz import main as bit_fuzz
+#from .files.ono import check_ono as ono
+from .files.onoObject import MediabyteHashObj
+
+
+omm = Convert.omm
+youtube = Convert.search_to_mixtape
+search = Convert.search
+history = Convert.search("")
+regex = Convert.regex_search
+
+input = Convert.omm
+version = Convert.version()
+readme = Convert.readme()
+docs = Convert.docs
