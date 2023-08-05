90581dba1bc5b2de4181f88c4fadfad6c91764cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:40 +0000

    Add package python-syra/0.4/python-syra-0.4.tar.gz

---
+++ packages/python-syra/python-syra-0.4.tar.gz/python-syra-0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+version = "0.4"
+
+setup(
+    name="python-syra",
+    version=version,
+    url="http://www.touchtechnology.com.au/",
+    author="Gary Reynolds",
+    author_email="gary@touch.asn.au",
+    description="Client for the Syra API.",
+    install_requires=[
+        "click",
+        "first",
+        "future",
+        "python-dateutil",
+        "suds-community",
+        "ujson",
+    ],
+    packages=["syra"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: Name Service (DNS)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    entry_points={"console_scripts": ["syra = syra.cli:main"]},
+)
