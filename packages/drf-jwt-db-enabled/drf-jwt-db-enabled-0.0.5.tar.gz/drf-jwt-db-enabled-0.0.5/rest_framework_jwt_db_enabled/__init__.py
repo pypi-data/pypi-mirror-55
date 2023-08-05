701998eca5c23b894ad832bb1aad8ba323789ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:11 +0000

    Add package drf-jwt-db-enabled/0.0.5/drf-jwt-db-enabled-0.0.5.tar.gz

---
+++ packages/drf-jwt-db-enabled/drf-jwt-db-enabled-0.0.5.tar.gz/drf-jwt-db-enabled-0.0.5/rest_framework_jwt_db_enabled/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'drf-jwt-db-enabled'
+__version__ = '1.11.0'
+__author__ = 'José Padilla'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014-2017 Blimp LLC'
+
+# Version synonym
+VERSION = __version__
