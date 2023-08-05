000080a5e6928882294b464d85e2dd09ed11af01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package descarteslabs/0.26.0/descarteslabs-0.26.0.tar.gz

---
+++ packages/descarteslabs/descarteslabs-0.26.0.tar.gz/descarteslabs-0.26.0/descarteslabs/workflows/types/math/__init__.py
@@ -0,0 +1,14 @@
+from .arithmetic import cos, log, log2, log10, normalized_difference, sin, sqrt, tan
+
+
+__all__ = [
+    # .arithmetic
+    "log",
+    "log2",
+    "log10",
+    "sqrt",
+    "cos",
+    "sin",
+    "tan",
+    "normalized_difference",
+]
---
+++ packages/descarteslabs/descarteslabs-0.26.0.tar.gz/descarteslabs-0.26.0/descarteslabs/workflows/types/math/arithmetic.py
@@ -0,0 +1,97 @@
+from ..core import typecheck_promote
+from ..primitives import Int, Float, Number
+from ..geospatial import Image
+from ..geospatial import ImageCollection
+
+
+@typecheck_promote((Int, Float, Image, ImageCollection))
+def log(obj):
+    """
+    Element-wise natural log of an `Image` or `ImageCollection`.
+
+    Can also be used with `Int` and `Float` types.
+    """
+    return_type = Float if isinstance(obj, Number) else type(obj)
+    return return_type._from_apply("log", obj)
+
+
+@typecheck_promote((Int, Float, Image, ImageCollection))
+def log2(obj):
+    """
+    Element-wise base 2 log of an `Image` or `ImageCollection`.
+
+    Can also be used with `Int` and `Float` types.
+    """
+    return_type = Float if isinstance(obj, Number) else type(obj)
+    return return_type._from_apply("log2", obj)
+
+
+@typecheck_promote((Int, Float, Image, ImageCollection))
+def log10(obj):
+    """
+    Element-wise base 10 log of an `Image` or `ImageCollection`.
+
+    Can also be used with `Int` and `Float` types.
+    """
+    return_type = Float if isinstance(obj, Number) else type(obj)
+    return return_type._from_apply("log10", obj)
+
+
+@typecheck_promote((Int, Float, Image, ImageCollection))
+def sqrt(obj):
+    """
+    Element-wise square root of an `Image` or `ImageCollection`.
+
+    Can also be used with `Int` and `Float` types.
+    """
+    return_type = Float if isinstance(obj, Number) else type(obj)
+    return return_type._from_apply("sqrt", obj)
+
+
+@typecheck_promote((Int, Float, Image, ImageCollection))
+def cos(obj):
+    """
+    Element-wise cosine of an `Image` or `ImageCollection`.
+
+    Can also be used with `Int` and `Float` types.
+    """
+    return_type = Float if isinstance(obj, Number) else type(obj)
+    return return_type._from_apply("cos", obj)
+
+
+@typecheck_promote((Int, Float, Image, ImageCollection))
+def sin(obj):
+    """
+    Element-wise sine of an `Image` or `ImageCollection`.
+
+    Can also be used with `Int` and `Float` types.
+    """
+    return_type = Float if isinstance(obj, Number) else type(obj)
+    return return_type._from_apply("sin", obj)
+
+
+@typecheck_promote((Int, Float, Image, ImageCollection))
+def tan(obj):
+    """
+    Element-wise tangent of an `Image` or `ImageCollection`.
+
+    Can also be used with `Int` and `Float` types.
+    """
+    return_type = Float if isinstance(obj, Number) else type(obj)
+    return return_type._from_apply("tan", obj)
+
+
+def normalized_difference(x, y):
+    """
+    Normalized difference helper function for computing an index such
+    as NDVI.
+
+    Example
+    -------
+    >>> import descarteslabs.workflows as wf
+    >>> col = wf.ImageCollection.from_id("landsat:LC08:01:RT:TOAR")
+    >>> nir, red = col.unpack_bands(["nir", "red"])
+    >>> ndvi = wf.normalized_difference(nir, red)
+    """
+
+    return (x - y) / (x + y)
