878fdeec1383daf5f11ade446b2dc9038b1098be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:38 +0000

    Add package graphql-booster/0.0.1a0/graphql_booster-0.0.1a0.tar.gz

---
+++ packages/graphql-booster/graphql_booster-0.0.1a0.tar.gz/graphql_booster-0.0.1a0/setup.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class PostInstallCommand(install):
+    def run(self):
+        if os.name == "nt":
+            activate_script = "activate.bat"
+        else:
+            activate_script = "activate"
+        gateway_path = os.path.join(os.curdir, "gateway")
+        activate_cmd = os.path.join(gateway_path, "node", "Scripts", activate_script)
+        subprocess.run("nodeenv node".split(), cwd=gateway_path)
+        subprocess.run([activate_cmd] + " & npm install".split(), cwd=gateway_path)
+        os.environ["GATEWAY_PATH"] = gateway_path
+        install.run(self)
+
+
+setup(
+    name="graphql_booster",
+    version="0.0.1a0",
+    author="jimtheplant",
+    author_email="jimtheplant1@gmail.com",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    cmdclass={
+        "install": PostInstallCommand
+    },
+    install_requires=[
+        "ariadne>=0.7.0",
+        "uvicorn",
+        "nodeenv"
+    ]
+)
