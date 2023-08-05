91adfa6edd2fbfb5a7d14accf25ba616c3816c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:40 +0000

    Add package cdk-static-website/0.0.1/cdk_static_website-0.0.1.tar.gz

---
+++ packages/cdk-static-website/cdk_static_website-0.0.1.tar.gz/cdk_static_website-0.0.1/setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name="cdk_static_website",
+    version="0.0.1",
+
+    description="A CDK Python construct to create static S3 websites. This is a port of the AWS static site example https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/static-site/static-site.ts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author="Juan Manuel Torres",
+
+    package_dir={"": "cdk"},
+    packages=setuptools.find_packages(where="cdk"),
+
+    install_requires=[
+        "aws-cdk.core",
+        "aws-cdk.aws-events",
+        "aws-cdk.aws-events-targets",
+        "aws-cdk.aws-certificatemanager",
+        "aws-cdk.aws-cloudfront",
+        "aws-cdk.aws-route53",
+        "aws-cdk.aws-route53-targets",
+        "aws-cdk.aws-s3",
+        "aws-cdk.aws-s3-deployment",
+    ],
+
+    python_requires=">=3.6",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+
+        "Intended Audience :: Developers",
+
+        "License :: OSI Approved :: Apache Software License",
+
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+
+        "Typing :: Typed",
+    ],
+)
