2329ad063c4243ad433d1ea3cbd9aec32f328d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:49 +0000

    Add package ocr-utils/0.1/ocr_utils-0.1.tar.gz

---
+++ packages/ocr-utils/ocr_utils-0.1.tar.gz/ocr_utils-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ocr_utils',
+      version='0.1',
+      description='debug OCR utils',
+      url='http://github.com/storborg/funniest',
+      author='Danh Vo',
+      author_email='vothanhdanh9526@gmail.com',
+      license='MIT',
+      packages=['ocr_utils'],
+      zip_safe=False)
