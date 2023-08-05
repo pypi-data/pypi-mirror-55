27004aef1368772b4a0e7964dc3a4caaf23f1e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package hibpcli/0.3.0/hibpcli-0.3.0.tar.gz

---
+++ packages/hibpcli/hibpcli-0.3.0.tar.gz/hibpcli-0.3.0/README.md
@@ -0,0 +1,54 @@
+[![Build Status](https://travis-ci.org/jugmac00/hibpcli.svg?branch=master)](https://travis-ci.org/jugmac00/hibpcli)
+[![Current version on PyPI](https://img.shields.io/pypi/v/hibpcli.svg)](https://pypi.org/project/hibpcli/)
+[![Requirements Status](https://requires.io/github/jugmac00/hibpcli/requirements.svg?branch=master)](https://requires.io/github/jugmac00/hibpcli/requirements/?branch=master)
+![](https://img.shields.io/pypi/l/hibpcli.svg)
+
+# hibpcli
+
+A command line interface for the "haveibeenpwned.com" API - speaks keepass.
+
+## current state
+
+A little bit alpha.
+
+## usage
+
+```
+$ hibpcli keepass --path PATHTOKEEPASSDB --password PASSWORDFORKEEPASSDB
+
+The passwords of following entries are leaked:
+[Entry: "test_title (test_user)"]
+```
+
+```
+$ hibpcli password --password test
+
+Please change your password!
+```
+
+
+## scope
+
+- check all passwords in a keepass db via hibp online API
+- check a single password
+
+## contributions, feature requests, bug reports
+
+Please use https://github.com/jugmac00/hibpcli/issues
+
+## run tests
+
+```
+pytest
+```
+
+## run tests with coverage
+
+```
+pytest --cov=hibpcli --cov=tests --cov-report term-missing
+```
+
+## thank you
+- click - https://click.palletsprojects.com
+- pykeepass - https://github.com/pschmitt/pykeepass
+- httpx - https://github.com/encode/httpx
---
+++ packages/hibpcli/hibpcli-0.3.0.tar.gz/hibpcli-0.3.0/hibpcli/cli.py
@@ -0,0 +1,63 @@
+import click
+
+from hibpcli.exceptions import ApiError
+from hibpcli.keepass import check_passwords_from_db
+from hibpcli.password import Password
+
+
+@click.group()
+def main():
+    """Command line interface to the haveibeenpwned.com API."""
+    pass
+
+
+@click.command()
+@click.option('--path', default=None, help='Path to KeePass database.')
+@click.option('--password', default=None, help='Password to the KeePass database.')
+def keepass(path, password):
+    """Check all passwords stored in the keepass database."""
+    if path is None:
+        path = click.prompt(
+            "Please enter the path to the database", type=click.Path(exists=True)
+        )
+    if password is None:
+         password = click.prompt(
+            "Please enter the master password for the database", hide_input=True
+        )
+    try:
+        rv = check_passwords_from_db(path=path, master_password=password)
+    except ApiError as e:
+        click.echo(str(e))
+    else:
+        if rv:
+            click.echo("The passwords of following entries are leaked:")
+            click.echo(rv)
+        else:
+            click.echo("Hooray, everything is safe!")
+
+
+@click.command()
+@click.option('--password', default=None, help='Password which should be checked.')
+def password(password):
+    """Check a single password."""
+    if password is None:
+         password = click.prompt(
+            "Please enter a password which should be checked", hide_input=True
+        )
+    p = Password(password)
+    try:
+        is_leaked = p.is_leaked()
+    except ApiError as e:
+        click.echo(str(e))
+    else:
+        if is_leaked:
+            click.echo("Please change your password!")
+        else:
+            click.echo("Your password is safe!")
+
+main.add_command(keepass)
+main.add_command(password)
+
+
+if __name__ == "__main__":
+    main()
