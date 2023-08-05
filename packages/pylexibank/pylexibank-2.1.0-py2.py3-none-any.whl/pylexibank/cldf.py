8732484fb72be8db7831a0eaf176b0caaed85f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:22 +0000

    Add package pylexibank/2.1.0/pylexibank-2.1.0-py2.py3-none-any.whl

---
+++ packages/pylexibank/pylexibank-2.1.0-py2.py3-none-any.whl/pylexibank/cldf.py
@@ -0,0 +1,377 @@
+import re
+from collections import defaultdict, OrderedDict
+from collections.abc import Mapping
+from itertools import chain
+from pathlib import Path
+import pkg_resources
+import logging
+
+import attr
+from csvw.metadata import Column
+from pycldf.dataset import Wordlist
+import pyclts.models
+from pyconcepticon.api import Concept
+
+from cldfbench.cldf import CLDFWriter
+from cldfbench.util import iter_requirements
+
+from pylexibank.transcription import Analysis, analyze
+
+__all__ = ['LexibankWriter']
+log = logging.getLogger('pylexibank')
+
+MD_NAME = 'cldf-metadata.json'
+ID_PATTERN = re.compile('[A-Za-z0-9_\-]+$')
+
+
+class LexibankWriter(CLDFWriter):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self._count = defaultdict(int)
+        self._cognate_count = defaultdict(int)
+
+    def write(self, **kw):
+        from pylexibank import ENTRY_POINT
+
+        super().write(**kw)
+        # We rewrite requirements.txt, excluding all lexibank dataset modules:
+        exclude = {'egg=' + ep.module_name for ep in pkg_resources.iter_entry_points(ENTRY_POINT)}
+        reqs = []
+        for req in iter_requirements():
+            if not any(mod in req for mod in exclude):
+                reqs.append(req)
+
+        self.cldf_spec.dir.joinpath('requirements.txt').write_text('\n'.join(reqs), encoding='utf8')
+
+    def __enter__(self):
+        super().__enter__()
+        default_cldf = Wordlist.from_metadata(Path(__file__).parent / MD_NAME)
+
+        self._obj_index = {}
+        for cls in [
+            self.dataset.lexeme_class,
+            self.dataset.language_class,
+            self.dataset.concept_class,
+            self.dataset.cognate_class,
+        ]:
+            self.objects[cls.__cldf_table__()] = []
+            self._obj_index[cls.__cldf_table__()] = set()
+
+            cols = set(
+                col.header for col in self.cldf[cls.__cldf_table__()].tableSchema.columns)
+            properties = set(
+                col.propertyUrl.uri for col in self.cldf[cls.__cldf_table__()].tableSchema.columns
+                if col.propertyUrl)
+            for field in cls.fieldnames():
+                try:
+                    col = default_cldf[cls.__cldf_table__(), field]
+                    #
+                    # We added Latitude and Longitude to the default metadata later, and want to
+                    # make sure, existing datasets are upgraded silently.
+                    #
+                    if field in ['Latitude', 'Longitude'] \
+                            and cls.__cldf_table__() == 'LanguageTable':  # pragma: no cover
+                        properties.add(col.propertyUrl.uri)
+                        self.cldf[cls.__cldf_table__(), field].propertyUrl = col.propertyUrl
+                        self.cldf[cls.__cldf_table__(), field].datatype = col.datatype
+                except KeyError:
+                    col = Column(name=field, datatype="string")
+                if (col.propertyUrl and col.propertyUrl.uri not in properties) or \
+                        ((not col.propertyUrl) and (field not in cols)):
+                    self.cldf[cls.__cldf_table__()].tableSchema.columns.append(col)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for table in ['FormTable', 'CognateTable', 'LanguageTable', 'ParameterTable']:
+            self.objects.setdefault(table, [])
+        # We only add concepts and languages that are referenced by forms!
+        for fk, table in [('Parameter_ID', 'ParameterTable'), ('Language_ID', 'LanguageTable')]:
+            refs = set(obj[fk] for obj in self.objects['FormTable'])
+            self.objects[table] = [obj for obj in self.objects[table] if obj['ID'] in refs]
+        super().__exit__(exc_type, exc_val, exc_tb)
+
+    def add_sources(self, *args):
+        if not args and self.dataset.raw_dir.joinpath('sources.bib').exists():
+            args = self.dataset.raw_dir.read_bib()
+        self.cldf.sources.add(*args)
+
+    def lexeme_id(self, kw):
+        self._count[(kw['Language_ID'], kw['Parameter_ID'])] += 1
+        return '{0}-{1}-{2}'.format(
+            kw['Language_ID'],
+            kw['Parameter_ID'],
+            self._count[(kw['Language_ID'], kw['Parameter_ID'])])
+
+    def cognate_id(self, kw):
+        self._cognate_count[kw['Form_ID']] += 1
+        return '{0}-{1}'.format(kw['Form_ID'], self._cognate_count[kw['Form_ID']])
+
+    def tokenize(self, item, string):
+        if self.dataset.tokenizer:
+            return self.dataset.tokenizer(item, string)
+
+    def add_form_with_segments(self, **kw):
+        """
+        :return: dict with the newly created lexeme
+        """
+        # Do we have morpheme segmentation on top of phonemes?
+        with_morphemes = '+' in self['FormTable', 'Segments'].separator
+
+        language, concept, value, form, segments = [kw.get(v) for v in [
+            'Language_ID', 'Parameter_ID', 'Value', 'Form', 'Segments']]
+
+        # check for required kws
+        if language is None or concept is None or value is None \
+                or form is None or segments is None:
+            raise ValueError('language, concept, value, form, and segments must be supplied')
+        kw.setdefault('Segments', segments)
+        kw.update(ID=self.lexeme_id(kw), Form=form)
+        lexeme = self._add_object(self.dataset.lexeme_class, **kw)
+
+        analysis = self.dataset.tr_analyses.setdefault(kw['Language_ID'], Analysis())
+        try:
+            segments = kw['Segments']
+            if with_morphemes:
+                segments = list(chain(*[s.split() for s in segments]))
+            _, _bipa, _sc, _analysis = analyze(self.args.clts.api, segments, analysis)
+
+            # update the list of `bad_words` if necessary; we precompute a
+            # list of data types in `_bipa` just to make the conditional
+            # checking easier
+            _bipa_types = [type(s) for s in _bipa]
+            if pyclts.models.UnknownSound in _bipa_types or '?' in _sc:
+                self.dataset.tr_bad_words.append(kw)
+        except ValueError:  # pragma: no cover
+            self.dataset.tr_invalid_words.append(kw)
+        except (KeyError, AttributeError):  # pragma: no cover
+            print(kw['Form'], kw)
+            raise
+        return lexeme
+
+    def add_form(self, with_morphemes=False, **kw):
+        """
+        :return: dict with the newly created form
+        """
+        language, concept, value, form = [kw.get(v) for v in [
+            'Language_ID', 'Parameter_ID', 'Value', 'Form']]
+
+        # check for required kws
+        if language is None or concept is None or value is None \
+                or form is None:
+            raise ValueError('language, concept, value, and form must be supplied')
+
+        # check for kws not allowed
+        if 'Segments' in kw:
+            raise ValueError('segmented data must be passed with add_form_with_segments')
+
+        # point to difference in value and form
+        if form != value:
+            log.debug('iter_forms split: "{0}" -> "{1}"'.format(value, form))
+
+        if form:
+            # try to segment the data now
+            kw.setdefault('Segments', self.tokenize(kw, form) or [])
+            if kw['Segments']:
+                return self.add_form_with_segments(**kw)
+
+            kw.update(ID=self.lexeme_id(kw), Form=form)
+            return self._add_object(self.dataset.lexeme_class, **kw)
+
+    def add_forms_from_value(self, split_value=None, **kw):
+        """
+        :return: list of dicts corresponding to newly created Lexemes
+        """
+        lexemes = []
+
+        if 'Segments' in kw:
+            raise ValueError('segmented data must be passed with add_form_with_segments')
+        if 'Form' in kw:
+            raise ValueError('forms must be passed with add_form')
+
+        svkw = {}
+        if split_value is None:
+            split_value = self.dataset.form_spec.split
+            svkw = dict(lexemes=self.dataset.lexemes)
+
+        # Do we have morpheme segmentation on top of phonemes?
+        with_morphemes = '+' in self['FormTable', 'Segments'].separator
+
+        for i, form in enumerate(split_value(kw, kw['Value'], **svkw)):
+            kw_ = kw.copy()
+            kw_['Form'] = form
+            if kw_['Form']:
+                kw_ = self.add_form(with_morphemes=with_morphemes, **kw_)
+                if kw_:
+                    lexemes.append(kw_)
+
+        return lexemes
+
+    def add_lexemes(self, split_value=None, **kw):
+        """
+        :return: list of dicts corresponding to newly created Lexemes
+        """
+        return self.add_forms_from_value(split_value=split_value, **kw)
+
+    def _add_object(self, cls, **kw):
+        # Instantiating an object will trigger potential validators:
+        d = attr.asdict(cls(**kw))
+        t = cls.__cldf_table__()
+        for key in ['ID', 'Language_ID', 'Parameter_ID', 'Cognateset_ID']:
+            # stringify/sluggify identifiers:
+            if d.get(key) is not None:
+                d[key] = '{0}'.format(d[key])
+                if not ID_PATTERN.match(d[key]):
+                    raise ValueError(
+                        'invalid CLDF identifier {0}-{1}: {2}'.format(t, key, d[key]))
+        if 'ID' not in d or d['ID'] not in self._obj_index[t]:
+            if 'ID' in d:
+                self._obj_index[t].add(d['ID'])
+            self.objects[t].append(d)
+        return d
+
+    def add_cognate(self, lexeme=None, **kw):
+        """
+        Add a row to `CognateTable`.
+
+        :param lexeme: An optional `FormTable` row (i.e. `dict`) to lookup lexeme data.
+        :param kw: Attribute data to intialise a `self.dataset.cognate class` instance.
+        :return: The (possibly augmented) instance data as `dict`.
+        """
+        if lexeme:
+            if not isinstance(lexeme, Mapping):  # pragma: no cover
+                raise TypeError('lexeme must be a mapping (`dict`) of lexeme attributes')
+            kw.setdefault('Form_ID', lexeme['ID'])
+            kw.setdefault('Form', lexeme['Form'])
+
+        kw.setdefault('ID', self.cognate_id(kw))
+        return self._add_object(self.dataset.cognate_class, **kw)
+
+    def add_language(self, **kw):
+        if 'Glottocode' in kw \
+                and hasattr(self.args, 'glottolog') \
+                and kw['Glottocode'] in self.args.glottolog.api.cached_languoids:
+            glang = self.args.glottolog.api.cached_languoids[kw['Glottocode']]
+            for key, attribute in [
+                ('Latitude', 'latitude'),
+                ('Longitude', 'longitude'),
+                ('Glottolog_Name', 'name'),
+                ('ISO639P3code', 'iso')
+            ]:
+                if kw.get(key) is None:
+                    kw[key] = getattr(glang, attribute)
+            if kw.get('Family') is None:
+                kw['Family'] = glang.lineage[0][0] if glang.lineage else glang.name
+            if kw.get('Macroarea') is None:
+                kw['Macroarea'] = glang.macroareas[0].name if glang.macroareas else None
+
+        return self._add_object(self.dataset.language_class, **kw)
+
+    def add_languages(self, id_factory='ID', lookup_factory=None):
+        """
+        Add languages as specified in a dataset's etc/languages.csv
+
+        :param id_factory: A callable taking a dict describing a language as argument and returning\
+        a value to be used as ID for the language or a `str` specifying a key in the language \
+        `dict`.
+        :param lookup_factory: A callable taking a language `dict` and returning a reverse \
+        lookup key to associate the generated ID with (or a `str` specifying a key in the language \
+        `dict`).
+        :return: The `list` of language IDs which have been added, if no `lookup_factory` was \
+        passed, otherwise an `OrderedDict`, mapping lookup to ID.
+        """
+        assert callable(id_factory) or isinstance(id_factory, str)
+        ids = OrderedDict()
+        for i, kw in enumerate(self.dataset.languages):
+            if (not kw.get('Glottocode')) and kw.get('ISO639P3code'):
+                kw['Glottocode'] = self.dataset.glottolog.glottocode_by_iso.get(kw['ISO639P3code'])
+            kw['ID'] = id_factory(kw) if callable(id_factory) else kw[id_factory]
+            if lookup_factory is None:
+                key = i
+            else:
+                key = lookup_factory(kw) if callable(lookup_factory) else kw[lookup_factory]
+            ids[key] = kw['ID']
+            self.add_language(**kw)
+        return ids if lookup_factory else list(ids.values())
+
+    def add_concept(self, **kw):
+        if kw.get('Concepticon_ID'):
+            kw.setdefault(
+                'Concepticon_Gloss',
+                self.dataset.concepticon.cached_glosses[int(kw['Concepticon_ID'])])
+        return self._add_object(self.dataset.concept_class, **kw)
+
+    def add_concepts(self, id_factory=lambda d: d.number, lookup_factory=None):
+        """
+        Add concepts as specified in a dataset's associated Concepticon concept list or in
+        etc/concepts.csv
+
+        :param id_factory: A callable taking a pyconcepticon.api.Concept object as argument and \
+        returning a value to be used as ID for the concept.
+        :param lookup_factory: A callable taking a `dict` object and returning a reverse \
+        lookup key to associate the generated ID with (or a `str` specifying an attribute in \
+        `Concept`).
+        :return: The `list` of concept IDs which have been added, if no `lookup_factory` was \
+        passed, otherwise an `OrderedDict`, mapping lookup to ID.
+        """
+        assert callable(id_factory) or isinstance(id_factory, str)
+
+        # Read pyconcepticon.Concept instances either from a conceptlist in Concepticon, or from
+        # etc/concepts.csv:
+        ids, concepts = OrderedDict(), []
+        if self.dataset.conceptlists:
+            for cl in self.dataset.conceptlists:
+                concepts.extend(cl.concepts.values())
+        else:
+            fields = Concept.public_fields()
+            for i, concept in enumerate(self.dataset.concepts, start=1):
+                kw, attrs = {}, {}
+                for k, v in concept.items():
+                    if k.lower() in fields:
+                        kw[k.lower()] = v
+                    else:
+                        attrs[k.lower()] = v
+
+                if not kw.get('id'):
+                    kw['id'] = str(i)
+                if not kw.get('number'):
+                    kw['number'] = str(i)
+                concepts.append(Concept(attributes=attrs, **kw))
+
+        # Now turn the concepts into `dict`s suitable to instantiate `self.dataset.concept_class`:
+        fieldnames = {f.lower(): f for f in self.dataset.concept_class.fieldnames()}
+        for i, c in enumerate(concepts):
+            try:
+                # `id_factory` might expect a pyconcepticon.Concept instance as input:
+                id_ = id_factory(c) if callable(id_factory) else getattr(c, id_factory)
+            except AttributeError:
+                id_ = None
+            attrs = dict(
+                ID=id_,
+                Name=c.label,
+                Concepticon_ID=c.concepticon_id,
+                Concepticon_Gloss=c.concepticon_gloss)
+            for fl, f in fieldnames.items():
+                if fl in c.attributes:
+                    attrs[f] = c.attributes[fl]
+            if attrs['ID'] is None:
+                attrs['ID'] = id_factory(attrs) if callable(id_factory) else attrs[id_factory]
+            if lookup_factory is None:
+                key = i
+            else:
+                key = lookup_factory(attrs) if callable(lookup_factory) else attrs[lookup_factory]
+            ids[key] = attrs['ID']
+            self.add_concept(**attrs)
+        return ids if lookup_factory else list(ids.values())
+
+    def align_cognates(self,
+                       alm=None,
+                       cognates=None,
+                       column='Segments',
+                       method='library'):
+        from pylexibank.lingpy_util import iter_alignments
+
+        iter_alignments(
+            alm or self,
+            cognates or self.objects['CognateTable'],
+            column=column,
+            method=method)
---
+++ packages/pylexibank/pylexibank-2.1.0-py2.py3-none-any.whl/pylexibank/util.py
@@ -0,0 +1,110 @@
+import re
+from collections import OrderedDict
+from pathlib import Path
+
+from termcolor import colored
+from tqdm import tqdm
+
+from clldutils.misc import slug
+from clldutils.badge import Colors, badge
+from clldutils import jsonlib
+from cldfbench.datadir import get_url
+from pycldf.sources import Source, Reference
+
+__all__ = ['progressbar', 'getEvoBibAsBibtex']
+YEAR_PATTERN = re.compile('\s+\(?(?P<year>[1-9][0-9]{3}(-[0-9]+)?)(\)|\.)')
+
+
+def progressbar(iterable=None, **kw):
+    kw.setdefault('leave', False)
+    kw.setdefault('desc', 'cldfbench')
+    return tqdm(iterable=iterable, **kw)
+
+
+# backwards compat:
+pb = progressbar
+
+
+def split_by_year(s):
+    match = YEAR_PATTERN.search(s)
+    if match:
+        return s[:match.start()].strip(), match.group('year'), s[match.end():].strip()
+    return None, None, s
+
+
+def get_reference(author, year, title, pages, sources, id_=None, genre='misc'):
+    kw = {'title': title}
+    id_ = id_ or None
+    if author and year:
+        id_ = id_ or slug(author + year)
+        kw.update(author=author, year=year)
+    elif title:
+        id_ = id_ or slug(title)
+
+    if not id_:
+        return
+
+    source = sources.get(id_)
+    if source is None:
+        sources[id_] = source = Source(genre, id_, **kw)
+
+    return Reference(source, pages)
+
+
+def get_badge(ratio, name):
+    if ratio >= 0.99:
+        color = Colors.brightgreen
+    elif ratio >= 0.9:
+        color = 'green'
+    elif ratio >= 0.8:
+        color = Colors.yellowgreen
+    elif ratio >= 0.7:
+        color = Colors.yellow
+    elif ratio >= 0.6:
+        color = Colors.orange
+    else:
+        color = Colors.red
+    ratio = int(round(ratio * 100))
+    return badge(name, '%s%%' % ratio, color, label="{0}: {1}%".format(name, ratio))
+
+
+def sorted_obj(obj):
+    res = obj
+    if isinstance(obj, dict):
+        res = OrderedDict()
+        obj.pop(None, None)
+        for k, v in sorted(obj.items()):
+            res[k] = sorted_obj(v)
+    elif isinstance(obj, (list, set)):
+        res = [sorted_obj(v) for v in obj]
+    return res
+
+
+def log_dump(fname, log=None):
+    if log:
+        log.info('file written: {0}'.format(colored(fname.as_posix(), 'green')))
+
+
+def jsondump(obj, fname, log=None):
+    fname = Path(fname)
+    if fname.exists():
+        d = jsonlib.load(fname)
+        d.update(obj)
+        obj = d
+    jsonlib.dump(sorted_obj(obj), fname, indent=4)
+    log_dump(fname, log=log)
+    return obj
+
+
+def getEvoBibAsBibtex(*keys, **kw):
+    """Download bibtex format and parse it from EvoBib"""
+    res = []
+    for key in keys:
+        bib = get_url(
+            "http://bibliography.lingpy.org/raw.php?key=" + key,
+            log=kw.get('log')).text
+        try:
+            res.append('@' + bib.split('@')[1].split('</pre>')[0])
+        except IndexError:  # pragma: no cover
+            raise KeyError('Missing entry {0} in evobib'.format(key))
+    return '\n\n'.join(res)
