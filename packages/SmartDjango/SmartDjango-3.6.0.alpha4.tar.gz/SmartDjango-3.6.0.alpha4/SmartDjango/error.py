f6c32940b8bec5c11d517ab5cac6c69cad4225bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package SmartDjango/3.6.0.alpha4/SmartDjango-3.6.0.alpha4.tar.gz

---
+++ packages/SmartDjango/SmartDjango-3.6.0.alpha4.tar.gz/SmartDjango-3.6.0.alpha4/SmartDjango/error.py
@@ -0,0 +1,26 @@
+from .http_code import HttpCode as Hc
+from smartify import E as BE, Attribute, BaseError, PError
+
+
+class E(BE):
+    def __init__(self, message: str, hc=Hc.OK):
+        super(E, self).__init__(message)
+        self.hc = hc
+
+    def d(self):
+        return Attribute.dictify(self, 'message->msg', 'eid', 'hc->http_code')
+
+
+def error_update(e: BE, hc=Hc.OK):
+    e.__class__ = E
+    setattr(e, 'hc', hc)
+
+
+error_update(BaseError.OK, hc=Hc.OK)
+error_update(BaseError.ERROR_GENERATE, hc=Hc.InternalServerError)
+error_update(PError.VALIDATOR_CRUSHED, hc=Hc.InternalServerError)
+error_update(PError.PROCESSOR_CRUSHED, hc=Hc.InternalServerError)
+error_update(PError.REQUIRE_DICT, hc=Hc.Forbidden)
+error_update(PError.REQUIRE_LIST, hc=Hc.Forbidden)
+error_update(PError.NULL_NOT_ALLOW, hc=Hc.Forbidden)
+error_update(PError.PATTERN_NOT_MATCH, hc=Hc.Forbidden)
