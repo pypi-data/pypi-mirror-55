69159be1f3e985a43cc6830bae06f433cde62ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:16 +0000

    Add package scicat/1.1.6/scicat-1.1.6-py3-none-any.whl

---
+++ packages/scicat/scicat-1.1.6-py3-none-any.whl/scicat/upload.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""upload to scicat"""
+import os
+import pwd
+import getpass
+import datetime
+import urllib
+
+import requests
+
+from scicat import api
+from scicat import login
+
+
+class Upload:
+    """upload"""
+    dataset = {}
+    token = ""
+    uri = ""
+    delete_uri = ""
+
+    def __init__(self):
+        apix = api.Api()
+        self.api = apix.api
+        username = getpass.getuser()
+        pwd_struct = pwd.getpwnam(username)
+        print(pwd_struct)
+        self.human_name = pwd_struct.pw_gecos
+        split_name = pwd_struct.pw_gecos.split(' ')
+        self.email = '.'.join(split_name) + "@esss.se"
+        print(self.email)
+
+    def create_json(self):
+        """create json"""
+        date = datetime.datetime.now().isoformat()
+        self.dataset = {
+            "pid": "xlfghz",
+            "owner": self.human_name,
+            "ownerEmail": self.email,
+            "contactEmail": self.email,
+            "sourceFolder": "/dram/",
+            "datasetName": "Test python upload",
+            "creationTime": date,
+            "keywords": [
+                "Test", "Derived", "Science"
+            ],
+            "description": "Test uploaded metadata from python",
+            "isPublished": False,
+            "ownerGroup": "p34123",
+            "type": "raw"
+        }
+
+    def get_token(self):
+        """get scicat token"""
+        login = Login()
+        self.token = login.token()
+        print(self.token)
+
+    def create_uri(self):
+        """create uri"""
+        # login = Login()
+        # token = login.login()
+        self.uri = os.path.join(self.api, "Datasets") + \
+            "?access_token=" + self.token
+        print(self.uri)
+
+    def upload_scicat(self):
+        """upload to scicat"""
+        self.get_token()
+        self.create_uri()
+        self.create_json()
+        pid = urllib.parse.quote_plus("20.500.12269/"+self.dataset["pid"])
+        delete_uri = os.path.join(
+            self.api, "Datasets", pid) + "?access_token=" + self.token
+        response = requests.delete(delete_uri)
+        print(delete_uri)
+        print(response.json())
+        response = requests.post(self.uri, json=self.dataset)
+        print(response.json())
+
+
+def upload(name):
+    """upload to scicat"""
+    print("uploading to scicat")
+    dataset = {
+        "datasetName": name
+    }
+    uploader = Upload()
+    uploader.upload_scicat()
+
+    print(dataset)
+
+
+def main():
+    """main"""
+    name = "nexus"
+    upload(name)
+
+
+if __name__ == "__main__":
+    main()
