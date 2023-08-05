080debc7040843b62cdbbeb55397e857ee85f3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:22 +0000

    Add package tc-admin/2.2.0/tc_admin-2.2.0-py3-none-any.whl

---
+++ packages/tc-admin/tc_admin-2.2.0-py3-none-any.whl/tcadmin/current/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+import asyncio
+from ..resources import Resources
+from . import hooks, clients, roles, aws_provisioner_workertypes, worker_pools, secrets
+
+
+async def resources(managed):
+    """
+    Fetch the existing resources that are managed by the provided list.
+    """
+    resources = Resources([], managed)
+
+    await asyncio.gather(
+        clients.fetch_clients(resources),
+        roles.fetch_roles(resources),
+        hooks.fetch_hooks(resources),
+        aws_provisioner_workertypes.fetch_aws_provisioner_workertypes(resources),
+        worker_pools.fetch_worker_pools(resources),
+        secrets.fetch_secrets(resources),
+    )
+    return resources
