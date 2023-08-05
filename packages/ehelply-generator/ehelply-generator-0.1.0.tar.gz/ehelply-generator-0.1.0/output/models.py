3fff0685c52f2b596be43c47895a3919eac9c53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:39 +0000

    Add package ehelply-generator/0.1.0/ehelply-generator-0.1.0.tar.gz

---
+++ packages/ehelply-generator/ehelply-generator-0.1.0.tar.gz/ehelply-generator-0.1.0/output/models.py
@@ -0,0 +1,59 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, JSON, Text, DateTime 
+from sqlalchemy.orm import relationship 
+ 
+import uuid 
+import datetime 
+ 
+from ehelply_bootstrapper.utils.state import State
+        
+
+class Blog(State.mysql.Base):
+    """
+    Represents a Blog
+    """
+    __tablename__ = "blogs"
+
+    uuid = Column(String(64), primary_key=True, index=True, unique=True, nullable=False)
+    meta_id = Column(String(64), unique=False, nullable=True)
+
+    blog_categories = relationship("BlogCategory", back_populates="blog", passive_deletes=True)
+
+    blog_posts = relationship("BlogPost", back_populates="blog", passive_deletes=True)
+
+
+class BlogCategory(State.mysql.Base):
+    """
+    Represents a BlogCategory
+    """
+    __tablename__ = "blog_categories"
+
+    uuid = Column(String(64), primary_key=True, index=True, unique=True, nullable=False)
+    blog_uuid = Column(String(64), ForeignKey('blogs.uuid', ondelete='CASCADE'), unique=False, nullable=False)
+    meta_id = Column(String(64), unique=False, nullable=True)
+
+    blog = relationship("Blog", back_populates="blog_categories", passive_deletes=True)
+
+    blog_posts = relationship("BlogPost", back_populates="blog_category", passive_deletes=True)
+
+
+class BlogPost(State.mysql.Base):
+    """
+    Represents a BlogPost
+    """
+    __tablename__ = "blog_posts"
+
+    uuid = Column(String(64), primary_key=True, index=True, unique=True, nullable=False)
+    blog_uuid = Column(String(64), ForeignKey('blogs.uuid', ondelete='CASCADE'), unique=False, nullable=False)
+    meta_uuid = Column(String(64), unique=False, nullable=True)
+    content_uuid = Column(String(64), unique=False, nullable=True)
+    category_uuid = Column(String(64), ForeignKey('blog_categories.uuid', ondelete='CASCADE'), unique=False, nullable=False)
+    locked = Column(Boolean, unique=False, nullable=False, default=False)
+    frozen = Column(Boolean, unique=False, nullable=False, default=False)
+    publish_at = Column(DateTime, unique=False, nullable=True)
+    deleted_at = Column(DateTime, unique=False, nullable=True)
+
+    blog = relationship("Blog", back_populates="blog_posts", passive_deletes=True)
+
+    blog_category = relationship("BlogCategory", back_populates="blog_posts", passive_deletes=True)
+
+# END OF GENERATED CODE
---
+++ packages/ehelply-generator/ehelply-generator-0.1.0.tar.gz/ehelply-generator-0.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ehelply-generator',
+    packages=find_packages(),  # this must be the same as the name above
+    version='0.1.0',
+    description='eHelply Generator',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Shawn Clake',
+    author_email='shawn.clake@gmail.com',
+    url='https://github.com/ehelply/Generator',
+    keywords=[],
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'pytest',
+        'sqlalchemy',
+        'pydantic',
+        'python-slugify',
+        'ehelply_bootstrapper',
+        'click'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ehelplygen=ehelply_generator.ehelply_gen:ehelplygen',  # command=package.module:function
+        ],
+    },
+
+)
