ea03e98a998d363974a5f66b8b5a840bb4d48433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package lopespdf/1.4/lopespdf-1.4-py3-none-any.whl

---
+++ packages/lopespdf/lopespdf-1.4-py3-none-any.whl/lopespdf/pdf2image.py
@@ -0,0 +1,17 @@
+""" This module provides functions to convert PDF to image."""
+
+
+class ConverterImg:
+    """ A simple converter for converting PDFs to image. """
+
+    def convert_img(self, path):
+        """
+    Convert the given PDF to text.
+
+    Parameters:
+    path (str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as a image.
+    """
+    print("pdf2image")
