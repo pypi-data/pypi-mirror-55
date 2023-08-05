521a3b87455ba427e3596a3f8544989ff23a5810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package tc-admin/2.1.1/tc-admin-2.1.1.tar.gz

---
+++ packages/tc-admin/tc-admin-2.1.1.tar.gz/tc-admin-2.1.1/tcadmin/resources/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+from .resources import Resources
+from .role import Role
+from .client import Client
+from .hook import Hook, Binding
+from .aws_provisioner_workertype import AwsProvisionerWorkerType
+from .worker_pool import WorkerPool
+
+__all__ = [
+    "Resources",
+    "Role",
+    "Hook",
+    "Binding",
+    "AwsProvisionerWorkerType",
+    "WorkerPool",
+    "Client",
+]
