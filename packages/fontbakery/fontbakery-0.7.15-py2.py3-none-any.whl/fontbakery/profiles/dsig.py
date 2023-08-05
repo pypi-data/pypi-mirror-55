6e9df18621bcf4297df8b41679e6d2e7f20e41fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:28 +0000

    Add package fontbakery/0.7.15/fontbakery-0.7.15-py2.py3-none-any.whl

---
+++ packages/fontbakery/fontbakery-0.7.15-py2.py3-none-any.whl/fontbakery/profiles/dsig.py
@@ -0,0 +1,28 @@
+from fontbakery.callable import check
+from fontbakery.checkrunner import FAIL, PASS
+from fontbakery.message import Message
+# used to inform get_module_profile whether and how to create a profile
+from fontbakery.fonts_profile import profile_factory # NOQA pylint: disable=unused-import
+
+@check(
+  id = 'com.google.fonts/check/dsig',
+  rationale = """
+    Some programs expect fonts to have a digital signature declared in their DSIG table in order to work properly.
+
+    This checks verifies that such signature is available in the font.
+
+    Typically, even a fake signature would be enough to make the fonts work. If needed, such dummy-placeholder can be added to the font by using the `gftools fix-dsig` script available at https://github.com/googlefonts/gftools
+  """
+)
+def com_google_fonts_check_dsig(ttFont):
+  """Does the font have a DSIG table?"""
+  if "DSIG" in ttFont:
+    yield PASS, "Digital Signature (DSIG) exists."
+  else:
+    yield FAIL,\
+          Message("lacks-signature",
+                  "This font lacks a digital signature (DSIG table)."
+                  " Some applications may require one (even if only a"
+                  " dummy placeholder) in order to work properly. You"
+                  " can add a DSIG table by running the"
+                  " `gftools fix-dsig` script.")
