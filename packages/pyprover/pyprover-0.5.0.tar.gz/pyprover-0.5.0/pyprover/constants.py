4e5f8ecc4f71a3e930674614ca4584ebbcc51b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package pyprover/0.5.0/pyprover-0.5.0.tar.gz

---
+++ packages/pyprover/pyprover-0.5.0.tar.gz/pyprover-0.5.0/pyprover/constants.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __coconut_hash__ = 0xa7804fe5
+
+# Compiled with Coconut version 1.4.1-post_dev3 [Ernest Scribbler]
+
+# Coconut Header: -------------------------------------------------------------
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+import sys as _coconut_sys, os.path as _coconut_os_path
+_coconut_file_path = _coconut_os_path.dirname(_coconut_os_path.abspath(__file__))
+_coconut_cached_module = _coconut_sys.modules.get(str("__coconut__"))
+if _coconut_cached_module is not None and _coconut_os_path.dirname(_coconut_cached_module.__file__) != _coconut_file_path:
+    del _coconut_sys.modules[str("__coconut__")]
+_coconut_sys.path.insert(0, _coconut_file_path)
+from __coconut__ import *
+from __coconut__ import _coconut, _coconut_MatchError, _coconut_tail_call, _coconut_tco, _coconut_igetitem, _coconut_base_compose, _coconut_forward_compose, _coconut_back_compose, _coconut_forward_star_compose, _coconut_back_star_compose, _coconut_forward_dubstar_compose, _coconut_back_dubstar_compose, _coconut_pipe, _coconut_back_pipe, _coconut_star_pipe, _coconut_back_star_pipe, _coconut_dubstar_pipe, _coconut_back_dubstar_pipe, _coconut_bool_and, _coconut_bool_or, _coconut_none_coalesce, _coconut_minus, _coconut_map, _coconut_partial, _coconut_get_function_match_error, _coconut_base_pattern_func, _coconut_addpattern, _coconut_sentinel, _coconut_assert
+if _coconut_sys.version_info >= (3,):
+    _coconut_sys.path.pop(0)
+
+# Compiled Coconut: -----------------------------------------------------------
+
+# Imports:
+
+sys = _coconut_sys
+
+# Utilities:
+
+def first_encodeable(symbols):
+    for sym in symbols:
+        try:
+            if hasattr(sys.stdout, "encoding") and sys.stdout.encoding is not None:
+                sym.encode(sys.stdout.encoding)
+            else:
+                sym.encode()
+        except UnicodeEncodeError:
+            pass
+        else:
+            return sym
+    raise ValueError("No encodable symbol in " + repr(symbols))
+
+# Installation:
+
+version = "0.5.0"
+requirements = ["pyparsing",]
+classifiers = ["Development Status :: 3 - Alpha", "License :: OSI Approved :: Apache Software License", "Topic :: Software Development :: Libraries :: Python Modules", "Operating System :: OS Independent",]
+
+# Symbols:
+
+all_top_syms = "\u22a4", "\u252c", "top", "true"
+top_sym = (first_encodeable)(all_top_syms)
+
+all_bot_syms = "\u22a5", "\u2534", "bot", "_|_", "false"
+bot_sym = (first_encodeable)(all_bot_syms)
+
+all_not_syms = "\xac", "~", "-"
+not_sym = (first_encodeable)(all_not_syms)
+
+all_imp_syms = "\u2192", "->"
+imp_sym = (first_encodeable)(all_imp_syms)
+
+all_and_syms = "\u2227", "/\\", "&"
+and_sym = (first_encodeable)(all_and_syms)
+
+all_or_syms = "\u2228", "\\/", "|"
+or_sym = (first_encodeable)(all_or_syms)
+
+all_forall_syms = "\u2200", "A", "FA", "fa", "forall"
+forall_sym = (first_encodeable)(all_forall_syms)
+
+all_exists_syms = "\u2203", "E", "TE", "te", "exists"
+exists_sym = (first_encodeable)(all_exists_syms)
+
+empty_var = "_"
