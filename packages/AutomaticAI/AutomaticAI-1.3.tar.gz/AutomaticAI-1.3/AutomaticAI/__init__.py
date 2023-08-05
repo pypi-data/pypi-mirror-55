30931e496d710620e58f403d7bc53035cb91c611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package AutomaticAI/1.3/AutomaticAI-1.3.tar.gz

---
+++ packages/AutomaticAI/AutomaticAI-1.3.tar.gz/AutomaticAI-1.3/AutomaticAI/__init__.py
@@ -0,0 +1,1 @@
+from AutomaticAI.ParticleSwarmOptimization import PSO
---
+++ packages/AutomaticAI/AutomaticAI-1.3.tar.gz/AutomaticAI-1.3/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+  name = 'AutomaticAI',         # How you named your package folder (MyLib)
+  packages = ['AutomaticAI'],   # Chose the same as "name"
+  version = '1.3',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A Hybrid Approach for Automatic Artificial Intelligence Algorithm Selection and Hyperparameter Tuning',   # Give a short description about your library
+  author = 'Czako Zoltan',                   # Type in your name
+  author_email = 'czakozoltan08@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/CzakoZoltan08/AutomaticAI',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/CzakoZoltan08/AutomaticAI/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['AI', 'Arificial Intelligence', 'Particle Swarm Optimization', 'Simulated Annealing', 'Automatic Algorithm Selection', 'Automatic parameter optimization'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'scikit-learn',
+          'numpy',
+		  'scipy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.6',
+	'Programming Language :: Python :: 3.7',
+  ],
+)
