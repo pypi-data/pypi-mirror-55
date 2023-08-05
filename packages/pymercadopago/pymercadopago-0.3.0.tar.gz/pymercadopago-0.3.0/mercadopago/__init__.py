0658928261117dae4744dfc9e5c72b76e445fb36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:01 +0000

    Add package pymercadopago/0.3.0/pymercadopago-0.3.0.tar.gz

---
+++ packages/pymercadopago/pymercadopago-0.3.0.tar.gz/pymercadopago-0.3.0/mercadopago/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from .api import Client
+from .errors import (
+    Error,
+    AuthenticationError,
+    NotFoundError,
+    BadRequestError
+)
+
+__version__ = '0.3.0'
