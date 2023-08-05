556aaab4a1da1a014dc7189562303007de956009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:22 +0000

    Add package okpy/1.14.18/okpy-1.14.18.tar.gz

---
+++ packages/okpy/okpy-1.14.18.tar.gz/okpy-1.14.18/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import client
+
+VERSION = client.__version__
+
+setup(
+    name='okpy',
+    version=VERSION,
+    author='John Denero, Soumya Basu, Stephen Martinis, Sharad Vikram, Albert Wu',
+    # author_email='',
+    description=('ok.py supports programming projects by running tests, '
+                'tracking progress, and assisting in debugging.'),
+    # long_description=long_description,
+    url='https://github.com/okpy/ok-client',
+    # download_url='https://github.com/okpy/ok/releases/download/v{}/ok'.format(VERSION),
+
+    license='Apache License, Version 2.0',
+    keywords=['education', 'autograding'],
+    packages=find_packages(include=[
+        'client',
+        'client.*',
+    ]),
+    package_data={
+        'client': ['config.ok'],
+    },
+    # install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'ok=client.cli.ok:main',
+            'ok-publish=client.cli.publish:main',
+            'ok-lock=client.cli.lock:main',
+            'ok-test=client.cli.test:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'requests==2.12.4',
+        'coverage==3.7.1'
+    ],
+)
