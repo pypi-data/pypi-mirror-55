de1cc89f256a522c86999c37aae197f8ae99a6fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package blockapi/0.0.37/blockapi-0.0.37.tar.gz

---
+++ packages/blockapi/blockapi-0.0.37.tar.gz/blockapi-0.0.37/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+PACKAGES = find_packages(where='.')
+
+setuptools.setup(
+    name='blockapi',
+    version='0.0.37',
+    author='Devmons s.r.o.',
+    description='BlockAPI library',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=PACKAGES,
+    install_requires=[
+        'requests', 
+        'pytz', 
+        'python-dateutil',
+        'coinaddrng',
+        'cfscrape',
+        'gevent',
+        'ethereum_input_decoder',
+        'web3'
+    ],
+)
+
