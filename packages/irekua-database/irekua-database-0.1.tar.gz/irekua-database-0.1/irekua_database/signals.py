5cffe9f91dfd9e6f6e0e7b30214179446609ce20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:55 +0000

    Add package irekua-database/0.1/irekua-database-0.1.tar.gz

---
+++ packages/irekua-database/irekua-database-0.1.tar.gz/irekua-database-0.1/irekua_database/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+
+@receiver(pre_save)
+def validate_model(sender, instance, raw=False, **kwargs):
+    try:
+        app_label = instance._meta.app_label
+    except AttributeError:
+        return
+
+    if app_label != 'database':
+        return
+
+    if not raw:
+        instance.full_clean()
