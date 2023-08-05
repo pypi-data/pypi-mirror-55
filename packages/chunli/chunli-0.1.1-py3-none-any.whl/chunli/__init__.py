dbcd682bed3d5845dd0aa47b903b92b7859deb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:11 +0000

    Add package chunli/0.1.1/chunli-0.1.1-py3-none-any.whl

---
+++ packages/chunli/chunli-0.1.1-py3-none-any.whl/chunli/__init__.py
@@ -0,0 +1,21 @@
+"""chunli"""
+
+from confdaora import confdaora_env
+from dictdaora import DictDaora
+
+
+__version__ = '0.1.1'
+
+
+class AppConfig(DictDaora):
+    redis_target: str = 'redis://'
+    debug: bool = False
+
+
+config: AppConfig = confdaora_env(AppConfig)
+
+
+from .app import app  # noqa isort:skip
+
+
+__all__ = ['app', 'config']
