2d8d73d44b7d0cf19bf77154fae8e8aa11030bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package pizdets/0.3.0/pizdets-0.3.0.tar.gz

---
+++ packages/pizdets/pizdets-0.3.0.tar.gz/pizdets-0.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='pizdets',
+      version='0.3.0',
+      description='PMachine Python package',
+      url='https://github.com/catalin-rusnac/pizdets',
+      author='Catalin Rusnac',
+      author_email='crusnac@ist.ac.at',
+      license='MIT',
+      packages=['pizdets',"pizdets.pm"],
+      install_requires=["zmq","google","pandas","protobuf","matplotlib"],
+      zip_safe=True,
+      python_requires='>=3.0')
+
+
+
+#%%
