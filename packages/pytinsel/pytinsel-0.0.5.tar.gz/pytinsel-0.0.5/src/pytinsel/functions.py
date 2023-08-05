6373a0a60d0bb5cdbe76a753a24848677eef07be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package pytinsel/0.0.5/pytinsel-0.0.5.tar.gz

---
+++ packages/pytinsel/pytinsel-0.0.5.tar.gz/pytinsel-0.0.5/src/pytinsel/functions.py
@@ -0,0 +1,70 @@
+import dis
+from inspect import signature
+from types import CodeType, FunctionType
+from functools import wraps
+
+
+def hof(func):
+
+    def wrapper(*args, **kwargs):
+
+        _SF = dis.opmap['STORE_FAST'].to_bytes(1, byteorder='little')
+        _LC = dis.opmap['LOAD_CONST'].to_bytes(1, byteorder='little')
+        _LF = dis.opmap['LOAD_FAST'].to_bytes(1, byteorder='little')
+
+        from inspect import signature
+        func_sig = signature(func)
+
+        byte_code = func.__code__.co_code
+        _byte_code = b''
+
+        for i in range(0, len(byte_code), 2):
+            ith_byte = bytes([byte_code[i]])
+            iplus1th_byte = bytes([byte_code[i + 1]])
+            if ith_byte == _LC:
+                _byte_code = _byte_code + ith_byte + bytes([int.from_bytes(iplus1th_byte, 'little') + 1])
+            else:
+                _byte_code = _byte_code + ith_byte + iplus1th_byte
+
+        _co_consts = ((func.__code__.co_consts[0],)) + (args[0],)
+        _new_co_consts = _co_consts + func.__code__.co_consts[1:]
+        if len(func.__code__.co_consts) > 1:
+            _co_consts = _co_consts + func.__code__.co_consts[1:]
+        _co_varnames = func.__code__.co_varnames[1:] + ('self',)
+
+        const_load = dis.opmap['LOAD_CONST'].to_bytes(1, byteorder='little') + b'\x01'
+        const_store = dis.opmap['STORE_FAST'].to_bytes(1, byteorder='little') + b'\x02'
+        __byte_code = const_load + const_store + _byte_code[:]
+        ___byte_code = b''
+        for i in range(0, len(__byte_code), 2):
+            ith_byte = bytes([__byte_code[i]])
+            iplus1th_byte = bytes([__byte_code[i + 1]])
+
+            if ith_byte == _LF and iplus1th_byte in [b'\x00']:
+                ___byte_code = ___byte_code + ith_byte + bytes([len(func.__code__.co_varnames) - 1])
+            elif ith_byte == _SF and iplus1th_byte in [b'\x02']:
+                ___byte_code = ___byte_code + ith_byte + bytes([len(func.__code__.co_varnames) - 1])
+            elif ith_byte == _LF and iplus1th_byte in list(map(lambda x: bytes([x]), list(range(1, len(func_sig.parameters))))):
+                ___byte_code = ___byte_code + ith_byte + bytes([int.from_bytes(iplus1th_byte, 'little') - 1])
+            else:
+                ___byte_code = ___byte_code + ith_byte + iplus1th_byte
+
+        _func_code = CodeType(
+            func.__code__.co_argcount - 1,
+            func.__code__.co_kwonlyargcount,
+            func.__code__.co_nlocals,
+            func.__code__.co_stacksize,
+            func.__code__.co_flags,
+            ___byte_code,
+            _co_consts,
+            func.__code__.co_names,
+            _co_varnames,
+            func.__code__.co_filename,
+            func.__code__.co_name,
+            func.__code__.co_firstlineno,
+            func.__code__.co_lnotab,
+            func.__code__.co_freevars,
+            func.__code__.co_cellvars
+        )
+        return FunctionType(_func_code, func.__globals__)
+    return wrapper
