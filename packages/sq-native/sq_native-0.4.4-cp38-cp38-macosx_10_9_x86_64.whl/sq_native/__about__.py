1f0ee1da22c0cda1b0e3ce4e737119d85c05a858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:53 +0000

    Add package sq-native/0.4.4/sq_native-0.4.4-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/sq-native/sq_native-0.4.4-cp38-cp38-macosx_10_9_x86_64.whl/sq_native/__about__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016, 2017, 2018, 2019 Sqreen. All rights reserved.
+# Please refer to our terms for more information:
+#
+#     https://www.sqreen.io/terms.html
+#
+
+__author__ = "Sqreen"
+__copyright__ = "Copyright 2016, 2017, 2018, 2019, Sqreen"
+__email__ = "contact@sqreen.io"
+__license__ = "proprietary"
+# The major version should strictly follow PowerWAF API version.
+__version__ = "0.4.4"
+__lib_version__ = "0.4.0"
