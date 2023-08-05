da352b79f8685c9b5d58556eb031a31adb51ac60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:20 +0000

    Add package data-view/0.1.1/data-view-0.1.1.tar.gz

---
+++ packages/data-view/data-view-0.1.1.tar.gz/data-view-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+     name='data-view',  
+     version='0.1.1',
+     author="Urev Oleg",
+     author_email="urevolegg@gmail.com",
+     description="Automated view of dataset",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     packages=find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3.6",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=['pandas', 'numpy', 'matplotlib', 'ipython', 'seaborn'],
+ )
