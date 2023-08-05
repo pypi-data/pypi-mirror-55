ff6f131607bba95adffe969d12679068c0aa4bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:44 +0000

    Add package jupyter-notebookparams/0.0.4/jupyter-notebookparams-0.0.4.tar.gz

---
+++ packages/jupyter-notebookparams/jupyter-notebookparams-0.0.4.tar.gz/jupyter-notebookparams-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='jupyter-notebookparams',
+    version='0.0.4',
+    author='Simon Li',
+    author_email='spli@dundee.ac.uk',
+    packages=[
+        'jupyter_notebookparams',
+    ],
+    url='https://github.com/manics/jupyter-notebookparams',
+    license='MIT',
+    package_data={
+        'jupyter_notebookparams': ['jupyter_notebookparams/static/main.js'],
+    },
+    description='Pass URL parameters to a Jupyter notebook',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'notebook',
+    ],
+    data_files=[(
+            'share/jupyter/nbextensions/jupyter-notebookparams', [
+                'jupyter_notebookparams/static/main.js'
+        ]),
+        ('etc/jupyter/nbconfig/notebook.d' , ['jupyter_notebookparams.json'])
+    ],
+    zip_safe=False,
+    include_package_data=True,
+)
