2e3f50b6338e21926b221e9fb9103767f623b926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package Exercise-publishing-package/0.0.1/Exercise_publishing_package-0.0.1-py3-none-any.whl

---
+++ packages/Exercise-publishing-package/Exercise_publishing_package-0.0.1-py3-none-any.whl/cars/sample.py
@@ -0,0 +1,14 @@
+from cars import nissan
+from cars import fiat
+
+nissan_car = nissan.Nissan()
+fiat_car = fiat.Fiat()
+
+
+# def main():
+#     pass
+
+
+if __name__ == "__main__":
+    nissan_car.get_models()
+    fiat_car.get_models()
