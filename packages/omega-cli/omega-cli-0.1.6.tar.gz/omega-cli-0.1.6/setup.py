2cb4e1128c3d61e2565c4490a34091eb3f9241b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package omega-cli/0.1.6/omega-cli-0.1.6.tar.gz

---
+++ packages/omega-cli/omega-cli-0.1.6.tar.gz/omega-cli-0.1.6/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='omega-cli',
+    version='0.1.6',
+    description='Omega installer for Numworks',
+    author='Quentin Guidée',
+    author_email='quentin.guidee@gmail.com',
+    url='https://github.com/Omega-Numworks/Omega-CLI-Installer/releases/download/0.1.6/omega-cli-0.1.6.tar.gz',
+    packages=['omega_cli'],
+    install_requires=[
+        'click',
+        'PyInquirer'
+    ],
+    entry_points={
+        'console_scripts': ['omega-cli=omega_cli.installer:main'],
+    }
+)
