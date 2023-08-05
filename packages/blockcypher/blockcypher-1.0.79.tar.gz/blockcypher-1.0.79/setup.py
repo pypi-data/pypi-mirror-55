424796726f9780de69eea8a3c5a26d78dfe9bd88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package blockcypher/1.0.79/blockcypher-1.0.79.tar.gz

---
+++ packages/blockcypher/blockcypher-1.0.79.tar.gz/blockcypher-1.0.79/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='blockcypher',
+      version='1.0.79',
+      description='BlockCypher Python Library',
+      author='Michael Flaxman',
+      author_email='mflaxman+blockcypher@gmail.com',
+      url='https://github.com/blockcypher/blockcypher-python/',
+      install_requires=[
+          'requests<3.0.0',
+          'python-dateutil<3.0.0',
+          'bitcoin==1.1.39',
+          ],
+      packages=['blockcypher'],
+      include_package_data=True,
+      package_data={"": ["LICENSE"]},
+      )
