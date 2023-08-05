8eab726cdd4311f3933b5b09cde95cdbafd6401c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:00 +0000

    Add package quisk/4.1.47/quisk-4.1.47.tar.gz

---
+++ packages/quisk/quisk-4.1.47.tar.gz/quisk-4.1.47/afedrinet/test.py
@@ -0,0 +1,12 @@
+import os,sys
+from ctypes import *
+os.environ['PATH'] = os.path.dirname(__file__) + ';' + os.environ['PATH']
+from sdr_control import *
+#import sdr_control
+
+#class self(control)
+cnt = Control()
+cnt.OpenHW()
+cnt.SetHWLO(28050000)
+cnt.SetHWSR(740740)
+cnt.CloseHW()
