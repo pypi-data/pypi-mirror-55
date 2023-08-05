cc79751e015564251d693cb8d6050b8d76ef7568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:20 +0000

    Add package textgrid-utils/0.0.4/textgrid_utils-0.0.4.tar.gz

---
+++ packages/textgrid-utils/textgrid_utils-0.0.4.tar.gz/textgrid_utils-0.0.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='textgrid_utils',
+    version='0.0.4',
+    description='utilities for working with textgrid files',
+    url='https://github.com/giphahne/textgrid-utils',
+    author='Dan Hahne',
+    author_email='contact@danhahne.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='linguistics textgrid praat',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['argcomplete'],
+    package_data={
+        '': ['package_data.dat'],
+    },
+    entry_points={
+        'console_scripts': [
+            'merge-and-mark-textgrid-tiers=textgrid_utils:merge_main',
+        ],
+    },
+)
