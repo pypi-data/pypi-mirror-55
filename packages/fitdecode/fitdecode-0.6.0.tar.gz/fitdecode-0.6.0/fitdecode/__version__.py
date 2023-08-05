706e54b875d36a2a566fad7a20489a7f556f8ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:35 +0000

    Add package fitdecode/0.6.0/fitdecode-0.6.0.tar.gz

---
+++ packages/fitdecode/fitdecode-0.6.0.tar.gz/fitdecode-0.6.0/fitdecode/__version__.py
@@ -0,0 +1,21 @@
+# fitdecode
+#
+# Copyright (c) 2018-2019 Jean-Charles Lefebvre
+# All rights reserved.
+#
+# This code is licensed under the MIT License.
+# See the LICENSE.txt file at the root of this project.
+
+__version__ = '0.6.0'
+version_info = (0, 6, 0)
+
+__title__ = 'fitdecode'
+__fancy_title__ = 'fitdecode'
+__description__ = 'FIT file parser and decoder'
+__url__ = 'https://github.com/polyvertex/fitdecode'
+__license__ = 'MIT'
+__author__ = 'Jean-Charles Lefebvre'
+__author_email__ = 'polyvertex@gmail.com'
+__copyright__ = 'Copyright 2018-2019 Jean-Charles Lefebvre'
+
+__keywords__ = ['fit', 'ant', 'file', 'parse', 'parser', 'decode', 'decoder']
---
+++ packages/fitdecode/fitdecode-0.6.0.tar.gz/fitdecode-0.6.0/fitdecode/exceptions.py
@@ -0,0 +1,45 @@
+# fitdecode
+#
+# Copyright (c) 2018-2019 Jean-Charles Lefebvre
+# All rights reserved.
+#
+# This code is licensed under the MIT License.
+# See the LICENSE.txt file at the root of this project.
+
+
+class FitError(Exception):
+    pass
+
+
+class FitHeaderError(FitError):
+    pass
+
+
+class FitCRCError(FitError):
+    pass
+
+
+class FitEOFError(FitError):
+    def __init__(self, expected, got, offset, message=''):
+        self.expected = expected  #: number of expected bytes
+        self.got = got  #: number of bytes read
+        self.offset = offset  #: the file offset from which reading took place
+
+        desc = f'expected {self.expected} bytes, got {self.got} @ {self.offset}'
+        if not message:
+            message = desc
+        else:
+            message += ' (' + desc + ')'
+
+        super().__init__(message)
+
+
+class FitParseError(FitError):
+    def __init__(self, offset, message=''):
+        self.offset = offset  #: the file offset from which reading took place
+
+        desc = 'FIT parsing error @ ' + str(offset)
+        if message:
+            desc += ': ' + message
+
+        super().__init__(desc)
