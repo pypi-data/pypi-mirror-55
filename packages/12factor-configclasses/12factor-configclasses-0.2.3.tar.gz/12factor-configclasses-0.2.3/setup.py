7f506417e5379bf12b9a08737d6445f4109d6357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package 12factor-configclasses/0.2.3/12factor-configclasses-0.2.3.tar.gz

---
+++ packages/12factor-configclasses/12factor-configclasses-0.2.3.tar.gz/12factor-configclasses-0.2.3/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['configclasses']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['starlette>=0.12.12,<0.13.0']
+
+extras_require = \
+{'dotenv': ['python-dotenv[dotenv]>=0.10.3,<0.11.0'],
+ 'full': ['tomlkit[toml]>=0.5.8,<0.6.0',
+          'python-dotenv[dotenv]>=0.10.3,<0.11.0',
+          'pyyaml[yaml]>=5.1,<6.0'],
+ 'toml': ['tomlkit[toml]>=0.5.8,<0.6.0'],
+ 'yaml': ['pyyaml[yaml]>=5.1,<6.0']}
+
+setup_kwargs = {
+    'name': '12factor-configclasses',
+    'version': '0.2.3',
+    'description': 'Like dataclasses but for config.',
+    'long_description': None,
+    'author': 'Pablo Cabezas',
+    'author_email': 'pabcabsal@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
