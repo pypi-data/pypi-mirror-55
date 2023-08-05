744430fde06722510b9cd774384c437f979c3a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:47 +0000

    Add package PyFineract/0.0.81/PyFineract-0.0.81.tar.gz

---
+++ packages/PyFineract/PyFineract-0.0.81.tar.gz/PyFineract-0.0.81/fineract/objects/__init__.py
@@ -0,0 +1,8 @@
+from .client import Client
+from .datatable import DataTable
+from .document import Document
+from .hook import Hook
+from .loan import Loan
+from .report import Report
+
+__all__ = ['Client', 'DataTable', 'Document', 'Report', 'Hook', 'Loan']
