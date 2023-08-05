8c8caa4c7fdec82faf5ed4a6089779f8a30e9fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:38 +0000

    Add package undictify/0.7.1/undictify-0.7.1.tar.gz

---
+++ packages/undictify/undictify-0.7.1.tar.gz/undictify-0.7.1/undictify/__init__.py
@@ -0,0 +1,8 @@
+from ._unpack import type_checked_call, type_checked_constructor
+
+name = "undictify"
+
+__author__ = "Tobias Hermann"
+__copyright__ = "Copyright 2018, Tobias Hermann"
+__email__ = "editgym@gmail.com"
+__license__ = "MIT"
