544238c1e39f0030502c3eeafc324c30bac80a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:17 +0000

    Add package azw3-converter/1/azw3-converter-1.tar.gz

---
+++ packages/azw3-converter/azw3-converter-1.tar.gz/azw3-converter-1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='azw3-converter',
+    version='1',
+    packages=['azw3'],
+    url='https://github.com/esafb52/azw3',
+    license='GPL',
+    author='masiha ahmadi',
+    author_email='esafb52@gmail.com',
+    description='basic utility tools using calibre app in windows 10 for convert docx to azw3 format for kindle'
+    ,
+)
