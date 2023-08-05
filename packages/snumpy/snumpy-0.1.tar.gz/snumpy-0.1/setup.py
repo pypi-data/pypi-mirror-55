e6b4aa6a3c6f786f748a9a5eb1f32e22a262bdb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:18 +0000

    Add package snumpy/0.1/snumpy-0.1.tar.gz

---
+++ packages/snumpy/snumpy-0.1.tar.gz/snumpy-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='snumpy',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Sagar gupta',
+      license='MIT',
+      packages=['snumpy'],
+      zip_safe=False)
