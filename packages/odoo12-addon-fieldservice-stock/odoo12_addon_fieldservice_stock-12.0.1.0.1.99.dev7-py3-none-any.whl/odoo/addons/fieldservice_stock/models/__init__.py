ebf79c8c13ddf6d3766214932472b94cfbcf3a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package odoo12-addon-fieldservice-stock/12.0.1.0.1.99.dev7/odoo12_addon_fieldservice_stock-12.0.1.0.1.99.dev7-py3-none-any.whl

---
+++ packages/odoo12-addon-fieldservice-stock/odoo12_addon_fieldservice_stock-12.0.1.0.1.99.dev7-py3-none-any.whl/odoo/addons/fieldservice_stock/models/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2018 - TODAY, Brian McMaster
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    fsm_location,
+    fsm_order,
+    stock,
+    fsm_equipment,
+    product_template,
+    stock_production_lot,
+    fsm_wizard,
+    stock_request,
+    stock_request_order,
+)
