b9dc3f5dfe24b0884728e0ad9bdb0ed090a9493a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:28 +0000

    Add package ILog/1.0.1/ILog-1.0.1.tar.gz

---
+++ packages/ILog/ILog-1.0.1.tar.gz/ILog-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='ILog',
+    version='1.0.1',
+    description='A simple log segmentation module',
+    author='wangyong',
+    author_email='15711294367@163.com',
+    url='https://github.com/wangyong321/wylog',
+    packages=find_packages()
+)
