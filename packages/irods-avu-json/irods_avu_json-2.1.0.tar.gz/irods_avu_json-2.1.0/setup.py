9d1242a72aa2c0d471fca9f27a569ec4878e025b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package irods-avu-json/2.1.0/irods_avu_json-2.1.0.tar.gz

---
+++ packages/irods-avu-json/irods_avu_json-2.1.0.tar.gz/irods_avu_json-2.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="irods_avu_json",
+    version="2.1.0",
+    author="Maastricht University - DataHub",
+    author_email="datahub@maastrichtuniversity.nl",
+    description="Bidirectional conversion between JSON(-LD) and iRODS AVUs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MaastrichtUniversity/irods_avu_json",
+    packages=['jsonavu'],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+)
