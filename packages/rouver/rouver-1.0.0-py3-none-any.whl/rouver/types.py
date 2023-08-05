368d43d60e3e54c4c0bb9623bbae3cbbc47dd721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:27 +0000

    Add package rouver/1.0.0/rouver-1.0.0-py3-none-any.whl

---
+++ packages/rouver/rouver-1.0.0-py3-none-any.whl/rouver/types.py
@@ -0,0 +1,26 @@
+from typing import Callable, Tuple, Dict, Any, Iterable, Sequence, Mapping
+
+from werkzeug.wrappers import Request
+
+# (name, value)
+Header = Tuple[str, str]
+
+WSGIEnvironment = Dict[str, Any]
+
+# (body) -> None
+StartResponseReturnType = Callable[[bytes], None]
+
+# (status: str, headers: List[Headers], exc_info) -> response
+StartResponse = Callable[..., StartResponseReturnType]
+
+WSGIResponse = Iterable[bytes]
+
+WSGIApplication = Callable[[WSGIEnvironment, StartResponse], WSGIResponse]
+
+# (method, path, callback)
+RouteDescription = Tuple[str, str, WSGIApplication]
+
+# (request, previous_args, path_part) -> result
+RouteTemplateHandler = Callable[[Request, Sequence[Any], str], Any]
+
+BadArgumentsDict = Mapping[str, str]
