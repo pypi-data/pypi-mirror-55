418d4622ef20f2398b5a51e6771181275fb0372c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:51 +0000

    Add package sense2vec/1.0.0a2/sense2vec-1.0.0a2.tar.gz

---
+++ packages/sense2vec/sense2vec-1.0.0a2.tar.gz/sense2vec-1.0.0a2/sense2vec/__init__.py
@@ -0,0 +1,5 @@
+from .sense2vec import Sense2Vec  # noqa: F401
+from .component import Sense2VecComponent  # noqa: F401
+from .util import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
