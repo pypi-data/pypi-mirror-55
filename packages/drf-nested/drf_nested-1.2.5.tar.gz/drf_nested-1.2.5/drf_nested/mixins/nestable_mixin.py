e4ebd0856f07ce37248d596f9e3b99db32bc1c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package drf-nested/1.2.5/drf_nested-1.2.5.tar.gz

---
+++ packages/drf-nested/drf_nested-1.2.5.tar.gz/drf_nested-1.2.5/drf_nested/mixins/nestable_mixin.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from rest_framework.fields import empty
+
+from .base_nestable_mixin import BaseNestableMixin
+from drf_nested.utils.queryset_to_instance import nested_validate, nested_update
+
+
+class NestableMixin(BaseNestableMixin):
+    write_source: Optional[str] = None
+
+    def __init__(self, instance=None, data=empty, **kwargs):
+        if 'write_source' in kwargs:
+            self.write_source = kwargs.pop('write_source')
+
+        super().__init__(instance, data, **kwargs)
+
+    @nested_validate
+    def validate(self, data):
+        return super().validate(data)
+
+    @nested_update
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
