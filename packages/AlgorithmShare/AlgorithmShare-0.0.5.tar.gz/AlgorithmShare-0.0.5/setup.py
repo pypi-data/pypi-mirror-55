c9acfaba85a2b299fdd5e8a9db818b7866ad2a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:57 +0000

    Add package AlgorithmShare/0.0.5/AlgorithmShare-0.0.5.tar.gz

---
+++ packages/AlgorithmShare/AlgorithmShare-0.0.5.tar.gz/AlgorithmShare-0.0.5/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='AlgorithmShare',
+    version='0.0.5',
+    author='qinwei',
+    author_email='qinwei17@otcaix.iscas.ac.cn',
+    url='http://earthdataminer.casearth.cn',
+    description='Intelligent Algorithm Share Framework',
+    long_description=open('README.md').read(),
+    packages=['AlgorithmShare'],
+    install_requires=[
+		'requests',
+		'urllib3'
+	]
+)
