a088a4a69d265999b413819f70a9b940a90a532a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:20 +0000

    Add package hibpcli/0.2.0/hibpcli-0.2.0-py3-none-any.whl

---
+++ packages/hibpcli/hibpcli-0.2.0-py3-none-any.whl/hibpcli/keepass.py
@@ -0,0 +1,16 @@
+from pykeepass import PyKeePass
+
+from hibpcli.password import Password
+
+
+def check_passwords_from_db(path, master_password):
+    """ - """
+    kp = PyKeePass(path, password=master_password)
+
+    leaked_entries = []
+    for entry in kp.entries:
+        p = Password(password=entry.password)
+        if p.is_leaked():
+            leaked_entries.append(entry)
+
+    return leaked_entries
