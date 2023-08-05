141b9e4299d205199504b7d65dc41655a8fceaf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:14 +0000

    Add package Auptimizer/1.0/Auptimizer-1.0.tar.gz

---
+++ packages/Auptimizer/Auptimizer-1.0.tar.gz/Auptimizer-1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+BASE_URL="https://github.com/LGE-ARC-AdvancedAI/auptimizer"
+
+setup(
+    name='Auptimizer',
+    version="1.0",
+    author="LG Electronics Inc.",
+    author_email="auptimizer@lge.com",
+    
+    license='SPDX-License-Identifier: GPL-3.0-or-later',
+    long_description=open('README.md').read(),
+
+    url=BASE_URL,
+    project_urls={
+        "Bug Tracker": BASE_URL+"/issues",
+        "Documentation": "https://lge-arc-advancedai.github.io/auptimizer/",
+        "Source Code": BASE_URL,
+    },
+    # install_requires=open("requirements.txt").readlines(),
+    packages=find_packages("src", exclude=["tests"]),
+    package_dir={"": "src"}
+)
