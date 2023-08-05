bca101a6d482eccec5a70ad496d608293bd3e365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:51 +0000

    Add package msal-extensions/0.1.3/msal-extensions-0.1.3.tar.gz

---
+++ packages/msal-extensions/msal-extensions-0.1.3.tar.gz/msal-extensions-0.1.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import re, io
+
+__version__ = re.search(
+    r'__version__\s*=\s*[rRfFuU]{0,2}[\'"]([^\'"]*)[\'"]',
+    io.open('msal_extensions/__init__.py', encoding='utf_8_sig').read()
+    ).group(1)
+
+setup(
+    name='msal-extensions',
+    version=__version__,
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+    ],
+    package_data={'': ['LICENSE']},
+    install_requires=[
+        'msal>=0.4.1,<2.0.0',
+        'portalocker~=1.0',
+    ],
+    tests_require=['pytest'],
+)
