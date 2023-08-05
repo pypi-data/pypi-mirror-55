b89f825c6ff03694da21e968500b0589c9250c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:17 +0000

    Add package cloudformation-utils/0.0.1/cloudformation-utils-0.0.1.tar.gz

---
+++ packages/cloudformation-utils/cloudformation-utils-0.0.1.tar.gz/cloudformation-utils-0.0.1/setup.py
@@ -0,0 +1,36 @@
+# coding=utf8
+from setuptools import setup
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='cloudformation-utils',
+      version='0.0.1',
+      description='Utility functions for working with AWS CloudFormation templates',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/NitorCreations/cloudformation-utils',
+      download_url='https://github.com/NitorCreations/cloudformation-utils',
+      author='Pasi Niemi',
+      author_email='pasi.niemi@nitor.com',
+      license='Apache 2.0',
+      packages=['cloudformation_utils'],
+      include_package_data=True,
+      scripts=[],
+      entry_points={
+          'console_scripts': [],
+      },
+      setup_requires=['pytest-runner'],
+      install_requires=[],
+      tests_require=[
+          'pytest==4.6.5',
+          'pytest-mock==1.10.4',
+          'pytest-cov==2.7.1',
+          'requests-mock==1.6.0',
+          'pytest-runner',
+          'mock==3.0.5'
+      ],
+      test_suite='tests')
