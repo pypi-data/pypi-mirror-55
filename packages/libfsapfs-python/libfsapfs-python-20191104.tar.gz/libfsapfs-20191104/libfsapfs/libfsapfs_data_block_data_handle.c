0f00bb41901f2fb086d2155d9d8eea0f47dbecfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:54 +0000

    Add package libfsapfs-python/20191104/libfsapfs-python-20191104.tar.gz

---
+++ packages/libfsapfs-python/libfsapfs-python-20191104.tar.gz/libfsapfs-20191104/libfsapfs/libfsapfs_data_block_data_handle.c
@@ -0,0 +1,583 @@
+/*
+ * The data block data handle functions
+ *
+ * Copyright (C) 2018-2019, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <memory.h>
+#include <types.h>
+
+#include "libfsapfs_data_block.h"
+#include "libfsapfs_data_block_data_handle.h"
+#include "libfsapfs_definitions.h"
+#include "libfsapfs_file_extent.h"
+#include "libfsapfs_file_system_data_handle.h"
+#include "libfsapfs_libbfio.h"
+#include "libfsapfs_libcerror.h"
+#include "libfsapfs_libfcache.h"
+#include "libfsapfs_libfdata.h"
+#include "libfsapfs_unused.h"
+
+/* Creates data handle
+ * Make sure the value data_handle is referencing, is set to NULL
+ * Returns 1 if successful or -1 on error
+ */
+int libfsapfs_data_block_data_handle_initialize(
+     libfsapfs_data_block_data_handle_t **data_handle,
+     libfsapfs_io_handle_t *io_handle,
+     libfsapfs_encryption_context_t *encryption_context,
+     libcdata_array_t *file_extents,
+     uint8_t is_sparse,
+     libcerror_error_t **error )
+{
+	libfsapfs_file_extent_t *file_extent = NULL;
+	static char *function                = "libfsapfs_data_block_data_handle_initialize";	
+	uint64_t logical_offset              = 0;
+	uint32_t segment_flags               = 0;
+	int extent_index                     = 0;
+	int number_of_extents                = 0;
+	int segment_index                    = 0;
+
+	if( data_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid data handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( *data_handle != NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_ALREADY_SET,
+		 "%s: invalid data handle value already set.",
+		 function );
+
+		return( -1 );
+	}
+	if( io_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid IO handle.",
+		 function );
+
+		return( -1 );
+	}
+	*data_handle = memory_allocate_structure(
+	                libfsapfs_data_block_data_handle_t );
+
+	if( *data_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_INSUFFICIENT,
+		 "%s: unable to create data handle.",
+		 function );
+
+		goto on_error;
+	}
+	if( memory_set(
+	     *data_handle,
+	     0,
+	     sizeof( libfsapfs_data_block_data_handle_t ) ) == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_SET_FAILED,
+		 "%s: unable to clear data handle.",
+		 function );
+
+		memory_free(
+		 *data_handle );
+
+		*data_handle = NULL;
+
+		return( -1 );
+	}
+	if( libfsapfs_file_system_data_handle_initialize(
+	     &( ( *data_handle )->file_system_data_handle ),
+	     io_handle,
+	     encryption_context,
+	     file_extents,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_INITIALIZE_FAILED,
+		 "%s: unable to create file system data handle.",
+		 function );
+
+		goto on_error;
+	}
+	if( libfdata_vector_initialize(
+	     &( ( *data_handle )->data_block_vector ),
+	     (size64_t) io_handle->block_size,
+	     (intptr_t *) ( *data_handle )->file_system_data_handle,
+	     (int (*)(intptr_t **, libcerror_error_t **)) &libfsapfs_file_system_data_handle_free,
+	     NULL,
+	     (int (*)(intptr_t *, intptr_t *, libfdata_vector_t *, libfdata_cache_t *, int, int, off64_t, size64_t, uint32_t, uint8_t, libcerror_error_t **)) &libfsapfs_file_system_data_handle_read_data_block,
+	     NULL,
+	     LIBFDATA_DATA_HANDLE_FLAG_NON_MANAGED,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_INITIALIZE_FAILED,
+		 "%s: unable to create data block vector.",
+		 function );
+
+		goto on_error;
+	}
+	if( libcdata_array_get_number_of_entries(
+	     file_extents,
+	     &number_of_extents,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_GET_FAILED,
+		 "%s: unable to retrieve number of entries from array.",
+		 function );
+
+		goto on_error;
+	}
+	for( extent_index = 0;
+	     extent_index < number_of_extents;
+	     extent_index++ )
+	{
+		if( libcdata_array_get_entry_by_index(
+		     file_extents,
+		     extent_index,
+		     (intptr_t **) &file_extent,
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_GET_FAILED,
+			 "%s: unable to retrieve file extent: %d.",
+			 function,
+			 extent_index );
+
+			goto on_error;
+		}
+		if( file_extent == NULL )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_VALUE_MISSING,
+			 "%s: missing file extent: %d.",
+			 function,
+			 extent_index );
+
+			goto on_error;
+		}
+		if( ( is_sparse == 0 )
+		 || ( file_extent->physical_block_number != 0 ) )
+		{
+			if( file_extent->logical_offset != logical_offset )
+			{
+				libcerror_error_set(
+				 error,
+				 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+				 LIBCERROR_ARGUMENT_ERROR_VALUE_OUT_OF_BOUNDS,
+				 "%s: invalid file extent: %d - logical offset value out of bounds.",
+				 function,
+				 extent_index );
+
+				goto on_error;
+			}
+		}
+		segment_flags = 0;
+
+		if( ( is_sparse != 0 )
+		 && ( file_extent->physical_block_number == 0 ) )
+		{
+			segment_flags = LIBFDATA_RANGE_FLAG_IS_SPARSE;
+		}
+		if( libfdata_vector_append_segment(
+		     ( *data_handle )->data_block_vector,
+		     &segment_index,
+		     extent_index,
+		     file_extent->physical_block_number * io_handle->block_size,
+		     file_extent->data_size,
+		     segment_flags,
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_APPEND_FAILED,
+			 "%s: unable to append extent: %d as data block vector segment.",
+			 function,
+			 extent_index );
+
+			goto on_error;
+		}
+		logical_offset += file_extent->data_size;
+	}
+	if( libfcache_cache_initialize(
+	     &( ( *data_handle )->data_block_cache ),
+	     LIBFSAPFS_MAXIMUM_CACHE_ENTRIES_DATA_BLOCKS,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_INITIALIZE_FAILED,
+		 "%s: unable to create data block cache.",
+		 function );
+
+		goto on_error;
+	}
+	( *data_handle )->io_handle = io_handle;
+
+	return( 1 );
+
+on_error:
+	if( *data_handle != NULL )
+	{
+		if( ( *data_handle )->data_block_cache != NULL )
+		{
+			libfcache_cache_free(
+			 &( ( *data_handle )->data_block_cache ),
+			 NULL );
+		}
+		if( ( *data_handle )->data_block_vector != NULL )
+		{
+			libfdata_vector_free(
+			 &( ( *data_handle )->data_block_vector ),
+			 NULL );
+		}
+		if( ( *data_handle )->file_system_data_handle != NULL )
+		{
+			libfsapfs_file_system_data_handle_free(
+			 &( ( *data_handle )->file_system_data_handle ),
+			 NULL );
+		}
+		memory_free(
+		 *data_handle );
+
+		*data_handle = NULL;
+	}
+	return( -1 );
+}
+
+/* Frees data handle
+ * Returns 1 if successful or -1 on error
+ */
+int libfsapfs_data_block_data_handle_free(
+     libfsapfs_data_block_data_handle_t **data_handle,
+     libcerror_error_t **error )
+{
+	static char *function = "libfsapfs_data_block_data_handle_free";
+	int result            = 1;
+
+	if( data_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid data handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( *data_handle != NULL )
+	{
+		if( libfcache_cache_free(
+		     &( ( *data_handle )->data_block_cache ),
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_FINALIZE_FAILED,
+			 "%s: unable to free data block cache.",
+			 function );
+
+			result = -1;
+		}
+		if( libfdata_vector_free(
+		     &( ( *data_handle )->data_block_vector ),
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_FINALIZE_FAILED,
+			 "%s: unable to free data block vector.",
+			 function );
+
+			result = -1;
+		}
+		if( libfsapfs_file_system_data_handle_free(
+		     &( ( *data_handle )->file_system_data_handle ),
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_FINALIZE_FAILED,
+			 "%s: unable to free file system data handle.",
+			 function );
+
+			result = -1;
+		}
+		memory_free(
+		 *data_handle );
+
+		*data_handle = NULL;
+	}
+	return( result );
+}
+
+/* Reads data from the current offset into a buffer
+ * Callback for the data stream
+ * Returns the number of bytes read or -1 on error
+ */
+ssize_t libfsapfs_data_block_data_handle_read_segment_data(
+         libfsapfs_data_block_data_handle_t *data_handle,
+         libbfio_handle_t *file_io_handle,
+         int segment_index LIBFSAPFS_ATTRIBUTE_UNUSED,
+         int segment_file_index LIBFSAPFS_ATTRIBUTE_UNUSED,
+         uint8_t *segment_data,
+         size_t segment_data_size,
+         uint32_t segment_flags LIBFSAPFS_ATTRIBUTE_UNUSED,
+         uint8_t read_flags LIBFSAPFS_ATTRIBUTE_UNUSED,
+         libcerror_error_t **error )
+{
+	libfsapfs_data_block_t *data_block = NULL;
+	static char *function              = "libfsapfs_data_block_data_handle_read_segment_data";
+	size_t data_block_offset           = 0;
+	size_t read_size                   = 0;
+	size_t segment_data_offset         = 0;
+	uint64_t block_number              = 0;
+
+	LIBFSAPFS_UNREFERENCED_PARAMETER( segment_index )
+	LIBFSAPFS_UNREFERENCED_PARAMETER( segment_file_index )
+	LIBFSAPFS_UNREFERENCED_PARAMETER( segment_flags )
+	LIBFSAPFS_UNREFERENCED_PARAMETER( read_flags )
+
+	if( data_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid data handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( data_handle->io_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_MISSING,
+		 "%s: invalid data handle - missing IO handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( data_handle->current_segment_offset < 0 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_VALUE_OUT_OF_BOUNDS,
+		 "%s: invalid data handle - current segment offset value out of bounds.",
+		 function );
+
+		return( -1 );
+	}
+	if( segment_data == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid segment data.",
+		 function );
+
+		return( -1 );
+	}
+	if( segment_data_size > (size_t) SSIZE_MAX )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_VALUE_EXCEEDS_MAXIMUM,
+		 "%s: invalid segment data size value exceeds maximum.",
+		 function );
+
+		return( -1 );
+	}
+	block_number      = data_handle->current_segment_offset / data_handle->io_handle->block_size;
+	data_block_offset = data_handle->current_segment_offset % data_handle->io_handle->block_size;
+
+	while( segment_data_size > 0 )
+	{
+		if( libfdata_vector_get_element_value_by_index(
+		     data_handle->data_block_vector,
+		     (intptr_t *) file_io_handle,
+		     (libfdata_cache_t *) data_handle->data_block_cache,
+		     block_number,
+		     (intptr_t **) &data_block,
+		     0,
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_GET_FAILED,
+			 "%s: unable to retrieve data block: %" PRIu64 ".",
+			 function,
+			 block_number );
+
+			return( -1 );
+		}
+		if( data_block == NULL )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_VALUE_MISSING,
+			 "%s: invalid data block: %" PRIu64 ".",
+			 function,
+			 block_number );
+
+			return( -1 );
+		}
+		if( data_block->data == NULL )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_VALUE_MISSING,
+			 "%s: invalid data block: %" PRIu64 " - missing data.",
+			 function,
+			 block_number );
+
+			return( -1 );
+		}
+		if( data_block_offset >= data_block->data_size )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_VALUE_OUT_OF_BOUNDS,
+			 "%s: invalid data block offset value out of bounds.",
+			 function );
+
+			return( -1 );
+		}
+		read_size = data_block->data_size - data_block_offset;
+
+		if( read_size > segment_data_size )
+		{
+			read_size = segment_data_size;
+		}
+		if( memory_copy(
+		     &( segment_data[ segment_data_offset ] ),
+		     &( data_block->data[ data_block_offset ] ),
+		     read_size ) == NULL )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_MEMORY,
+			 LIBCERROR_MEMORY_ERROR_COPY_FAILED,
+			 "%s: unable to copy data block: %" PRIu64 " data.",
+			 function,
+			 block_number );
+
+			return( -1 );
+		}
+		data_block_offset    = 0;
+		segment_data_offset += read_size;
+		segment_data_size   -= read_size;
+
+		block_number++;
+	}
+	data_handle->current_segment_offset += segment_data_offset;
+
+	return( (ssize_t) segment_data_offset );
+}
+
+/* Seeks a certain offset of the data
+ * Callback for the data stream
+ * Returns the offset if seek is successful or -1 on error
+ */
+off64_t libfsapfs_data_block_data_handle_seek_segment_offset(
+         libfsapfs_data_block_data_handle_t *data_handle,
+         intptr_t *file_io_handle LIBFSAPFS_ATTRIBUTE_UNUSED,
+         int segment_index LIBFSAPFS_ATTRIBUTE_UNUSED,
+         int segment_file_index LIBFSAPFS_ATTRIBUTE_UNUSED,
+         off64_t segment_offset,
+         libcerror_error_t **error )
+{
+	static char *function = "libfsapfs_data_block_data_handle_seek_segment_offset";
+
+	LIBFSAPFS_UNREFERENCED_PARAMETER( file_io_handle )
+	LIBFSAPFS_UNREFERENCED_PARAMETER( segment_index )
+	LIBFSAPFS_UNREFERENCED_PARAMETER( segment_file_index )
+
+	if( data_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid data handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( segment_offset < 0 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_VALUE_OUT_OF_BOUNDS,
+		 "%s: invalid segment offset value out of bounds.",
+		 function );
+
+		return( -1 );
+	}
+	data_handle->current_segment_offset = segment_offset;
+
+	return( segment_offset );
+}
+
