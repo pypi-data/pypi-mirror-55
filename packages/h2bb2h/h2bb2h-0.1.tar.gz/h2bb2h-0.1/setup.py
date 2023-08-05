e2126d72eadf9eeee9d3b3c0a4df0a0a839fb3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:31 +0000

    Add package h2bb2h/0.1/h2bb2h-0.1.tar.gz

---
+++ packages/h2bb2h/h2bb2h-0.1.tar.gz/h2bb2h-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages  
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+     name='h2bb2h',
+     version='0.1',
+     packages = find_packages(),
+     author="Samarth Tripathi",
+     author_email="samarthtripathi@gmail.com",
+     description="Human to Byte , Byte to Human",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/Samarth-Tripathi/h2bb2h",
+ )
