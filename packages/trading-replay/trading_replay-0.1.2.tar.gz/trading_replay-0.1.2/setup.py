4867061b25ae47ff8b0c5064642314b7c0d7d3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:33 +0000

    Add package trading-replay/0.1.2/trading_replay-0.1.2.tar.gz

---
+++ packages/trading-replay/trading_replay-0.1.2.tar.gz/trading_replay-0.1.2/setup.py
@@ -0,0 +1,61 @@
+import os
+import codecs
+import sys
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+class UploadCommand(Command):
+    """Support setup.py publish."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except FileNotFoundError:
+            pass
+        self.status("Building Source distribution…")
+        os.system("{0} setup.py sdist bdist_wheel".format(sys.executable))
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("sudo twine upload dist/*")
+        sys.exit()
+
+
+
+setup(
+    name="trading_replay",
+    version="0.1.2",
+    description="A set of redis libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["replay"],
+    install_requires=['redis', 'rolling'], 
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass={"upload": UploadCommand},
+    
+)
