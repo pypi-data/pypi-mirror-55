6054c9d02e32b9ee2eb32110dc6ff5a14faa5adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:59 +0000

    Add package procrunner/1.1.0/procrunner-1.1.0.tar.gz

---
+++ packages/procrunner/procrunner-1.1.0.tar.gz/procrunner-1.1.0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup, find_packages
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = [
+    "six",
+    'pywin32; sys_platform=="win32"',
+]
+
+setup_requirements = []
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+if needs_pytest:
+    setup_requirements.append("pytest-runner")
+
+test_requirements = ["mock", "pytest"]
+
+setup(
+    author="Markus Gerstel",
+    author_email="scientificsoftware@diamond.ac.uk",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    description="Versatile utility function to run external processes",
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="procrunner",
+    name="procrunner",
+    packages=find_packages(include=["procrunner"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/DiamondLightSource/python-procrunner",
+    version="1.1.0",
+    zip_safe=False,
+)
