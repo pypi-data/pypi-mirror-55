fe14439149e84f654689911d6a488fe6baafe791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package azure-keyvault-secrets/4.0.0/azure-keyvault-secrets-4.0.0.zip

---
+++ packages/azure-keyvault-secrets/azure-keyvault-secrets-4.0.0.zip/azure-keyvault-secrets-4.0.0/azure/keyvault/secrets/__init__.py
@@ -0,0 +1,8 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+from ._models import DeletedSecret, KeyVaultSecret, SecretProperties
+from ._client import SecretClient
+
+__all__ = ["SecretClient", "KeyVaultSecret", "SecretProperties", "DeletedSecret"]
