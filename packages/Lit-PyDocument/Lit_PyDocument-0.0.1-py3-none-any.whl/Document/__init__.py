1b021f8b7b94c119f4a86db932b67cffd6126e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:14 +0000

    Add package Lit-PyDocument/0.0.1/Lit_PyDocument-0.0.1-py3-none-any.whl

---
+++ packages/Lit-PyDocument/Lit_PyDocument-0.0.1-py3-none-any.whl/Document/__init__.py
@@ -0,0 +1,9 @@
+class Document:
+
+    def write_about(self, string):
+        if(string == "assignment"):
+            print("In Python, an assigment refers to a newly created object")
+        else:
+            print("enter a valid string")
+
+
