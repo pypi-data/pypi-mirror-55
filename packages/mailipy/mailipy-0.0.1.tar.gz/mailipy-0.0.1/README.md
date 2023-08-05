e12846f46febe4348bb40741fa158b94c9fc81b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package mailipy/0.0.1/mailipy-0.0.1.tar.gz

---
+++ packages/mailipy/mailipy-0.0.1.tar.gz/mailipy-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Mailipy
+
+This is a software to make the task of sending bulk emails to a list of contacts easier.
---
+++ packages/mailipy/mailipy-0.0.1.tar.gz/mailipy-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="mailipy",
+    version="0.0.1",
+    author="William Di Luigi",
+    author_email="williamdiluigi@gmail.com",
+    description="Bulk send emails easily",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
