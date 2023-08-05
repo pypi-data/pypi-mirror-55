6e104886c25ab7d6018448c7d79185015ef76e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:13 +0000

    Add package treeviz/1.0/treeviz-1.0.tar.gz

---
+++ packages/treeviz/treeviz-1.0.tar.gz/treeviz-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'treeviz',
+  packages = ['treeviz'],
+  version = '1.0',
+  license='MIT',
+  description = 'Print tree in bash manner',
+  author = 'Chen Tsu Pei',
+  author_email = 'a5560648@gmail.com',
+  url = 'https://github.com/tsupei/treeviz',
+  download_url="https://github.com/tsupei/treeviz/archive/v1.0.tar.gz",
+  keywords = ['tree', 'treeviz'],
+  classifiers = [],
+)
