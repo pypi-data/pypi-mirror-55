7a6d3f709f411de1797c8298089a5aa7bebd60e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:22 +0000

    Add package pytest-quarantine/1.0.0/pytest_quarantine-1.0.0.tar.gz

---
+++ packages/pytest-quarantine/pytest_quarantine-1.0.0.tar.gz/pytest_quarantine-1.0.0/CHANGELOG.md
@@ -0,0 +1,46 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [1.0.0] - 2019-11-03
+
+### Added
+
+- Features, usage, etc. in README ([#15](https://github.com/EnergySage/pytest-quarantine/pull/15))
+- Plugin functionality and tests ([#16](https://github.com/EnergySage/pytest-quarantine/pull/16))
+- More tests ([#17](https://github.com/EnergySage/pytest-quarantine/pull/17))
+
+## [0.0.3] - 2019-11-02
+
+### Changed
+
+- Use GitHub CLI for release
+
+## [0.0.2] - 2019-11-01
+
+### Changed
+
+- Transfer to EnergySage
+
+## [0.0.1] - 2019-10-22
+
+### Changed
+
+- Tweak contributing guidelines
+
+## [0.0.0] - 2019-10-21
+
+### Added
+
+- Initial project structure, tooling, and contributing guidelines
+
+[Unreleased]: https://github.com/EnergySage/pytest-quarantine/compare/1.0.0...HEAD
+[1.0.0]: https://github.com/EnergySage/pytest-quarantine/releases/tag/1.0.0
+[0.0.3]: https://github.com/EnergySage/pytest-quarantine/releases/tag/0.0.3
+[0.0.2]: https://github.com/EnergySage/pytest-quarantine/releases/tag/0.0.2
+[0.0.1]: https://github.com/EnergySage/pytest-quarantine/releases/tag/0.0.1
+[0.0.0]: https://github.com/EnergySage/pytest-quarantine/releases/tag/0.0.0
---
+++ packages/pytest-quarantine/pytest_quarantine-1.0.0.tar.gz/pytest_quarantine-1.0.0/tests/test_quarantine.py
@@ -0,0 +1,121 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+import textwrap
+
+import pytest
+from pytest_quarantine import DEFAULT_QUARANTINE
+
+
+def test_default_file():
+    assert DEFAULT_QUARANTINE == "quarantine.txt"
+
+
+def test_options(testdir):
+    result = testdir.runpytest("--help")
+
+    result.stdout.fnmatch_lines(
+        ["quarantine:", "*--save-quarantine=*", "*--quarantine=*"]
+    )
+
+
+@pytest.fixture
+def failing_tests(testdir):
+    return testdir.makepyfile(
+        test_failing_tests="""\
+        import pytest
+
+        @pytest.fixture
+        def error():
+            assert False
+
+        def test_error(error):
+            assert True
+
+        def test_failure():
+            assert False
+
+        def test_pass():
+            assert True
+        """
+    )
+
+
+@pytest.mark.parametrize("quarantine_path", [None, ".quarantine"])
+def test_save_failing_tests(quarantine_path, testdir, failing_tests):
+    args = ["--save-quarantine"]
+    if quarantine_path:
+        args.append(quarantine_path)
+    else:
+        quarantine_path = DEFAULT_QUARANTINE
+
+    result = testdir.runpytest(*args)
+
+    result.stdout.fnmatch_lines(["save_quarantine: {}".format(quarantine_path)])
+    result.assert_outcomes(passed=1, failed=1, error=1)
+
+    quarantine = textwrap.dedent(
+        """\
+        test_failing_tests.py::test_error
+        test_failing_tests.py::test_failure
+        """
+    )
+    assert testdir.tmpdir.join(quarantine_path).read() == quarantine
+
+
+def test_no_save_with_passing_tests(testdir):
+    testdir.makepyfile(
+        """\
+        import pytest
+
+        def test_pass():
+            assert True
+        """
+    )
+
+    result = testdir.runpytest("--save-quarantine")
+
+    result.assert_outcomes(passed=1)
+    assert testdir.tmpdir.join(DEFAULT_QUARANTINE).check(exists=False)
+
+
+@pytest.mark.parametrize("quarantine_path", [None, ".quarantine"])
+def test_full_quarantine(quarantine_path, testdir, failing_tests):
+    args = ["--quarantine"]
+    if quarantine_path:
+        args.append(quarantine_path)
+    else:
+        quarantine_path = DEFAULT_QUARANTINE
+
+    quarantine = textwrap.dedent(
+        """\
+        test_failing_tests.py::test_error
+        test_failing_tests.py::test_failure
+        """
+    )
+    testdir.tmpdir.join(quarantine_path).write(quarantine)
+
+    result = testdir.runpytest(*args)
+
+    result.stdout.fnmatch_lines(["quarantine: {}".format(quarantine_path)])
+    result.assert_outcomes(passed=1, xfailed=2)
+
+
+def test_partial_quarantine(testdir, failing_tests):
+    quarantine = textwrap.dedent(
+        """\
+        test_failing_tests.py::test_failure
+        """
+    )
+    testdir.tmpdir.join(DEFAULT_QUARANTINE).write(quarantine)
+
+    result = testdir.runpytest("--quarantine")
+
+    result.assert_outcomes(passed=1, error=1, xfailed=1)
+
+
+def test_missing_quarantine(testdir, failing_tests):
+    result = testdir.runpytest("--quarantine")
+
+    result.assert_outcomes(passed=1, failed=1, error=1)
