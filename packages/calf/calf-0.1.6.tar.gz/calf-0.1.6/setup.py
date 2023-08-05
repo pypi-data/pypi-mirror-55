0bb9366ba9f0ccd13b74f049514b373ddfcde38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:11 +0000

    Add package calf/0.1.6/calf-0.1.6.tar.gz

---
+++ packages/calf/calf-0.1.6.tar.gz/calf-0.1.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='calf',
+    version='0.1.6',
+    python_requires='~=3.5',
+    author='Isaac To',
+    author_email='isaac.to@gmail.com',
+    description='Command Argument Loading Functions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/isaacto/calf',
+    packages=setuptools.find_packages(),
+    package_data={'calf': ['py.typed']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
