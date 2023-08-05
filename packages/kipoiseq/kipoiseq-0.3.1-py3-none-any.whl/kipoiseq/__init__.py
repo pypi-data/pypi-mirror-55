cfb31414ce74c92ba6f2f1174afec55530b7069a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:12 +0000

    Add package kipoiseq/0.3.1/kipoiseq-0.3.1-py3-none-any.whl

---
+++ packages/kipoiseq/kipoiseq-0.3.1-py3-none-any.whl/kipoiseq/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+__author__ = 'Kipoi team'
+__email__ = 'avsec@in.tum.de'
+__version__ = '0.3.1'
+
+from . import dataloaders
+from . import extractors
+from . import transforms
+
+from .dataclasses import Variant, Interval
