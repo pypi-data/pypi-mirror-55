bf72135230d45b4745ccfb54228ec0c1244db844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package atomic-cloud/0.0.2/atomic_cloud-0.0.2-py3-none-any.whl

---
+++ packages/atomic-cloud/atomic_cloud-0.0.2-py3-none-any.whl/aws/iam.py
@@ -0,0 +1,15 @@
+
+###########################################################################
+# IAM Roles and Profiles
+###########################################################################
+def list_roles():
+    pass
+
+def get_role(id: str):
+    pass
+
+def list_profiles():
+    pass
+
+def get_profile(id: str):
+    pass
