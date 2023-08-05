ce74ed57c1df0003bad390aae84214baaa052eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package cvtorch/0.0.5/cvtorch-0.0.5.tar.gz

---
+++ packages/cvtorch/cvtorch-0.0.5.tar.gz/cvtorch-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import sys, os
+
+with open('README.md', 'rb') as f:
+    readme = f.read().decode('utf-8')
+
+install_requires = [
+        'torch',
+        'torchvision',
+        'numpy',
+        'opencv-python',
+        ]
+
+setup(
+    name='cvtorch',
+    version='0.0.5',
+    description='vision tools based on opencv',
+    long_description=readme,
+    author='iHateTa11B0y',
+    author_email='1187203155@qq.com',
+    install_requires=install_requires,
+    packages=["cvtorch"],
+
+)
