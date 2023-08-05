70fc17a360c42c0d0729e4c5ef9d3edecfb8918f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package niloofar/1.3.1/niloofar-1.3.1.tar.gz

---
+++ packages/niloofar/niloofar-1.3.1.tar.gz/niloofar-1.3.1/niloofar/abolfazl.py
@@ -0,0 +1,15 @@
+class Nil(object):
+    def __init__(self, *args):
+        super(Nil, self).__init__(*args)
+        print("god is great like god")
+
+    def abolfazl(self,a):
+        if a>0:
+             return True
+        else:
+            return False
+
+
+
+
+
