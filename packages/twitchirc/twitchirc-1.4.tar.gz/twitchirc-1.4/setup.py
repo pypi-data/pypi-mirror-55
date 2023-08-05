d43f5a1fddf37939ce4f34f45ef2f0919145a723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:26 +0000

    Add package twitchirc/1.4/twitchirc-1.4.tar.gz

---
+++ packages/twitchirc/twitchirc-1.4.tar.gz/twitchirc-1.4/setup.py
@@ -0,0 +1,28 @@
+#  Library to make crating bots for Twitch chat easier.
+#  Copyright (c) 2019 Maciej Marciniak
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+setup(
+    name='twitchirc',
+    python_requires='>=3.6',
+    version='1.4',
+    packages=['twitchirc'],
+    url='',
+    license='GPLv3',
+    author='mm2pl',
+    author_email='',
+    description='Unofficial library for Twitch irc.'
+)
