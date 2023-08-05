823d17721978332899b6983456ee4736ee36c76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:54 +0000

    Add package yaml2txt/1.3/yaml2txt-1.3.tar.gz

---
+++ packages/yaml2txt/yaml2txt-1.3.tar.gz/yaml2txt-1.3/README.md
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+
+<h2>Converting .yaml file to text file</h2>
+
+<h1></h1>
+
+<h4>Framework for converting .yaml file to text file.</h4>
+
+<p></p>
+
+<p></p>
+
+<!-- How to make paragraph lines-->
+<p>How to use the framework.</p>
+
+<h3>Step 1</h3>
+<ol>
+    <li>"pip install yaml2txt" in your machine</li>
+</ol>
+
+
+<h3>Step 2</h3>
+<ol>
+  <li>import yaml2txt</li>
+</ol>
+
+
+
+<p></p>
+
+<h1></h1>
+
+
+<p>You can contact: david.johnnes@gmail.com  'for support'.</p>
+
+<h1></h1>
+
+
---
+++ packages/yaml2txt/yaml2txt-1.3.tar.gz/yaml2txt-1.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='yaml2txt',
+    version='1.3',
+    url="https://johnnes-smarts.ch",
+    license='License :: OSI Approved :: MIT License',
+    author='David Johnnes',
+    author_email='david.johnnes@gmail.com',
+    description='Framework for converting .yaml or .yml file to text file',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='DevOps, Audit Automation, Audit Orchestration, Vendor-Neutral configurations Audit',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
