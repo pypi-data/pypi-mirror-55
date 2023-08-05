4cecc1111600a79b4cdc9e5d7b7b9ac7b78c8afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package pybuildkite/0.0.3/pybuildkite-0.0.3-py3-none-any.whl

---
+++ packages/pybuildkite/pybuildkite-0.0.3-py3-none-any.whl/pybuildkite/client.py
@@ -0,0 +1,154 @@
+import requests
+
+
+class Client(object):
+    """
+    Internal API Client
+    """
+
+    def __init__(self):
+        """
+        Create class
+        """
+        self.access_token = ""
+
+    def is_access_token_set(self):
+        """
+        Has this client got an access token set in it
+
+        :return: true or false
+        """
+        return not self.access_token == ""
+
+    def set_client_access_token(self, access_token):
+        """
+        Set an access token to use for API calls
+        :param access_token: The token
+        """
+        self.access_token = access_token
+
+    def request(self, method, url, query_params=None, body=None, headers=None):
+        """
+        Make a request to the API
+
+        The request will be authorised if the access token is set
+
+        :param method: HTTP method to use
+        :param url: URL to call
+        :param query_params: Query parameters to use
+        :param body: Body of the request
+        :param headers: Dictionary of headers to use in HTTP request
+        :return: If headers are set response text is returned, otherwise parsed response is returned
+        """
+
+        query_params = self._clean_query_params(query_params or {})
+
+        if self.access_token:
+            query_params["access_token"] = self.access_token
+
+        if body:
+            body = self._clean_query_params(body)
+
+        query_params["per_page"] = "100"
+        query_params = self._convert_query_params_to_string_for_bytes(query_params)
+        response = requests.request(
+            method, url, headers=headers, params=str.encode(query_params), json=body
+        )
+        response.raise_for_status()
+
+        if method == "DELETE":
+            return response.ok
+        else:
+            return response.json()
+
+    def get(self, url, query_params=None, headers=None):
+        """
+        Make a GET request to the API
+
+        The request will be authorised if the access token is set
+
+        :param url: URL to call
+        :param query_params: Query parameters to append to URL
+        :param headers: Dictionary of headers to use in HTTP request
+        :return: If headers are set response text is returned, otherwise parsed response is returned
+        """
+
+        return self.request("GET", url=url, query_params=query_params, headers=headers)
+
+    def post(self, url, body=None, headers=None, query_params=None):
+        """
+        Make a POST request to the API
+
+        The request will be authorised if the access token is set
+
+        :param url: URL to call
+        :param body: Body of the request
+        :param query_params: Query parameters to append to URL
+        :param headers: Dictionary of headers to use in HTTP request
+        :return: If headers are set response text is returned, otherwise parsed response is returned
+        """
+
+        return self.request(
+            "POST", url=url, query_params=query_params, body=body, headers=headers
+        )
+
+    def put(self, url, body=None, headers=None, query_params=None):
+        """
+        Make a PUT request to the API
+
+        The request will be authorised if the access token is set
+
+        :param url: URL to call
+        :param body: Body of the request
+        :param headers: Dictionary of headers to use in HTTP request
+        :param query_params: Query parameters to append to URL
+        :return: If headers are set response text is returned, otherwise parsed response is returned
+        """
+
+        return self.request(
+            "PUT", url=url, query_params=query_params, body=body, headers=headers
+        )
+
+    def delete(self, url, body=None, headers=None, query_params=None):
+        """
+        Make a DELETE request to the API
+
+        The request will be authorised if the access token is set
+
+        :param url: URL to call
+        :param body: Body of the request
+        :param query_params: Query parameters to append to URL
+        :param headers: Dictionary of headers to use in HTTP request
+        :return: If headers are set response text is returned, otherwise parsed response is returned
+        """
+
+        return self.request(
+            "DELETE", url=url, query_params=query_params, body=body, headers=headers
+        )
+
+    @staticmethod
+    def _clean_query_params(query_params):
+        """
+
+        :param query_params:
+        :return:
+        """
+        return {key: value for key, value in query_params.items() if value is not None}
+
+    @staticmethod
+    def _convert_query_params_to_string_for_bytes(query_params):
+        """
+        Required to set multiple build states i.e ?state[]=running&state[]=scheduled
+
+        :param query_params: query parameters
+        :return: bytes of query param
+        """
+        query_string = ""
+        for key, value in query_params.items():
+            if query_string != "":
+                query_string += "&"
+            if key == "state":
+                query_string += value
+            else:
+                query_string += key + "=" + value
+        return query_string
---
+++ packages/pybuildkite/pybuildkite-0.0.3-py3-none-any.whl/pybuildkite/pipelines.py
@@ -0,0 +1,79 @@
+from pybuildkite.client import Client
+
+
+class Pipelines(Client):
+    """
+    Pipeline operations for the Buildkite API
+    """
+
+    def __init__(self, client, base_url):
+        """
+        Construct the class
+
+        :param client:
+        :param base_url:
+        """
+        self.client = client
+        self.path = base_url + "organizations/{}/pipelines/"
+
+    def list_pipelines(self, organization):
+        """
+        Returns a paginated list of an organization’s pipelines.
+
+        :param organization: Organization slug
+        :return: Returns a paginated list of an organization’s pipelines.
+        """
+        return self.client.get(self.path.format(organization))
+
+    def get_pipeline(self, organization, pipeline_name):
+        """
+        Get a pipeline
+
+        :param organization: Organization slug
+        :param pipeline_name: Pipeline slug
+        :return: A pipeline
+        """
+        return self.client.get(self.path.format(organization) + pipeline_name)
+
+    def create_pipeline(
+        self,
+        organization,
+        pipeline_name,
+        git_repository,
+        build_steps=[
+            dict(
+                type="script",
+                name=":pipeline:",
+                command="buildkite-agent pipeline upload",
+            )
+        ],
+    ):
+        """
+        Create a pipeline
+        :param build_steps: list of build pipeline steps
+        Command: { "type": "script", "name": "Script", "command": "command.sh" }
+        Wait for all previous steps to finish: { "type": "waiter" }
+        Block pipeline (see the job unblock API): { "type": "manual" }
+        Trigger pipeline: { "type": "trigger", "trigger_project_slug": "a-pipeline" }
+        :param organization: Organization slug
+        :param pipeline_name:Pipeline slug
+        :param git_repository: repo URL
+        :return:
+        """
+        data = {
+            "name": pipeline_name,
+            "repository": git_repository,
+            "steps": build_steps,
+        }
+
+        return self.client.post(self.path.format(organization), body=data)
+
+    def delete_pipeline(self, organization, pipeline):
+        """
+        Delete a pipeline
+        :param organization: Organization slug
+        :param pipeline:Pipeline slug
+        :return:
+        """
+        url = self.path.format(organization) + pipeline
+        return self.client.delete(url)
