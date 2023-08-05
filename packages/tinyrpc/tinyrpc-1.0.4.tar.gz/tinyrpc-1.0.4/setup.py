9189d2cd38c7ae307cb83d8c7f6e3a773a71afb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package tinyrpc/1.0.4/tinyrpc-1.0.4.tar.gz

---
+++ packages/tinyrpc/tinyrpc-1.0.4.tar.gz/tinyrpc-1.0.4/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='tinyrpc',
+    version='1.0.4',
+    description='A small, modular, transport and protocol neutral RPC '
+                'library that, among other things, supports JSON-RPC and zmq.',
+    long_description=read('README.rst'),
+    long_description_content_type="text/x-rst",
+    packages=find_packages(exclude=['tests', 'examples']),
+    keywords='json rpc json-rpc jsonrpc 0mq zmq zeromq',
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    maintainer='Leo Noordergraaf',
+    maintainer_email='leo@noordergraaf.net',
+    url='http://github.com/mbr/tinyrpc',
+    license='MIT',
+    install_requires=['six'],
+    extras_require={
+        'gevent': ['gevent'],
+        'httpclient': ['requests', 'websocket-client', 'gevent-websocket'],
+        'msgpack': ['msgpack'],
+        'websocket': ['gevent-websocket'],
+        'wsgi': ['werkzeug'],
+        'zmq': ['pyzmq'],
+        'jsonext': ['jsonext']
+    }
+)
