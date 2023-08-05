fbeea1395a8707c51615beff56575367df437b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:18 +0000

    Add package bareasgi-jinja2/3.1.0/bareasgi-jinja2-3.1.0.tar.gz

---
+++ packages/bareasgi-jinja2/bareasgi-jinja2-3.1.0.tar.gz/bareasgi-jinja2-3.1.0/bareasgi_jinja2/__init__.py
@@ -0,0 +1,8 @@
+"""Exports from bareasgi_jinja2"""
+
+from .jinja2_template_provider import add_jinja2, template
+
+__all__ = [
+    "add_jinja2",
+    "template"
+]
---
+++ packages/bareasgi-jinja2/bareasgi-jinja2-3.1.0.tar.gz/bareasgi-jinja2-3.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['bareasgi_jinja2']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bareasgi>=3.3,<4.0', 'jinja2>=2.10,<3.0']
+
+setup_kwargs = {
+    'name': 'bareasgi-jinja2',
+    'version': '3.1.0',
+    'description': 'Jinja2 support for bareasgi',
+    'long_description': "# bareASGI-jinja2\n\nJinja2 support for [bareASGI](http://github.com/rob-blackbourn/bareasgi) (read the [documentation](https://bareasgi-jinja2.readthedocs.io/en/latest/))\n\n## Usage\n\nTry the following.\n\n```python\nfrom typing import Mapping, Any\nimport jinja2\nimport os.path\nimport uvicorn\nfrom bareasgi import Application\nimport bareasgi_jinja2\n\nhere = os.path.abspath(os.path.dirname(__file__))\n\n@bareasgi_jinja2.template('example1.html')\nasync def http_request_handler(scope, info, matches, content):\n    return {'name': 'rob'}\n\napp = Application()\n\nenv = jinja2.Environment(\n    loader=jinja2.FileSystemLoader(os.path.join(here, 'templates')),\n    autoescape=jinja2.select_autoescape(['html', 'xml']),\n    enable_async=True\n)\n\nbareasgi_jinja2.add_jinja2(app, env)\n\napp.http_router.add({'GET'}, '/example1', http_request_handler)\n\nuvicorn.run(app, port=9010)\n\n```\n",
+    'author': 'Rob Blackbourn',
+    'author_email': 'rob.blackbourn@gmail.com',
+    'url': 'https://github.com/rob-blackbourn/bareasgi-jinja2',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
