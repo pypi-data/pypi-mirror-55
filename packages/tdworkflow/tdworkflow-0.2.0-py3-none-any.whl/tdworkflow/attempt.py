933d7c70a753f6e6fb3364c968086f3db207b8ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:47 +0000

    Add package tdworkflow/0.2.0/tdworkflow-0.2.0-py3-none-any.whl

---
+++ packages/tdworkflow/tdworkflow-0.2.0-py3-none-any.whl/tdworkflow/attempt.py
@@ -0,0 +1,55 @@
+import dataclasses
+from typing import Dict
+
+from .project import Project
+from .workflow import Workflow
+
+
+@dataclasses.dataclass
+class Attempt:
+    id: int
+    sessionId: int = -1
+    sessionUuid: str = ""
+    sessionTime: str = ""
+    workflow: Workflow = None
+    project: Project = None
+    index: int = -1
+    retryAttemptName: str = ""
+    done: bool = False
+    success: bool = False
+    cancelRequested: bool = False
+    params: Dict = None
+    createdAt: str = ""
+    finishedAt: str = ""
+
+    def __post_init__(self):
+        self.id = int(self.id)
+        self.project = Project(**self.project) if self.project else None
+        self.workflow = Workflow(**self.workflow) if self.workflow else None
+
+    @property
+    def session_id(self):
+        return self.sessionId
+
+    @property
+    def session_uuid(self):
+        return self.sessionUuid
+
+    @property
+    def sesesion_time(self):
+        return self.sessionTime
+
+    @property
+    def retry_attempt_name(self):
+        return self.retryAttemptName
+
+    @property
+    def cancel_requested(self):
+        return self.cancelRequested
+
+    @property
+    def finished_at(self):
+        return self.finishedAt
+
+    def finished(self):
+        return bool(self.finished_at)
