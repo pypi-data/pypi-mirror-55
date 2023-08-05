bd39e98ebcba2605bd8c089b2d06d14d161f3933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package geoopt/0.1.0/geoopt-0.1.0-py3-none-any.whl

---
+++ packages/geoopt/geoopt-0.1.0-py3-none-any.whl/geoopt/__init__.py
@@ -0,0 +1,22 @@
+from . import manifolds
+from . import optim
+from . import tensor
+from . import samplers
+from . import linalg
+
+from .tensor import ManifoldParameter, ManifoldTensor
+from .manifolds import (
+    Stiefel,
+    EuclideanStiefelExact,
+    CanonicalStiefel,
+    EuclideanStiefel,
+    Euclidean,
+    Sphere,
+    SphereExact,
+    PoincareBall,
+    PoincareBallExact,
+    ProductManifold,
+    Scaled,
+)
+
+__version__ = "0.1.0"
