2063eb063d6be2232db9ab54f67b787fbcba502b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:44 +0000

    Add package pyqcs/0.0.11/pyqcs-0.0.11.tar.gz

---
+++ packages/pyqcs/pyqcs-0.0.11.tar.gz/pyqcs-0.0.11/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension, find_packages
+
+
+basic_gates = Extension(
+                        "pyqcs.gates.implementations.basic_gates"
+                        , sources=["src/pyqcs/gates/implementations/basic_gates.c"]
+                        , extra_compile_args=["-fstack-protector", "-Wno-unused-variable"]
+                    )
+
+setup(
+        name="pyqcs"
+        , version="0.0.11"
+        , description="A quantum computing simulator."
+        , long_description = open("README.rst").read()
+        , ext_modules=[basic_gates]
+        , packages=find_packages(where="src")
+        , package_dir={"pyqcs": "src/pyqcs"}
+        , install_requires=["numpy"]
+        , project_urls={
+            "Source Code": "https://github.com/daknuett/pyqcs"
+        }
+        , author="Daniel Knüttel"
+        , author_email="daniel.knuettel@daknuett.eu"
+    )
+
