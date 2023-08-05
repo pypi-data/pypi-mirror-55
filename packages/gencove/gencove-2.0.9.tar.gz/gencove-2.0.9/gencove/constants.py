e0411408ed47d085f5d0a71bbf732b7839a0a49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:42 +0000

    Add package gencove/2.0.9/gencove-2.0.9.tar.gz

---
+++ packages/gencove/gencove-2.0.9.tar.gz/gencove-2.0.9/gencove/constants.py
@@ -0,0 +1,43 @@
+"""Describe all constants in Gencove CLI."""
+from collections import namedtuple
+
+HOST = "https://api.gencove.com"
+
+_APIEndpoint = namedtuple(
+    "ApiEndpoint",
+    [
+        "get_jwt",
+        "refresh_jwt",
+        "verify_jwt",
+        "upload_details",
+        "get_upload_credentials",
+        "project_samples",
+        "sample_details",
+        "sample_sheet",
+    ],
+)
+API_ENDPOINTS = _APIEndpoint(
+    "/api/v2/jwt-create/",
+    "/api/v2/jwt-refresh/",
+    "/api/v2/jwt-verify/",
+    "/api/v2/uploads-post-data/",
+    "/api/v2/upload-credentials/",
+    "/api/v2/project-samples/{id}",
+    "/api/v2/samples/{id}",
+    "/api/v2/sample-sheet/",
+)
+
+_SampleStatuses = namedtuple("SampleStatuses", ["succeeded", "failed"])
+SAMPLE_STATUSES = _SampleStatuses("succeeded", "failed")
+
+_SampleAssignmentStatus = namedtuple(
+    "SampleAssignmentStatus", ["all", "unassigned", "assigned"]
+)
+SAMPLE_ASSIGNMENT_STATUS = _SampleAssignmentStatus(
+    "all", "unassigned", "assigned"
+)
+
+Credentials = namedtuple("Credentials", ["email", "password"])
+Optionals = namedtuple("Optionals", ["host"])
+
+MAX_RETRY_TIME_SECONDS = 300  # 5 minutes
