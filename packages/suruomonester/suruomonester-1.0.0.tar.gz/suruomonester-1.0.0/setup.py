4a335c7c4ec03f2ca3e82b2fe10835dced6d1a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:26 +0000

    Add package suruomonester/1.0.0/suruomonester-1.0.0.tar.gz

---
+++ packages/suruomonester/suruomonester-1.0.0.tar.gz/suruomonester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="suruomonester",
+    version='1.0.0',
+    py_modules=['suruomonester'],
+    author="suruomo",
+    author_email="suruomo@hotmail.com",
+    url="http://github.com/suruomo",
+    description="A simple printer for nested lists",
+)
