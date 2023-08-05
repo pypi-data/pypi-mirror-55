8643fc7e7ff8092c1cba3011acc36ea27fd370ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:15 +0000

    Add package foolbox-native/0.6.2/foolbox-native-0.6.2.tar.gz

---
+++ packages/foolbox-native/foolbox-native-0.6.2.tar.gz/foolbox-native-0.6.2/foolbox/ext/native/__init__.py
@@ -0,0 +1,10 @@
+from os.path import join as _join
+from os.path import dirname as _dirname
+
+with open(_join(_dirname(__file__), "VERSION")) as _f:
+    __version__ = _f.read().strip()
+
+
+from . import models  # noqa: F401
+from . import attacks  # noqa: F401
+from . import utils  # noqa: F401
