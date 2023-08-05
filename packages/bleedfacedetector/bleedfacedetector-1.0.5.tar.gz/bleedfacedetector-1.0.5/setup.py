a6b0585a1762e8ad82a7717975d20d4695862f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package bleedfacedetector/1.0.5/bleedfacedetector-1.0.5.tar.gz

---
+++ packages/bleedfacedetector/bleedfacedetector-1.0.5.tar.gz/bleedfacedetector-1.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+
+
+setup(
+    name="bleedfacedetector",
+    version="1.0.5",
+    description="A Python package that lets users use 4 different face detectors",
+    long_description='You can find the usage for this library here: "https://github.com/TahaAnwar/bleedfacedetector"',
+    long_description_content_type="text/markdown",
+    url="https://github.com/TahaAnwar/bleedfacedetector",
+    author="Taha Anwar",
+    author_email="taha@bleedai.com",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+		'Intended Audience :: Developers',     
+        'Topic :: Software Development :: Build Tools',
+		
+    ],
+    packages=["bleedfacedetector"],
+    include_package_data=True,
+    install_requires=["dlib>=19.8.1","opencv-python","numpy"],
+    
+)
