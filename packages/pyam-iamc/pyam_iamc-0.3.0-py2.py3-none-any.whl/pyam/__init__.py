f8acb1547c661799e08321bfdb455eb312d899e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:42 +0000

    Add package pyam-iamc/0.3.0/pyam_iamc-0.3.0-py2.py3-none-any.whl

---
+++ packages/pyam-iamc/pyam_iamc-0.3.0-py2.py3-none-any.whl/pyam/__init__.py
@@ -0,0 +1,25 @@
+from pyam.core import *
+from pyam.utils import *
+from pyam.statistics import *
+from pyam.timeseries import *
+from pyam.read_ixmp import *
+from pyam.logger import *
+from pyam.run_control import *
+from pyam.iiasa import read_iiasa  # noqa: F401
+
+# in Jupyter notebooks: disable autoscroll, activate warnings
+try:
+    get_ipython().run_cell_magic(u'javascript', u'',
+                                 u'IPython.OutputArea.prototype._should_scroll = function(lines) { return false; }')
+    warnings.simplefilter('default')
+
+    def custom_formatwarning(msg, category, filename, lineno, line=''):
+        # ignore everything except the message
+        return str(msg) + '\n'
+    warnings.formatwarning = custom_formatwarning
+except Exception:
+    pass
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
