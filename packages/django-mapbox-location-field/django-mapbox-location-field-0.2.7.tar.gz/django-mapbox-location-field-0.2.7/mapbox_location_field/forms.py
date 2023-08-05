d548117950bea8c62c7206349c05a590a313e1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:09 +0000

    Add package django-mapbox-location-field/0.2.7/django-mapbox-location-field-0.2.7.tar.gz

---
+++ packages/django-mapbox-location-field/django-mapbox-location-field-0.2.7.tar.gz/django-mapbox-location-field-0.2.7/mapbox_location_field/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+from .widgets import MapInput, AddressAutoHiddenInput
+
+
+class LocationField(forms.CharField):
+    """custom form field for picking location"""
+
+    def __init__(self, *args, **kwargs):
+        map_attrs = kwargs.pop("map_attrs", None)
+        self.widget = MapInput(map_attrs=map_attrs, )
+
+        super().__init__(*args, **kwargs)
+        self.error_messages = {"required": "Please pick a location, it's required", }
+
+
+class AddressAutoHiddenField(forms.CharField):
+    """custom form field which uses AddressAutoHiddenInput"""
+    widget = AddressAutoHiddenInput()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.label = ""
