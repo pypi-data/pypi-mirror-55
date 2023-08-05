a3a8fcd6525c65e90c429b194ec0f424b4b03d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:36 +0000

    Add package hellowworlde/0.1/hellowworlde-0.1.tar.gz

---
+++ packages/hellowworlde/hellowworlde-0.1.tar.gz/hellowworlde-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hellowworlde',
+    version='0.1',
+    description='Hellow Worlde',
+    author='agung',
+    author_email='agung.96tm@gmail.com',
+    license='MIT',
+    packages=['hellowworlde'],
+    zip_safe=False
+)
