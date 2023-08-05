f20b10d6b75c83aebfe4e5e91c5a283fe19bb458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:44 +0000

    Add package cnewtonprop/0.0.1.dev1/cnewtonprop-0.0.1.dev1-py2.py3-none-any.whl

---
+++ packages/cnewtonprop/cnewtonprop-0.0.1.dev1-py2.py3-none-any.whl/cnewtonprop/__init__.py
@@ -0,0 +1,3 @@
+"""Top-level package for cnewtonprop."""
+
+__version__ = '0.0.1.dev1'
---
+++ packages/cnewtonprop/cnewtonprop-0.0.1.dev1-py2.py3-none-any.whl/cnewtonprop/propagator.pyx
@@ -0,0 +1,2 @@
+def _step(A, complex[::1] v, double dt, func, int m_max, int maxrestart, double tol):
+    pass
