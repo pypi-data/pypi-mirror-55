dba44a90b0727b187f7a059b73c51ed25b16149a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:15 +0000

    Add package hibpcli/0.1.0/hibpcli-0.1.0-py3-none-any.whl

---
+++ packages/hibpcli/hibpcli-0.1.0-py3-none-any.whl/hibpcli/cli.py
@@ -0,0 +1,34 @@
+import click
+
+from hibpcli.keepass import check_passwords_from_db
+
+
+@click.group()
+def main():
+    """Command line interface to the haveibeenpwned.com API."""
+    pass
+
+
+@click.command()
+def keepass():
+    """Check all passwords stored in the keepass database."""
+    path = click.prompt(
+        "Please enter the path to the database", type=click.Path(exists=True)
+    )
+    master_password = click.prompt(
+        "Please enter the master password for the database", hide_input=True
+    )
+    # needs error handling
+    rv = check_passwords_from_db(path=path, master_password=master_password)
+    if rv:
+        click.echo("The passwords of following entries are leaked:")
+        click.echo(rv)
+    else:
+        click.echo("Hooray, everything is safe!")
+
+
+main.add_command(keepass)
+
+
+if __name__ == "__main__":
+    main()
