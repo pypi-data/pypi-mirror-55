b930b51bad66872de2bc2b62c3887fd95bbf546d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:50 +0000

    Add package easy-spotify/v0.3-alpha/easy_spotify-v0.3-alpha.tar.gz

---
+++ packages/easy-spotify/easy_spotify-v0.3-alpha.tar.gz/easy_spotify-v0.3-alpha/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'easy_spotify',         # How you named your package folder (MyLib)
+  packages = ['easy_spotify'],   # Chose the same as "name"
+  version = 'v0.3-alpha',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Simple access the Spotify WEB API for general requests about artists, albums or songs',   # Give a short description about your library
+  author = 'Tilney-Bassett Oktarian',                   # Type in your name
+  author_email = 'ogt@connect.ust.hk',      # Type in your E-Mail
+  url = 'https://github.com/user/OktarianTB',
+  download_url = 'https://github.com/OktarianTB/easy_spotify/archive/v0.3-alpha.tar.gz',
+  keywords = ['SPOTIFY', 'WEB API'],   # Keywords that define your package best
+  install_requires=[
+          'requests'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
