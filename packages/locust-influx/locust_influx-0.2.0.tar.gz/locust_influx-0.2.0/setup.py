630aa46f168356327f827e9f6c4667452b61d41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package locust-influx/0.2.0/locust_influx-0.2.0.tar.gz

---
+++ packages/locust-influx/locust_influx-0.2.0.tar.gz/locust_influx-0.2.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+from locust_influx.__version__ import __version__
+
+with open('requirements.txt', 'r') as f:
+    requires = f.read()
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='locust_influx',
+    version=__version__,
+    url='https://github.com/lucrib/locust_influx',
+    license='Apache-2.0',
+    author='Lucas Ribeiro',
+    author_email='lucasribeiro1990@gmail.com',
+    description='Report locust metrics to influxdb.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={'': ['LICENSE']},
+    package_dir={'locust_influx': 'locust_influx'},
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    project_urls={
+        'Documentation': 'https://locust_influx.github.io',
+        'Source': 'https://github.com/lucrib/locust_influx',
+    },
+)
