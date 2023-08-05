3fd0a236ebd555a4c6c1c88e3c12dbd4ce5fbffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:21 +0000

    Add package lightweight/1.0.0.dev21/lightweight-1.0.0.dev21.tar.gz

---
+++ packages/lightweight/lightweight-1.0.0.dev21.tar.gz/lightweight-1.0.0.dev21/lightweight/content/markdown.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import date, datetime
+from pathlib import Path
+from typing import Optional, Union, TYPE_CHECKING
+
+from jinja2 import Template
+
+from lightweight.files import FileName
+from .content import Content
+from .lwmd import LwMarkdown
+
+if TYPE_CHECKING:
+    from lightweight import SitePath, Site
+
+
+@dataclass(frozen=True)
+class MarkdownPage(Content):
+    filename: FileName  # name of markdown file
+    source_path: Path
+    source: str  # the contents of a file
+    template: Template
+
+    title: Optional[str] = None
+    summary: Optional[str] = None
+    created: Optional[datetime] = None
+    updated: Optional[datetime] = None
+
+    def render(self, site: Site):
+        md_paths = {
+            str(content.source_path): path.url
+            for path, content in site.items()
+            if isinstance(content, MarkdownPage)
+        }
+        locations = {str(path): path.url for path in site}
+        link_mapping = dict(**md_paths, **locations)
+        html, toc_html = LwMarkdown(link_mapping).render(self.source)
+        return RenderedMarkdown(
+            html=html,
+            toc_html=toc_html,
+            # TODO:mdrachuk:2019-08-19: extract title from YAML Front Matter
+            # TODO:mdrachuk:2019-08-19: extract title from first heading
+            title=self.title,
+            summary=self.summary,
+            created=self.created,
+            updated=self.updated,
+        )
+
+    def write(self, path: SitePath):
+        path.create(self.template.render(
+            site=path.site,
+            source=self,
+            markdown=self.render(path.site)
+        ))
+
+
+def markdown(md_path: Union[str, Path], template: Template, **fields) -> MarkdownPage:
+    path = Path(md_path)
+    with path.open() as f:
+        source = f.read()
+    return MarkdownPage(
+        filename=FileName(path.name),
+        source_path=path,
+        source=source,
+        template=template,
+        **fields
+    )
+
+
+@dataclass(frozen=True)
+class RenderedMarkdown:
+    html: str
+    toc_html: str
+
+    title: Optional[str]
+    summary: Optional[str]
+    updated: Optional[date]
+    created: Optional[date]
---
+++ packages/lightweight/lightweight-1.0.0.dev21.tar.gz/lightweight-1.0.0.dev21/lightweight/path.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+from pathlib import Path, PurePath
+from typing import Union, Tuple, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from lightweight import Site
+
+
+class SitePath:
+    """An implementation of Path interface.
+    File system operations performed on real_path; relative path is used for all other operations.
+
+    `__str__` returns relative path representation.
+
+    Changed defaults:
+    - mkdir -- parents and exists_ok are made `True`.
+
+    Added:
+    - create -- create file with contents.
+    """
+
+    def __init__(self, path: Union[Path, str], site: Site):
+        self.site = site
+        self.relative_path = Path(path) if isinstance(path, str) else path
+
+    @property
+    def real_path(self):
+        return self.site.out / self.relative_path
+
+    @property
+    def name(self):
+        return self.relative_path.name
+
+    @property
+    def parts(self) -> Tuple[str, ...]:
+        return self.relative_path.parts
+
+    @property
+    def parent(self) -> SitePath:
+        return self.site.path(self.relative_path.parent)
+
+    @property
+    def suffix(self) -> str:
+        return self.relative_path.suffix
+
+    @property
+    def url(self) -> str:
+        location = str(self.with_suffix('') if self.suffix == '.html' else self)
+        return f'{self.site.url}/{location}'
+
+    def absolute(self) -> Path:
+        return self.real_path.absolute()
+
+    def exists(self) -> bool:
+        return self.real_path.exists()
+
+    def mkdir(self, mode=0o777, parents=True, exist_ok=True):
+        return self.real_path.mkdir(mode=mode, parents=parents, exist_ok=exist_ok)
+
+    def __truediv__(self, other: Union[SitePath, PurePath, str]):
+        other_path: Union[PurePath, str]
+        if isinstance(other, SitePath):
+            other_path = other.relative_path
+        elif isinstance(other, PurePath) or isinstance(other, str):
+            other_path = other
+        else:
+            raise ValueError(f'Cannot make a path with {other}')
+        return self.site.path(self.relative_path / other_path)
+
+    def __str__(self):
+        return str(self.relative_path)
+
+    def with_name(self, name: str) -> SitePath:
+        return self.site.path(self.relative_path.with_name(name))
+
+    def open(self, mode='r', buffering=-1, encoding=None, errors=None, newline=None):
+        return self.real_path.open(mode=mode, buffering=buffering, encoding=encoding, errors=errors, newline=newline)
+
+    def with_suffix(self, suffix: str) -> SitePath:
+        return self.site.path(self.relative_path.with_suffix(suffix))
+
+    def create(self, content: Union[str, bytes]) -> None:
+        self.parent.mkdir()
+        binary_mode = isinstance(content, bytes)
+        with self.open('wb' if binary_mode else 'w') as f:
+            f.write(content)
