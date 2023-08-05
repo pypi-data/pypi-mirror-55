02926c7f3eb3b454114e5b5e37b8f1f52e96a317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:24 +0000

    Add package pyexecutors/0.0.5/pyexecutors-0.0.5.tar.gz

---
+++ packages/pyexecutors/pyexecutors-0.0.5.tar.gz/pyexecutors-0.0.5/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name='pyexecutors',
+    packages=['pyexecutors'],
+    version='0.0.5',
+    license='MIT',  
+    description=
+    """
+         a lite-weight library to efficiently run series of 
+        asynchronous and synchronous tasks concurrently
+        without worrying about managing different threads
+        in your own.
+        
+    """,  
+    author='TANMAY MAJUMDAR',  
+    author_email='tanmaymajumdar5612@gmail.com',  
+    url='https://github.com/tanmay23235616/pyexecutors',  
+    download_url='https://github.com/tanmay23235616/pyexecutors/archive/0.0.5.tar.gz',
+    keywords=['PYEXECUTOR', 'TANMAY23235616', 'MULTITHREADING'],  
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        
+        'Intended Audience :: Developers',  
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3',  
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
