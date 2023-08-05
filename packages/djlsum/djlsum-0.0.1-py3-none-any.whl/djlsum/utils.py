9a12f469c63faa779c78cf41a78b1626b3ab954d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:43 +0000

    Add package djlsum/0.0.1/djlsum-0.0.1-py3-none-any.whl

---
+++ packages/djlsum/djlsum-0.0.1-py3-none-any.whl/djlsum/utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 17:04:02 2018
+
+@author: Jo
+"""
+
+#!/usr/bin/env python
+
+#-*- coding:utf-8 -*-
+
+def run():
+    print('This is a run package!')
+    print('建龙的帅是无法用语言描述的!')
+def sum(num1,num2):
+    print(num1+num2)
+    return num1+num2
+if __name__ == '__main__':
+      run()
