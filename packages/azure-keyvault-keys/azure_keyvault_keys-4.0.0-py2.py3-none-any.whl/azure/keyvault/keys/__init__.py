a62d82be88bd4718943384c6a410fa6930a70fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package azure-keyvault-keys/4.0.0/azure_keyvault_keys-4.0.0-py2.py3-none-any.whl

---
+++ packages/azure-keyvault-keys/azure_keyvault_keys-4.0.0-py2.py3-none-any.whl/azure/keyvault/keys/__init__.py
@@ -0,0 +1,18 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# -------------------------------------
+from ._enums import KeyCurveName, KeyOperation, KeyType
+from ._models import DeletedKey, JsonWebKey, KeyProperties, KeyVaultKey
+from ._client import KeyClient
+
+__all__ = [
+    "KeyClient",
+    "JsonWebKey",
+    "KeyVaultKey",
+    "KeyCurveName",
+    "KeyOperation",
+    "KeyType",
+    "DeletedKey",
+    "KeyProperties",
+]
