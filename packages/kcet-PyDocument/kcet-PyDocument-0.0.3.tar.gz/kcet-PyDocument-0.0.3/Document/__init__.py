995ccf6898c39703170a02fdbb06173145d164fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:52 +0000

    Add package kcet-PyDocument/0.0.3/kcet-PyDocument-0.0.3.tar.gz

---
+++ packages/kcet-PyDocument/kcet-PyDocument-0.0.3.tar.gz/kcet-PyDocument-0.0.3/Document/__init__.py
@@ -0,0 +1,8 @@
+class Document:
+ 
+    def write_about(self, string):
+        if(string == "iterable"):
+            print("An iterable is an object capable of returning its members one by one")
+        else:
+            print("enter a valid string")
+
