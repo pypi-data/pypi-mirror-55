ac0a59ee17fd0f2e529e93746c0cc549a7748503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:02 +0000

    Add package igeBullet/0.1.6/igeBullet-0.1.6.tar.gz

---
+++ packages/igeBullet/igeBullet-0.1.6.tar.gz/igeBullet-0.1.6/igeBullet.h
@@ -0,0 +1,26 @@
+#include <Python.h>
+
+typedef struct {
+	PyObject_HEAD
+		void* btshape;
+} shape_obj;
+
+typedef struct {
+	PyObject_HEAD
+		void* btbody;
+		PyObject* world;
+} rigidbody_obj;
+
+typedef struct {
+	PyObject_HEAD
+		void* broadphase;
+		void* dispatcher;
+		void* solver;
+		void* ghostPairCallback;
+		void* collisionConfiguration;
+		void* btworld;
+#if defined _WIN32
+		__int64 freq;
+#endif
+		double lasttime;
+} world_obj;
---
+++ packages/igeBullet/igeBullet-0.1.6.tar.gz/igeBullet-0.1.6/igeBullet_doc_en.h
@@ -0,0 +1,47 @@
+
+/////////////////////////////////////
+PyDoc_STRVAR(shape_doc,
+	"3d physics shape\n");
+
+PyDoc_STRVAR(getMeshData_doc,
+	"get rendering data\n");
+
+
+/////////////////////////////////////
+PyDoc_STRVAR(rigidbody_doc,
+	"3d physics rigid body\n");
+
+PyDoc_STRVAR(rigidbody_position_doc,
+	"position of rigid body\n");
+
+PyDoc_STRVAR(rigidbody_rotation_doc,
+	"rotation of rigid body\n");
+
+PyDoc_STRVAR(getShape_doc,
+	"get shape object\n");
+
+
+/////////////////////////////////////
+PyDoc_STRVAR(dynworld_doc,
+	"3d physics world\n");
+
+PyDoc_STRVAR(add_doc,
+	"Add rigidbody or constarain to world\n");
+PyDoc_STRVAR(remove_doc,
+	"Remove rigidbody or constarain from world\n");
+PyDoc_STRVAR(clear_doc,
+	"Clear rigidbody and constarain\n");
+PyDoc_STRVAR(step_doc,
+	"step simulation\n");
+PyDoc_STRVAR(wait_doc,
+	"wait until async simultion is end\n");
+PyDoc_STRVAR(gravity_doc,
+	"gravity vector\n");
+
+PyDoc_STRVAR(getNumCollisionObjects_doc,
+	"get number of collision objects in the world\n");
+
+PyDoc_STRVAR(getRigidBody_doc,
+	"get collision object\n");
+
+
