7ede4047651fc234d4d63e824214e49cebaa7981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:48 +0000

    Add package dlutils/0.0.6/dlutils-0.0.6-py3-none-any.whl

---
+++ packages/dlutils/dlutils-0.0.6-py3-none-any.whl/dlutils/__init__.py
@@ -0,0 +1,25 @@
+# Copyright 2018-2019 Stanislav Pidhorskyi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from .batch_provider import batch_provider
+from . import download
+from . import epoch
+from . import measures
+from . import random_rotation
+from . import reader
+from . import shuffle
+from . import timer
+
+use_cuda = None
