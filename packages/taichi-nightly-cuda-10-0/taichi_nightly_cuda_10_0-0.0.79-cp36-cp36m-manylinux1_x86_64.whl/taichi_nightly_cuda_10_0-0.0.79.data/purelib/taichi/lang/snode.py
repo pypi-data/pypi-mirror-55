4736899afc370efa1bab51795e38275c0661165f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:43 +0000

    Add package taichi-nightly-cuda-10-0/0.0.79/taichi_nightly_cuda_10_0-0.0.79-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/taichi-nightly-cuda-10-0/taichi_nightly_cuda_10_0-0.0.79-cp36-cp36m-manylinux1_x86_64.whl/taichi_nightly_cuda_10_0-0.0.79.data/purelib/taichi/lang/snode.py
@@ -0,0 +1,42 @@
+from .core import taichi_lang_core
+
+class SNode:
+  def __init__(self, ptr):
+    self.ptr = ptr
+
+  def dense(self, indices, dimensions):
+    if isinstance(dimensions, int):
+      dimensions = [dimensions] * len(indices)
+    return SNode(self.ptr.dense(indices, dimensions))
+
+  def dynamic(self, index, dimension):
+    assert len(index) == 1
+    return SNode(self.ptr.dynamic(index[0], dimension))
+
+  def pointer(self):
+    return SNode(self.ptr.pointer())
+
+  def bitmasked(self, val=True):
+    self.ptr.bitmasked(val)
+    return self
+
+  def place(self, *args):
+    from .expr import Expr
+    for arg in args:
+      if isinstance(arg, Expr):
+        self.ptr.place(Expr(arg).ptr)
+      elif isinstance(arg, list):
+        for x in arg:
+          self.place(x)
+      else:
+        arg.place(self)
+    return self
+
+  def lazy_grad(self):
+    self.ptr.lazy_grad()
+
+  def parent(self):
+    return SNode(self.ptr.snode().parent)
+
+  def data_type(self):
+    return self.ptr.data_type()
