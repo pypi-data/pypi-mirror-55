cdaa574b771b4b3ea32320cff48f174dea614402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:30 +0000

    Add package Dunner/0.0.1.5/Dunner-0.0.1.5.tar.gz

---
+++ packages/Dunner/Dunner-0.0.1.5.tar.gz/Dunner-0.0.1.5/README.md
@@ -0,0 +1,25 @@
+# dunner - Make your data science projects easier
+
+A Helper pip package for data cleaning, organizing, visualizing. machine learning and deep learning.
+
+
+### Assumptions:
+
++ Assuming python is installed on your system.
+
+
+# Description
+    
+It consists of three main modules:
+
+- `data_operations`: tools for some useful data operations
+- `data_cleaner`: tools to clean data
+- `preprocess_helper`: tools to help data operation and data cleaning
+
+# Installation
+ 
+## Normal installation
+
+```bash
+pip install dunner
+```
---
+++ packages/Dunner/Dunner-0.0.1.5.tar.gz/Dunner-0.0.1.5/setup.py
@@ -0,0 +1,25 @@
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+   name='Dunner',
+   version='0.0.1.5',
+   author="Emre Begen",
+   author_email="ebegen51@gmail.com",
+   description="A helper package for data science projects",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   url="https://github.com/ebegen/Dunner",
+   packages=setuptools.find_packages(),
+   classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+   ],
+   python_requires='>=3.7',
+ )
