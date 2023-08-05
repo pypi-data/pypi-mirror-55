6f851b53eaa724ff951f780ae1321796d5046a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package ethct/19.11.3/ethct-19.11.3.tar.gz

---
+++ packages/ethct/ethct-19.11.3.tar.gz/ethct-19.11.3/ethct.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+import argparse
+from web3 import Web3, HTTPProvider
+from constants import *
+from ethct_contract import *
+from ethct_helper import *
+
+def check_config():
+    with open(default_config_path, 'r') as f:
+        config = json.load(f)
+        if config['infurakey'] == '' and config['network'] != 'local':
+            print('please config your infura apikey')
+            exit()
+
+def check_privkey():
+    with open(default_config_path, 'r') as f:
+        config = json.load(f)
+        if len(config['accounts']) == 0:
+            print('please config your private key')
+            exit()
+        if config['defaultAccount'] >= len(config['accounts']):
+            print('please reset your defaultAccount')
+            exit()
+        
+def main():
+    parser = argparse.ArgumentParser()
+    # compile contract
+    parser.add_argument('-c', '--compile', help = "compile contract")
+    parser.add_argument('-s', '--save', help = "save compile output", action = 'store_true', default = False)
+    # deploy contract
+    parser.add_argument('-d', '--deploy', help = "deploy contract")
+    parser.add_argument('-n', '--network', help = "choose network")
+    parser.add_argument('-v', '--value', help = "deploy tx value", default = None, type = float)
+    parser.add_argument('--contract', help = "contract name to be deployed")
+    parser.add_argument('--args', help = "contract constructor arguments")
+    # call contract function
+    parser.add_argument('--address', help = "contract address")
+    parser.add_argument('--abi', help = "contract ABI file")
+    parser.add_argument('--call', help = "call contract function")
+    # tools
+    # get tx by hash
+    parser.add_argument('--gettx', help = "get transaction info by txhash")
+    # get block
+    parser.add_argument('--getblock', help = "get block by hash/block_number")
+    # get contract storage
+    parser.add_argument('--getstorage', help = "get contract storage")
+    parser.add_argument('--position', help = "contract storage position", type = int, default = 0)
+    # get account balance
+    parser.add_argument('--getbalance', help = "get account balance")
+    # config
+    parser.add_argument('--config', help = "add config options", action = "store_true")
+    parser.add_argument('--privkey', help = "add private key")
+    parser.add_argument('--infurakey', help = "set infura apikey")
+    parser.add_argument('--account', help = "set default account", default = None)
+    parser.add_argument('--delete', help = "delete account", type = int)
+
+    parser.add_argument('--showconfig', help = "print config", action = "store_true")
+    # sendtx
+    parser.add_argument('--sendtx', help = "send tx", action = "store_true")
+    parser.add_argument('--to', help = "tx receiver")
+    parser.add_argument('--data', help = "tx data")
+    parser.add_argument('--nonce', help = "tx nonce")
+    # get nonce
+    parser.add_argument('--getnonce', help = "get nonce of an address")
+
+    args = parser.parse_args()
+    network = NETWORK
+    if args.network is not None:
+        network = args.network
+    web3 = Web3(HTTPProvider(URL[network]))
+    if not args.config:
+        if not os.path.exists(default_config_path):
+            print('use `ethct --config --privkey <PRIVKEY> --infurakey <APIKEY> --network <NETWORK>` to config the tool first')
+            exit()
+        check_config()
+
+    if args.compile:
+        contract = Contract(args.compile)
+        contract.compile(contract = args.contract, save = args.save)
+    elif args.deploy:
+        check_privkey()
+        contract = Contract(args.deploy, provider_url = URL[network])
+        init_args = []
+        if args.args:
+            init_args = args.args.split(' ')
+        contract.deploy(contract = args.contract, args = init_args, value = args.value)
+    elif args.call:
+        check_privkey()
+        abifile = None
+        if args.contract and 'build' in os.listdir('./'):
+            for f in os.listdir('build'):
+                if args.contract + '.abi' == f:
+                    abifile = './build/' + f
+        if args.contract and abifile is None:
+            print('ABI not found for contract ' + args.contract)
+            exit()
+        if args.abi:
+            abifile = args.abi
+        contract = Contract(abifile = abifile, address = args.address, provider_url = URL[network])
+        arg_list = args.call.split(' ')
+        func_name = arg_list[0]
+        contract.call(func_name, arg_list[1:])
+    elif args.gettx:
+        txinfo = web3.eth.getTransaction(args.gettx)
+        print(txinfo)
+    elif args.getblock:
+        block = None
+        fulltx = False
+        if args.getblock in ['latest', 'earliest', 'pending']:
+            block = web3.eth.getBlock('latest', full_transactions = fulltx)
+        elif len(args.getblock) > 15:
+            block = web3.eth.getBlock(args.getblock, full_transactions = fulltx)
+        elif len(args.getblock) <= 15:
+            block = web3.eth.getBlock(int(args.getblock), full_transactions = fulltx)
+        print(block)
+    elif args.getstorage:
+        result = web3.eth.getStorageAt(args.getstorage, int(args.position))
+        print(result.hex())
+    elif args.getbalance:
+        result = web3.eth.getBalance(web3.toChecksumAddress(args.getbalance))
+        balance = web3.fromWei(result, 'ether')
+        print(balance)
+    elif args.config:
+        config = {}
+        if not os.path.exists(default_config_path):
+            config = {
+                    'accounts': [],
+                    'defaultAccount': 0,
+                    'network': 'ropsten',
+                    'infurakey': '',
+            }
+        else:
+            config = json.load(open(default_config_path, 'r'))
+        if args.network:
+            config['network'] = args.network
+        if args.privkey:
+            config['accounts'].append(args.privkey)
+            config['defaultAccount'] = len(config['accounts']) - 1
+        if args.infurakey:
+            config['infurakey'] = args.infurakey
+        if args.account is not None:
+            if int(args.account) >= 0 and int(args.account) < len(config['accounts']):
+                config['defaultAccount'] = int(args.account)
+            else:
+                print('account index out of range')
+                exit()
+        if args.delete:
+            config['accounts'].pop(args.delete)
+            config['defaultAccount'] = 0
+        f = open(default_config_path, 'w')
+        json.dump(config, f)
+    elif args.showconfig:
+        if not os.path.exists(default_config_path):
+            print('config file not found')
+        else:
+            config = json.load(open(default_config_path))
+            print(config)
+    elif args.sendtx:
+        if not args.value:
+            print('please provide tx value with `--value` option')
+            exit()
+        if not args.to:
+            print('please provider tx receiver with `--to` option')
+            exit()
+        nonce = None
+        data = ""
+        if args.nonce:
+            nonce = int(args.nonce)
+        if args.data:
+            data = args.data
+        sendtx(web3 = web3, to = args.to, value = args.value, nonce = nonce, data = data)
+    elif args.getnonce:
+        nonce = web3.eth.getTransactionCount(args.getnonce)
+        print(nonce)
+
+if __name__ == '__main__':
+    main()
---
+++ packages/ethct/ethct-19.11.3.tar.gz/ethct-19.11.3/ethct_contract.py
@@ -0,0 +1,139 @@
+import os
+import json
+import subprocess
+import platform
+from constants import *
+from web3 import Web3, HTTPProvider
+
+class Contract:
+
+    def __init__(self,
+            sourcefile = None,
+            abifile = None,
+            bytecodefile = None,
+            address = None,
+            provider_url = None):
+        if sourcefile is not None:
+            self.sourcefile = sourcefile
+        if provider_url is not None:
+            self.provider_url = provider_url
+        else:
+            self.provider_url = URL[NETWORK]
+        self.web3 = Web3(HTTPProvider(self.provider_url))
+        self.account = self.web3.eth.account.privateKeyToAccount(PRIVATE_KEY)
+        self.web3.eth.defaultAccount = self.account
+
+        if abifile is not None:
+            self.abi = json.load(open(abifile))
+        if bytecodefile is not None:
+            self.bytecode = open(bytecodefile).read()
+        if address is not None:
+            self.address = self.web3.toChecksumAddress(address)
+        if hasattr(self, 'abi') and hasattr(self, 'address'):
+            self.contract = self.web3.eth.contract(self.address, abi = self.abi)
+
+    def compile(self, contract = None, save = True):
+        if hasattr(self, 'abi') and hasattr(self, 'bytecode'):
+            print('contract has been compiled')
+            return
+        with open(self.sourcefile, 'r') as f:
+            source = f.read()
+            cmd = 'solc ' + self.sourcefile + ' --abi --bin -o ' + os.getcwd() + '/build --overwrite'
+            ret = subprocess.call(cmd, shell = True)
+            if ret != 0:
+                print('compile error, check if you have installed the `solc` solidity compile/correct the error in your code')
+                exit()
+        abifile = ''
+        bytecodefile = ''
+        for f in os.listdir(os.getcwd() + '/build'):
+            if contract is not None:
+                if contract + '.abi' == f:
+                    abifile = f
+                if contract + '.bin' == f:
+                    bytecodefile = f
+            else:
+                if 'abi' in f:
+                    abifile = f
+                if 'bin' in f:
+                    bytecodefile = f
+        self.abi = json.load(open(os.getcwd() + '/build/' + abifile))
+        self.bytecode = open(os.getcwd() + '/build/' + bytecodefile).read()
+        if not save:
+            cmd = "rm -rf build"
+            if 'windows' in platform.platform().lower():
+                cmd = "rmdir /s /q build"
+            subprocess.call(cmd, shell = True)
+
+    def deploy(self, contract = None, args = [], value = None, overwrite = False, show = True):
+        if hasattr(self, 'address') and not overwrite:
+            print('contract has been deployed')
+            return
+        if not hasattr(self, 'abi') or not hasattr(self, 'bytecode'):
+            self.compile(contract = contract, save = True)
+
+        deploy_tx = self.web3.eth.contract(abi = self.abi, bytecode = self.bytecode).constructor(*args).buildTransaction({
+            'from': self.account.address,
+            'value': 0 if value is None else self.web3.toWei(str(value), 'ether'),
+            'nonce': self.web3.eth.getTransactionCount(self.account.address),
+            'gas': 1000000,
+            'gasPrice': self.web3.toWei('21', 'gwei'),
+            })
+        signed = self.web3.eth.account.signTransaction(deploy_tx, self.account.privateKey)
+        txhash = self.web3.eth.sendRawTransaction(signed.rawTransaction)
+        receipt = self.web3.eth.waitForTransactionReceipt(txhash)
+        self.address = receipt['contractAddress']
+        if show:
+            print('contract address:', self.address)
+            print('receipt:', receipt)
+        return receipt
+
+    def call(self, func_name, arg_list, show = True):
+        abiinfo = {}
+        for info in self.abi:
+            if info['name'] == func_name:
+                abiinfo = info
+                break
+        func = self.contract.functions.__getitem__(func_name)
+        # for payable function call
+        value_item = None
+        for item in arg_list:
+            if 'value:' in item:
+                value_item = item
+        value = None
+        if value_item is not None and abiinfo['payable']:
+            arg_list.remove(value_item)
+            value = self.web3.toWei(value_item.split(':')[-1], 'ether')
+        # type convert
+        for i in range(len(abiinfo['inputs'])):
+            if abiinfo['inputs'][i]['type'] == 'bytes32':
+                if str(arg_list[i])[:2] == '0x':
+                    arg_list[i] = str(arg_list[i])
+                else:
+                    arg_list[i] = str(arg_list[i]).encode()
+            if abiinfo['inputs'][i]['type'] == 'address':
+                arg_list[i] = self.web3.toChecksumAddress(arg_list[i])
+            if 'int' in abiinfo['inputs'][i]['type']:
+                arg_list[i] = int(arg_list[i])
+        # call functions
+        if abiinfo['constant']:
+            result = func(*arg_list).call({'from': self.account.address}) 
+            if isinstance(result, bytes):
+                result = result.decode()
+            if show:
+                print(result)
+            return result
+        else:
+            tx = func(*arg_list).buildTransaction({
+                'from': self.account.address,
+                'value': 0 if value is None else value,
+                'nonce': self.web3.eth.getTransactionCount(self.account.address),
+                'gas': 1000000,
+                'gasPrice': self.web3.toWei('21', 'gwei'),
+                })
+            signed = self.web3.eth.account.signTransaction(tx, self.account.privateKey)
+            txhash = self.web3.eth.sendRawTransaction(signed.rawTransaction)
+            receipt = self.web3.eth.waitForTransactionReceipt(txhash)
+            if show:
+                print(receipt)
+            return receipt
+
