3774387e22810c5a8244e88a4deeb5eaedf5cf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:41 +0000

    Add package cazoo-cloudwatch-events/0.1.5/cazoo_cloudwatch_events-0.1.5.tar.gz

---
+++ packages/cazoo-cloudwatch-events/cazoo_cloudwatch_events-0.1.5.tar.gz/cazoo_cloudwatch_events-0.1.5/cazoo_cloudwatch_events/events.py
@@ -0,0 +1,22 @@
+import boto3
+from typing import List
+import boto3
+
+def put_events(events, log, client=None, region='eu-west-1'):
+    ''' Function to push events to CW Events
+        and control possible errors
+    '''
+    if not client:
+        cient = boto3.client("events", region_name=region)
+
+    cloudwatch_client = boto3.client('events', 'eu-west-1')
+    response = cloudwatch_client.put_events(
+        Entries=events
+    )
+    failed_entry_count: int = response.get("FailedEntryCount")
+    if failed_entry_count != 0:
+        event_logs: List[dict] = response.get("Entries")
+        log.error(
+            f"Could not push cloudwatch events. {failed_entry_count} events failed to push.",
+            extra=event_logs)
+        raise Exception("Did not push all required events to cloudwatch")
