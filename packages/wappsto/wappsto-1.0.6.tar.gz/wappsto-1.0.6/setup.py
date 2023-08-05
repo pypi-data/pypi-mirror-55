4b1daab7667f0a5a234ddc0530793b12a61aab64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:19 +0000

    Add package wappsto/1.0.6/wappsto-1.0.6.tar.gz

---
+++ packages/wappsto/wappsto-1.0.6.tar.gz/wappsto-1.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="wappsto",
+    version="1.0.6",
+    author="Seluxit A/S",
+    author_email="support@seluxit.com",
+    license="Apache-2.0",
+    description="Python Package to connect to wappsto.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wappsto/wappsto-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    packages=find_packages(),
+    tests_require=[
+        'pytest',
+    ],
+    python_requires='>3.6.0',
+)
