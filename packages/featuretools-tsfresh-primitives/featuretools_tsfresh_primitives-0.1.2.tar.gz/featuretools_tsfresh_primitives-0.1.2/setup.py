0ef44ac314eec51581f597515184c5cd0513ec81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:17 +0000

    Add package featuretools-tsfresh-primitives/0.1.2/featuretools_tsfresh_primitives-0.1.2.tar.gz

---
+++ packages/featuretools-tsfresh-primitives/featuretools_tsfresh_primitives-0.1.2.tar.gz/featuretools_tsfresh_primitives-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='featuretools_tsfresh_primitives',
+    version='0.1.2',
+    author='Feature Labs, Inc.',
+    author_email='support@featurelabs.com',
+    description='TSFresh primitives for featuretools',
+    license='MIT',
+    install_requires=['tsfresh>=0.11.2', 'featuretools>=0.7.0,<0.12.0'],
+    tests_require=open('test-requirements.txt').readlines(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'featuretools_plugin': [
+            'tsfresh = featuretools_tsfresh_primitives.__init__',
+        ],
+    },
+)
