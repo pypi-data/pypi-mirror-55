55dd4cca9efa129de050820b7e6f47217a9ff729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:35 +0000

    Add package universions/0.1.0.dev201911021426/universions-0.1.0.dev201911021426.tar.gz

---
+++ packages/universions/universions-0.1.0.dev201911021426.tar.gz/universions-0.1.0.dev201911021426/universions/node/__init__.py
@@ -0,0 +1,26 @@
+"""Module for node versions."""
+
+import subprocess
+from typing import Optional, Union
+from pathlib import Path
+from universions import Version, parse_semver
+
+
+def get_node_versions(
+    node_path: Optional[Union[Path, str]] = None
+) -> Optional[Version]:
+    """Get the Java versions.
+
+    Args:
+        node_path: The path to the node to check. If not defined, it uses "node"
+    Returns:
+        The Node version.
+
+    """
+    if node_path is None:
+        node_path = "node"
+    if isinstance(node_path, Path):
+        node_path = str(node_path.absolute())
+    version_string = subprocess.check_output([node_path, "--version"])
+    stripped = str(version_string, encoding="utf-8").strip()
+    return parse_semver(stripped, remove_initial_v=True)
