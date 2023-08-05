ed5cbb64b6d493cf88c477f357f10ee8ac1121c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:35 +0000

    Add package qencode3/0.9.18/qencode3-0.9.18.tar.gz

---
+++ packages/qencode3/qencode3-0.9.18.tar.gz/qencode3-0.9.18/setup.py
@@ -0,0 +1,28 @@
+"""A setuptools based setup module.
+"""
+
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='qencode3',
+    version='0.9.18',
+    description='Qencode Python SDK',
+    url='https://github.com/qencode-dev/qencode-api-python3-client',
+    # url=here,
+    author='Qencode Developer',
+    author_email='team@qencode.com',
+    license='proprietary',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3.6'
+
+    ],
+    keywords='qencode3, qencode.com, cloud.qencode.com',
+    packages=['qencode3']
+)   
