0ddef3c61a6d18d7b78f119f064f058b7b33d551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:22 +0000

    Add package apricot-select/0.4.0/apricot_select-0.4.0-py3.7.egg

---
+++ packages/apricot-select/apricot_select-0.4.0-py3.7.egg/apricot/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+# Author: Jacob Schreiber <jmschreiber91@gmail.com>
+
+from .featureBased import FeatureBasedSelection
+from .facilityLocation import FacilityLocationSelection
+from .mixture import SubmodularMixtureSelection
+
+__version__ = '0.4.0'
