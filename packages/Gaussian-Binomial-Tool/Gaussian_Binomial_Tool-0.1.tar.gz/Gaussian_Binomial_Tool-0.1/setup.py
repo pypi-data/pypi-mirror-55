8c69ce8d5ac011acf46ae570aed5e284aad9e006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:34 +0000

    Add package Gaussian-Binomial-Tool/0.1/Gaussian_Binomial_Tool-0.1.tar.gz

---
+++ packages/Gaussian-Binomial-Tool/Gaussian_Binomial_Tool-0.1.tar.gz/Gaussian_Binomial_Tool-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Gaussian_Binomial_Tool',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Gaussian_Binomial_Tool'],
+      zip_safe=False)
