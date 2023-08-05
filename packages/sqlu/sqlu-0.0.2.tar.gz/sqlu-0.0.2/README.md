4e6c7a4246472ba462414ff2d8c43bb3de8941f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:12 +0000

    Add package sqlu/0.0.2/sqlu-0.0.2.tar.gz

---
+++ packages/sqlu/sqlu-0.0.2.tar.gz/sqlu-0.0.2/README.md
@@ -0,0 +1,11 @@
+# sqlu
+
+sqlu is a simple tool for transforming or extracting info from sql statements
+
+## installation
+
+pip install squ
+
+## Useful functions
+
+- extract_tables_from_select: extract tables/views list from a select statement
---
+++ packages/sqlu/sqlu-0.0.2.tar.gz/sqlu-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sqlu", # Replace with your own username
+    version="0.0.2",
+    author="Matteo Redaelli",
+    author_email="matteo.redaelli@gmail.com",
+    description="sqlu is a simple utility for transforming or extracting info from sql statements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/matteo.redaelli/sqlu",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['flatten_json', 'moz_sql_parser'],
+    python_requires='>=3.6',
+)
