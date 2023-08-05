fc8b2180a2791fdcb6c337d1693a3fa55e2a66af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:05 +0000

    Add package dask-geomodeling/2.0.4/dask-geomodeling-2.0.4.tar.gz

---
+++ packages/dask-geomodeling/dask-geomodeling-2.0.4.tar.gz/dask-geomodeling-2.0.4/dask_geomodeling/config.py
@@ -0,0 +1,10 @@
+import dask.config
+import os
+
+defaults = {
+    "root": os.getcwd(),
+    "raster-limit": 12 * (1024 ** 2),  # ca. 100 MB of float64
+    "geometry-limit": 10000,
+}
+
+dask.config.update_defaults({"geomodeling": defaults})
