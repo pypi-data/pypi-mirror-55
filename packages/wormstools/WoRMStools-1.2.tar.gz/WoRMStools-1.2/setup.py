dea61e5fdf6adc9d7c4953b3da1f63966d486a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:01 +0000

    Add package wormstools/1.2/WoRMStools-1.2.tar.gz

---
+++ packages/wormstools/WoRMStools-1.2.tar.gz/WoRMStools-1.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import setuptools
+from distutils.core import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(name="WoRMStools",
+      version='1.2',
+      long_description = readme,
+      long_description_content_type='text/markdown',
+      author='Ulises Rosas',
+      author_email='ulisesfrosasp@gmail.com',
+      url='https://github.com/Ulises-Rosas/OBISdat',
+      packages = ['WoRMStools'],
+      package_dir = {'WoRMStools': 'src'},
+      scripts = ['src/worms.py'],
+      setup_requires=['wheel'],
+      classifiers=['Programming Language :: Python :: 3']
+      )
+
