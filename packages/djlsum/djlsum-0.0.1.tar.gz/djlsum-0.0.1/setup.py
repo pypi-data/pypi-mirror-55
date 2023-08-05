cf01bdae28e1252a554741845ac5d59a9a784422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:44 +0000

    Add package djlsum/0.0.1/djlsum-0.0.1.tar.gz

---
+++ packages/djlsum/djlsum-0.0.1.tar.gz/djlsum-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='djlsum',
+    version='0.0.1',
+    author="deng jianlong",
+    author_email="jldeng3@163.com",
+    install_requires=['docutils>=0.3'],
+    python_requires='>=3',
+    packages=find_packages(),
+)
