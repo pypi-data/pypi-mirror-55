90737fc35913735504bf26bf0afeb62f052671ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:21 +0000

    Add package aiowirecard/0.0.7/aiowirecard-0.0.7.tar.gz

---
+++ packages/aiowirecard/aiowirecard-0.0.7.tar.gz/aiowirecard-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aiowirecard",
+    version="0.0.7",
+    author="jersobh",
+    author_email="jersobh@gmail.com",
+    description="Wirecard API asyncio wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jersobh/aiowirecard",
+    install_requires=["aiohttp"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
