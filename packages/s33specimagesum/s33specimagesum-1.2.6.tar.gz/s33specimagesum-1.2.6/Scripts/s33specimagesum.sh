5a6d7916f1c358480e97217c7c5f56ae3a365020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:46 +0000

    Add package s33specimagesum/1.2.6/s33specimagesum-1.2.6.tar.gz

---
+++ packages/s33specimagesum/s33specimagesum-1.2.6.tar.gz/s33specimagesum-1.2.6/Scripts/s33specimagesum.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+if [ -f "$HOME/Anaconda3/bin/python" ]; then
+	ANACONDA_LOCATION="$HOME/Anaconda3"
+elif [ -f "$HOME/anaconda3/python" ]; then
+	ANACONDA_LOCATION="$HOME/anaconda3"
+elif [ -f "/APSshare/anaconda3/x86_64/bin/python" ]; then
+	ANACONDA_LOCATION=/APSshare/anaconda3/x86_64
+else 
+	echo "Cannot find Anaconda Python"
+	exit 1
+fi
+$ANACONDA_LOCATION\Scripts\activate.bat
+
+conda activate s33specimagesum
+
+python -m 33specimagesum
