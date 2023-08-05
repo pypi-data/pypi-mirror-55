0ec1c5435635bbd6f8748abbea8ab475e27df630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:47 +0000

    Add package DeepCell-Tracking/0.2.0/Deepcell_Tracking-0.2.0.tar.gz

---
+++ packages/DeepCell-Tracking/Deepcell_Tracking-0.2.0.tar.gz/Deepcell_Tracking-0.2.0/deepcell_tracking/utils_test.py
@@ -0,0 +1,73 @@
+# Copyright 2016-2019 The Van Valen Lab at the California Institute of
+# Technology (Caltech), with support from the Paul Allen Family Foundation,
+# Google, & National Institutes of Health (NIH) under Grant U24CA224309-01.
+# All rights reserved.
+#
+# Licensed under a modified Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.github.com/vanvalenlab/deepcell-tracking/LICENSE
+#
+# The Work provided may be used for non-commercial academic purposes only.
+# For any other use of the Work, including commercial use, please contact:
+# vanvalenlab@gmail.com
+#
+# Neither the name of Caltech nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for tracking_utils"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+from deepcell_tracking import utils
+
+
+def _get_image(img_h=300, img_w=300):
+    bias = np.random.rand(img_w, img_h) * 64
+    variance = np.random.rand(img_w, img_h) * (255 - 64)
+    img = np.random.rand(img_w, img_h) * variance + bias
+    return img
+
+
+class TestTrackingUtils(object):
+
+    def test_sorted_nicely(self):
+        # test image file sorting
+        expected = ['test_001_dapi', 'test_002_dapi', 'test_003_dapi']
+        unsorted = ['test_003_dapi', 'test_001_dapi', 'test_002_dapi']
+        np.testing.assert_array_equal(expected, utils.sorted_nicely(unsorted))
+        # test montage folder sorting
+        expected = ['test_0_0', 'test_1_0', 'test_1_1']
+        unsorted = ['test_1_1', 'test_0_0', 'test_1_0']
+        np.testing.assert_array_equal(expected, utils.sorted_nicely(unsorted))
+
+    def test_count_pairs(self):
+        batches = 1
+        frames = 2
+        classes = 4
+        prob = 0.5
+        expected = batches * frames * classes * (classes + 1) / prob
+
+        # channels_last
+        y = np.random.randint(low=0, high=classes + 1,
+                              size=(batches, frames, 30, 30, 1))
+        pairs = utils.count_pairs(y, same_probability=prob)
+        assert pairs == expected
+
+        # channels_first
+        y = np.random.randint(low=0, high=classes + 1,
+                              size=(batches, 1, frames, 30, 30))
+        pairs = utils.count_pairs(
+            y, same_probability=prob, data_format='channels_first')
+        assert pairs == expected
