627396e0631dacbc9fc02261e68228794f8eba78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:00 +0000

    Add package Lysort/0.1.0/Lysort-0.1.0.tar.gz

---
+++ packages/Lysort/Lysort-0.1.0.tar.gz/Lysort-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Lysort',
+      version='0.1.0',
+      description='There have Six sort functions to sort data',
+      url = 'https://github.com/bookisLY/Lysort',
+      author='bookist123',
+      author_email='421736951@qq.com',
+      license='MIT',
+      packages=['Lysort'])
+
