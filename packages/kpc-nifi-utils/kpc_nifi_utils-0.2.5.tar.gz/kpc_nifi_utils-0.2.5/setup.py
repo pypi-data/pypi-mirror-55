798fa2d5375bf20b6e00b98c3f9086a05aff4115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:58 +0000

    Add package kpc-nifi-utils/0.2.5/kpc_nifi_utils-0.2.5.tar.gz

---
+++ packages/kpc-nifi-utils/kpc_nifi_utils-0.2.5.tar.gz/kpc_nifi_utils-0.2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kpc_nifi_utils',
+    packages=find_packages(),
+    version='0.2.5',
+    description='Connector utils and configs for implementing with NIFI',
+    author='Praiwan N.',
+    author_email='npraiwan@outlook.com',
+    license='Apache2',
+    url='https://github.com/kingpowerclick/kpc-nifi-utils',
+    install_requires=[
+        'pymongo',
+        'argparse',
+        'pytz',
+        'snowflake-connector-python==2.0.2',
+        'python-tds',
+        'pg8000'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+    ]
+)
