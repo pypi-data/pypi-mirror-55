e8b1d900899798f0e361361163d52f2e29d46a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:09 +0000

    Add package piicatcher/0.5.0/piicatcher-0.5.0-py2.py3-none-any.whl

---
+++ packages/piicatcher/piicatcher-0.5.0-py2.py3-none-any.whl/piicatcher/command_line.py
@@ -0,0 +1,34 @@
+import argparse
+import yaml
+import logging
+
+from piicatcher.db.explorer import parser as db_parser
+from piicatcher.files.explorer import parser as files_parser
+from piicatcher.config import set_global_config
+from piicatcher.orm.models import init
+
+
+def get_parser(parser_cls=argparse.ArgumentParser):
+    parser = parser_cls()
+    parser.add_argument("-c", "--config-file", help="Path to config file")
+    parser.add_argument("-l", "--log-level", help="Logging Level", default="WARNING")
+
+    sub_parsers = parser.add_subparsers()
+    db_parser(sub_parsers)
+    files_parser(sub_parsers)
+
+    return parser
+
+
+def dispatch(ns):
+    logging.basicConfig(level=getattr(logging, ns.log_level.upper()))
+    if ns.config_file is not None:
+        with open(ns.config_file, 'r') as stream:
+            set_global_config(yaml.load(stream))
+            init()
+
+    ns.func(ns)
+
+
+def main():
+    dispatch(get_parser().parse_args())
