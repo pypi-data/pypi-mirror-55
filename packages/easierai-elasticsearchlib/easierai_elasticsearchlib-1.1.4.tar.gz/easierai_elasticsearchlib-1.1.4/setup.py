5ac2a13e8663e119fde0c2acace616763c69d521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:12 +0000

    Add package easierai-elasticsearchlib/1.1.4/easierai_elasticsearchlib-1.1.4.tar.gz

---
+++ packages/easierai-elasticsearchlib/easierai_elasticsearchlib-1.1.4.tar.gz/easierai_elasticsearchlib-1.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+ 
+setup(name='easierai_elasticsearchlib',
+      version='1.1.4',
+      url='https://gitlab.atosresearch.eu/ari/ioe-ai/data-elasticsearch_python',
+      license='ATOS',
+      author='IoE Lab',
+      author_email='juan.carrascoa.external@atos.net',
+      description='This library manages the communication between python programs and elasticsearch database',
+      long_description=open('README.md').read(),
+      packages=find_packages(),
+      zip_safe=False)
