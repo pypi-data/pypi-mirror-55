a0cf122aa1633f887e21396ff62a2528c9329479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:27 +0000

    Add package dl-ext/1.0.1/dl_ext-1.0.1-py3-none-any.whl

---
+++ packages/dl-ext/dl_ext-1.0.1-py3-none-any.whl/dl_ext/vision_ext/models/__init__.py
@@ -0,0 +1,11 @@
+# from .psmnet import psmnet
+# from .pointnet import *
+#
+# try:
+#     from .maskrcnn import *
+# except :
+#     from warnings import warn
+#
+#     warn(
+#         'maskrcnn is not available! Please install maskrcnn_benchmark according to '
+#         'https://github.com/facebookresearch/maskrcnn-benchmark')
