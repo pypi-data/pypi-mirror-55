bb3d4f15e512ec405ad46b64989ac11a62c650d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package ad9xdds/0.3.2/ad9xdds-0.3.2.tar.gz

---
+++ packages/ad9xdds/ad9xdds-0.3.2.tar.gz/ad9xdds-0.3.2/setup.py
@@ -0,0 +1,24 @@
+# Set __version__ in the setup.py
+with open('ad9xdds/version.py') as f: exec(f.read())
+
+from setuptools import setup
+
+setup(name='ad9xdds',
+      description='Ad9xDds is library dedicated to handle AD9854 and AD9912 development board.',
+      version=__version__,
+      packages=['ad9xdds'],
+      install_requires=['iopy>=0.2.0',
+                        'pyparallel',
+                        'PyQt5'],
+      url='https://gitlab.com/bendub/ad9xdds',
+      author='Benoit Dubois',
+      author_email='benoit.dubois@femto-st.fr',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering']
+)
