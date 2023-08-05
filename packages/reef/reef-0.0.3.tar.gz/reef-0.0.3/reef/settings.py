ff2f25a6475d9a797b08086dc030c742fca1d533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:09 +0000

    Add package reef/0.0.3/reef-0.0.3.tar.gz

---
+++ packages/reef/reef-0.0.3.tar.gz/reef-0.0.3/reef/settings.py
@@ -0,0 +1,19 @@
+# This is a settings file
+
+import os
+
+
+API_ENDPOINT = "https://api.hubstaff.com/v1"
+AUTH_ENDPOINT = API_ENDPOINT + "/auth"
+ORGS_ENDPOINT = API_ENDPOINT + "/organizations"
+USERS_ENDPOINT = API_ENDPOINT + "/users"
+PROJECTS_ENDPOINT = API_ENDPOINT + "/users/{id}/projects"
+ORG_MEMBERS_ENDPOINT = API_ENDPOINT + "/organizations/{id}/members"
+CUSTOM_BY_MEMBER_TEAM_ENDPOINT = API_ENDPOINT + "/custom/by_member/team"
+FOLDER_NAME = "html_reports"
+
+APP_TOKEN = os.environ.get(
+    "HS_APP_TOKEN", "TAgq83AeDd6PKGu9hoB86JhTkaAkLqU3_tabEQmi3ws"
+)
+EMAIL = os.environ.get("HS_EMAIL", "thatiparthysreenivas@gmail.com")
+PASSWORD = os.environ.get("HS_PASSWORD", "Hubstaff@1434")
---
+++ packages/reef/reef-0.0.3.tar.gz/reef-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as fd:
+    install_requires = fd.read().splitlines()
+
+setup(
+    name="reef",
+    version="0.0.3",
+    url="https://srinivasreddy.dev",
+    license="BSD",
+    maintainer="Srinivas Reddy Thatiparthy",
+    maintainer_email="thatiparthysreenivas@gmail.com",
+    description=" A miniproject to test python and remote development skills.",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+)
