e4ae0800f563279ce7b626884fb92fa35ed99865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:44 +0000

    Add package epsagon/1.27.0/epsagon-1.27.0-py3-none-any.whl

---
+++ packages/epsagon/epsagon-1.27.0-py3-none-any.whl/epsagon/modules/botocore.py
@@ -0,0 +1,42 @@
+"""
+botocore patcher module.
+"""
+
+from __future__ import absolute_import
+import wrapt
+from epsagon.modules.general_wrapper import wrapper
+from ..events.botocore import BotocoreEventFactory
+from .requests import _wrapper as _requests_wrapper
+
+
+def _wrapper(wrapped, instance, args, kwargs):
+    """
+    General wrapper for botocore instrumentation.
+    :param wrapped: wrapt's wrapped
+    :param instance: wrapt's instance
+    :param args: wrapt's args
+    :param kwargs: wrapt's kwargs
+    :return: None
+    """
+    return wrapper(BotocoreEventFactory, wrapped, instance, args, kwargs)
+
+
+def patch():
+    """
+    Patch module.
+    :return: None
+    """
+
+    wrapt.wrap_function_wrapper(
+        'botocore.client',
+        'BaseClient._make_api_call',
+        _wrapper
+    )
+
+    # botocore no longer vendor requests in new version
+    # https://github.com/boto/botocore/pull/1829
+    wrapt.wrap_function_wrapper(
+        'botocore.vendored.requests',
+        'Session.send',
+        _requests_wrapper
+    )
