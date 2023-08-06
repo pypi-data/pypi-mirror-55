66745e9706f33b79836fc968e9a14e8df8084e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:53 +0000

    Add package getnet-py/0.2.1/getnet_py-0.2.1-py2.py3-none-any.whl

---
+++ packages/getnet-py/getnet_py-0.2.1-py2.py3-none-any.whl/getnet/services/base.py
@@ -0,0 +1,37 @@
+import re
+
+
+class ServiceBase:
+    _api: 'API' = None
+    path: str = None
+
+    def __init__(self, client) -> None:
+        if not self.path:
+            raise NotImplementedError("The classes parameter path must be defined")
+
+        self._api = client
+
+    def _format_url(self, path=None, **kwargs) -> str:
+        data = {}
+
+        path = self.path + path if path is not None else self.path
+
+        matchs = re.search(r"{(\w+)}", path)
+        if matchs:
+            data = {}.fromkeys(list(matchs.groups()), "")
+
+        data.update(**kwargs)
+
+        return path.format(**data).rstrip("/")
+
+    def _get(self, *args, **kwargs):
+        return self._api.get(*args, **kwargs)
+
+    def _post(self, *args, **kwargs):
+        return self._api.post(*args, **kwargs)
+
+    def _put(self, *args, **kwargs):
+        return self._api.put(*args, **kwargs)
+
+    def _delete(self, *args, **kwargs):
+        return self._api.delete(*args, **kwargs)
