62f42a420351886e18e51b364acbfce476c49dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:34 +0000

    Add package ZoomFoundry/6.0.4/ZoomFoundry-6.0.4-py3-none-any.whl

---
+++ packages/ZoomFoundry/ZoomFoundry-6.0.4-py3-none-any.whl/zoom/cli/new.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+    zoom new cli
+"""
+
+# pylint: disable=unused-argument
+import os
+import shutil
+import sys
+from argparse import ArgumentParser
+
+import zoom
+
+
+def make_app(pathname, template):
+    """Make an app from an app template"""
+    template_dir = zoom.tools.zoompath('zoom', 'templates', 'apps', template)
+    if os.path.isdir(template_dir):
+        shutil.copytree(template_dir, pathname)
+    else:
+        print('no template called {!r} {!r}'.format(template, template_dir))
+
+
+def new(name=None):
+    """Create a new app"""
+
+    parser = ArgumentParser(
+        description='create a new app',
+        usage='zoom new [options] name'
+    )
+
+    parser.add_argument("-v", "--verbose", action='store_true',
+                        help='verbose console logging')
+    parser.add_argument('name', nargs='?', default=None)
+    args = parser.parse_args()
+
+    pathname = args.name
+    if pathname:
+        try:
+            make_app(pathname, 'basic')
+        except FileExistsError as e:
+            print('App exists:', pathname)
+            sys.exit(1)
+    else:
+        print('missing app name (-h for help)')
