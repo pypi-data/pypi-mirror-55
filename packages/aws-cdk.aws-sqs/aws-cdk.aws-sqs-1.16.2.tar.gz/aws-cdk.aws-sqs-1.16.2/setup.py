a90ffbe31267df0fb435debf9d6cf01d4c0109d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:04 +0000

    Add package aws-cdk.aws-sqs/1.16.2/aws-cdk.aws-sqs-1.16.2.tar.gz

---
+++ packages/aws-cdk.aws-sqs/aws-cdk.aws-sqs-1.16.2.tar.gz/aws-cdk.aws-sqs-1.16.2/setup.py
@@ -0,0 +1,46 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-sqs",
+    "version": "1.16.2",
+    "description": "CDK Constructs for AWS SQS",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_sqs",
+        "aws_cdk.aws_sqs._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_sqs._jsii": [
+            "aws-sqs@1.16.2.jsii.tgz"
+        ],
+        "aws_cdk.aws_sqs": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.4",
+        "publication>=0.0.3",
+        "aws-cdk.aws-cloudwatch~=1.16,>=1.16.2",
+        "aws-cdk.aws-iam~=1.16,>=1.16.2",
+        "aws-cdk.aws-kms~=1.16,>=1.16.2",
+        "aws-cdk.core~=1.16,>=1.16.2"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
