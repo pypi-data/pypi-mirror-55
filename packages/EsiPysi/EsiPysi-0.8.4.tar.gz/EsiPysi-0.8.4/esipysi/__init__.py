583f84c2da89c951d10ca6b7c902f5b24d906717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:52 +0000

    Add package EsiPysi/0.8.4/EsiPysi-0.8.4.tar.gz

---
+++ packages/EsiPysi/EsiPysi-0.8.4.tar.gz/EsiPysi-0.8.4/esipysi/__init__.py
@@ -0,0 +1,12 @@
+try:
+    from .esipysi import EsiPysi
+    from .op import EsiOp
+    from .auth import EsiAuth
+    import logging
+
+except ImportError:
+    pass
+
+__version__ = '0.8.4'
+__name__ = 'EsiPysi'
+
