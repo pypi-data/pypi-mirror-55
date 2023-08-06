7ba4b674119f87cce72b010d0f3c53f4e25bc7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:08 +0000

    Add package mlboard-client/0.0.5rc3/mlboard_client-0.0.5rc3.tar.gz

---
+++ packages/mlboard-client/mlboard_client-0.0.5rc3.tar.gz/mlboard_client-0.0.5rc3/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+requires = [
+    "cytoolz",
+    "requests",
+]
+
+dev_requires = [
+    "pytest",
+    "pytest-cov",
+    "flake8",
+    "autopep8",
+    "mypy",
+    "pytest-mypy",
+    "typing_extensions",
+    "twine",
+]
+
+setup(
+    name="mlboard_client",
+    version="0.0.5.rc3",
+    description="mlboard client",
+    author='Xinyuan Yao',
+    author_email='yao.ntno@google.com',
+    license="MIT",
+    packages=['mlboard_client'],
+    install_requires=requires,
+    extras_require={
+        'dev': dev_requires
+    },
+    python_requires='>=3.6',
+)
