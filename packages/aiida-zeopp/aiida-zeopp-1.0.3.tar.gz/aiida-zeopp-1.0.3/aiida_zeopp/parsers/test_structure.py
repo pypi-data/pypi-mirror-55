edbeb8774e6f90343ded3c00853c72fc3fc9d366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:40 +0000

    Add package aiida-zeopp/1.0.3/aiida-zeopp-1.0.3.tar.gz

---
+++ packages/aiida-zeopp/aiida-zeopp-1.0.3.tar.gz/aiida-zeopp-1.0.3/aiida_zeopp/parsers/test_structure.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+import os
+import aiida_zeopp.parsers.structure as parsers
+import aiida_zeopp.tests as zt
+"""Test CSSR parser"""
+
+
+class CssrParserTestCase(zt.PluginTestCase):
+
+    BACKEND = zt.get_backend()
+
+    def test_parse_hkust(self):
+
+        with open(os.path.join(zt.TEST_DIR, 'MgO.cssr'), 'r') as f:
+            string = f.read()
+
+        parser = parsers.CssrParser
+        parser.parse(string)
