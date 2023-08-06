423a5fc86f85c5e7842bbe7e5a664a174e4bcea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:21 +0000

    Add package lutils/2.10.2.2/lutils-2.10.2.2.tar.gz

---
+++ packages/lutils/lutils-2.10.2.2.tar.gz/lutils-2.10.2.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='lutils',  
+     version='2.10.2.2',
+     #this will add to \Scripts folder
+     #scripts=['lutils.py'] ,
+     author="Tran Duc Loi",
+     author_email="loitranduc@gmail.com",
+     description="A Public Loitd utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/loitd/lutils",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
