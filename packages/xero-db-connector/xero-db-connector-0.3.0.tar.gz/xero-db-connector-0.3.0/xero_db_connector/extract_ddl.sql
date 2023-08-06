8e3d6f88d08fd3f9f456bff9b5f11c3faca4f193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:26 +0000

    Add package xero-db-connector/0.3.0/xero-db-connector-0.3.0.tar.gz

---
+++ packages/xero-db-connector/xero-db-connector-0.3.0.tar.gz/xero-db-connector-0.3.0/xero_db_connector/extract_ddl.sql
@@ -0,0 +1,71 @@
+DROP TABLE IF EXISTS "xero_extract_tracking_categories";
+DROP TABLE IF EXISTS "xero_extract_tracking_options";
+DROP TABLE IF EXISTS "xero_extract_contacts";
+DROP TABLE IF EXISTS "xero_extract_invoices";
+DROP TABLE IF EXISTS "xero_extract_invoice_lineitems";
+DROP TABLE IF EXISTS "xero_extract_lineitem_tracking";
+DROP TABLE IF EXISTS "xero_extract_accounts";
+
+CREATE TABLE IF NOT EXISTS "xero_extract_trackingcategories" (
+  "Name" TEXT,
+  "Status" TEXT,
+  "TrackingCategoryID" TEXT
+);
+
+CREATE TABLE IF NOT EXISTS "xero_extract_trackingoptions" (
+  "TrackingOptionID" TEXT,
+  "Name" TEXT,
+  "Status" TEXT,
+  "TrackingCategoryID" TEXT
+);
+
+CREATE TABLE IF NOT EXISTS "xero_extract_contacts" (
+"ContactID" TEXT,
+  "Name" TEXT,
+  "ContactStatus" TEXT,
+  "IsSupplier" INTEGER,
+  "IsCustomer" INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS "xero_extract_invoices" (
+"Type" TEXT,
+  "InvoiceID" TEXT,
+  "InvoiceNumber" TEXT,
+  "Reference" TEXT,
+  "CurrencyRate" REAL,
+  "DateString" TEXT,
+  "Date" TEXT,
+  "Status" TEXT,
+  "LineAmountTypes" TEXT,
+  "Total" REAL,
+  "UpdatedDateUTC" TEXT,
+  "CurrencyCode" TEXT,
+  "ContactID" TEXT
+);
+
+CREATE TABLE IF NOT EXISTS "xero_extract_invoice_lineitems" (
+  "LineItemID" TEXT,
+  "InvoiceID" TEXT,
+  "Description" TEXT,
+  "UnitAmount" REAL,
+  "LineAmount" REAL,
+  "AccountCode" TEXT,
+  "Quantity" REAL
+);
+
+CREATE TABLE IF NOT EXISTS "xero_extract_lineitem_tracking" (
+"Name" TEXT,
+  "Option" TEXT,
+  "TrackingCategoryID" TEXT,
+  "TrackingOptionID" TEXT,
+  "LineItemID" TEXT
+);
+
+CREATE TABLE IF NOT EXISTS "xero_extract_accounts" (
+"AccountID" TEXT,
+  "Code" TEXT,
+  "Name" TEXT,
+  "Status" TEXT,
+  "Type" TEXT,
+  "CurrencyCode" TEXT
+);
