2f9e4e4bcc5b96e21b02d99c87b10592b73dbe8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:49 +0000

    Add package opencroplib/0.1.5/opencroplib-0.1.5-py2.py3-none-any.whl

---
+++ packages/opencroplib/opencroplib-0.1.5-py2.py3-none-any.whl/opencroplib/__init__.py
@@ -0,0 +1,9 @@
+
+__version__ = "0.1.5"
+from . import time_utils
+from . import atmophere
+from . import radiation
+from . import aerodynamic
+from . import plant
+from . import evapotranspiration
+from . import test_opencroplib
