90188a8d9873fc94bad9529aa6e87ec39aa92330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:05 +0000

    Add package probability-distributions-ms/0.1/probability_distributions_ms-0.1.tar.gz

---
+++ packages/probability-distributions-ms/probability_distributions_ms-0.1.tar.gz/probability_distributions_ms-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_distributions_ms',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probability_distributions_ms'],
+      zip_safe=False)
