ad2e80d68cc2971cb12ceab5418a468e8065e888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:11 +0000

    Add package pyobjc-framework-Quartz/6.1/pyobjc-framework-Quartz-6.1.tar.gz

---
+++ packages/pyobjc-framework-Quartz/pyobjc-framework-Quartz-6.1.tar.gz/pyobjc-framework-Quartz-6.1/Modules/_QuickLookUI.m
@@ -0,0 +1,25 @@
+/*
+ * Manual wrappers for QuickLookUI
+ */
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+#include "pyobjc-api.h"
+
+#import <Quartz/Quartz.h>
+
+#include "_QuickLookUI_protocols.m"
+
+static PyMethodDef mod_methods[] = {{
+    0,
+    0,
+    0,
+}};
+
+PyObjC_MODULE_INIT(_QuickLookUI)
+{
+    PyObject* m = PyObjC_MODULE_CREATE(_QuickLookUI);
+    if (!m)
+        PyObjC_INITERROR();
+
+    PyObjC_INITDONE();
+}
