5afea1dcfc291f32d17afc334c7481d47acf70fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:58 +0000

    Add package ywh2bt/0.2a2/ywh2bt-0.2a2-py3-none-any.whl

---
+++ packages/ywh2bt/ywh2bt-0.2a2-py3-none-any.whl/ywh2bt/utils/__init__.py
@@ -0,0 +1,44 @@
+import getpass
+
+__all__ = ["read_input", "get_all_subclasses"]
+
+
+"""
+Utils Module, define simply fonction.
+"""
+
+
+def read_input(text, secret=False):
+    """
+    Wrapper for input and getpass with color print text.
+    :param text: text to  show before read input.
+    :param bool secret: User input is invisible or not.
+    """
+
+    print(text, flush=True, end="")
+    if secret:
+        return getpass.getpass(prompt="")
+    return input()
+
+
+def get_all_subclasses(cls, ret=None, cls_attr_filter={}):
+    """
+    get subclasses from an ancestor recurively.
+    You can use on attribute value filter.
+
+    :param class cls: input class
+    :param list ret: return list
+    :param dict cls_attr_filter: filter for subclasse attribute on value.
+    """
+    for sub in cls.__subclasses__():
+        keep = True
+        for key, item in cls_attr_filter.items():
+            if sub.__dict__.get(key, None) != item:
+                keep = False
+                break
+        if keep:
+            if ret is None or not isinstance(ret, list):
+                ret = []
+            ret.append(sub)
+        get_all_subclasses(sub, ret=ret, cls_attr_filter=cls_attr_filter)
+    return ret if ret is not None else []
