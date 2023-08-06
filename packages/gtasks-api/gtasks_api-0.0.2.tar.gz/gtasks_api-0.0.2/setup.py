a02466dd61316590c0f03d12f2f377a5addeaa45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:19 +0000

    Add package gtasks-api/0.0.2/gtasks_api-0.0.2.tar.gz

---
+++ packages/gtasks-api/gtasks_api-0.0.2.tar.gz/gtasks_api-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name = 'gtasks_api',
+    version = "0.0.2",
+    description = 'Based on quickstart sample by Google',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'BlueBlueBlob',
+    author_email = 'adrien.lesot@gmail.com',
+    url = 'https://github.com/BlueBlueBlob/gtasks_api',
+    license = 'MIT',
+    install_requires = [
+        "google-api-python-client",
+        "google-auth-httplib2",
+        "google-auth-oauthlib",
+        "iso8601"
+    ],
+    packages= setuptools.find_packages(),
+    keywords = ['google', 'tasks', 'task', 'gtasks', 'gtask']
+)
