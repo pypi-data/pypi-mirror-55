93700e762dfdf21b19d646a92ba9c62b584654f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:07 +0000

    Add package opencontainers/0.0.1/opencontainers-0.0.1-py3.7.egg

---
+++ packages/opencontainers/opencontainers-0.0.1-py3.7.egg/opencontainers/digest/set-extra.py
@@ -0,0 +1,13 @@
+        # It might also be a list with an object inside
+        #elif isinstance(self.attType, list):
+        #    if isinstance(value, list) and value:
+        #        innerType = self.attType[0]
+
+                # If we also have an inner structure, update it
+        #        if self._is_struct(innerType):
+        #            newStruct = innerType()
+
+        #           # TODO what if there are more than one? How to load objects here?
+        #            # Load the provided value
+        #            newStruct.load(value[0])
+
