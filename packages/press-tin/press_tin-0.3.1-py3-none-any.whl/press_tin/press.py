970f587432bbeada94f3338128b65600bfaf5645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:49 +0000

    Add package press-tin/0.3.1/press_tin-0.3.1-py3-none-any.whl

---
+++ packages/press-tin/press_tin-0.3.1-py3-none-any.whl/press_tin/press.py
@@ -0,0 +1,12 @@
+class kor:
+    
+    
+    def __init__(self):
+        pass
+        
+        
+    def output(self):
+        press_ls = ['조선일보','중앙일보','동아일보',
+                    '한국일보','경향신문','한겨레']
+        
+        print(f'Korea Press: {press_ls}\nTo be continue...')
---
+++ packages/press-tin/press_tin-0.3.1-py3-none-any.whl/press_tin/press_kr.py
@@ -0,0 +1,10 @@
+class press_kr:
+    
+    def __init__(self):
+        self.press_ls = []
+        
+        
+    def press_output(self):
+        press_kr = ['조선일보','중앙일보','동아일보',
+                    '한국일보','경향신문','한겨레']
+        print(f'Korea Press: {press_kr}\nTo be continue...')
