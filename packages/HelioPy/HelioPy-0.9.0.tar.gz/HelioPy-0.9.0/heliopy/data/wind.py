532ffea889c3c88dc8b81c24060e5f9789ac565b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:28 +0000

    Add package HelioPy/0.9.0/HelioPy-0.9.0.tar.gz

---
+++ packages/HelioPy/HelioPy-0.9.0.tar.gz/HelioPy-0.9.0/heliopy/data/wind.py
@@ -0,0 +1,97 @@
+"""
+Methods for importing data from the WIND spacecraft.
+All data is publically available at ftp://spdf.gsfc.nasa.gov/pub/data/wind.
+See https://wind.nasa.gov/data_sources.php for more information on different
+data products.
+"""
+import astropy.units as u
+from heliopy.data import cdasrest
+
+
+def _docstring(identifier, description):
+    return cdasrest._docstring(identifier, 'W', description)
+
+
+def _wind(starttime, endtime, identifier, badvalues=None, units=None,
+          intervals='monthly'):
+    """
+    Generic method for downloading ACE data.
+    """
+    dl = cdasrest.CDASDwonloader('wi', identifier, 'wind', badvalues=badvalues,
+                                 units=units)
+    # Override intervals
+    if intervals == 'daily':
+        dl.intervals = dl.intervals_daily
+    else:
+        dl.intervals = dl.intervals_monthly
+    return dl.load(starttime, endtime)
+
+
+# Actual download functions start here
+def swe_h1(starttime, endtime):
+    identifier = 'WI_H1_SWE'
+    badvalues = 99999.9
+    units = {'ChisQ_DOF_nonlin': u.dimensionless_unscaled}
+    return _wind(starttime, endtime, identifier,
+                 badvalues=badvalues, units=units)
+
+
+swe_h1.__doc__ = _docstring(
+    'WI_H1_SWE', '92-second Solar Wind Alpha and Proton Anisotropy Analysis')
+
+
+def mfi_h0(starttime, endtime):
+    identifier = 'WI_H0_MFI'
+    units = {'BGSEa_0': u.nT, 'BGSEa_1': u.nT, 'BGSEa_2': u.nT}
+    return _wind(starttime, endtime, identifier, units=units,
+                 intervals='daily')
+
+
+mfi_h0.__doc__ = _docstring(
+    'WI_H0_MFI', 'Composite magnetic field')
+
+
+def mfi_h2(starttime, endtime):
+    identifier = 'WI_H2_MFI'
+    return _wind(starttime, endtime, identifier, intervals='daily')
+
+
+mfi_h2.__doc__ = _docstring(
+    'WI_H2_MFI', 'High resolution magnetic field')
+
+
+def threedp_pm(starttime, endtime):
+    identifier = 'WI_PM_3DP'
+    return _wind(starttime, endtime, identifier, intervals='daily')
+
+
+threedp_pm.__doc__ = _docstring(
+    'WI_PM_3DP', '1 spin resolution ion (proton and alpha) moment')
+
+
+def threedp_e0_emfits(starttime, endtime):
+    identifier = 'WI_EMFITS_E0_3DP'
+    return _wind(starttime, endtime, identifier)
+
+
+threedp_e0_emfits.__doc__ = _docstring(
+    'WI_EMFITS_E0_3DP', 'Electron moment')
+
+
+def swe_h3(starttime, endtime):
+    badvalues = -9.99999985e+30
+    identifier = 'WI_H3_SWE'
+    return _wind(starttime, endtime, identifier, badvalues=badvalues)
+
+
+swe_h3.__doc__ = _docstring(
+    'WI_H3_SWE', '12 second electron pitch angle')
+
+
+def threedp_elpd(starttime, endtime):
+    identifier = 'WI_ELPD_3DP'
+    return _wind(starttime, endtime, identifier)
+
+
+threedp_elpd.__doc__ = _docstring(
+    'WI_ELPD_3DP', 'Electron pitch angle')
---
+++ packages/HelioPy/HelioPy-0.9.0.tar.gz/HelioPy-0.9.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+import os
+import versioneer
+import sys
+
+if sys.version_info < (3, 6):
+    sys.exit('Python versions older than 3.6 are not supported.')
+
+# Config for read the docs
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    from distutils import dir_util
+    ret = dir_util.copy_tree('examples/data',
+                             '/home/docs/heliopy/data')
+    print(ret)
+
+setup(name='HelioPy',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Python for space physics',
+      url='https://heliopy.org/',
+      author='David Stansby',
+      author_email='dstansby@gmail.com',
+      license='GPL-3.0',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Science/Research',
+                   'Natural Language :: English',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Scientific/Engineering :: Physics'],
+      keywords='physics, space-physics',
+      include_package_data=True,
+      install_requires=['numpy',
+                        'astropy'],
+      extras_require={'spice': ['spiceypy'],
+                      'data': ['pandas', 'sunpy', 'requests', 'cdflib',
+                               'wget'],
+                      'coordinates': ['sunpy']},
+      python_requires='>=3.6',
+      packages=['heliopy',
+                'heliopy.data',
+                'heliopy.models',
+                'heliopy.spice',
+                'heliopy.util'],
+      package_data={'heliopy': ['heliopyrc']})
