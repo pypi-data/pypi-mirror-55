4dc6d6e98908ef4cdd785ad32aaf21b2ef5e7b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:11 +0000

    Add package aws-cdk.aws-elasticloadbalancingv2-targets/1.16.0/aws-cdk.aws-elasticloadbalancingv2-targets-1.16.0.tar.gz

---
+++ packages/aws-cdk.aws-elasticloadbalancingv2-targets/aws-cdk.aws-elasticloadbalancingv2-targets-1.16.0.tar.gz/aws-cdk.aws-elasticloadbalancingv2-targets-1.16.0/setup.py
@@ -0,0 +1,47 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-elasticloadbalancingv2-targets",
+    "version": "1.16.0",
+    "description": "Integration targets for AWS ElasticLoadBalancingV2",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_elasticloadbalancingv2_targets",
+        "aws_cdk.aws_elasticloadbalancingv2_targets._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_elasticloadbalancingv2_targets._jsii": [
+            "aws-elasticloadbalancingv2-targets@1.16.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_elasticloadbalancingv2_targets": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.2",
+        "publication>=0.0.3",
+        "aws-cdk.assets~=1.16,>=1.16.0",
+        "aws-cdk.aws-elasticloadbalancingv2~=1.16,>=1.16.0",
+        "aws-cdk.aws-iam~=1.16,>=1.16.0",
+        "aws-cdk.aws-lambda~=1.16,>=1.16.0",
+        "aws-cdk.core~=1.16,>=1.16.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
