cb6f700fd5c6ca2aee4d711e7ccd2ce5a89bdcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:30 +0000

    Add package peerplays/0.3.12/peerplays-0.3.12.tar.gz

---
+++ packages/peerplays/peerplays-0.3.12.tar.gz/peerplays-0.3.12/peerplaysbase/objecttypes.py
@@ -0,0 +1,31 @@
+ts = [
+    "null",
+    "base",
+    "account",
+    "asset",
+    "force_settlement",
+    "committee_member",
+    "witness",
+    "limit_order",
+    "call_order",
+    "custom",
+    "proposal",
+    "operation_history",
+    "withdraw_permission",
+    "vesting_balance",
+    "worker",
+    "balance",
+    "tournament",
+    "tournament_details",
+    "match",
+    "game",
+    "sport",
+    "event_group",
+    "event",
+    "betting_market_rules",
+    "betting_market_group",
+    "betting_market",
+    "bet"
+]
+
+object_type = {k: ts.index(k) for k in ts}
