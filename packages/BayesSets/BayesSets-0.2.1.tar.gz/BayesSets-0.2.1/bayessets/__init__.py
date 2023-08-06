4c183f2969931afdb79c2fc65a3e3c3a3a026b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:37 +0000

    Add package BayesSets/0.2.1/BayesSets-0.2.1.tar.gz

---
+++ packages/BayesSets/BayesSets-0.2.1.tar.gz/BayesSets-0.2.1/bayessets/__init__.py
@@ -0,0 +1,1 @@
+from .bayessets import BernoulliBayesianSet
---
+++ packages/BayesSets/BayesSets-0.2.1.tar.gz/BayesSets-0.2.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+DESCRIPTION = "Implementation of the Bayesian Sets algorithm and variants"
+
+setup(
+    name="BayesSets",
+    version="0.2.1",
+    packages=["bayessets"],
+    install_requires=[
+        'numpy>=1.13.3',
+        'scipy>=0.19.1'
+    ],
+
+    author="Diorge Brognara",
+    author_email="diorge.bs@gmail.com",
+    description=DESCRIPTION,
+    license="MIT",
+    keywords="bayes set expansion machine learning information retrieval",
+    url="https://github.com/MaLL-UFSCar/bayessets"
+)
