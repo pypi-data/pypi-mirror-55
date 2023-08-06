829318ec6d8a1f8d1b4289eb71b402df090b43b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:40 +0000

    Add package bionic/0.6.0/bionic-0.6.0.tar.gz

---
+++ packages/bionic/bionic-0.6.0.tar.gz/bionic-0.6.0/bionic/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from .flow import Flow, FlowBuilder  # noqa: F401
+from .decorators import (  # noqa: F401
+    version, output, outputs, gather, persist, memoize, pyplot, immediate
+)
+
+from . import protocol  # noqa: F401
+
+__version__ = u'0.6.0'
