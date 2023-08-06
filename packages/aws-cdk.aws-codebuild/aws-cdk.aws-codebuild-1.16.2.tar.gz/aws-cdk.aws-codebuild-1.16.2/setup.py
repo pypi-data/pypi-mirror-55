a386fc863dc6f5b62e4646644d3e7c0b3160ea27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:54 +0000

    Add package aws-cdk.aws-codebuild/1.16.2/aws-cdk.aws-codebuild-1.16.2.tar.gz

---
+++ packages/aws-cdk.aws-codebuild/aws-cdk.aws-codebuild-1.16.2.tar.gz/aws-cdk.aws-codebuild-1.16.2/setup.py
@@ -0,0 +1,55 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-codebuild",
+    "version": "1.16.2",
+    "description": "CDK Constructs for AWS CodeBuild",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_codebuild",
+        "aws_cdk.aws_codebuild._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_codebuild._jsii": [
+            "aws-codebuild@1.16.2.jsii.tgz"
+        ],
+        "aws_cdk.aws_codebuild": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.4",
+        "publication>=0.0.3",
+        "aws-cdk.assets~=1.16,>=1.16.2",
+        "aws-cdk.aws-cloudwatch~=1.16,>=1.16.2",
+        "aws-cdk.aws-codecommit~=1.16,>=1.16.2",
+        "aws-cdk.aws-ec2~=1.16,>=1.16.2",
+        "aws-cdk.aws-ecr~=1.16,>=1.16.2",
+        "aws-cdk.aws-ecr-assets~=1.16,>=1.16.2",
+        "aws-cdk.aws-events~=1.16,>=1.16.2",
+        "aws-cdk.aws-iam~=1.16,>=1.16.2",
+        "aws-cdk.aws-kms~=1.16,>=1.16.2",
+        "aws-cdk.aws-s3~=1.16,>=1.16.2",
+        "aws-cdk.aws-s3-assets~=1.16,>=1.16.2",
+        "aws-cdk.aws-secretsmanager~=1.16,>=1.16.2",
+        "aws-cdk.core~=1.16,>=1.16.2"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
