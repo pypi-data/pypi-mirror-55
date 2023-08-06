acd6562ff2b5e3ae2f118c6568a6d499ab913053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:31 +0000

    Add package datasetmaker/0.12.23/datasetmaker-0.12.23.tar.gz

---
+++ packages/datasetmaker/datasetmaker-0.12.23.tar.gz/datasetmaker-0.12.23/datasetmaker/clients/__init__.py
@@ -0,0 +1,31 @@
+"""Datasetmaker clients."""
+
+from .esv import ESVClient
+from .hdi import HDIClient
+from .meps import MEPs
+from .mynewsflash.client import MyNewsFlashClient
+from .nobel import NobelClient
+from .scb.client import SCBClient
+from .sipri import SIPRI
+from .skolverket.client import SkolverketClient
+from .unsc import UNSC
+from .valforsk import ValforskClient
+from .waqi import WAQIClient
+from .wikipedia.client import WikipediaClient
+from .worldbank.client import WorldBank
+
+available = {
+    'esv': ESVClient,
+    'hdi': HDIClient,
+    'meps': MEPs,
+    'mynewsflash': MyNewsFlashClient,
+    'nobel': NobelClient,
+    'scb': SCBClient,
+    'sipri': SIPRI,
+    'skolverket': SkolverketClient,
+    'unsc': UNSC,
+    'valforsk': ValforskClient,
+    'waqi': WAQIClient,
+    'wikipedia': WikipediaClient,
+    'worldbank': WorldBank,
+}
