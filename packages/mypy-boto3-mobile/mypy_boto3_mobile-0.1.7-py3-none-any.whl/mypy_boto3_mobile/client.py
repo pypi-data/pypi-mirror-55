244922c7003dda1e2d361f2ffee6e6a8d3d0bf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:55 +0000

    Add package mypy-boto3-mobile/0.1.7/mypy_boto3_mobile-0.1.7-py3-none-any.whl

---
+++ packages/mypy-boto3-mobile/mypy_boto3_mobile-0.1.7-py3-none-any.whl/mypy_boto3_mobile/client.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+
+from botocore.client import BaseClient
+from botocore.paginate import Paginator
+from botocore.waiter import Waiter
+
+
+class Client(BaseClient):
+
+    # pylint: disable=arguments-differ
+    def can_paginate(self, operation_name: str = None) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def create_project(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete_project(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_bundle(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_project(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def export_bundle(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def export_project(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def generate_presigned_url(
+        self,
+        ClientMethod: str = None,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = None,
+        HttpMethod: str = None,
+    ) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_paginator(self, operation_name: str = None) -> Paginator:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_waiter(self, waiter_name: str = None) -> Waiter:
+        pass
+
+    # pylint: disable=arguments-differ
+    def list_bundles(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def list_projects(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def update_project(self) -> None:
+        pass
