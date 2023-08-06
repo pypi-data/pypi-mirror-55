7e1d912cc76c8d1c51744b4ab689e4b61e406f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:06 +0000

    Add package odoo12-addon-mindev/12.0.0.2.0/odoo12_addon_mindev-12.0.0.2.0-py3-none-any.whl

---
+++ packages/odoo12-addon-mindev/odoo12_addon_mindev-12.0.0.2.0-py3-none-any.whl/odoo/addons/mindev/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2011 Minorisa, S.L. <http://www.minorisa.net>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import group
+from . import menu
+from . import tools_translate
