66625f11f9fd93062978c7fff2bd2b0fb9bdaf1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:41 +0000

    Add package parma/0.1.2/parma-0.1.2-py2.py3-none-any.whl

---
+++ packages/parma/parma-0.1.2-py2.py3-none-any.whl/parma/__init__.py
@@ -0,0 +1,9 @@
+from .core import (polyharmonic_interpolator,
+                   polyharmonic_hermite_interpolator,
+                   multiquadric_hermite_interpolator)
+
+
+__version__ = '0.1.2'
+
+__all__ = ['polyharmonic_interpolator', 'polyharmonic_hermite_interpolator',
+           'multiquadric_hermite_interpolator']
