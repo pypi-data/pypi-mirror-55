3bfecfbe1c49c3c8333baef19988834b46f8908b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:57 +0000

    Add package sighthound-api/0.61/sighthound_api-0.61.tar.gz

---
+++ packages/sighthound-api/sighthound_api-0.61.tar.gz/sighthound_api-0.61/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='sighthound_api',
+     version='0.61',
+     author="Phil Mottin",
+     author_email="philmottin@gmail.com",
+     description="Sighthound API for integration with Home Assistant",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/philmottin",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
