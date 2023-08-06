d2ee5c3c16feebf8e8fdf0c4a06b6a8a2ad19efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:04 +0000

    Add package pylint-starlark-plugin/0.1/pylint-starlark-plugin-0.1.tar.gz

---
+++ packages/pylint-starlark-plugin/pylint-starlark-plugin-0.1.tar.gz/pylint-starlark-plugin-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'pylint-starlark-plugin',         # How you named your package folder (MyLib)
+  packages = ['.'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Run pylint on Starklark files.',   # Give a short description about your library
+  author = 'Peter Hagen',                   # Type in your name
+  author_email = 'peter@phgn.io',      # Type in your E-Mail
+  url = 'https://github.com/phgn0/pylint-starlark-plugin',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/phgn0/pylint-starlark-plugin/archive/v0.1.tar.gz',    # I explain this later on
+  keywords = ['pylint', 'starlark', 'plugin'],   # Keywords that define your package best
+  install_requires=[
+    'jedi',
+    'astroid',
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
