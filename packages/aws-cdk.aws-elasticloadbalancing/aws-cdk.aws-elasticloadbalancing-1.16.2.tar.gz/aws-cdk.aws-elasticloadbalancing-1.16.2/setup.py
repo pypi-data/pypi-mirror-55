c3c1b7e39e2041fe13e635d442362720ac4b455e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:57 +0000

    Add package aws-cdk.aws-elasticloadbalancing/1.16.2/aws-cdk.aws-elasticloadbalancing-1.16.2.tar.gz

---
+++ packages/aws-cdk.aws-elasticloadbalancing/aws-cdk.aws-elasticloadbalancing-1.16.2.tar.gz/aws-cdk.aws-elasticloadbalancing-1.16.2/setup.py
@@ -0,0 +1,44 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-elasticloadbalancing",
+    "version": "1.16.2",
+    "description": "CDK Constructs for AWS ElasticLoadBalancing",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_elasticloadbalancing",
+        "aws_cdk.aws_elasticloadbalancing._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_elasticloadbalancing._jsii": [
+            "aws-elasticloadbalancing@1.16.2.jsii.tgz"
+        ],
+        "aws_cdk.aws_elasticloadbalancing": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.4",
+        "publication>=0.0.3",
+        "aws-cdk.aws-ec2~=1.16,>=1.16.2",
+        "aws-cdk.core~=1.16,>=1.16.2"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
