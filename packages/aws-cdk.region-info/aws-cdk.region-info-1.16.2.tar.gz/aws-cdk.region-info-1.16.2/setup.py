be99dc77f56587fe67e02f01ba3caa469f2e32f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:06 +0000

    Add package aws-cdk.region-info/1.16.2/aws-cdk.region-info-1.16.2.tar.gz

---
+++ packages/aws-cdk.region-info/aws-cdk.region-info-1.16.2.tar.gz/aws-cdk.region-info-1.16.2/setup.py
@@ -0,0 +1,42 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.region-info",
+    "version": "1.16.2",
+    "description": "AWS region information, such as service principal names",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.region_info",
+        "aws_cdk.region_info._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.region_info._jsii": [
+            "region-info@1.16.2.jsii.tgz"
+        ],
+        "aws_cdk.region_info": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.4",
+        "publication>=0.0.3"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
