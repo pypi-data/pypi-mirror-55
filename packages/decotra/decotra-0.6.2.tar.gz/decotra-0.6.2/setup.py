bd9217eafd86b96d472905541b99b031ac6af604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:50 +0000

    Add package decotra/0.6.2/decotra-0.6.2.tar.gz

---
+++ packages/decotra/decotra-0.6.2.tar.gz/decotra-0.6.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['decotra']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.10,<2.0', 'numpy>=1.17,<2.0']
+
+setup_kwargs = {
+    'name': 'decotra',
+    'version': '0.6.2',
+    'description': '',
+    'long_description': None,
+    'author': 'funwarioisii',
+    'author_email': 'mottirioisii@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
