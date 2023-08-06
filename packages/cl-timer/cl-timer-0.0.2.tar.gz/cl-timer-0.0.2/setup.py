8a5d6c394b6f6c796e623ca3b9d3ac52d91123cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:03 +0000

    Add package cl-timer/0.0.2/cl-timer-0.0.2.tar.gz

---
+++ packages/cl-timer/cl-timer-0.0.2.tar.gz/cl-timer-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cl-timer",
+    version="0.0.2",
+    author="Arin Khare",
+    author_email="arinmkhare@gmail.com",
+    description="A Cubing Timer for the Terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lol-cubes/cl-timer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    scripts=[
+        'cl-timer/art.py',
+        'cl-timer/graphics.py',
+        'cl-timer/scramble.py',
+        'cl-timer/timer.py',
+        'cl-timer/cl-timer'
+    ]
+)
