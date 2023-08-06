6bf535acaf526706ecc17e8dd79f90eab2c7316a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:05 +0000

    Add package yamlfmt/1.1.0/yamlfmt-1.1.0.tar.gz

---
+++ packages/yamlfmt/yamlfmt-1.1.0.tar.gz/yamlfmt-1.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README") as f:
+    long_description = f.read()
+
+classifiers = [
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 2",
+    "Topic :: Utilities",
+]
+
+setup(
+    author="Manuel Mendez",
+    author_email="mmendez534@gmail.com",
+    classifiers=classifiers,
+    description="An opinionated yaml formatter based on ruamel.yaml",
+    install_requires=["ruamel.yaml<0.16"],
+    keywords="yaml formatter",
+    license="GPLV3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="yamlfmt",
+    scripts=["yamlfmt"],
+    url="https://github.com/mmlb/yamlfmt",
+    version="1.1.0",
+)
