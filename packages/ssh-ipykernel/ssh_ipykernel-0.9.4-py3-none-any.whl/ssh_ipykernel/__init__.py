4d24c9012e42047bd69f8dc6288fea443caeea30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:54 +0000

    Add package ssh-ipykernel/0.9.4/ssh_ipykernel-0.9.4-py3-none-any.whl

---
+++ packages/ssh-ipykernel/ssh_ipykernel-0.9.4-py3-none-any.whl/ssh_ipykernel/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for SSH Kernel."""
+
+__author__ = """Bernhard Walter"""
+__email__ = 'b_walter@arcor.de'
+from ._version import __version__, __version_info__
