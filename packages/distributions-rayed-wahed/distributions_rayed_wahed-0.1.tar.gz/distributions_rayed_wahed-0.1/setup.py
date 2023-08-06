b65890d5aad6798a4d5770cddf920aa7cac8818a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:42 +0000

    Add package distributions-rayed-wahed/0.1/distributions_rayed_wahed-0.1.tar.gz

---
+++ packages/distributions-rayed-wahed/distributions_rayed_wahed-0.1.tar.gz/distributions_rayed_wahed-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_rayed_wahed',
+      version='0.1',
+      description='Gaussian Distributions',
+      packages=['distributions_rayed_wahed'],
+      zip_safe=False)
