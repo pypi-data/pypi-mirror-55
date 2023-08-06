3422d7860ea2591a3c54aa3b6f175cfec585e132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:37 +0000

    Add package salure-helpers/9.9.4/salure_helpers-9.9.4-py3-none-any.whl

---
+++ packages/salure-helpers/salure_helpers-9.9.4-py3-none-any.whl/salure_helpers/planning_nl.py
@@ -0,0 +1,157 @@
+import zeep
+from zeep import Client
+from zeep.helpers import serialize_object
+
+
+class PlanningNl(object):
+    def __init__(self, username, password):
+        """
+        This package is meant to get from or push data to planning.nl. An timeschedule tool. The requests are all made with soap.
+        See for documentation of the SOAP API the following link: https://wiki.visibox.nl/display/VIS/Algemene+informatie
+        :param username: The username for planning.nl
+        :param password: The corresponding password
+        """
+        client = Client('https://api.visibox.nl/v1/authentication?wsdl')
+        request_body = {'username': username, 'password': password}
+        response = client.service.login(request_body)
+        self.token = response['authenticationToken']
+
+    def get_human_resources(self, employee_id=None):
+        """
+        Get all the employees with their basic information (like personal details).
+        :param employee_id: An optional filter to receive data from only 1 employee
+        :return: the data returned from the SOAP request
+        """
+        # Determine the client for the resources
+        client = Client('https://api.visibox.nl/v1/resource?wsdl')
+        # Get all the employee resources without filters
+        if employee_id == None:
+            request_body = {
+                'authentication': {
+                    'token': self.token
+                },
+                'humanResource': {
+                    'resourceClass': 'PERSONNEL',
+                    'humanResourceMatcher': {
+                        'allowMultiple': 'true'
+                    }
+                }
+            }
+        else:
+            request_body = {
+                'authentication': {
+                    'token': self.token
+                },
+                'humanResource': {
+                    'resourceClass': 'PERSONNEL',
+                    'humanResourceMatcher': {
+                        'allowMultiple': 'true',
+                        'number': employee_id
+                    }
+                }
+            }
+        response = client.service.getHumanResources(**request_body)['matcherResults']['matcherResult'][0]['entities']['entity']
+        data = serialize_object(response)
+        return data
+
+    def get_departments(self):
+        """
+        Get all the departments from planning.nl. Only the departments, no employees etc.
+        :return: the data returned from the SOAP request
+        """
+        client = Client('https://api.visibox.nl/v1/department?wsdl')
+        request_body = {
+            'authentication': {
+                'token': self.token
+            },
+            'departmentMatcher': {
+                'allowMultiple': 'true'
+            }
+        }
+        response = client.service.getDepartments(**request_body)['matcherResults']['matcherResult'][0]['entities']['entity']
+        data = serialize_object(response)
+        return data
+
+    def get_resource_sorts(self):
+        """
+        Get all the resource sorts which are used in the planning.nl environment. This could be the job descriptions for example
+        :return: the data returned from the SOAP request
+        """
+        client = Client('https://api.visibox.nl/v1/resource?wsdl')
+        request_body = {
+            'authentication': {
+                'token': self.token
+            },
+            'resourceSort': {
+                'resourceClass': 'PERSONNEL',
+                'resourceSortMatcher': {
+                    'allowMultiple': 'true'
+                }
+            }
+        }
+        response = client.service.getResourceSorts(**request_body)['matcherResults']['matcherResult'][0]['entities']['entity']
+        data = serialize_object(response)
+        return data
+
+    def get_resource_types(self):
+        """
+        Get all the resource types which are used in the planning.nl environment. This could be the departments for example
+        :return: the data returned from the SOAP request
+        """
+        client = Client('https://api.visibox.nl/v1/resource?wsdl')
+        request_body = {
+            'authentication': {
+                'token': self.token
+            },
+            'resourceType': {
+                'resourceClass': 'PERSONNEL',
+                'resourceTypeMatcher': {
+                    'allowMultiple': 'true'
+                }
+            }
+        }
+        response = client.service.getResourceTypes(**request_body)['matcherResults']['matcherResult'][0]['entities']['entity']
+        data = serialize_object(response)
+        return data
+
+    def get_human_departments(self):
+        """
+        Get the ID's of employees with their department ID's
+        :return: the data returned from the SOAP request
+        """
+        client = Client('https://api.visibox.nl/v1/resource?wsdl')
+        request_body = {
+            'authentication': {
+                'token': self.token
+            },
+            'resourceDepartment': {
+                'resourceClass': 'PERSONNEL',
+                'resourceDepartmentMatcher': {
+                    'allowMultiple': 'true',
+                    'humanResourceMatcher': {
+                        'allowMultiple': 'true'
+                    },
+                    'departmentMatcher': {
+                        'allowMultiple': 'true'
+                    }
+                }
+            }
+        }
+        response = client.service.getResourceDepartments(**request_body)['matcherResults']['matcherResult'][0]['entities']['entity']
+        data = serialize_object(response)
+        return data
+
+    def push_human_recources(self):
+        client = Client('https://api.visibox.nl/v1/resource?wsdl')
+
+    def logout(self):
+        client = Client('https://api.visibox.nl/v1/authentication?wsdl')
+        # Logout so that the token is destroyed. The logoutRequest key is mandatory but because there is no available value set a zeep.xsd.SkipValue
+        request_body = {
+            'authentication': {
+                'token': self.token
+            },
+            'logoutRequest': zeep.xsd.SkipValue
+        }
+        response = client.service.logout(**request_body)
+        return response
