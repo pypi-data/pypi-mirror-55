65a92997f23dbd3157c6565c1828ab7ca1ffab4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:20 +0000

    Add package weather-forcaster/1.0.1/weather-forcaster-1.0.1.tar.gz

---
+++ packages/weather-forcaster/weather-forcaster-1.0.1.tar.gz/weather-forcaster-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="weather-forcaster",
+    version="1.0.1",
+    description="A Python package to get weather forcast for any location.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ragubass/weather-forcaster",
+    author="Ragu Sekaran",
+    author_email="seleniumragu@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["weather_forcaster"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "weather-forcaster=weather_forcaster.main:main",
+        ]
+    },
+)
