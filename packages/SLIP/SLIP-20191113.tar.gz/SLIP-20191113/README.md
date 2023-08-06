5685e997a63e7ed4ef69dac05fc1540a77b800b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:43 +0000

    Add package SLIP/20191113/SLIP-20191113.tar.gz

---
+++ packages/SLIP/SLIP-20191113.tar.gz/SLIP-20191113/README.md
@@ -0,0 +1,10 @@
+[![PyPI version](https://badge.fury.io/py/SLIP.svg)](https://badge.fury.io/py/SLIP)
+[![Research software impact](http://depsy.org/api/package/pypi/SLIP/badge.svg)](http://depsy.org/package/python/SLIP)
+
+SLIP: a Simple Library for Image Processing
+===========================================
+
+This library collects different Image Processing tools for use with the  [LogGabor](https://pythonhosted.org/LogGabor/) and  [SparseEdges](https://pythonhosted.org/SparseEdges/) libraries.
+
+More information is available @ http://nbviewer.ipython.org/github/bicv/SLIP/blob/master/SLIP.ipynb
+Tests for the packages are available @ http://nbviewer.ipython.org/github/bicv/SLIP/blob/master/test_SLIP.ipynb
---
+++ packages/SLIP/SLIP-20191113.tar.gz/SLIP-20191113/default_param.py
@@ -0,0 +1,25 @@
+{
+# Image
+'N_X' : 256, # size of images
+'N_Y' : 256, # size of images
+'do_mask'  : True, # used for instance in the Matching Pursuit algorithm self.pe.do_mask
+'mask_exponent': 3., #sharpness of the mask
+'use_cache' : True,
+'verbose': 0,
+'figpath': 'results',
+'matpath': 'cache_dir',
+'datapath': 'database',
+'figsize': 14.,
+'formats': ['pdf', 'png', 'jpg'],
+'dpi': 450,
+'seed': None, # a seed for the Random Number Generator (RNG) for picking images in databases, set to None or a given number to freeze the RNG
+'N_image': None, # number of images to pick in a database, set to None if you want to pick all in the database
+# whitening parameters:
+'white_name_database' : 'kodakdb',
+'white_n_learning' : 0,
+'white_N' : .07,
+'white_f_0' : .38, # olshausen = 0.2
+'white_alpha' : 1.4,
+'white_steepness' : 4.,
+'white_recompute' : False,
+}
