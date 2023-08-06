6a157194f291c3b056642ce15d8cff2f4a98ff5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:52 +0000

    Add package garnett/0.6.1/garnett-0.6.1-py3-none-any.whl

---
+++ packages/garnett/garnett-0.6.1-py3-none-any.whl/garnett/getarfilereader.py
@@ -0,0 +1,165 @@
+# Copyright (c) 2019 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+"""getar-file reader for the Glotzer Group, University of Michigan.
+
+Authors: Matthew Spellings, Carl Simon Adorf
+
+.. code::
+
+    reader = GetarFileReader()
+    traj = reader.read(open('trajectory.tar', 'rb'))
+"""
+
+import json
+import logging
+import collections
+
+import numpy as np
+import gtar
+
+from .trajectory import _RawFrameData, Box, Frame, Trajectory
+from .shapes import _parse_type_shape
+
+logger = logging.getLogger(__name__)
+
+
+class GetarFrame(Frame):
+    """Interface to grab getar frame data.
+
+    :param trajectory: gtar.GTAR trajectory object to read from
+    :param records: Dictionary of gtar.Record objects
+    :param frame: Frame name inside the trajectory
+    :param default_type: The default particle type
+    :type default_type: str
+    """
+
+    def __init__(self, trajectory, records, frame, default_type, default_box):
+        super(GetarFrame, self).__init__()
+        self._trajectory = trajectory
+        self._records = records
+        self._frame = frame
+        self._default_type = default_type
+        self._default_box = default_box
+
+    def __str__(self):
+        return "GetarFrame({})".format(self._records)
+
+    def read(self):
+        raw_frame = _RawFrameData()
+        raw_frame.shapedef = collections.OrderedDict()
+        prop_map = {
+            'position': 'positions',
+            'orientation': 'orientations',
+            'velocity': 'velocities',
+            'angular_momentum_quat': 'angmom',
+            'image': 'image'}
+        supported_records = ['position', 'orientation', 'velocity',
+                             'mass', 'charge', 'diameter',
+                             'moment_inertia', 'angular_momentum_quat',
+                             'image']
+        for name in supported_records:
+            try:
+                values = self._trajectory.getRecord(
+                    self._records[name], self._frame)
+            except KeyError:
+                values = None
+
+            if values is not None:
+                frame_prop = prop_map.get(name, name)
+                setattr(raw_frame, frame_prop, values)
+
+        if 'type' in self._records and 'type_names.json' in self._records:
+            names = json.loads(self._trajectory.getRecord(
+                self._records['type_names.json'], self._frame))
+            types = self._trajectory.getRecord(
+                self._records['type'], self._frame)
+            raw_frame.types = [names[t] for t in types]
+        else:
+            raw_frame.types = len(raw_frame.positions) * [self._default_type]
+
+        if 'box' in self._records:
+            # Read dimension if stored
+            if 'dimensions' in self._records:
+                dimensions = self._trajectory.getRecord(
+                             self._records['dimensions'], self._frame)[0]
+            # Fallback to detection based on z coordinates
+            else:
+                zs = raw_frame.positions[:, 2]
+                dimensions = 2 if np.allclose(zs, 0.0, atol=1e-7) else 3
+
+            box = self._trajectory.getRecord(self._records['box'], self._frame)
+            gbox = Box(
+                **dict(
+                    zip(['Lx', 'Ly', 'Lz', 'xy', 'xz', 'yz'], box),
+                    dimensions=dimensions)
+            )
+            raw_frame.box = np.array(gbox.get_box_matrix())
+            raw_frame.box_dimensions = int(dimensions)
+        else:
+            raw_frame.box = self._default_box
+
+        if 'type_names.json' in self._records and 'type_shapes.json' in self._records:
+            names = json.loads(self._trajectory.getRecord(
+                self._records['type_names.json'], self._frame))
+            shapes = json.loads(self._trajectory.getRecord(
+                self._records['type_shapes.json'], self._frame))
+            for name, shape in zip(names, shapes):
+                shape_def = _parse_type_shape(shape)
+                raw_frame.shapedef.update({name: shape_def})
+
+        return raw_frame
+
+
+class GetarFileReader(object):
+    """getar-file reader for the Glotzer Group, University of Michigan.
+
+    Authors: Matthew Spellings, Carl Simon Adorf
+
+    Read GEneric Trajectory ARchive files, a binary format designed
+    for efficient, extensible storage of trajectory data.
+
+    This class provides a wrapper for the gtar library.
+
+    .. code::
+
+        reader = GetarFileReader()
+        with open('trajectory.tar', 'rb') as file:
+            traj = reader.read(file)
+    """
+
+    def read(self, stream, default_type='A', default_box=None):
+        """Read binary stream and return a trajectory instance.
+
+        :param stream: The stream, which contains the GeTarFile.
+        :type stream: A file-like binary stream.
+        :param default_type: The default particle type for
+                             posfile dialects without type definition.
+        :type default_type: str
+        :param default_box: The default_box value is used if no
+                            box is specified in the libgetar file.
+                            Defaults to [Lx=Ly=Lz=1.0].
+        :type default_box: :class:`numpy.ndarray`
+        """
+        if default_box is None:
+            default_box = np.diag([1.0] * 3)
+        try:
+            filename = stream.name
+        except AttributeError:
+            raise NotImplementedError(
+                "The current implementation of the GeTarFileReader requires "
+                "file objects with name attribute, such as NamedTemporaryFile "
+                "as the underlying library is reading the file by filename "
+                "and not directly from the stream.")
+        _trajectory = gtar.GTAR(filename, 'r')
+        _records = {rec.getName(): rec for rec in _trajectory.getRecordTypes() if not rec.getGroup()}
+        # assume that we care primarily about positions
+        try:
+            self._frames = _trajectory.queryFrames(_records['position'])
+        except KeyError:
+            raise RuntimeError("Given trajectory '{}' contained no "
+                               "positions.".format(stream))
+        frames = [GetarFrame(_trajectory, _records, idx, default_type, default_box)
+                  for idx in self._frames]
+        logger.info("Read {} frames.".format(len(frames)))
+        return Trajectory(frames)
---
+++ packages/garnett/garnett-0.6.1-py3-none-any.whl/garnett/gsdhoomdfilereader.py
@@ -0,0 +1,308 @@
+# Copyright (c) 2019 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+"""Hoomd-GSD-file reader for the Glotzer Group, University of Michigan.
+
+Author: Carl Simon Adorf
+
+This module provides a wrapper for the trajectory reader
+implementation as part of the gsd package.
+
+A gsd file may not contain all shape information.
+To provide additional information it is possible
+to pass a frame object, whose properties
+are copied into each frame of the gsd trajectory.
+
+The example is given for a hoomd-blue xml frame:
+
+.. code::
+
+    pos_reader = PosFileReader()
+    gsd_reader = GSDHOOMDFileReader()
+
+    with open('init.pos') as posfile:
+        with open('dump.gsd') as gsdfile:
+            pos_frame = pos_reader.read(posfile)[0]
+            traj = gsd_reader.read(gsdfile, pos_frame)
+"""
+
+import logging
+import warnings
+import copy
+import collections
+
+import numpy as np
+
+from .trajectory import _RawFrameData, Frame, Trajectory
+from .shapes import SphereShape, ConvexPolyhedronShape, ConvexSpheropolyhedronShape, \
+    PolygonShape, SpheropolygonShape, EllipsoidShape, _parse_type_shape
+
+try:
+    from gsd.fl import GSDFile
+    NATIVE = True
+except ImportError:
+    NATIVE = False
+from .pygsd import GSDFile as PyGSDFile
+from . import gsdhoomd
+
+
+logger = logging.getLogger(__name__)
+
+
+def _box_matrix(box):
+    lx, ly, lz, xy, xz, yz = box
+    return np.array([
+        [lx, 0.0, 0.0],
+        [xy * ly, ly, 0.0],
+        [xz * lz, yz * lz, lz]]).T
+
+
+def _parse_shape_definitions(frame, gsdfile, frame_index):
+
+    def get_chunk(i, chunk, default=None):
+        if gsdfile.chunk_exists(i, chunk):
+            return gsdfile.read_chunk(i, chunk)
+        elif gsdfile.chunk_exists(0, chunk):
+            return gsdfile.read_chunk(0, chunk)
+        else:
+            return default
+
+    shapedefs = collections.OrderedDict()
+    types = frame.particles.types
+
+    if get_chunk(frame_index, 'particles/type_shapes') is not None:
+        # --------------------------------------------------
+        # Parsing from GSD Shape Visualization Specification
+        # --------------------------------------------------
+
+        type_shapes = frame.particles.type_shapes
+        for typename, type_shape in zip(types, type_shapes):
+            shapedefs[typename] = _parse_type_shape(type_shape)
+        return shapedefs
+    else:
+
+        # -----------------------
+        # Parsing from HPMC State
+        # -----------------------
+
+        # Spheres
+        if get_chunk(frame_index, 'state/hpmc/sphere/radius') is not None:
+            radii = get_chunk(frame_index, 'state/hpmc/sphere/radius')
+            orientables = get_chunk(frame_index, 'state/hpmc/sphere/orientable')
+            # Since the orientable chunk was only added in HOOMD Schema version 1.3,
+            # not all GSD files may have it. Thus, it is set to False in such cases.
+            if orientables is None:
+                orientables = [False]*len(radii)
+            for typename, radius, orientable in zip(types, radii, orientables):
+                shapedefs[typename] = SphereShape(
+                    diameter=radius*2, orientable=orientable, color=None)
+            return shapedefs
+
+        # Convex Polyhedra
+        elif get_chunk(frame_index, 'state/hpmc/convex_polyhedron/N') is not None:
+            N = get_chunk(frame_index, 'state/hpmc/convex_polyhedron/N')
+            N_start = [sum(N[:i]) for i in range(len(N))]
+            N_end = [sum(N[:i+1]) for i in range(len(N))]
+            verts = get_chunk(frame_index, 'state/hpmc/convex_polyhedron/vertices')
+            verts_split = [verts[start:end] for start, end in zip(N_start, N_end)]
+            for typename, typeverts in zip(types, verts_split):
+                shapedefs[typename] = ConvexPolyhedronShape(
+                    vertices=typeverts, color=None)
+            return shapedefs
+
+        # Convex Spheropolyhedra
+        elif get_chunk(frame_index, 'state/hpmc/convex_spheropolyhedron/N') is not None:
+            N = get_chunk(frame_index, 'state/hpmc/convex_spheropolyhedron/N')
+            N_start = [sum(N[:i]) for i in range(len(N))]
+            N_end = [sum(N[:i+1]) for i in range(len(N))]
+            verts = get_chunk(frame_index,
+                              'state/hpmc/convex_spheropolyhedron/vertices')
+            verts_split = [verts[start:end] for start, end in zip(N_start, N_end)]
+            sweep_radii = get_chunk(frame_index,
+                                    'state/hpmc/convex_spheropolyhedron/sweep_radius')
+            for typename, typeverts, radius in zip(types, verts_split, sweep_radii):
+                shapedefs[typename] = ConvexSpheropolyhedronShape(
+                    vertices=typeverts, rounding_radius=radius, color=None)
+            return shapedefs
+
+        # Ellipsoid
+        elif get_chunk(frame_index, 'state/hpmc/ellipsoid/a') is not None:
+            a_all = get_chunk(frame_index, 'state/hpmc/ellipsoid/a')
+            b_all = get_chunk(frame_index, 'state/hpmc/ellipsoid/b')
+            c_all = get_chunk(frame_index, 'state/hpmc/ellipsoid/c')
+            for typename, a, b, c in zip(types, a_all, b_all, c_all):
+                shapedefs[typename] = EllipsoidShape(
+                    a=a, b=b, c=c, color=None)
+            return shapedefs
+
+        # Convex Polygons
+        elif get_chunk(frame_index, 'state/hpmc/convex_polygon/N') is not None:
+            N = get_chunk(frame_index, 'state/hpmc/convex_polygon/N')
+            N_start = [sum(N[:i]) for i in range(len(N))]
+            N_end = [sum(N[:i+1]) for i in range(len(N))]
+            verts = get_chunk(frame_index, 'state/hpmc/convex_polygon/vertices')
+            verts_split = [verts[start:end] for start, end in zip(N_start, N_end)]
+            for typename, typeverts in zip(types, verts_split):
+                shapedefs[typename] = PolygonShape(
+                    vertices=typeverts, color=None)
+            return shapedefs
+
+        # Convex Spheropolygons
+        elif get_chunk(frame_index, 'state/hpmc/convex_spheropolygon/N') is not None:
+            N = get_chunk(frame_index, 'state/hpmc/convex_spheropolygon/N')
+            N_start = [sum(N[:i]) for i in range(len(N))]
+            N_end = [sum(N[:i+1]) for i in range(len(N))]
+            verts = get_chunk(frame_index, 'state/hpmc/convex_spheropolygon/vertices')
+            verts_split = [verts[start:end] for start, end in zip(N_start, N_end)]
+            sweep_radii = get_chunk(frame_index,
+                                    'state/hpmc/convex_spheropolygon/sweep_radius')
+            for typename, typeverts, radius in zip(types, verts_split, sweep_radii):
+                shapedefs[typename] = SpheropolygonShape(
+                    vertices=typeverts, rounding_radius=radius, color=None)
+            return shapedefs
+
+        # Simple Polygons
+        elif get_chunk(frame_index, 'state/hpmc/simple_polygon/N') is not None:
+            N = get_chunk(frame_index, 'state/hpmc/simple_polygon/N')
+            N_start = [sum(N[:i]) for i in range(len(N))]
+            N_end = [sum(N[:i+1]) for i in range(len(N))]
+            verts = get_chunk(frame_index, 'state/hpmc/simple_polygon/vertices')
+            verts_split = [verts[start:end] for start, end in zip(N_start, N_end)]
+            for typename, typeverts in zip(types, verts_split):
+                shapedefs[typename] = PolygonShape(
+                    vertices=typeverts, color=None)
+            return shapedefs
+
+        # If no shapes were detected, return None
+        else:
+            return None
+
+
+class GSDHoomdFrame(Frame):
+    """Extends the Frame object for GSD files.
+
+    :param traj:
+        Trajectory containing the frame to cast
+    :type traj:
+        :class:`trajectory.Trajectory`
+    :param frame_index:
+        The index of the frame to cast
+    :type frame_index:
+        int
+    :param t_frame:
+        A frame containing shape information that is not encoded in
+        the GSD-format. By default, shape information is read from the
+        passed frame object, if one provided. Otherwise, shape information
+        is read from the gsd file.
+    :type :
+        :class:`trajectory.Frame`
+    :param gsdfile:
+        A gsd file object.
+    :type gsdfile:
+        :class:`gsd.fl.GSDFile`
+    """
+
+    def __init__(self, traj, frame_index, t_frame, gsdfile):
+        self.traj = traj
+        self.frame_index = frame_index
+        self.t_frame = t_frame
+        self.gsdfile = gsdfile
+        super(GSDHoomdFrame, self).__init__()
+
+    def read(self):
+        raw_frame = _RawFrameData()
+        frame = self.traj.read_frame(self.frame_index)
+        # If frame is provided, read shape data from it
+        if self.t_frame is not None:
+            raw_frame.data = copy.deepcopy(self.t_frame.data)
+            raw_frame.data_keys = copy.deepcopy(self.t_frame.data_keys)
+            raw_frame.box_dimensions = self.t_frame.box.dimensions
+            try:
+                raw_frame.shapedef = copy.deepcopy(self.t_frame.shapedef)
+            except AttributeError:
+                pass
+        else:
+            # Fallback to gsd shape data if no frame is provided
+            raw_frame.shapedef = _parse_shape_definitions(frame, self.gsdfile, self.frame_index)
+        raw_frame.box = _box_matrix(frame.configuration.box)
+        raw_frame.box_dimensions = int(frame.configuration.dimensions)
+        raw_frame.types = [frame.particles.types[t] for t in frame.particles.typeid]
+        raw_frame.positions = frame.particles.position
+        raw_frame.orientations = frame.particles.orientation
+        raw_frame.velocities = frame.particles.velocity
+        raw_frame.mass = frame.particles.mass
+        raw_frame.charge = frame.particles.charge
+        raw_frame.diameter = frame.particles.diameter
+        raw_frame.moment_inertia = frame.particles.moment_inertia
+        raw_frame.angmom = frame.particles.angmom
+        raw_frame.image = frame.particles.image
+        return raw_frame
+
+    def __str__(self):
+        return "GSDHoomdFrame(# frames={})".format(len(self.traj))
+
+
+class GSDHOOMDFileReader(object):
+    """Hoomd-GSD-file reader for the Glotzer Group, University of Michigan.
+
+    Author: Carl Simon Adorf
+
+    This class provides a wrapper for the trajectory reader
+    implementation as part of the gsd package.
+
+    A gsd file may not contain all shape information.
+    To provide additional information it is possible
+    to pass a frame object, whose properties
+    are copied into each frame of the gsd trajectory.
+
+    The example is given for a HOOMD-blue XML frame:
+
+    .. code::
+
+        xml_reader = HOOMDXMLFileReader()
+        gsd_reader = GSDHOOMDFileReader()
+
+        with open('init.xml') as xmlfile:
+            with open('dump.gsd', 'rb') as gsdfile:
+                xml_frame = xml_reader.read(xmlfile)[0]
+                traj = gsd_reader.read(gsdfile, xml_frame)
+
+    About the read_gsd_shape_data parameter: This parameter was removed. By default,
+                shape information is read from a passed frame object, if one
+                provided. Otherwise, shape information is read from the gsd file.
+
+    """
+
+    def __init__(self):
+        pass
+
+    def read(self, stream, frame=None):
+        """Read binary stream and return a trajectory instance.
+
+        :param stream: The stream, which contains the gsd-file.
+        :type stream: A file-like binary stream
+        :param frame: A frame containing shape information
+            that is not encoded in the GSD-format. By default,
+            shape information is read from the passed frame object,
+            if one provided. Otherwise, shape information
+            is read from the gsd file.
+        :type frame: :class:`trajectory.Frame`"""
+        if NATIVE:
+            try:
+                gsdfile = GSDFile(stream.name, stream.mode)
+                traj = gsdhoomd.HOOMDTrajectory(gsdfile)
+            except AttributeError:
+                logger.info(
+                    "Unable to open file stream natively, falling back "
+                    "to pure python GSD reader.")
+                gsdfile = PyGSDFile(stream)
+                traj = gsdhoomd.HOOMDTrajectory(gsdfile)
+        else:
+            warnings.warn("Native GSD library not available. "
+                          "Falling back to pure python reader.")
+            gsdfile = PyGSDFile(stream)
+            traj = gsdhoomd.HOOMDTrajectory(gsdfile)
+        frames = [GSDHoomdFrame(traj, i, t_frame=frame, gsdfile=gsdfile)
+                  for i in range(len(traj))]
+        logger.info("Read {} frames.".format(len(frames)))
+        return Trajectory(frames)
