ae80cddc72ab2d6dfdaee563b3b3ac528106df62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:04 +0000

    Add package pytopojson/0.0.3/pytopojson-0.0.3.tar.gz

---
+++ packages/pytopojson/pytopojson-0.0.3.tar.gz/pytopojson-0.0.3/topojson/prueba.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+import sys
+
+# Third-party imports
+# from pytopojson import topojson
+
+# Application-specific imports
+
+
+if __name__ == '__main__':
+    import atomicwrites
