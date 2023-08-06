3c075d630fdce10b442324cd095fcea32dc60db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:48 +0000

    Add package pytorch-metric-learning/0.9.13/pytorch_metric_learning-0.9.13.tar.gz

---
+++ packages/pytorch-metric-learning/pytorch_metric_learning-0.9.13.tar.gz/pytorch_metric_learning-0.9.13/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytorch_metric_learning",
+    version="0.9.13",
+    author="Kevin Musgrave",
+    author_email="tkm45@cornell.edu",
+    description="A flexible and extensible metric learning library, written in PyTorch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TakeshiMusgrave/pytorch_metric_learning",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+          'torch',
+          'torchvision',
+          'numpy',
+          'tqdm',
+          'matplotlib'
+    ],
+)
