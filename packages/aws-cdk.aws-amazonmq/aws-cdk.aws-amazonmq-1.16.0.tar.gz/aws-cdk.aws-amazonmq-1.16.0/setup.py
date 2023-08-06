2333c4ff6753e4689b85f2bf5dc975afb2baeb70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:10 +0000

    Add package aws-cdk.aws-amazonmq/1.16.0/aws-cdk.aws-amazonmq-1.16.0.tar.gz

---
+++ packages/aws-cdk.aws-amazonmq/aws-cdk.aws-amazonmq-1.16.0.tar.gz/aws-cdk.aws-amazonmq-1.16.0/setup.py
@@ -0,0 +1,43 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-amazonmq",
+    "version": "1.16.0",
+    "description": "The CDK Construct Library for AWS::AmazonMQ",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_amazonmq",
+        "aws_cdk.aws_amazonmq._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_amazonmq._jsii": [
+            "aws-amazonmq@1.16.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_amazonmq": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.2",
+        "publication>=0.0.3",
+        "aws-cdk.core~=1.16,>=1.16.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
