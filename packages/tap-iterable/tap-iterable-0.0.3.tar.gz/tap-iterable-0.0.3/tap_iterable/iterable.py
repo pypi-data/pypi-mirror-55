afd94ae7341e6053102e6710e4122cdef9364267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:33 +0000

    Add package tap-iterable/0.0.3/tap-iterable-0.0.3.tar.gz

---
+++ packages/tap-iterable/tap-iterable-0.0.3.tar.gz/tap-iterable-0.0.3/tap_iterable/iterable.py
@@ -0,0 +1,167 @@
+#
+# Module dependencies.
+#
+
+from datetime import datetime, timedelta
+from singer import utils
+from urllib.parse import urlencode
+import backoff
+import json
+import requests
+import logging
+
+
+logger = logging.getLogger()
+
+
+""" Simple wrapper for Iterable. """
+class Iterable(object):
+
+  def __init__(self, api_key, start_date=None, api_window_in_days=30):
+    self.api_key = api_key
+    self.uri = "https://api.iterable.com/api/"
+    self.api_window_in_days = float(api_window_in_days)
+    self.MAX_BYTES = 10240
+    self.CHUNK_SIZE = 512
+
+
+  def _now(self):
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+  def _daterange(self, start_date, end_date):
+    total_days = (utils.strptime_with_tz(end_date) - utils.strptime_with_tz(start_date)).days
+    if total_days >= self.api_window_in_days:
+      for n in range(int(total_days / self.api_window_in_days)):
+        yield (utils.strptime_with_tz(start_date) + n * timedelta(self.api_window_in_days)).strftime("%Y-%m-%d %H:%M:%S")
+    else:
+      yield start_date
+
+
+  def _get_end_datetime(self, startDateTime):
+    endDateTime = utils.strptime_with_tz(startDateTime) + timedelta(self.api_window_in_days)
+    return endDateTime.strftime("%Y-%m-%d %H:%M:%S")
+
+
+  def retry_handler(details):
+    logger.info("Received 429 -- sleeping for %s seconds",
+                details['wait'])
+
+  #
+  # The actual `get` request.
+  #
+
+  @backoff.on_exception(backoff.expo,
+                        requests.exceptions.HTTPError,
+                        on_backoff=retry_handler,
+                        max_tries=10)
+  def _get(self, path, stream=True, **kwargs):
+    uri = "{uri}{path}".format(uri=self.uri, path=path)
+
+    # Add query params, including `api_key`.
+    params = { "api_key": self.api_key }
+    for key, value in kwargs.items():
+      params[key] = value
+    uri += "?{params}".format(params=urlencode(params))
+
+    logger.info("GET request to {uri}".format(uri=uri))
+    response = requests.get(uri, stream=stream)
+    response.raise_for_status()
+    return response
+
+  #
+  # The common `get` request.
+  #
+
+  def get(self, path, **kwargs):
+    response = self._get(path, **kwargs)
+    return response.json()
+
+  #
+  # Get custom user fields, used for generating `users` schema in `discover`.
+  #
+
+  def get_user_fields(self):
+    return self.get("users/getFields")
+
+  #
+  # Methods to retrieve data per stream/resource.
+  #
+
+  def lists(self, column_name=None, bookmark=None):
+    res = self.get("lists")
+    for l in res["lists"]:
+      yield l
+
+
+  def list_users(self, column_name=None, bookmark=None):
+    res = self.get("lists")
+    for l in res["lists"]:
+      kwargs = {
+        "listId": l["id"]
+      }
+      users = self._get("lists/getUsers", **kwargs)
+      for user in users:
+        yield {
+          "email": user,
+          "listId": l["id"],
+          "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S %Z")
+        }
+
+
+  def campaigns(self, column_name=None, bookmark=None):
+    res = self.get("campaigns")
+    for c in res["campaigns"]:
+      yield c
+
+
+  def channels(self, column_name, bookmark):
+    res = self.get("channels")
+    for c in res["channels"]:
+      yield c
+
+
+  def message_types(self, column_name=None, bookmark=None):
+    res = self.get("messageTypes")
+    for m in res["messageTypes"]:
+      yield m
+
+
+  def templates(self, column_name, bookmark):
+    template_types = [
+      "Base",
+      "Blast",
+      "Triggered",
+      "Workflow"
+    ]
+    message_mediums = [
+      "Email",
+      "Push",
+      "InApp",
+      "SMS"
+    ]
+    for template_type in template_types:
+      for medium in message_mediums:
+        res = self.get("templates", templateTypes=template_type, messageMedium=medium)
+        for t in res["templates"]:
+          yield t
+
+
+  def metadata(self, column_name=None, bookmark=None):
+    tables = self.get("metadata")
+    for t in tables["results"]:
+      keys = self.get("metadata/{table_name}".format(table_name=t["name"]))
+      for k in keys["results"]:
+        value = self.get("metadata/{table_name}/{key}".format(table_name=k["table"], key=k["key"]))
+        yield value
+
+
+  def get_data_export_generator(self, data_type_name, bookmark=None):
+    now = self._now()
+    kwargs = {}
+    for start_date_time in self._daterange(bookmark, now):
+      kwargs["startDateTime"] = start_date_time
+      kwargs["endDateTime"] = self._get_end_datetime(startDateTime=start_date_time)
+      def get_data():
+        return self._get("export/data.json", dataTypeName=data_type_name, **kwargs)
+      yield get_data
---
+++ packages/tap-iterable/tap-iterable-0.0.3.tar.gz/tap-iterable-0.0.3/tap_iterable/streams.py
@@ -0,0 +1,313 @@
+#
+# Module dependencies.
+#
+
+import os
+import json
+import datetime
+import pytz
+import singer
+import time
+import tempfile
+from singer import metadata
+from singer import utils
+from singer.metrics import Point
+from dateutil.parser import parse
+from tap_iterable.context import Context
+
+
+logger = singer.get_logger()
+KEY_PROPERTIES = ['id']
+
+
+def get_abs_path(path):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
+
+def epoch_to_datetime_string(milliseconds):
+    datetime_string = None
+    try:
+        datetime_string = time.strftime("%Y-%m-%d %H:%M:%S %Z", time.localtime(milliseconds / 1000))
+    except TypeError:
+        # If fails, it means format already datetime string.
+        datetime_string = milliseconds
+        pass
+    return datetime_string
+
+
+class Stream():
+    name = None
+    replication_method = None
+    replication_key = None
+    stream = None
+    key_properties = KEY_PROPERTIES
+    session_bookmark = None
+
+
+    def __init__(self, client=None):
+        self.client = client
+
+
+    def is_session_bookmark_old(self, value):
+        if self.session_bookmark is None:
+            return True
+        # Assume value is in epoch milliseconds.
+        value_in_date_time = epoch_to_datetime_string(value)
+        return utils.strptime_with_tz(value_in_date_time) > utils.strptime_with_tz(self.session_bookmark)
+
+
+    def update_session_bookmark(self, value):
+        # Assume value is epoch milliseconds.
+        value_in_date_time = epoch_to_datetime_string(value)
+        if self.is_session_bookmark_old(value_in_date_time):
+            self.session_bookmark = value_in_date_time
+
+
+    # Reads and converts bookmark from state.
+    def get_bookmark(self, state, name=None):
+        name = self.name if not name else name
+        return (singer.get_bookmark(state, name, self.replication_key)) or Context.config["start_date"]
+
+
+    # Converts and writes bookmark to state.
+    def update_bookmark(self, state, value, name=None):
+        name = self.name if not name else name
+        # when `value` is None, it means to set the bookmark to None
+        # Assume value is epoch time
+        value_in_date_time = epoch_to_datetime_string(value)
+        if value_in_date_time is None or self.is_bookmark_old(state, value_in_date_time, name):
+            singer.write_bookmark(state, name, self.replication_key, value_in_date_time)
+
+
+    def is_bookmark_old(self, state, value, name=None):
+        # Assume value is epoch time.
+        value_in_date_time = epoch_to_datetime_string(value)
+        current_bookmark = self.get_bookmark(state, name)
+        return utils.strptime_with_tz(value_in_date_time) > utils.strptime_with_tz(current_bookmark)
+
+
+    def load_schema(self):
+        schema_file = "schemas/{}.json".format(self.name)
+        with open(get_abs_path(schema_file)) as f:
+            schema = json.load(f)
+        return schema
+
+
+    def load_metadata(self):
+        return metadata.get_standard_metadata(schema=self.load_schema(),
+                                              key_properties=self.key_properties,
+                                              valid_replication_keys=[self.replication_key],
+                                              replication_method=self.replication_method)
+
+
+    # The main sync function.
+    def sync(self, state):
+        get_data = getattr(self.client, self.name)
+        bookmark = self.get_bookmark(state)
+        res = get_data(self.replication_key, bookmark)
+
+        if self.replication_method == "INCREMENTAL":
+            # These streams results are not ordered, so store highest value bookmark in session.
+            for item in res:
+                self.update_session_bookmark(item[self.replication_key])
+                yield (self.stream, item)
+            self.update_bookmark(state, self.session_bookmark)
+
+        else:
+            for item in res:
+                yield (self.stream, item)
+
+
+    def sync_data_export(self, state):
+        get_generator = getattr(self.client, "get_data_export_generator")
+        bookmark = self.get_bookmark(state)
+        fns = get_generator(self.data_type_name, bookmark)
+        for fn in fns:
+            res = fn()
+            count = 0
+            start_time = time.time()
+            with tempfile.NamedTemporaryFile() as tf:
+                for item in res.iter_lines():
+                    if item:
+                        tf.write(item)
+                        count += 1
+                        tf.write(b'\n')
+                write_time = time.time()
+                logger.info('wrote {} records to temp file in {} seconds'.format(count, int(write_time - start_time)))
+                with open(tf.name, 'r', encoding='utf-8') as tf_reader:
+                    for line in tf_reader:
+                        # json load line with line feed removed, but
+                        # sometimes the last line does not end with a line
+                        # feed, so check
+                        if line[-1] == '\n':
+                            rec = json.loads(line[:-1])
+                        else:
+                            rec = json.loads(line)
+                        try:
+                            rec["transactionalData"] = json.loads(rec["transactionalData"])
+                        except KeyError:
+                            pass
+                        self.update_session_bookmark(rec[self.replication_key])
+                        yield (self.stream, rec)
+                logger.info('Read and emitted {} records from temp file in {} seconds'.format(count, int(time.time() - write_time)))
+
+            self.update_bookmark(state, self.session_bookmark)
+            singer.write_state(state)
+
+
+class Lists(Stream):
+    name = "lists"
+    replication_method = "FULL_TABLE"
+
+
+class ListUsers(Stream):
+    name = "list_users"
+    replication_method = "FULL_TABLE"
+
+
+class Campaigns(Stream):
+    name = "campaigns"
+    replication_method = "INCREMENTAL"
+    replication_key = "updatedAt"
+
+
+class Channels(Stream):
+    name = "channels"
+    replication_method = "FULL_TABLE"
+
+
+class MessageTypes(Stream):
+    name = "message_types"
+    replication_method = "FULL_TABLE"
+
+
+class Templates(Stream):
+    name = "templates"
+    replication_method = "INCREMENTAL"
+    replication_key = "updatedAt"
+    key_properties = [ "templateId" ]
+
+
+class Metadata(Stream):
+    name = "metadata"
+    replication_method = "FULL_TABLE"
+    key_properties = [ "key" ]
+
+
+class EmailBounce(Stream):
+    name = "email_bounce"
+    replication_method = "INCREMENTAL"
+    replication_key = "createdAt"
+    key_properties = []
+    data_type_name = "emailBounce"
+
+    def sync(self, state):
+        return self.sync_data_export(state)
+
+
+class EmailClick(Stream):
+    name = "email_click"
+    replication_method = "INCREMENTAL"
+    key_properties = []
+    replication_key = "createdAt"
+    data_type_name = "emailClick"
+
+    def sync(self, state):
+        return self.sync_data_export(state)
+
+
+class EmailComplaint(Stream):
+    name = "email_complaint"
+    replication_method = "INCREMENTAL"
+    key_properties = []
+    replication_key = "createdAt"
+    data_type_name = "emailComplaint"
+
+    def sync(self, state):
+        return self.sync_data_export(state)
+
+
+class EmailOpen(Stream):
+    name = "email_open"
+    replication_method = "INCREMENTAL"
+    key_properties = []
+    replication_key = "createdAt"
+    data_type_name = "emailOpen"
+
+    def sync(self, state):
+        return self.sync_data_export(state)
+
+
+class EmailSend(Stream):
+    name = "email_send"
+    replication_method = "INCREMENTAL"
+    key_properties = []
+    replication_key = "createdAt"
+    data_type_name = "emailSend"
+
+    def sync(self, state):
+        return self.sync_data_export(state)
+
+
+class EmailSendSkip(Stream):
+    name = "email_send_skip"
+    replication_method = "INCREMENTAL"
+    key_properties = []
+    replication_key = "createdAt"
+    data_type_name = "emailSendSkip"
+
+    def sync(self, state):
+        return self.sync_data_export(state)
+
+
+class EmailSubscribe(Stream):
+    name = "email_subscribe"
+    replication_method = "INCREMENTAL"
+    key_properties = []
+    replication_key = "createdAt"
+    data_type_name = "emailSubscribe"
+
+    def sync(self, state):
+        return self.sync_data_export(state)
+
+
+class EmailUnsubscribe(Stream):
+    name = "email_unsubscribe"
+    replication_method = "INCREMENTAL"
+    key_properties = []
+    replication_key = "createdAt"
+    data_type_name = "emailUnsubscribe"
+
+    def sync(self, state):
+        return self.sync_data_export(state)
+
+
+class Users(Stream):
+    name = "users"
+    replication_method = "INCREMENTAL"
+    key_properties = []
+    replication_key = "createdAt"
+    data_type_name = "user"
+
+    def sync(self, state):
+        return self.sync_data_export(state)
+
+
+STREAMS = {
+    "lists": Lists,
+    "list_users": ListUsers,
+    "campaigns": Campaigns,
+    "channels": Channels,
+    "message_types": MessageTypes,
+    "templates": Templates,
+    "metadata": Metadata,
+    "email_bounce": EmailBounce,
+    "email_click": EmailClick,
+    "email_complaint": EmailComplaint,
+    "email_open": EmailOpen,
+    "email_send": EmailSend,
+    "email_send_skip": EmailSendSkip,
+    "email_subscribe": EmailSubscribe,
+    "email_unsubscribe": EmailUnsubscribe,
+    "users": Users
+}
