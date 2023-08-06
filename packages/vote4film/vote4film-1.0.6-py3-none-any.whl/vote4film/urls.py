5009afb1036d65a1a7cdad61727aa6c8d9b042b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:00 +0000

    Add package vote4film/1.0.6/vote4film-1.0.6-py3-none-any.whl

---
+++ packages/vote4film/vote4film-1.0.6-py3-none-any.whl/vote4film/urls.py
@@ -0,0 +1,40 @@
+"""vote4film URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
+
+import calender.urls
+import films.urls
+import schedule.urls
+import votes.urls
+import web.urls
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("calender/", include((calender.urls.urlpatterns, calender.urls.app_name))),
+    path("films/", include((films.urls.urlpatterns, films.urls.app_name))),
+    path("schedule/", include((schedule.urls.urlpatterns, schedule.urls.app_name))),
+    path("votes/", include((votes.urls.urlpatterns, votes.urls.app_name))),
+    path("", include((web.urls.urlpatterns, web.urls.app_name))),
+]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
