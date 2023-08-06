f7bafd38cca013417e2c037b9a625ee9e4de88cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:04 +0000

    Add package generateGUID/1.0/generateGUID-1.0-py3-none-any.whl

---
+++ packages/generateGUID/generateGUID-1.0-py3-none-any.whl/generate_GUID/generate_GUID.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from strip_accents import text_to_id
+import hashlib
+import re
+import datetime
+
+# flake8: noqa: C901
+def generate_GUID2(nom, prenom, age, sexe):
+
+    if '-' in age:
+        if re.match(r"^[0-9]{2}\D", age):
+            date_time_str = str(age)
+            date_time_obj = datetime.datetime.strptime(date_time_str, r'%d-%m-%Y').strftime(r'%d-%m-%Y')
+            key = nom + prenom + date_time_obj + sexe
+
+            keygood = text_to_id(key)
+
+            GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+
+            return GUID
+
+        if re.match(r"^[0-9]{4}\D", age):
+            date_time_str = str(age)
+            date_time_obj = datetime.datetime.strptime(date_time_str,
+            r'%Y-%m-%d').strftime(r'%d-%m-%Y')
+            key = nom + prenom + date_time_obj + sexe
+            keygood = text_to_id(key)
+            GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+            return GUID
+
+    if '/' in age:
+        if re.match(r"^[0-9]{2}\D", age):
+            date_time_str = str(age)
+            date_time_obj = datetime.datetime.strptime(date_time_str, r'%d/%m/%Y').strftime(r'%d/%m/%Y')
+
+            key = nom + prenom + date_time_obj + sexe
+
+            keygood = text_to_id(key)
+            GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+
+            return GUID
+
+        if re.match(r"^[0-9]{4}\D", age):
+            date_time_str = str(age)
+            date_time_obj = datetime.datetime.strptime(date_time_str, r'%Y/%m/%d').strftime(r'%d/%m/%Y')
+
+            key = nom + prenom + date_time_obj + sexe
+
+            keygood = text_to_id(key)
+            GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+
+            return GUID
+
+    if r"\\" in age:
+        if re.match(r"^[0-9]{2}\D", age):
+            date_time_str = str(age)
+            date_time_obj = datetime.datetime.strptime(date_time_str, r'%d\%m\%d').strftime(r'%d\%m\%Y')
+
+            key = nom + prenom + date_time_obj + sexe
+
+            keygood = text_to_id(key)
+            GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+
+            return GUID
+
+        if re.match(r"^[0-9]{4}\D", age):
+            date_time_str = str(age)
+            date_time_obj = datetime.datetime.strptime(date_time_str, r'%Y\%m\%d').strftime(r'%d\%m\%Y')
+
+            key = nom + prenom + date_time_obj + sexe
+
+            keygood = text_to_id(key)
+            GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+
+            return GUID
+
+    if ' ' in age:
+        if re.match(r"^[0-9]{2}\D", age):
+            date_time_str = str(age)
+            date_time_obj = datetime.datetime.strptime(date_time_str, r'%d %m %Y').strftime(r'%d %m %Y')
+
+            key = nom + prenom + date_time_obj + sexe
+            keygood = text_to_id(key)
+            GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+
+            return GUID
+
+        if re.match(r"^[0-9]{4}\D", age):
+            date_time_str = str(age)
+            date_time_obj = datetime.datetime.strptime(date_time_str, r'%Y %m %d').strftime(r'%d %m %Y')
+
+            key = nom + prenom + date_time_obj + sexe
+            keygood = text_to_id(key)
+            GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+
+            return GUID
+    else:
+
+        key = nom + prenom + age + sexe
+        keygood = text_to_id(key)
+        GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+
+        return GUID
+
+
+def generate_GUID(keyInput):
+
+    key = keyInput
+    keygood = text_to_id(key)
+    GUID = hashlib.sha256(keygood.encode('utf-8')).hexdigest()
+    return GUID
