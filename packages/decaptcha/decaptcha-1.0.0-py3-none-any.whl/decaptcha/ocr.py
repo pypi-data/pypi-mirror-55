b0ae701de0c538c8bae7d6c0c72f36a27432b1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:35 +0000

    Add package decaptcha/1.0.0/decaptcha-1.0.0-py3-none-any.whl

---
+++ packages/decaptcha/decaptcha-1.0.0-py3-none-any.whl/decaptcha/ocr.py
@@ -0,0 +1,42 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+from tesserocr import image_to_text
+from typing import Optional
+import re
+
+
+def ocr(
+    img: "Image",
+    left: Optional[int] = 0,
+    upper: Optional[int] = 0,
+    right: Optional[int] = None,
+    lower: Optional[int] = None,
+) -> Optional[str]:
+    """Takes an image object and returns a string dump of specified region, using optical character recognition."""
+    try:
+        selection = img.crop((left, upper, right, lower))
+    except:
+        selection = img
+    stringdump = image_to_text(selection)
+    try:
+        return re.sub("[^A-Za-z ]+", "", stringdump)
+    except:
+        return stringdump
+
+
+if __name__ == "__main__":
+    """
+    cmd usage:
+    $ python ocr.py 0 0 50 50
+    """
+    from sys import argv
+    import pyscreenshot as ImageGrab
+
+    img = ImageGrab.grab()
+
+    try:
+        print(ocr(img, int(argv[1]), int(argv[2]), int(argv[3]), int(argv[4])))
+    except:
+        print(ocr(img))
