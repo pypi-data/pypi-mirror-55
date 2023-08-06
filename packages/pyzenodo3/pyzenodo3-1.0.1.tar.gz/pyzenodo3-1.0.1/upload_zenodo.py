b8127ba8dbe5f9d114284db13b7251e760ac8ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:44 +0000

    Add package pyzenodo3/1.0.1/pyzenodo3-1.0.1.tar.gz

---
+++ packages/pyzenodo3/pyzenodo3-1.0.1.tar.gz/pyzenodo3-1.0.1/upload_zenodo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+inspired by https://github.com/darvasd/upload-to-zenodo/
+"""
+from argparse import ArgumentParser, Namespace
+import pyzenodo3.upload as zup
+
+
+def cmdparse() -> Namespace:
+    p = ArgumentParser(description="Upload data to Zenodo staging")
+    p.add_argument("apikey", help="Zenodo API key", nargs="?")
+    p.add_argument("inifn", help="mymeta.ini file with author, title, etc.")
+    p.add_argument("path", help="directory or file to upload to Zenodo", nargs="?")
+    return p.parse_args()
+
+
+def main():
+    p = cmdparse()
+
+    metafn = zup.meta(p.inifn)
+    zup.upload(metafn, p.path, p.apikey)
+
+
+if __name__ == "__main__":
+    main()
