e1d55ad9d2cbe904e41ba3ace4913df0f2b62dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:55 +0000

    Add package lxmlx/2.0.2/lxmlx-2.0.2-py3-none-any.whl

---
+++ packages/lxmlx/lxmlx-2.0.2-py3-none-any.whl/lxmlx/__init__.py
@@ -0,0 +1,6 @@
+__version__      = '2.0.2'
+__description__  = 'Helpers and utilities to be used with lxml'
+__url__          = 'https://github.com/innodatalabs/lxmlx'
+__author__       = 'Mike Kroutikov'
+__author_email__ = 'mkroutikov@innodata.com'
+__keywords__     = 'lxml xml events sax'
