713c783eb541a9162236622338867bc401c78004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:34 +0000

    Add package HermesNetwork-gabriel-oliveira/0.3.dev0/HermesNetwork-gabriel_oliveira-0.3.dev0.tar.gz

---
+++ packages/HermesNetwork-gabriel-oliveira/HermesNetwork-gabriel_oliveira-0.3.dev0.tar.gz/HermesNetwork-gabriel_oliveira-0.3.dev0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.txt", "r") as fh:
+        long_description = fh.read()
+
+setuptools.setup(
+    name="HermesNetwork-gabriel_oliveira", # Replace with your own username
+    version="0.3dev",
+    author="Gabriel De Oliveira",
+    author_email="gabriel.oli01001@gmail.com",
+    description="A high speed networks system",
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    url="https://test.pypi.org/legacy/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.6',
+    )
