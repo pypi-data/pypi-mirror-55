63f2706097bf6f2b2fe0a5435c5a443ad3bc8477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:19 +0000

    Add package conference-radar/0.3.2/conference_radar-0.3.2-py3-none-any.whl

---
+++ packages/conference-radar/conference_radar-0.3.2-py3-none-any.whl/conrad/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from .__version__ import __version__
+
+
+USER_HOME = os.path.expanduser("~")
+CONRAD_HOME = os.path.join(USER_HOME, ".conrad")
+SQL_ALCHEMY_CONN = "sqlite:///{}/conrad.db".format(CONRAD_HOME)
+
+
+if not os.path.exists(CONRAD_HOME):
+    os.makedirs(CONRAD_HOME)
