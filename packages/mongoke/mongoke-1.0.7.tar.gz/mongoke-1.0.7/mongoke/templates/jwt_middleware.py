ddd42c57a2f325c6e1fdab33d222d5ea434c1be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:01 +0000

    Add package mongoke/1.0.7/mongoke-1.0.7.tar.gz

---
+++ packages/mongoke/mongoke-1.0.7.tar.gz/mongoke-1.0.7/mongoke/templates/jwt_middleware.py
@@ -0,0 +1,37 @@
+# jwt_header
+# jwt_sheme
+# jwt_required
+# jwt_secret
+# jwt_algorithms   
+jwt_middleware = '''
+import jwt
+from bson import ObjectId
+from aiohttp import web
+from ..logger import logger
+
+JWT_ALGORITHMS = ${{ repr(jwt_algorithms) }}
+
+
+async def jwt_middleware(app, handler):
+    async def middleware(request: web.Request):
+        request.jwt_payload = {}
+        jwt_token = request.headers.get(${{ repr(jwt_header) }}, '').replace(${{ repr(jwt_sheme) }}, '').strip()
+        if not jwt_token:
+            ${{
+                indent_to('            ', """
+                raise web.HTTPUnauthorized(
+                    reason='Missing authorization token',
+                )
+                """) if jwt_required else indent_to('            ', 'return await handler(request)')
+            }}
+        try:
+            payload = jwt.decode(jwt_token, verify=${{ repr(bool(jwt_required)) }}, secret=${{ repr(jwt_secret) }}, algorithms=[JWT_ALGORITHMS])
+        except (jwt.InvalidTokenError) as exc:
+            logger.exception(exc, exc_info=exc)
+            msg = 'Invalid authorization token, ' + str(exc)
+            raise web.HTTPForbidden(reason=msg)
+        else:
+            request.jwt_payload = payload
+        return await handler(request)
+    return middleware
+'''
