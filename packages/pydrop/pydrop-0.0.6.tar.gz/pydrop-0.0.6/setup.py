3e40baf6c6b0ee76b425dfc36d667a85ef1f413b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:15 +0000

    Add package pydrop/0.0.6/pydrop-0.0.6.tar.gz

---
+++ packages/pydrop/pydrop-0.0.6.tar.gz/pydrop-0.0.6/setup.py
@@ -0,0 +1,49 @@
+import sys
+import os
+import sys
+import setuptools
+from setuptools import find_packages
+from setuptools.command.test import test as TestCommand
+from distutils.version import StrictVersion
+from setuptools import __version__ as setuptools_version
+
+if StrictVersion(setuptools_version) < StrictVersion('38.3.0'):
+    raise SystemExit(
+        'Your `setuptools` version is old. '
+        'Please upgrade setuptools by running `pip install -U setuptools` '
+        'and try again.'
+    )
+def readme():
+    with open('README.md') as f:
+        return f.read()
+setuptools.setup(
+    name='pydrop',
+    version='0.0.6',
+    packages=find_packages(),
+    url='https://github.com/samapriya/pydrop',
+    install_requires=['requests>=2.19.1','arrow>=0.12.1'],
+    license='Apache 2.0',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: GIS',
+    ),
+    author='Samapriya Roy',
+    author_email='samapriya.roy@gmail.com',
+    description='Python Client for Digital Ocean Droplets',
+    entry_points={
+        'console_scripts': [
+            'pydrop=pydrop.pydrop:main',
+        ],
+    },
+)
