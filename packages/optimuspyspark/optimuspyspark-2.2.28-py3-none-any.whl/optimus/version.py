1726fc42a4ae52e95b747f6b4a58c6af9f4ce726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:43 +0000

    Add package optimuspyspark/2.2.28/optimuspyspark-2.2.28-py3-none-any.whl

---
+++ packages/optimuspyspark/optimuspyspark-2.2.28-py3-none-any.whl/optimus/version.py
@@ -0,0 +1,9 @@
+def _safe_int(string):
+    try:
+        return int(string)
+    except ValueError:
+        return string
+
+
+__version__ = '2.2.28'
+VERSION = tuple(_safe_int(x) for x in __version__.split('.'))
