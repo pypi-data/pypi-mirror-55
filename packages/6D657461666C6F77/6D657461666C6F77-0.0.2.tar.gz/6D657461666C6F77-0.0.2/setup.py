59df7b5c14ce1f8bb1701cecaa97d54b58a255a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:05 +0000

    Add package 6D657461666C6F77/0.0.2/6D657461666C6F77-0.0.2.tar.gz

---
+++ packages/6D657461666C6F77/6D657461666C6F77-0.0.2.tar.gz/6D657461666C6F77-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+version = '0.0.2'
+
+setup(name='6D657461666C6F77',
+      version=version,
+      author='Savin Goyal',
+      author_email='savin@netflix.com',
+      license='Apache License 2.0',
+      packages=find_packages())
