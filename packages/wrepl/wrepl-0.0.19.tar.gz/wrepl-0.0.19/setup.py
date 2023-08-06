dca89116b2ed72cce2beba57a51f738e6112440a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:21 +0000

    Add package wrepl/0.0.19/wrepl-0.0.19.tar.gz

---
+++ packages/wrepl/wrepl-0.0.19.tar.gz/wrepl-0.0.19/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='wrepl',
+        version="0.0.19",
+        description="Watch-Read-Eval-Print Loop",
+        url='https://github.com/octaltree/wrepl',
+        author='octaltree',
+        author_email='octalreee@gmail.com',
+        license='MIT',
+        install_requires=['watchdog', 'dill'],
+        keywords='wrepl',
+        classifiers=[
+            'Development Status :: 3 - Alpha'],
+        packages=find_packages(exclude=('tests')),
+        entry_points={'console_scripts': ['wrepl=wrepl.__init__:parse']})
