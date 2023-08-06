7be63d2e327633c6dc9b96db84395ae4e47cde05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:08 +0000

    Add package seismicSignalAnalysis/0.1.2/seismicSignalAnalysis-0.1.2.tar.gz

---
+++ packages/seismicSignalAnalysis/seismicSignalAnalysis-0.1.2.tar.gz/seismicSignalAnalysis-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name = 'seismicSignalAnalysis',
+    version = '0.1.2',
+    keywords = ('seismic', 'signal'),
+    description = 'seismic wave analysis',
+    license = 'MIT License',
+
+    author = 'Junjun Guo',
+    author_email = 'guojj_ce@163.com',
+	url='https://github.com/Junjun1guo/seismicWaveAnalysis',
+    packages = find_packages(),
+    platforms = 'any',
+)
