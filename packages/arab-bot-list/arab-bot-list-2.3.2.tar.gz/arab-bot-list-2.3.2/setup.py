b8cd8433b76354e483e8be362a3bfced59f1becc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:25 +0000

    Add package arab-bot-list/2.3.2/arab-bot-list-2.3.2.tar.gz

---
+++ packages/arab-bot-list/arab-bot-list-2.3.2.tar.gz/arab-bot-list-2.3.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name='arab-bot-list',
+    url='https://github.com',
+    author='Jake',
+    author_email='jakediserin@gmail.com',
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['requests'],
+    version='2.3.2',
+    license='MIT',
+    description='The official API package for Arab\'s bot list using Python 3',
+
+)
