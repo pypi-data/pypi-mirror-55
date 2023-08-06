195f7e8e4c893f8a90d6b63e05e9776d71f5faa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:12 +0000

    Add package qhsdk/0.0.2/qhsdk-0.0.2.tar.gz

---
+++ packages/qhsdk/qhsdk-0.0.2.tar.gz/qhsdk-0.0.2/qhsdk/__init__.py
@@ -0,0 +1,19 @@
+"""qhsdk 是基于 Python 的为奇货可查客户服务的专用API接口"""
+
+"""
+0.0.1
+发布测试版本
+
+0.0.2
+调试接口
+"""
+
+__version__ = '0.0.2'
+__author__ = 'qhkch'
+
+"""
+奇货可查-工具
+"""
+from qhsdk.qhkc.qhkc_tool import (qhkc_tool_foreign,
+                                  qhkc_tool_gdp,
+                                  qhkc_tool_nebula)
