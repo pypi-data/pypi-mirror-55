85e03a3ae6adef9f0fdad146f78c2e4d51c6793b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:24 +0000

    Add package cibuildwheel/1.0.0/cibuildwheel-1.0.0.tar.gz

---
+++ packages/cibuildwheel/cibuildwheel-1.0.0.tar.gz/cibuildwheel-1.0.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='cibuildwheel',
+    version='1.0.0',
+    install_requires=['bashlex!=0.13'],
+    description="Build Python wheels on CI with minimal configuration.",
+    long_description='For readme please see http://github.com/joerick/cibuildwheel',
+    author="Joe Rickerby",
+    author_email='joerick@mac.com',
+    url='https://github.com/joerick/cibuildwheel',
+    packages=['cibuildwheel',],
+    license="BSD",
+    zip_safe=False,
+    package_data={
+        'cibuildwheel': ['resources/*'],
+    },
+    # Supported python versions
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    keywords='ci wheel packaging pypi travis appveyor macos linux windows',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cibuildwheel = cibuildwheel.__main__:main',
+        ],
+    },
+)
