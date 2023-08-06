cfdd5e572c48eb0ba17fcbe00ead37308c9cdb5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:25 +0000

    Add package music-metadata-tools/1.0.0/music_metadata_tools-1.0.0-py2.py3-none-any.whl

---
+++ packages/music-metadata-tools/music_metadata_tools-1.0.0-py2.py3-none-any.whl/apic_tool/__init__.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+################################################################################
+#                                  apic-tool                                   #
+#       Insert cover images to and extract cover images from music files       #
+#                            (C) 2015, 2019 Mischif                            #
+#       Released under version 3.0 of the Non-Profit Open Source License       #
+################################################################################
+
+from apic_tool.workers import SUPPORTED_MUSIC
+
+
+__version__ = "2.0.0"
+
+SUPPORTED_IMAGES = ["gif", "jpg", "jpeg", "png"]
---
+++ packages/music-metadata-tools/music_metadata_tools-1.0.0-py2.py3-none-any.whl/id3autosort/__init__.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+################################################################################
+#                                 id3autosort                                  #
+#  A collection of tools for manipulating and interacting with music metadata  #
+#                     (C) 2009-10, 2015, 2019 Jeremy Brown                     #
+#       Released under version 3.0 of the Non-Profit Open Source License       #
+################################################################################
+
+__version__ = '3.0.2'
