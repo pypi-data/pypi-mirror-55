735998a3b2a1567048373520021b7def574b257f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:20 +0000

    Add package zeb/1.0/zeb-1.0.tar.gz

---
+++ packages/zeb/zeb-1.0.tar.gz/zeb-1.0/setup.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='zeb',
+    version='1.0',
+    description='这是本地发布的第一个版本',
+    author='zeb',
+    author_email='2353059090@qq.com',
+    py_modules=['zeb.demo1','zeb.demo2']
+)
