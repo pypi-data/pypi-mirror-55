eaed7cc105c55706dc42da0442f031ceb0608d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:15 +0000

    Add package dictances/1.5.2/dictances-1.5.2.tar.gz

---
+++ packages/dictances/dictances-1.5.2.tar.gz/dictances-1.5.2/dictances/kullback_leibler.py
@@ -0,0 +1,34 @@
+"""Determine the Kullback Leibler divergence beetween the given dictionaries."""
+from math import log, inf
+from typing import Dict
+from .distances_utils import sort
+
+
+def kullback_leibler(a: Dict, b: Dict) -> float:
+    """Determine the Kullback Leibler divergence beetween the given dictionaries.
+
+    Parameters
+    ----------------------------
+    a: Dict,
+        First dictionary to consider.
+    b: Dict,
+        Second dictionary to consider.
+
+    Returns
+    ----------------------------
+    Return the Kullback Leibler divergence beetween the given dictionaries.
+    """
+    total = 0
+    overlap = 0
+    big, small = sort(a, b)
+    big_get = big.__getitem__
+    for key, small_value in small.items():
+        try:
+            total += small_value * log(small_value / big_get(key))
+            overlap += 1
+        except KeyError:
+            pass
+
+    if overlap == 0 and (len(a) != 0 or len(b) != 0):
+        return inf
+    return total
