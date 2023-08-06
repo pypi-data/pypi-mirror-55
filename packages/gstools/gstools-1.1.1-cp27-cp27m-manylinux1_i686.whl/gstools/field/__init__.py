528002ad4c411dd867a0dc70f1ff9cf1ab2d8ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:08 +0000

    Add package gstools/1.1.1/gstools-1.1.1-cp27-cp27m-manylinux1_i686.whl

---
+++ packages/gstools/gstools-1.1.1-cp27-cp27m-manylinux1_i686.whl/gstools/field/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+GStools subpackage providing tools for spatial random fields.
+
+.. currentmodule:: gstools.field
+
+Subpackages
+^^^^^^^^^^^
+
+.. autosummary::
+    generator
+    upscaling
+    base
+
+Spatial Random Field
+^^^^^^^^^^^^^^^^^^^^
+
+.. autosummary::
+   SRF
+
+----
+"""
+from __future__ import absolute_import
+
+from gstools.field.srf import SRF
+
+__all__ = ["SRF"]
