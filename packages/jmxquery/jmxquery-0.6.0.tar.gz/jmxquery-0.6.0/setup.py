23cc6f633f41c87005ab197e7f47e647285b669b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:14 +0000

    Add package jmxquery/0.6.0/jmxquery-0.6.0.tar.gz

---
+++ packages/jmxquery/jmxquery-0.6.0.tar.gz/jmxquery-0.6.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+"""
+  Refer to http://peterdowns.com/posts/first-time-with-pypi.html for instructions on how to 
+  push new versions of this module to PyPI. Use the following command to push new versions of the module:
+  
+  python3 setup.py sdist upload -r pypi
+  
+  Make sure you update the MANIFEST.in file if you change the name of the JAR file so its included in the 
+  module when submitted
+"""
+
+# Get the long description from the README file
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'jmxquery',
+  packages = ['jmxquery'],
+  version = '0.6.0',
+  description = 'A JMX Interface for Python to Query runtime metrics in a JVM',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'David Gildeh',
+  author_email = 'david.gildeh@outlyer.com',
+  url = 'https://github.com/dgildeh/jmxquery',
+  keywords = ['java', 'jmx', 'metrics', 'monitoring'],
+  classifiers = [],
+  include_package_data=True,
+  zip_safe = False
+)
