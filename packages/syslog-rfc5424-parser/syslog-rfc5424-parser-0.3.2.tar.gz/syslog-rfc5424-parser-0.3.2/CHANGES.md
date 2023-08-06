2464eec8ea3ab199c8d35f4756f9f76a0780a002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:09 +0000

    Add package syslog-rfc5424-parser/0.3.2/syslog-rfc5424-parser-0.3.2.tar.gz

---
+++ packages/syslog-rfc5424-parser/syslog-rfc5424-parser-0.3.2.tar.gz/syslog-rfc5424-parser-0.3.2/CHANGES.md
@@ -0,0 +1,32 @@
+0.3.2
+----
+- Fix `DeprecationWarning` (thanks again to @pvinci)
+- Drop Python 3.3 from officially-supported list of interpreters
+- Add Python 3.7 to interpreter list
+
+0.3.1
+-----
+- Only install `enum34` as a dependency on older version of Python (thanks Github user @pvinci)
+
+0.3.0
+-----
+- Switch from PyParsing to Lark for an almost 3x speedup
+- No changes to user-facing API
+- Add documentation via ReadTheDocs
+
+0.2.0
+-----
+- Allow message bodies to contain newlines. If you want to split on newlines, do it yourself up-front. Reported by
+  GitHub user @tfogwill
+
+0.1.6
+-----
+- Require `pyparsing` 2.3 or above
+
+0.1.5
+-----
+- Pin `pyparsing` to less than version 2.3 until I make this work with the new API for grouping (reported by Github user @tfogwill)
+
+0.1.4
+-----
+- Properly handle messages with SD pairs that have an empty value (reported by Github user @eyalleshem)
---
+++ packages/syslog-rfc5424-parser/syslog-rfc5424-parser-0.3.2.tar.gz/syslog-rfc5424-parser-0.3.2/setup.py
@@ -0,0 +1,41 @@
+import sys
+
+from setuptools import setup, find_packages
+
+
+def install_requires():
+    if sys.version_info >= (3, 4):
+        return ['lark-parser==0.6.*']
+    else:
+        return ['lark-parser==0.6.*', 'enum34']
+
+setup(
+    name="syslog-rfc5424-parser",
+    version="0.3.2",
+    author="James Brown",
+    author_email="jbrown@easypost.com",
+    url="https://github.com/easypost/syslog-rfc5424-parser",
+    description="Parser for RFC5424-compatible Syslog messages",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    license="ISC",
+    install_requires=install_requires(),
+    project_urls={
+        'Issue Tracker': 'https://github.com/easypost/syslog-rfc5424-parser/issues',
+        'Documentations': 'https://syslog-rfc5424-parser.readthedocs.io/en/latest/',
+    },
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: ISC License (ISCL)",
+    ]
+)
