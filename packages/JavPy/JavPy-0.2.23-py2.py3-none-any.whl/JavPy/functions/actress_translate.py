c666825a08270dd16a0ed70be5a2f926314017aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:51 +0000

    Add package JavPy/0.2.23/JavPy-0.2.23-py2.py3-none-any.whl

---
+++ packages/JavPy/JavPy-0.2.23-py2.py3-none-any.whl/JavPy/functions/actress_translate.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+from JavPy.sources.javmodel_com import JavModelCom
+from JavPy.sources.warashi_asian_pornstars_fr import WarashiAsianPornStarsFr
+from JavPy.utils.requester import spawn_many, Task
+from future.builtins import filter
+from JavPy.utils.common import cache
+
+
+class ActressTranslate:
+    sources_en2jp = [JavModelCom, WarashiAsianPornStarsFr]
+
+    @staticmethod
+    @cache
+    def translate2jp(actress):
+        res = list(
+            filter(
+                lambda x: x,
+                spawn_many(
+                    [
+                        Task(source.translate2jp, actress)
+                        for source in ActressTranslate.sources_en2jp
+                    ]
+                ).wait_for_one_finished(),
+            )
+        )
+        if not res:
+            return None
+        else:
+            return res[0]
+
+
+if __name__ == "__main__":
+    print(ActressTranslate.translate2jp("Eimi Fukada"))
