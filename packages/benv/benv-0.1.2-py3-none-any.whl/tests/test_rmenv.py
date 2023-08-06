ceebf2b0df5040bfcd09a972fe686b95b74ddfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:20 +0000

    Add package benv/0.1.2/benv-0.1.2-py3-none-any.whl

---
+++ packages/benv/benv-0.1.2-py3-none-any.whl/tests/test_rmenv.py
@@ -0,0 +1,13 @@
+"""Test rmenv functionality"""
+
+from click.testing import CliRunner
+
+from benv.cli import rmenv
+
+
+def test_rmenv():
+    """Test rmenv"""
+    runner = CliRunner()
+    result = runner.invoke(rmenv, ['dummy'])
+    assert result.exit_code == 0
+    assert result.output == "Removed environment dummy\n"
