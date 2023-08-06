543241241f0b6287153ac0348485c1f8f55ce565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:07 +0000

    Add package txdatadog/1.3.0/txdatadog-1.3.0-py2.py3-none-any.whl

---
+++ packages/txdatadog/txdatadog-1.3.0-py2.py3-none-any.whl/txdatadog/tests/strategies.py
@@ -0,0 +1,37 @@
+import string
+
+from hypothesis import strategies
+
+from txdatadog import metrics
+
+
+metric_name = strategies.text(
+    string.ascii_letters + string.digits + ".",
+).map(lambda name: name.encode("ascii"))
+
+metric_without_value = strategies.one_of(
+    strategies.builds(Metric, name=metric_name) for Metric in [
+        metrics.Counter,
+        metrics.Gauge,
+        metrics.Histogram,
+        metrics.Set,
+        metrics.Timer,
+    ]
+)
+tag = strategies.tuples(
+    strategies.text(string.ascii_letters + string.digits + ".").map(
+        lambda name: name.encode("utf-8"),
+    ),
+    strategies.text(string.ascii_letters + string.digits + ".").map(
+        lambda name: name.encode("utf-8"),
+    ),
+)
+value = strategies.floats(allow_nan=False, allow_infinity=False)
+
+
+@strategies.composite
+def metric(draw):
+    tags = draw(strategies.sets(tag))
+    return draw(metric_without_value).with_value(
+        value=draw(value),
+    ).with_tags(*tags)
