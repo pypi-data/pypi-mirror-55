cc625950ad7aeef1a802f71bfd2c83dd728e9729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:33 +0000

    Add package pangeamt-tea/0.0.12/pangeamt-tea-0.0.12.tar.gz

---
+++ packages/pangeamt-tea/pangeamt-tea-0.0.12.tar.gz/pangeamt-tea-0.0.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import sys
+
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError:
+    pass
+
+setup(
+    name='pangeamt-tea',
+    setup_requires="setupmeta",
+    python_requires='>=3.7',
+    author='Pangeamt',
+    entry_points={"console_scripts": ["tea=pangeamt_tea.main:main"]},
+)
