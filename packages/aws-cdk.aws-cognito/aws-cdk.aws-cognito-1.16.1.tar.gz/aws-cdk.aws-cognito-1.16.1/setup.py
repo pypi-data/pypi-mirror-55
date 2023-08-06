68e483c20b9ea6761a37b42ed41ec06f3835be1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:17 +0000

    Add package aws-cdk.aws-cognito/1.16.1/aws-cdk.aws-cognito-1.16.1.tar.gz

---
+++ packages/aws-cdk.aws-cognito/aws-cdk.aws-cognito-1.16.1.tar.gz/aws-cdk.aws-cognito-1.16.1/setup.py
@@ -0,0 +1,45 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-cognito",
+    "version": "1.16.1",
+    "description": "The CDK Construct Library for AWS::Cognito",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_cognito",
+        "aws_cdk.aws_cognito._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_cognito._jsii": [
+            "aws-cognito@1.16.1.jsii.tgz"
+        ],
+        "aws_cdk.aws_cognito": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.3",
+        "publication>=0.0.3",
+        "aws-cdk.aws-iam~=1.16,>=1.16.1",
+        "aws-cdk.aws-lambda~=1.16,>=1.16.1",
+        "aws-cdk.core~=1.16,>=1.16.1"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
