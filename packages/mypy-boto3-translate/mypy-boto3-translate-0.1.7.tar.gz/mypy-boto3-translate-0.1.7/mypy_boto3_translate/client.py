ca673724f847d001fb0adbd5073f463e80f67d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:59 +0000

    Add package mypy-boto3-translate/0.1.7/mypy-boto3-translate-0.1.7.tar.gz

---
+++ packages/mypy-boto3-translate/mypy-boto3-translate-0.1.7.tar.gz/mypy-boto3-translate-0.1.7/mypy_boto3_translate/client.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+
+from botocore.client import BaseClient
+from botocore.paginate import Paginator
+from botocore.waiter import Waiter
+
+
+class Client(BaseClient):
+
+    # pylint: disable=arguments-differ
+    def can_paginate(self, operation_name: str = None) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete_terminology(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def generate_presigned_url(
+        self,
+        ClientMethod: str = None,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = None,
+        HttpMethod: str = None,
+    ) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_paginator(self, operation_name: str = None) -> Paginator:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_terminology(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_waiter(self, waiter_name: str = None) -> Waiter:
+        pass
+
+    # pylint: disable=arguments-differ
+    def import_terminology(
+        self,
+        Name: str,
+        MergeStrategy: str,
+        TerminologyData: Dict[str, Any],
+        Description: str = None,
+        EncryptionKey: Dict[str, Any] = None,
+    ) -> Dict[str, Any]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def list_terminologies(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def translate_text(self) -> None:
+        pass
