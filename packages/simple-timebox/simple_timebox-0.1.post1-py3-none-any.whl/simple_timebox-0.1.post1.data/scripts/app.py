18a084e6337fd9788a97a6e60c2fa417e9cd0f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:53 +0000

    Add package simple-timebox/0.1.post1/simple_timebox-0.1.post1-py3-none-any.whl

---
+++ packages/simple-timebox/simple_timebox-0.1.post1-py3-none-any.whl/simple_timebox-0.1.post1.data/scripts/app.py
@@ -0,0 +1,69 @@
+from time import sleep
+import click
+
+from timebox.lib.hue import *
+from timebox.lib.sound import sound
+from timebox.lib.support import plural
+
+
+@click.command()
+@click.option(
+    "--time", "-t", default=45, help="Duration of the time box in minutes",
+    show_default=True
+)
+@click.option(
+    "--beeps",
+    "-b",
+    default=3,
+    help="Numbers of beeps to notify that the session is finished",
+    show_default=True
+)
+@click.option(
+    "--before_end",
+    "-be",
+    default=15,
+    help="Notify with an alert X minutes before the session ends.",
+    show_default=True
+)
+@click.option(
+    "--beeps_in_session",
+    "-bis",
+    default=1,
+    help="Number of beeps to notify you that there is X minutes left of session",
+    show_default=True
+)
+@click.option(
+    "--alert-midway/--no-alert-midway",
+    default=False
+)
+def main(time, beeps, beeps_in_session, before_end, alert_midway):
+    time = abs(time)
+    if alert_midway & (time > before_end):
+        alert = time - before_end
+        click.echo(
+            info(
+                f"You will be notified after {orange(alert)} {plural('minute', alert)}, and when the session ends after {orange(time)} {plural('minute', time)}"
+            )
+        )
+        sleep(alert * 60)
+        sound(beeps=beeps_in_session, seconds=1.15, seconds_between_beep=0)
+        click.echo(
+            run(
+                f"It's {orange(before_end)} {plural('minute', before_end)} left of session"
+            )
+        )
+        sleep((before_end * 60) - (beeps_in_session * 1.5))
+        sound(beeps=beeps)
+    else:
+        click.echo(
+            info(
+                f"You will be notified when your session ends after {orange(time)} {plural('minute', time)}"
+            )
+        )
+        sleep(time * 60)
+        sound(beeps=beeps)
+    click.echo(good("Hope your had an awesome session!"))
+
+
+if __name__ == "__main__":
+    main()
