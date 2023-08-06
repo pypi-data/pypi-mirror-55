58ca519ed8e49b83e00ace70b57b7b28fb4cd821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:24 +0000

    Add package bdata/4.3.3/bdata-4.3.3.tar.gz

---
+++ packages/bdata/bdata-4.3.3.tar.gz/bdata-4.3.3/bdata/__init__.py
@@ -0,0 +1,16 @@
+from bdata.bdata import bdata
+from bdata.bdata import bdict
+from bdata.bdata import bhist
+from bdata.bdata import bscaler
+from bdata.bdata import bvar
+from bdata.bdata import bcontainer
+from bdata.bdata import life
+from bdata.bjoined import bjoined
+from bdata import mudpy
+
+import os
+
+__all__ = ['bdata','mudpy','bjoined']
+__version__ = '4.3.3'
+__author__ = 'Derek Fujimoto'
+_mud_data = os.path.join(os.environ['HOME'],'.bdata')
