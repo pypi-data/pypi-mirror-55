3648841a9fab97393eb3a562a9c87ec7578928b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:55 +0000

    Add package mypy-boto3-elastictranscoder/0.1.8/mypy_boto3_elastictranscoder-0.1.8-py3-none-any.whl

---
+++ packages/mypy-boto3-elastictranscoder/mypy_boto3_elastictranscoder-0.1.8-py3-none-any.whl/mypy_boto3_elastictranscoder/waiter.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+
+from botocore.waiter import Waiter
+
+
+class JobComplete(Waiter):
+    # pylint: disable=arguments-differ
+    def wait(self, Id: str, WaiterConfig: Dict[str, Any] = None) -> None:
+        pass
