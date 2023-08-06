f837974342d5c7ca50524c722461da2bf74e15fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:55 +0000

    Add package cyclist/0.0.5/cyclist-0.0.5.tar.gz

---
+++ packages/cyclist/cyclist-0.0.5.tar.gz/cyclist-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cyclist",
+    version="0.0.5",
+    author="Ben Andrews",
+    author_email="mortimermcmiresa@gmail.com",
+    description="Cycle-based data analysis toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MortimerMcMire/cyclist",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	install_requires=[
+	'pandas>=0.11.0'
+	],
+)
