640248293a3021ffb82549a326bd4c7d92b17033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:17 +0000

    Add package sinnia-shared/0.1.1/sinnia_shared-0.1.1.tar.gz

---
+++ packages/sinnia-shared/sinnia_shared-0.1.1.tar.gz/sinnia_shared-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='sinnia_shared',
+      version='0.1.1',
+      description='Sinnia Utilities',
+      url='https://github.com/sinnia/scripts/tree/develop/shared',
+      author='Sinnia',
+      author_email='sonia.segura@sinnia.com',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+      install_requires=[
+        'pymysql',
+        'pyyaml'
+      ],
+      zip_safe=False)
