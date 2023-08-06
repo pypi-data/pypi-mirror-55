f36160927c8cc463ebb6dae4ee64a1d98bc2796c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:54 +0000

    Add package waldur-mastermind/4.1.3/waldur_mastermind-4.1.3-py3-none-any.whl

---
+++ packages/waldur-mastermind/waldur_mastermind-4.1.3-py3-none-any.whl/waldur_rancher/extension.py
@@ -0,0 +1,24 @@
+from waldur_core.core import WaldurExtension
+
+
+class RancherExtension(WaldurExtension):
+
+    class Settings:
+        WALDUR_RANCHER = {
+            'RANCHER_NODE_CLOUD_INIT_TEMPLATE': '#cloud-config\n'
+                                                'packages: \n'
+                                                '  - curl\n'
+                                                'runcmd:\n'
+                                                '  - curl -fsSL '
+                                                'https://get.docker.com -o get-docker.sh; sh get-docker.sh\n'
+                                                '  - [ sh, -c, "{command}" ]\n'
+        }
+
+    @staticmethod
+    def django_app():
+        return 'waldur_rancher'
+
+    @staticmethod
+    def rest_urls():
+        from .urls import register_in
+        return register_in
