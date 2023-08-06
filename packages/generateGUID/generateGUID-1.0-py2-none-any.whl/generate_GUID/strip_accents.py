1a046916fb6049ac93ed4d45bc020fc50e6397ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:04 +0000

    Add package generateGUID/1.0/generateGUID-1.0-py2-none-any.whl

---
+++ packages/generateGUID/generateGUID-1.0-py2-none-any.whl/generate_GUID/strip_accents.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import unicodedata
+import re
+# Function to remove special characters and convert
+# them in lower case from typical french names
+
+
+def strip_accents(text):
+    """
+    Strip accents from input String.
+    :param text: The input string.
+    :type text: String.
+    :returns: The processed String.
+    :rtype: String.
+    """
+    try:
+        text = unicode(text, 'utf-8')
+    except (TypeError, NameError):  # unicode is a default on python 3
+        pass
+    text = unicodedata.normalize('NFD', text)
+    text = text.encode('ascii', 'ignore')
+    text = text.decode("utf-8")
+    return str(text)
+
+
+def text_to_id(text):
+    """
+    Convert input text to id.
+    :param text: The input string.
+    :type text: String.
+    :returns: The processed String.
+    :rtype: String.
+    """
+
+    text = strip_accents(text)
+    text = re.sub('[ ]+', '_', text)
+    text = re.sub('[^0-9a-zA-Z_-]', '', text)
+    text = re.sub('-', '', text)
+    text = re.sub('_', '', text)
+    text = text.replace(r'\/', '')
+    text = text.replace(r'\\', ' ')
+    return text.upper()
