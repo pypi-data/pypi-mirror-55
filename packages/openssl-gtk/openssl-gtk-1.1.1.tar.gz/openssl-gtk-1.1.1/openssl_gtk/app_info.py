0095c31e46b7e07c69324ed2f287565291e34732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:19 +0000

    Add package openssl-gtk/1.1.1/openssl-gtk-1.1.1.tar.gz

---
+++ packages/openssl-gtk/openssl-gtk-1.1.1.tar.gz/openssl-gtk-1.1.1/openssl_gtk/app_info.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Bu dosya uygulama hakkında bilgi tutan değişkenleri içerir.
+"""
+import os
+
+__author__ = "Muhammed Çamsarı"
+__appname__ = "OpenSSL-GTK"
+__definition__ = "Terminal kullanmadan Openssl işlemlerinizi gerçekleştirin."
+__copyright__ = "Copyright (c) 2019 " + __author__
+__license__ = "MIT"
+__version__ = "1.1.1"
+__status__ = "Beta"
+__email__ = "muhammedcamsari@icloud.com"
+__pgp__ = 'F294 1D36 A8C8 101B EEB0  16A7 B260 DBA5 2DAA 962A'
+__legalnote__ = __appname__ + ' hiçbir garanti vermez.\nKullanım amacı tamamen kullanıcının sorumluluğundadır. \nDaha fazla bilgi için ' + __license__ + ' lisansını inceleyiniz.'
+__logo__ = os.path.dirname(__file__) + '/logo.png'
+
+cipher_list = ['aes-128-cbc', 'aes-128-ecb', 'aes-192-cbc', 'aes-192-ecb', 'aes-256-cbc (Önerilen)', 'aes-256-ecb']
+digest_list = ['md5', 'md_gost94', 'ripemd160', 'sha1', 'sha224', 'sha256 (Önerilen)', 'sha384', 'sha512', 'streebog256', 'streebog512', 'whirlpool']
+
+genrsa_list = ['aes128', 'aes192', 'aes256', 'aria128', 'aria192', 'aria256', 'camellia128', 'camellia192', 'camellia256', 'des', 'des3', 'idea']
+genrsa_size_list = ['1024', '2048', '3072', '4096']
+
+surum_notlari = """
+  1.1.1 sürümü, aşağıdaki değişiklikleri barındırır.
+
+ * opy-logger modulüyle ilgili düzenlemeler yapıldı.
+
+
+   1.1 sürümü, aşağıdaki değişiklikleri barındırır.
+ * GenRSA modeli eklendi.
+ * GenRSA modeli ile açık anahtarı dışa aktarabilme özelliği eklendi.
+ * Log yönetimi için Opy-Logger entegrasyonu tamamlandı.
+ * Hakkımızda ve sürüm notları penceresindeki görüntü sorunu giderildi.
+ * Dosya seçme penceresindeki PyGTKDeprecationWarning hatası giderildi.
+ * Hata mesajları penceresindeki PyGTKDeprecationWarning hataları giderildi.
+ * Sürüm notları penceresindeki başlık metnine durum bilgisi eklendi.
+ * Hakkında penceresindeki yasal uyarı metninde düzenlemeler yapıldı.
+
+
+
+OpenSSL-GTK hala geliştirme aşamasında ve hatayla karşılaşma olasılığınız yüksek.
+Lütfen karşılaştığınız hataları geliştiriye bildirin: muhammedcamsari@icloud.com
+"""
---
+++ packages/openssl-gtk/openssl-gtk-1.1.1.tar.gz/openssl-gtk-1.1.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openssl-gtk",
+    version="1.1.1",
+    author="Muhammed Çamsarı",
+    license='MIT',
+    author_email="Muhammedcamsari@icloud.com",
+    description="Perform your Openssl operations without using a terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/muhammedcamsari/openssl-gtk",
+    keywords=['openssl', 'openssl-gui', 'security', 'encrypt'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Desktop Environment :: Gnome",
+        "Natural Language :: Turkish",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    scripts=["openssl-gui.py"],
+    packages = ['openssl_gtk'],
+    install_requires = ['opy-logger>=2.0'],
+
+)
