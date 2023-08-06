f3ce20960532ea096519194e8e6d6f59e800a9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:40 +0000

    Add package fetchai-ledger-api/0.8.2/fetchai-ledger-api-0.8.2.tar.gz

---
+++ packages/fetchai-ledger-api/fetchai-ledger-api-0.8.2.tar.gz/fetchai-ledger-api-0.8.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fetchai-ledger-api',
+    version='0.8.2',
+    description='Tools and utilities for interacting with the ledger on the fetch network',
+    url='https://github.com/fetchai/ledger-api-py',
+    author='Edward FitzGerald',
+    author_email='edward.fitzgerald@fetch.ai',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'scripts']),
+    install_requires=['requests', 'ecdsa', 'msgpack', 'base58'],
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage', 'pytest'],
+    }
+)
