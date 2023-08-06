408dcc4f825598f5253d850ccc623af9c3b236ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:26 +0000

    Add package pymysql-pooling/1.0.6/pymysql-pooling-1.0.6.tar.gz

---
+++ packages/pymysql-pooling/pymysql-pooling-1.0.6.tar.gz/pymysql-pooling-1.0.6/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymysql-pooling",
+    version="1.0.6",
+    description="pymysql-based database connection pool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prprprus/PyMySQLPool",
+    packages=setuptools.find_packages(),
+    install_requires=["PyMySQL"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
