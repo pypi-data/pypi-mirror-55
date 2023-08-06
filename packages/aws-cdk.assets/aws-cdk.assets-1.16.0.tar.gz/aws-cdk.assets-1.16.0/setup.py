509a842702305d078c5d65b8db8f18f637dc1874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:10 +0000

    Add package aws-cdk.assets/1.16.0/aws-cdk.assets-1.16.0.tar.gz

---
+++ packages/aws-cdk.assets/aws-cdk.assets-1.16.0.tar.gz/aws-cdk.assets-1.16.0/setup.py
@@ -0,0 +1,44 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.assets",
+    "version": "1.16.0",
+    "description": "Integration of CDK apps with local assets",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.assets",
+        "aws_cdk.assets._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.assets._jsii": [
+            "assets@1.16.0.jsii.tgz"
+        ],
+        "aws_cdk.assets": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.2",
+        "publication>=0.0.3",
+        "aws-cdk.core~=1.16,>=1.16.0",
+        "aws-cdk.cx-api~=1.16,>=1.16.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
