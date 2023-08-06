01aaea324195f5ae2e0c7d57f4a59e9196ba939d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:13 +0000

    Add package adhesive/0.11.2/adhesive-0.11.2-py3-none-any.whl

---
+++ packages/adhesive/adhesive-0.11.2-py3-none-any.whl/adhesive/model/ActiveEvent.py
@@ -0,0 +1,97 @@
+import uuid
+from typing import Optional
+
+from adhesive.execution import token_utils
+from adhesive.graph.ProcessTask import ProcessTask
+from adhesive.model.ActiveEventStateMachine import ActiveEventStateMachine
+from adhesive.model.ActiveLoopType import ActiveLoopType
+
+
+class ActiveEvent:
+    """
+    An event that passes through the system. It can fork
+    in case there are multiple executions going down.
+    """
+    def __init__(self,
+                 execution_id: str,
+                 parent_id: Optional['str'],
+                 context: 'ExecutionToken') -> None:
+        self.execution_id = execution_id
+        self.token_id: str = str(uuid.uuid4())
+        self.parent_id = parent_id
+
+        if not isinstance(context, ExecutionToken):
+            raise Exception(f"Not an execution token: {context}")
+
+        self._task = context.task
+        self.context = context
+
+        self.state = ActiveEventStateMachine()
+        self.state.active_event = self
+
+        self.future = None
+
+        self.loop_type: Optional[ActiveLoopType] = None
+
+    def __getstate__(self):
+        return {
+            "execution_id": self.execution_id,
+            "token_id": self.token_id,
+            "parent_id": self.parent_id,
+            "_task": self._task,
+            "context": self.context
+        }
+
+    def clone(self,
+              task: ProcessTask,
+              parent_id: str) -> 'ActiveEvent':
+        """
+        Clone the current event for another task id target.
+        """
+        result = ActiveEvent(
+            execution_id=self.execution_id,
+            parent_id=parent_id,  # FIXME: why, if this is a clone
+            context=self.context.clone(task)
+        )
+        result.context.token_id = result.token_id
+
+        # if we are exiting the current loop, we need to switch to the
+        # parent loop.
+        # FIXME: this probably doesn't belong here
+        # FIXME: the self.task != task is probably wrong, since we want to support
+        # boolean looping expressions.
+        if hasattr(self.context.task, "loop")\
+                and self.context.task.loop \
+                and self.context.loop \
+                and self.context.loop.task == self.context.task \
+                and self.context.task != task \
+                and parent_id == self.parent_id:
+            result.context.loop = self.context.loop.parent_loop
+        else:
+            result.context.loop = self.context.loop
+
+        result.context.task_name = token_utils.parse_name(
+                result.context,
+                result.context.task.name)
+
+        return result
+
+    @property
+    def task(self) -> ProcessTask:
+        return self._task
+
+    @task.setter
+    def task(self, task: ProcessTask) -> None:
+        if not isinstance(task, ProcessTask):
+            raise Exception(f"Not a task: {task}")
+
+        self.context.task = task
+        self._task = task
+
+    def __repr__(self) -> str:
+        # the task.token_id should be the same as the self.texecutionoken_id
+        return f"ActiveEvent({self.token_id}, {self.state.state}): " \
+               f"({self.task.id}):{self.context.task_name}"
+
+
+from adhesive.execution.ExecutionToken import ExecutionToken
