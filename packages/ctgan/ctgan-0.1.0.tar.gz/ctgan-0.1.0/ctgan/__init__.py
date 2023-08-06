b94ecbd547ddc6c6452ce2506b2338ba1b30693b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:58 +0000

    Add package ctgan/0.1.0/ctgan-0.1.0.tar.gz

---
+++ packages/ctgan/ctgan-0.1.0.tar.gz/ctgan-0.1.0/ctgan/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for ctgan."""
+
+__author__ = 'MIT Data To AI Lab'
+__email__ = 'dailabmit@gmail.com'
+__version__ = '0.1.0'
+
+from ctgan.model import CTGANSynthesizer
+
+__all__ = (
+    'CTGANSynthesizer',
+)
