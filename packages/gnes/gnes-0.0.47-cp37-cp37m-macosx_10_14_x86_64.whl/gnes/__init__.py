b311681f4f538fb72f96d18d61c49116e1c8cd2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:14 +0000

    Add package gnes/0.0.47/gnes-0.0.47-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/gnes/gnes-0.0.47-cp37-cp37m-macosx_10_14_x86_64.whl/gnes/__init__.py
@@ -0,0 +1,7 @@
+# do not change this line manually
+# this is managed by git tag and updated on every release
+__version__ = '0.0.47'
+
+# do not change this line manually
+# this is managed by shell/make-proto.sh and updated on every execution
+__proto_version__ = '0.0.10'
