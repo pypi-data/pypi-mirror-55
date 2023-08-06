4cd34061899d8d3501d81e0907ffebcbef957805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:47 +0000

    Add package bert4tf/1.0.3/bert4tf-1.0.3-py3-none-any.whl

---
+++ packages/bert4tf/bert4tf-1.0.3-py3-none-any.whl/bert4tf/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+'''
+@Author  :   Xu
+ 
+@Software:   PyCharm
+ 
+@File    :   __init__.py.py
+ 
+@Time    :   2019-11-03 15:07
+ 
+@Desc    :
+ 
+'''
+
+from . import modeling
+from . import tokenization
+from . import optimization
+from . import extract_features
