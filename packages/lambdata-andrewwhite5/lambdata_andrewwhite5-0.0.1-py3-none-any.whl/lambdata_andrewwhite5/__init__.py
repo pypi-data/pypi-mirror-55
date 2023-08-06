8b1774f9cab1798d686224ac69a42323f82c3963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:37 +0000

    Add package lambdata-andrewwhite5/0.0.1/lambdata_andrewwhite5-0.0.1-py3-none-any.whl

---
+++ packages/lambdata-andrewwhite5/lambdata_andrewwhite5-0.0.1-py3-none-any.whl/lambdata_andrewwhite5/__init__.py
@@ -0,0 +1,14 @@
+"""
+lambdata - a collection of data science helper functions.
+"""
+
+# import sys
+# print(sys.builtin_module_names)
+
+# help('modules')
+
+import pandas as pd
+import numpy as np
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
