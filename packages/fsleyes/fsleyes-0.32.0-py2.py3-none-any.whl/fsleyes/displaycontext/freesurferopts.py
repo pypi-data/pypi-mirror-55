6bf6f6842f30c62ae4a15aab5fcb2136028c2f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:44 +0000

    Add package fsleyes/0.32.0/fsleyes-0.32.0-py2.py3-none-any.whl

---
+++ packages/fsleyes/fsleyes-0.32.0-py2.py3-none-any.whl/fsleyes/displaycontext/freesurferopts.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+#
+# freesurferopts.py - The FreesurferOpts class.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""This module provides the :class:`FreesurferOpts` class, which contains
+settings for displaying a :class:`.FreesurferMesh` overlay.
+"""
+
+
+import fsl.data.freesurfer  as fslfs
+import fsl.data.mghimage    as fslmgh
+import fsl.transform.affine as affine
+from . import                  meshopts
+
+
+class FreesurferOpts(meshopts.MeshOpts):
+    """The :class:`FreesurferOpts` class, which contains settings for
+    displaying a :class:`.FreesurferMesh` overlay.
+
+    Freesurfer surface vertices are defined in a coordinate system which
+    differs from the world coordinate system of the source image. This
+    class customises some behaviour of the :class:`.MeshOpts` class so
+    that this difference is taken into account.
+    """
+
+
+    def __init__(self, overlay, *args, **kwargs):
+        """Create a ``FreesurferOpts`` instance.
+
+        All arguments are passed to the :class:`.MeshOpts` constructor.
+        """
+
+        self.getProp('coordSpace').setDefault('torig', self)
+        self.coordSpace = 'torig'
+
+        # Find surface files that
+        # contain other vertex sets
+        vertFiles  = [overlay.dataSource] + \
+                     fslfs.relatedGeometryFiles(overlay.dataSource)
+
+        vdataFiles = [None] + fslfs.relatedVertexDataFiles(overlay.dataSource)
+
+        self.getProp('vertexSet') .setChoices(vertFiles,  instance=self)
+        self.getProp('vertexData').setChoices(vdataFiles, instance=self)
+
+        kwargs['useTorig'] = True
+
+        meshopts.MeshOpts.__init__(self, overlay, *args, **kwargs)
+
+
+    def getTransform(self, from_, to):
+        """Overrides :meth:`.MeshOpts.getTransform`. If the
+        :attr:`.MeshOpts.coordSpace` property is ``'torig'``, and one of
+        ``from_`` or ``to`` is ``'mesh'``, the transform is adjusted to
+        account for the difference between Freesurfer's RAS and RAStkr spaces.
+        """
+
+        ref   = self.refImage
+        xform = meshopts.MeshOpts.getTransform(self, from_, to)
+
+        if isinstance(ref, fslmgh.MGHImage) and self.coordSpace == 'torig':
+
+            surf2world = ref.surfToWorldMat
+            world2surf = ref.worldToSurfMat
+
+            if   from_ == 'mesh': xform = affine.concat(xform, surf2world)
+            elif to    == 'mesh': xform = affine.concat(world2surf, xform)
+
+        return xform
+
+
+    def transformCoords(self, coords, from_, to, **kwargs):
+        """Overrides :meth:`.MeshOpts.transformCoords`. If the
+        :attr:`.MeshOpts.coordSpace` property is ``'torig'``, and one of
+        ``from_`` or ``to`` is ``'mesh'``, the coordinate transform is
+        adjusted to take into account the difference between Freesurfer's RAS
+        and RAStkr spaces.
+        """
+
+        ref  = self.refImage
+        pre  = None
+        post = None
+
+        if isinstance(ref, fslmgh.MGHImage) and self.coordSpace == 'torig':
+            if from_ == 'mesh': pre  = ref.surfToWorldMat
+            if to    == 'mesh': post = ref.worldToSurfMat
+
+        coords = meshopts.MeshOpts.transformCoords(
+            self, coords, from_, to, pre=pre, post=post, **kwargs)
+
+        return coords
