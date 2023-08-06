9fbf72e51766ddb9c88c286cf7b7d207aa56d3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:10 +0000

    Add package hdk-pkg-cri/0.0.8/hdk-pkg-cri-0.0.8.tar.gz

---
+++ packages/hdk-pkg-cri/hdk-pkg-cri-0.0.8.tar.gz/hdk-pkg-cri-0.0.8/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+@author: He Dekun
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "hdk-pkg-cri", # Replace with your own username
+    version = "0.0.8",
+    author = "He Dekun",
+    author_email = "hede0001@e.ntu.edu.sg",
+    description = "A small example package for CRI PMT test.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/HDKidd/hdk-pkg-cri",
+    packages = setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
