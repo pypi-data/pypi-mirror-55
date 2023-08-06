4b5e09043add9c722d3a54b64d013c6e01343fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:04 +0000

    Add package distributions-WC/0.1/distributions_WC-0.1.tar.gz

---
+++ packages/distributions-WC/distributions_WC-0.1.tar.gz/distributions_WC-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_WC',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions_WC'],
+      author_email='wbchristerson@gmail.com',
+      zip_safe=False)
