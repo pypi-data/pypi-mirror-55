731b5a6f7702e587d83c80b37ab9a294c850be97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:25 +0000

    Add package staple/0.3.2/staple-0.3.2.tar.gz

---
+++ packages/staple/staple-0.3.2.tar.gz/staple-0.3.2/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'Click>=6.0',
+    'numpy',
+    'SimpleITK==1.2.0',
+    'sphinx-rtd-theme',
+]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Fernando Perez-Garcia",
+    author_email='fernando.perezgarcia.17@ucl.ac.uk',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+    ],
+    description="Implementation of the STAPLE segmentation algorithm",
+    entry_points={
+        'console_scripts': [
+            'staple=staple.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    include_package_data=True,
+    keywords='staple',
+    name='staple',
+    packages=find_packages(include=['staple']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/fepegar/staple',
+    version='0.3.2',
+    zip_safe=False,
+)
---
+++ packages/staple/staple-0.3.2.tar.gz/staple-0.3.2/staple/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for staple."""
+
+__author__ = """Fernando Perez-Garcia"""
+__email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
+__version__ = '0.3.2'
+
+from .staple import *
