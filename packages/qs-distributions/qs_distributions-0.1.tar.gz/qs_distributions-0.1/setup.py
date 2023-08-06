c2112580f6439a6873df67307d363d37636972c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:53 +0000

    Add package qs-distributions/0.1/qs_distributions-0.1.tar.gz

---
+++ packages/qs-distributions/qs_distributions-0.1.tar.gz/qs_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='qs_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['qs_distributions'],
+      author = 'Quintin Sheridan',
+      author_email = 'qsherida@alumni.nd.edu',
+      zip_safe=False)
