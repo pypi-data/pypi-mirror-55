be487c53e61621a08fc66bd063a52b550ebc4cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:32 +0000

    Add package sjhl-middleground/0.1/sjhl-middleground-0.1.tar.gz

---
+++ packages/sjhl-middleground/sjhl-middleground-0.1.tar.gz/sjhl-middleground-0.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/11 10:55
+# @Author  : DarryDai
+# @File    : setup.py
+# @Software: PyCharm
+
+from setuptools import setup
+setup(name='sjhl-middleground',
+      version='0.1',
+      description='A tech middle ground of shijuehulian company',
+      url='http://baidu.com',
+      author='DarryDai',
+      author_email='darrydai@qq.com',
+      license='MIT',
+      packages=['sjhl-middleground'],
+      zip_safe=False)
---
+++ packages/sjhl-middleground/sjhl-middleground-0.1.tar.gz/sjhl-middleground-0.1/sjhl-middleground/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/11 10:56
+# @Author  : DarryDai
+# @File    : __init__.py
+# @Software: PyCharm
+
+def start():
+    print("import successful")
