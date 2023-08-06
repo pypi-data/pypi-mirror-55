d8bb38f4f97baf85a1e5eac29951c45affe33581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:24 +0000

    Add package gis-tools/0.15.92/gis_tools-0.15.92.tar.gz

---
+++ packages/gis-tools/gis_tools-0.15.92.tar.gz/gis_tools-0.15.92/gistools/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+""" Module summary description.
+
+More detailed description.
+"""
+
+# __all__ = []
+__version__ = '0.15.92'
+__author__ = 'Benjamin Pillot'
+__copyright__ = 'Copyright 2019, Benjamin Pillot'
+__email__ = 'benjaminpillot@riseup.net'
+
+# from gistools.layer import *
+# from gistools.raster import *
+# from gistools.network import *
+# from gistools.stats import *
