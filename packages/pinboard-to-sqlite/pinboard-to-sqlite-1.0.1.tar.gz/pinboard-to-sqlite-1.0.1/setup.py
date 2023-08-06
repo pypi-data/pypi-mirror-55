912f1060308252ef84a339a0a6955ab676a402a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:45 +0000

    Add package pinboard-to-sqlite/1.0.1/pinboard-to-sqlite-1.0.1.tar.gz

---
+++ packages/pinboard-to-sqlite/pinboard-to-sqlite-1.0.1.tar.gz/pinboard-to-sqlite-1.0.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pinboard_to_sqlite']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0',
+ 'python-dateutil>=2.8.1,<3.0.0',
+ 'requests>=2.22.0,<3.0.0',
+ 'sqlite-utils>=1.12.1,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['pinboard-to-sqlite = pinboard_to_sqlite.cli:cli']}
+
+setup_kwargs = {
+    'name': 'pinboard-to-sqlite',
+    'version': '1.0.1',
+    'description': 'Save data from Pinboard to a SQLite database',
+    'long_description': None,
+    'author': 'Jacob Kaplan-Moss',
+    'author_email': 'jacob@jacobian.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
