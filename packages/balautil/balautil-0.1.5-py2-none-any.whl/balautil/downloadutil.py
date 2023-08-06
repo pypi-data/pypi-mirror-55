20c9cc5dc56d11038d88d7591e87c650e63781f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:10 +0000

    Add package balautil/0.1.5/balautil-0.1.5-py2-none-any.whl

---
+++ packages/balautil/balautil-0.1.5-py2-none-any.whl/balautil/downloadutil.py
@@ -0,0 +1,21 @@
+from .requestutil import *
+from pathlib import Path
+## Common Data Util methods
+
+def download(url, fname=None, folder=None):
+    """
+    Retirve the url content & save into file name {fname}
+    """
+    if not fname:
+        fname = url.split("/")[-1]
+    resp = GetRaw(url)
+    try:
+        if folder: 
+            p = Path(folder); p.mkdir(exist_ok=True)
+            if not str(folder).endswith("/"):
+                folder = folder + "/"
+            fname = folder + fname
+        with open(fname, 'wb') as f: f.write(resp.content)
+    except Exception as e:
+        print("something went wrong while writing.")
+        print(e)
