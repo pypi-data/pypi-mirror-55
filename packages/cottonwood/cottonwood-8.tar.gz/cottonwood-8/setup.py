2c251096f63d12bb1c451146a858c74f61908a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:19 +0000

    Add package cottonwood/8/cottonwood-8.tar.gz

---
+++ packages/cottonwood/cottonwood-8.tar.gz/cottonwood-8/setup.py
@@ -0,0 +1,34 @@
+import setuptools as st
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+st.setup(
+    name='cottonwood',
+    version='8',
+    description='A flexible machine learning framework',
+    url='http://github.com/brohrer/cottonwood',
+    download_url='https://github.com/brohrer/cottonwood/tags/',
+    author='Brandon Rohrer',
+    author_email='brohrer@gmail.com',
+    license='MIT',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={
+        "": [
+            "README.md",
+            "LICENSE",
+        ],
+        "cottonwood": [],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
