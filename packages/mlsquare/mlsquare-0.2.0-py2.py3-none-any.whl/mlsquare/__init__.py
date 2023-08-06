c12b131a3606d59d9e863f3ea1b84cc60f1a7db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:44 +0000

    Add package mlsquare/0.2.0/mlsquare-0.2.0-py2.py3-none-any.whl

---
+++ packages/mlsquare/mlsquare-0.2.0-py2.py3-none-any.whl/mlsquare/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+# __version__ = 'v0.1.1'
+__author__ = "MLSquare"
+__copyright__ = "MLSquare"
+__license__ = "mit"
+
+from .core import dope
+from .base import registry
+from .architectures import sklearn
