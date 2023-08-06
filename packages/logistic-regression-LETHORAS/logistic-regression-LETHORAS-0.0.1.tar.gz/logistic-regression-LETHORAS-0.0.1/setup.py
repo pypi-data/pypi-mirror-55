3efbb446e89c43bba67b0ff46d6f33172422ded0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:39 +0000

    Add package logistic-regression-LETHORAS/0.0.1/logistic-regression-LETHORAS-0.0.1.tar.gz

---
+++ packages/logistic-regression-LETHORAS/logistic-regression-LETHORAS-0.0.1.tar.gz/logistic-regression-LETHORAS-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="logistic-regression-LETHORAS", # Replace with your own username
+    version="0.0.1",
+    author="Sebastian Koenig",
+    author_email="sebastian@koenig-global.org",
+    description="stochastic regression",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
