60034a313c3ba649ccc88143bd7a948cb605283b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:17 +0000

    Add package doreah/1.2.3/doreah-1.2.3.tar.gz

---
+++ packages/doreah/doreah-1.2.3.tar.gz/doreah-1.2.3/doreah/__init__.py
@@ -0,0 +1,45 @@
+name = "doreah"
+version = 1,2,3
+versionstr = ".".join(str(n) for n in version)
+author = {
+	"name": "Johannes Krattenmacher",
+	"email": "python@krateng.dev",
+	"github": "krateng"
+}
+requires = [
+	"lxml",
+	"mechanicalsoup",
+	"requests",
+	"pyyaml",
+	"beautifulsoup4"
+]
+
+
+__all__ = [
+	"caching",
+	"io",
+	"logging",
+	"persistence",
+	"pyhp",
+	"regular",
+	"scraping"
+	"settings",
+	"timing",
+	"tsv"
+
+]
+
+
+
+
+# useful things for everyone
+
+
+def deprecated(func):
+	"""Function decorator to deprecate a function"""
+
+	def newfunc(*args,**kwargs):
+		print("\033[93m" + "Function " + func.__name__ + " is deprecated!" + "\033[0m")
+		return func(*args,**kwargs)
+
+	return newfunc
