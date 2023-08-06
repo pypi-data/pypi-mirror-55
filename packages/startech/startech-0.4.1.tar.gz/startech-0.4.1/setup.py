3551b2563ff3424357e3cb5227b28d437aa05a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:40 +0000

    Add package startech/0.4.1/startech-0.4.1.tar.gz

---
+++ packages/startech/startech-0.4.1.tar.gz/startech-0.4.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+pkg_version='0.4.1'
+setup(
+  name = 'startech',
+  packages = ['startech'],
+  version = pkg_version,
+  license='MIT',
+  description = 'A small library made to make common tasks easier',
+  author = 'Star Technology',
+  author_email = 'startechsheffield@gmx.com',
+  url = 'https://github.com/startechsheffield/startech',
+  download_url = 'https://github.com/startechsheffield/startech/archive/'+pkg_version+'.tar.gz',
+  keywords = ['SMALL', 'CONVENIENCE', 'RELEASE-CANDIDATE'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
