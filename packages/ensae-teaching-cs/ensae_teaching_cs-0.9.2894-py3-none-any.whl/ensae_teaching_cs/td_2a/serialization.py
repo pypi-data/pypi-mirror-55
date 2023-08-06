182a2417314f4108339b85f86ff3beb7bdfa301f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:43 +0000

    Add package ensae-teaching-cs/0.9.2894/ensae_teaching_cs-0.9.2894-py3-none-any.whl

---
+++ packages/ensae-teaching-cs/ensae_teaching_cs-0.9.2894-py3-none-any.whl/ensae_teaching_cs/td_2a/serialization.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+@file
+@brief Sérialization
+
+"""
+
+import pickle
+
+
+def df2list(df):
+    """
+    Converts a dataframe into a list of lists.
+
+    @param      df      DataFrame
+    @return             list of lists
+
+    .. faqref::
+        :tag: pandas
+        :title: Convertir un DataFrame en une liste de listes ?
+
+    ::
+
+        df = DataFrame( ... )
+        l  = df.values.tolist()
+
+    .. faqref::
+        :tag: pandas
+        :title: Comment vérifier que deux DataFrame sont égaux (2) ?
+
+        Comparer deux `DataFrame <http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html>`_
+        avec l'opérateur ``==`` ne fonctionne pas.
+        On obtient un message d'erreur ::
+
+            ValueError: The truth value of a DataFrame is ambiguous. Use a.empty, a.bool(), a.item(), a.any() or a.all().
+
+        Il faut au préalable convertir le Dataframe en le convertissant en liste ::
+
+            df.values.tolist() == df2.values.tolist()
+    """
+    return df.vales.tolist()
+
+
+def dump_object(obj, filename_or_stream):
+    """
+    Sérialize un objet dans un stream ou un fichier.
+
+    @param      obj                     objet à sérialiser
+    @param      filename_or_stream      nom de fichier ou stream
+
+    .. faqref::
+        :tag: python
+        :title: Comment gagner du temps lors de la lecture de données ?
+
+        .. index:: sérialisation, pickle, dill
+
+        Les languages informatiques définissent des structures de données
+        qui permettent une utilisation rapide et cela n'a souvent rien
+        à voir avec la façon dont on lit ces données.
+        La plupart des données apparaissent dans des fichiers
+        texte ou fichiers plat. Pour les utiliser, le programme les
+        charges en mémoires ce qui peut prendre du temps.
+        La première fois qu'on s'en sert, c'st inévitable.
+        La seconde fois, on peut stocker les données
+        telles qu'elles sont en mémoire.
+        Le second chargement est plus rapide.
+
+        ::
+
+            obj = ... # n'importe quoi de sérialisable
+            dump_object(obj, "object_sur_disque.bin")
+
+        Pour recharger les données, on écrit :
+
+        ::
+
+            obj = load_object("object_sur_disque.bin")
+
+        Le code de ces deux fonctions fait intervenir
+        le module `pickle <https://docs.python.org/3/library/pickle.html>`_.
+        Il suffit pour la plupart des usages.
+        Pour un usage plus exotique, il faut voir le module
+        `dill <https://pypi.python.org/pypi/dill>`_.
+    """
+    if isinstance(filename_or_stream, str):
+        stream = open(filename_or_stream, "wb")
+        close = True
+    else:
+        stream = filename_or_stream
+        close = False
+
+    pickle.dump(obj, stream)
+
+    if close:
+        stream.close()
+
+
+def load_object(filename_or_stream):
+    """
+    Charge un objet en mémoire après qu'il a été sérialisé.
+
+    @param      filename_or_stream      nom de fichier ou stream
+    @return                             objet
+    """
+    if isinstance(filename_or_stream, str):
+        stream = open(filename_or_stream, "rb")
+        close = True
+    else:
+        stream = filename_or_stream
+        close = False
+
+    obj = pickle.load(stream)
+
+    if close:
+        stream.close()
+
+    return obj
