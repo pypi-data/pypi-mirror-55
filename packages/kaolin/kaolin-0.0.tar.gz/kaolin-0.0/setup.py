4171879f5ae15592778ca0cb59c65bd5c34b96e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:33 +0000

    Add package kaolin/0.0/kaolin-0.0.tar.gz

---
+++ packages/kaolin/kaolin-0.0.tar.gz/kaolin-0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    # Metadata
+    name='kaolin',
+    version='0.0',
+    author='nvidia',
+    description='',
+    url='',
+    long_description='',
+    license='',
+    python_requires='>3.6',
+)
