71a488f3dc0d44777736af9a645e2f4cdff2e616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:44 +0000

    Add package oceandb-mongodb-driver/0.2.0/oceandb_mongodb_driver-0.2.0-py2.py3-none-any.whl

---
+++ packages/oceandb-mongodb-driver/oceandb_mongodb_driver-0.2.0-py2.py3-none-any.whl/oceandb_mongodb_driver/indexes.py
@@ -0,0 +1,18 @@
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+
+price = "service.attributes.main.price"
+service_license = "service.attributes.main.license"
+sample = "service.attributes.additionalInformation.links.type"
+categories = "service.attributes.additionalInformation.categories"
+tags = "service.attributes.additionalInformation.tags"
+created = "created"
+datePublished = "service.attributes.main.datePublished"
+dateCreated = "service.attributes.main.dateCreated"
+updated_frequency = "service.attributes.additionalInformation.updateFrequency"
+metadata_type = "service.attributes.main.type"
+service_type = "service.type"
+
+list_indexes = [price, service_license, sample, categories, tags,
+                created, updated_frequency, service_type, metadata_type]
