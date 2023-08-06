02a066b012ee7c2cc1ceed8b7ce99d32215e611d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:55 +0000

    Add package NucDetect/0.5.1.dev1/NucDetect-0.5.1.dev1.tar.gz

---
+++ packages/NucDetect/NucDetect-0.5.1.dev1.tar.gz/NucDetect-0.5.1.dev1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NucDetect",
+    version="0.5.1.dev1",
+    description=("Module to identify intranuclear proteins on basis of"
+                 "fluorescence images."),
+    author="Romano Weiss",
+    url="https://github.com/SilMon/NucDetect",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.7"
+    ],
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+)
