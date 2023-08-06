fdec465f6216c2b0c61df41dd6056e8b29885d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:11 +0000

    Add package pytopojson/0.0.4/pytopojson-0.0.4.tar.gz

---
+++ packages/pytopojson/pytopojson-0.0.4.tar.gz/pytopojson-0.0.4/tests/test_basics.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class Geo2TopoTestCase(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/pytopojson/pytopojson-0.0.4.tar.gz/pytopojson-0.0.4/tests/test_geo2topo.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+import json
+import subprocess
+import unittest
+
+# Third-party imports
+
+# Application-specific imports
+from tests import in_delta
+
+
+class Geo2TopoTestCase(unittest.TestCase):
+    def geo2topo(self, output, options=''):
+        options = ' '.join(options.split()).split(' ')
+        p = subprocess.run(['python', 'bin/geo2topo.py', *options],
+                           capture_output=True)
+        out, err = p.stdout, p.stderr
+        actual = json.loads(out.decode('utf8').strip().replace("'", '"'))
+        with open(output, 'r') as f:
+            expected = json.load(f)
+
+        self.assertTrue(in_delta.in_delta(actual, expected))
+
+    def test_polygons(self):
+        self.geo2topo('tests/pytopojson/polygon-no-quantization.json',
+                      'polygon=tests/geojson/polygon-clockwise.json')
+
+    def test_quantized_polygons(self):
+        self.geo2topo('tests/pytopojson/polygon.json',
+                      '-q 1E4 polygon=tests/geojson/polygon-clockwise.json')
+
+    def test_empty_geometries(self):
+        self.geo2topo('tests/pytopojson/empty.json',
+                      'multilinestring=tests/geojson/empty-multilinestring.json'
+                      ' multipoint=tests/geojson/empty-multipoint.json'
+                      ' multipolygon=tests/geojson/empty-multipolygon.json'
+                      ' multipolygon2=tests/geojson/empty-multipolygon2.json'
+                      ' polygon=tests/geojson/empty-polygon.json')
