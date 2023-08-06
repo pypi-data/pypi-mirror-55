f5ef56e6a4b4fd99ed962a3a0a64ebc63968cd18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:28 +0000

    Add package pytorchvis/0.0.1/pytorchvis-0.0.1.tar.gz

---
+++ packages/pytorchvis/pytorchvis-0.0.1.tar.gz/pytorchvis-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytorchvis",
+    version="0.0.1",
+    author="Anuj Shah",
+    author_email="anujonline645@gmail.com",
+    description="A package to visualize CNN in PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anujshah1003/pytorchvis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
