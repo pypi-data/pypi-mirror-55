b1cf73f2634e8f0f56856c6fec130f534af9be57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:29 +0000

    Add package pybids/0.9.5/pybids-0.9.5.tar.gz

---
+++ packages/pybids/pybids-0.9.5.tar.gz/pybids-0.9.5/bids/layout/__init__.py
@@ -0,0 +1,19 @@
+from .layout import BIDSLayout, add_config_paths, parse_file_entities
+from .models import (BIDSFile, BIDSImageFile, BIDSDataFile, BIDSJSONFile,
+                     Config, Entity, Tag)
+# Backwards compatibility
+from bids_validator import BIDSValidator
+
+__all__ = [
+    "BIDSLayout",
+    "BIDSValidator",
+    "add_config_paths",
+    "parse_file_entities",
+    "BIDSFile",
+    "BIDSImageFile",
+    "BIDSDataFile",
+    "BIDSJSONFile",
+    "Config",
+    "Entity",
+    "Tag"
+]
