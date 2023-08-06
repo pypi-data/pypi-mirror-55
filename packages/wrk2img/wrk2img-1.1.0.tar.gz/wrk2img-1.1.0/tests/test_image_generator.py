ef77bcb625e7dfc9f93801ffba69bd867a8401f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:55 +0000

    Add package wrk2img/1.1.0/wrk2img-1.1.0.tar.gz

---
+++ packages/wrk2img/wrk2img-1.1.0.tar.gz/wrk2img-1.1.0/tests/test_image_generator.py
@@ -0,0 +1,32 @@
+import unittest
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from wrk2img import ImageGenerator
+
+
+class TestImageGenerator(unittest.TestCase):
+
+    def setUp(self):
+        self.image_generator = ImageGenerator()
+
+    def test_generate_image(self):
+        data = {
+            '748868.53 req/s': {
+                50: 250e-6,
+                75: 491e-6,
+                90: 700e-6,
+                99: 5.8e-3,
+            }
+        }
+        image = self.image_generator.generate_image(data, "localhost")
+        self.assertIsNotNone(image)
+        # TODO: write some assert
+        with TemporaryDirectory(dir='.') as tempdir:
+            output = Path(tempdir).resolve().joinpath('output.png')
+            self.image_generator.save_image(image, output)
+            self.assertTrue(output.exists())
+
+
+if __name__ == '__main__':
+    unittest.main()
