b3df1f6933e3323512c3004f7472b3f7cfadefe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:18 +0000

    Add package snorlaxse/0.0.1/snorlaxse-0.0.1.tar.gz

---
+++ packages/snorlaxse/snorlaxse-0.0.1.tar.gz/snorlaxse-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='snorlaxse',
+    version="0.0.1",
+    author="snorlaxse",
+    author_email="841145636@qq.com",
+    description="upload some self useful code",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/Captainzj/snorlax_package.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+)
