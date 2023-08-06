50aaab27132323c12f438678c5f333d69ee01dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:19 +0000

    Add package rfc6266-content-disposition/0.0.6/rfc6266-content-disposition-0.0.6.tar.gz

---
+++ packages/rfc6266-content-disposition/rfc6266-content-disposition-0.0.6.tar.gz/rfc6266-content-disposition-0.0.6/setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup
+
+setup(
+    author='Gabriel de Perthuis, ekzhu',
+    author_email='g2p.code+rfc6266@gmail.com',
+    description='Parse and generate Content-Disposition headers (a fork maintained by ekzhu)',
+    url='https://github.com/ekzhu/rfc6266',
+    keywords='rfc6266 Content-Disposition http attachments',
+    name='rfc6266-content-disposition',
+    version='0.0.6',  # semver
+    license='GNU LGPL',
+    platforms='OS-independent',
+    py_modules=['rfc6266', 'test_rfc6266'],
+    install_requires=['LEPL'],
+    use_2to3=True,
+    long_description=open('README').read(),
+    classifiers=(
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Topic :: Internet :: WWW/HTTP',
+    ),
+)
+
