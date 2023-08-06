1b49ee79f5ce7006407d799feb1361543ae5d788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:12 +0000

    Add package sphinxcontrib-vcs/0.2.2/sphinxcontrib-vcs-0.2.2.tar.gz

---
+++ packages/sphinxcontrib-vcs/sphinxcontrib-vcs-0.2.2.tar.gz/sphinxcontrib-vcs-0.2.2/README.md
@@ -0,0 +1,46 @@
+# sphinxcontrib-vcs
+
+[![Build Status](https://travis-ci.org/t2y/sphinxcontrib-vcs.svg?branch=master)](https://travis-ci.org/t2y/sphinxcontrib-vcs/)
+
+*sphinxcontrib-vcs* provides showing commit history in version control system.
+
+## Setup
+
+    $ pip install sphinxcontrib-vcs
+
+## Usage
+
+Add *'sphinxcontrib.vcs'* as extensions into conf.py.
+
+    $ vi conf.py
+    extensions = ['sphinxcontrib.vcs']
+
+Then, repository directives are available as follows.
+
+### Git
+
+    .. git::
+        :number_of_revisions: 20
+        :with_ref_url:
+        :include_diff:
+
+For more information have a look at [the documentation](https://sphinxcontrib-vcs.readthedocs.io/).
+
+## Acknowledgments
+
+*sphinxcontrib-vcs* is inspired from the [sphinx-git](https://github.com/OddBloke/sphinx-git).
+
+## ChangeLog
+
+### 0.2.1 (2019-08-04)
+
+* fix wrong Docutils DTD list_item node thanks to amedama41
+
+### 0.2.0 (2018-11-17)
+
+* drop Python 2.7 support and mercurial feature
+* support internal git repository such as GitHub Enterprise
+
+### 0.1.0 (2016-09-06)
+
+* first release
---
+++ packages/sphinxcontrib-vcs/sphinxcontrib-vcs-0.2.2.tar.gz/sphinxcontrib-vcs-0.2.2/setup.py
@@ -0,0 +1,52 @@
+import re
+from os import path
+from setuptools import find_packages, setup
+
+version_py = open('sphinxcontrib/vcs.py').read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", version_py))
+desc = 'Sphinx extension to show commit history in version control system'
+
+cur_dir = path.abspath(path.dirname(__file__))
+with open(path.join(cur_dir, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='sphinxcontrib-vcs',
+    version=metadata['version'],
+    description=desc,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries',
+        'Environment :: Console',
+        'Framework :: Sphinx :: Extension',
+        'Topic :: Documentation',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Utilities',
+    ],
+    url='https://github.com/t2y/sphinxcontrib-vcs',
+    license='BSD',
+    author='Tetsuya Morimoto',
+    author_email='tetsuya.morimoto@gmail.com',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(),
+    namespace_packages=['sphinxcontrib'],
+    include_package_data=True,
+    install_requires=[
+        'GitPython',
+        'Sphinx',
+    ],
+    tests_require=[
+        'flake8', 'mock', 'nose', 'reportlab', 'sphinx-testing',
+    ],
+)
