80553d0d8f7184c43df7165cd305953967976b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:17 +0000

    Add package codesignal-1-to-5/0.0.1/codesignal_1_to_5-0.0.1.tar.gz

---
+++ packages/codesignal-1-to-5/codesignal_1_to_5-0.0.1.tar.gz/codesignal_1_to_5-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="codesignal_1_to_5",
+    version="0.0.1",
+    author="Kummari Naresh",
+    author_email="kummarinaresh35@gmail.com",
+    description="Code signals_1_to_5 programs Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/knaresh8464/Codesignals_10_programs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
