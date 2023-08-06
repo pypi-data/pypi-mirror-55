6b1c5d7fc977856be593aa716a53d62140a2c76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:18 +0000

    Add package pangeamt-tea/0.0.52/pangeamt_tea-0.0.52-py3-none-any.whl

---
+++ packages/pangeamt-tea/pangeamt_tea-0.0.52-py3-none-any.whl/pangeamt_tea/project/workflow/workflow.py
@@ -0,0 +1,39 @@
+import os
+
+
+class Workflow:
+    DIR = 'workflow'
+
+    def __init__(self, project, config):
+        self._project = project
+        self._config = config
+
+    @staticmethod
+    def new(project, force=False):
+        pass
+        # if force=True remove remove the workflow first
+        # Create the worflow dir if the directory doesn't exists else raise WorkflowAlreadyExists
+        # Create the config.json threw the project.worflow.config
+
+    @staticmethod
+    def load(project):
+        pass
+        # Return a Workflow
+
+    @staticmethod
+    def remove(project):
+        pass
+        # Remove the worflow dir
+
+
+    def clean(self):
+        pass
+
+
+
+
+class WorkflowAlreadyExists(Exception):
+    def __init__(self, project):
+
+        message = f'The workflow already exists in project {project.name} '
+        super().__init__(message)
