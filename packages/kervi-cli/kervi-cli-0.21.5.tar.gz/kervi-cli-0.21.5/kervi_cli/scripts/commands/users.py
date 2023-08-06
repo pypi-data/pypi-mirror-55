54ca19674e7936b29e7cbd1a292d94b74021bb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:12 +0000

    Add package kervi-cli/0.21.5/kervi-cli-0.21.5.tar.gz

---
+++ packages/kervi-cli/kervi-cli-0.21.5.tar.gz/kervi-cli-0.21.5/kervi_cli/scripts/commands/users.py
@@ -0,0 +1,14 @@
+import click
+
+@click.group()
+def users():
+    """Manage kervi users"""
+    pass
+
+# @users.command()
+# @click.argument('user_name', "user name")
+# @click.password_option(help="user password")
+# @click.option('--name',  help='name of user')
+# @click.option('--groups',  help='groups the')
+# def add(name, user_name, password, groups):
+#     """Add a user to the application"""
