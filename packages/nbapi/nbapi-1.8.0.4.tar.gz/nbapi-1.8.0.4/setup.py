f1d227125bcda8f9395812b21f1348eb7696d5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:25 +0000

    Add package nbapi/1.8.0.4/nbapi-1.8.0.4.tar.gz

---
+++ packages/nbapi/nbapi-1.8.0.4.tar.gz/nbapi-1.8.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+version="1.8.0.4"
+open("version.txt","w+").write(version)
+setuptools.setup(
+    name="nbapi",
+    version=version,
+    author="LazyNeko",
+    author_email="nekobot.help@gmail.com",
+    description="A small anime API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lazyneko1/nbapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
