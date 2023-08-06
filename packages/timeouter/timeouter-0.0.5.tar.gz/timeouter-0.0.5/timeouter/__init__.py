f39feb06a0c6fe8d73199c98e2815836abcb6aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:44 +0000

    Add package timeouter/0.0.5/timeouter-0.0.5.tar.gz

---
+++ packages/timeouter/timeouter-0.0.5.tar.gz/timeouter-0.0.5/timeouter/__init__.py
@@ -0,0 +1,46 @@
+__version__ = '0.0.5'
+
+import threading
+import timeouter.core
+
+g = threading.local()
+
+
+class TimeoutException(Exception):
+    pass
+
+
+def init(timeout):
+    g._to = timeouter.core.Timer(timeout)
+
+
+def check(message=None):
+    return g._to.check(message)
+
+
+def get(chunks=1):
+    return g._to.get(chunks=chunks)
+
+
+def has(timeout):
+    return g._to.has(timeout)
+
+
+def reset():
+    return g._to.reset()
+
+
+def set_timeout(timeout):
+    return g._to.set_timeout(timeout)
+
+
+def set_exception_class(e):
+    g._to.set_exception_class(e)
+
+
+def set_default_exception_class(e):
+    timeouter.core.TimeoutException = e
+
+
+set_default_exception_class(TimeoutException)
+Timer = timeouter.core.Timer
