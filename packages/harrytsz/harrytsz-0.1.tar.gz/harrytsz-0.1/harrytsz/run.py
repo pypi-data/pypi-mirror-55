bfe07063a96a51fdacbe1b8c9991de2f64f961c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:27 +0000

    Add package harrytsz/0.1/harrytsz-0.1.tar.gz

---
+++ packages/harrytsz/harrytsz-0.1.tar.gz/harrytsz-0.1/harrytsz/run.py
@@ -0,0 +1,20 @@
+def run():
+
+	import jieba
+
+	txt = open("D:/demo/input.txt", "r").read()
+	res = open("D:/demo/output.txt", "w")
+	words = jieba.cut(txt)
+	size = 4
+	# print("size: ", size)
+	i = 0
+	for word in words:
+		if i == size:
+			res.write('\n')
+			i = 0
+		else:
+			res.write(word)
+			i += 1
+
+	# txt.close()
+	res.close()
---
+++ packages/harrytsz/harrytsz-0.1.tar.gz/harrytsz-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='harrytsz',
+      version='0.1',
+      description='harrytsz magic toolbox',
+      url='http://github.com/harrytsz/harrytsz',
+      author='Harrytsz',
+      author_email='taosizhan@gmail.com',
+      license='MIT',
+      packages=['harrytsz'],
+      zip_safe=False)
