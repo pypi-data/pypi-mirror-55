7f75c57102399300b2ff05149abe924caaff73d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:41 +0000

    Add package contendo-utils/0.2.27/contendo_utils-0.2.27.tar.gz

---
+++ packages/contendo-utils/contendo_utils-0.2.27.tar.gz/contendo_utils-0.2.27/contendo_utils/__init__.py
@@ -0,0 +1,8 @@
+from .contendo_logging import contendo_logging_setup
+from .contendo_logging import contendo_function_logger, contendo_classfunction_logger
+from .dict_to_object import DictToObject
+from .ProUtils import ProUtils
+from .BigqueryUtils import BigqueryUtils
+from .contendo_configuration_manager import ContendoConfigurationManager
+from .producer_consumers_engine import ProducerConsumersEngine
+from .simple_text_interaction import SimpleTextInteraction
