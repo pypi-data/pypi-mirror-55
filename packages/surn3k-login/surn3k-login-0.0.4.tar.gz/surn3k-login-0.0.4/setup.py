e7cdb8bf7b3409f0734f6a821f18a086a7ee8156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:23 +0000

    Add package surn3k-login/0.0.4/surn3k-login-0.0.4.tar.gz

---
+++ packages/surn3k-login/surn3k-login-0.0.4.tar.gz/surn3k-login-0.0.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='surn3k-login',
+    version='0.0.4',
+    author='zh',
+    author_email='fyved24@163.com',
+    url='http://blog.fyved24.de',
+    description='河南工业大学校园网登陆器',
+    packages=['surn3k'],
+    install_requires=[
+        'requests',
+        'argparse',
+        'configparser'
+    ],
+    entry_points={
+        'console_scripts': [
+            'login=surn3k:login',
+        ]
+    }
+)
