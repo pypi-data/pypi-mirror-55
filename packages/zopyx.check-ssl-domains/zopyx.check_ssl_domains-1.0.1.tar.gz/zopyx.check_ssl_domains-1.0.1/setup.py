a9a0ff6a9e71059452b071eb416b235abef523cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:21 +0000

    Add package zopyx.check-ssl-domains/1.0.1/zopyx.check_ssl_domains-1.0.1.tar.gz

---
+++ packages/zopyx.check-ssl-domains/zopyx.check_ssl_domains-1.0.1.tar.gz/zopyx.check_ssl_domains-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools import find_packages
+from os import path
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open(path.join('README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+   name='zopyx.check_ssl_domains',
+   version='1.0.1',
+   long_description=long_description,
+   description='Check expiration date of SSL/TLS certs on web host',
+   author='Andreas Jung',
+   author_email='info@zopyx.com',
+   install_requires=required,
+   packages=find_packages(),
+   entry_points = {
+        'console_scripts': ['ssl-check-domains=zopyx.checker:main']
+   }
+)
+
