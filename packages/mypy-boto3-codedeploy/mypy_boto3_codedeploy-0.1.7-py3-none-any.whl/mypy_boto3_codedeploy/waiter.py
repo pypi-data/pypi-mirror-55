022a376a2331ed7f730a7d08f6b9020daa1f6e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:51 +0000

    Add package mypy-boto3-codedeploy/0.1.7/mypy_boto3_codedeploy-0.1.7-py3-none-any.whl

---
+++ packages/mypy-boto3-codedeploy/mypy_boto3_codedeploy-0.1.7-py3-none-any.whl/mypy_boto3_codedeploy/waiter.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+
+from botocore.waiter import Waiter
+
+
+class DeploymentSuccessful(Waiter):
+
+    # pylint: disable=arguments-differ
+    def wait(self, deploymentId: str, WaiterConfig: Dict[str, Any] = None) -> None:
+        pass
