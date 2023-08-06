cd64f9712357ad3bb3bbf623de4f3ea974315fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:20 +0000

    Add package kcd-starling/0.1.1/kcd_starling-0.1.1-py3-none-any.whl

---
+++ packages/kcd-starling/kcd_starling-0.1.1-py3-none-any.whl/scrapper_core/driver.py
@@ -0,0 +1,14 @@
+from typing import Text
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_soap(url: Text, **kw) -> BeautifulSoup:
+    # TODO Retry Logic required and add test case
+    res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'})
+    return BeautifulSoup(res.content, "html.parser")
+
+
+def get_selenium(url: Text, **kw) -> None:
+    # TODO Implementation required
+    pass
