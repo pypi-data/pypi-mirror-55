00d568f091d44a656496e7720927344920f32128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:41 +0000

    Add package muffin/0.32.10/muffin-0.32.10.tar.gz

---
+++ packages/muffin/muffin-0.32.10.tar.gz/muffin-0.32.10/muffin/__init__.py
@@ -0,0 +1,18 @@
+"""Muffin is a web framework based on aiohttp."""
+
+# Package information
+# ===================
+
+__version__ = "0.32.10"
+__project__ = "muffin"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+from aiohttp.web import *
+
+CONFIGURATION_ENVIRON_VARIABLE = 'MUFFIN_CONFIG'
+
+from .app import Application, Handler
+from .utils import to_coroutine, MuffinException, local, import_submodules
+
+#  pylama:ignore=E402,W0611,W0401
