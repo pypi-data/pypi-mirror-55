4bc821c7d5408ad30dc7638470b0b557264f5299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:10 +0000

    Add package torchani/1.1/torchani-1.1-py3-none-any.whl

---
+++ packages/torchani/torchani-1.1-py3-none-any.whl/torchani/nn.py
@@ -0,0 +1,91 @@
+import torch
+from torch import Tensor
+from typing import Tuple, NamedTuple, Optional
+
+
+class SpeciesEnergies(NamedTuple):
+    species: Tensor
+    energies: Tensor
+
+
+class ANIModel(torch.nn.ModuleList):
+    """ANI model that compute energies from species and AEVs.
+
+    Different atom types might have different modules, when computing
+    energies, for each atom, the module for its corresponding atom type will
+    be applied to its AEV, after that, outputs of modules will be reduced along
+    different atoms to obtain molecular energies.
+
+    The resulting energies are in Hartree.
+
+    Arguments:
+        modules (:class:`collections.abc.Sequence`): Modules for each atom
+            types. Atom types are distinguished by their order in
+            :attr:`modules`, which means, for example ``modules[i]`` must be
+            the module for atom type ``i``. Different atom types can share a
+            module by putting the same reference in :attr:`modules`.
+    """
+
+    def __init__(self, modules):
+        super(ANIModel, self).__init__(modules)
+
+    def forward(self, species_aev: Tuple[Tensor, Tensor],
+                cell: Optional[Tensor] = None,
+                pbc: Optional[Tensor] = None) -> SpeciesEnergies:
+        assert cell is None
+        assert pbc is None
+        species, aev = species_aev
+        species_ = species.flatten()
+        aev = aev.flatten(0, 1)
+
+        output = aev.new_zeros(species_.shape)
+
+        for i, m in enumerate(self):
+            mask = (species_ == i)
+            midx = mask.nonzero().flatten()
+            if midx.shape[0] > 0:
+                input_ = aev.index_select(0, midx)
+                output.masked_scatter_(mask, m(input_).flatten())
+        output = output.view_as(species)
+        return SpeciesEnergies(species, torch.sum(output, dim=1))
+
+
+class Ensemble(torch.nn.ModuleList):
+    """Compute the average output of an ensemble of modules."""
+
+    def __init__(self, modules):
+        super(Ensemble, self).__init__(modules)
+        self.size = len(modules)
+
+    def forward(self, species_input: Tuple[Tensor, Tensor],
+                cell: Optional[Tensor] = None,
+                pbc: Optional[Tensor] = None) -> SpeciesEnergies:
+        assert cell is None
+        assert pbc is None
+        sum_ = 0
+        for x in self:
+            sum_ += x(species_input)[1]
+        species, _ = species_input
+        return SpeciesEnergies(species, sum_ / self.size)
+
+
+class Sequential(torch.nn.ModuleList):
+    """Modified Sequential module that accept Tuple type as input"""
+
+    def __init__(self, *modules):
+        super(Sequential, self).__init__(modules)
+
+    def forward(self, input_: Tuple[Tensor, Tensor],
+                cell: Optional[Tensor] = None,
+                pbc: Optional[Tensor] = None):
+        for module in self:
+            input_ = module(input_, cell=cell, pbc=pbc)
+            cell = None
+            pbc = None
+        return input_
+
+
+class Gaussian(torch.nn.Module):
+    """Gaussian activation"""
+    def forward(self, x: Tensor) -> Tensor:
+        return torch.exp(- x * x)
