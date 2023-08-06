295ebba1df912c3fdb0900577c8f5be3633468b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:26 +0000

    Add package kcd-starling/0.1.3/kcd-starling-0.1.3.tar.gz

---
+++ packages/kcd-starling/kcd-starling-0.1.3.tar.gz/kcd-starling-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kcd-starling',
+    version='0.1.3',
+    description='Skeleton for Scrapper',
+    author='Terry Cho',
+    author_email='terry@kcd.co.kr',
+    packages=find_packages(exclude=['test']),
+    zip_safe=False,
+    include_package_data=True
+)
