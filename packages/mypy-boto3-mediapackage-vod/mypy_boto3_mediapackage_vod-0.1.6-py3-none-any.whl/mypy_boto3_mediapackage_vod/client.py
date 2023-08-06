6ceb83a489663648586e04c208014f16f4edada5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:51 +0000

    Add package mypy-boto3-mediapackage-vod/0.1.6/mypy_boto3_mediapackage_vod-0.1.6-py3-none-any.whl

---
+++ packages/mypy-boto3-mediapackage-vod/mypy_boto3_mediapackage_vod-0.1.6-py3-none-any.whl/mypy_boto3_mediapackage_vod/client.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+
+from botocore.client import BaseClient
+from botocore.paginate import Paginator
+from botocore.waiter import Waiter
+
+
+class Client(BaseClient):
+
+    # pylint: disable=arguments-differ
+    def can_paginate(self, operation_name: str = None) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def create_asset(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def create_packaging_configuration(
+        self,
+        Id: str,
+        PackagingGroupId: str,
+        CmafPackage: Dict[str, Any] = None,
+        DashPackage: Dict[str, Any] = None,
+        HlsPackage: Dict[str, Any] = None,
+        MssPackage: Dict[str, Any] = None,
+    ) -> Dict[str, Any]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def create_packaging_group(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete_asset(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete_packaging_configuration(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete_packaging_group(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_asset(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_packaging_configuration(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_packaging_group(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def generate_presigned_url(
+        self,
+        ClientMethod: str = None,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = None,
+        HttpMethod: str = None,
+    ) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_paginator(self, operation_name: str = None) -> Paginator:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_waiter(self, waiter_name: str = None) -> Waiter:
+        pass
+
+    # pylint: disable=arguments-differ
+    def list_assets(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def list_packaging_configurations(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def list_packaging_groups(self) -> None:
+        pass
