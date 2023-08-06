f2a4c09241efb83bc5bc6d0bd99b957cd8ab7787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:58 +0000

    Add package ng-utils/0.0.2/ng_utils-0.0.2.tar.gz

---
+++ packages/ng-utils/ng_utils-0.0.2.tar.gz/ng_utils-0.0.2/ng_utils/v29x.py
@@ -0,0 +1,19 @@
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+__metaclass__ = type
+
+from .cpython import *
