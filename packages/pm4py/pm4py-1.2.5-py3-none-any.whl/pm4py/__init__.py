7096890f3c2670f816e4e345eade9a024433830d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:53 +0000

    Add package pm4py/1.2.5/pm4py-1.2.5-py3-none-any.whl

---
+++ packages/pm4py/pm4py-1.2.5-py3-none-any.whl/pm4py/__init__.py
@@ -0,0 +1,12 @@
+import time
+
+time.clock = time.process_time
+
+from pm4py import algo, evaluation, objects, util, visualization, statistics, streaming
+
+__version__ = '1.2.5'
+__doc__ = "Process Mining for Python"
+__author__ = 'PADS'
+__author_email__ = 'pm4py@pads.rwth-aachen.de'
+__maintainer__ = 'PADS'
+__maintainer_email__ = "pm4py@pads.rwth-aachen.de"
