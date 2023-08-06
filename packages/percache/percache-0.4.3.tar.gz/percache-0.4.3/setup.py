3c19f1b479906458f76fd0a2ab2b607428db90e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:15 +0000

    Add package percache/0.4.3/percache-0.4.3.tar.gz

---
+++ packages/percache/percache-0.4.3.tar.gz/percache-0.4.3/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+import os.path
+
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
+
+version = '0.4.3'
+
+with open(README) as fp:
+    longdesc = fp.read()
+
+setup(name='percache',
+    version=version,
+    description='Persistently cache results of callables',
+    long_description=longdesc,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Intended Audience :: Developers'
+    ],
+    author='Oben Sonne',
+    author_email='obensonne@googlemail.com',
+    license='MIT License',
+    url='https://hg.sr.ht/~obensonne/percache',
+    py_modules=['percache']
+)
+
