dfda8ace3551fe324e0951727f55b81579201364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:53 +0000

    Add package galgek-ukp-nomousauto-grivind/0.1.2/galgek-ukp-nomousauto-grivind-0.1.2.tar.gz

---
+++ packages/galgek-ukp-nomousauto-grivind/galgek-ukp-nomousauto-grivind-0.1.2.tar.gz/galgek-ukp-nomousauto-grivind-0.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md','r') as fh:
+	long_description = fh.read()
+
+setuptools.setup(name='galgek-ukp-nomousauto-grivind',
+	  version = '0.1.2',
+	  description = 'Decription',
+	  long_description = long_description,
+	  long_description_content_type = 'text/markdown',
+	  url='http://github.com/kshamruk/galgek-ukp-nomousauto-grivind',
+	  author = 'kshamruk',
+	  author_email = 'c.shamruk@gmail.com',
+	  license='MIT',
+	  packages=setuptools.find_packages(),
+	  classifiers = [
+	  			'Programming Language :: Python :: 3',
+	  			'License :: OSI Approved :: MIT License',
+	  			'Operating System :: OS Independent',
+	  			],
+	  install_requires = ['scipy','numpy','opencv-python',
+	  'torch','numba','efficientnet_pytorch','tqdm'],
+	  python_requires = '>=3.6')
