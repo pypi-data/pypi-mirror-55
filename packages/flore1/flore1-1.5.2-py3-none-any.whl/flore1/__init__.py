b89dd333450416f4d035e107f84501115b2ec5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:34 +0000

    Add package flore1/1.5.2/flore1-1.5.2-py3-none-any.whl

---
+++ packages/flore1/flore1-1.5.2-py3-none-any.whl/flore1/__init__.py
@@ -0,0 +1,12 @@
+"""/////////////////////////
+///
+///   File: __init__.py
+///   Author: Anicet Nougaret
+///   License: CC BY-SA (see FLORE1/license.txt)
+///
+/////////////////////////"""
+
+name = "flore1"
+
+# local
+from .engine_locals import *
