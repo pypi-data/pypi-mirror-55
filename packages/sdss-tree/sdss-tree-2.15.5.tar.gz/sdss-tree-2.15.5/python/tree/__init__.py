bb85d4f80e042a81c4b6493465f8ef68def306a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:38 +0000

    Add package sdss-tree/2.15.5/sdss-tree-2.15.5.tar.gz

---
+++ packages/sdss-tree/sdss-tree-2.15.5.tar.gz/sdss-tree-2.15.5/python/tree/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import yaml
+
+from tree.tree import Tree
+
+# Inits the logging system. Only shell logging, and exception and warning catching.
+# File logging can be started by calling log.start_file_logger(name).
+from .misc import log
+
+
+NAME = 'tree'
+
+# Loads config
+with open(os.path.dirname(__file__) + '/etc/{0}.cfg'.format(NAME)) as ff:
+    config = yaml.load(ff)
+
+
+__version__ = '2.15.5'
