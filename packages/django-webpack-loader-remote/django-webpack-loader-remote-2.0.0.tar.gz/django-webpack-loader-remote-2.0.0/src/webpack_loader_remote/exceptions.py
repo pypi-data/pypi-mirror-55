d828279578303cae525e5b483869d32a6d44ec04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:09 +0000

    Add package django-webpack-loader-remote/2.0.0/django-webpack-loader-remote-2.0.0.tar.gz

---
+++ packages/django-webpack-loader-remote/django-webpack-loader-remote-2.0.0.tar.gz/django-webpack-loader-remote-2.0.0/src/webpack_loader_remote/exceptions.py
@@ -0,0 +1,41 @@
+__all__ = (
+    "WebpackError",
+    "WebpackLoaderRemoteBadStatsError",
+    "WebpackLoaderRemoteTimeoutError",
+    "WebpackBundleLookupError",
+)
+
+
+class BaseWebpackLoaderRemoteException(Exception):
+    """
+    Base exception for django-webpack-loader-remote.
+    """
+
+
+class WebpackError(BaseWebpackLoaderRemoteException):
+    """
+    General webpack loader error.
+    """
+
+
+class WebpackLoaderRemoteBadStatsError(BaseWebpackLoaderRemoteException):
+    """
+    The stats file does not contain valid data.
+    """
+
+
+class WebpackLoaderRemoteTimeoutError(BaseWebpackLoaderRemoteException):
+    """
+    The bundle took too long to compile.
+    """
+
+
+class WebpackBundleLookupError(BaseWebpackLoaderRemoteException):
+    """
+    The bundle name was invalid.
+    """
+
+class PresignedURLKeysMissing(BaseWebpackLoaderRemoteException):
+    """
+    The AWS keys were not found.
+    """
