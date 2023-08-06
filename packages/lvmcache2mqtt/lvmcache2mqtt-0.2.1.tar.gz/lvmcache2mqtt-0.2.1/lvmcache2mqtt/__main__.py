17b1be17dd4f73ea166311f28e85f9d24054b178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:52 +0000

    Add package lvmcache2mqtt/0.2.1/lvmcache2mqtt-0.2.1.tar.gz

---
+++ packages/lvmcache2mqtt/lvmcache2mqtt-0.2.1.tar.gz/lvmcache2mqtt-0.2.1/lvmcache2mqtt/__main__.py
@@ -0,0 +1,38 @@
+import json
+import time
+
+import paho.mqtt.client as mqtt
+
+from . import Config, StatsCollector
+
+def main():
+    config_file = 'config.yml'
+    config = Config(config_file)
+
+
+    mqtt_config = config.get_mqtt()
+    mq = mqtt.Client()
+    mq.connect(mqtt_config['host'])
+    mq.on_log = log
+
+    collector = StatsCollector()
+    while(True):
+        for device in config.get_devices():
+            data = collector.collect(device['device'])
+            data['device'] = device['device']
+            data['device_alias'] = device['device_alias']
+            data['cache_read_hits_percentage'] = int(data['cache_read_hits'])/ (int(data['cache_read_hits']) + int(data['cache_read_misses'])) * 100
+            data['cache_write_hits_percentage'] = int(data['cache_write_hits']) / (int(data['cache_write_hits']) + int(data['cache_write_misses'])) * 100
+            print(data)
+            mq.publish(device['target_topic'], json.dumps(data))
+        
+        time.sleep(30)
+
+    
+def log(self, client, userdata, level, buf):
+    if level >= MQTT_LOG_INFO:
+        print(buf)
+
+
+if __name__ == '__main__':
+    main()
