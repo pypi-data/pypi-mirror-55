b3ad4537044a8ef2143394f7b72cbbf6d2bb54fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:05 +0000

    Add package aws-cdk.core/1.16.2/aws-cdk.core-1.16.2.tar.gz

---
+++ packages/aws-cdk.core/aws-cdk.core-1.16.2.tar.gz/aws-cdk.core-1.16.2/setup.py
@@ -0,0 +1,43 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.core",
+    "version": "1.16.2",
+    "description": "AWS Cloud Development Kit Core Library",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.core",
+        "aws_cdk.core._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.core._jsii": [
+            "core@1.16.2.jsii.tgz"
+        ],
+        "aws_cdk.core": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.4",
+        "publication>=0.0.3",
+        "aws-cdk.cx-api~=1.16,>=1.16.2"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
