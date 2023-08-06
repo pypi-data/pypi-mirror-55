796ca37e1ed84404f9cff2eba2b3713906c14d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:32 +0000

    Add package nanome/0.9.0/nanome-0.9.0-py2.py3-none-any.whl

---
+++ packages/nanome/nanome-0.9.0-py2.py3-none-any.whl/nanome/_internal/_ui/_serialization/_button_serializer.py
@@ -0,0 +1,139 @@
+from nanome._internal._util._serializers import _StringSerializer, _ColorSerializer, _Vector3Serializer
+from nanome.util.enums import VertAlignOptions, HorizAlignOptions
+from . import _UIBaseSerializer
+from .. import _Button
+
+from nanome._internal._util._serializers import _TypeSerializer
+
+class _ButtonSerializer(_TypeSerializer):
+    def __init__(self):
+        self.string = _StringSerializer()
+        self.color = _ColorSerializer()
+        self.vector = _Vector3Serializer()
+
+    def version(self):
+        return 2
+
+    def name(self):
+        return "Button"
+
+    def serialize(self, version, value, context):
+        if (version == 0 ):
+            safe_id = (context._plugin_id << 24) & 0x7FFFFFFF
+            safe_id |= value._content_id
+        else:
+            safe_id = value._content_id
+        context.write_int(safe_id)
+
+        context.write_bool(value._selected)
+        context.write_bool(value._unusable)
+        context.write_bool(value._text._active)
+        context.write_using_serializer(self.string, value._text._value_idle)
+        context.write_using_serializer(self.string, value._text._value_selected)
+        context.write_using_serializer(self.string, value._text._value_highlighted)
+        context.write_using_serializer(self.string, value._text._value_selected_highlighted)
+        context.write_using_serializer(self.string, value._text._value_unusable)
+        context.write_bool(value._text._auto_size)
+        context.write_float(value._text._min_size)
+        context.write_float(value._text._max_size)
+        context.write_float(value._text._size)
+        context.write_bool(value._text._underlined)
+        context.write_bool(value._text._bolded)
+        context.write_uint((value._text._vertical_align))
+        context.write_uint((value._text._horizontal_align))
+
+        if version >= 2:
+            context.write_bool(value._icon._active)
+            context.write_using_serializer(self.string, value._icon._value_idle)
+            context.write_using_serializer(self.string, value._icon._value_selected)
+            context.write_using_serializer(self.string, value._icon._value_highlighted)
+            context.write_using_serializer(self.string, value._icon._value_selected_highlighted)
+            context.write_using_serializer(self.string, value._icon._value_unusable)
+            data = []
+            if (value._icon._value_idle != ""):
+                with open(value._icon._value_idle, "rb") as f:
+                    data = f.read()
+            context.write_byte_array(data)
+            data = []
+            if (value._icon._value_selected != ""):
+                with open(value._icon._value_selected, "rb") as f:
+                    data = f.read()
+            context.write_byte_array(data)
+            data = []
+            if (value._icon._value_highlighted != ""):
+                with open(value._icon._value_highlighted, "rb") as f:
+                    data = f.read()
+            context.write_byte_array(data)
+            data = []
+            if (value._icon._value_selected_highlighted != ""):
+                with open(value._icon._value_selected_highlighted, "rb") as f:
+                    data = f.read()
+            context.write_byte_array(data)
+            data = []
+            if (value._icon._value_unusable != ""):
+                with open(value._icon._value_unusable, "rb") as f:
+                    data = f.read()
+            context.write_byte_array(data)
+            context.write_using_serializer(self.color, value._icon._color_idle)
+            context.write_using_serializer(self.color, value._icon._color_selected)
+            context.write_using_serializer(self.color, value._icon._color_highlighted)
+            context.write_using_serializer(self.color, value._icon._color_selected_highlighted)
+            context.write_using_serializer(self.color, value._icon._color_unusable)
+            context.write_float(value._icon._sharpness)
+            context.write_float(value._icon._size)
+            context.write_float(value._icon._ratio)
+            context.write_using_serializer(self.vector, value._icon._position)
+            context.write_using_serializer(self.vector, value._icon._rotation)
+
+    def deserialize(self, version, context):
+        value = _Button._create()
+        value._content_id = context.read_int()
+        if (version == 0):
+            id_mask = 0x00FFFFFF
+            value._content_id &= id_mask
+
+        value._selected = context.read_bool()
+        value._unusable = context.read_bool()
+        value._text._active = context.read_bool()
+        value._text._value_idle = context.read_using_serializer(self.string)
+        value._text._value_selected = context.read_using_serializer(self.string)
+        value._text._value_highlighted = context.read_using_serializer(self.string)
+        value._text._value_selected_highlighted = context.read_using_serializer(self.string)
+        value._text._value_unusable = context.read_using_serializer(self.string)
+        value._text._auto_size = context.read_bool()
+        value._text._min_size = context.read_float()
+        value._text._max_size = context.read_float()
+        value._text._size = context.read_float()
+        value._text._underlined = context.read_bool()
+        value._text._bolded = context.read_bool()
+        vert = context.read_uint()
+        horiz = context.read_uint()
+        value._text._vertical_align = VertAlignOptions(vert)
+        value._text._horizontal_align = HorizAlignOptions(horiz)
+
+        if version >= 2:
+            value._icon._active = context.read_bool()
+            value._icon._value_idle = context.read_using_serializer(self.string)
+            value._icon._value_selected = context.read_using_serializer(self.string)
+            value._icon._value_highlighted = context.read_using_serializer(self.string)
+            value._icon._value_selected_highlighted = context.read_using_serializer(self.string)
+            value._icon._value_unusable = context.read_using_serializer(self.string)
+            context.read_byte_array()
+            context.read_byte_array()
+            context.read_byte_array()
+            context.read_byte_array()
+            context.read_byte_array()
+            value._icon._color_idle = context.read_using_serializer(self.color)
+            value._icon._color_selected = context.read_using_serializer(self.color)
+            value._icon._color_highlighted = context.read_using_serializer(self.color)
+            value._icon._color_selected_highlighted = context.read_using_serializer(self.color)
+            value._icon._color_unusable = context.read_using_serializer(self.color)
+            value._icon._sharpness = context.read_float()
+            value._icon._size = context.read_float()
+            value._icon._ratio = context.read_float()
+            value._icon._position = context.read_using_serializer(self.vector)
+            value._icon._rotation = context.read_using_serializer(self.vector)
+
+        return value
+
+_UIBaseSerializer.register_type("Button", _UIBaseSerializer.ContentType.ebutton, _ButtonSerializer())
---
+++ packages/nanome/nanome-0.9.0-py2.py3-none-any.whl/nanome/api/ui/button.py
@@ -0,0 +1,335 @@
+import nanome
+from nanome._internal._ui import _Button
+from . import UIBase
+
+class Button(_Button, UIBase):
+    HorizAlignOptions = nanome.util.enums.HorizAlignOptions
+    VertAlignOptions = nanome.util.enums.VertAlignOptions
+
+    def __init__(self, text = None, icon = None):
+        # type: (str, str)
+        _Button.__init__(self)
+        UIBase.__init__(self)
+        self.text = self._text
+        self.icon = self._icon
+        if (text != None):
+            self.set_all_text(text)
+        if (icon != None):
+            self.set_all_icon(icon)
+
+    def register_pressed_callback(self, func):
+        _Button._register_pressed_callback(self, func)
+
+    def register_hover_callback(self, func):
+        _Button._register_hover_callback(self, func)
+
+    def set_all_text(self, text):
+        # type: (str)
+        """
+        | Sets the text value of every state to the given text.
+
+        :param text: The text to display on the button
+        :type text: str
+        """
+        self.text.value_idle = text
+        self.text.value_selected = text
+        self.text.value_highlighted = text
+        self.text.value_selected_highlighted = text
+        self.text.value_unusable = text
+
+    def set_all_icon(self, icon):
+        # type: (str)
+        """
+        | Sets the path to the icon for every state.
+        | Enables the button icon
+
+        :param icon: The path to the icon file
+        :type icon: str
+        """
+        self.icon.active = True
+        self.icon.value_idle = icon
+        self.icon.value_selected = icon
+        self.icon.value_highlighted = icon
+        self.icon.value_selected_highlighted = icon
+        self.icon.value_unusable = icon
+
+    @property
+    def selected(self):
+        # type: () -> bool
+        return self._selected
+    @selected.setter
+    def selected(self, value):
+        # type: (bool)
+        self._selected = value
+
+    @property
+    def unusable(self):
+        # type: () -> bool
+        return self._unusable
+    @unusable.setter
+    def unusable(self, value):
+        # type: (bool)
+        self._unusable = value
+
+    class ButtonText(_Button.ButtonText):
+        @property
+        def active(self):
+            # type: () -> bool
+            return self._active
+        @active.setter
+        def active(self, value):
+            # type: (bool)
+            self._active = value
+
+        @property
+        def value_idle(self):
+            # type: () -> str
+            return self._value_idle
+        @value_idle.setter
+        def value_idle(self, value):
+            # type: (str)
+            if type(value) is not str:
+                value = str(value)
+            self._value_idle = value
+
+        @property
+        def value_selected(self):
+            # type: () -> str
+            return self._value_selected
+        @value_selected.setter
+        def value_selected(self, value):
+            # type: (str)
+            if type(value) is not str:
+                value = str(value)
+            self._value_selected = value
+
+        @property
+        def value_highlighted(self):
+            # type: () -> str
+            return self._value_highlighted
+        @value_highlighted.setter
+        def value_highlighted(self, value):
+            # type: (str)
+            if type(value) is not str:
+                value = str(value)
+            self._value_highlighted = value
+
+        @property
+        def value_selected_highlighted(self):
+            # type: () -> str
+            return self._value_selected_highlighted
+        @value_selected_highlighted.setter
+        def value_selected_highlighted(self, value):
+            # type: (str)
+            if type(value) is not str:
+                value = str(value)
+            self._value_selected_highlighted = value
+
+        @property
+        def value_unusable(self):
+            # type: () -> str
+            return self._value_unusable
+        @value_unusable.setter
+        def value_unusable(self, value):
+            # type: (str)
+            if type(value) is not str:
+                value = str(value)
+            self._value_unusable = value
+
+        @property
+        def auto_size(self):
+            # type: () -> bool
+            return self._auto_size
+        @auto_size.setter
+        def auto_size(self, value):
+            # type: (bool)
+            self._auto_size = value
+
+        @property
+        def min_size(self):
+            # type: () -> float
+            return self._min_size
+        @min_size.setter
+        def min_size(self, value):
+            # type: (float)
+            self._min_size = value
+
+        @property
+        def max_size(self):
+            # type: () -> float
+            return self._max_size
+        @max_size.setter
+        def max_size(self, value):
+            # type: (float)
+            self._max_size = value
+
+        @property
+        def size(self):
+            # type: () -> float
+            return self._size
+        @size.setter
+        def size(self, value):
+            # type: (float)
+            self._size = value
+
+        @property
+        def underlined(self):
+            # type: () -> bool
+            return self._underlined
+        @underlined.setter
+        def underlined(self, value):
+            # type: (bool)
+            self._underlined = value
+
+        @property
+        def bolded(self):
+            # type: () -> bool
+            return self._bolded
+        @bolded.setter
+        def bolded(self, value):
+            # type: (bool)
+            self._bolded = value
+
+        @property
+        def vertical_align(self):
+            # type: () -> VertAlignOptions
+            return self._vertical_align
+        @vertical_align.setter
+        def vertical_align(self, value):
+            # type: (VertAlignOptions)
+            self._vertical_align = value
+
+        @property
+        def horizontal_align(self):
+            # type: () -> HorizAlignOptions
+            return self._horizontal_align
+        @horizontal_align.setter
+        def horizontal_align(self, value):
+            # type: (HorizAlignOptions)
+            self._horizontal_align = value
+    _Button.ButtonText._create = ButtonText
+
+    class ButtonIcon(_Button.ButtonIcon):
+        @property
+        def active(self):
+            return self._active
+        @active.setter
+        def active(self, value):
+            self._active = value
+
+        @property
+        def value_idle(self):
+            return self._value_idle
+        @value_idle.setter
+        def value_idle(self, value):
+            if type(value) is not str:
+                value = str(value)
+            self._value_idle = value
+
+        @property
+        def value_selected(self):
+            return self._value_selected
+        @value_selected.setter
+        def value_selected(self, value):
+            if type(value) is not str:
+                value = str(value)
+            self._value_selected = value
+
+        @property
+        def value_highlighted(self):
+            return self._value_highlighted
+        @value_highlighted.setter
+        def value_highlighted(self, value):
+            if type(value) is not str:
+                value = str(value)
+            self._value_highlighted = value
+
+        @property
+        def value_selected_highlighted(self):
+            return self._value_selected_highlighted
+        @value_selected_highlighted.setter
+        def value_selected_highlighted(self, value):
+            if type(value) is not str:
+                value = str(value)
+            self._value_selected_highlighted = value
+
+        @property
+        def value_unusable(self):
+            return self._value_unusable
+        @value_unusable.setter
+        def value_unusable(self, value):
+            if type(value) is not str:
+                value = str(value)
+            self._value_unusable = value
+
+        @property
+        def color_idle(self):
+            return self._color_idle
+        @color_idle.setter
+        def color_idle(self, value):
+            self._color_idle = value
+
+        @property
+        def color_selected(self):
+            return self._color_selected
+        @color_selected.setter
+        def color_selected(self, value):
+            self._color_selected = value
+
+        @property
+        def color_highlighted(self):
+            return self._color_highlighted
+        @color_highlighted.setter
+        def color_highlighted(self, value):
+            self._color_highlighted = value
+
+        @property
+        def color_selected_highlighted(self):
+            return self._color_selected_highlighted
+        @color_selected_highlighted.setter
+        def color_selected_highlighted(self, value):
+            self._color_selected_highlighted = value
+
+        @property
+        def color_unusable(self):
+            return self._color_unusable
+        @color_unusable.setter
+        def color_unusable(self, value):
+            self._color_unusable = value
+
+        @property
+        def sharpness(self):
+            return self._sharpness
+        @sharpness.setter
+        def sharpness(self, value):
+            self._sharpness = value
+
+        @property
+        def size(self):
+            return self._size
+        @size.setter
+        def size(self, value):
+            self._size = value
+
+        @property
+        def ratio(self):
+            return self._ratio
+        @ratio.setter
+        def ratio(self, value):
+            self._ratio = value
+
+        @property
+        def position(self):
+            return self._position
+        @position.setter
+        def position(self, value):
+            self._position = value
+
+        @property
+        def rotation(self):
+            return self._rotation
+        @rotation.setter
+        def rotation(self, value):
+            self._rotation = value
+    _Button.ButtonIcon._create = ButtonIcon
+_Button._create = Button
