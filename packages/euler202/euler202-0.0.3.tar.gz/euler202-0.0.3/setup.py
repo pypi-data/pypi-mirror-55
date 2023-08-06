e87686856e6803a65c5eddddb1d65b5903259098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:02 +0000

    Add package euler202/0.0.3/euler202-0.0.3.tar.gz

---
+++ packages/euler202/euler202-0.0.3.tar.gz/euler202-0.0.3/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='euler202',
+    version='0.0.3',
+    description='Solves Euler problem 202',
+    author='Mark Bell',
+    author_email='mcbell@illinois.edu',
+    url='https://github.com/MarkCBell/euler202',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    license='MIT License',
+)
