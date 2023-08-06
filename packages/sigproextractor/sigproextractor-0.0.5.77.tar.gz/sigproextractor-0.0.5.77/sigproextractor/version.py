051cd9c47d655674a70101e4392857c1812859d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:16 +0000

    Add package sigproextractor/0.0.5.77/sigproextractor-0.0.5.77.tar.gz

---
+++ packages/sigproextractor/sigproextractor-0.0.5.77.tar.gz/sigproextractor-0.0.5.77/sigproextractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROEXTRACTOR SETUP.PY
+short_version = '0.0.5.77'
+version = '0.0.5.77'
+Update = 'Heirarchy option deleted, clustering deleted and signatures orders by the mutation burden'
+    
+    
