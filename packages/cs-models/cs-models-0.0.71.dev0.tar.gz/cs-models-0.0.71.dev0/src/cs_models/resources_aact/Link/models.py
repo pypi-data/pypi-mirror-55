d578e6641f7372f3a2bfd3fd629c2209766a62d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:34 +0000

    Add package cs-models/0.0.71.dev0/cs-models-0.0.71.dev0.tar.gz

---
+++ packages/cs-models/cs-models-0.0.71.dev0.tar.gz/cs-models-0.0.71.dev0/src/cs_models/resources_aact/Link/models.py
@@ -0,0 +1,29 @@
+from src.cs_models.aact_database import Base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Text,
+    ForeignKey,
+)
+
+
+class LinkModel(Base):
+    __tablename__ = 'links'
+    __table_args__ = (
+        {'schema': 'ctgov'},
+    )
+
+    id = Column(Integer, primary_key=True)
+    nct_id = Column(
+        String,
+        ForeignKey('ctgov.studies.nct_id'),
+        nullable=False,
+    )
+    url = Column(String)
+    description = Column(Text)
+
+    def __repr__(self):
+        return "<Link(id='{}', nct_id='{}', " \
+               "url='{}', description='{}'>"\
+            .format(self.id, self.nct_id, self.url, self.description)
