6fb483242469c0a3354330e3c1e9c5cbbda27106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:17 +0000

    Add package needle-forms/0.0.5/needle-forms-0.0.5.tar.gz

---
+++ packages/needle-forms/needle-forms-0.0.5.tar.gz/needle-forms-0.0.5/setup.py
@@ -0,0 +1,22 @@
+#pragm
+import setuptools
+
+setuptools.setup(
+    name="needle-forms",
+    version="0.0.5",
+    author="dmiranda",
+    author_email="dmiranda@ciandt.com",
+    description="Abstraction layer to GCP resources",
+    url="https://github.com/ciandt-dev/needle-forms",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'google-cloud-datastore>=1.8.0',
+        'google-cloud-pubsub>=0.41.0',
+        'google-cloud-bigquery>=1.21.0',
+    ],
+)
