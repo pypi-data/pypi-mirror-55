6d1643938e3d158531c505da646dc245b617f12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:11 +0000

    Add package gobbler/0.1.9/gobbler-0.1.9.tar.gz

---
+++ packages/gobbler/gobbler-0.1.9.tar.gz/gobbler-0.1.9/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+VERSION = '0.1.9'
+
+setup(
+    name='gobbler', version=VERSION, packages=['gobble'],
+    include_package_data=True,
+    package_data={'': ['*.png']},
+    url='https://github.com/rayssharma/gobble',
+    download_url='https://github.com/rayssharma/gobble/tarball/{}'.format(
+        VERSION), license='MIT', author='Ray Sharma',
+    author_email='ramon.sharma1@gmail.com',
+    description='Turkey-themed image catalog to boost the holiday mood.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['pillow>=6.2.1', 'pytest'], entry_points={
+        'console_scripts': ['gobble=gobble.gobble:main'],
+    }, classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ])
