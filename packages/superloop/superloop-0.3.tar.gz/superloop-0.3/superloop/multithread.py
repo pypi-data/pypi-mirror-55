f3b917310b4bbbb7e07e87c3533f0d87d010aa04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:33 +0000

    Add package superloop/0.3/superloop-0.3.tar.gz

---
+++ packages/superloop/superloop-0.3.tar.gz/superloop-0.3/superloop/multithread.py
@@ -0,0 +1,30 @@
+####################### ENGINE FUNCTIONS ########################
+
+import initialize
+import threading
+import time
+import datetime
+
+def multithread_engine(ntw_object,redirect,commands):
+	
+	start_time = datetime.datetime.now()
+	index = 0
+	for i in ntw_object:
+		if(redirect == 'push_cfgs'):
+			arguments = commands[index]
+		elif(redirect == 'exec_command' or redirect == 'get_config'):
+			### ARGUMENT(S)/COMMAND(S) ARE BEING BLACKHOLED
+			arguments = commands
+		my_thread = threading.Thread(target=getattr(ntw_object[index],redirect) , args=(arguments,))
+		my_thread.start()
+
+		index = index + 1
+
+	main_thread = threading.currentThread()
+	for some_thread in threading.enumerate():
+		if some_thread != main_thread:
+#			print(some_thread)
+			some_thread.join()
+
+	print('[!] [SUCCESS] [{}]\n'.format(datetime.datetime.now() - start_time))
+
---
+++ packages/superloop/superloop-0.3.tar.gz/superloop-0.3/superloop/processdb.py
@@ -0,0 +1,26 @@
+### THIS MODULE PROCESS THE YAML FILE TO CREATE THE DATABASES OF BOTH
+### NODES AND TEMPLATES
+
+from node_create import node_create
+import yaml
+
+def process_nodes():
+
+	with open("/database/nodes.yaml") as yaml_file:
+		node_object = yaml.load(yaml_file)
+
+	return node_object
+	
+def process_templates():
+
+	with open("/database/templates.yaml") as yaml_file:
+		template_object = yaml.load(yaml_file)
+
+	return template_object
+
+def process_encrypted():
+
+	with open("/database/encrypted.yaml") as yaml_file:
+		passwords = yaml.load(yaml_file)
+
+	return passwords
