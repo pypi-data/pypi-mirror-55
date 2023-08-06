beb32508761057c3731f9fe0413477025fd3fdbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:53 +0000

    Add package mypy-boto3-elastictranscoder/0.1.7/mypy-boto3-elastictranscoder-0.1.7.tar.gz

---
+++ packages/mypy-boto3-elastictranscoder/mypy-boto3-elastictranscoder-0.1.7.tar.gz/mypy-boto3-elastictranscoder-0.1.7/README.md
@@ -0,0 +1,23 @@
+# mypy-boto3-elastictranscoder submodule
+
+Provides type annotations for `boto3.elastictranscoder` service
+
+## Installation
+
+```bash
+pip install mypy-boto3[elastictranscoder]
+```
+
+## Usage
+
+```python
+import boto3
+from mypy_boto3.elastictranscoder import Client, ServiceResource
+
+client: Client = boto3.client("elastictranscoder")
+resource: ServiceResource = boto3.resource("elastictranscoder")
+
+# now your IDE can suggest you method and arguments names
+# and mypy can check types
+```
+
---
+++ packages/mypy-boto3-elastictranscoder/mypy-boto3-elastictranscoder-0.1.7.tar.gz/mypy-boto3-elastictranscoder-0.1.7/setup.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from setuptools import setup
+
+from mypy_boto3_elastictranscoder.version import __version__ as version
+
+
+ROOT_PATH = Path(__file__).absolute().parent
+
+long_description = (ROOT_PATH / 'README.md').read_text()
+
+setup(
+    name="mypy-boto3-elastictranscoder",
+    version=version,
+    packages=["mypy_boto3_elastictranscoder"],
+    url="https://github.com/vemel/mypy_boto3",
+    license="MIT License",
+    author="Vlad Emelianov",
+    author_email="vlad.emelianov.nz@gmail.com",
+    description="Mypy-friendly boto3 type annotations for elastictranscoder service.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Typing :: Typed",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={"mypy_boto3_elastictranscoder": ["py.typed"]},
+    install_requires=["mypy-boto3"],
+    zip_safe=False,
+)
