4e7b4d349fe9ea915cfa95d102f62150101916fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:06 +0000

    Add package aiida-zeopp/1.0.2/aiida_zeopp-1.0.2-py2.py3-none-any.whl

---
+++ packages/aiida-zeopp/aiida_zeopp-1.0.2-py2.py3-none-any.whl/aiida_zeopp/calculations/test_network.py
@@ -0,0 +1,89 @@
+""" Tests for calculations
+
+"""
+from __future__ import absolute_import
+import os
+import io
+import pytest
+
+
+def test_submit_HKUST1(clear_database, network_code, basic_options):  # pylint: disable=unused-argument,invalid-name,invalid-name
+    """Test submitting a calculation"""
+    from aiida_zeopp.tests import TEST_DIR
+    from aiida_zeopp.calculations.network import NetworkCalculation
+    from aiida.engine import run_get_node
+
+    # Prepare input parameters
+    from aiida.plugins import DataFactory
+    parameters = DataFactory('zeopp.parameters')(dict={'cssr': True})
+    structure = DataFactory('cif')(file=os.path.join(TEST_DIR, 'HKUST-1.cif'),
+                                   parse_policy='lazy')
+
+    inputs = {
+        'code': network_code,
+        'parameters': parameters,
+        'structure': structure,
+        'metadata': {
+            'options': basic_options,
+            'label': 'aiida_zeopp format conversion',
+            'description': 'Test converting .cif to .cssr format',
+        },
+    }
+
+    _result, node = run_get_node(NetworkCalculation, **inputs)
+
+    cssr = io.open(os.path.join(TEST_DIR, 'HKUST-1.cssr'),
+                   'r',
+                   encoding='utf8').read()
+    assert cssr == node.outputs.structure_cssr.get_content()
+
+
+def test_submit_MgO(clear_database, network_code, basic_options):  # pylint: disable=unused-argument,invalid-name
+    """Test submitting a calculation
+
+    This includes a radii file.
+    """
+    from aiida_zeopp.tests import TEST_DIR
+    from aiida_zeopp.calculations.network import NetworkCalculation
+    from aiida.engine import run_get_node
+    from aiida.plugins import DataFactory
+
+    # Prepare input parameters
+    parameters = DataFactory('zeopp.parameters')(dict={
+        'cssr': True,
+        'res': True,
+        'sa': [1.82, 1.82, 5000],
+        'vsa': [1.82, 1.82, 5000],
+        'volpo': [1.82, 1.82, 5000],
+        'chan': 1.2,
+        'ha': False,
+        'strinfo': True,
+        'gridG': True,
+        'oms': False,
+    })
+
+    structure = DataFactory('cif')(file=os.path.join(TEST_DIR, 'MgO.cif'),
+                                   parse_policy='lazy')
+
+    atomic_radii = DataFactory('singlefile')(
+        file=os.path.join(TEST_DIR, 'MgO.rad'))
+
+    # set up calculation
+    inputs = {
+        'code': network_code,
+        'parameters': parameters,
+        'structure': structure,
+        'atomic_radii': atomic_radii,
+        'metadata': {
+            'options': basic_options,
+            'label': 'aiida_zeopp format conversion',
+            'description': 'Test converting .cif to .cssr format',
+        },
+    }
+
+    _result, node = run_get_node(NetworkCalculation, **inputs)
+
+    assert pytest.approx(
+        node.outputs.output_parameters.get_attribute('Density'),
+        0.01) == 3.18223
+    assert node.outputs.output_parameters.get_attribute('ASA_m^2/g') == 0.0
