44818180d54445ba185decf07c965108d7afc0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:48 +0000

    Add package node.ext.ugm/0.9.10/node.ext.ugm-0.9.10.tar.gz

---
+++ packages/node.ext.ugm/node.ext.ugm-0.9.10.tar.gz/node.ext.ugm-0.9.10/src/node/ext/ugm/__init__.py
@@ -0,0 +1,7 @@
+from node.ext.ugm._api import Group
+from node.ext.ugm._api import Groups
+from node.ext.ugm._api import Principal
+from node.ext.ugm._api import Principals
+from node.ext.ugm._api import Ugm
+from node.ext.ugm._api import User
+from node.ext.ugm._api import Users
