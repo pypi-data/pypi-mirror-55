52cc3e30d5eb7de6bd301c5040cefaffc38aa906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:18 +0000

    Add package pangeamt-nlp/0.1.22/pangeamt_nlp-0.1.22-py3-none-any.whl

---
+++ packages/pangeamt-nlp/pangeamt_nlp-0.1.22-py3-none-any.whl/pangeamt_nlp/multilingual_ressource/__init__.py
@@ -0,0 +1,18 @@
+from pangeamt_nlp.multilingual_ressource.bilingual.bilingual import Bilingual
+from pangeamt_nlp.multilingual_ressource.bilingual.bilingual_reader import BilingualReader
+from pangeamt_nlp.multilingual_ressource.tmx.tmx import Tmx
+from pangeamt_nlp.multilingual_ressource.tmx.tmx_reader_bilingual import TmxReaderBilingualText
+from pangeamt_nlp.multilingual_ressource.af.af import Af
+from pangeamt_nlp.multilingual_ressource.af.af_reader import AfReader
+from pangeamt_nlp.multilingual_ressource.dataset.dataset import Dataset
+
+
+__all__ = [
+    Bilingual,
+    BilingualReader,
+    Tmx,
+    TmxReaderBilingualText,
+    Af,
+    AfReader,
+    Dataset
+]
