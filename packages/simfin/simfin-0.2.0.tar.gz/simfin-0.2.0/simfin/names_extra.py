5bf2669aa5d47eb65cc2d661d9a10e12a54624b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:38 +0000

    Add package simfin/0.2.0/simfin-0.2.0.tar.gz

---
+++ packages/simfin/simfin-0.2.0.tar.gz/simfin-0.2.0/simfin/names_extra.py
@@ -0,0 +1,46 @@
+##########################################################################
+#
+# Names that makes it easier to address individual data-columns.
+#
+# This supplements names.py which has been auto-generated from the SimFin
+# database. This allows us to define more convenience names than are
+# available in the SimFin database. This module is imported in names.py
+# so all the names defined here will also be available from names.py
+#
+##########################################################################
+# SimFin - Simple financial data for Python.
+# www.simfin.com - www.github.com/simfin/simfin
+# See README.md for instructions and LICENSE.txt for license details.
+##########################################################################
+
+# Sales / Number of Shares.
+SPS = SALES_PER_SHARE = 'Sales Per Share'
+
+# Net Income / Number of Shares.
+EPS = EARNINGS_PER_SHARE = 'Earnings Per Share'
+
+# Total Equity / Number of Shares.
+BVPS = BOOK_VALUE_PER_SHARE = EQUITY_PER_SHARE = 'Equity Per Share'
+
+# Price / Earnings Ratio.
+PE = 'P/E'
+
+# Price / Sales Ratio.
+PSALES = 'P/Sales'
+
+# Price / Book-Value Ratio.
+PBOOK = 'P/Book'
+
+# Price / Free Cash Flow.
+PFCF = 'P/FCF'
+
+# Net Income / Revenue.
+NET_PROFIT_MARGIN = 'Net Profit Margin'
+
+# Net Income / Total Assets.
+ROA = 'Return on Assets'
+
+# Net Income / Shareholder Equity.
+ROE = 'Return on Equity'
+
+##########################################################################
