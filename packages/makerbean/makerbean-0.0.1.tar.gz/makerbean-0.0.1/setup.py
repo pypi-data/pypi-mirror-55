4d42ba4a233f0f3a2aa1c0ed190b3f83297bdd69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:56 +0000

    Add package makerbean/0.0.1/makerbean-0.0.1.tar.gz

---
+++ packages/makerbean/makerbean-0.0.1.tar.gz/makerbean-0.0.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Author: Anderson
+# @Date:   2019-11-14 17:45:03
+# @Last Modified by:   Anderson
+# @Last Modified time: 2019-11-14 17:54:01
+import setuptools
+
+
+setuptools.setup(
+    name="makerbean",
+    version="0.0.1",
+    author="MakerBi",
+    author_email="andersonby@163.com",
+    description="A small educational package",
+    long_description_content_type="text/markdown",
+    url="https://makerbean.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
