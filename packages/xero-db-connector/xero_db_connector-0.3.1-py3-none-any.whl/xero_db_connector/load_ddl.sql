d408b0154fdc71c5ba28c5b431e74d3e4d8cbc95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:19 +0000

    Add package xero-db-connector/0.3.1/xero_db_connector-0.3.1-py3-none-any.whl

---
+++ packages/xero-db-connector/xero_db_connector-0.3.1-py3-none-any.whl/xero_db_connector/load_ddl.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS "xero_load_invoices";
+
+CREATE TABLE IF NOT EXISTS "xero_load_invoices" (
+  "Type" TEXT,
+  "InvoiceID" TEXT,
+  "InvoiceNumber" TEXT,
+  "CurrencyRate" REAL,
+  "Date" DATE, -- YYYY-MM-DD e.g. 2019-01-01
+  "Status" TEXT,
+  "Total" REAL,
+  "CurrencyCode" TEXT,
+  "ContactID" TEXT
+);
+
+DROP TABLE IF EXISTS "xero_load_invoices_mapping";
+
+CREATE TABLE IF NOT EXISTS "xero_load_invoices_mapping" (
+  "InvoiceID" TEXT,
+  "XeroInvoiceID" TEXT -- this is what is returned by Xero
+);
+
+DROP TABLE IF EXISTS "xero_load_invoice_lineitems";
+
+CREATE TABLE IF NOT EXISTS "xero_load_invoice_lineitems" (
+  "InvoiceID" TEXT, -- internal use only. Not sent to Xero
+  "LineItemID" TEXT, -- internal use only. Not sent to Xero
+  "Description" TEXT,
+  "UnitAmount" REAL,
+  "LineAmount" REAL,
+  "AccountCode" TEXT,
+  "Quantity" REAL,
+  "TaxType" TEXT DEFAULT 'NONE'
+);
+
+DROP TABLE IF EXISTS "xero_load_lineitem_tracking";
+
+CREATE TABLE IF NOT EXISTS "xero_load_lineitem_tracking" (
+  "Name" TEXT,
+  "Option" TEXT,
+  "LineItemID" TEXT -- internal use only. Not sent to Xero
+);
