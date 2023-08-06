c9aaaa73cf2c1aa28f460b91eebdbb2f29231c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:37 +0000

    Add package easymultilogging/0.0.5/easymultilogging-0.0.5.tar.gz

---
+++ packages/easymultilogging/easymultilogging-0.0.5.tar.gz/easymultilogging-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easymultilogging",
+    version="0.0.5",
+    author="Praneeth Ponnekanti",
+    author_email="praneeth.ponnekanti@gmail.com",
+    description="A package to help multiLogging in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="",
+    packages=setuptools.find_packages(),
+    py_modules = ["easymultilogging"],
+    package_dir = {'' : 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
