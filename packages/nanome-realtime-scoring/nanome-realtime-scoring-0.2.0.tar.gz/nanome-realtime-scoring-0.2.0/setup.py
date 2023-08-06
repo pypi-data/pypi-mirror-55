f911da6a56ccba127aba8c9c39bce93c443dcc5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:54 +0000

    Add package nanome-realtime-scoring/0.2.0/nanome-realtime-scoring-0.2.0.tar.gz

---
+++ packages/nanome-realtime-scoring/nanome-realtime-scoring-0.2.0.tar.gz/nanome-realtime-scoring-0.2.0/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), 'r') as file:
+	README = file.read()
+
+setup(
+	name = 'nanome-realtime-scoring',
+	packages=find_packages(),
+	version = '0.2.0',
+	license='MIT',
+	description = 'Nanome Plugin displaying docking score of complexes in the workspace, in realtime',
+	long_description = README,
+    long_description_content_type = "text/markdown",
+	author = 'Nanome',
+	author_email = 'hello@nanome.ai',
+	url = 'https://github.com/nanome-ai/plugin-realtime-scoring',
+	platforms="any",
+	keywords = ['virtual-reality', 'chemistry', 'python', 'api', 'plugin'],
+	install_requires=['nanome'],
+	entry_points={"console_scripts": ["nanome-realtime-scoring = nanome_realtime_scoring.RealtimeScoring:main"]},
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+
+		'Intended Audience :: Science/Research',
+		'Topic :: Scientific/Engineering :: Chemistry',
+
+		'License :: OSI Approved :: MIT License',
+
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+	],
+	package_data={
+        "nanome_realtime_scoring": [
+            "*.json",
+			"dsx/*"
+        ]
+	},
+)
