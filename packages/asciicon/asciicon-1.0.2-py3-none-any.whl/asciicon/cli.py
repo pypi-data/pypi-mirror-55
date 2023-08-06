0fb9797581ef42219c2f1c1a1392d056d97450f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:25 +0000

    Add package asciicon/1.0.2/asciicon-1.0.2-py3-none-any.whl

---
+++ packages/asciicon/asciicon-1.0.2-py3-none-any.whl/asciicon/cli.py
@@ -0,0 +1,17 @@
+import os
+import click
+
+@click.command()
+@click.argument('name', default='Christ')
+def start(name):
+    """This cli provides short prayers and ascii art icons
+    """
+    click.echo(click.style("Christ is with us", fg='green', bold=True))
+
+    if name.lower() == 'christ':
+        with open(os.path.join(os.path.dirname(__file__), 'icons', 'christ.txt')) as f:
+            lines = f.readlines()   
+            
+    click.clear()
+    for l in lines:
+        click.secho(l, nl=False)
