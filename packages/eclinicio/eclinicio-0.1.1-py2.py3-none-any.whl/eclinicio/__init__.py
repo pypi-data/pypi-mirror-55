34dcbc1f557c1019943a207b8b7775a91d953e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:45 +0000

    Add package eclinicio/0.1.1/eclinicio-0.1.1-py2.py3-none-any.whl

---
+++ packages/eclinicio/eclinicio-0.1.1-py2.py3-none-any.whl/eclinicio/__init__.py
@@ -0,0 +1,34 @@
+import sys
+from .evloop import EventLoop
+from .server_factory import ServerFactory
+from .sslsocket import ServerSocket, ClientSocket, UPiServerSocket, UPiClientSocket
+from .sslsocket import (hostname, decode, encode, loads, dumps)
+
+__all__ = ['EventLoop', 'ServerSocket', 'ClientSocket',
+           "UPiServerSocket", "UPiClientSocket" 'hostname',
+           'decode', 'encode', 'loads', 'dumps']
+
+
+class PickleableFile(object):
+    def __init__(self, filename, mode='rb'):
+        self.filename = filename
+        self.mode = mode
+        self.file = open(filename, mode)
+
+    def __getstate__(self):
+        state = dict(filename=self.filename, mode=self.mode, closed=self.file.closed)
+        if not self.file.closed:
+            state['filepos'] = self.file.tell()
+        return state
+
+    def __setstate__(self, state):
+        self.filename = state['filename']
+        self.mode = state['mode']
+        self.file = open(self.filename, self.mode)
+        if state['closed']:
+            self.file.close()
+        else:
+            self.file.seek(state['filepos'])
+
+    def __getattr__(self, attr):
+        return getattr(self.file, attr)
