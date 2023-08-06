b61f91a21ee9987a8f8394595e45025e58159d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:56 +0000

    Add package real-robots/0.1.15/real_robots-0.1.15.tar.gz

---
+++ packages/real-robots/real_robots-0.1.15.tar.gz/real_robots-0.1.15/real_robots/__init__.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for real-robots."""
+
+__author__ = """S.P. Mohanty"""
+__email__ = 'mohanty@aicrowd.com'
+__version__ = '0.1.15'
+
+import os
+from gym.envs.registration import register
+
+from .evaluate import evaluate  # noqa F401
+
+register(
+    id='REALRobot-v0',
+    entry_point='real_robots.envs:REALRobotEnv',
+)
+
+register(
+    id='REALRobotSingleObj-v0',
+    entry_point='real_robots.envs:REALRobotEnvSingleObj',
+)
+
+
+def getPackageDataPath():
+    import real_robots
+    return os.path.join(
+                real_robots.__path__[0],
+                "data"
+            )
+
+
+def copy_over_data_into_pybullet(force_copy=False):
+    """
+    If the package specific data has not already
+    been copied over into pybullet_data, then
+    copy them over.
+    """
+    import pybullet_data
+
+    pybullet_data_path = pybullet_data.getDataPath()
+    is_data_absent = \
+        "kuka_gripper_description" not in os.listdir(pybullet_data_path)
+    if force_copy or is_data_absent:
+        import shutil
+        source_data_path = os.path.join(
+                                getPackageDataPath(),
+                                "kuka_gripper_description")
+        target_data_path = os.path.join(
+                                pybullet_data_path,
+                                "kuka_gripper_description")
+        print(
+            "[REALRobot] Copying over data into pybullet_data_path."
+            "This is a one time operation.")
+        shutil.copytree(source_data_path, target_data_path)
+
+
+copy_over_data_into_pybullet()
---
+++ packages/real-robots/real_robots-0.1.15.tar.gz/real_robots-0.1.15/real_robots/policy.py
@@ -0,0 +1,81 @@
+class BasePolicy:
+
+    def step(self, observation, reward, done):
+        """
+        The step function will receive the observation, reward and done signals
+        from the environment and computes the next action to take.
+
+        Parameters
+        ----------
+        observation : dict
+            The dictionary will contain the following entries:
+                - "joint_positions"
+                    Values of the joints position of the arm,
+                    including the gripper.
+                - "touch_sensors"
+                    Values recorded by the touch sensors.
+                - "retina"
+                    Image of the environment from the camera
+                    above the table.
+                - "goal"
+                    Image of the goal, showing how the objects
+                    should be arranged in the environment.
+        reward: float
+            This will be always zero.
+        done: bool
+            This will be True when:
+                - intrisic phase ends
+                - an extrinsic trial ends
+            otherwise it will always be false.
+        """
+        raise NotImplementedError(
+            "the step function has not been implemented. "
+            "Please derive your custom controller from this class "
+            "and implement atleast the step function. And optionally "
+            "the other available interfaces."
+        )
+
+    def start_intrinsic_phase(self):
+        """
+        The evaluator will call this function to signal the start of the
+        Intrinsic Phase.
+        Next observation will be from the intrinsic phase.
+        """
+        pass
+
+    def end_intrinsic_phase(self):
+        """
+        The evaluator will call this function to signal the end of the
+        Intrinsic Phase.
+        """
+        pass
+
+    def start_extrinsic_phase(self):
+        """
+        The evaluator will call this function to signal the start of the
+        Extrinsic Phase.
+        Next observation will be from the extrinsic phase.
+        """
+        pass
+
+    def end_extrinsic_phase(self):
+        """
+        The evaluator will call this function to signal the end of the
+        Extrinsic Phase.
+        """
+        pass
+
+    def start_extrinsic_trial(self):
+        """
+        The evaluator will call this function to signal the start of each
+        extrinsic trial.
+        Next observation will have a (new) goal.
+        """
+        pass
+
+    def end_extrinsic_trial(self):
+        """
+        The evaluator will call this function to signal the end of each
+        extrinsic trial.
+        """
+        pass
