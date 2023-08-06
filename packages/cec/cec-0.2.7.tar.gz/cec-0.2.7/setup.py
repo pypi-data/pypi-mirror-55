9242149394570139ec3d0dac8efcc03d7db59f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:43 +0000

    Add package cec/0.2.7/cec-0.2.7.tar.gz

---
+++ packages/cec/cec-0.2.7.tar.gz/cec-0.2.7/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+# Remove the "-Wstrict-prototypes" compiler option, which isn't valid for C++.
+import distutils.sysconfig
+cfg_vars = distutils.sysconfig.get_config_vars()
+if "CFLAGS" in cfg_vars:
+    cfg_vars["CFLAGS"] = cfg_vars["CFLAGS"].replace("-Wstrict-prototypes", "")
+if "OPT" in cfg_vars:
+    cfg_vars["OPT"] = cfg_vars["OPT"].replace("-Wstrict-prototypes", "")
+
+python_cec = Extension('cec', sources = [ 'cec.cpp', 'device.cpp' ], 
+                        include_dirs=['include'],
+                        libraries = [ 'cec' ])
+
+setup(name='cec', version='0.2.7',
+      description="Python bindings for libcec",
+      license='GPLv2',
+      data_files=['COPYING'],
+      ext_modules=[python_cec])
