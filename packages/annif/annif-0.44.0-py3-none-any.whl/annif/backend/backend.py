38224527d2855b200a95342ce3a3d03a4b235492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:08 +0000

    Add package annif/0.44.0/annif-0.44.0-py3-none-any.whl

---
+++ packages/annif/annif-0.44.0-py3-none-any.whl/annif/backend/backend.py
@@ -0,0 +1,77 @@
+"""Common functionality for backends."""
+
+import abc
+from annif import logger
+
+
+class AnnifBackend(metaclass=abc.ABCMeta):
+    """Base class for Annif backends that perform analysis. The
+    non-implemented methods should be overridden in subclasses."""
+
+    name = None
+    needs_subject_index = False
+
+    DEFAULT_PARAMS = {'limit': 100}
+
+    def __init__(self, backend_id, config_params, datadir):
+        """Initialize backend with specific parameters. The
+        parameters are a dict. Keys and values depend on the specific
+        backend type."""
+        self.backend_id = backend_id
+        self.datadir = datadir
+        self.config_params = config_params
+
+    def default_params(self):
+        return self.DEFAULT_PARAMS
+
+    @property
+    def params(self):
+        params = {}
+        params.update(self.default_params())
+        params.update(self.config_params)
+        return params
+
+    def train(self, corpus, project):
+        """train the model on the given document or subject corpus"""
+        pass  # default is to do nothing, subclasses may override
+
+    def initialize(self):
+        """This method can be overridden by backends. It should cause the
+        backend to pre-load all data it needs during operation."""
+        pass
+
+    @abc.abstractmethod
+    def _suggest(self, text, project, params):
+        """This method should implemented by backends. It implements
+        the suggest functionality, with pre-processed parameters."""
+        pass  # pragma: no cover
+
+    def suggest(self, text, project, params=None):
+        """Suggest subjects for the input text and return a list of subjects
+        represented as a list of SubjectSuggestion objects."""
+        self.initialize()
+        beparams = dict(self.params)
+        if params:
+            beparams.update(params)
+        return self._suggest(text, project, params=beparams)
+
+    def debug(self, message):
+        """Log a debug message from this backend"""
+        logger.debug("Backend {}: {}".format(self.backend_id, message))
+
+    def info(self, message):
+        """Log an info message from this backend"""
+        logger.info("Backend {}: {}".format(self.backend_id, message))
+
+    def warning(self, message):
+        """Log a warning message from this backend"""
+        logger.warning("Backend {}: {}".format(self.backend_id, message))
+
+
+class AnnifLearningBackend(AnnifBackend):
+    """Base class for Annif backends that can perform online learning"""
+
+    @abc.abstractmethod
+    def learn(self, corpus, project):
+        """further train the model on the given document or subject corpus"""
+        pass  # pragma: no cover
---
+++ packages/annif/annif-0.44.0-py3-none-any.whl/tests/test_backend.py
@@ -0,0 +1,51 @@
+"""Unit tests for backends in Annif"""
+
+import pytest
+import logging
+import annif
+import annif.backend
+import annif.corpus
+
+
+def test_get_backend_nonexistent():
+    with pytest.raises(ValueError):
+        annif.backend.get_backend("nonexistent")
+
+
+def test_get_backend_dummy(app, project):
+    dummy_type = annif.backend.get_backend("dummy")
+    dummy = dummy_type(backend_id='dummy', config_params={},
+                       datadir=app.config['DATADIR'])
+    result = dummy.suggest(text='this is some text', project=project)
+    assert len(result) == 1
+    assert result[0].uri == 'http://example.org/dummy'
+    assert result[0].label == 'dummy'
+    assert result[0].score == 1.0
+
+
+def test_learn_dummy(app, project, tmpdir):
+    dummy_type = annif.backend.get_backend("dummy")
+    dummy = dummy_type(backend_id='dummy', config_params={},
+                       datadir=app.config['DATADIR'])
+
+    tmpdir.join('doc1.txt').write('doc1')
+    tmpdir.join('doc1.tsv').write('<http://example.org/key1>\tkey1')
+    tmpdir.join('doc2.txt').write('doc2')
+    tmpdir.join('doc2.tsv').write('<http://example.org/key2>\tkey2')
+    docdir = annif.corpus.DocumentDirectory(str(tmpdir))
+
+    dummy.learn(docdir, project)
+
+    result = dummy.suggest(text='this is some text', project=project)
+    assert len(result) == 1
+    assert result[0].uri == 'http://example.org/key1'
+    assert result[0].label == 'key1'
+    assert result[0].score == 1.0
+
+
+def test_fill_params_with_defaults(app):
+    dummy_type = annif.backend.get_backend('dummy')
+    dummy = dummy_type(backend_id='dummy', config_params={},
+                       datadir=app.config['DATADIR'])
+    expected_default_params = {'limit': 100}  # From AnnifBackend class
+    assert expected_default_params == dummy.params
