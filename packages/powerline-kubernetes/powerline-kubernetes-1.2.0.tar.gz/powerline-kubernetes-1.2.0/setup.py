0d538e0ab41e97805deb915c4dc1de9cb3d977ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:40 +0000

    Add package powerline-kubernetes/1.2.0/powerline-kubernetes-1.2.0.tar.gz

---
+++ packages/powerline-kubernetes/powerline-kubernetes-1.2.0.tar.gz/powerline-kubernetes-1.2.0/setup.py
@@ -0,0 +1,24 @@
+# vim:fileencoding=utf-8:noet:tabstop=4:softtabstop=4:shiftwidth=4:expandtab:
+import pathlib
+from setuptools import setup
+
+setup(
+    name             = 'powerline-kubernetes',
+    description      = 'A Powerline segment to show Kubernetes context',
+    long_description = (pathlib.Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    version          = '1.2.0',
+    keywords         = 'powerline kubernetes context',
+    license          = 'MIT',
+    author           = 'Vincent De Smet',
+    author_email     = 'vincent.drl@gmail.com',
+    url              = 'https://github.com/so0k/powerline-kubernetes',
+    packages         = ['powerline_kubernetes'],
+    install_requires = ['powerline-status', 'kubernetes'],
+    classifiers      = [
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Terminals'
+    ]
+)
