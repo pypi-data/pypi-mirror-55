02ccb536f6acc2332f67430efdee5a15ccfe0995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:24 +0000

    Add package doorstopnewone/0.1/doorstopnewone-0.1.tar.gz

---
+++ packages/doorstopnewone/doorstopnewone-0.1.tar.gz/doorstopnewone-0.1/doorstopnew/__init__.py
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+
+"""Package for doorstop."""
+
+
+'''Changes : Commented out some modules which were throwing error 
+             and added new import statements  '''
+
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+from doorstopnew.common import DoorstopError, DoorstopInfo, DoorstopWarning
+#from doorstopnew.core.document import Document
+#from doorstopnew.core.item import Item
+#from doorstopnew.core.tree import Tree
+#from doorstopnew.core.builder import build
+#from doorstopnew.core.builder import find_document, find_item
+from doorstopnew.core import (
+    document,
+    item,
+    tree,
+    build,
+    builder,
+    editor,
+    exporter,
+    find_document,
+    find_item,
+    importer,
+    publisher,
+)
+
+__project__ = 'Doorstopnew'
+
+try:
+    __version__ = get_distribution(__project__).version
+except DistributionNotFound:
+    __version__ = '(local)'
+
+CLI = 'doorstopnew'
+GUI = 'doorstopnew-gui'
+SERVER = 'doorstopnew-server'
+VERSION = "{0} v{1}".format(__project__, __version__)
+DESCRIPTION = "Requirements management using version control."
