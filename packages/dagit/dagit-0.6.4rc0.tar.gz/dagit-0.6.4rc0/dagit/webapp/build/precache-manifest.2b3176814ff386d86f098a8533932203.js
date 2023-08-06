33d846c19f0fcb0b577ffdf93635979370b50aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:51 +0000

    Add package dagit/0.6.4rc0/dagit-0.6.4rc0.tar.gz

---
+++ packages/dagit/dagit-0.6.4rc0.tar.gz/dagit-0.6.4rc0/dagit/webapp/build/precache-manifest.2b3176814ff386d86f098a8533932203.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2dade5688fae702ef38aee7b5ecbd44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93af7b0b308e3919a5b7",
+    "url": "/static/css/2.f5035d2e.chunk.css"
+  },
+  {
+    "revision": "93af7b0b308e3919a5b7",
+    "url": "/static/js/2.2ca95277.chunk.js"
+  },
+  {
+    "revision": "71ecf84e01440a20d46f",
+    "url": "/static/js/main.f2ca0de5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9468a8ebaf94983770942fae8c470448",
+    "url": "/static/media/icon-dependency-arrow.9468a8eb.svg"
+  },
+  {
+    "revision": "c38e7cf02acb4df1cb996268f777db43",
+    "url": "/static/media/icon-file.c38e7cf0.svg"
+  },
+  {
+    "revision": "0a65b35680fd43d941da24725278ad53",
+    "url": "/static/media/icon-link.0a65b356.svg"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/static/media/icons-20.90247cfd.ttf"
+  },
+  {
+    "revision": "cf6b9dd9cbb5700820172af20b801559",
+    "url": "/static/media/nav-logo.cf6b9dd9.png"
+  }
+]);
