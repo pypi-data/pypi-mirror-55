5f34e86dcea1d050108306fac39308b0325bb7c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:45 +0000

    Add package cpi-inflation/2019.11.1/cpi_inflation-2019.11.1.tar.gz

---
+++ packages/cpi-inflation/cpi_inflation-2019.11.1.tar.gz/cpi_inflation-2019.11.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cpi_inflation',
+    version='2019.11.1',
+    url='https://github.com/jacoduplessis/cpi_inflation',
+    author='Jaco du Plessis',
+    author_email='jaco@jacoduplessis.co.za',
+    packages=['cpi_inflation'],
+)
