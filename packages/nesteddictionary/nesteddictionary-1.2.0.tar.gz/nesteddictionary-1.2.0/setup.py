0e2b39c365244c772e457b3899cf3f7b22e9ad0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:04 +0000

    Add package nesteddictionary/1.2.0/nesteddictionary-1.2.0.tar.gz

---
+++ packages/nesteddictionary/nesteddictionary-1.2.0.tar.gz/nesteddictionary-1.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nesteddictionary",
+    version="1.2.0",
+    author="Jacob Flanagan",
+    author_email="flanagan.jacob@gmail.com",
+    description="Wrapper for the dict class that extends the functionality for nested dicts including navigating using keypaths and nested key searching. This includes mixed dicts and lists.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jacobflanagan/nesteddictionary",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
