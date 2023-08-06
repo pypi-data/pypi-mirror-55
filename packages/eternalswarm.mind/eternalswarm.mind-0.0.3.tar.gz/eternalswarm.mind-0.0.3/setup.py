6a40d06807709c99efb6ad57e78dc69dc1259407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:11 +0000

    Add package eternalswarm.mind/0.0.3/eternalswarm.mind-0.0.3.tar.gz

---
+++ packages/eternalswarm.mind/eternalswarm.mind-0.0.3.tar.gz/eternalswarm.mind-0.0.3/setup.py
@@ -0,0 +1,15 @@
+import os
+
+from setuptools import setup
+
+setup(
+    name="eternalswarm.mind",
+    version='0.0.3',
+    packages=['eternalswarm.mind'],
+    namespace_packages=['eternalswarm'],
+    install_requires=['grpcio'],
+    author='Matthijs Gielen',
+    author_email='eternalswarm@mwgielen.com',
+    url='https://gitlab.com/eternal-swarm/mind',
+    description='The protobuf files to talk to the eternalswarm drone.'
+)
