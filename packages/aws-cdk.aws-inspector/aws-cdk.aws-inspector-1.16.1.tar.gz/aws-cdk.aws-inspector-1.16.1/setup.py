4cbf5a03521826af5c3652027ce4cf235ad61d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:18 +0000

    Add package aws-cdk.aws-inspector/1.16.1/aws-cdk.aws-inspector-1.16.1.tar.gz

---
+++ packages/aws-cdk.aws-inspector/aws-cdk.aws-inspector-1.16.1.tar.gz/aws-cdk.aws-inspector-1.16.1/setup.py
@@ -0,0 +1,43 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-inspector",
+    "version": "1.16.1",
+    "description": "The CDK Construct Library for AWS::Inspector",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_inspector",
+        "aws_cdk.aws_inspector._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_inspector._jsii": [
+            "aws-inspector@1.16.1.jsii.tgz"
+        ],
+        "aws_cdk.aws_inspector": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.3",
+        "publication>=0.0.3",
+        "aws-cdk.core~=1.16,>=1.16.1"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
