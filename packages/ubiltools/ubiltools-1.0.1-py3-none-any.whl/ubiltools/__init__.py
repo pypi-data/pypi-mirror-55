904fae1598a9741a64edacab9be1975248974e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:02 +0000

    Add package ubiltools/1.0.1/ubiltools-1.0.1-py3-none-any.whl

---
+++ packages/ubiltools/ubiltools-1.0.1-py3-none-any.whl/ubiltools/__init__.py
@@ -0,0 +1,7 @@
+from .decorators import *
+from .getkey import getKey
+from .term_colors import *
+
+__version__ = '1.0.1'
+__author__ = 'Geoff Hellstrand <ubil@hotmail.com>'
+__license__ = 'GPL-3.0'
