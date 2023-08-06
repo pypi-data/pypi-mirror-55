c906fce26946bff0a6e0715e9bca5c0585ef5375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:45 +0000

    Add package canal-dispatch/0.1.1/canal-dispatch-0.1.1.tar.gz

---
+++ packages/canal-dispatch/canal-dispatch-0.1.1.tar.gz/canal-dispatch-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools  import setup
+
+setup(
+    name= 'canal-dispatch',
+    version= '0.1.1',
+    packages = ['canalDispatch'],
+    author='wangzhongshan',
+    author_email='artcoding@163.com',
+    license='MIT',
+    url='https://github.com/artstylecode/binlogdispatch'
+)
