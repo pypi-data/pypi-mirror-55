829e0b48368c5fb3d35e6aa9ec8f1d2f417b6ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:52 +0000

    Add package mypy-boto3-dax/0.1.7/mypy_boto3_dax-0.1.7-py3-none-any.whl

---
+++ packages/mypy-boto3-dax/mypy_boto3_dax-0.1.7-py3-none-any.whl/mypy_boto3_dax/client.py
@@ -0,0 +1,135 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+from typing import List
+
+from botocore.client import BaseClient
+from botocore.paginate import Paginator
+from botocore.waiter import Waiter
+
+
+class Client(BaseClient):
+
+    # pylint: disable=arguments-differ
+    def can_paginate(self, operation_name: str = None) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def create_cluster(
+        self,
+        ClusterName: str,
+        NodeType: str,
+        ReplicationFactor: int,
+        IamRoleArn: str,
+        Description: str = None,
+        AvailabilityZones: List[Any] = None,
+        SubnetGroupName: str = None,
+        SecurityGroupIds: List[Any] = None,
+        PreferredMaintenanceWindow: str = None,
+        NotificationTopicArn: str = None,
+        ParameterGroupName: str = None,
+        Tags: List[Any] = None,
+        SSESpecification: Dict[str, Any] = None,
+    ) -> Dict[str, Any]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def create_parameter_group(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def create_subnet_group(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def decrease_replication_factor(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete_cluster(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete_parameter_group(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete_subnet_group(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_clusters(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_default_parameters(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_events(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_parameter_groups(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_parameters(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def describe_subnet_groups(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def generate_presigned_url(
+        self,
+        ClientMethod: str = None,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = None,
+        HttpMethod: str = None,
+    ) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_paginator(self, operation_name: str = None) -> Paginator:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_waiter(self, waiter_name: str = None) -> Waiter:
+        pass
+
+    # pylint: disable=arguments-differ
+    def increase_replication_factor(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def list_tags(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def reboot_node(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def tag_resource(self, ResourceName: str, Tags: List[Any]) -> Dict[str, Any]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def untag_resource(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def update_cluster(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def update_parameter_group(
+        self, ParameterGroupName: str, ParameterNameValues: List[Any]
+    ) -> Dict[str, Any]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def update_subnet_group(self) -> None:
+        pass
