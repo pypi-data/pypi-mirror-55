cb4390d0f869fcf479069258c9e05be9b393d5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:41 +0000

    Add package nb2workflow/1.0.1/nb2workflow-1.0.1-py3-none-any.whl

---
+++ packages/nb2workflow/nb2workflow-1.0.1-py3-none-any.whl/nb2workflow/container.py
@@ -0,0 +1,150 @@
+from __future__ import print_function
+
+import os
+import argparse
+import json
+import docker
+import shutil
+import tempfile
+import checksumdir
+ 
+def build_python(dockefile):
+    dockerfile.append("RUN yum install -y python")
+    dockerfile.append("RUN curl https://bootstrap.pypa.io/get-pip.py | python")
+
+def import_repo(repo_source,target):
+    print("importing repo",repo_source,"to",target)
+    if os.path.isdir(repo_source):
+        shutil.copytree(repo_source, target)
+    else:
+        raise NotImplementedError
+
+    return checksumdir.dirhash(target)
+
+def prepare_image(repo_source,from_image, service=True):
+    tempdir=tempfile.mkdtemp()
+
+    rel_repo_path="repo"
+    repo_path=os.path.join(tempdir,rel_repo_path)
+
+    repo_hash=import_repo(repo_source,repo_path)
+    
+    dockerfile=[]
+
+    dockerfile.append("FROM {}".format(from_image))
+    dockerfile.append("ARG REPO_PATH=./{}".format(rel_repo_path))
+    dockerfile.append("ADD $REPO_PATH/requirements.txt /requirements.txt")
+    dockerfile.append("RUN pip install -r /requirements.txt".format(repo_hash))
+    dockerfile.append("ADD $REPO_PATH /repo")
+    dockerfile.append("RUN touch /repo-hash-{}; pip install -r /repo/requirements.txt".format(repo_hash))
+    dockerfile.append("RUN useradd -ms /bin/bash oda")
+    dockerfile.append("ARG nb2workflow_revision".format(from_image))
+    dockerfile.append("RUN git clone https://github.com/volodymyrss/nb2workflow.git /nb2workflow; cd /nb2workflow; git reset --hard $nb2workflow_revision; pip install -r requirements.txt; pip install .; rm -rf /nb2workflow") 
+    dockerfile.append("USER oda")
+    dockerfile.append("WORKDIR /workdir")
+
+    if service:
+        dockerfile.append("ENTRYPOINT nb2service /repo/ --host 0.0.0.0" )
+    else:
+        dockerfile.append('ENTRYPOINT ["nbrun", "/repo/"]' )
+
+    open(os.path.join(tempdir,"Dockerfile"),"w").write(("\n".join(dockerfile))+"\n")
+
+    return tempdir
+
+
+def build_image(tempdir,tag_image,nb2workflow_revision):
+    cli=docker.from_env()
+    
+    print("-- building image, tagging as",tag_image)
+    r=cli.api.build(
+                path=tempdir,
+                tag=tag_image,
+                quiet=False,
+                buildargs=dict(nb2workflow_revision=nb2workflow_revision),
+               # stream=True,
+                rm=True,
+            )
+    for k in r:
+        try:
+            print(json.loads(k)['stream'].strip())
+        except:
+            print(k)
+
+    return True
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('repo', metavar='repo', type=str)
+    parser.add_argument('--run', action='store_true')
+    parser.add_argument('--build', action='store_true')
+    parser.add_argument('--job', action='store_true')
+    parser.add_argument('--name', metavar='TAG', type=str, default="nb2worker")
+    parser.add_argument('--from-image', metavar='FROM IMAGE', type=str, default="python:3.6")
+    parser.add_argument('--tag-image', metavar='TAG', type=str, default="")
+    parser.add_argument('--host', metavar='host', type=str, default="127.0.0.1")
+    parser.add_argument('--port', metavar='port', type=int, default=9191)
+    parser.add_argument('--nb2wrev', metavar='TAG', type=str, default="master")
+    parser.add_argument('--volume', metavar='mount:mount', type=str, nargs="*")
+    parser.add_argument('--store-dockerfile', metavar='location', type=str, default=None)
+
+
+    args = parser.parse_args()
+
+    repo_path=args.repo
+    tag_image=args.tag_image
+
+    if args.tag_image == "":
+        tag_image=os.path.basename(os.path.abspath(repo_path))
+
+    tempdir=prepare_image(repo_path,args.from_image, service=not args.job)
+
+    if args.store_dockerfile:
+        shutil.copy(os.path.join(tempdir,"Dockerfile"),args.store_dockerfile)
+        print("stored Dockerfile as",args.store_dockerfile)
+
+    if args.build:
+        build_result=build_image(tempdir,tag_image,args.nb2wrev)
+
+        if build_result is None:
+            raise Exception("failed to build")
+
+        print("built:",build_result)
+
+        if args.run:
+            if not args.job:
+                print("running",tag_image,"service on",args.port)
+                cli=docker.from_env()
+                c=cli.containers.run(
+                    tag_image,
+                    user=os.getuid(),
+                    ports={ 9191: (args.host, args.port) },
+                    name=args.name,
+                    detach=True,
+                    volumes=dict([
+                        (os.getcwd(),{"bind":"/workdir","mode":"rw"}),
+                    ]+[v.split(":",1) for v in args.volume]),
+                )
+
+                for r in c.attach(stream=True):
+                    print(c,r.strip())
+            else:
+                print("running",tag_image)
+                cli=docker.from_env()
+                c=cli.containers.run(
+                    tag_image,
+                    user=os.getuid(),
+                    name=args.name,
+                    detach=True,
+                    volumes=dict([
+                        (os.getcwd(),{"bind":"/workdir","mode":"rw"}),
+                    ]+[v.split(":",1) for v in args.volume]),
+                    entrypoint=["nbrun", "/repo/*ipynb"],
+                )
+
+                for r in c.attach(stream=True):
+                    print(c,r.strip())
+
+if __name__=="__main__":
+    main()
---
+++ packages/nb2workflow/nb2workflow-1.0.1-py3-none-any.whl/nb2workflow/cwl.py
@@ -0,0 +1,95 @@
+import argparse
+import logging
+
+import cwlgen
+
+import nb2workflow.nbadapter as nbadapter
+
+def python_type2cwl_type(pt):
+    if pt == str:
+        return 'string'
+
+    return pt.__name__
+
+def nb2cwl(notebook_fn, cwl_fn):
+    nba = nbadapter.NotebookAdapter(notebook_fn)
+
+    
+    tool_object = cwlgen.CommandLineTool(
+                    tool_id=nba.name, 
+                    base_command="python",
+                    label=None, 
+                    doc=None,
+                    cwl_version="v1.0", 
+                    stdin=None,
+                    stderr=None, 
+                    stdout=None, 
+                    path=None)
+
+    tool_object.arguments=["-m","nb2workflow.nbadapter",notebook_fn]
+
+
+    for par in nba.extract_parameters().values():
+        tool_object.inputs.append(
+            cwlgen.CommandInputParameter(
+                         par['name'], 
+                         param_type=python_type2cwl_type(par['python_type']),
+                         label=None, 
+                         secondary_files=None, 
+                         param_format=None,
+                         streamable=None, 
+                         doc=None, 
+                         input_binding=dict(prefix="--inp="+par['name']+"=", separate=False), 
+                         default=None)
+        )
+
+    #tool_object.outputs.append(
+    #    cwlgen.CommandOutputParameter('log',
+    #                                  param_type='stdout',
+    #                                  doc='log')
+    #)
+
+    for n, o in nba.extract_output_declarations().items():
+        tool_object.outputs.append(
+            cwlgen.CommandOutputParameter(n,
+                                          param_type='string',
+                                          doc='lines found with the pattern')
+        )
+
+    tool_object.export()
+
+    tool_object.export(cwl_fn)
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('notebook', metavar='notebook', type=str)
+    parser.add_argument('cwl', metavar='cwl', type=str)
+    parser.add_argument('--publish', metavar='upstream-url', type=str, default=None)
+    parser.add_argument('--publish-as', metavar='published url', type=str, default=None)
+    parser.add_argument('--debug', action="store_true")
+    parser.add_argument('--container', action="store_true")
+
+    args = parser.parse_args()
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+
+    root = logging.getLogger()
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+    if args.debug:
+        root.setLevel(logging.DEBUG)
+        handler.setLevel(logging.DEBUG)
+    else:
+        root.setLevel(logging.INFO)
+        handler.setLevel(logging.INFO)
+
+    nb2cwl(args.notebook, args.cwl)
+
+
+if __name__ == "__main__":
+    main()
