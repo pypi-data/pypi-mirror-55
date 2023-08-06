4cb7c8a686b28f8161ff9e44f100fe44072eed63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:02 +0000

    Add package calidh/0.1/calidh-0.1.tar.gz

---
+++ packages/calidh/calidh-0.1.tar.gz/calidh-0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'calidh',         
+  packages = ['calidh'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'The most useless dependency.',   
+  author = 'Moodboard Analytics',                   
+  author_email = 'webmaster@mdbrd.in',      
+  url = 'https://gitlab.com/MoodboardAnalytics/calidh',   
+  download_url = 'https://gitlab.com/MoodboardAnalytics/calidh/-/archive/v0.1/calidh-v0.1.tar.gz',    
+  keywords = ['MOST', 'MEANINGLESS', 'KEYWORD'],   
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',      
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
