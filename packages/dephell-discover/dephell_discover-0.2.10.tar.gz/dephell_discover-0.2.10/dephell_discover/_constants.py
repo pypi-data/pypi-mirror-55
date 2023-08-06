9a69da64fff24bd0f64817acd9cc926572c366c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:42 +0000

    Add package dephell-discover/0.2.10/dephell_discover-0.2.10.tar.gz

---
+++ packages/dephell-discover/dephell_discover-0.2.10.tar.gz/dephell_discover-0.2.10/dephell_discover/_constants.py
@@ -0,0 +1,31 @@
+
+BAD_DIRS_ANY = {
+    '__pycache__',
+    '.ipynb_checkpoints',
+    'pip-wheel-metadata',
+}
+
+# any hidden dirs (.*) will be excluded automatically
+BAD_DIRS_ROOT = {
+    'build',
+    'cache',
+    'dist',
+    'env',
+    'htmlcov',
+    'output',
+    'tests',
+    'venv',
+}
+
+BAD_EXTS = {
+    'egg',
+    'log',
+    'manifest',
+    'pyc',
+    'pyd',
+    'pyo',
+    'so',
+    'spec',
+}
+
+DOCSTRING = 'DOCSTRING'
