3d362cc2fdd16831dbb4e26b48929bee71a656c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:08 +0000

    Add package RouToolPa/0.80/RouToolPa-0.80.tar.gz

---
+++ packages/RouToolPa/RouToolPa-0.80.tar.gz/RouToolPa-0.80/setup.py
@@ -0,0 +1,14 @@
+__author__ = 'mahajrod'
+import os
+from setuptools import setup, find_packages
+
+dependencies = ['scipy', 'numpy', 'pandas', 'matplotlib', 'matplotlib-venn',
+                'biopython', 'xmltodict', 'bcbio-gff', 'statsmodels']
+
+setup(name='RouToolPa',
+      version='0.80',
+      packages=find_packages(),
+      author='Sergei F. Kliver',
+      author_email='mahajrod@gmail.com',
+      install_requires=dependencies,
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),)
