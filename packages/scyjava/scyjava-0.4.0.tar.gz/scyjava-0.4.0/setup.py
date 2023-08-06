4db793c40832edd6d46e1ad0fffb99ce6bbc6bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:20 +0000

    Add package scyjava/0.4.0/scyjava-0.4.0.tar.gz

---
+++ packages/scyjava/scyjava-0.4.0.tar.gz/scyjava-0.4.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import scyjava_config
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md')) as f:
+    scyjava_long_description = f.read()
+
+setuptools.setup(
+    name='scyjava',
+    python_requires='>=3',
+    packages=['scyjava', 'scyjava.convert'],
+    py_modules=['scyjava_config'],
+    version=scyjava_config.version,
+    author='Philipp Hanslovsky, Curtis Rueden',
+    author_email='hanslovskyp@janelia.hhmi.org',
+    description='scyjava',
+    long_description=scyjava_long_description,
+    long_description_content_type='text/markdown',
+    license='Public domain',
+    url='https://github.com/scijava/scyjava',
+    install_requires=['pyjnius', 'jgo'],
+)
