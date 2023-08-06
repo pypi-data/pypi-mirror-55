f22cec737a869877847b04cb82539845ce799866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:22 +0000

    Add package membrane/0.0.3/membrane-0.0.3.tar.gz

---
+++ packages/membrane/membrane-0.0.3.tar.gz/membrane-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="membrane",  # Replace with your own username
+    version="0.0.3",
+    author="jscul",
+    author_email="cullen.scott.john@gmail.com",
+    description="A Flask and Flask-RESTful argument parser.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jscul/membrane.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
