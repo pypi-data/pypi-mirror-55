93dd31b18fba6c31ca33b018f028b5175b24fb8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:11 +0000

    Add package heavy_celery/0.3.0/heavy_celery-0.3.0-py2.py3-none-any.whl

---
+++ packages/heavy_celery/heavy_celery-0.3.0-py2.py3-none-any.whl/heavy_celery/base.py
@@ -0,0 +1,140 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import uuid
+import logging
+import celery
+import json
+import yaml
+import six
+
+from django.core.files import File
+from django.core.files.base import ContentFile
+from django.utils import timezone
+from django.utils.functional import cached_property
+
+from . import utils
+
+logger = logging.getLogger(__name__)
+
+
+class Task(celery.Task):
+    @cached_property
+    def task(self):
+        if not hasattr(self, '_task') or self._task is None:
+            from . import models
+            if self.request.id is None:
+                task_id = uuid.uuid4().hex
+            else:
+                task_id = self.request.id
+            self._task, _ = models.CeleryTask.objects.get_or_create(task_id=task_id)
+        return self._task
+
+    def apply_async(self, args=None, kwargs=None, *args_, **kwargs_):
+        from . import models
+        user = kwargs_.get('user')
+        if user is None:
+            user = utils.get_user()
+        task_id = kwargs_.pop('task_id', None)
+        if task_id is None:
+            task_id = uuid.uuid4().hex
+        models.CeleryTask.objects.create(
+            user=user, task_id=task_id, task_path=self.name,
+            args=yaml.dump(args if args is not None else ()),
+            kwargs=yaml.dump(kwargs if kwargs is not None else {}))
+        result = super(Task, self).apply_async(args=args, kwargs=kwargs, task_id=task_id, *args_, **kwargs_)
+        logger.debug('created celery task : %s %s name=%s', user, result.task_id, self.name)
+        return result
+
+    def __call__(self, *args, **kwargs):
+        self._task = None
+        logger.debug("%s %s is started.", self.request.id, self.task)
+        try:
+            utils.set_task(self.task)
+            if self.task.status == 'cancel':
+                self.task.status = 'cancelled'
+                return
+            elif self.task.status != 'sent':
+                self.task.status = 'retry_rejected'
+                return
+            self.task.status = 'started'
+            self.task.start_at = timezone.now()
+            self.task.save()
+            retval = self.run(*args, **kwargs)
+            self.task.status = 'ended'
+            return self.handle_retval(retval)
+        except SystemExit:
+            raise  # no log in revoke
+        except:
+            if self.task.status == 'started':
+                self.task.status = 'failed'
+            logger.exception('celery task failed')
+            raise
+        finally:
+            if self.task.status == 'ended':
+                self.task.status = 'succeeded'
+            elif self.task.status == 'started':
+                self.task.status = 'revoked'
+
+            self.task.end_at = timezone.now()
+            self.task.save()
+            utils.reset_task()
+            logger.debug("%s is terminated.", self.request.id)
+
+    def on_success(self, retval, task_id, args, kwargs):
+        if (self.task.status == 'revoked' or self.task.status == 'cancelled' or
+                self.task.status == 'retry_rejected'):
+            return
+        if self.task.status == 'revoking':
+            self.task.status = 'revoked'
+        # self.task.status = 'succeeded'
+        self.task.save()
+        logger.debug("%s is on success.", self.request.id)
+
+    def on_retry(self, exc, task_id, args, kwargs, einfo):
+        self.task.status = 'retried'
+        self.task.stack_trace = str(einfo)
+        self.task.save()
+        logger.debug("%s is on retry. %s", self.request.id, einfo)
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        self.task.status = 'failed'
+        self.task.stack_trace = str(einfo)
+        self.task.save()
+        logger.debug("%s is on failure. %s", self.request.id, einfo)
+
+    def after_return(self, status, retval, task_id, args, kwargs, einfo):
+        if self.task.status == 'succeeded':
+            self.task.result_text = json.dumps(retval)
+            self.task.save()
+        logger.debug("%s after_return status=%s.", self.request.id, status)
+
+    def handle_retval(self, retval):
+        return retval
+
+
+class FileTask_(Task):
+
+    def handle_retval(self, retval):
+        try:
+            filename = '{}.{}'.format(uuid.uuid4().hex, self.ext)
+            if hasattr(retval, 'read'):
+                self.task.result_data.save(filename, File(retval), save=True)
+            elif isinstance(retval, six.text_type):
+                self.task.result_data.save(filename, ContentFile(
+                    retval.encode(getattr(self, 'encode', 'UTF-8'))), save=True)
+            elif isinstance(retval, six.string_types) or isinstance(retval, six.binary_type):
+                self.task.result_data.save(filename, ContentFile(retval), save=True)
+            else:
+                return retval
+            return filename
+        except:
+            logger.exception('FileTask_ failed')
+            raise
+
+
+def FileTask(extension, base_class=FileTask_):
+    name = extension[0].upper() + extension[1].lower()
+    return type('{}FileTask'.format(name), (base_class, ), {'ext': extension})
---
+++ packages/heavy_celery/heavy_celery-0.3.0-py2.py3-none-any.whl/heavy_celery/utils.py
@@ -0,0 +1,101 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+import uuid
+import threading
+from datetime import datetime
+
+from django.conf import settings
+from django.utils import timezone
+# from django.utils.module_loading import import_string
+from croniter import croniter
+
+from . import middlewares
+
+_app = None
+_task = {}
+_user = {}
+_get_request = None
+_worker_id = None
+
+
+def set_task(task):
+    task.worker_id = get_worker_id()
+    task.save(update_fields=['worker_id'])
+    _task[threading.current_thread()] = task
+    _user[threading.current_thread()] = task.user
+
+
+def reset_task():
+    _task.pop(threading.current_thread(), None)
+    _user.pop(threading.current_thread(), None)
+
+
+def get_worker_id():
+    global _worker_id
+    if _worker_id is None:
+        _worker_id = uuid.uuid4().hex
+    return _worker_id
+
+
+def _return_none():
+    return None
+
+
+def _get_global_request(attr=None):
+    th_local = middlewares.GlobalRequestMiddleware.thread_local
+    if hasattr(th_local, 'request'):
+        if attr:
+            return getattr(th_local.request, attr, None)
+        return th_local.request
+    return None
+
+
+def get_user():
+    global _get_request
+
+    user = _user.get(threading.current_thread(), None)
+    if user:
+        return user
+
+    MIDDLEWARE = settings.MIDDLEWARE
+    if MIDDLEWARE is None:
+        MIDDLEWARE = settings.MIDDLEWARE_CLASSES
+
+    if _get_request is None:
+        _get_request = _return_none
+        if 'heavy_celery.middlewares.GlobalRequestMiddleware' in MIDDLEWARE:
+            try:
+                _get_request = _get_global_request
+            except ImportError:
+                pass
+
+        if 'django_busybody.middlewares.GlobalRequestMiddleware' in MIDDLEWARE:
+            try:
+                from django_busybody.tools import get_global_request
+                _get_request = get_global_request
+            except ImportError:
+                pass
+
+        elif 'crequest.middleware.CrequestMiddleware' in MIDDLEWARE:
+            try:
+                from crequest.middleware import CrequestMiddleware
+                _get_request = CrequestMiddleware.get_request
+            except ImportError:
+                pass
+
+    request = _get_request()
+    if request:
+        return request.user
+    return None
+
+
+def get_now():
+    return timezone.localtime(timezone.now()).replace(second=0, microsecond=0)
+
+
+def get_next_cron(cron_expr, now=None):
+    if now is None:
+        now = get_now()
+    return croniter(cron_expr, now).get_next(datetime)
