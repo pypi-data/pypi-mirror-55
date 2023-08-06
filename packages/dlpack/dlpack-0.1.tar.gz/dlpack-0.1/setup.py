ab4f38ac5cecc51b53bf8ab36e13b9ce645e8fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:43 +0000

    Add package dlpack/0.1/dlpack-0.1.tar.gz

---
+++ packages/dlpack/dlpack-0.1.tar.gz/dlpack-0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name = "dlpack",
+    packages = ["dlpack"],
+    version = "0.1",
+    license="Apache-2.0",
+    description = "Machine learning for systems",
+    author = "dlpack",
+    author_email = "tianqi.tchen@gmail.com",
+    url = "https://github.com/dmlc/dlpack",
+    keywords = [],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+  ],
+)
