2358a50a0c11579fd4a924e1f42d0bf4d0fc7e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:04 +0000

    Add package tweepy-trc/0.2.3/tweepy-trc-0.2.3.tar.gz

---
+++ packages/tweepy-trc/tweepy-trc-0.2.3.tar.gz/tweepy-trc-0.2.3/tweepy_trc/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'tweepy-trc'
+__description__ = 'Integrates .trc / .twurlrc credentials into Tweepy'
+__url__ = 'https://github.com/palevell/tweepy-trc'
+__version__ = '0.2.3'
+__build__ = 0x001000
+__author__ = 'Patrick A. Levell'
+__author_email__ = 'patrick29501@gmx.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Patrick A. Levell'
