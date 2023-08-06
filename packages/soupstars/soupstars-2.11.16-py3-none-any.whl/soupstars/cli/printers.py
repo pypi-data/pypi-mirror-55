0f27f0c0ec04135262a2a2d7dac4e3b4291c2b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:07 +0000

    Add package soupstars/2.11.16/soupstars-2.11.16-py3-none-any.whl

---
+++ packages/soupstars/soupstars-2.11.16-py3-none-any.whl/soupstars/cli/printers.py
@@ -0,0 +1,34 @@
+import json
+
+from pygments import highlight, lexers, formatters
+from terminaltables import SingleTable
+
+pygments_style = "native"
+
+
+def jsonify(data):
+    formatted_json = json.dumps(data, sort_keys=True, indent=2)
+    colorful_json = highlight(
+        formatted_json,
+        lexers.JsonLexer(),
+        formatters.Terminal256Formatter(style=pygments_style)
+    )
+    print(colorful_json)
+
+
+def pythonify(module):
+    result = highlight(
+        module,
+        lexers.PythonLexer(),
+        formatters.Terminal256Formatter(style=pygments_style)
+    )
+    print(result)
+
+
+def tableify(headers, rows):
+    data = [headers]
+    for row in rows:
+        row = [str(item) if item else '' for item in row]
+        data.append(row)
+    table = SingleTable(data)
+    print(table.table)
