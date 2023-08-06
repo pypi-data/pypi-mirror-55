d10020fa0ec31abe1eb4cdebe1fb129929067279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:24 +0000

    Add package recipe-scrapers/5.1.0/recipe_scrapers-5.1.0.tar.gz

---
+++ packages/recipe-scrapers/recipe_scrapers-5.1.0.tar.gz/recipe_scrapers-5.1.0/recipe_scrapers/bonappetit.py
@@ -0,0 +1,55 @@
+from ._abstract import AbstractScraper
+from ._utils import normalize_string, get_yields
+
+
+class BonAppetit(AbstractScraper):
+
+    @classmethod
+    def host(self):
+        return 'bonappetit.com'
+
+    def title(self):
+        return self.soup.find(
+            'h1',
+            {'itemprop': 'name'}
+        ).get_text()
+
+    def total_time(self):
+        return 0
+
+    def yields(self):
+        return get_yields(
+            self.soup.find(
+                'span',
+                {'class': "recipe__header__servings recipe__header__servings--basically"}
+            ).find('span')
+        )
+
+    def image(self):
+        image = self.soup.find(
+            'meta',
+            {'property': 'og:image', 'content': True}
+        )
+        return image['content'] if image else None
+
+    def ingredients(self):
+        ingredients = self.soup.findAll(
+            'li',
+            {'class': "ingredient"}
+        )
+
+        return [
+            normalize_string(ingredient.get_text())
+            for ingredient in ingredients
+        ]
+
+    def instructions(self):
+        instructions = self.soup.find(
+            'div',
+            {'class': 'steps-wrapper'}
+        ).findAll('li')
+
+        return '\n'.join([
+            normalize_string(instruction.get_text())
+            for instruction in instructions
+        ])
