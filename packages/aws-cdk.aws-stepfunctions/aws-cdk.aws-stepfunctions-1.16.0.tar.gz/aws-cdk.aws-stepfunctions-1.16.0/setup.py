22d633ebc4a44d0a57a1f86a1aba5f941531acc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:12 +0000

    Add package aws-cdk.aws-stepfunctions/1.16.0/aws-cdk.aws-stepfunctions-1.16.0.tar.gz

---
+++ packages/aws-cdk.aws-stepfunctions/aws-cdk.aws-stepfunctions-1.16.0.tar.gz/aws-cdk.aws-stepfunctions-1.16.0/setup.py
@@ -0,0 +1,46 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-stepfunctions",
+    "version": "1.16.0",
+    "description": "The CDK Construct Library for AWS::StepFunctions",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_stepfunctions",
+        "aws_cdk.aws_stepfunctions._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_stepfunctions._jsii": [
+            "aws-stepfunctions@1.16.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_stepfunctions": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.2",
+        "publication>=0.0.3",
+        "aws-cdk.aws-cloudwatch~=1.16,>=1.16.0",
+        "aws-cdk.aws-events~=1.16,>=1.16.0",
+        "aws-cdk.aws-iam~=1.16,>=1.16.0",
+        "aws-cdk.core~=1.16,>=1.16.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
