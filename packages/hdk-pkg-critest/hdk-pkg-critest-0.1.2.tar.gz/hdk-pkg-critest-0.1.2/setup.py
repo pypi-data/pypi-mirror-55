4fb27531ac13809c1580c25fc85d0c65fc9cc168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:33 +0000

    Add package hdk-pkg-critest/0.1.2/hdk-pkg-critest-0.1.2.tar.gz

---
+++ packages/hdk-pkg-critest/hdk-pkg-critest-0.1.2.tar.gz/hdk-pkg-critest-0.1.2/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+@author: He Dekun
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "hdk-pkg-critest", # Replace with your own username
+    version = "0.1.2",
+    author = "He Dekun",
+    author_email = "hede0001@e.ntu.edu.sg",
+    description = "A small example package for CRI PMT test.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/HDKidd/hdk-pkg-critest",
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
