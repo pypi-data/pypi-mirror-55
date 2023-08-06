ba1fd1d150fbe99727603f44b75ee1ca73c5ae5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:25 +0000

    Add package s2/0.1.1/s2-0.1.1.tar.gz

---
+++ packages/s2/s2-0.1.1.tar.gz/s2-0.1.1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open('requirements.txt', encoding='utf-8') as f:
+    requirements = [line.strip() for line in f if line]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Joao Carabetta",
+    author_email='joao.carabetta@gmail.com',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Python bindings for S2, a hierarchical square geospatial indexing system.",
+    entry_points={
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    name='s2',
+    packages=find_packages(include=['s2', 's2.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/JoaoCarabetta/s2_py',
+    version='0.1.1',
+    zip_safe=False,
+)
