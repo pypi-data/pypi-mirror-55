7eadd182f2a87bfeda558e797260209e24710052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:01 +0000

    Add package osrm-py/0.5/osrm-py-0.5.tar.gz

---
+++ packages/osrm-py/osrm-py-0.5.tar.gz/osrm-py-0.5/osrm.py
@@ -0,0 +1,359 @@
+import asyncio
+import collections.abc
+import enum
+import logging
+import numbers
+import random
+
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+
+from urllib.parse import urlencode
+
+logger = logging.getLogger(__name__)
+
+try:
+    import aiohttp
+except ImportError:
+    aiohttp = None
+
+try:
+    import requests
+except ImportError:
+    requests = None
+
+if not (aiohttp or requests):
+    logger.error('Could not import none of modules \'aiohttp\' or \'requests\'')
+
+
+class overview(enum.Enum):
+    simplified = 'simplified'
+    full = 'full'
+    false = 'false'
+
+
+# alias for avoiding name collision
+osrm_overview = overview
+
+
+class geometries(enum.Enum):
+    polyline = 'polyline'
+    polyline6 = 'polyline6'
+    geojson = 'geojson'
+
+
+# alias for avoiding name collision
+osrm_geometries = geometries
+
+
+class gaps(enum.Enum):
+    split = 'split'
+    ignore = 'ignore'
+
+
+# alias for avoiding name collision
+osrm_gaps = gaps
+
+
+class continue_straight(enum.Enum):
+    default = 'default'
+    true = 'true'
+    false = 'false'
+
+
+# alias for avoiding name collision
+osrm_continue_straight = continue_straight
+
+
+class OSRMException(Exception):
+    pass
+
+
+class OSRMServerException(OSRMException):
+    pass
+
+
+class OSRMClientException(OSRMException):
+    pass
+
+
+def _check_pairs(items):
+    ''' checking that 'items' has format [[Number, Number], ...]'''
+    return (
+        isinstance(items, collections.abc.Iterable) and
+        all([isinstance(p, collections.abc.Iterable) for p in items]) and
+        all([
+            isinstance(p[0], numbers.Number) and
+            isinstance(p[1], numbers.Number) and
+            len(p) == 2
+            for p in items]))
+
+
+class BaseRequest:
+
+    def __init__(self, coordinates, radiuses=[], bearings=[], hints=[]):
+        assert _check_pairs(coordinates), \
+            '''coordinates must be in format [[longitude, latitude],...]'''
+        assert all([
+            -180 <= lon <= 180 and -90 <= lat <= 90
+            for lon, lat in coordinates]), \
+            ''''longitude' should be -180..180 and 'latitude' should be -90..90 (actual: {})'''.format(coordinates)
+        assert _check_pairs(bearings), \
+            '''bearings must be in format [[value, range],...]'''
+        assert all([
+            0 <= bvalue <= 360 and 0 <= brange <= 180
+            for bvalue, brange in bearings]), \
+            '''bearing 'value' should be 0..360 and 'range' should be 0..180 (actual: {})'''.format(bearings)
+        assert isinstance(radiuses, list)
+        assert isinstance(bearings, list)
+        assert isinstance(hints, list)
+
+        self.coordinates = coordinates
+        self.radiuses = radiuses
+        self.bearings = bearings
+        self.hints = hints
+
+    def get_coordinates(self):
+        return self._encode_pairs(self.coordinates)
+
+    def get_options(self):
+        return {
+            'radiuses': self._encode_array(self.radiuses),
+            'bearings': self._encode_pairs(self.bearings),
+            'hints': self._encode_array(self.hints)
+        }
+
+    def _encode_array(self, value):
+        return ';'.join(map(lambda x: str(x) if x else "", value))
+
+    def _encode_bool(self, value):
+        return 'true' if value else 'false'
+
+    def _encode_pairs(self, coordinates):
+        return ';'.join([','.join(map(str, coord)) for coord in coordinates])
+
+    def decode_response(self, url, status, response):
+        if status == 200:
+            return json.loads(response)
+        elif status == 400:
+            raise OSRMClientException(json.loads(response))
+        raise OSRMServerException(url, response)
+
+
+class NearestRequest(BaseRequest):
+
+    service = 'nearest'
+
+    def __init__(self, number=1, **kwargs):
+        super().__init__(**kwargs)
+        self.number = number
+
+    def get_options(self):
+        options = super().get_options()
+        options['number'] = self.number
+        return options
+
+
+class RouteRequest(BaseRequest):
+
+    service = 'route'
+
+    def __init__(
+            self,
+            alternatives=False,
+            steps=False, annotations=False,
+            geometries=geometries.geojson,
+            overview=overview.simplified,
+            continue_straight=continue_straight.default,
+            **kwargs):
+        super().__init__(**kwargs)
+
+        assert isinstance(alternatives, bool)
+        assert isinstance(steps, bool)
+        assert isinstance(annotations, bool)
+        assert isinstance(geometries, osrm_geometries)
+        assert isinstance(overview, osrm_overview)
+        assert isinstance(continue_straight, osrm_continue_straight)
+
+        self.alternatives = alternatives
+        self.steps = steps
+        self.annotations = annotations
+        self.geometries = geometries
+        self.overview = overview
+        self.continue_straight = continue_straight
+
+    def get_options(self):
+        options = super().get_options()
+        options.update({
+            'alternatives': self._encode_bool(self.alternatives),
+            'steps':        self._encode_bool(self.steps),
+            'annotations':  self._encode_bool(self.annotations),
+            'geometries':   self.geometries.value,
+            'overview':     self.overview.value
+        })
+        if self.continue_straight != continue_straight.default:
+            options['continue_straight'] = self.continue_straight.value
+        return options
+
+
+class MatchRequest(RouteRequest):
+
+    service = 'match'
+
+    def __init__(
+            self,
+            timestamps=[],
+            gaps=gaps.split,
+            tidy=False,
+            **kwargs):
+        super().__init__(**kwargs)
+        assert isinstance(timestamps, list)
+        assert isinstance(gaps, osrm_gaps)
+        assert isinstance(tidy, bool)
+
+        self.timestamps = timestamps
+        self.gaps = gaps
+        self.tidy = tidy
+
+    def get_options(self):
+        options = super().get_options()
+        options.pop('alternatives', None)
+        options['timestamps'] = self._encode_array(self.timestamps)
+
+        # Don't send default values (for compatibility with 5.6)
+        if self.gaps.value != osrm_gaps.split:
+            options['gaps'] = self.gaps.value
+        if self.tidy:
+            options['tidy'] = self._encode_bool(self.tidy)
+        return options
+
+
+class BaseClient:
+
+    def __init__(
+            self,
+            host='http://localhost:5000',
+            version='v1', profile='driving',
+            timeout=5, max_retries=5):
+        assert isinstance(host, str)
+        assert isinstance(version, str)
+        assert isinstance(profile, str)
+        assert isinstance(timeout, numbers.Number)
+        assert isinstance(max_retries, int) and max_retries >= 1
+
+        self.host = host
+        self.version = version
+        self.profile = profile
+        self.timeout = timeout
+        self.max_retries = max_retries
+
+    def _build_request(self, request):
+        url = '{host}/{service}/{version}/{profile}/{coordinates}'.format(
+            host=self.host,
+            service=request.service,
+            version=self.version,
+            profile=self.profile,
+            coordinates=request.get_coordinates())
+        params = {
+            k: v
+            for k, v in request.get_options().items()
+            if v
+        }
+        logger.debug('request url=%s; params=%s', url, params)
+        return (url, params)
+
+
+class Client(BaseClient):
+
+    def __init__(self, *args, session=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not requests:
+            raise RuntimeError('Module \'requests\' is not available')
+        self.session = session or requests.Session()
+        self.a = requests.adapters.HTTPAdapter(max_retries=self.max_retries)
+        self.session.mount('http://', self.a)
+
+    def nearest(self, **kwargs):
+        return self._request(
+            NearestRequest(**kwargs)
+        )
+
+    def route(self, **kwargs):
+        return self._request(
+            RouteRequest(**kwargs)
+        )
+
+    def match(self, **kwargs):
+        return self._request(
+            MatchRequest(**kwargs)
+        )
+
+    def _request(self, request):
+        if not requests:
+            raise RuntimeError('Module \'requests\' is not available')
+        url, params = self._build_request(request)
+        response = self.session.get(url, params=params, timeout=self.timeout)
+        return request.decode_response(url, response.status_code, response.text)
+
+
+class AioHTTPClient(BaseClient):
+
+    BACKOFF_MAX = 120
+    BACKOFF_FACTOR = 0.5
+
+    def __init__(self, *args, session=None, loop=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not aiohttp:
+            raise RuntimeError('Module \'aiohttp\' is not available')
+        if not session:
+            self.loop = loop or asyncio.get_event_loop()
+            self.session = aiohttp.ClientSession(loop=self.loop)
+        else:
+            self.session = session
+
+    async def nearest(self, **kwargs):
+        return await self._request(
+            NearestRequest(**kwargs)
+        )
+
+    async def route(self, **kwargs):
+        return await self._request(
+            RouteRequest(**kwargs)
+        )
+
+    async def match(self, **kwargs):
+        return await self._request(
+            MatchRequest(**kwargs)
+        )
+
+    def exp_backoff(self, attempt):
+        timeout = min(self.timeout * (2 ** attempt), self.BACKOFF_MAX)
+        return timeout + random.uniform(0, self.BACKOFF_FACTOR * timeout)
+
+    async def _request(self, request):
+        url, params = self._build_request(request)
+        attempt = 0
+        while attempt < self.max_retries:
+            try:
+                # This is a workaround for the https://github.com/aio-libs/aiohttp/issues/1901
+                request_url = "{}?{}".format(url, urlencode(params))
+                async with self.session.get(
+                        request_url, timeout=self.timeout) as response:
+                    body = await response.text()
+                    return request.decode_response(
+                        response.url, response.status, body)
+            except asyncio.TimeoutError:
+                timeout = self.exp_backoff(attempt)
+                logger.info(
+                    'Timeout error url=%s (remaining tries %s, sleeping %.2f secs)',
+                    url, self.max_retries - attempt, timeout)
+                await asyncio.sleep(timeout)
+                attempt += 1
+
+        raise OSRMServerException(url, 'server timeout')
+
+    async def close(self):
+        await self.session.close()
---
+++ packages/osrm-py/osrm-py-0.5.tar.gz/osrm-py-0.5/test.py
@@ -0,0 +1,228 @@
+import asyncio
+import functools
+import logging
+import unittest
+from unittest.mock import MagicMock
+
+import aiohttp
+
+import osrm
+
+logging.basicConfig(level=logging.DEBUG)
+
+OSRM_HOST = 'http://127.0.0.1:5000'
+SAMPLE_ROUTE = [[-74.005482, 40.679220], [-74.005389, 40.679495],
+                [-74.005014, 40.679746], [-74.004927, 40.679718],
+                [-74.004694, 40.679651]]
+
+
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        self.client = osrm.Client(host=OSRM_HOST, timeout=1)
+        self.mock_session = MagicMock()
+        self.mock_client = osrm.Client(
+            host=OSRM_HOST, session=self.mock_session)
+
+    def test_server_error(self):
+        self.mock_session.get.return_value = MagicMock(
+            status_code=500,
+            text='unexpected error')
+        with self.assertRaises(osrm.OSRMServerException):
+            self.mock_client.nearest(
+                coordinates=[[-74.0056, 40.6197]],
+                number=10
+            )
+
+    def test_nearest(self):
+        response = self.client.nearest(
+            coordinates=[[-74.00578245683002, 40.60600816104437]],
+            radiuses=[70],
+            number=20
+        )
+        assert response['code'] == 'Ok'
+        assert len(response['waypoints']) == 12
+
+        response = self.client.nearest(
+            coordinates=[[-74.00578245683002, 40.60600816104437]],
+            radiuses=[70],
+            bearings=[[45, 30]],
+            number=20
+        )
+        assert response['code'] == 'Ok'
+        assert len(response['waypoints']) == 3
+
+        with self.assertRaises(AssertionError):
+            self.client.nearest(
+                coordinates=[[-200, 100]]
+            )
+        with self.assertRaises(AssertionError):
+            self.client.nearest(
+                coordinates=[[-74.00578245683002, 40.60600816104437]],
+                bearings=[[720, 360]],
+            )
+
+        with self.assertRaises(osrm.OSRMClientException) as cm:
+            self.client.nearest(
+                coordinates=[[-74.00578245683002, 40.60600816104437]],
+                radiuses=[10],
+                number=10
+            )
+        assert cm.exception.args[0]['code'] == 'NoSegment'
+
+    def test_route(self):
+        response = self.client.route(
+            coordinates=[[-74.0056, 40.6197], [-74.0034, 40.6333]],
+            overview=osrm.overview.full
+        )
+        assert response['code'] == 'Ok'
+
+        response = self.client.route(
+            coordinates=[[-74.0056, 40.6197], [-74.0034, 40.6333]],
+            overview=osrm.overview.full,
+            continue_straight=osrm.continue_straight.true
+        )
+        assert response['code'] == 'Ok'
+
+    def test_match(self):
+        response = self.client.match(
+            coordinates=SAMPLE_ROUTE,
+            radiuses=[9, 9, 10, 3, 3],
+            overview=osrm.overview.full
+        )
+        assert response['code'] == 'Ok'
+
+        response = self.client.match(
+            coordinates=SAMPLE_ROUTE,
+            radiuses=[9, 9, 10, 3, 3],
+            timestamps=[10, 11, 1000, 1001, 1002],
+            gaps=osrm.gaps.split
+        )
+        assert response['code'] == 'Ok'
+        assert len(response['matchings']) == 2
+
+        response = self.client.match(
+            coordinates=SAMPLE_ROUTE,
+            radiuses=[9, 9, 10, 3, 3],
+            timestamps=[10, 11, 1000, 1001, 1002],
+            gaps=osrm.gaps.ignore
+        )
+        assert response['code'] == 'Ok'
+        assert len(response['matchings']) == 1
+
+
+def run_in_loop(f):
+    @functools.wraps(f)
+    def wrapper(testcase, *args, **kwargs):
+        coro = asyncio.coroutine(f)
+        future = asyncio.wait_for(coro(testcase, *args, **kwargs), timeout=15)
+        return testcase.loop.run_until_complete(future)
+    return wrapper
+
+
+class TestAioHTTPClient(unittest.TestCase):
+
+    def setUp(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+        async def _setUp():
+            self.session = aiohttp.ClientSession(loop=self.loop)
+            self.client = osrm.AioHTTPClient(
+                host=OSRM_HOST, session=self.session, timeout=0.2
+            )
+
+        self.mock_session = MagicMock()
+        self.mock_client = osrm.AioHTTPClient(
+            host=OSRM_HOST, session=self.mock_session,
+            timeout=0.2, max_retries=3
+        )
+
+        self.loop.run_until_complete(_setUp())
+
+    def tearDown(self):
+        self.loop.run_until_complete(self.session.close())
+        self.loop.close()
+
+    @run_in_loop
+    async def test_nearest(self):
+        response = await self.client.nearest(
+            coordinates=[[-74.0056, 40.6197]],
+            number=10
+        )
+        assert response['code'] == 'Ok'
+
+    @run_in_loop
+    async def test_match(self):
+        response = await self.client.match(
+            coordinates=SAMPLE_ROUTE,
+            radiuses=[9, 9, 10, 3, 3],
+            overview=osrm.overview.full,
+            tidy=False,
+            gaps=osrm.gaps.split
+        )
+        assert response['code'] == 'Ok'
+
+    @run_in_loop
+    async def test_route(self):
+        response = await self.client.route(
+            coordinates=[[-74.0056, 40.6197], [-74.0034, 40.6333]],
+            overview=osrm.overview.full
+        )
+        assert response['code'] == 'Ok'
+
+    @run_in_loop
+    async def test_retry(self):
+        counter = 0
+
+        def mock_get(url, **kwargs):
+            nonlocal counter
+            if counter < 2:
+                counter += 1
+                raise asyncio.TimeoutError('timeout')
+            return aiohttp.client._RequestContextManager(
+                asyncio.coroutine(
+                    lambda: MagicMock(
+                        status=200,
+                        url=url,
+                        text=asyncio.coroutine(lambda: '{}')
+                    )
+                )()
+            )
+
+        self.mock_session.get = mock_get
+
+        await self.mock_client.nearest(
+            coordinates=[[-74.0056, 40.6197]], number=10
+        )
+        assert counter == 2
+
+    @run_in_loop
+    async def test_exceeded_max_retry(self):
+        counter = 0
+
+        def mock_get(url, **kwargs):
+            nonlocal counter
+            counter += 1
+            raise asyncio.TimeoutError('timeout')
+
+        self.mock_session.get = mock_get
+        with self.assertRaises(osrm.OSRMServerException) as cm:
+            await self.mock_client.nearest(
+                coordinates=[[-74.0056, 40.6197]], number=10
+            )
+        assert cm.exception.args[1] == 'server timeout'
+        assert counter == self.mock_client.max_retries
+
+    @run_in_loop
+    async def test_real_timeout(self):
+        client = osrm.AioHTTPClient(
+            host=OSRM_HOST, timeout=0.01, max_retries=1)
+        with self.assertRaises(osrm.OSRMServerException) as cm:
+            await client.match(
+                coordinates=[[-73.999, 40.724], [-73.994, 40.728]],
+                radiuses=[150, 150]
+            )
+            await client.close()
+
+        assert cm.exception.args[1] == 'server timeout'
