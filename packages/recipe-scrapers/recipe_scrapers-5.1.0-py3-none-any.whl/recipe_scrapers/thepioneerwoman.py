a35912f80681af6768a16b1d823f6ccb4932aea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:23 +0000

    Add package recipe-scrapers/5.1.0/recipe_scrapers-5.1.0-py3-none-any.whl

---
+++ packages/recipe-scrapers/recipe_scrapers-5.1.0-py3-none-any.whl/recipe_scrapers/thepioneerwoman.py
@@ -0,0 +1,55 @@
+from ._abstract import AbstractScraper
+from ._utils import get_minutes, normalize_string, get_yields
+
+
+class ThePioneerWoman(AbstractScraper):
+
+    @classmethod
+    def host(self):
+        return 'thepioneerwoman.com'
+
+    def title(self):
+        return self.soup.find(
+            'h3',
+            {'class': 'recipe-title'}
+        ).get_text()
+
+    def total_time(self):
+        return sum([
+            get_minutes(dd)
+            for dd in self.soup.find(
+                'div',
+                {'class': 'recipe-summary-time'}
+            ).findAll('dd')
+        ])
+
+    def yields(self):
+        return get_yields(self.soup.find(
+            'span', {'itemprop': 'recipeYield'}
+        ))
+
+    def image(self):
+        image = self.soup.find(
+            'meta',
+            {'property': 'og:image', 'content': True}
+        )
+        return image['content'] if image else None
+
+    def ingredients(self):
+        ingredients = self.soup.find(
+            'ul',
+            {'class': "list-ingredients"}
+        ).findAll('li')
+
+        return [
+            normalize_string(ingredient.get_text())
+            for ingredient in ingredients
+        ]
+
+    def instructions(self):
+        instructions = self.soup.findAll(
+            'div',
+            {'class': 'panel-body'}
+        )[-1]
+
+        return normalize_string(instructions.get_text()).replace('.', '.\n')
