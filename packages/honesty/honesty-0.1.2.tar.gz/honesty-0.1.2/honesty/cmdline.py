4389c7fd9df250c8fcb3588ed094eaa92acb9a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:50 +0000

    Add package honesty/0.1.2/honesty-0.1.2.tar.gz

---
+++ packages/honesty/honesty-0.1.2.tar.gz/honesty-0.1.2/honesty/cmdline.py
@@ -0,0 +1,173 @@
+import asyncio
+import functools
+import sys
+from pathlib import Path
+from typing import Any, List, Optional
+
+import click
+import pkg_resources
+
+from honesty.__version__ import __version__
+from honesty.api import async_download_many
+from honesty.cache import Cache
+from honesty.checker import has_nativemodules, is_pep517, run_checker
+from honesty.releases import Package, async_parse_index, parse_index
+
+
+# TODO type
+def wrap_async(coro: Any) -> Any:
+    @functools.wraps(coro)
+    def inner(*args: Any, **kwargs: Any) -> Any:
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(coro(*args, **kwargs))
+
+    return inner
+
+
+@click.group()
+@click.version_option(__version__, prog_name="honesty")
+def cli() -> None:
+    pass
+
+
+@cli.command(help="List available archives")
+@click.option("--fresh", "-f", is_flag=True, type=bool)
+@click.argument("package_name")
+@wrap_async
+async def list(fresh: bool, package_name: str) -> None:
+    async with Cache(fresh_index=fresh) as cache:
+        package = await async_parse_index(package_name, cache)
+
+    print(f"package {package.name}")
+    print("releases:")
+    for k, v in package.releases.items():
+        print(f"  {k}:")
+        for f in v.files:
+            print(f"    {f.basename}")
+
+
+@cli.command(help="Check for consistency among archives")
+@click.option("--verbose", "-v", is_flag=True, type=bool)
+@click.option("--fresh", "-f", is_flag=True, type=bool)
+@click.argument("package_name")
+def check(verbose: bool, fresh: bool, package_name: str) -> None:
+    with Cache(fresh_index=fresh) as cache:
+        package_name, operator, version = package_name.partition("==")
+        package = parse_index(package_name, cache)
+        selected_versions = select_versions(package, operator, version)
+
+        if verbose:
+            click.echo(f"check {package_name} {selected_versions}")
+
+        rc = 0
+        for v in selected_versions:
+            rc |= run_checker(package, v, verbose=verbose, cache=cache)
+
+    if rc != 0:
+        sys.exit(rc)
+
+
+@cli.command(help="Check for presence of pep517 markers")
+@click.option("--verbose", "-v", is_flag=True, type=bool)
+@click.option("--fresh", "-f", is_flag=True, type=bool)
+@click.argument("package_name")
+def ispep517(verbose: bool, fresh: bool, package_name: str) -> None:
+    with Cache(fresh_index=fresh) as cache:
+        package_name, operator, version = package_name.partition("==")
+        package = parse_index(package_name, cache)
+        selected_versions = select_versions(package, operator, version)
+
+        if verbose:
+            click.echo(f"check {package_name} {selected_versions}")
+
+        rc = 0
+        for v in selected_versions:
+            rc |= is_pep517(package, v, verbose=verbose, cache=cache)
+
+    if rc != 0:
+        sys.exit(rc)
+
+
+@cli.command(help="Check for native modules in bdist")
+@click.option("--verbose", "-v", is_flag=True, type=bool)
+@click.option("--fresh", "-f", is_flag=True, type=bool)
+@click.argument("package_name")
+def native(verbose: bool, fresh: bool, package_name: str) -> None:
+    with Cache(fresh_index=fresh) as cache:
+        package_name, operator, version = package_name.partition("==")
+        package = parse_index(package_name, cache)
+        selected_versions = select_versions(package, operator, version)
+
+        if verbose:
+            click.echo(f"check {package_name} {selected_versions}")
+
+        rc = 0
+        for v in selected_versions:
+            rc |= has_nativemodules(package, v, verbose=verbose, cache=cache)
+
+    if rc != 0:
+        sys.exit(rc)
+
+
+@cli.command(help="Download an sdist, print path on stdout")
+@click.option("--verbose", "-v", is_flag=True, type=bool)
+@click.option("--fresh", "-f", is_flag=True, type=bool)
+@click.option("--dest", help="Directory to store in", default="")
+@click.option(
+    "--index-url", help="Alternate index url (uses HONESTY_INDEX_URL or pypi by default"
+)
+@click.argument("package_name")
+@wrap_async
+async def download(
+    verbose: bool, fresh: bool, dest: str, index_url: Optional[str], package_name: str
+) -> None:
+    dest_path: Optional[Path]
+    if dest:
+        dest_path = Path(dest)
+        dest_path.mkdir(parents=True, exist_ok=True)
+    else:
+        dest_path = None
+
+    async with Cache(fresh_index=fresh, index_url=index_url) as cache:
+        package_name, operator, version = package_name.partition("==")
+        package = await async_parse_index(package_name, cache)
+        selected_versions = select_versions(package, operator, version)
+
+        if verbose:
+            click.echo(f"check {package_name} {selected_versions}")
+
+        rc = await async_download_many(
+            package, versions=selected_versions, dest=dest_path, cache=cache
+        )
+
+    sys.exit(rc)
+
+
+def select_versions(package: Package, operator: str, selector: str) -> List[str]:
+    """
+    Given operator='==' and selector='*' or '2.0', return a list of the matching
+    versions, in increasing order.
+    """
+    if not package.releases:
+        raise click.ClickException(f"No releases at all for {package.name}")
+
+    if operator not in ("", "=="):
+        raise click.ClickException("Only '==' is supported")
+
+    if selector == "":
+        # latest
+        version = sorted(package.releases, key=pkg_resources.parse_version)[-1]
+        return [version]
+    elif selector == "*":
+        versions: List[str] = sorted(package.releases, key=pkg_resources.parse_version)
+        return versions
+    else:
+        if selector not in package.releases:
+            raise click.ClickException(
+                f"The version {selector} does not exist for {package.name}"
+            )
+        return [selector]
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/honesty/honesty-0.1.2.tar.gz/honesty-0.1.2/honesty/releases.py
@@ -0,0 +1,135 @@
+import asyncio
+import enum
+import re
+from dataclasses import dataclass
+from typing import Any, Dict, List, Match, Optional, Tuple
+
+from .cache import Cache
+
+# Apologies in advance, "parsing" html via regex
+ENTRY_RE = re.compile(
+    r'href="(?P<url>[^"#]+\/(?P<basename>[^#]+))#(?P<checksum>[^="]+=[a-f0-9]+)"'
+)
+NUMERIC_VERSION = re.compile(
+    r"^(?P<package>.*?)-(?P<version>[0-9][^-]*?)"
+    r"(?P<suffix>(?P<platform>\.macosx|\.linux)?-.*)?$"
+)
+
+
+SDIST_EXTENSIONS = (".tar.gz", ".zip", ".tar.bz2")
+
+# TODO IntFlag, or a separate field for platform
+class FileType(enum.IntEnum):
+    UNKNOWN = 0
+    SDIST = 1
+    BDIST_WHEEL = 2
+    BDIST_EGG = 3
+    BDIST_DUMB = 4
+
+
+class UnexpectedFilename(Exception):
+    pass
+
+
+def guess_file_type(filename: str) -> FileType:
+    if filename.endswith(".egg"):
+        return FileType.BDIST_EGG
+    elif filename.endswith(".whl"):
+        return FileType.BDIST_WHEEL
+    elif filename.endswith(SDIST_EXTENSIONS):
+        filename = remove_suffix(filename)
+        match = NUMERIC_VERSION.match(filename)
+        # Some oddly-named files are not likely to be loaded by pip either.
+        if match is None:
+            raise UnexpectedFilename(filename)
+        # bdist_dumb can't be easily discerned
+        if match.group("platform"):
+            return FileType.BDIST_DUMB
+        elif match.group("suffix") and match.group("suffix").startswith("-macosx"):
+            return FileType.BDIST_DUMB
+        return FileType.SDIST
+    else:  # .exe and .rpm at least
+        return FileType.UNKNOWN
+
+
+@dataclass
+class FileEntry:
+    url: str  # https://files.pythonhosted.../foo-1.0.tgz
+    basename: str  # foo-1.0.tgz
+    checksum: str  # 'sha256=<foo>'
+    file_type: FileType
+    version: str  # TODO: better type
+    requires_python: Optional[str] = None  # '&gt;=3.6'
+    # TODO extract upload date?
+
+    @classmethod
+    def from_entry_match(cls, match: Match[Any]) -> "FileEntry":
+        d = match.groupdict()
+        d["file_type"] = guess_file_type(d["basename"])
+        d["version"] = guess_version(d["basename"])[1]
+        obj = cls(**d)
+        return obj
+
+
+@dataclass
+class PackageRelease:
+    version: str
+    files: List[FileEntry]
+
+
+@dataclass
+class Package:
+    name: str
+    releases: Dict[str, PackageRelease]
+
+
+def remove_suffix(basename: str) -> str:
+    suffixes = [".egg", ".whl", ".zip", ".gz", ".bz2", ".tar"]
+    for s in suffixes:
+        if basename.endswith(s):
+            basename = basename[: -len(s)]
+    return basename
+
+
+# TODO itu-r-468-weighting-1.0.3.tar.gz
+# TODO uttt-0.3-1.tar.gz
+def guess_version(basename: str) -> Tuple[str, str]:
+    """
+    Returns (package name, version) or raises.
+    """
+    # This should use whatever setuptools/pip/etc use, but I spent about 10
+    # minutes and couldn't find it tonight.
+    basename = remove_suffix(basename)
+
+    match = NUMERIC_VERSION.match(basename)
+    if not match:
+        raise UnexpectedFilename(basename)
+    return match.group(1), match.group(2)
+
+
+def parse_index(pkg: str, cache: Cache, strict: bool = False) -> Package:
+    loop = asyncio.get_event_loop()
+    package: Package = loop.run_until_complete(async_parse_index(pkg, cache, strict))
+    return package
+
+
+async def async_parse_index(pkg: str, cache: Cache, strict: bool = False) -> Package:
+    package = Package(name=pkg, releases={})
+    with open(await cache.async_fetch(pkg, url=None)) as f:
+        for match in ENTRY_RE.finditer(f.read()):
+            # TODO guess_version can also raise this, but they currently use the
+            # same regex; this should get parsed out once maybe in a method on
+            # FileEntry.
+            try:
+                fe = FileEntry.from_entry_match(match)
+            except UnexpectedFilename:
+                if not strict:
+                    continue
+                raise
+
+            v = fe.version
+            if v not in package.releases:
+                package.releases[v] = PackageRelease(version=v, files=[])
+            package.releases[v].files.append(fe)
+
+    return package
