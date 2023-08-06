7c31774412d5ace286abeefd0aba0ceba2aa1d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:46 +0000

    Add package dynastes/0.1.3/dynastes-0.1.3.tar.gz

---
+++ packages/dynastes/dynastes-0.1.3.tar.gz/dynastes-0.1.3/dynastes/blocks/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .localized_attention_blocks import LocalizedSelfAttentionBlock1D
+
+# Cleanup symbols to avoid polluting namespace.
+del absolute_import
+del division
+del print_function
