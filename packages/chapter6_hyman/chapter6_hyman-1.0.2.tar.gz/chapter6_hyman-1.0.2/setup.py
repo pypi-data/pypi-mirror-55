25027132de6d35040e5419f944cbdf6d4e0946bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:29 +0000

    Add package chapter6_hyman/1.0.2/chapter6_hyman-1.0.2.tar.gz

---
+++ packages/chapter6_hyman/chapter6_hyman-1.0.2.tar.gz/chapter6_hyman-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'chapter6_hyman',
+        version = '1.0.2',
+        py_modules = ['chapter6_hyman'],
+        author = 'hyman',
+        author_email = '578046099@qq.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of athletelist,继承类/派生子类',
+
+    )
