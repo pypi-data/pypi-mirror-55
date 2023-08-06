ebd40c589cfd4324140b0463b9c1cc93123d381d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:20 +0000

    Add package firestore-user-authentication/0.0.1/firestore%20user%20authentication-0.0.1.tar.gz

---
+++ packages/firestore-user-authentication/firestore%20user%20authentication-0.0.1.tar.gz/firestore user authentication-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='firestore user authentication',
+        version='0.0.1',
+        license="GNU",
+        description='Firestore user authentication function for Python',
+        author='Ryan Chen',
+        author_email='ryanjchen2@gmail.com',
+        url='https://github.com/fatcat2/firestore-auth',
+        packages=['firestore_auth'],
+        keywords=["google", "cloud", "firestore", "authentication"],
+        install_requires=["google-cloud-firestore"],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+            'Programming Language :: Python :: 3.7',
+        ],
+)
