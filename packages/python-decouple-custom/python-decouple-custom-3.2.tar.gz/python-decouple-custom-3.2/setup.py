b6f0dd89dd1562c459175caf99861283ea873229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:09 +0000

    Add package python-decouple-custom/3.2/python-decouple-custom-3.2.tar.gz

---
+++ packages/python-decouple-custom/python-decouple-custom-3.2.tar.gz/python-decouple-custom-3.2/setup.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+from setuptools import setup
+import os
+
+
+README = os.path.join(os.path.dirname(__file__), "README.rst")
+
+setup(
+    name="python-decouple-custom",
+    version="3.2",
+    description="Strict separation of settings from code.",
+    long_description=open(README).read(),
+    author="Alex Seitsinger",
+    author_email="software@alexseitsinger.com",
+    license="MIT",
+    py_modules=["decouple"],
+    zip_safe=False,
+    platforms="any",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Flask",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+    url="http://github.com/alexseitsinger/python-decouple-custom/",
+)
