df45a290003be9bba88e63423939379db0240d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:01 +0000

    Add package aws-cdk.aws-route53/1.16.2/aws-cdk.aws-route53-1.16.2.tar.gz

---
+++ packages/aws-cdk.aws-route53/aws-cdk.aws-route53-1.16.2.tar.gz/aws-cdk.aws-route53-1.16.2/setup.py
@@ -0,0 +1,46 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-route53",
+    "version": "1.16.2",
+    "description": "CDK Constructs for AWS Route53",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_route53",
+        "aws_cdk.aws_route53._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_route53._jsii": [
+            "aws-route53@1.16.2.jsii.tgz"
+        ],
+        "aws_cdk.aws_route53": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.4",
+        "publication>=0.0.3",
+        "aws-cdk.aws-ec2~=1.16,>=1.16.2",
+        "aws-cdk.aws-logs~=1.16,>=1.16.2",
+        "aws-cdk.core~=1.16,>=1.16.2",
+        "aws-cdk.cx-api~=1.16,>=1.16.2"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
