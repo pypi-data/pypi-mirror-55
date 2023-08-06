b55ad6e05390f7156f6c90ad70cf9a2cadc5fe4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:03 +0000

    Add package spela/0.0.0/spela-0.0.0.tar.gz

---
+++ packages/spela/spela-0.0.0.tar.gz/spela-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='spela',
+    version='0.0.0',
+    packages=['spela', 'utils'],
+    url='https://github.com/kongkip/spela.git',
+    long_description_content_type="text/markdown",
+    author='Evans Kiplagat',
+    author_email='evanskiplagat3@gmail.com',
+    license='MIT',
+    description='', install_requires=['numpy']
+)
