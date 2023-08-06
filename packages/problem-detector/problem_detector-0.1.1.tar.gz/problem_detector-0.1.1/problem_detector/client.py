67e01329d13267ac2cbe17b1b5bd53a6664f4472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:53 +0000

    Add package problem-detector/0.1.1/problem_detector-0.1.1.tar.gz

---
+++ packages/problem-detector/problem_detector-0.1.1.tar.gz/problem_detector-0.1.1/problem_detector/client.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import os
+import yaml
+
+
+class Client(object):
+    def __init__(self):
+        pkg_path = os.path.dirname(__file__)
+        config_file_path = os.path.join(pkg_path, 'config/default.yaml')
+        with open(config_file_path, 'r') as config_file:
+            config = yaml.load(config_file, yaml.BaseLoader)
+            self.slack_proxy_server_ip = config['slack_proxy_server_ip']
+        self.port = '8000'
+
+    def post_message(self, message):
+        route = 'post-message'
+        url = "http://{}:{}/{}".format(self.slack_proxy_server_ip, self.port, route)
+        request_text = {
+            "text": message,
+        }
+        response = requests.post(url, data=json.dumps(request_text))
+        return response.status_code
+
+    def post_file(self, file):
+        route = 'post-file'
+        url = "http://{}:{}/{}".format(self.slack_proxy_server_ip, self.port, route)
+
+        request_data = dict()
+        request_file = dict()
+
+        request_file['upload_file'] = file
+        response = requests.post(url, data=request_data, files=request_file)
+        return response.status_code
