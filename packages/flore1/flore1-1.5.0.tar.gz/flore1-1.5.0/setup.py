be2b365077d5ba6ffe9f984a5d477f9076d4619c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:04 +0000

    Add package flore1/1.5.0/flore1-1.5.0.tar.gz

---
+++ packages/flore1/flore1-1.5.0.tar.gz/flore1-1.5.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="flore1",
+    version="1.5.0",
+    author="Anicet Nougaret",
+    author_email="anicet.nougaret@zohomail.eu",
+    description="The first 2D python game engine rendering in the terminal.",
+    url="https://github.com/AnicetNgrt/flore1",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'pillow',
+	  'keyboard'
+      ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "Environment :: Console :: Curses",
+        "Intended Audience :: Developers"
+    ],
+    python_requires=' >= 3.7',
+)
