4722ce7103deca95c041fec222061a44aaf8890c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:09 +0000

    Add package NlpToolkit-NGram/1.0.1/NlpToolkit-NGram-1.0.1.tar.gz

---
+++ packages/NlpToolkit-NGram/NlpToolkit-NGram-1.0.1.tar.gz/NlpToolkit-NGram-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-NGram',
+    version='1.0.1',
+    packages=['NGram'],
+    url='https://github.com/olcaytaner/NGram-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='NGram library'
+)
