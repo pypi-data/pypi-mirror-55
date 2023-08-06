be9d584dbbc68d420155df465e202c61911d0e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:37 +0000

    Add package startech/0.2.2/startech-0.2.2.tar.gz

---
+++ packages/startech/startech-0.2.2.tar.gz/startech-0.2.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'startech',
+  packages = ['startech'],
+  version = '0.2.2',
+  license='MIT',
+  description = 'A small library made to make common tasks easier',
+  author = 'Star Technology',
+  author_email = 'startechsheffield@gmx.com',
+  url = 'https://github.com/startechsheffield/startech',
+  download_url = 'https://github.com/startechsheffield/startech/archive/0.2.2.tar.gz',
+  keywords = ['SMALL', 'CONVENIENCE', 'RELEASE-CANDIDATE'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
