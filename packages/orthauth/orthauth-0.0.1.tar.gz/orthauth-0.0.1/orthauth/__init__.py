247e602de7129cf686087a1489cfc918376f64d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:46 +0000

    Add package orthauth/0.0.1/orthauth-0.0.1.tar.gz

---
+++ packages/orthauth/orthauth-0.0.1.tar.gz/orthauth-0.0.1/orthauth/__init__.py
@@ -0,0 +1,6 @@
+from orthauth.core import (Secrets,
+                           AuthConfig,
+                           configure,
+                           configure_relative,)
+
+__version__ = '0.0.1'
