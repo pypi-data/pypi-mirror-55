cb976767295fb5c79f87b2ed42c431207290e1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:21 +0000

    Add package gedit/0.0.1/gedit-0.0.1.tar.gz

---
+++ packages/gedit/gedit-0.0.1.tar.gz/gedit-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gedit',
+    version='0.0.1',
+    packages=['gedit'],
+    url='https://github.com/colyk/gedit',
+    license='MIT',
+    author='colykxer',
+    author_email='colykxer@gmail.com',
+    description='Tools for geographical data processing'
+)
