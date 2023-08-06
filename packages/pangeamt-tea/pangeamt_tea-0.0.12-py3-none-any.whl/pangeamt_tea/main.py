d4a1d6602d92f8a53de72f377769aff27a68ad65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:33 +0000

    Add package pangeamt-tea/0.0.12/pangeamt_tea-0.0.12-py3-none-any.whl

---
+++ packages/pangeamt-tea/pangeamt_tea-0.0.12-py3-none-any.whl/pangeamt_tea/main.py
@@ -0,0 +1,19 @@
+import asyncio
+import argparse
+
+
+
+def main():
+    asyncio.run(run())
+
+
+async def run():
+    parser = argparse.ArgumentParser(description='Tea')
+    parser.add_argument('task', metavar='task', type=str,
+                    help='The task to execute')
+
+
+    args = parser.parse_args()
+    print(args)
+    await asyncio.sleep(1)
+    print("Tea sample 2")
