895a8d700148b48db2e85422a82db524df0bee43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:08 +0000

    Add package Truvari/1.3.4/Truvari-1.3.4.tar.gz

---
+++ packages/Truvari/Truvari-1.3.4.tar.gz/Truvari-1.3.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='Truvari',
+    version='1.3.4',
+    author="ACEnglish",
+    author_email="acenglish@gmail.com",
+    url="https://github.com/spiralgenetics/truvari",
+    packages=['truvari',],
+    license='MIT',
+    scripts=["truvari/truvari", "truvari/consistency_report"],
+    description="Structural variant comparison tool for VCFs",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "python-Levenshtein==0.12.0",
+        "progressbar2==3.41.0",
+        "pysam==0.15.2",
+        "pyfaidx==0.5.5.2",
+        "intervaltree==3.0.2",
+    ],
+)
