4c8c6757aedf973a75b023b4fcbf307a8be0bcd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:57 +0000

    Add package systemdlint/1.1.3/systemdlint-1.1.3.tar.gz

---
+++ packages/systemdlint/systemdlint-1.1.3.tar.gz/systemdlint-1.1.3/systemdlint/conf/knownMandatory.py
@@ -0,0 +1,11 @@
+KNOWN_MANDATORY = {
+    "Unit": ["Description"],
+    "Address": ["Address"],
+    "BridgeFDB": ["MACAddress"],
+    "Link": ["MACAddress"],
+    "Mount": ["What", "Where"],
+    "NetDev": ["Name"],
+    "L2TPSession": ["Name"],
+    "Peer": ["Name"],
+    "IPv6AddressLabel": ["Label", "Prefix"]
+}
