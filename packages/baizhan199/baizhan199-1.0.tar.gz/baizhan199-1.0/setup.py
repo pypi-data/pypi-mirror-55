14520ca4d8a7a908ebc9cf8fa0ae681b66366596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:22 +0000

    Add package baizhan199/1.0/baizhan199-1.0.tar.gz

---
+++ packages/baizhan199/baizhan199-1.0.tar.gz/baizhan199-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='baizhan199',
+    version='1.0',
+    description='测试',
+    author='Mr.yang',
+    author_email='2803544962@qq.com',
+    py_modules=['baizhan199.demo01']
+)
