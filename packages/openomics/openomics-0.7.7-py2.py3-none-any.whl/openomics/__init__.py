169c7808416ccfa8a27630342e26b630d0df02c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:21 +0000

    Add package openomics/0.7.7/openomics-0.7.7-py2.py3-none-any.whl

---
+++ packages/openomics/openomics-0.7.7-py2.py3-none-any.whl/openomics/__init__.py
@@ -0,0 +1,39 @@
+from __future__ import print_function, division, absolute_import
+# -*- coding: utf-8 -*-
+
+"""Top-level package for openomics."""
+
+__author__ = """Nhat (Jonny) Tran"""
+__email__ = 'nhat.tran@mavs.uta.edu'
+__version__ = '0.7.7'
+
+try:
+    from .clinical import ClinicalData
+
+    from .multiomics import (
+        MultiOmics
+    )
+
+    from .genomics import (
+        SomaticMutation, DNAMethylation, CopyNumberVariation
+    )
+
+    from .transcriptomics import (
+        MessengerRNA, MicroRNA, LncRNA
+    )
+
+    # from .database import (
+    #     annotation,
+    #     disease,
+    #     interaction,
+    #     ontology
+    # )
+
+
+except ImportError as e:
+    msg = (
+        "OpenOmics requirements are not installed.\n\n"
+        "Please pip install as follows:\n\n"
+        "  pip install openomics --upgrade  # or pip install"
+    )
+    raise ImportError(str(e) + "\n\n" + msg)
