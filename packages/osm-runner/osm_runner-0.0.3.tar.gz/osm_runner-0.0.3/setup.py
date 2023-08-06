ec7bec507dc4b75534778349869a3d04b1579b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:00 +0000

    Add package osm-runner/0.0.3/osm_runner-0.0.3.tar.gz

---
+++ packages/osm-runner/osm_runner-0.0.3.tar.gz/osm_runner-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='osm_runner',
+    packages=['osm_runner'],
+    version='0.0.3',
+    description='A simple ORM that leverages the GeoAccessor of the ArcGIS API for Python to integrate OSM data with the ArcGIS platform.',
+    long_description='A simple ORM that leverages the GeoAccessor of the ArcGIS API for Python to integrate OSM data with the ArcGIS platform.',
+    author='Jeffrey Scarmazzi',
+    author_email='jscarmazzi@esri.com',
+    url='https://github.com/Jwmazzi/osm_runner',
+    download_url='https://github.com/jwmazzi/osm_runner/archive/0.0.3.tar.gz',
+    keywords=['ArcGIS API for Python', 'OpenStreetMap'],
+    classifiers=[],
+    install_requires=['requests', 'pandas', 'arcgis']
+)
