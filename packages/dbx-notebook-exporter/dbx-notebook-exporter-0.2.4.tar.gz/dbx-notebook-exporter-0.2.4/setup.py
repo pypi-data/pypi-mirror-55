8ce92a2012f6a292f6ea96d44bc9b3d47d7aab4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:57 +0000

    Add package dbx-notebook-exporter/0.2.4/dbx-notebook-exporter-0.2.4.tar.gz

---
+++ packages/dbx-notebook-exporter/dbx-notebook-exporter-0.2.4.tar.gz/dbx-notebook-exporter-0.2.4/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['dbxnotebookexporter']
+
+package_data = \
+{'': ['*'], 'dbxnotebookexporter': ['Databricks/*', 'Json/*', 'Python/*']}
+
+install_requires = \
+['ipython>=7,<8', 'nbconvert>=5,<6']
+
+entry_points = \
+{'nbconvert.exporters': ['dbx-json-notebook = '
+                         'DbxNotebookExporter.Json.JsonNotebookExporter:JsonNotebookExporter',
+                         'dbx-python-notebook = '
+                         'DbxNotebookExporter.Python.PythonNotebookExporter:PythonNotebookExporter']}
+
+setup_kwargs = {
+    'name': 'dbx-notebook-exporter',
+    'version': '0.2.4',
+    'description': 'Databrics Notebook Exporter',
+    'long_description': 'Databricks Notebook Exporter for nbconvert\n',
+    'author': 'Jiri Koutny',
+    'author_email': 'jiri.koutny@datasentics.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/DataSentics/dbx-notebook-exporter',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5.6',
+}
+
+
+setup(**setup_kwargs)
