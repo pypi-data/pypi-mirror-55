4bb65f5cc8e8376612589d7f8a538500dc3c6fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:34 +0000

    Add package distributions-michvi120/0.1/distributions_michvi120-0.1.tar.gz

---
+++ packages/distributions-michvi120/distributions_michvi120-0.1.tar.gz/distributions_michvi120-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_michvi120',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_michvi120'],
+      zip_safe=False)
