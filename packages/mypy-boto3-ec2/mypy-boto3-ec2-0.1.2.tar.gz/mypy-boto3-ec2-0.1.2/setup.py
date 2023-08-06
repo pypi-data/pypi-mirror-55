ccaadc465f476f1dea62b92bceca13f03152371a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:14 +0000

    Add package mypy-boto3-ec2/0.1.2/mypy-boto3-ec2-0.1.2.tar.gz

---
+++ packages/mypy-boto3-ec2/mypy-boto3-ec2-0.1.2.tar.gz/mypy-boto3-ec2-0.1.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+from mypy_boto3_ec2.version import __version__ as version
+
+with open("../../README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="mypy-boto3-ec2",
+    version=version,
+    packages=["mypy_boto3_ec2"],
+    url="https://github.com/vemel/mypy_boto3",
+    license="MIT License",
+    author="Vlad Emelianov",
+    author_email="vlad.emelianov.nz@gmail.com",
+    description="Mypy-friendly boto3 type annotations for ec2 service.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Typing :: Typed",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={"mypy_boto3_ec2": ["py.typed"]},
+    zip_safe=False,
+)
