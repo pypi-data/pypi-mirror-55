d0344afc242db5885d07a177764d391209e9bbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:29 +0000

    Add package vortexasdk/0.1.0/vortexasdk-0.1.0-py3-none-any.whl

---
+++ packages/vortexasdk/vortexasdk-0.1.0-py3-none-any.whl/vortexasdk/utils.py
@@ -0,0 +1,12 @@
+def _to_list(a) -> list:
+    if a is None:
+        return []
+    elif isinstance(a, list):
+        return a
+    else:
+        return [a]
+
+
+def convert_values_to_list(data: dict) -> dict:
+    """Convert each value to a list."""
+    return {k: _to_list(v) for k, v in data.items()}
