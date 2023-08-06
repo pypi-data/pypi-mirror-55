7d897ee155b2d5d8ad0de363672901f658b75aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:13 +0000

    Add package mysqltokenparser/2.1.2/mysqltokenparser-2.1.2.tar.gz

---
+++ packages/mysqltokenparser/mysqltokenparser-2.1.2.tar.gz/mysqltokenparser-2.1.2/mysqltokenparser/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for mysqltokenparser."""
+
+__author__ = """gra55"""
+__email__ = 'shuai.grass@gmail.com'
+__version__ = '2.1.2'
+
+from .mysqltokenparser import mysql_token_parser
+from .utils import *
+
+__all__ = ['mysql_token_parser']
