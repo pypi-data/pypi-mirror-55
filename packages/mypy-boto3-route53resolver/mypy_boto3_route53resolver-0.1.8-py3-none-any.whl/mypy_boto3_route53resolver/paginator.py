98192c56be9c217588b529382f56ad3995f8217e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:10 +0000

    Add package mypy-boto3-route53resolver/0.1.8/mypy_boto3_route53resolver-0.1.8-py3-none-any.whl

---
+++ packages/mypy-boto3-route53resolver/mypy_boto3_route53resolver-0.1.8-py3-none-any.whl/mypy_boto3_route53resolver/paginator.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+
+from botocore.paginate import Paginator
+
+
+class ListTagsForResource(Paginator):
+    # pylint: disable=arguments-differ
+    def paginate(
+        self, ResourceArn: str, PaginationConfig: Dict[str, Any] = None
+    ) -> Dict[str, Any]:
+        pass
