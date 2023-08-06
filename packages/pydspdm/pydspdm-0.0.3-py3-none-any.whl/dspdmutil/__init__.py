3c839cb5429aa4d8816189b91e1b8ff145362291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:16 +0000

    Add package pydspdm/0.0.3/pydspdm-0.0.3-py3-none-any.whl

---
+++ packages/pydspdm/pydspdm-0.0.3-py3-none-any.whl/dspdmutil/__init__.py
@@ -0,0 +1,11 @@
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+
+def countActiveWell(well):
+    output_well = [x for x in well if x["IS_ACTIVE"] == False]
+    # count all well retrieved from API
+    print("total well count:" , len(well))
+    # count well which is actived
+    print("active well count:" , len(output_well))
