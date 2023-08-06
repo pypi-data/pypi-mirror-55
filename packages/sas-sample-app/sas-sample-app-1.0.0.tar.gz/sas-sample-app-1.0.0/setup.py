a0520990c3650066818e1e69eda474347bd51f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:53 +0000

    Add package sas-sample-app/1.0.0/sas-sample-app-1.0.0.tar.gz

---
+++ packages/sas-sample-app/sas-sample-app-1.0.0.tar.gz/sas-sample-app-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sas-sample-app',
+    version = '1.0.0',
+    description = 'step 1 Tutorial',
+    url = 'http://bitbucket.org/avilay/sample-app',
+    author = 'Viswanathan Muthiah',
+    author_email = 'viswanathan.m1@tcs.com',
+    license = 'MIT',
+    packages = find_packages(),
+    #install_requires =['numpy','pandas','pickle'],
+    setup_requires = ['wheel']
+)
