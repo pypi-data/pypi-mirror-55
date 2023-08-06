3d71579dad72678362df219a0dc86db81b31aaf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:47 +0000

    Add package kubediag/0.0.1-dev.3/kubediag-0.0.1-dev.3.tar.gz

---
+++ packages/kubediag/kubediag-0.0.1-dev.3.tar.gz/kubediag-0.0.1-dev.3/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'kubediag',
+  packages = ['kubediag'],
+  version = '0.0.1-dev.3',
+  license='MIT',
+  description = 'Tool to diagnose common Kubernetes issues',
+  author = 'Jason Forte',
+  author_email = 'fortejas@amazon.com',
+  url = 'https://github.com/fortejas/kubediag',
+  download_url = 'https://github.com/fortejas/kubediag/archive/0.0.1.tar.gz',
+  keywords = ['k8s', 'kubernetes', 'cli', 'troubleshooting'],
+  scripts=['bin/kubediag'],
+  install_requires=[
+          'colorama',
+          'kubernetes',
+          'requests'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
