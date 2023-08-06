c1245a6a342ab2dfb8b6e56f1ef8f688a23d28c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:46 +0000

    Add package BitSightAPI/0.3.1/BitSightAPI-0.3.1.tar.gz

---
+++ packages/BitSightAPI/BitSightAPI-0.3.1.tar.gz/BitSightAPI-0.3.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BitSightAPI",
+    version="0.3.1",
+    author="InfosecSapper",
+    author_email="contact@infosecsapper.com",
+    description="A Python wrapper for the BitSight API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/infosecsapper/BitSightAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    python_requires='>=3.7',
+)
