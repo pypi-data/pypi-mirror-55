8b58b66444b52088bcfe74f4430288ee04b19d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:09 +0000

    Add package autodiffy/0.0.2.3/autodiffy-0.0.2.3.tar.gz

---
+++ packages/autodiffy/autodiffy-0.0.2.3.tar.gz/autodiffy-0.0.2.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autodiffy",
+    version="0.0.2.3",
+    author="Will Fried, Andrew Chia, Ankith Harathi, Wei-Hung Hsu",
+    author_email=" ",
+    description="Automatic Differentiation package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CS207-Feiyu-Group8/cs207-FinalProject",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.15.2',
+                      'pythonds>=1.2.1',
+    ],
+    tests_require=['pytest-timeout==1.2.1',
+                   'pytest==3.4.2', 
+                   'pytest-cov==2.5.1',
+                   'pytest-dependency==0.4'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
