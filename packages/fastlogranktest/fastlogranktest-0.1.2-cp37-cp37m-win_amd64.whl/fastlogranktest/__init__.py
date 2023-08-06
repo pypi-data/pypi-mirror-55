9b47f75d3dcb61da205c3a89f65a0de8c8b0debd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:57 +0000

    Add package fastlogranktest/0.1.2/fastlogranktest-0.1.2-cp37-cp37m-win_amd64.whl

---
+++ packages/fastlogranktest/fastlogranktest-0.1.2-cp37-cp37m-win_amd64.whl/fastlogranktest/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (print_function, absolute_import,
+                        division, unicode_literals)
+
+__author__ = 'Andreas Stelzer'
+__email__ = 'ga63nep@mytum.de'
+__version__ = '0.1.0'
+
+from .fastlogranktest import logrank_test, multi_logrank_test
