43cf0c49bd62829f4567b3b1cdc91fde996fc3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:20 +0000

    Add package specqp/1.1.1/specqp-1.1.1.tar.gz

---
+++ packages/specqp/specqp-1.1.1.tar.gz/specqp-1.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+      name='specqp',
+      version='1.1.1',
+      description='Quick plotting, correcting and fitting of spectroscopic data',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/Shipilin/specqp.git',
+      author='Mikhail Shipilin',
+      author_email='mikhail.shipilin@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      unit_test='pytest',
+      install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'lmfit'],
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: MacOS :: MacOS X",
+                   "Environment :: MacOS X",
+                   "Development Status :: 5 - Production/Stable"]
+      )
