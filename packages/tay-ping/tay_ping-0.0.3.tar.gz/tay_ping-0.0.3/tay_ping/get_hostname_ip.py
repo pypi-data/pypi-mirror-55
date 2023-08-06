1c126167971a3955189ae247e8071b43786b938b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:54 +0000

    Add package tay-ping/0.0.3/tay_ping-0.0.3.tar.gz

---
+++ packages/tay-ping/tay_ping-0.0.3.tar.gz/tay_ping-0.0.3/tay_ping/get_hostname_ip.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import socket
+
+
+def get_ip(this_hostname):
+    """
+    根据主机名获取对应的IP地址
+    :param this_hostname: 填写一个主机名
+    :return: 没有返回值，只是在控制台打印信息
+    """
+    try:
+        # 如果ping不通代表主机名解析还没有生效或者机器不存在
+        ip = socket.gethostbyname(this_hostname)
+        print("{} {}".format(ip, this_hostname))
+        print("123")
+    except:
+        print("{} 无法Ping通。。。".format(this_hostname))
