7ad4d3ebe6b9b239682639ae1bd9ec285d0fdd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:19 +0000

    Add package datasetmaker/0.12.20/datasetmaker-0.12.20.tar.gz

---
+++ packages/datasetmaker/datasetmaker-0.12.20.tar.gz/datasetmaker-0.12.20/datasetmaker/__init__.py
@@ -0,0 +1,6 @@
+from datasetmaker.api import create_client  # noqa
+from datasetmaker.merge import merge_packages  # noqa
+import datasetmaker.extensions  # noqa register accessors etc
+
+
+__version__ = '0.12.20'
