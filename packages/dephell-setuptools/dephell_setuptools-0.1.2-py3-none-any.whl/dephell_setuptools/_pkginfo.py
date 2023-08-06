630c01e4c7055a5047b2f733113f9f3c5c659919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:19 +0000

    Add package dephell-setuptools/0.1.2/dephell_setuptools-0.1.2-py3-none-any.whl

---
+++ packages/dephell-setuptools/dephell_setuptools-0.1.2-py3-none-any.whl/dephell_setuptools/_pkginfo.py
@@ -0,0 +1,20 @@
+# built-in
+import json
+import subprocess
+
+# app
+from ._base import BaseReader
+
+
+class PkgInfoReader(BaseReader):
+    @property
+    def content(self):
+        if self.path.is_file() and self.path.name != 'setup.py':
+            return None
+
+        cmd = ['pkginfo', '--json', str(self.path)]
+        result = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        if result.returncode != 0:
+            return None
+        content = json.loads(result.stdout.decode())
+        return self._clean(content)
