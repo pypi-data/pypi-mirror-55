72831ddb63e5b679ad111b5b9388bf14712d2cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:20 +0000

    Add package dynastes/0.0.2/dynastes-0.0.2.tar.gz

---
+++ packages/dynastes/dynastes-0.0.2.tar.gz/dynastes-0.0.2/dynastes/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from dynastes import layers
+from dynastes import ops
+from dynastes import util
+from dynastes import blocks
+
+__version__ = "0.0.2"
+# Cleanup symbols to avoid polluting namespace.
+del absolute_import
+del division
+del print_function
