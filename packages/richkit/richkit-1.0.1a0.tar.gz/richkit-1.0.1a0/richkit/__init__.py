4f4a971cdd72b49559618f4b328149e2ccd87644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:05 +0000

    Add package richkit/1.0.1a0/richkit-1.0.1a0.tar.gz

---
+++ packages/richkit/richkit-1.0.1a0.tar.gz/richkit-1.0.1a0/richkit/__init__.py
@@ -0,0 +1,13 @@
+"""dat is the Domain Analysis Toolkit
+
+See the `README
+<https://github.com/aau-network-security/domain-analysis-toolkit/blob/master/README.md>`_
+for a general introduction.
+
+"""
+
+__all__ = [
+    'analyse',
+    'lookup',
+    'retrieve',
+]
