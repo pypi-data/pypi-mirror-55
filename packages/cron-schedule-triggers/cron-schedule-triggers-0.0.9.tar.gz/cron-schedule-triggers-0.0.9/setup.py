2b00b4dd340020e0f5fe2395b09d55645dc8fe52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:46 +0000

    Add package cron-schedule-triggers/0.0.9/cron-schedule-triggers-0.0.9.tar.gz

---
+++ packages/cron-schedule-triggers/cron-schedule-triggers-0.0.9.tar.gz/cron-schedule-triggers-0.0.9/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='cron-schedule-triggers',
+      version='0.0.9',
+      description='Cron Schedule Triggers ~ A library for determining Quartz Cron schedule trigger dates.',
+      url='https://gitlab.com/dameon.andersen/cstriggers',
+      author='Dameon Andersen',
+      author_email='dameon.andersen@facteon.global',
+      packages=setuptools.find_packages(),
+      python_requires='>=3.6',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      licence="MIT",
+      zip_safe=False,
+      keywords="quartz cron notation cronjob aws rate schedule scheduling office/business triggers tasks jobs runner "
+               "rq queue apscheduler calendar date datetime",
+      install_requires=[
+            line.rstrip('\n') for line
+            in open("cstriggers/requirements/base.txt", "r")
+      ]
+)
