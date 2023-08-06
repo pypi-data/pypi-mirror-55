8100c6546e079d34b1920e85f87c9763c4373d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:46 +0000

    Add package mercurial_path_pattern/1.4.0/mercurial_path_pattern-1.4.0.tar.gz

---
+++ packages/mercurial_path_pattern/mercurial_path_pattern-1.4.0.tar.gz/mercurial_path_pattern-1.4.0/setup.py
@@ -0,0 +1,38 @@
+
+VERSION = '1.4.0'
+
+# pylint:disable=missing-docstring,unused-import,import-error
+
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = open("README.txt").read()
+
+setup(
+    name="mercurial_path_pattern",
+    version=VERSION,
+    author='Marcin Kasperski',
+    author_email='Marcin.Kasperski@mekk.waw.pl',
+    url='http://bitbucket.org/Mekk/mercurial-path_pattern',
+    description='Mercurial Path Pattern Extension',
+    long_description=LONG_DESCRIPTION,
+    license='BSD',
+    py_modules=[
+        'mercurial_path_pattern',
+    ],
+    install_requires=[
+        'mercurial_extension_utils>=1.5.0',
+    ],
+    keywords="mercurial hg path alias",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: DFSG approved',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Version Control'
+        # 'Topic :: Software Development :: Version Control :: Mercurial',
+    ],
+    zip_safe=True)
