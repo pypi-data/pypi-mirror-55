6f1ace42c62c1a22284ff400a77589d7e958b6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:32 +0000

    Add package sicomor-especificaciones/0.0.3/sicomor-especificaciones-0.0.3.tar.gz

---
+++ packages/sicomor-especificaciones/sicomor-especificaciones-0.0.3.tar.gz/sicomor-especificaciones-0.0.3/sicomor_especificaciones/inventario.py
@@ -0,0 +1,16 @@
+from .hardware_inv import HARDWARE_INV
+from .sistop_inv   import SISTOP_INV
+from .software_inv import SOFTWARE_INV
+
+INVENTARIO = HARDWARE_INV + SISTOP_INV + SOFTWARE_INV
+
+
+def get_recursos():
+	"""
+		Retorna lista de inventarios validos.
+	"""
+	recursos = []
+	for x in INVENTARIO:
+		if x["estado"]:
+			recursos.append(x)
+	return recursos
---
+++ packages/sicomor-especificaciones/sicomor-especificaciones-0.0.3.tar.gz/sicomor-especificaciones-0.0.3/sicomor_especificaciones/sistop_inv.py
@@ -0,0 +1,17 @@
+# identificador xxyyzzzz
+# xx-> tipo1: 10: HW
+#             11: SW
+# yy-> tipo2: SW: 01 SISTOP
+
+SISTOP_INV = [
+	{	"tipo1": "HW",
+		"tipo2": "DISK",
+		"identificador": 11010001,
+		"descripcion": "Nombre completo Sistema Operativo",
+		"unidad": "str",
+		"estado": True,
+		# ----------------------------------------------------
+		"manejador": {"id": 1,
+					  "clase": "",
+					  "function": ""}, }
+]
