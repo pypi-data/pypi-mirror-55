646840a7b3566d2c1b139693c7a2db16097c0308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:19 +0000

    Add package mastercard-mdes-customer-service/2.0.8/mastercard_mdes_customer_service-2.0.8-py2-none-any.whl

---
+++ packages/mastercard-mdes-customer-service/mastercard_mdes_customer_service-2.0.8-py2-none-any.whl/mastercardmdescustomerservice/__init__.py
@@ -0,0 +1,22 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardmdescustomerservice.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardmdescustomerservice.accountholdermessaging import *
+from mastercardmdescustomerservice.search import *
+from mastercardmdescustomerservice.systemstatus import *
+from mastercardmdescustomerservice.tokenactivate import *
+from mastercardmdescustomerservice.tokenactivationmethods import *
+from mastercardmdescustomerservice.tokencomments import *
+from mastercardmdescustomerservice.tokendelete import *
+from mastercardmdescustomerservice.tokenresendactivationcode import *
+from mastercardmdescustomerservice.tokenresetmobilepin import *
+from mastercardmdescustomerservice.tokenstatushistory import *
+from mastercardmdescustomerservice.tokensuspend import *
+from mastercardmdescustomerservice.tokenunsuspend import *
+from mastercardmdescustomerservice.tokenupdate import *
+from mastercardmdescustomerservice.transactions import *
+from mastercardmdescustomerservice.updatetokenassurance import *
