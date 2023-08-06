9401413c5eb885fed60b90da2f1e8ef773a7440b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:40 +0000

    Add package make-c/0.1.1/make_c-0.1.1.tar.gz

---
+++ packages/make-c/make_c-0.1.1.tar.gz/make_c-0.1.1/make_c/__init__.py
@@ -0,0 +1,13 @@
+from .__about__ import (
+    __version__,
+    __title__,
+    __summary__
+)
+from .editors import CProgramClasses
+
+__all__ = [
+    "__version__",
+    "__title__",
+    "__summary__",
+    CProgramClasses
+]
