9d0617be4af1df9904d7bee661d65850da6bf5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:26 +0000

    Add package skiply/0.5.66/skiply-0.5.66.tar.gz

---
+++ packages/skiply/skiply-0.5.66.tar.gz/skiply-0.5.66/skiply/cdm/base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# coding: utf8
+
+# Copyright 2019 Skiply
+# 
+from __future__ import unicode_literals
+
+import logging
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declared_attr, declarative_base
+
+from sqlalchemy import Column, Integer
+
+logging.info(os.environ['SQLALCHEMY_DATABASE_URI'])
+SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+logging.info(SQLALCHEMY_DATABASE_URI)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_recycle=280, pool_size=10) # pool_size/overflow
+db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class SkiplyBase(object):
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+# Base = declarative_base(cls=SkiplyBase)
+# Base.query = db_session.query_property()
