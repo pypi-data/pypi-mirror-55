384856fd500c9c70c085f39b54e2fae203aa755d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:59 +0000

    Add package mypy-boto3-sqs/0.1.7/mypy_boto3_sqs-0.1.7-py3-none-any.whl

---
+++ packages/mypy-boto3-sqs/mypy_boto3_sqs-0.1.7-py3-none-any.whl/mypy_boto3_sqs/service_resource.py
@@ -0,0 +1,195 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+from typing import List
+
+from boto3.resources.base import ServiceResource as Boto3ServiceResource
+from boto3.resources.collection import ResourceCollection
+
+# pylint: disable=import-self
+import mypy_boto3_sqs.service_resource as mypy_boto3_sqs_scope
+
+
+class ServiceResource(Boto3ServiceResource):
+    queues: mypy_boto3_sqs_scope.queues
+
+    # pylint: disable=arguments-differ
+    def Message(
+        self, queue_url: str = None, receipt_handle: str = None
+    ) -> mypy_boto3_sqs_scope.Message:
+        pass
+
+    # pylint: disable=arguments-differ
+    def Queue(self, url: str = None) -> mypy_boto3_sqs_scope.Queue:
+        pass
+
+    # pylint: disable=arguments-differ
+    def create_queue(
+        self,
+        QueueName: str,
+        Attributes: Dict[str, Any] = None,
+        tags: Dict[str, Any] = None,
+    ) -> mypy_boto3_sqs_scope.Queue:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_available_subresources(self) -> List[str]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_queue_by_name(self) -> None:
+        pass
+
+
+class Message(Boto3ServiceResource):
+    message_id: str
+    md5_of_body: str
+    body: str
+    attributes: Dict[str, Any]
+    md5_of_message_attributes: str
+    message_attributes: Dict[str, Any]
+    queue_url: str
+    receipt_handle: str
+
+    # pylint: disable=arguments-differ
+    def change_visibility(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_available_subresources(self) -> List[str]:
+        pass
+
+
+class Queue(Boto3ServiceResource):
+    attributes: Dict[str, Any]
+    url: str
+    dead_letter_source_queues: mypy_boto3_sqs_scope.dead_letter_source_queues
+
+    # pylint: disable=arguments-differ
+    def add_permission(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def change_message_visibility_batch(self, Entries: List[Any]) -> Dict[str, Any]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def delete_messages(self, Entries: List[Any]) -> Dict[str, Any]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def get_available_subresources(self) -> List[str]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def load(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def purge(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def receive_messages(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def reload(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def remove_permission(self) -> None:
+        pass
+
+    # pylint: disable=arguments-differ
+    def send_message(
+        self,
+        MessageBody: str,
+        DelaySeconds: int = None,
+        MessageAttributes: Dict[str, Any] = None,
+        MessageSystemAttributes: Dict[str, Any] = None,
+        MessageDeduplicationId: str = None,
+        MessageGroupId: str = None,
+    ) -> Dict[str, Any]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def send_messages(self, Entries: List[Any]) -> Dict[str, Any]:
+        pass
+
+    # pylint: disable=arguments-differ
+    def set_attributes(self, Attributes: Dict[str, Any]) -> None:
+        pass
+
+
+class queues(ResourceCollection):
+    @classmethod
+    # pylint: disable=arguments-differ
+    def all(cls) -> None:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def filter(cls) -> None:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def iterator(cls) -> ResourceCollection:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def limit(cls) -> None:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def page_size(cls) -> None:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def pages(cls) -> List[Boto3ServiceResource]:
+        pass
+
+
+class dead_letter_source_queues(ResourceCollection):
+    @classmethod
+    # pylint: disable=arguments-differ
+    def all(cls) -> None:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def filter(cls) -> None:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def iterator(cls) -> ResourceCollection:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def limit(cls) -> None:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def page_size(cls) -> None:
+        pass
+
+    @classmethod
+    # pylint: disable=arguments-differ
+    def pages(cls) -> List[Boto3ServiceResource]:
+        pass
