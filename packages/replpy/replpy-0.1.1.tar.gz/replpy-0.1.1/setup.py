a646f04492c4ba2128c70e98abed3123cd327218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:09 +0000

    Add package replpy/0.1.1/replpy-0.1.1.tar.gz

---
+++ packages/replpy/replpy-0.1.1.tar.gz/replpy-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['replpy']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'replpy',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Ben G',
+    'author_email': 'ben.gordon@toasttab.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
