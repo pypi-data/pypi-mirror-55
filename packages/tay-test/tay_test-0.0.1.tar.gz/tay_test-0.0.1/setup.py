1e7ea350f879ffea855740eca5127e5648eabac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:30 +0000

    Add package tay-test/0.0.1/tay_test-0.0.1.tar.gz

---
+++ packages/tay-test/tay_test-0.0.1.tar.gz/tay_test-0.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tay_test",  # Replace with your own username
+    version="0.0.1",
+    author="tay3223",
+    author_email="tay3223@qq.com",
+    description="Tay的一个简单测试",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/tay3223/tmp.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
