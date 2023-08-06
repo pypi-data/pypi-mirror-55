46705e023031f992aab84adec56ef7b4d66c59ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:37 +0000

    Add package autoflpy/0.2.4/autoflpy-0.2.4.tar.gz

---
+++ packages/autoflpy/autoflpy-0.2.4.tar.gz/autoflpy-0.2.4/autoflpy/autoflpy_batch.py
@@ -0,0 +1,6 @@
+"""
+Automated running of all log files in a folder to create flight logs.
+"""
+
+# TODO: Needs to read the date, flight number on the date, time of flight and
+# airfield. Needs to update the input files.
