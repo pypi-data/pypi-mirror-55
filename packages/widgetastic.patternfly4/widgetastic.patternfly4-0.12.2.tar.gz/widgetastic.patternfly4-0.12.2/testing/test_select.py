99d2ef821e32e4a4b7a2639fba5d3c8c113fa98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:35 +0000

    Add package widgetastic.patternfly4/0.12.2/widgetastic.patternfly4-0.12.2.tar.gz

---
+++ packages/widgetastic.patternfly4/widgetastic.patternfly4-0.12.2.tar.gz/widgetastic.patternfly4-0.12.2/testing/test_select.py
@@ -0,0 +1,40 @@
+import pytest
+
+from widgetastic_patternfly4 import Select
+from widgetastic_patternfly4 import SelectItemNotFound
+
+
+@pytest.fixture
+def select(browser):
+    return Select(
+        browser,
+        locator=('.//div[@id="ws-react-c-select-single"]//div[contains(@class, "pf-c-select")]'),
+    )
+
+
+def test_select_is_displayed(select):
+    assert select.is_displayed
+
+
+def test_select_items(select):
+    assert set(select.items) == {"Choose...", "Mr", "Miss", "Mrs", "Ms", "Dr", "Other"}
+    assert select.has_item("Mr")
+    assert not select.has_item("Non existing item")
+    assert select.item_enabled("Miss")
+
+
+def test_select_open(select):
+    assert not select.is_open
+    select.open()
+    assert select.is_open
+    select.close()
+    assert not select.is_open
+
+
+def test_select_item_select(select):
+    select.fill("Mr")
+    assert select.read() == "Mr"
+    assert not select.is_open
+    with pytest.raises(SelectItemNotFound):
+        select.fill("Non existing item")
+    assert not select.is_open
