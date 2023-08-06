b14bd2306e86aa028aaeec47c04d4ff7e7e92868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:58 +0000

    Add package mypy-boto3-importexport/0.1.8/mypy_boto3_importexport-0.1.8-py3-none-any.whl

---
+++ packages/mypy-boto3-importexport/mypy_boto3_importexport-0.1.8-py3-none-any.whl/mypy_boto3_importexport/paginator.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+
+from botocore.paginate import Paginator
+
+
+class ListJobs(Paginator):
+    # pylint: disable=arguments-differ
+    def paginate(
+        self, APIVersion: str = None, PaginationConfig: Dict[str, Any] = None
+    ) -> Dict[str, Any]:
+        pass
