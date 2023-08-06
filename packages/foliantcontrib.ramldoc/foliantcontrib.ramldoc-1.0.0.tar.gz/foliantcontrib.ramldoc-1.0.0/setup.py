2112e66c8ef0a262fd6fd47e42f6b94f31968689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:54 +0000

    Add package foliantcontrib.ramldoc/1.0.0/foliantcontrib.ramldoc-1.0.0.tar.gz

---
+++ packages/foliantcontrib.ramldoc/foliantcontrib.ramldoc-1.0.0.tar.gz/foliantcontrib.ramldoc-1.0.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+SHORT_DESCRIPTION = 'Documentation generator for RAML API'
+
+try:
+    with open('README.md', encoding='utf8') as readme:
+        LONG_DESCRIPTION = readme.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = SHORT_DESCRIPTION
+
+
+setup(
+    name='foliantcontrib.ramldoc',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    version='1.0.0',
+    author='Daniil Minukhin',
+    author_email='ddddsa@gmail.com',
+    packages=['foliant.preprocessors'],
+    license='MIT',
+    platforms='any',
+    install_requires=[
+        'foliant>=1.0.8',
+        'foliantcontrib.utils.combined_options>=1.0.7',
+        'foliantcontrib.utils.preprocessor_ext',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+    ]
+)
