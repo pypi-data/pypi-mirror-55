d9dd17ce9b384752ba9c9eab635b5836376d1b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:30 +0000

    Add package aim.models/6.1.0/aim.models-6.1.0.tar.gz

---
+++ packages/aim.models/aim.models-6.1.0.tar.gz/aim.models-6.1.0/src/aim/models/exceptions.py
@@ -0,0 +1,23 @@
+class InvalidAimFieldType(Exception):
+    pass
+
+class InvalidAimProjectFile(Exception):
+    pass
+
+class UnusedAimProjectField(Exception):
+    pass
+
+class InvalidAimReference(Exception):
+    pass
+
+class InvalidCFNMapping(Exception):
+    pass
+
+class InvalidAimBucket(Exception):
+    pass
+
+class InvalidAWSResourceName(Exception):
+    pass
+
+class TroposphereConversionError(Exception):
+    pass
---
+++ packages/aim.models/aim.models-6.1.0.tar.gz/aim.models-6.1.0/src/aim/models/storages.py
@@ -0,0 +1,6 @@
+from aim.models import schemas
+from zope.interface import implementer
+from zope.schema.fieldproperty import FieldProperty
+from aim.models.base import Named, Deployable, Regionalized
+
+
