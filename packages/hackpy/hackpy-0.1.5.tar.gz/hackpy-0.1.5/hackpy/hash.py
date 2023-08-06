a17ddbc1a2fbc297325db7dc7079820477ac5666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:48 +0000

    Add package hackpy/0.1.5/hackpy-0.1.5.tar.gz

---
+++ packages/hackpy/hackpy-0.1.5.tar.gz/hackpy-0.1.5/hackpy/hash.py
@@ -0,0 +1,17 @@
+import hashlib
+
+# Download module list with hash
+module_hashes = {
+        'nircmd.exe': 'ea448e2d5be3197a422ad413598c6c43',
+        'webcam.exe': 'ea678b48940aebe8fbf9a189949fc4a3',
+        }
+
+def md5(file):
+    ##|
+    ##| Get file md5
+    ##|
+    hash_md5 = hashlib.md5()
+    with open(file, "rb", encoding = "utf8", errors = 'ignore') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
