a9f39416c85ebac6fb9a77dcc16be45e6d45434a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:13 +0000

    Add package desert/2019.11.6/desert-2019.11.6-py2.py3-none-any.whl

---
+++ packages/desert/desert-2019.11.6-py2.py3-none-any.whl/desert/__init__.py
@@ -0,0 +1,59 @@
+import dataclasses
+import typing as t
+
+import attr
+import marshmallow
+
+import desert._make
+import desert._version
+
+
+def schema(cls: t.Type, many: bool = False) -> marshmallow.Schema:
+    """Build a marshmallow schema *instance* for the class.
+    """
+    return desert._make.class_schema(cls)(many=many)
+
+
+def schema_class(cls: t.Type) -> t.Type[marshmallow.Schema]:
+    """Build a marshmallow schema *class* for the class.
+    """
+    return desert._make.class_schema(cls)
+
+
+def metadata(
+    field: marshmallow.fields.Field
+) -> t.Dict["desert._make._DesertSentinel", t.Dict[t.Any, marshmallow.fields.Field]]:
+    """Specify a marshmallow field in the field metadata.
+
+    .. code-block:: python
+
+        x: int = attr.ib(metadata=desert.metadata(marshmallow.fields.Int()))
+    """
+    return {desert._make._DESERT_SENTINEL: {"marshmallow_field": field}}
+
+
+def field(marshmallow_field: marshmallow.fields.Field, **kw) -> dataclasses.Field:
+    """Specify a marshmallow field in the metadata for a ``dataclasses.dataclass``.
+
+    .. code-block:: python
+
+        @dataclasses.dataclass
+        class A:
+            x: int = desert.field(marshmallow.fields.Int())
+    """
+    return dataclasses.field(**kw, metadata=metadata(marshmallow_field))
+
+
+def ib(marshmallow_field: marshmallow.fields.Field, **kw) -> attr._make._CountingAttr:
+    """Specify a marshmallow field in the metadata for an ``attr.dataclass``.
+
+    .. code-block:: python
+
+        @attr.dataclass
+        class A:
+            x: int = desert.ib(marshmallow.fields.Int())
+    """
+    return attr.ib(**kw, metadata=metadata(marshmallow_field))
+
+
+__version__ = desert._version.__version__
