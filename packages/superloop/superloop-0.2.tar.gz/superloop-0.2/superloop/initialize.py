17159793884f7fb072b545d6d59f5f67839c332e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:20 +0000

    Add package superloop/0.2/superloop-0.2.tar.gz

---
+++ packages/superloop/superloop-0.2.tar.gz/superloop-0.2/superloop/initialize.py
@@ -0,0 +1,27 @@
+
+def variables():
+
+	### NTW_DEVICE IS A LIST OF THE MATCHED NODES
+	global ntw_device
+
+	### TYPE IS A LIST OF THE MATCH NODE'S TYPE (FIREWALL, SWITCH OR ROUTER)
+	global templates 
+
+	### ELEMENT IS A LIST OF THE INDEXES OF ALL THE MATCHED NODES
+	global element
+
+	### CONFIGURATION IS A LIST OF THE CONFIGS TO BE PUSHED TO DEVICE
+	global configuration
+
+	### RENDERED_CONFIG IS A LIST OF THE CONFIGS RENDERED FROM THE TEMPLATE
+	global rendered_config
+
+	### BACKUP_CONFIG IS A LIST OF CONFIGS CAPTURED FROM THE DEVICE AT THE TIME OF AUDIT DIFF
+	global backup_config
+
+	ntw_device = []
+	templates = []
+	element = []
+	configuration = []
+	rendered_config = []
+	backup_config = []
---
+++ packages/superloop/superloop-0.2.tar.gz/superloop-0.2/superloop/snmp.py
@@ -0,0 +1,39 @@
+# THIS MODULE FETCHES INFORMATION FROM THE DEVICE VIA SNMP
+
+from snmp_helper import snmp_get_oid
+from snmp_helper import snmp_extract 
+from processdb import process_encrypted
+import base64
+import subprocess
+
+def snmp(argument_node):
+
+	index = 0
+	USERNAME_STRING = base64.b64decode(process_encrypted()[index]['username'])
+	PASSWORD_STRING = process_encrypted()[index]['password']
+	COMMUNITY_STRING = process_encrypted()[index]['snmp']
+	SNMP_PORT = 161
+
+	HOSTNAME_OID = '1.3.6.1.2.1.1.5.0'
+	device = (argument_node,base64.b64decode(COMMUNITY_STRING),SNMP_PORT)
+	snmp_hostname = snmp_data(device,HOSTNAME_OID,SNMP_PORT)
+
+	data = [{
+		'hostname': '{}'.format(snmp_hostname),
+		'ip': "{}".format(argument_node),
+		'username':'{}'.format(USERNAME_STRING),
+		'password': '{}'.format(PASSWORD_STRING),
+		'platform':'cisco',
+		'os':'ios',
+		'type':'ios'
+		}
+	]
+
+	return data
+
+def snmp_data(device,oid,port):
+
+	snmp_data = snmp_get_oid(device,oid,display_errors=True)
+	snmp_hostname = snmp_extract(snmp_data)
+
+	return snmp_hostname 
