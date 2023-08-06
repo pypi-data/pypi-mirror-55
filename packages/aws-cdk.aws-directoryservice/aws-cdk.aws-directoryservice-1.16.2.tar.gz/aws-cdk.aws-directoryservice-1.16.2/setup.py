5f6f314b818353b186c04fdcd286817a18dd12cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:55 +0000

    Add package aws-cdk.aws-directoryservice/1.16.2/aws-cdk.aws-directoryservice-1.16.2.tar.gz

---
+++ packages/aws-cdk.aws-directoryservice/aws-cdk.aws-directoryservice-1.16.2.tar.gz/aws-cdk.aws-directoryservice-1.16.2/setup.py
@@ -0,0 +1,43 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-directoryservice",
+    "version": "1.16.2",
+    "description": "The CDK Construct Library for AWS::DirectoryService",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_directoryservice",
+        "aws_cdk.aws_directoryservice._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_directoryservice._jsii": [
+            "aws-directoryservice@1.16.2.jsii.tgz"
+        ],
+        "aws_cdk.aws_directoryservice": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.4",
+        "publication>=0.0.3",
+        "aws-cdk.core~=1.16,>=1.16.2"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
