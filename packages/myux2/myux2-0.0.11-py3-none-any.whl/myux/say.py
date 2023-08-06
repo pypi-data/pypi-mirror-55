12815793cb20994998618e23fcc8238230097405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:19 +0000

    Add package myux2/0.0.11/myux2-0.0.11-py3-none-any.whl

---
+++ packages/myux2/myux2-0.0.11-py3-none-any.whl/myux/say.py
@@ -0,0 +1,7 @@
+def hello(name):
+    print('{}さん、こんにちは。'.format(name))
+    print('Hello, {}.'.format(name))
+
+def bye(name):
+    print('{}さん、さようなら。'.format(name))
+    print('Good-bye, {}.'.format(name))
