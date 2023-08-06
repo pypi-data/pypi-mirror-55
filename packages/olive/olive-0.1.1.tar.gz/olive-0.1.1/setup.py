b84d5da9b7e06de6f1711a4e4b226e6a57b67e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:20 +0000

    Add package olive/0.1.1/olive-0.1.1.tar.gz

---
+++ packages/olive/olive-0.1.1.tar.gz/olive-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('requirements.txt', encoding='utf-8') as f:
+    requirements = f.read().split('\n')
+
+setuptools.setup(name='olive',
+                 version='0.1.1',
+                 description='a python wrapper for an AIS Database API',
+                 install_requires=requirements,
+                 url='https://github.com/GeoBigData/AISDatabase',
+                 author='Elizabeth Golden, Rachel Wegener, Melissa Dozier',
+                 author_email='elizabeth.golden@digitalglobe.com',
+                 license='',
+                 packages=setuptools.find_packages(),
+                 python_requires='>=3.6')
+
+#python3 -m twine upload dist/*
