155c756a2fbb56dee98fc8c217c6ea6868f28767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:09 +0000

    Add package at101-distributions/0.1/at101_distributions-0.1.tar.gz

---
+++ packages/at101-distributions/at101_distributions-0.1.tar.gz/at101_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='at101_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['at101_distributions'],
+      author='Alex Terry',
+      zip_safe=False)
