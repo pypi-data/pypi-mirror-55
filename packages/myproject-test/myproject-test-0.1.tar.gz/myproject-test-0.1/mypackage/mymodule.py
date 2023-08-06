04904f968c8f809f4fb55536d2b1585ffd14d794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:26 +0000

    Add package myproject-test/0.1/myproject-test-0.1.tar.gz

---
+++ packages/myproject-test/myproject-test-0.1.tar.gz/myproject-test-0.1/mypackage/mymodule.py
@@ -0,0 +1,13 @@
+import sys
+
+print("This is mymodule")
+
+def greet(name):
+    print(f'Hello, {name}!')
+
+def greet_main():
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+    else:
+        name = 'unknown human'
+    greet(name)
