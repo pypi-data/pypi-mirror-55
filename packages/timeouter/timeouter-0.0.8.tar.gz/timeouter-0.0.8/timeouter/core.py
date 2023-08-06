5e22eac972e80410e7c48bbda80aa1096394787b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:48 +0000

    Add package timeouter/0.0.8/timeouter-0.0.8.tar.gz

---
+++ packages/timeouter/timeouter-0.0.8.tar.gz/timeouter-0.0.8/timeouter/core.py
@@ -0,0 +1,29 @@
+import time
+
+
+class Timer():
+
+    def __init__(self, timeout):
+        self.set_timeout(timeout)
+        self.reset()
+        self.TimeoutException = TimeoutError
+
+    def has(self, timeout=0):
+        return self.get() - timeout > 0
+
+    def check(self, message=None):
+        if self.get() <= 0:
+            raise self.TimeoutException(
+                message) if message is not None else self.TimeoutException
+
+    def get(self, laps=1):
+        return (self.start_time + self._timeout - time.monotonic()) / laps
+
+    def reset(self):
+        self.start_time = time.monotonic()
+
+    def set_timeout(self, timeout):
+        self._timeout = float(timeout)
+
+    def set_exception_class(self, e):
+        self.TimeoutException = e
