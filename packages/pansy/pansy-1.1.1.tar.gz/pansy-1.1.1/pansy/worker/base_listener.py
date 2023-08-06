a9575f8b3fd6ad3f7dd0dc9527bb3d337a3797be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:40 +0000

    Add package pansy/1.1.1/pansy-1.1.1.tar.gz

---
+++ packages/pansy/pansy-1.1.1.tar.gz/pansy-1.1.1/pansy/worker/base_listener.py
@@ -0,0 +1,30 @@
+class BaseListener:
+    worker = None
+
+    def __init__(self, worker):
+        self.worker = worker
+
+    def on_update(self, dt, task_list):
+        """
+        必须继承实现
+        :param dt: 等待时间
+        :param task_list: 消息列表
+        :return:
+        """
+        pass
+
+    def on_start(self):
+        """
+        当worker启动后
+        可继承实现
+        :return:
+        """
+        pass
+
+    def on_stop(self):
+        """
+        当worker停止前
+        可继承实现
+        :return:
+        """
+        pass
