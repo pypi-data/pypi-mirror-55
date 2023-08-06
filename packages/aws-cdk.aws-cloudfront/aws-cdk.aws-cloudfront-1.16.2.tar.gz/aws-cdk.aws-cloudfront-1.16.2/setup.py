04bb3b9a5c6c024a759323ad76b9aca8cf05d3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:53 +0000

    Add package aws-cdk.aws-cloudfront/1.16.2/aws-cdk.aws-cloudfront-1.16.2.tar.gz

---
+++ packages/aws-cdk.aws-cloudfront/aws-cdk.aws-cloudfront-1.16.2.tar.gz/aws-cdk.aws-cloudfront-1.16.2/setup.py
@@ -0,0 +1,48 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-cloudfront",
+    "version": "1.16.2",
+    "description": "CDK Constructs for AWS CloudFront",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_cloudfront",
+        "aws_cdk.aws_cloudfront._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_cloudfront._jsii": [
+            "aws-cloudfront@1.16.2.jsii.tgz"
+        ],
+        "aws_cdk.aws_cloudfront": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.4",
+        "publication>=0.0.3",
+        "aws-cdk.aws-certificatemanager~=1.16,>=1.16.2",
+        "aws-cdk.aws-iam~=1.16,>=1.16.2",
+        "aws-cdk.aws-kms~=1.16,>=1.16.2",
+        "aws-cdk.aws-lambda~=1.16,>=1.16.2",
+        "aws-cdk.aws-s3~=1.16,>=1.16.2",
+        "aws-cdk.core~=1.16,>=1.16.2"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
