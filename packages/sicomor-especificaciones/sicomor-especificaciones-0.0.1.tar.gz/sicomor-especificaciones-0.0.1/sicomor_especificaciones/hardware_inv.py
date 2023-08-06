b6f2d968d5b862745e2c0cf8b3f876621749efac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:18 +0000

    Add package sicomor-especificaciones/0.0.1/sicomor-especificaciones-0.0.1.tar.gz

---
+++ packages/sicomor-especificaciones/sicomor-especificaciones-0.0.1.tar.gz/sicomor-especificaciones-0.0.1/sicomor_especificaciones/hardware_inv.py
@@ -0,0 +1,5 @@
+HARDWARE_INV = [
+	{
+	
+	}
+]
---
+++ packages/sicomor-especificaciones/sicomor-especificaciones-0.0.1.tar.gz/sicomor-especificaciones-0.0.1/sicomor_especificaciones/software_inv.py
@@ -0,0 +1,21 @@
+# identificador xxyyzzzz
+# xx-> tipo1: 10: HW
+#             11: SW
+# yy-> tipo2: HW: 01 PROC
+#				  10 MEM
+#				  11 DISK
+#			  SW: 01 SISTOP
+SOFTWARE_INV = [
+	{	"tipo1": "SW",
+		"tipo2": "SISTOP",
+		"identificador": 11010001,
+		"descripcion": "Nombre completo Sistema Operativo",
+		"unidad": "str",
+		"estado": True,
+		#----------------------------------------------------
+		"manejador": { "id": 1,
+					   "clase": "",
+					   "function": "",
+					}
+	}
+]
