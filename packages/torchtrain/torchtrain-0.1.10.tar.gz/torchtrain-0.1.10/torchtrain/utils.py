e6aba3898b80b21f03a148a785bb320c5fd753a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:25 +0000

    Add package torchtrain/0.1.10/torchtrain-0.1.10.tar.gz

---
+++ packages/torchtrain/torchtrain-0.1.10.tar.gz/torchtrain-0.1.10/torchtrain/utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+import torch
+
+
+def distribute_model(model, config):
+    if config["device"] != "cpu":
+        model = torch.nn.DataParallel(
+            model,
+            device_ids=eval(config["cuda_list"]),
+            dim=config["data_parallel_dim"],
+        )
+        model = model.to(config["device"])
+    return model
+
+
+def filter_dict(d, to_save):
+    return {k: v for k, v in d.items() if k in to_save} if to_save else dict(d)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def set_random_seeds(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
