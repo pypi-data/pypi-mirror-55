4c99b0fcc6089d21f828815e275c2a5253b09121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:39 +0000

    Add package larq/0.8.0/larq-0.8.0-py3-none-any.whl

---
+++ packages/larq/larq-0.8.0-py3-none-any.whl/larq/__init__.py
@@ -0,0 +1,21 @@
+import larq.layers as layers
+import larq.activations as activations
+import larq.callbacks as callbacks
+import larq.constraints as constraints
+import larq.math as math
+import larq.metrics as metrics
+import larq.models as models
+import larq.quantizers as quantizers
+import larq.optimizers as optimizers
+
+__all__ = [
+    "layers",
+    "activations",
+    "callbacks",
+    "constraints",
+    "math",
+    "metrics",
+    "models",
+    "quantizers",
+    "optimizers",
+]
