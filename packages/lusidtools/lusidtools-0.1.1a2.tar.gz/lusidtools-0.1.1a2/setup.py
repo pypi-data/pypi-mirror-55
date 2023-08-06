b36f04ec18a382e6b2a05ac0f5f7408a6ada29c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:40 +0000

    Add package lusidtools/0.1.1a2/lusidtools-0.1.1a2.tar.gz

---
+++ packages/lusidtools/lusidtools-0.1.1a2.tar.gz/lusidtools-0.1.1a2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages  # noqa: H301
+
+version = {}
+with open("./__version__.py") as fp:
+    exec(fp.read(), version)
+
+setup(
+    name='lusidtools',
+    version=version['__version__'],
+    description='Python Tools for LUSID',
+    url='https://github.com/finbourne/lusid-python-tools',
+    author='FINBOURNE Technology',
+    author_email='engineering@finbourne.com',
+    license='MIT',
+    keywords=["FINBOURNE", "LUSID", "LUSID API", "python"],
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    python_requires='>=3.6'
+)
