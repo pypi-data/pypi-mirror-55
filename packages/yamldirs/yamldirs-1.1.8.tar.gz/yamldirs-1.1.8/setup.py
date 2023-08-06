a164730caaad54bc8b10de09c2657682b11b759e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:14 +0000

    Add package yamldirs/1.1.8/yamldirs-1.1.8.tar.gz

---
+++ packages/yamldirs/yamldirs-1.1.8.tar.gz/yamldirs-1.1.8/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""yamldirs - create directories and files (incl. contents) from yaml spec.
+"""
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+classifiers = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Topic :: Software Development :: Libraries
+"""
+
+version = '1.1.8'
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='yamldirs',
+    version=version,
+    install_requires=[
+        'ruamel.yaml'
+    ],
+    author='Bjorn Pettersen',
+    author_email='bp@datakortet.no',
+    url='https://github.com/datakortet/yamldirs',
+    description=__doc__.strip(),
+    classifiers=[line for line in classifiers.split('\n') if line],
+    long_description=open('README.rst').read(),
+    cmdclass={'test': PyTest},
+    entry_points={
+        'console_scripts': [
+            'yamldirs = yamldirs.yamldirs_cmd:main',
+        ]
+    },
+    packages=find_packages(),
+    zip_safe=False,
+)
