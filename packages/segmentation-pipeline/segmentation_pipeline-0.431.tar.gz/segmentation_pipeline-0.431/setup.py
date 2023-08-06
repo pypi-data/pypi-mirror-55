c891a7b70e1ff502dac4ad4116617d379d071f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:07 +0000

    Add package segmentation-pipeline/0.431/segmentation_pipeline-0.431.tar.gz

---
+++ packages/segmentation-pipeline/segmentation_pipeline-0.431.tar.gz/segmentation_pipeline-0.431/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import setuptools
+setup(name='segmentation_pipeline',
+      version='0.431',
+      description='Declaraqtively configured pipeline for image segmentation',
+      url='https://github.com/petrochenko-pavel-a/segmentation_training_pipeline',
+      author='Petrochenko Pavel',
+      author_email='petrochenko.pavel.a@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      include_package_data=True,
+      #dependency_links=['https://github.com/aleju/imgaug'],
+      install_requires=["musket_core"],
+      zip_safe=False)
