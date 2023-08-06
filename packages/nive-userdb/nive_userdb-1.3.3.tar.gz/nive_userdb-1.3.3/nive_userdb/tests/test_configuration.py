9010f89a6156742d87a19cd3e4ad9ecc0a7578ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:49 +0000

    Add package nive-userdb/1.3.3/nive_userdb-1.3.3.tar.gz

---
+++ packages/nive-userdb/nive_userdb-1.3.3.tar.gz/nive_userdb-1.3.3/nive_userdb/tests/test_configuration.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from nive.helper import FormatConfTestFailure
+
+from nive_userdb import app, root, user
+
+
+
+
+class TestConf(unittest.TestCase):
+
+    def test_conf1(self):
+        r=app.configuration.test()
+        if not r:
+            return
+        print(FormatConfTestFailure(r))
+        self.assert_(False, "Configuration Error")
+
+    def test_conf2(self):
+        r=root.configuration.test()
+        if not r:
+            return
+        print(FormatConfTestFailure(r))
+        self.assert_(False, "Configuration Error")
+
+    def test_conf3(self):
+        r=user.configuration.test()
+        if not r:
+            return
+        print(FormatConfTestFailure(r))
+        self.assert_(False, "Configuration Error")
+
