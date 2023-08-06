785d796e14792cb173a543c213852a7d7b04fce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:02 +0000

    Add package pangeamt-tea/0.0.30/pangeamt_tea-0.0.30-py3-none-any.whl

---
+++ packages/pangeamt-tea/pangeamt_tea-0.0.30-py3-none-any.whl/pangeamt_tea/console_scripts/tea2.py
@@ -0,0 +1,29 @@
+import anyio
+import asyncclick as click
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+async def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        click.echo('I was invoked without subcommand')
+    else:
+        click.echo('I am about to invoke %s' % ctx.invoked_subcommand)
+
+
+@cli.command()
+@click.option("--dir", default=None, help="Directory where the project Will be created")
+@click.option("--customer", help="Customer name")
+@click.option("--src_lang", help="Source language")
+@click.option("--tgt_lang", help="Target language")
+@click.option("--flavor", default=None, help="Flavor")
+@click.option("--version", default=None, help="Flavor")
+async def new(dir, customer, src_lang, tgt_lang, flavor, version):
+    '''
+    Create a new project
+    '''
+    click.echo('The subcommand')
+
+
+def main():
+    cli(_anyio_backend="asyncio")  # or asyncio, or curio
