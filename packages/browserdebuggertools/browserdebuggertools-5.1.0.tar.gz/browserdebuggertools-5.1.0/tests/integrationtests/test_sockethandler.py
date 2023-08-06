57ebe8af65624893f4756401c11507b9d3b92fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:54 +0000

    Add package browserdebuggertools/5.1.0/browserdebuggertools-5.1.0.tar.gz

---
+++ packages/browserdebuggertools/browserdebuggertools-5.1.0.tar.gz/browserdebuggertools-5.1.0/tests/integrationtests/test_sockethandler.py
@@ -0,0 +1,118 @@
+import socket
+from unittest import TestCase
+
+from mock import MagicMock
+
+from browserdebuggertools.sockethandler import SocketHandler
+
+MODULE_PATH = "browserdebuggertools.sockethandler."
+
+
+class MockSocketHandler(SocketHandler):
+
+    def __init__(self):
+        self._websocket = MagicMock()
+
+        self._next_result_id = 0
+        self._domains = []
+        self._results = {}
+        self._events = {}
+        self._internal_events = {}
+
+
+class SocketHandlerTest(TestCase):
+
+    def setUp(self):
+        self.socket_handler = MockSocketHandler()
+
+
+class Test_SocketHandler_can_get_messages(SocketHandlerTest):
+
+    def test_get_results(self):
+        mock_result = {"key": "value"}
+        mock_message = '{"id": 1, "result": {"key": "value"}}'
+        self.socket_handler._websocket.recv.side_effect = [mock_message, None]
+
+        self.socket_handler._flush_messages()
+
+        self.assertEqual(mock_result, self.socket_handler._results[1])
+
+    def test_get_errors(self):
+        mock_error = {"error": {"key": "value"}}
+        mock_message = '{"id": 1, "error": {"key": "value"}}'
+        self.socket_handler._websocket.recv.side_effect = [mock_message, None]
+
+        self.socket_handler._flush_messages()
+
+        self.assertEqual(mock_error, self.socket_handler._results[1])
+
+    def test_get_events(self):
+        mock_event = {"method": "MockDomain.mockEvent", "params": {"key": "value"}}
+        mock_message = '{"method": "MockDomain.mockEvent", "params": {"key": "value"}}'
+        self.socket_handler._events["MockDomain"] = []
+        self.socket_handler._websocket.recv.side_effect = [mock_message, None]
+
+        self.socket_handler._flush_messages()
+
+        self.assertIn(mock_event, self.socket_handler._events["MockDomain"])
+
+    def test_get_mixed(self):
+        mock_result = {"key": "value"}
+        mock_error = {"error": {"key": "value"}}
+        mock_event = {"method": "MockDomain.mockEvent", "params": {"key": "value"}}
+        mock_result_message = '{"id": 1, "result": {"key": "value"}}'
+        mock_error_message = '{"id": 2, "error": {"key": "value"}}'
+        mock_event_message = '{"method": "MockDomain.mockEvent", "params": {"key": "value"}}'
+
+        self.socket_handler._events["MockDomain"] = []
+        self.socket_handler._websocket.recv.side_effect = [
+            mock_result_message, mock_error_message, mock_event_message, None
+        ]
+
+        self.socket_handler._flush_messages()
+
+        self.assertEqual(mock_result, self.socket_handler._results[1])
+        self.assertEqual(mock_error, self.socket_handler._results[2])
+        self.assertIn(mock_event, self.socket_handler._events["MockDomain"])
+
+    def test_get_messages_then_except(self):
+        mock_result = {"key": "value"}
+        mock_message = '{"id": 1, "result": {"key": "value"}}'
+        self.socket_handler._websocket.recv.side_effect = [mock_message, socket.error]
+
+        self.socket_handler._flush_messages()
+
+        self.assertEqual(mock_result, self.socket_handler._results[1])
+
+
+class Test_SocketHandler_get_events(SocketHandlerTest):
+
+    def test_get_events_returns_copy(self):
+        mock_message = '{"method": "MockDomain.mockEvent", "params": {"key": "value"}}'
+        self.socket_handler._domains = ["MockDomain"]
+        self.socket_handler._events["MockDomain"] = []
+        self.socket_handler._websocket.recv.side_effect = [mock_message, None, None]
+
+        events = self.socket_handler.get_events("MockDomain")
+
+        events[0] = ""
+
+        self.assertEqual(
+            {"method": "MockDomain.mockEvent", "params": {"key": "value"}},
+            self.socket_handler.get_events("MockDomain")[0]
+        )
+
+    def test_get_events_clear(self):
+        mock_message = '{"method": "MockDomain.mockEvent", "params": {"key": "value"}}'
+        self.socket_handler._domains = ["MockDomain"]
+        self.socket_handler._events["MockDomain"] = []
+        self.socket_handler._websocket.recv.side_effect = [mock_message, None, None]
+
+        events = self.socket_handler.get_events("MockDomain")
+
+        events[0] = ""
+
+        self.assertEqual(
+            {"method": "MockDomain.mockEvent", "params": {"key": "value"}},
+            self.socket_handler.get_events("MockDomain")[0]
+        )
