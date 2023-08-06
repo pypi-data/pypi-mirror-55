a03d80d836b70a1fdd4534a0d25f491f6ddb940c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:54 +0000

    Add package mypy-boto3/0.1.0/mypy_boto3-0.1.0-py3-none-any.whl

---
+++ packages/mypy-boto3/mypy_boto3-0.1.0-py3-none-any.whl/mypy_boto3/secretsmanager/paginator.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Dict
+
+from botocore.paginate import Paginator
+
+
+class ListSecrets(Paginator):
+
+    # pylint: disable=arguments-differ
+    def paginate(self, PaginationConfig: Dict[str, Any] = None) -> Dict[str, Any]:
+        pass
