1fc7fda2e12fad998d6f43e24a40f40e7091b6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:19 +0000

    Add package zcross/0.0.2/zcross-0.0.2.tar.gz

---
+++ packages/zcross/zcross-0.0.2.tar.gz/zcross-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+     name='zcross',  
+     version='0.0.2',
+     scripts=[] ,
+     author="Michele Renda",
+     author_email="michele.renda@cern.ch",
+     description="An utility to read low-pressure gaseous cross sections data",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://gitlab.com/micrenda/zcross-python",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+         "Operating System :: OS Independent",
+     ],
+     python_requires='>=3.0',
+ )
