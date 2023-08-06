99e109c589d5b605b592be1dc60a4f8e6cd08199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:00 +0000

    Add package nanome-docking/0.1.5/nanome-docking-0.1.5.tar.gz

---
+++ packages/nanome-docking/nanome-docking-0.1.5.tar.gz/nanome-docking-0.1.5/nanome_docking/_SettingsMenu.py
@@ -0,0 +1,24 @@
+import nanome
+from nanome.api.ui.image import Image as Image
+
+import os
+
+class SettingsMenu():
+    def __init__(self, docking_plugin, closed_callback):
+        self._plugin = docking_plugin
+        self._score_all_frames = False
+
+        self._menu = nanome.ui.Menu.io.from_json(os.path.join(os.path.dirname(__file__), '_advanced_settings.json'))
+        self._menu.register_closed_callback(closed_callback)
+
+        self._btn_score_all_frames = self._menu.root.find_node('Button').get_content()
+        self._btn_score_all_frames.register_pressed_callback()
+    
+    def get_score_frames(self):
+        return self._score_all_frames
+
+    def toggle_score_all_frames(self):
+        self._score_all_frames = not self._score_all_frames
+        text = "on" if self._score_all_frames else "off"
+        self._btn_score_all_frames.set_all_text(text)
+        self._plugin.update_content(self._btn_score_all_frames)
