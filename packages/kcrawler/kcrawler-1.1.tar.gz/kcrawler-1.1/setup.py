4a5441796bea0ea0da092bc14701a6227f4fd487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:15 +0000

    Add package kcrawler/1.1/kcrawler-1.1.tar.gz

---
+++ packages/kcrawler/kcrawler-1.1.tar.gz/kcrawler-1.1/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kcrawler",
+    version="1.1",
+    author="ken",
+    author_email="kenbliky@gmail.com",
+    description="A python crawler authored by Ken.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kenblikylee/kcrawler",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=[
+        u'requests>=2.21.0',
+        u'beautifulsoup4>=4.7.1',
+        u'pandas>=0.25.1',
+        u'matplotlib>=3.0.3',
+        u'xlwt>=1.3.0',
+        u'Pillow>=6.1.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'kcrawler = kcrawler.cli:main',
+            'kcjuejin = kcrawler.cli:juejin',
+            'kcanjuke = kcrawler.cli:anjuke',
+            'kcargs = kcrawler.cli:args',
+        ]
+    },
+    python_requires='>=3.6',
+)
