0fc8c31e99452eafb1f6d2b84112c20dbe4c9ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:25 +0000

    Add package model-metadata/0.6.0/model_metadata-0.6.0.tar.gz

---
+++ packages/model-metadata/model_metadata-0.6.0.tar.gz/model_metadata-0.6.0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+import versioneer
+
+
+def read_requirements():
+    import os
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    requirements_file = os.path.join(path, "requirements.txt")
+    try:
+        with open(requirements_file, "r") as req_fp:
+            requires = req_fp.read().split()
+    except IOError:
+        return []
+    else:
+        return [require.split() for require in requires]
+
+
+setup(
+    name="model_metadata",
+    version=versioneer.get_version(),
+    description="Model Metadata",
+    author="Eric Hutton",
+    author_email="huttone@colorado.edu",
+    url="http://csdms.colorado.edu",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    setup_requires=["setuptools"],
+    install_requires=[
+        "binaryornot",
+        "click",
+        "jinja2",
+        "packaging",
+        "pyyaml",
+        "py-scripting",
+        "six",
+    ],
+    packages=find_packages(),
+    cmdclass=versioneer.get_cmdclass(),
+    entry_points={
+        "console_scripts": ["mmd=model_metadata.cli.main:mmd"],
+        "bmi.plugins": ["bmi_mmd=model_metadata.cli.main:configure_parser_mmd"],
+    },
+)
