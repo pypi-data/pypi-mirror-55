3a0631da87e2428d99fb37051fe964f96b95e097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:45 +0000

    Add package tableau-tools/4.9.7/tableau_tools-4.9.7.tar.gz

---
+++ packages/tableau-tools/tableau_tools-4.9.7.tar.gz/tableau_tools-4.9.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='tableau_tools',
+    version='4.9.7',
+    packages=['tableau_tools', 'tableau_tools.tableau_rest_api', 'tableau_tools.tableau_documents', 'tableau_tools.examples'],
+    url='https://github.com/bryantbhowell/tableau_tools',
+    license='',
+    author='Bryant Howell',
+    author_email='bhowell@tableau.com',
+    description='A library for programmatically working with Tableau files and Tableau Server, including the REST API',
+    install_requires=['requests'],
+    use_2to3=True
+)
