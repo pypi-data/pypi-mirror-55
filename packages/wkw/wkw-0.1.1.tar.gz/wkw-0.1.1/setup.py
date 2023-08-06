c1d1d4db67977b77c0bb8777dae2b78f4a1b7df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:03 +0000

    Add package wkw/0.1.1/wkw-0.1.1.tar.gz

---
+++ packages/wkw/wkw-0.1.1.tar.gz/wkw-0.1.1/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+
+import os
+import subprocess
+import shutil
+import platform
+
+
+class BuildPyCommand(build_py):
+    """Modified build command to compile C library."""
+
+    def __build_c_library(self):
+        this_dir = os.path.dirname(__file__)
+        c_dir = os.path.normpath(os.path.join(this_dir, "..", "c"))
+
+        # building C library
+        subprocess.call(["cargo", "clean"], cwd=c_dir)
+        subprocess.call(["cargo", "build", "--release"], cwd=c_dir)
+
+        lib_name_platform = {
+            "Linux": "libwkw.so",
+            "Windows": "wkw.dll",
+            "Darwin": "libwkw.dylib",
+        }
+        lib_name = lib_name_platform[platform.system()]
+        lib_file = os.path.join(c_dir, "target", "release", lib_name)
+        header_file = os.path.join(c_dir, "include", "wkw.h")
+
+        # copying to lib dir
+        lib_dir = os.path.join(this_dir, "wkw", "lib")
+
+        if os.path.exists(lib_dir):
+            shutil.rmtree(lib_dir)
+
+        os.makedirs(lib_dir)
+        if platform.system() == "Windows":
+            shutil.copy(
+                os.path.join(c_dir, "liblz4.dll"), os.path.join(lib_dir, "liblz4.dll")
+            )
+        shutil.copy(lib_file, os.path.join(lib_dir, lib_name))
+        shutil.copy(header_file, os.path.join(lib_dir, "wkw.h"))
+
+    def run(self):
+        self.__build_c_library()
+        build_py.run(self)
+
+
+setup(
+    name="wkw",
+    use_scm_version={"root": ".."},
+    setup_requires=["setuptools_scm"],
+    author="Alessandro Motta",
+    author_email="alessandro.motta@brain.mpg.de",
+    url="https://github.com/scalableminds/webknossos-wrap",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["cffi", "numpy"],
+    cmdclass={"build_py": BuildPyCommand},
+)
