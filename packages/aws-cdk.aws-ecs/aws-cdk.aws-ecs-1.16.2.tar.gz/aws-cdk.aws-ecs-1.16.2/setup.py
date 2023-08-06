a3c4334ed99fe6006f5bb9105a8d8a62c0b0b2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:56 +0000

    Add package aws-cdk.aws-ecs/1.16.2/aws-cdk.aws-ecs-1.16.2.tar.gz

---
+++ packages/aws-cdk.aws-ecs/aws-cdk.aws-ecs-1.16.2.tar.gz/aws-cdk.aws-ecs-1.16.2/setup.py
@@ -0,0 +1,65 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-ecs",
+    "version": "1.16.2",
+    "description": "The CDK Construct Library for AWS::ECS",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_ecs",
+        "aws_cdk.aws_ecs._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_ecs._jsii": [
+            "aws-ecs@1.16.2.jsii.tgz"
+        ],
+        "aws_cdk.aws_ecs": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.4",
+        "publication>=0.0.3",
+        "aws-cdk.aws-applicationautoscaling~=1.16,>=1.16.2",
+        "aws-cdk.aws-autoscaling~=1.16,>=1.16.2",
+        "aws-cdk.aws-autoscaling-hooktargets~=1.16,>=1.16.2",
+        "aws-cdk.aws-certificatemanager~=1.16,>=1.16.2",
+        "aws-cdk.aws-cloudformation~=1.16,>=1.16.2",
+        "aws-cdk.aws-cloudwatch~=1.16,>=1.16.2",
+        "aws-cdk.aws-ec2~=1.16,>=1.16.2",
+        "aws-cdk.aws-ecr~=1.16,>=1.16.2",
+        "aws-cdk.aws-ecr-assets~=1.16,>=1.16.2",
+        "aws-cdk.aws-elasticloadbalancing~=1.16,>=1.16.2",
+        "aws-cdk.aws-elasticloadbalancingv2~=1.16,>=1.16.2",
+        "aws-cdk.aws-iam~=1.16,>=1.16.2",
+        "aws-cdk.aws-lambda~=1.16,>=1.16.2",
+        "aws-cdk.aws-logs~=1.16,>=1.16.2",
+        "aws-cdk.aws-route53~=1.16,>=1.16.2",
+        "aws-cdk.aws-route53-targets~=1.16,>=1.16.2",
+        "aws-cdk.aws-secretsmanager~=1.16,>=1.16.2",
+        "aws-cdk.aws-servicediscovery~=1.16,>=1.16.2",
+        "aws-cdk.aws-sns~=1.16,>=1.16.2",
+        "aws-cdk.aws-sqs~=1.16,>=1.16.2",
+        "aws-cdk.aws-ssm~=1.16,>=1.16.2",
+        "aws-cdk.core~=1.16,>=1.16.2",
+        "aws-cdk.cx-api~=1.16,>=1.16.2"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
