ea11e9e1d9ce1e4f155f7fa3e8661c11bc2270a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:36 +0000

    Add package tfc-client/0.6.6/tfc_client-0.6.6.tar.gz

---
+++ packages/tfc-client/tfc_client-0.6.6.tar.gz/tfc_client-0.6.6/tfc_client/models/notification_configuration.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Optional, List, Any, Dict
+
+from pydantic import HttpUrl
+
+from . import KebabCaseBaseModel
+from .data import AttributesModel
+from ..enums import NotificationTrigger, NotificationsDestinationType
+
+
+class DeliveryResponseModel(KebabCaseBaseModel):
+    url: HttpUrl
+    body: str
+    code: int
+    headers: Dict[str, List[str]]
+    sent_at: datetime
+    successful: bool
+
+
+class NotificationConfigurationModel(AttributesModel):
+    enabled: bool = True
+    name: str
+    url: HttpUrl
+    destination_type: NotificationsDestinationType
+    token: Optional[str]
+    triggers: List[NotificationTrigger]
+    delivery_responses: Optional[List[DeliveryResponseModel]]
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
