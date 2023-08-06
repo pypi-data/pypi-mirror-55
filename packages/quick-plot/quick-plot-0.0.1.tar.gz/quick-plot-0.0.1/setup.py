f3147253586c6607181c145248e8182c8e80a0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:38 +0000

    Add package quick-plot/0.0.1/quick-plot-0.0.1.tar.gz

---
+++ packages/quick-plot/quick-plot-0.0.1.tar.gz/quick-plot-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quick-plot",
+    version="0.0.1",
+    author="Gary Klindt",
+    author_email="gary.klindt@gmail.com",
+    description="convenience wrapper around matplotlib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/garykl/quick-plot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: Unix",
+    ],
+    python_requires='>=3.6'
+)
