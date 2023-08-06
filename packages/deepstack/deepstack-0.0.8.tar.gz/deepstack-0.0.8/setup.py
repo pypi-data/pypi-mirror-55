00f027bb3d9fec3f4afb60628aac0b474ef605f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:26 +0000

    Add package deepstack/0.0.8/deepstack-0.0.8.tar.gz

---
+++ packages/deepstack/deepstack-0.0.8.tar.gz/deepstack-0.0.8/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+def setup_package():
+    metadata = dict(name='deepstack',
+                    packages=['deepstack'],
+                    maintainer='Julio Borges',
+                    long_description='DeepStack: Ensembling Deep Learning Models into the next Performance Level',
+                    long_description_content_type='text/x-rst',
+                    description='DeepStack: Ensembling Deep Learning Models into the next Performance Level',
+                    license='MIT',
+                    url='https://github.com/jcborges/DeepStack',
+                    download_url='https://github.com/jcborges/DeepStack/archive/v_0.0.8.tar.gz',
+                    version='0.0.8',
+                    install_requires=[
+                        'numpy>=1.16.4',
+                        'keras>=2.2.5',
+                        'tensorflow>=1.14.0',
+                        'scikit-learn>=0.21.2'
+                    ],
+                    )
+
+    setup(**metadata)
+
+
+if __name__ == "__main__":
+    setup_package()
