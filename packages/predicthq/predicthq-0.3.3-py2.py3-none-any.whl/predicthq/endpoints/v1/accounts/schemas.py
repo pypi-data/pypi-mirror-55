e34aa0ee83bd6520c5f7a9371283395db71a22d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:50 +0000

    Add package predicthq/0.3.3/predicthq-0.3.3-py2.py3-none-any.whl

---
+++ packages/predicthq/predicthq-0.3.3-py2.py3-none-any.whl/predicthq/endpoints/v1/accounts/schemas.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import, print_function
+
+from predicthq.endpoints.schemas import Model, StringType, DateTimeType, ModelType
+
+
+class Industry(Model):
+
+    id = StringType()
+    name = StringType()
+
+
+class Account(Model):
+
+    id = StringType()
+    name = StringType()
+    description = StringType()
+    industry = ModelType(Industry)
+    created_at = DateTimeType()
+    updated_at = DateTimeType()
