b55c598f4c3e102bb11c16c335db70ca1b51ecb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:16 +0000

    Add package YTReviewsAPI/0.1.6/YTReviewsAPI-0.1.6-py3-none-any.whl

---
+++ packages/YTReviewsAPI/YTReviewsAPI-0.1.6-py3-none-any.whl/app/config.py
@@ -0,0 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    APP_URL = os.environ.get('APP_URL') or 'http://localhost:5000'    
