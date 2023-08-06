ba54530e8b9b9f84899adb14d503fe6faed67337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:19 +0000

    Add package LOONTEST/1.2.3/LOONTEST-1.2.3.tar.gz

---
+++ packages/LOONTEST/LOONTEST-1.2.3.tar.gz/LOONTEST-1.2.3/loon/tk.py
@@ -0,0 +1,5 @@
+import tkinter
+tk = tkinter.Tk()
+tk.withdraw() 
+tk.eval('lappend auto_path ../Tcl')
+tk.eval('package require loon')
---
+++ packages/LOONTEST/LOONTEST-1.2.3.tar.gz/LOONTEST-1.2.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LOONTEST", # Replace with your own username
+    version="1.2.3",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description= "test",
+    url="https://github.com/pw2wang/loon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
