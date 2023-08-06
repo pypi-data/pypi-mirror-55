b962bddbcb1efa0466308a8212d848865ad25a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:54 +0000

    Add package galgek-ukp-nomousauto-grivind/0.1.3/galgek-ukp-nomousauto-grivind-0.1.3.tar.gz

---
+++ packages/galgek-ukp-nomousauto-grivind/galgek-ukp-nomousauto-grivind-0.1.3.tar.gz/galgek-ukp-nomousauto-grivind-0.1.3/galgek_ukp_nomousauto_grivind/train_setup.py
@@ -0,0 +1,143 @@
+import torch
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from .data_conversion import imread, preprocess_image, get_mask_and_regr
+from .model import MyUNet
+
+n_epochs = 10
+BATCH_SIZE = 2
+PATH = '../input/pku-autonomous-driving/'
+
+train = pd.read_csv(PATH + 'train.csv')
+test = pd.read_csv(PATH + 'sample_submission.csv')
+
+class CarDataset(Dataset):
+    """Car dataset."""
+
+    def __init__(self, dataframe, root_dir, training=True, transform=None):
+        self.df = dataframe
+        self.root_dir = root_dir
+        self.transform = transform
+        self.training = training
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        # Get image name
+        idx, labels = self.df.values[idx]
+        img_name = self.root_dir.format(idx)
+        
+        # Augmentation
+        flip = False
+        if self.training:
+            flip = np.random.randint(2) == 1
+        
+        # Read image
+        img0 = imread(img_name, True)
+        img = preprocess_image(img0, flip=flip)
+        img = np.rollaxis(img, 2, 0)
+        
+        # Get mask and regression maps
+        mask, regr = get_mask_and_regr(img0, labels, flip=False)
+        regr = np.rollaxis(regr, 2, 0)
+        
+        return [img, mask, regr]
+
+train_images_dir = PATH + 'train_images/{}.jpg'
+test_images_dir = PATH + 'test_images/{}.jpg'
+
+df_train, df_dev = train_test_split(train, test_size=0.01, random_state=42)
+df_test = test
+
+# Create dataset objects
+train_dataset = CarDataset(df_train, train_images_dir, training=True)
+dev_dataset = CarDataset(df_dev, train_images_dir, training=False)
+test_dataset = CarDataset(df_test, test_images_dir, training=False)
+
+train_loader = DataLoader(dataset=train_dataset, 
+						  batch_size=BATCH_SIZE, 
+						  shuffle=True, num_workers=4)
+dev_loader = DataLoader(dataset=dev_dataset, 
+	                    batch_size=BATCH_SIZE, 
+	                    shuffle=False, num_workers=4)
+test_loader = DataLoader(dataset=test_dataset, 
+						 batch_size=BATCH_SIZE, 
+						 shuffle=False, num_workers=4)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+model = MyUNet(8).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=max(n_epochs, 10) * len(train_loader) // 3, gamma=0.1)
+
+
+def criterion(prediction, mask, regr, size_average=True):
+    # Binary mask loss
+    pred_mask = torch.sigmoid(prediction[:, 0])
+#     mask_loss = mask * (1 - pred_mask)**2 * torch.log(pred_mask + 1e-12) + (1 - mask) * pred_mask**2 * torch.log(1 - pred_mask + 1e-12)
+    mask_loss = mask * torch.log(pred_mask + 1e-12) + (1 - mask) * torch.log(1 - pred_mask + 1e-12)
+    mask_loss = -mask_loss.mean(0).sum()
+    
+    # Regression L1 loss
+    pred_regr = prediction[:, 1:]
+    regr_loss = (torch.abs(pred_regr - regr).sum(1) * mask).sum(1).sum(1) / mask.sum(1).sum(1)
+    regr_loss = regr_loss.mean(0)
+    
+    # Sum
+    loss = mask_loss + regr_loss
+    if not size_average:
+        loss *= prediction.shape[0]
+    return loss
+
+def train_model(epoch, history=None):
+    model.train()
+
+    for batch_idx, (img_batch, mask_batch, regr_batch) in enumerate(tqdm(train_loader)):
+        img_batch = img_batch.to(device)
+        mask_batch = mask_batch.to(device)
+        regr_batch = regr_batch.to(device)
+        
+        optimizer.zero_grad()
+        output = model(img_batch)
+        loss = criterion(output, mask_batch, regr_batch)
+        if history is not None:
+            history.loc[epoch + batch_idx / len(train_loader), 'train_loss'] = loss.data.cpu().numpy()
+        
+        loss.backward()
+        
+        optimizer.step()
+        exp_lr_scheduler.step()
+    
+    print('Train Epoch: {} \tLR: {:.6f}\tLoss: {:.6f}'.format(
+        epoch,
+        optimizer.state_dict()['param_groups'][0]['lr'],
+        loss.data))
+
+def evaluate_model(epoch, history=None):
+    model.eval()
+    loss = 0
+    
+    with torch.no_grad():
+        for img_batch, mask_batch, regr_batch in dev_loader:
+            img_batch = img_batch.to(device)
+            mask_batch = mask_batch.to(device)
+            regr_batch = regr_batch.to(device)
+
+            output = model(img_batch)
+
+            loss += criterion(output, mask_batch, regr_batch, size_average=False).data
+    
+    loss /= len(dev_loader.dataset)
+    
+    if history is not None:
+        history.loc[epoch, 'dev_loss'] = loss.cpu().numpy()
+    
+    print('Dev loss: {:.4f}'.format(loss))
