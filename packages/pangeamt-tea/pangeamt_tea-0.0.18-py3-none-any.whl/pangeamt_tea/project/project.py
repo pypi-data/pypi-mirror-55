245dae028e45f60813653ffbc453c71527b4870f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:51 +0000

    Add package pangeamt-tea/0.0.18/pangeamt_tea-0.0.18-py3-none-any.whl

---
+++ packages/pangeamt-tea/pangeamt_tea-0.0.18-py3-none-any.whl/pangeamt_tea/project/project.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Project:
+    def __init__(self, project_dir):
+        self._dir = project_dir
+        # load the config file and create the config
+        pass
+
+
+    @staticmethod
+    def create(parent_dir:str, customer:str, src_lang:str, tgt_lang:str, flavor:str, version:int):
+        pass
+        # TODO create the project directory
+        # TODO Create the dataset directory
+        # TODO Create the config file
+
+
+
