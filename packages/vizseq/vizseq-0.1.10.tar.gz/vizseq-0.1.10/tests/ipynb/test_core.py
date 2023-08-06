4f30637192a70cdf94843a6d0ac7219cc2af67cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:16 +0000

    Add package vizseq/0.1.10/vizseq-0.1.10.tar.gz

---
+++ packages/vizseq/vizseq-0.1.10.tar.gz/vizseq-0.1.10/tests/ipynb/test_core.py
@@ -0,0 +1,37 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import unittest
+import os
+from typing import Union, List, Optional, Type, Dict
+
+from vizseq.scorers import VizSeqScorer, VizSeqScore
+
+
+class VizSeqIpynbTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        dataset_root = 'examples/data/translation_wmt14_en_de_test'
+        if not os.path.isdir(dataset_root):
+            raise NotADirectoryError(f'{dataset_root} does not exist.')
+        with open(f'{dataset_root}/src_0.txt') as f:
+            self.source = [l.strip() for l in f]
+        with open(f'{dataset_root}/ref_0.txt') as f:
+            self.references = [[l.strip() for l in f]]
+        with open(f'{dataset_root}/pred_onlineA.0.txt') as f:
+            self.hypothesis = [l.strip() for l in f]
+
+    def test_view_stats(self):
+        pass
+
+    def test_view_examples(self):
+        pass
+
+    def test_view_scores(self):
+        pass
+
+    def test_view_n_grams(self):
+        pass
