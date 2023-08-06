0706d0d0585bf803db989b80e5d5214649217c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:40 +0000

    Add package Draugr/0.4.0/Draugr-0.4.0-py36-none-any.whl

---
+++ packages/Draugr/Draugr-0.4.0-py36-none-any.whl/draugr/writers/mock_writer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from draugr.writers.writer import Writer
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = """
+Created on 27/04/2019
+
+@author: cnheider
+"""
+
+
+class MockWriter(Writer):
+    def _close(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def _open(self):
+        return self
+
+    def _scalar(self, *args, **kwargs):
+        pass
+
+
+if __name__ == "__main__":
+    with MockWriter() as w:
+        w.scalar("a", 2)
