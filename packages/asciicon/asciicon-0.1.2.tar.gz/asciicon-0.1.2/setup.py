1089bf31a0cfd108ef1fa095a0f8d8a45619d82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:20 +0000

    Add package asciicon/0.1.2/asciicon-0.1.2.tar.gz

---
+++ packages/asciicon/asciicon-0.1.2.tar.gz/asciicon-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='asciicon',
+    version='0.1.2',
+    py_modules=['asciicon'],
+    include_package_data=True,
+    package_data={'asciicon': ['asciicons/icons/*.txt']},
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': ['asciicon = asciicon.cli:start']
+      }
+)
