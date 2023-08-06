2a89f3a94c20035f5c9271058b03e8911b89ce69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:14 +0000

    Add package s2aio/1.18/s2aio-1.18.tar.gz

---
+++ packages/s2aio/s2aio-1.18.tar.gz/s2aio-1.18/README.md
@@ -0,0 +1,34 @@
+ ![](https://github.com/MrYsLab/s2aio/blob/master/images/happy.png)
+
+Program, control and monitor an [Arduino](https://www.arduino.cc/) board using the [Scratch](scratch.mit.edu) or [Snap!](http://snap.berkeley.edu/) visual programming languages without having to learn C++. 
+
+![](https://github.com/MrYsLab/s2aio/blob/master/images/examples/blink.png)
+
+### **Included with s2aio are programming blocks** for the _**following languages**_:
+
+[![Join the chat at https://gitter.im/MrYsLab/s2aio](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/MrYsLab/s2aio?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+* English (EN)
+* Chinese (zh-CN)
+* Chinese (zh-TW)
+* Dutch (NL)
+* French (FR)
+* German (DE)
+* Greek (GR)
+* Korean (KO)
+* Italian (IT)
+* Portuguese (PT)
+* Polish (PL)
+* Spanish (ES)
+
+
+### **Features**
+* Written in Python using the new Python 3.5 asyncio library. 
+* Cross platform - works with Windows, Mac and Linux (including Debian Raspberry Pi).
+* Easy to install.
+* Easy to use.
+
+### Want to learn more? Check out the [Wiki pages](https://github.com/MrYsLab/s2aio/wiki).
+
+This project was developed using [Pycharm](https://www.jetbrains.com/pycharm/) <a href="https://www.jetbrains.com/pycharm/">
+  <img src="https://github.com/MrYsLab/python_banyan/blob/master/images/icon_PyCharm.png" alt="PyCharm" style="width:42px;height:42px;border:0">
+</a>
---
+++ packages/s2aio/s2aio-1.18.tar.gz/s2aio-1.18/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='s2aio',
+    version='1.18',
+    packages=['s2aio'],
+    install_requires=['pymata-aio>=2.22',
+                      'aiohttp>=3.3.2'],
+    package_data={'s2aio': [('configuration/*.cfg'),
+                            ('miscellaneous/*.txt, *.csv, *.ods'),
+                            ('ScratchFiles/ExtensionDescrptors/*.s2e'),
+                            ('ScratchFiles/ScratchProjects/*.sb2'),
+                            ('Snap!Files/*.xml')]},
+    entry_points={
+        'console_scripts': [
+            's2aio = s2aio.__main__:main'
+        ]
+    },
+    url='https://github.com/MrYsLab/s2aio/wiki',
+    download_url='https://github.com/MrYsLab/s2aio',
+    license='GNU General Public License v3 (GPLv3)',
+    author='Alan Yorinks',
+    author_email='MisterYsLab@gmail.com',
+    description='A Scratch 2.0 (Offline) Hardware Extension for Arduino',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['Firmata', 'Arduino', 'Scratch'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Education',
+    ],
+)
