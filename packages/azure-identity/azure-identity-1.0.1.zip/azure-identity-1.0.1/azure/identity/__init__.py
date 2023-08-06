1d7d2f30f2e6910110b9d5ed751dee9757d7b2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:09 +0000

    Add package azure-identity/1.0.1/azure-identity-1.0.1.zip

---
+++ packages/azure-identity/azure-identity-1.0.1.zip/azure-identity-1.0.1/azure/identity/__init__.py
@@ -0,0 +1,36 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+"""Credentials for Azure SDK clients."""
+
+from ._constants import KnownAuthorities
+from ._credentials import (
+    AuthorizationCodeCredential,
+    CertificateCredential,
+    ChainedTokenCredential,
+    ClientSecretCredential,
+    DefaultAzureCredential,
+    DeviceCodeCredential,
+    EnvironmentCredential,
+    InteractiveBrowserCredential,
+    ManagedIdentityCredential,
+    SharedTokenCacheCredential,
+    UsernamePasswordCredential,
+)
+
+
+__all__ = [
+    "AuthorizationCodeCredential",
+    "CertificateCredential",
+    "ChainedTokenCredential",
+    "ClientSecretCredential",
+    "DefaultAzureCredential",
+    "DeviceCodeCredential",
+    "EnvironmentCredential",
+    "InteractiveBrowserCredential",
+    "KnownAuthorities",
+    "ManagedIdentityCredential",
+    "SharedTokenCacheCredential",
+    "UsernamePasswordCredential",
+]
