326f17a3620bb52c0a45461fab6c79da0f7aa5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:37 +0000

    Add package relion-yolo-it/0.1.0/relion_yolo_it-0.1.0.tar.gz

---
+++ packages/relion-yolo-it/relion_yolo_it-0.1.0.tar.gz/relion_yolo_it-0.1.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup, find_packages
+
+requirements = ["cryolo>=1.4.1", "gemmi==0.2.8", "numpy==1.14.5"]
+setup_requirements = []
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="relion_yolo_it",
+    author="Donovan Webb",
+    author_email="donovan.webb@diamond.ac.uk",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Natural Language :: English",
+    ],
+    description="Updated relion_it with cryolo wrappers",
+    long_description=long_description,
+    long_description_content_type='text/plain',
+    install_requires=requirements,
+    license="GPLv2",
+    include_package_data=True,
+    packages=find_packages(),
+    python_requires='==3.6.8',
+    setup_requires=setup_requirements,
+    version="0.1.0",
+    zip_safe=False,
+)
+
