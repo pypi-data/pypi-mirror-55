578de46d027c1b56443b4bda9dd6fee237fa7395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:46 +0000

    Add package status-reminder/0.0.1/status-reminder-0.0.1.tar.gz

---
+++ packages/status-reminder/status-reminder-0.0.1.tar.gz/status-reminder-0.0.1/reminder/__init__.py
@@ -0,0 +1,7 @@
+class Reminder:
+    def __init__(self):
+        print("Reminder init")
+
+    def test(self):
+        print("Reminder test")
+
---
+++ packages/status-reminder/status-reminder-0.0.1.tar.gz/status-reminder-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="status-reminder",
+    version="0.0.1",
+    author="Hongjie Gao",
+    author_email="1801213783@pku.edu.cn",
+    description="Task status reminder and API exception reminder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alazycoder/Reminder",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
