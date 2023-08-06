9b0825ad180ea131a8edee4b19cacb96ec7142e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:41 +0000

    Add package getnet-py/0.2.0/getnet-py-0.2.0.tar.gz

---
+++ packages/getnet-py/getnet-py-0.2.0.tar.gz/getnet-py-0.2.0/getnet/__init__.py
@@ -0,0 +1,17 @@
+import logging
+
+from .api import API
+from .exceptions import APIException
+
+__version__ = "0.2.0"
+
+import requests
+
+if requests.__version__ < "2.0.0":
+    msg = (
+        "You are using requests version %s, which is older than "
+        "getnet-py expects, please upgrade to 2.0.0 or later."
+    )
+    raise Warning(msg % requests.__version__)
+
+logging.getLogger("getnet-py").addHandler(logging.NullHandler())
