9b446780d42e4502b65171474a21caafa5b84e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:24 +0000

    Add package nofus/0.1.1/nofus-0.1.1-py3-none-any.whl

---
+++ packages/nofus/nofus-0.1.1-py3-none-any.whl/nofus/__init__.py
@@ -0,0 +1,8 @@
+"""
+Nofus: Nate's One-File Utility Stash
+"""
+__version__ = "0.1.1"
+__author__ = 'Nate Collins'
+
+from .configfile import ConfigFile
+from .logger import Logger, LoggingInterface
