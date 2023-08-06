c5afceb038f023806a1db8a3daf9fc79b51aa241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:43 +0000

    Add package nicepay/1.1.7/nicepay-1.1.7.tar.gz

---
+++ packages/nicepay/nicepay-1.1.7.tar.gz/nicepay-1.1.7/README.rst
@@ -0,0 +1,43 @@
+===================
+nicepay
+===================
+
+|pypi version| |license| |build status|
+
+``NICEPay`` Payment Solution is a product of PT IONPAY NETWORKS that provide
+various electronic payment over the Internet safely without separate security device.
+NICEPay have integrate with Banks via host to host connection for better and faster performance.
+Using latest technology, NICEPay confidence to grow your business together.
+
+Docs: https://docs.nicepay.co.id
+
+
+Installation
+============
+
+Install from PyPI:
+
+::
+
+    $ pip install nicepay
+
+
+Running tests
+--------------
+
+To run the test use:
+
+::
+
+    $ python tests.py             # without the response
+    $ python tests.py show_print  # show the response
+
+
+.. |pypi version| image:: https://img.shields.io/pypi/v/nicepay.svg
+   :target: https://pypi.python.org/pypi/nicepay
+
+.. |license| image:: https://img.shields.io/badge/license-MIT-green.svg
+   :target: https://raw.githubusercontent.com/titipbeliin/nicepay-python/master/LICENSE
+
+.. |build status| image:: https://travis-ci.org/titipbeliin/nicepay-python.svg?branch=master
+   :target: https://travis-ci.org/titipbeliin/nicepay-python
---
+++ packages/nicepay/nicepay-1.1.7.tar.gz/nicepay-1.1.7/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+__version__ = '1.1.7'
+__author__ = 'Agus Makmun (Summon Agus)'
+__author_email__ = 'summon.agus@gmail.com'
+
+setup(
+    name='nicepay',
+    packages=['nicepay'],
+    version=__version__,
+    platforms=['Linux'],
+    url='https://github.com/titipbeliin/nicepay-python/',
+    download_url='https://github.com/titipbeliin/nicepay-python/tarball/v%s' % __version__,
+    description="Python API for nicepay.",
+    long_description=open("README.rst").read(),
+    license='MIT',
+    author=__author__,
+    author_email=__author_email__,
+    keywords=['nicepay', 'python api'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development'
+    ]
+)
