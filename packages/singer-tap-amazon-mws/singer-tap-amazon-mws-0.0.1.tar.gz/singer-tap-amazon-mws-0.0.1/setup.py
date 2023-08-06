c8a0438cf44ab82e369d38243762e88a387735df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:31 +0000

    Add package singer-tap-amazon-mws/0.0.1/singer-tap-amazon-mws-0.0.1.tar.gz

---
+++ packages/singer-tap-amazon-mws/singer-tap-amazon-mws-0.0.1.tar.gz/singer-tap-amazon-mws-0.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='singer-tap-amazon-mws',
+      version='0.0.1',
+      description='Singer.io tap for extracting data from the Amazon MWS API. Forked from https://github.com/fishtown-analytics/tap-amazon-mws',
+      author='Fishtown Analytics',
+      url='http://fishtownanalytics.com',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_amazon_mws'],
+      install_requires=[
+          'tap-framework==0.0.4',
+          'mws',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-amazon-mws=tap_amazon_mws:main
+      ''',
+      packages=find_packages(),
+      package_data={
+          'tap_amazon_mws': [
+              'schemas/*.json'
+          ]
+      })
