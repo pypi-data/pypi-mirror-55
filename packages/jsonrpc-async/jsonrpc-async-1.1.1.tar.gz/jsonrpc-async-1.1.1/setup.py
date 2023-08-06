211e2f578063421ce0ba97cde1491c4423c8bf60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:25 +0000

    Add package jsonrpc-async/1.1.1/jsonrpc-async-1.1.1.tar.gz

---
+++ packages/jsonrpc-async/jsonrpc-async-1.1.1.tar.gz/jsonrpc-async-1.1.1/setup.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+
+try:
+    from setuptools import setup
+except ImportError:
+    import sys
+    print("Please install the `setuptools` package in order to install this library", file=sys.stderr)
+    raise
+
+setup(
+    name='jsonrpc-async',
+    version='1.1.1',
+    author='Adam Mills',
+    author_email='adam@armills.info',
+    packages=('jsonrpc_async',),
+    license='BSD',
+    keywords='json-rpc async asyncio',
+    url='http://github.com/armills/jsonrpc-async',
+    description='''A JSON-RPC client library for asyncio''',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'jsonrpc-base>=1.0.3',
+        'aiohttp>=3.0.0',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+)
