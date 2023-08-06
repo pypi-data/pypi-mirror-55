92558c2dd69c5f7c2421bb18e11323bceef4d375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:44 +0000

    Add package pysubtypes/0.2.0/pysubtypes-0.2.0.tar.gz

---
+++ packages/pysubtypes/pysubtypes-0.2.0.tar.gz/pysubtypes-0.2.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from os import path
+
+__version__ = "0.2.0"
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="pysubtypes",
+    version=__version__,
+    description="Provides subclasses for common python types with additional functionality and convenience methods.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matthewgdv/subtypes",
+    license="MIT",
+    classifiers=[
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Developers",
+      "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        "aenum",
+        "bs4",
+        "case_conversion",
+        "clipboard",
+        "dateutil",
+        "django",
+        "inflect",
+        "lazy_property",
+        "maybe-else",
+        "numpy",
+        "pandas",
+        "parsedatetime",
+        "regex",
+        "tabulate"
+    ],
+    author="Matt GdV",
+    author_email="matthewgdv@gmail.com"
+)
---
+++ packages/pysubtypes/pysubtypes-0.2.0.tar.gz/pysubtypes-0.2.0/subtypes/__init__.py
@@ -0,0 +1,23 @@
+__all__ = [
+    "Enum", "AutoEnum",
+    "Markup",
+    "Singleton",
+    "NameSpace", "NameSpaceDict",
+    "Str",
+    "List_",
+    "Dict_",
+    "DateTime",
+    "Frame",
+    "Process"
+]
+
+from .enum import Enum, AutoEnum
+from .markup import Markup
+from .singleton import Singleton
+from .namespace import NameSpace
+from .str import Str
+from .list import List_
+from .dict import Dict_
+from .datetime import DateTime
+from .frame import Frame
+from .process import Process
