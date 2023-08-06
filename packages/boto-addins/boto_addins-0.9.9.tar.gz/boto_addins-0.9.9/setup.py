da6afe0cc7097c22ff66e9aaa5ea3bab655d871c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:28 +0000

    Add package boto-addins/0.9.9/boto_addins-0.9.9.tar.gz

---
+++ packages/boto-addins/boto_addins-0.9.9.tar.gz/boto_addins-0.9.9/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="boto_addins",
+    version="0.9.9",
+    author="Uploadcare",
+    author_email="ak@uploadcare.com",
+    description="Async proxy libraries for AWS services.",
+    install_requires=[
+        'tornado>=6.0.0',
+        'tornado-botocore>=1.5.0',
+        'boto',
+        'YURL',
+    ],
+    keywords="aws amazon S3 storage "
+             "lambda request-response invoke",
+    url="http://packages.python.org/an_example_pypi_project",
+    packages=['boto_addins'],
+)
