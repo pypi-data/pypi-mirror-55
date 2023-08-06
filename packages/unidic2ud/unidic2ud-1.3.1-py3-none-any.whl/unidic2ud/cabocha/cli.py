5e7cc7efbe3142660b4d5b27ecf0662c08e655d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:29 +0000

    Add package unidic2ud/1.3.1/unidic2ud-1.3.1-py3-none-any.whl

---
+++ packages/unidic2ud/unidic2ud-1.3.1-py3-none-any.whl/unidic2ud/cabocha/cli.py
@@ -0,0 +1,69 @@
+import sys
+
+def main():
+  argc=len(sys.argv)
+  i=1
+  f=0
+  u="ipadic"
+  while i<argc:
+    o=sys.argv[i]
+    if o.startswith("-f"):
+      if o=="-f":
+        i+=1
+        f=int(sys.argv[i])
+      else:
+        f=int(sys.argv[i][2:])
+      if f<0 or f>4:
+        usage()
+    elif o.startswith("-U"):
+      if o=="-U":
+        i+=1
+        u=sys.argv[i]
+      else:
+        u=sys.argv[i][2:]
+    elif o=="-h" or o=="--help":
+      usage()
+    else:
+      break
+    i+=1
+  else:
+    ja=parser(u,f)
+    while True:
+      try:
+        s=input()
+      except:
+        return
+      print(ja(s),end="")
+  ja=parser(u,f)
+  while i<argc:
+    p=open(sys.argv[i],"r",encoding="utf-8")
+    s=p.read()
+    p.close()
+    print(ja(s),end="")
+    i+=1
+
+class parser(object):
+  def __init__(self,UniDic,format):
+    self.format=format
+    if format==4:
+      from unidic2ud import load
+      self.load=load(UniDic)
+    else:
+      from unidic2ud.cabocha import Parser
+      self.load=Parser(UniDic)
+  def __call__(self,sentence):
+    if self.format==4:
+      return self.load(sentence,raw=True)
+    return self.load.parse(sentence).toString(self.format)
+
+def usage():
+  print("Usage: udcabocha -U UniDic [-f 0-4] file",file=sys.stderr)
+  from unidic2ud import dictlist
+  s=dictlist()
+  if s>"":
+    print(" Dict: "+s.replace(".udpipe","(udpipe)").replace("\n"," ").rstrip(),file=sys.stderr)
+  sys.exit()
+
+if __name__=="__main__":
+  main()
+
