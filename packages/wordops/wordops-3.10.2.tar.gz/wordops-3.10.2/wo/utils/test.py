1c356cfef6078e0eecc3d86a90e23c760876f062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:06 +0000

    Add package wordops/3.10.2/wordops-3.10.2.tar.gz

---
+++ packages/wordops/wordops-3.10.2.tar.gz/wordops-3.10.2/wo/utils/test.py
@@ -0,0 +1,15 @@
+"""Testing utilities for WordOps"""
+from cement.utils.test import *
+from wo.cli.main import WOTestApp
+
+
+class WOTestCase(CementTestCase):
+    app_class = WOTestApp
+
+    def setUp(self):
+        """Override setup actions (for every test)."""
+        super(WOTestCase, self).setUp()
+
+    def tearDown(self):
+        """Override teardown actions (for every test)."""
+        super(WOTestCase, self).tearDown()
