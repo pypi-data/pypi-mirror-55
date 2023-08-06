3d71c817aa533537e0642ce7c59309e142085eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:59 +0000

    Add package testkernel/1.6.3/testkernel-1.6.3.tar.gz

---
+++ packages/testkernel/testkernel-1.6.3.tar.gz/testkernel-1.6.3/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='testkernel',
+    version='1.6.3',
+    packages=['kernels', 'kernels/att_kernel', 'kernels/intel_kernel', 'kernels/riscv_kernel', 'assembler', 'assembler/Intel', 'assembler/RISCV'],
+)
