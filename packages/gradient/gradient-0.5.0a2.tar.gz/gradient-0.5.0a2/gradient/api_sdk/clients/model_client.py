2b46faf69ef85bb0ce254a2ccbe236f25cccff75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:14 +0000

    Add package gradient/0.5.0a2/gradient-0.5.0a2.tar.gz

---
+++ packages/gradient/gradient-0.5.0a2.tar.gz/gradient-0.5.0a2/gradient/api_sdk/clients/model_client.py
@@ -0,0 +1,24 @@
+from .base_client import BaseClient
+from .. import repositories
+
+
+class ModelsClient(BaseClient):
+    def list(self, experiment_id=None, project_id=None):
+        """Get list of models
+
+        :param str experiment_id: Experiment ID
+        :param str project_id: Project ID
+
+        :rtype: list[models.Model]
+        """
+        repository = repositories.ListModels(api_key=self.api_key, logger=self.logger)
+        models_list = repository.list(experiment_id=experiment_id, project_id=project_id)
+        return models_list
+
+    def delete(self, model_id):
+        """Delete a model
+
+        :param str model_id: Model ID
+        """
+        repository = repositories.DeleteModel(api_key=self.api_key, logger=self.logger)
+        repository.delete(model_id)
---
+++ packages/gradient/gradient-0.5.0a2.tar.gz/gradient-0.5.0a2/gradient/api_sdk/models/__init__.py
@@ -0,0 +1,11 @@
+from .artifact import Artifact
+from .deployment import Deployment
+from .experiment import BaseExperiment, MultiNodeExperiment, SingleNodeExperiment, MpiMultiNodeExperiment
+from .hyperparameter import Hyperparameter
+from .job import Job
+from .log import LogRow
+from .machine import Machine, MachineEvent, MachineUtilization
+from .model import Model
+from .notebook import Notebook
+from .project import Project
+from .tensorboard import Instance, Tensorboard
