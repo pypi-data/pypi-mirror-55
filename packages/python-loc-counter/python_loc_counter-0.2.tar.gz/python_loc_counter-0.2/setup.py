632a070c1750e7a21e40bcca15abb67560963fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:55 +0000

    Add package python-loc-counter/0.2/python_loc_counter-0.2.tar.gz

---
+++ packages/python-loc-counter/python_loc_counter-0.2.tar.gz/python_loc_counter-0.2/setup.py
@@ -0,0 +1,30 @@
+#from distutils.core import setup
+from os import path
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'python_loc_counter',
+  packages = ['python_loc_counter'],
+  version = '0.2',
+  license='MIT', 
+  description = 'Get LOC metrics for python scripts',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  author = 'Bryce Vonilten',
+  author_email = 'ki6bpd@gmail.com',
+  url = 'https://github.com/BryceV/python_loc_counter', 
+  download_url = 'https://github.com/BryceV/python_loc_counter/archive/v.02.tar.gz',
+  keywords = ['python', 'lines of code', 'loc'],
+  install_requires=[ ],
+  classifiers=[
+    'Development Status :: 3 - Alpha', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
