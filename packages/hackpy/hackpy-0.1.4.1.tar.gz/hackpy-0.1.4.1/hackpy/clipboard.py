19a2bc04677f9cdbd48df6a4fd524d97a126e169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:53 +0000

    Add package hackpy/0.1.4.1/hackpy-0.1.4.1.tar.gz

---
+++ packages/hackpy/hackpy-0.1.4.1.tar.gz/hackpy-0.1.4.1/hackpy/clipboard.py
@@ -0,0 +1,13 @@
+import pyperclip
+
+
+class clipboard:
+    ##|
+    ##| hackpy.clipboard.set('Text') # Copy text to clipboard
+    ##| print('Data in clipboard:' + clipboard.get()) # Get text from clipboard
+    ##|
+    def set(text):
+        pyperclip.copy(text)
+
+    def get():
+        return pyperclip.paste()
