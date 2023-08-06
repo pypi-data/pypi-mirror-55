486152a437a6bc05b08dcd58cfe9028150a31acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:28 +0000

    Add package os-netloc-rule/0.2/os-netloc-rule-0.2.tar.gz

---
+++ packages/os-netloc-rule/os-netloc-rule-0.2.tar.gz/os-netloc-rule-0.2/src/os_netloc_rule/matcher.py
@@ -0,0 +1,15 @@
+class Matcher(object):
+    def __init__(self):
+        pass
+
+    def match(self, netloc):
+        pass
+
+    def load(self, netloc, rule, cmp=None):
+        pass
+
+    def delete(self, netloc):
+        pass
+
+    def dump(self):
+        pass
