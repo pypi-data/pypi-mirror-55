42eecd28c79ea2f0927579c433cc1f09d0e229c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:10 +0000

    Add package aws-cdk.aws-codecommit/1.16.0/aws-cdk.aws-codecommit-1.16.0.tar.gz

---
+++ packages/aws-cdk.aws-codecommit/aws-cdk.aws-codecommit-1.16.0.tar.gz/aws-cdk.aws-codecommit-1.16.0/setup.py
@@ -0,0 +1,45 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-codecommit",
+    "version": "1.16.0",
+    "description": "CDK Constructs for AWS CodeCommit",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_codecommit",
+        "aws_cdk.aws_codecommit._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_codecommit._jsii": [
+            "aws-codecommit@1.16.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_codecommit": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.2",
+        "publication>=0.0.3",
+        "aws-cdk.aws-events~=1.16,>=1.16.0",
+        "aws-cdk.aws-iam~=1.16,>=1.16.0",
+        "aws-cdk.core~=1.16,>=1.16.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
