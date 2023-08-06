24e0aace5ef3f9f3677d9baaa829818c55cd49e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:01 +0000

    Add package behave-restful/0.2.0/behave_restful-0.2.0-py2.py3-none-any.whl

---
+++ packages/behave-restful/behave_restful-0.2.0-py2.py3-none-any.whl/behave_restful/about.py
@@ -0,0 +1,33 @@
+"""
+Provides properties with information about ``behave_restful``.
+"""
+
+project = 'behave-restful'
+version = '0.2'
+release = '0.2.0'
+description = 'Implements Gherking language for REST services.'
+copyright = '2017 Abantos'
+author = 'Isaac Rodriguez'
+author_email = 'oss.abantos@outlook.com'
+url = 'https://github.com/behave-restful/behave-restful'
+license = 'MIT'
+keywords = 'rest bdd behave gherkin test automation testing'
+
+classifiers = [
+    'Development Status :: 1 - Planning',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Software Development :: Testing :: Acceptance',
+    'Topic :: Software Development :: Testing :: BDD',
+]
