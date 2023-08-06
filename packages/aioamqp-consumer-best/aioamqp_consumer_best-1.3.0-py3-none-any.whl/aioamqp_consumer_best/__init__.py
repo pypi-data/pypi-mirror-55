325145373b0d67ee9418495ced24dda26a238dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:25 +0000

    Add package aioamqp-consumer-best/1.3.0/aioamqp_consumer_best-1.3.0-py3-none-any.whl

---
+++ packages/aioamqp-consumer-best/aioamqp_consumer_best-1.3.0-py3-none-any.whl/aioamqp_consumer_best/__init__.py
@@ -0,0 +1,8 @@
+from .base_middlewares import Filter, FilterNones, Map, SkipAll, ToBulks
+from .connect import connect_and_open_channel
+from .consumer import Consumer
+from .declare_queue import declare_queue
+from .message import Message
+from .middlewares import Process, ProcessBulk, load_json
+from .records import ConnectionParams, Exchange, ExchangeType, Queue, QueueBinding
+from ._load_balancing_policy import LoadBalancingPolicyABC, RoundRobinPolicy
