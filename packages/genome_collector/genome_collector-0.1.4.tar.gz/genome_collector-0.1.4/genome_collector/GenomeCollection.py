21fe5894b5d184cf10dde03f0a74d160a432772e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:52 +0000

    Add package genome_collector/0.1.4/genome_collector-0.1.4.tar.gz

---
+++ packages/genome_collector/genome_collector-0.1.4.tar.gz/genome_collector-0.1.4/genome_collector/GenomeCollection.py
@@ -0,0 +1,138 @@
+import os
+import json
+
+import proglog
+from Bio import SeqIO
+
+from .mixins.BlastMixin import BlastMixin
+from .mixins.NCBIMixin import NCBIMixin
+from .mixins.FileManagerMixin import FileManagerMixin
+from .mixins.BowtieMixin import BowtieMixin
+
+
+class GenomeCollection(BlastMixin, NCBIMixin, FileManagerMixin, BowtieMixin):
+    """Collection of local data files including genomes and BLAST databases.
+
+    Parameters
+    ==========
+
+    data_dir
+      Path to the directory where all genomes informations, sequences and
+      blast database will be kept. The default value "local_default" means
+      that the system-specific user data directory will be used, so either
+      ``~/.local/share/genome_collector`` on Linux, or
+      ``~/Library/Application Support/genome_collector`` on MacOS, etc.
+
+    logger
+      Logger to which the messages will be sent when downloading files or
+      building blast databases. Use "bar" for a default bar logger, None
+      for no logging, or any Proglog logger.
+
+    Attributes
+    ==========
+
+    autodownload
+      When an absent local data file is requested and ``autodownload`` is True,
+      the file will be automatically downloaded over the Internet. Otherwise
+      a FileNotFoundError is raised. Attribute ``autodownload`` can be set to
+      False on a single ``collection`` instance, or at class level with
+      ``GenomeCollection.autodownload = False`` to globally prevent Genome
+      Collector from downloadind data files.
+
+    default_dir
+      Default directory to use when parameter data_dir='default'. This
+      attribute allows to set a global default_dir at the beginning of a
+      script with ``GlobalCollection.default_dir = '/my/new/dir/'``
+
+    messages_prefix
+      Prefix appearing as "[prefix] " in all logging messages.
+
+    datafiles_extensions
+      Dictionnary linking data file types to standardized file extensions.
+
+    """
+
+    messages_prefix = "[genome_collector] "
+
+    def __init__(self, data_dir="default", logger="bar"):
+        if data_dir == "default":
+            data_dir = self.default_dir
+        self.data_dir = data_dir
+        self._logger = proglog.default_bar_logger(logger)
+        self._time_of_last_entrez_call = None
+
+    def _log_message(self, message):
+        """Send a message (with prefix) to the logger)"""
+        self._logger(message=self.messages_prefix + message)
+
+    def get_taxid_infos(self, taxid):
+        """Return a dict with data about the taxid.
+
+        Examples
+        ========
+
+        >>> collection.get_taxid_infos(511145)
+        >>> {
+        >>>      'Organism Name': 'Escherischia Coli', 
+        >>>      'DefLine': 'A well-studied enteric bacterium',
+        >>>      'Organism_Kingdom': 'Bacteria',
+        >>>      'AssemblyID': '1755381',
+        >>>      ...
+        >>> }
+
+
+
+        """
+        taxid = str(taxid)
+        path = self.datafile_path(taxid=taxid, data_type="infos")
+        if not os.path.exists(path):
+            if not self.autodownload:
+                error_message = (
+                    "No infos for taxid %s found locally, and parameter "
+                    "autodownload_enabled is set to False in "
+                    "genome_collector.settings"
+                ) % taxid
+                raise FileNotFoundError(error_message)
+            self.download_taxid_genome_infos_from_ncbi(taxid)
+        with open(path, "r") as f:
+            return json.load(f)
+
+    def get_taxid_genome_data_path(self, taxid, data_type="genomic_fasta"):
+        """Return a path to the taxid's genome sequence. Download if needed.
+        
+        ``data_type`` is either genomic_fasta, genomic_genbank, genomic_gff,
+        or protein_fasta
+        """
+        taxid = str(taxid)
+        path = self.datafile_path(taxid=taxid, data_type=data_type)
+        if not os.path.exists(path):
+            if not self.autodownload:
+                error_message = (
+                    "No genome for taxid %s found locally, and parameter "
+                    "autodownload_enabled is set to False in "
+                    "genome_collector.settings"
+                ) % taxid
+                raise FileNotFoundError(error_message)
+            self.download_taxid_genome_data_from_ncbi(
+                taxid, data_type=data_type
+            )
+        return path
+
+    def get_taxid_biopython_records(
+        self, taxid, source_type="genomic_genbank", as_iterator=False
+    ):
+        """Return a list of biopython records for the genome's chromosome.
+        
+        Even if there is a single record (for genomes with a single chromosome,
+        a list is returned.
+
+        For huge genomes, use the ``as_iterator`` option to return a Python
+        iterator, which avoids to load all chromosomes at once in memory.
+        """
+        path = self.get_taxid_genome_data_path(taxid, data_type=source_type)
+        data_format = source_type.split("_")[1]
+        records = SeqIO.parse(path, data_format)
+        if as_iterator:
+            return records
+        else:
+            return list(records)
---
+++ packages/genome_collector/genome_collector-0.1.4.tar.gz/genome_collector-0.1.4/genome_collector/mixins/BowtieMixin.py
@@ -0,0 +1,47 @@
+"""Mixin for Bowtie methods, inherited by GenomeCollection.""" 
+
+import subprocess
+import os
+
+class BowtieMixin:
+    """All methods are directly accessible to GenomeCollection instances."""
+
+    def generate_bowtie_index_for_taxid(self, taxid, version="1"):
+        """Generate a Bowtie (1 or 2) index for the given TaxID."""
+        taxid = str(taxid)
+        fa_path = self.get_taxid_genome_data_path(
+            taxid, data_type="genomic_fasta"
+        )
+        db_path = self.datafile_path(
+            taxid=taxid, data_type="bowtie%s_index" % version
+        )
+        bowtie_args = ["bowtie%s-build" % version, fa_path, db_path]
+
+        self._log_message(
+            "Generating Bowtie%s index for taxid %s" % (version, taxid)
+        )
+        pipe = subprocess.PIPE
+        process = subprocess.run(bowtie_args, stdout=pipe, stderr=pipe)
+        if process.returncode:
+            error_message = (
+                "Bowtie%s index generation for TaxID %s failed with error: %s"
+            ) % (version, taxid, process.stderr)
+            raise OSError(error_message)
+        self._log_message(
+            "Done generating Bowtie%s index for taxid %s" % (version, taxid)
+        )
+
+    def get_taxid_bowtie_index_path(self, taxid, version="1"):
+        """Get a path to the Bowtie (1 or 2) index for the given TaxID.
+        
+        This will download data and generate the index if necessary.
+        This requires Bowtie (1 or 2) installed.
+        """
+        taxid = str(taxid)
+        index_path = self.datafile_path(
+            taxid=taxid, data_type="bowtie%s_index" % version
+        )
+        expected_file_extension = {"1": ".1.ebwt", "2": ".1.bt2"}
+        if not os.path.exists(index_path + expected_file_extension[version]):
+            self.generate_bowtie_index_for_taxid(taxid, version=version)
+        return index_path
