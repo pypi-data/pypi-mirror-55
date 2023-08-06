8db7b42d2d6d43e23470d7f0d00c3cdb4e4799c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:38 +0000

    Add package bioutils/0.5.2/bioutils-0.5.2-py2.py3-none-any.whl

---
+++ packages/bioutils/bioutils-0.5.2-py2.py3-none-any.whl/bioutils/normalize.py
@@ -0,0 +1,387 @@
+# -*- coding: utf-8 -*-
+"""Provides functionality for normalizing alleles, ensuring comparable representations.
+
+"""
+
+import copy
+import enum
+import math
+import logging
+
+import attr
+
+_logger = logging.getLogger(__name__)
+debug = False
+
+NormalizationMode = enum.Enum("NormalizationMode", "EXPAND LEFTSHUFFLE RIGHTSHUFFLE TRIMONLY VCF")
+"""Enum passed to normalize to select the normalization mode.
+
+Attributes:
+    EXPAND: Normalize alleles to maximal extent both left and right.
+    LEFTSHUFFLE: Normalize alleles to maximal extent left.
+    RIGHTSHUFFLE: Normalize alleles to maximal extent right.
+    TRIMONLY: Only trim the common prefix and suffix of alleles.
+    VCF: Normalize with VCF. 
+"""
+
+
+def normalize(
+        sequence,
+        interval,
+        alleles,
+        mode=NormalizationMode.EXPAND,
+        bounds=None,
+        anchor_length=0,
+):
+    """Normalizes the alleles that co-occur on sequence at interval, ensuring comparable representations.
+
+    Args:
+        sequence (str or iterable): The reference sequence; must support indexing and ``__getitem__``.
+        interval (2-tuple of int): The location of alleles in the reference sequence as ``(start, end)``. 
+            Interbase coordinates. 
+        alleles (iterable of str): The sequences to be normalized. The first element 
+            corresponds to the reference sequence being unchanged and must be None.
+        bounds (2-tuple of int, optional): Maximal extent of normalization left and right. 
+            Must be provided if sequence doesn't support ``__len__``. Defaults to ``(0, len(sequence))``.
+        mode (NormalizationMode Enum or string, optional): A NormalizationMode Enum or the corresponding string.
+            Defaults to ``EXPAND``.
+        anchor (int, optional): number of flanking residues left and right. Defaults to ``0``.
+
+    Returns: 
+        tuple: ``(new_interval, [new_alleles])``
+
+    Raises:
+        ValueError: If normalization mode is VCF and `anchor_length` is nonzero.
+        ValueError: If the interval start is greater than the end.
+        ValueError: If the first (reference) allele is not `None`.
+        ValueError: If there are not at least two distinct alleles.
+    
+    Examples:
+        >>> sequence = "CCCCCCCCACACACACACTAGCAGCAGCA"
+        >>> normalize(sequence, interval=(22,25), alleles=(None, "GC", "AGCAC"), mode='TRIMONLY')
+        ((22, 24), ('AG', 'G', 'AGCA'))
+
+        >>> normalize(sequence, interval=(22, 22), alleles=(None, 'AGC'), mode='RIGHTSHUFFLE')
+        ((29, 29), ('', 'GCA'))
+
+        >>> normalize(sequence, interval=(22, 22), alleles=(None, 'AGC'), mode='EXPAND')
+        ((19, 29), ('AGCAGCAGCA', 'AGCAGCAGCAGCA'))
+    """
+
+    interval = _Interval(*interval)
+    if interval.start > interval.end:
+        raise ValueError("Interval start > end; must be start <= end")
+
+    if bounds is None:
+        bounds = _Interval(0, len(sequence))
+    else:
+        bounds = _Interval(*bounds)
+
+    left_anchor = right_anchor = anchor_length
+
+    if not isinstance(mode, NormalizationMode):
+        mode = NormalizationMode[mode]    # e.g., mode="LEFTSHUFFLE" OK
+
+    if mode == NormalizationMode.VCF:
+        if anchor_length:
+            raise ValueError("May not provide non-zero anchor size with VCF normalization mode")
+        mode = NormalizationMode.LEFTSHUFFLE
+        left_anchor = 1
+        right_anchor = 0
+
+    if alleles[0] is not None:
+        raise ValueError("First allele, the reference allele, must be None")
+    alleles = list(alleles)    # in case tuple
+    alleles[0] = sequence[interval.start:interval.end]
+    if len(set(alleles)) < 2:
+        raise ValueError("Must have at least two distinct alleles to normalize")
+
+    if debug:
+        _print_state(interval, bounds, sequence=sequence, alleles=alleles, comment="Starting state")
+
+    # Trim: remove common suffix, prefix, and adjust interval to match
+    l_trimmed, alleles = trim_left(alleles)
+    interval.start += l_trimmed
+    r_trimmed, alleles = trim_right(alleles)
+    interval.end -= r_trimmed
+    if debug:
+        _print_state(interval, bounds, sequence=sequence, alleles=alleles, comment="After trimming")
+
+    lens = [len(a) for a in alleles]
+
+    if mode == NormalizationMode.LEFTSHUFFLE:
+        dist = roll_left(sequence, alleles, interval.start - 1, bounds.start)
+        for i, a in enumerate(alleles):
+            if lens[i]:
+                adist = -dist % lens[i]
+                alleles[i] = a[adist:] + a[:adist]
+        interval.start -= dist
+        interval.end -= dist
+
+    elif mode == NormalizationMode.RIGHTSHUFFLE:
+        dist = roll_right(sequence, alleles, interval.end, bounds.end - 1)
+        for i, a in enumerate(alleles):
+            if lens[i]:
+                adist = dist % lens[i]
+                alleles[i] = a[adist:] + a[:adist]
+        interval.start += dist
+        interval.end += dist
+
+    elif mode == NormalizationMode.EXPAND:
+        ldist = roll_left(sequence, alleles, interval.start - 1, bounds.start)
+        rdist = roll_right(sequence, alleles, interval.end, bounds.end - 1)
+
+        lseq = sequence[interval.start - ldist:interval.start]
+        rseq = sequence[interval.end:interval.end + rdist]
+        alleles = [lseq + a + rseq for a in alleles]
+
+        interval.start -= ldist
+        interval.end += rdist
+
+    if debug:
+        _print_state(interval, bounds, sequence=sequence, alleles=alleles, comment=f"After {mode}")
+
+    # Add left and/or right flanking sequence
+    if left_anchor or right_anchor:
+        anchor_left = max(bounds.start, interval.start - left_anchor)
+        anchor_right = min(bounds.end, interval.end + right_anchor)
+        left_anchor_seq = sequence[anchor_left:interval.start]
+        right_anchor_seq = sequence[interval.end:anchor_right]
+        interval.start = anchor_left
+        interval.end = anchor_right
+        alleles = [left_anchor_seq + a + right_anchor_seq for a in alleles]
+        if debug:
+            _print_state(interval, bounds, sequence=sequence, alleles=alleles, comment="After anchoring")
+
+    return (interval.start, interval.end), tuple(alleles)
+
+
+############################################################################
+# INTERNAL
+
+
+@attr.s
+class _Interval:
+    start = attr.ib(int)
+    end = attr.ib(int)
+
+
+# TODO: Rewrite trim_* code -- no need to modify array each time!
+def trim_left(alleles):
+    """Removes common prefix of given alleles.
+
+    Args:
+        alleles (list of str): A list of alleles.
+        
+    Returns:
+        tuple: ``(number_trimmed, [new_alleles])``.
+
+    Examples:
+        >>> trim_left(["","AA"])
+        (0, ['', 'AA'])
+
+        >>> trim_left(["A","AA"])
+        (1, ['', 'A'])
+
+        >>> trim_left(["AT","AA"])
+        (1, ['T', 'A'])
+
+        >>> trim_left(["AA","AA"])
+        (2, ['', ''])
+
+        >>> trim_left(["CAG","CG"])
+        (1, ['AG', 'G'])
+    """
+
+    trimmed = 0
+    while all(len(a) > 0 for a in alleles):
+        a0 = alleles[0]
+        for a in alleles[1:]:
+            if a0[0] != a[0]:
+                return trimmed, alleles
+        alleles = [a[1:] for a in alleles]
+        trimmed += 1
+    return (trimmed, alleles)
+
+
+def trim_right(alleles):
+    """Removes common suffix of given alleles.
+
+    Args:
+        alleles (list of str): A list of alleles.
+
+    Returns:
+        tuple: ``(number_trimmed, [new_alleles])``.
+
+    Examples:
+        >>> trim_right(["","AA"])
+        (0, ['', 'AA'])
+
+        >>> trim_right(["A","AA"])
+        (1, ['', 'A'])
+
+        >>> trim_right(["AT","AA"])
+        (0, ['AT', 'AA'])
+
+        >>> trim_right(["AA","AA"])
+        (2, ['', ''])
+
+        >>> trim_right(["CAG","CG"])
+        (1, ['CA', 'C'])
+    """
+
+    trimmed = 0
+    while all(len(a) > 0 for a in alleles):
+        a0 = alleles[0]
+        for a in alleles[1:]:
+            if a0[-1] != a[-1]:
+                return trimmed, alleles
+        alleles = [a[:-1] for a in alleles]
+        trimmed += 1
+    return (trimmed, alleles)
+
+
+def roll_left(sequence, alleles, ref_pos, bound):
+    """Determines common distance all alleles can be rolled (circularly permuted) left 
+    within the reference sequence without altering it.
+
+    Args:
+        sequence (str): The reference sequence.
+        alleles (list of str): The sequences to be normalized.
+        ref_pos (int): The beginning index for rolling.
+        bound (int): The lower bound index in the reference sequence for normalization, hence also for rolling.
+    
+    Returns:
+        int: The distance that the alleles can be rolled.
+    """
+
+    # circularly permute sequence d steps, using modulo arithmetic
+    lens = [len(a) for a in alleles]
+    d = 0
+    max_d = ref_pos - bound
+    while (d <= max_d and not any(a and a[-(d + 1) % lens[i]] != sequence[ref_pos - d] for i, a in enumerate(alleles))):
+        d += 1
+    return d
+
+
+def roll_right(sequence, alleles, ref_pos, bound):
+    """Determines common distance all alleles can be rolled (circularly permuted) right 
+    within the reference sequence without altering it.
+
+    Args:
+        sequence (str): The reference sequence.
+        alleles (list of str): The sequences to be normalized.
+        ref_pos (int): The start index for rolling.
+        bound (int): The upper bound index in the reference sequence for normalization, hence also for rolling.
+    Returns:
+        int: The distance that the alleles can be rolled
+    """
+    
+    # circularly permute sequence d steps, using modulo arithmetic
+    lens = [len(a) for a in alleles]
+    d = 0
+    max_d = bound - ref_pos
+    while (d <= max_d and not any(a and a[d % lens[i]] != sequence[ref_pos + d] for i, a in enumerate(alleles))):
+        d += 1
+    return d
+
+
+############################################################################
+# Debugging
+
+pfx = "        "
+
+
+def _print_state(interval, bounds, sequence, alleles, comment):
+    """
+    
+    
+    
+    
+    
+    """
+    line = pfx + "  " * interval.start + "^"
+    if interval.end > interval.start:
+        line += "-" * ((interval.end - interval.start - 1) * 2 + 1) + "^"
+    print(line + f" [{interval.start},{interval.end}): {alleles}   | {comment}")
+    return
+
+    margin = 15
+    leftseq = sequence[max(0, interval.start - margin):interval.start]
+    rightseq = sequence[interval.end:interval.end + margin]
+
+    row_fmt = "{:>20.20s}{:>20.20s}{:^20.20s}{:<20.20s}{:<20.20s}"
+    rows = [
+        row_fmt.format(str(bounds.start), "", f"[{interval.start},{interval.end})", "", str(bounds.end)),
+        row_fmt.format("//", "|", "", "|", "//"),
+        row_fmt.format("", leftseq, alleles[0], rightseq, "")
+    ] + [row_fmt.format("", "", a, "", "") for a in alleles[1:]]
+    print("\n".join(rows))
+
+
+def _print_seq_row(sequence):
+    print(pfx + " ".join("0123456789" * math.ceil(len(sequence) / 10)))
+    print(pfx + " " + " ".join(sequence))
+
+
+if __name__ == "__main__":    # pragma: no cover
+    from functools import partial
+
+    if debug:
+        _logger.setLevel("DEBUG")
+
+    sequence = "CCCCCCCCACACACACACTAGCAGCAGCAT"
+    #                        1                   2                   3
+    #    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0
+    #     C C C C C C C C A C A C A C A C A C T A G C A G C A G C A T
+
+    tests = [
+    #{"interval": (5,5), "alleles": [None, "C"]},
+    #{"interval": (5,6), "alleles": [None, "CC"]},
+    #{"interval": (5,6), "alleles": [None, ""]},
+    #{"interval": (13,13), "alleles": [None, "CA"]},
+    #{"interval": (14,14), "alleles": [None, "AC"]},
+    #{"interval": (13,15), "alleles": [None, ""]},
+        {
+            "interval": (22, 22),
+            "alleles": [None, "AGC"]
+        },
+        {
+            "interval": (22, 22),
+            "alleles": [None, "AGCT"]
+        },
+        {
+            "interval": (22, 22),
+            "alleles": [None, "AGC", "AGCT"]
+        },
+    #{"interval": (22,25), "alleles": [None, ""]},
+    #{"interval": (22,25), "alleles": [None, "", "AGC"]},
+    #{"interval": (22,25), "alleles": [None, "", "AGCAGC"]},
+    ]
+
+    normalize_seq = partial(normalize, sequence=sequence)
+    normalize_trim = partial(normalize_seq, mode=NormalizationMode.TRIMONLY)
+    normalize_left = partial(normalize_seq, mode=NormalizationMode.LEFTSHUFFLE)
+    normalize_right = partial(normalize_seq, mode=NormalizationMode.RIGHTSHUFFLE)
+    normalize_expand = partial(normalize_seq, mode=NormalizationMode.EXPAND)
+    normalize_vcf = partial(normalize_seq, mode=NormalizationMode.VCF)
+
+    debug = True
+
+    def test1(**kwargs):
+        print(f"* {kwargs}")
+        _print_seq_row(sequence)
+        result = normalize_seq(**kwargs)
+        kwargs["mode"] = str(kwargs["mode"])
+        print(f"assert {result} == normalize_seq({kwargs})")
+
+    for test in tests:
+        print("############################################################################")
+        for mode in ("EXPAND", ):    # "LEFTSHUFFLE", "RIGHTSHUFFLE", "EXPAND"):
+            for bm in (None, ):
+                if bm is None:
+                    bounds = None
+                else:
+                    bounds = (test["interval"][0] - bm, test["interval"][1] + bm)
+                test["bounds"] = bounds
+                test1(mode=mode, **test)
---
+++ packages/bioutils/bioutils-0.5.2-py2.py3-none-any.whl/bioutils/sequences.py
@@ -0,0 +1,472 @@
+# -*- coding: utf-8 -*-
+"""Simple functions and lookup tables for nucleic acid and amino acid sequences.
+"""
+
+import logging
+import re
+from string import ascii_lowercase
+
+
+_logger = logging.getLogger(__name__)
+
+aa3_to_aa1_lut = {
+    "Ala": "A",
+    "Arg": "R",
+    "Asn": "N",
+    "Asp": "D",
+    "Cys": "C",
+    "Gln": "Q",
+    "Glu": "E",
+    "Gly": "G",
+    "His": "H",
+    "Ile": "I",
+    "Leu": "L",
+    "Lys": "K",
+    "Met": "M",
+    "Phe": "F",
+    "Pro": "P",
+    "Ser": "S",
+    "Thr": "T",
+    "Trp": "W",
+    "Tyr": "Y",
+    "Val": "V",
+    "Xaa": "X",
+    "Ter": "*",
+    "Sec": "",
+}
+
+aa1_to_aa3_lut = {v: k for k, v in aa3_to_aa1_lut.items()}
+
+dna_to_aa1_lut = {      # NCBI standard translation table
+    'AAA': 'K',
+    'AAC': 'N',
+    'AAG': 'K',
+    'AAT': 'N',
+    'ACA': 'T',
+    'ACC': 'T',
+    'ACG': 'T',
+    'ACT': 'T',
+    'AGA': 'R',
+    'AGC': 'S',
+    'AGG': 'R',
+    'AGT': 'S',
+    'ATA': 'I',
+    'ATC': 'I',
+    'ATG': 'M',
+    'ATT': 'I',
+    'CAA': 'Q',
+    'CAC': 'H',
+    'CAG': 'Q',
+    'CAT': 'H',
+    'CCA': 'P',
+    'CCC': 'P',
+    'CCG': 'P',
+    'CCT': 'P',
+    'CGA': 'R',
+    'CGC': 'R',
+    'CGG': 'R',
+    'CGT': 'R',
+    'CTA': 'L',
+    'CTC': 'L',
+    'CTG': 'L',
+    'CTT': 'L',
+    'GAA': 'E',
+    'GAC': 'D',
+    'GAG': 'E',
+    'GAT': 'D',
+    'GCA': 'A',
+    'GCC': 'A',
+    'GCG': 'A',
+    'GCT': 'A',
+    'GGA': 'G',
+    'GGC': 'G',
+    'GGG': 'G',
+    'GGT': 'G',
+    'GTA': 'V',
+    'GTC': 'V',
+    'GTG': 'V',
+    'GTT': 'V',
+    'TAA': '*',
+    'TAC': 'Y',
+    'TAG': '*',
+    'TAT': 'Y',
+    'TCA': 'S',
+    'TCC': 'S',
+    'TCG': 'S',
+    'TCT': 'S',
+    'TGA': '*',
+    'TGC': 'C',
+    'TGG': 'W',
+    'TGT': 'C',
+    'TTA': 'L',
+    'TTC': 'F',
+    'TTG': 'L',
+    'TTT': 'F',
+}
+
+
+complement_transtable = bytes.maketrans(b"ACGT", b"TGCA")
+
+
+def aa_to_aa1(seq):
+    """Coerces string of 1- or 3-letter amino acids to 1-letter representation.
+
+    Args:
+        seq (str): An amino acid sequence.
+        
+    Returns:
+        str: The sequence as one of 1-letter amino acids.    
+
+    Examples:
+        >>> aa_to_aa1("CATSARELAME")
+        'CATSARELAME'
+
+        >>> aa_to_aa1("CysAlaThrSerAlaArgGluLeuAlaMetGlu")
+        'CATSARELAME'
+
+        >>> aa_to_aa1(None)
+    """
+
+    if seq is None:
+        return None
+    return aa3_to_aa1(seq) if looks_like_aa3_p(seq) else seq
+
+
+def aa_to_aa3(seq):
+    """Coerces string of 1- or 3-letter amino acids to 3-letter representation.
+
+    Args:
+        seq (str): An amino acid sequence.
+        
+    Returns:   
+        str: The sequence as one of 3-letter amino acids.    
+
+    Examples:
+        >>> aa_to_aa3("CATSARELAME")
+        'CysAlaThrSerAlaArgGluLeuAlaMetGlu'
+
+        >>> aa_to_aa3("CysAlaThrSerAlaArgGluLeuAlaMetGlu")
+        'CysAlaThrSerAlaArgGluLeuAlaMetGlu'
+
+        >>> aa_to_aa3(None)
+    """
+
+    if seq is None:
+        return None
+    return aa1_to_aa3(seq) if not looks_like_aa3_p(seq) else seq
+
+
+def aa1_to_aa3(seq):
+    """Converts string of 1-letter amino acids to 3-letter amino acids.
+
+    Should only be used if the format of the sequence is known; otherwise use ``aa_to_aa3()``.
+
+    Args:
+        seq (str): An amino acid sequence as 1-letter amino acids.
+        
+    Returns:  
+        str: The sequence as 3-letter amino acids.  
+    
+    Raises:
+        KeyError: If the sequence is not of 1-letter amino acids.
+        
+    Examples:
+        >>> aa1_to_aa3("CATSARELAME")
+        'CysAlaThrSerAlaArgGluLeuAlaMetGlu'
+
+        >>> aa1_to_aa3(None)
+    """
+
+    if seq is None:
+        return None
+    return "".join(aa1_to_aa3_lut[aa1] for aa1 in seq)
+
+
+def aa3_to_aa1(seq):
+    """Converts string of 3-letter amino acids to 1-letter amino acids.
+
+    Should only be used if the format of the sequence is known; otherwise use ``aa_to_aa1()``.
+
+    Args:
+        seq (str): An amino acid sequence as 3-letter amino acids.
+        
+    Returns:   
+        str: The sequence as 1-letter amino acids.   
+    
+    Raises:
+        KeyError: If the sequence is not of 3-letter amino acids.
+    
+    Examples:
+        >>> aa3_to_aa1("CysAlaThrSerAlaArgGluLeuAlaMetGlu")
+        'CATSARELAME'
+
+        >>> aa3_to_aa1(None)
+    """
+
+    if seq is None:
+        return None
+    return "".join(aa3_to_aa1_lut[aa3]
+                   for aa3 in [seq[i:i + 3] for i in range(0, len(seq), 3)])
+
+
+def complement(seq):
+    """Retrieves the complement of a sequence.
+
+    Args:
+        seq (str): A nucleotide sequence.
+        
+    Returns:    
+        str: The complement of the sequence.
+        
+    Examples:
+        >>> complement("ATCG")
+        'TAGC'
+
+        >>> complement(None)
+    """
+
+    if seq is None:
+        return None
+    return seq.translate(complement_transtable)
+
+
+def elide_sequence(s, flank=5, elision="..."):
+    """Trims the middle of the sequence, leaving the right and left flanks.
+
+    Args:
+        s (str): A sequence.
+        flank (int, optional): The length of each flank. Defaults to five.
+        elision (str, optional): The symbol used to represent the part trimmed. Defaults to '...'.
+    
+        Returns:    
+            str: The sequence with the middle replaced by ``elision``.
+        
+    Examples:
+        >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        'ABCDE...VWXYZ'
+
+        >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ", flank=3)
+        'ABC...XYZ'
+
+        >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ", elision="..")
+        'ABCDE..VWXYZ'
+
+        >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ", flank=12)
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+        >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ", flank=12, elision=".")
+        'ABCDEFGHIJKL.OPQRSTUVWXYZ'
+    """
+
+    elided_sequence_len = flank + flank + len(elision)
+    if len(s) <= elided_sequence_len:
+        return s
+    return s[:flank] + elision + s[-flank:]
+
+
+def looks_like_aa3_p(seq):
+    """Indicates whether a string looks like a 3-letter AA string.
+
+    Args:
+        seq (str): A sequence.
+        
+    Returns:
+        bool: Whether the string is of the format of a 3-letter AA string.
+    """
+    return (seq is not None and (len(seq) % 3 == 0) and
+            (len(seq) == 0 or seq[1] in ascii_lowercase))
+
+
+def normalize_sequence(seq):
+    """Converts sequence to normalized representation for hashing.
+
+    Essentially, removes whitespace and asterisks, and uppercases the string.
+
+    Args:
+        seq (str): The sequence to be normalized.
+        
+    Returns:
+        str: The sequence as a string of uppercase letters.    
+    
+    Raises:
+        RuntimeError: If the sequence contains non-alphabetic characters (besides '*').
+        
+    Examples:
+        >>> normalize_sequence("ACGT")
+        'ACGT'
+
+        >>> normalize_sequence("  A C G T * ")
+        'ACGT'
+
+        >>> normalize_sequence("ACGT1")
+        Traceback (most recent call last):
+        ...
+        RuntimeError: Normalized sequence contains non-alphabetic characters
+    """
+
+    nseq = re.sub(r"[\s\*]", "", seq).upper()
+    m = re.search("[^A-Z]", nseq)
+    if m:
+        _logger.debug("Original sequence: " + seq)
+        _logger.debug("Normalized sequence: " + nseq)
+        _logger.debug("First non-[A-Z] at {}".format(m.start()))
+        raise RuntimeError("Normalized sequence contains non-alphabetic characters")
+    return nseq
+
+
+def reverse_complement(seq):
+    """Converts a sequence to its reverse complement.
+
+    Args:
+        seq (str): A nucleotide sequence.
+        
+    Returns:    
+        str: The reverse complement of the sequence.
+    
+    Examples:
+        >>> reverse_complement("ATCG")
+        'CGAT'
+
+        >>> reverse_complement(None)  
+    """
+
+    if seq is None:
+        return None
+    return "".join(reversed(complement(seq)))
+
+
+def replace_t_to_u(seq):
+    """Replaces the T's in a sequence with U's.
+
+    Args:
+        seq (str): A nucleotide sequence.
+        
+    Returns:    
+        str: The sequence with the T's replaced by U's.
+        
+    Examples:
+        >>> replace_t_to_u("ACGT")
+        'ACGU'
+
+        >>> replace_t_to_u(None)
+    """
+
+    if seq is None:
+        return None
+    return seq.replace("T", "U").replace("t", "u")
+
+
+def replace_u_to_t(seq):
+    """Replaces the U's in a sequence with T's.
+
+    Args:
+        seq (str): A nucleotide sequence.
+        
+    Returns: 
+        str: The sequence with the U's replaced by T's.   
+    
+    Examples:
+        >>> replace_u_to_t("ACGU")
+        'ACGT'
+
+        >>> replace_u_to_t(None)
+    """
+
+    if seq is None:
+        return None
+    return seq.replace("U", "T").replace("u", "t")
+
+
+def translate_cds(seq, full_codons=True, ter_symbol="*"):
+    """Translates a DNA or RNA sequence into a single-letter amino acid sequence.
+    
+    Uses the NCBI standard translation table.
+
+    Args:
+        seq (str): A nucleotide sequence.
+        full_codons (bool, optional): If ``True``, forces sequence to have length 
+            that is a multiple of 3 and raises an error otherwise. 
+            If False, ``ter_symbol`` will be added as the last amino acid.
+            This corresponds to biopython's behavior of padding the last codon with ``N``s.
+            Defaults to ``True``.
+        ter_symbol (str, optional): Placeholder for the last amino acid if 
+            sequence length is not divisible by three and ``full_codons`` is False. 
+            Defaults to ``'*'``
+
+    Returns:
+        str: The corresponding single letter amino acid sequence.    
+    
+    Raises:
+        ValueError: If ``full_codons`` and the sequence is not a multiple of three.
+        ValueError: If a codon is undefined in the table.
+        
+    Examples:
+        >>> translate_cds("ATGCGA")
+        'MR'
+
+        >>> translate_cds("AUGCGA")
+        'MR'
+
+        >>> translate_cds(None)
+
+        >>> translate_cds("")
+        ''
+
+        >>> translate_cds("AUGCG")
+        Traceback (most recent call last):
+        ...
+        ValueError: Sequence length must be a multiple of three
+
+        >>> translate_cds("AUGCG", full_codons=False)
+        'M*'
+
+        >>> translate_cds("AUGCGQ")
+        Traceback (most recent call last):
+        ...
+        ValueError: Codon CGQ at position 4..6 is undefined in codon table
+    """
+
+    if seq is None:
+        return None
+
+    if len(seq) == 0:
+        return ""
+
+    if full_codons and len(seq) % 3 != 0:
+        raise ValueError("Sequence length must be a multiple of three")
+
+    seq = replace_u_to_t(seq)
+    seq = seq.upper()
+
+    protein_seq = list()
+    for i in range(0, len(seq) - len(seq) % 3, 3):
+        try:
+            aa = dna_to_aa1_lut[seq[i:i + 3]]
+        except KeyError:
+            raise ValueError("Codon {} at position {}..{} is undefined in codon table".format(
+                seq[i:i + 3], i+1, i+3))
+        protein_seq.append(aa)
+
+    # check for trailing bases and add the ter symbol if required
+    if not full_codons and len(seq) % 3 != 0:
+        protein_seq.append(ter_symbol)
+
+    return ''.join(protein_seq)
+
+
+## <LICENSE>
+## Copyright 2014 Bioutils Contributors
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+## 
+##     http://www.apache.org/licenses/LICENSE-2.0
+## 
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## </LICENSE>
+
