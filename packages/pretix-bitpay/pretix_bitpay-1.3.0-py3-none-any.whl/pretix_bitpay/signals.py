6e7d1bda70a2f9d152c45c8da4c38957d5f78299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:07 +0000

    Add package pretix-bitpay/1.3.0/pretix_bitpay-1.3.0-py3-none-any.whl

---
+++ packages/pretix-bitpay/pretix_bitpay-1.3.0-py3-none-any.whl/pretix_bitpay/signals.py
@@ -0,0 +1,54 @@
+import json
+from collections import OrderedDict
+
+from django import forms
+from django.dispatch import receiver
+from django.template.loader import get_template
+from django.utils.translation import ugettext_lazy as _
+
+from pretix.base.signals import (
+    logentry_display, register_global_settings,
+    register_payment_providers, requiredaction_display, )
+
+
+@receiver(register_payment_providers, dispatch_uid="payment_bitpay")
+def register_payment_provider(sender, **kwargs):
+    from .payment import BitPay
+    return BitPay
+
+
+@receiver(signal=logentry_display, dispatch_uid="bitpay_logentry_display")
+def pretixcontrol_logentry_display(sender, logentry, **kwargs):
+    if logentry.action_type != 'pretix_bitpay.event':
+        return
+
+    data = json.loads(logentry.data)
+    event_type = data.get('event', {}).get('name', '?')
+    return _('BitPay reported an event: {}').format(event_type)
+
+
+@receiver(signal=requiredaction_display, dispatch_uid="bitpay_requiredaction_display")
+def pretixcontrol_action_display(sender, action, request, **kwargs):
+    if not action.action_type.startswith('pretix_bitpay'):
+        return
+
+    data = json.loads(action.data)
+
+    if action.action_type == 'pretix_bitpay.refund':
+        template = get_template('pretix_bitpay/action_refund.html')
+    elif action.action_type == 'pretix_bitpay.overpaid':
+        template = get_template('pretix_bitpay/action_overpaid.html')
+
+    ctx = {'data': data, 'event': sender, 'action': action}
+    return template.render(ctx, request)
+
+
+@receiver(register_global_settings, dispatch_uid='bitpay_global_settings')
+def register_global_settings(sender, **kwargs):
+    return OrderedDict([
+        ('payment_bitpay_pem', forms.CharField(
+            label=_('BitPay client: Private Key'),
+            required=False,
+            widget=forms.Textarea
+        )),
+    ])
