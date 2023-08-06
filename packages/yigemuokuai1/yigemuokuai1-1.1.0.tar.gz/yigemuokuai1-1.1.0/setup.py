5ed3072bf084446c53278f4341609519f2a15061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:45 +0000

    Add package yigemuokuai1/1.1.0/yigemuokuai1-1.1.0.tar.gz

---
+++ packages/yigemuokuai1/yigemuokuai1-1.1.0.tar.gz/yigemuokuai1-1.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+        name       ='yigemuokuai1',
+        version    ='1.1.0',
+        py_modules =['yigemuokuai1'],
+        author     ='hfpython',
+        author_email='759208974@QQ.com',
+        url         ='http://192.168.101.231',
+        description ='a simple list',
+        )
+
+
