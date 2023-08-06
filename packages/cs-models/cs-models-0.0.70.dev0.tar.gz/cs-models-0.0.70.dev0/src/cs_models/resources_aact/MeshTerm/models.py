027c4a0acfc642f8606f52dd22a831f8d3080793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:34 +0000

    Add package cs-models/0.0.70.dev0/cs-models-0.0.70.dev0.tar.gz

---
+++ packages/cs-models/cs-models-0.0.70.dev0.tar.gz/cs-models-0.0.70.dev0/src/cs_models/resources_aact/MeshTerm/models.py
@@ -0,0 +1,31 @@
+from src.cs_models.aact_database import Base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
+
+
+class MeshTermModel(Base):
+    __tablename__ = 'mesh_terms'
+    __table_args__ = (
+        {'schema': 'ctgov'},
+    )
+
+    id = Column(Integer, primary_key=True)
+    qualifier = Column(String)
+    tree_number = Column(String)
+    description = Column(String)
+    mesh_term = Column(String)
+    downcase_mesh_term = Column(String)
+
+    def __repr__(self):
+        return "<MeshTerm(id='{}', qualifier='{}', " \
+               "tree_number='{}', description='{}', mesh_term='{}'>"\
+            .format(
+                self.id,
+                self.qualifier,
+                self.tree_number,
+                self.description,
+                self.mesh_term,
+            )
