6328ab7fc20f6e63c8f4f1dbc7ff14863cec567f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:57 +0000

    Add package getnet-py/1.0.0/getnet_py-1.0.0-py2.py3-none-any.whl

---
+++ packages/getnet-py/getnet_py-1.0.0-py2.py3-none-any.whl/tests/test_service_token.py
@@ -0,0 +1,16 @@
+import unittest
+
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+from getnet.services import TokenCardService
+
+
+@mock.patch("getnet.API")
+class ServiceTokenTest(unittest.TestCase):
+    def test_invalid_card_number(self, APIClass):
+        token = TokenCardService(APIClass)
+        with self.assertRaises(AttributeError):
+            token.create("123", "312")
