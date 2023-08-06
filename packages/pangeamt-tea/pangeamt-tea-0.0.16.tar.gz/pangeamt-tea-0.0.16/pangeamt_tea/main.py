011b47b54a435858934d19629ff7e59ab5570e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:19 +0000

    Add package pangeamt-tea/0.0.16/pangeamt-tea-0.0.16.tar.gz

---
+++ packages/pangeamt-tea/pangeamt-tea-0.0.16.tar.gz/pangeamt-tea-0.0.16/pangeamt_tea/main.py
@@ -0,0 +1,47 @@
+import asyncio
+import argparse
+import os
+
+
+
+def main():
+    asyncio.run(run())
+
+
+async def run():
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(description='Tea', dest="cmd")
+
+    parser_init = subparser.add_parser("init")
+    parser_init.add_argument('-parent_dir', type=str, default=None)
+    parser_init.add_argument('-customer', type=str, default=None)
+    parser_init.add_argument('-src_lang', type=str, default=None)
+    parser_init.add_argument('-tgt_lang', type=str, default=None)
+    parser_init.add_argument('-flavor', type=str, default=None)
+    parser_init.add_argument('-version', type=str, default=None)
+
+    parser_clean = subparser.add_parser("clean")
+    args = parser.parse_args()
+    print(args)
+    print(os.getcwd())
+
+
+
+async def task_init():
+    customer = input("Customer: ")
+    src_lang = input("Source language: ")
+    tgt_lang = input("Target language: ")
+    flavor = input("Flavor ")
+
+    project_dir = customer + '_' + src_lang + '_' + tgt_lang
+    if flavor:
+        project_dir += '_' + flavor
+    if os.path.isdir(project_dir):
+        raise ValueError(f'Project {project_dir} already exist\'s')
+
+    os.mkdir(project_dir)
+    print(f"run `cd {project_dir}`")
+
+
+if __name__ == '__main__':
+    main()
