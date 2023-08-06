7c0caff7a20c988cce03d9ef893871816baa2ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:29 +0000

    Add package crnsimulator/0.7/crnsimulator-0.7.tar.gz

---
+++ packages/crnsimulator/crnsimulator-0.7.tar.gz/crnsimulator-0.7/README.md
@@ -0,0 +1,103 @@
+# crnsimulator 
+
+Simulate chemical recation networks (CRNs) using ordinary differential
+equations (ODEs).
+
+## Examples
+### Using the `crnsimulator` executable:
+
+Create a test file with your CRN:
+
+File: ozzy.crn
+```
+# Oscillator Test
+A + B -> B + B [k = 0.2]
+B + C -> C + C [k = 0.3]
+C + A -> A + A [k = 1]
+```
+
+And pipe it into the crnsimulator:
+```sh
+~$ crnsimulator -o ozzy < ozzy.crn
+```
+This writes the ODE system to an executable python script: `ozzy.py`
+
+Check the command line parameters of ozzy.py. You have to set initial species
+concentrations, and choose an output-format, e.g.:
+```sh
+~$ python ./ozzy.py --p0 A=0.1 B=1e-2 C=1e-3 --t8 10000 --pyplot ozzy.pdf
+```
+This example plots a simulation on a linear-time scale (0 - 10000) to the file `ozzy.pdf` .
+
+### Tips and Tricks:
+
+You can pass the command line options for ozzy.py directly to `crnsimulator`.
+This will automatically simulate your ODE system. Use --force to overwrite an
+existing `ozzy.py` script.
+```sh
+~$ crnsimulator --p0 A=0.1 B=1e-2 C=1e-3 --t8 10000 -o ozzy --pyplot ozzy.pdf < ozzy.crn
+```
+
+You can specify the CRN in a single line:
+
+```sh
+~$ echo "A+B->2B [k=0.2]; B+C->2C [k=0.4]; C+A->2A" | crnsimulator --p0 A=0.1 B=1e-2 C=1e-3 --t8 10000 -o ozzy --pyplot ozzy.pdf
+```
+
+You can specify default initial concentrations of species:
+
+```sh
+~$ echo "A @i 0.1; B @i 1e-2; A+B->2B [k=0.2]; B+C->2C [k=0.4]; C+A->2A" | crnsimulator --p0 C=1e-3 --t8 10000 -o ozzy --pyplot ozzy.pdf
+```
+
+If you can set which species appear in the legend using --pyplot-lables. If you 
+are writing a new executable (you may need --force), then you can also control the order:
+
+```sh
+~$ echo "A @i 0.1; B @i 1e-2; A+B->2B [k=0.2]; B+C->2C [k=0.4]; C+A->2A" | crnsimulator --p0 C=1e-3 --t8 10000 -o ozzy --pyplot ozzy.pdf --force --pyplot-labels C B
+```
+
+
+### Using the `crnsimulator` library:
+
+The easiest way to get started is by looking at the crnsimulator script itself.
+However, here is a small example using the above oscillating CRN.
+
+```py
+>>> from crnsimulator import ReactionGraph
+>>> crn  = [[['A', 'B'],['B','B'],0.2],
+            [['B', 'C'],['C','C'],0.8],
+            [['C', 'A'],['A','A'],0.9]]
+>>> RG = ReactionGraph(crn)
+>>> filename, odename = RG.write_ODE_lib(filename='ozzy.py')
+>>> print('Wrote ODE system file:', filename)
+Wrote ODE system file: ozzy.py
+```
+
+Then go ahead and execute `ozzy.py`
+```sh
+~$ python ./ozzy.py --p0 1=1e-6 2=2e-6 3=5e-6 --t8 1e8 --pyplot ozzy.pdf
+```
+
+... or load it as python library.
+
+```py
+>>> from crnsimulator import get_integrator
+>>> integrate = get_integrator(odename, filename)
+>>> integrate(args) # args = <argparse.ArgumentParser()>
+```
+
+
+## Installation
+```sh
+~$ python setup.py install
+```
+
+### local installation
+```sh
+~$ python setup.py install --user
+```
+  
+## Version
+0.7
+
---
+++ packages/crnsimulator/crnsimulator-0.7.tar.gz/crnsimulator-0.7/crnsimulator/solver.py
@@ -0,0 +1,135 @@
+#
+# CRN-to-MultiDiGraph-to-ODE translation utilities.
+#
+# Written by Stefan Badelt (badelt@caltech.edu).
+#
+# Use at your own risk.
+#
+
+import types
+import importlib.machinery
+
+import crnsimulator.odelib_template
+
+def get_integrator(odename, filename):
+    """Workaround to avoid deprecation warnings for the imp module.
+    """
+    loader = importlib.machinery.SourceFileLoader(odename, filename)
+    mod = types.ModuleType(loader.name)
+    loader.exec_module(mod)
+    return getattr(mod, 'integrate')
+
+def writeODElib(svars, odeM, jacobian=None, rdict=None, concvect=None,
+                odename='odesystem', filename='./odesystem', template=None):
+    """ Write an ODE system into an executable python script.
+
+    Args:
+      svars <list[str]>: Sorted list of variables. The sorting defines the order
+        for specifying concentrations.
+      odeM <sympy.Matrix()>: A matrix that contains the ODE system.
+      jacobian <optional: sympy.Matrix()> : The jacobi Matrix corresponding to
+        odeM.
+      rdict <optional: dict()>: If your odeM contains rates in form of variable
+        names, then you need to supply this dictionary mapping names to float values.
+      concvect <optional: list(): Specify default initial species concentrations
+        in the order defined by svars.
+      odename <optional: str>: Name of your ODE function (no special characters!)
+      filename <optional: str>: Specify the name of the ODE library.
+      template <optional: str>: Specify an alternative template library file.
+
+    Returns:
+      filename<str>, odename<str>
+
+    """
+    if not template:
+        template = crnsimulator.odelib_template.__file__[:]
+        if template[-1] == 'c':
+            template = template[:-1]
+
+    svars = list(map(str, svars))
+
+    odetemp = ''
+    with open(template, 'r') as tfile:
+        odetemp = tfile.read()
+
+    # REPLACE NAMES
+    odetemp = odetemp.replace("#<&>ODENAME<&>#", odename)
+    odetemp = odetemp.replace("#<&>FILENAME<&>#", filename)
+
+    # DEFAULT RATES
+    ratestring = ',\n'.join(
+        "  '{}' : {}".format(k, rdict[k]) for k in sorted(rdict.keys()))
+    odetemp = odetemp.replace("#<&>RATES<&>#", ratestring)
+
+    # ODEINT FUNCTION
+    functionstring = "def {}(p0, t0, r):\n".format(odename)
+    # Initialize arguments
+    functionstring += "    {} = p0\n".format(', '.join(svars))
+    functionstring += "    if not r : r = rates\n\n"
+    for k in sorted(rdict.keys()):
+        functionstring += "    {} = r['{}']\n".format(k, k)
+    functionstring += "\n"
+    # Write the ODEs
+    for i in range(len(svars)):
+        functionstring += "    d{}dt = {}\n".format(svars[i], odeM[i])
+    # return
+    if len(svars) == 1:
+        functionstring += "    return np.array({})".format(
+            ', '.join(['d' + x + 'dt' for x in svars]))
+    else:
+        functionstring += "    return np.array([{}])".format(
+            ', '.join(['d' + x + 'dt' for x in svars]))
+    odetemp = odetemp.replace("#<&>ODECALL<&>#", functionstring)
+
+    if jacobian:
+        # JACOBIAN FUNCTION
+        jacobianstring = "def {}(p0, t0, r):\n".format('jacobian')
+        # Initialize arguments
+        jacobianstring += "    {} = p0\n".format(', '.join(svars))
+        jacobianstring += "    if not r : r = rates\n\n"
+        for k in sorted(rdict.keys()):
+            jacobianstring += "    {} = r['{}']\n".format(k, k)
+        jacobianstring += "\n"
+
+        # Write the jacobian
+        jacobianstring += "    J = [[[] for i in range(len(p0))] " + \
+            "for j in range(len(p0))]\n"
+
+        vl = len(svars)
+        i, j = 0, 0
+        for row in jacobian:
+            jacobianstring += "    J[{}][{}] = {}\n".format(i, j, row)
+            if j < vl - 1:
+                j += 1
+            else:
+                i += 1
+                j = 0
+
+        if len(svars) == 1:
+            jacobianstring += "    J = J[0]\n"
+
+        # return
+        jacobianstring += "    return J"
+        odetemp = odetemp.replace("#<&>JACOBIAN<&>#", jacobianstring)
+        odetemp = odetemp.replace("#<&>JCALL<&>#", 'Dfun = jacobian')
+
+    # SORTED VARIABLE NAMES in integrate()
+    svarstring = 'svars = ' + '[{}]'.format(
+        ', '.join([str('"' + x + '"') for x in svars]))
+    odetemp = odetemp.replace("#<&>SORTEDVARS<&>#", svarstring)
+
+    # Default concentrations in integrate()
+    concstring = ''
+
+    if concvect:
+        for e, c in enumerate(concvect):
+            if c:
+                concstring += "p0[{}] = {}\n    ".format(e, c)
+    odetemp = odetemp.replace("#<&>DEFAULTCONCENTRATIONS<&>#", concstring)
+
+    if filename[-3:] != '.py':
+        filename += '.py'
+    with open(filename, 'w') as ofile:
+        ofile.write(odetemp)
+
+    return filename, odename
