a362a84512675d45a3b843294170e244f81a3e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:54 +0000

    Add package selen-kaa/0.0.1/selen-kaa-0.0.1.tar.gz

---
+++ packages/selen-kaa/selen-kaa-0.0.1.tar.gz/selen-kaa-0.0.1/README.md
@@ -0,0 +1,1 @@
+A lightweight wrapper of Selenium Python binding.
---
+++ packages/selen-kaa/selen-kaa-0.0.1.tar.gz/selen-kaa-0.0.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    """Reads the installation requirements from requirements.pip"""
+    with open("requirements.pip") as reqfile:
+        return [line for line in reqfile.read().split("\n") if not line.startswith(('#', '-'))]
+
+
+def get_test_requirements():
+    """Reads the installation requirements for tests"""
+    with open("test-requirements.pip") as test_reqfile:
+        return [line for line in test_reqfile.read().split("\n") if not line.startswith(('#', '-'))]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="selen-kaa",
+    version="0.0.1",
+    author="Viktor Grygorchuk",
+    author_email="vvgrygorchuk@gmail.com",
+    keywords=['Selenium', 'Test Automation'],
+    description="A lightweight wrapper around Selenium python repo.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VicGrygorchyk/selen_kaa.git",
+    download_url='https://github.com/VicGrygorchyk/selen_kaa/archive/0.0.1.tar.gz',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "test*"]),
+    install_requires=get_requirements(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    test_suite="tests",
+    tests_require=get_test_requirements(),
+    python_requires='>=3.6',
+)
