35ead73bf6b2a01a314d966204d04705765f49b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:28 +0000

    Add package aiven-client/2.3.1/aiven-client-2.3.1.tar.gz

---
+++ packages/aiven-client/aiven-client-2.3.1.tar.gz/aiven-client-2.3.1/tests/test_cli.py
@@ -0,0 +1,32 @@
+# Copyright 2015, Aiven, https://aiven.io/
+#
+# This file is under the Apache License, Version 2.0.
+# See the file `LICENSE` for details.
+
+# pylint: disable=no-member
+from aiven.client.cli import AivenCLI
+import pytest
+
+pytestmark = [pytest.mark.unittest, pytest.mark.all]
+
+
+def test_cli():
+    with pytest.raises(SystemExit) as excinfo:
+        AivenCLI().run(args=["--help"])
+    assert excinfo.value.code == 0
+
+
+def test_cloud_list():
+    AivenCLI().run(args=["cloud", "list"])
+
+
+def test_service_plans():
+    AivenCLI().run(args=["service", "plans"])
+
+
+def test_service_types_v():
+    AivenCLI().run(args=["service", "types", "-v"])
+
+
+def test_help():
+    AivenCLI().run(args=["help"])
