813874caba9d8c182ce6538daccdc22bb85d859c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:49 +0000

    Add package papyru/0.0.1.dev32/papyru-0.0.1.dev32.tar.gz

---
+++ packages/papyru/papyru-0.0.1.dev32.tar.gz/papyru-0.0.1.dev32/papyru/resource.py
@@ -0,0 +1,39 @@
+import traceback
+from logging import getLogger
+
+from django.core.exceptions import ObjectDoesNotExist
+
+from .problem import Problem
+
+# defined in RFC 7231
+HTTP_METHODS = set(['get', 'head', 'post', 'put', 'delete', 'connect',
+                    'options', 'trace'])
+
+_logger = getLogger(__name__)
+
+
+class Resource:
+    def __call__(self, request, *args):
+        try:
+            method = getattr(self.__class__, request.method.lower())
+        except AttributeError:
+            allowed = set(dir(self.__class__)) & HTTP_METHODS
+            return Problem.method_not_allowed(allowed).to_response()
+
+        try:
+            return method(self, request, *args)
+
+        except Problem as problem:
+            if problem.should_log:
+                _logger.error('[%s] %s: %s' % (int(problem.status),
+                                               problem.title,
+                                               problem.detail))
+            return problem.to_response()
+
+        except ObjectDoesNotExist:
+            return Problem.not_found().to_response()
+
+        except Exception as exc:
+            _logger.error(traceback.format_exc())
+            return Problem.internal_error(
+                'unexpected error: %s' % exc).to_response()
---
+++ packages/papyru/papyru-0.0.1.dev32.tar.gz/papyru-0.0.1.dev32/papyru/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from tests.test_middleware import *  # noqa
+from tests.test_xml_helper import *  # noqa
+
+unittest.main()
