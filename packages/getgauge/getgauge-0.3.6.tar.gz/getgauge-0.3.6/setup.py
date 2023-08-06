93120c1e8159c9728eeb60fec99b830b24097d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:54 +0000

    Add package getgauge/0.3.6/getgauge-0.3.6.tar.gz

---
+++ packages/getgauge/getgauge-0.3.6.tar.gz/getgauge-0.3.6/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='getgauge',
+    packages=['getgauge', 'getgauge/messages'],
+    version='0.3.6',
+    description='Enables Python support for Gauge',
+    author='Gauge Team',
+    author_email='getgauge@outlook.com',
+    url='https://github.com/getgauge/gauge-python',
+    download_url='https://github.com/getgauge/gauge-python/archive/v0.2.4.zip',
+    keywords=['testing', 'gauge', 'gauge-python', 'getgauge', 'automation'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=['redBaron', 'parso', 'ptvsd==4.1.4', 'grpcio==1.15.0', 'protobuf>=3.5.2', 'six'],
+    extras_require={
+		':python_version == "2.7"': ['futures']
+	},
+)
