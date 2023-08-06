3945efb76edc5e3a15e0f5201862ddaa4437d005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:16 +0000

    Add package configreset/1.0.3/configreset-1.0.3.tar.gz

---
+++ packages/configreset/configreset-1.0.3.tar.gz/configreset-1.0.3/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from os.path import dirname, join
+
+from setuptools import setup, find_packages
+
+project_dir = dirname(__file__)
+
+
+
+setup(
+    name="configreset",
+    version='1.0.3',
+    description="configreset",
+    author="lvjiyong",
+    url="https://github.com/lvjiyong/configreset",
+    license="GPL",
+    include_package_data=True,
+    packages=find_packages(exclude=()),
+    long_description='configreset',
+    maintainer='lvjiyong',
+    platforms=["any"],
+    maintainer_email='lvjiyong@gmail.com',
+    install_requires=['six'],
+)
