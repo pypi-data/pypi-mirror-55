60c5c155c9a9e13f388147f033719b484117a54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:35 +0000

    Add package treeart/0.0.1/treeart-0.0.1.tar.gz

---
+++ packages/treeart/treeart-0.0.1.tar.gz/treeart-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name="treeart",
+    packages=["treeart"],
+    version="0.0.1",
+    license="MIT",
+    description="Draw ASCII trees easily",
+    url="https://github.com/taliesinb/treeart",
+    download_url="https://github.com/taliesinb/treeart/archive/v0.0.1.tar.gz",
+    author="Taliesin Beynon",
+    author_email="contact@taliesin.ai",
+    keywords=["ascii", "tree", "art", "binary"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
