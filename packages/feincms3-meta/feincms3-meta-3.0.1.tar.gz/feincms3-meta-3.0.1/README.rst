0e7ac0f2d13ea49501074f19a6f38723b19d7528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:49 +0000

    Add package feincms3-meta/3.0.1/feincms3-meta-3.0.1.tar.gz

---
+++ packages/feincms3-meta/feincms3-meta-3.0.1.tar.gz/feincms3-meta-3.0.1/README.rst
@@ -0,0 +1,19 @@
+=============
+feincms3-meta
+=============
+
+.. image:: https://travis-ci.org/matthiask/feincms3-meta.svg?branch=master
+   :target: https://travis-ci.org/matthiask/feincms3-meta
+
+Helpers and mixins for making meta and `open graph <http://ogp.me/>`__
+tags less annoying.
+
+Documentation is available as a part of `feincms3
+<https://feincms3.readthedocs.io/>`__'s documentation.
+
+The guide is here: `Meta and OpenGraph tags
+<https://feincms3.readthedocs.io/en/latest/guides/meta-opengraph-tags.html>`__
+The guide's title
+
+Despite its name and the fact that it is documented as a part of
+feincms3, feincms3-meta can also be used standalone.
---
+++ packages/feincms3-meta/feincms3-meta-3.0.1.tar.gz/feincms3-meta-3.0.1/feincms3_meta/models.py
@@ -0,0 +1,92 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from imagefield.fields import ImageField
+
+
+class MetaMixin(models.Model):
+    meta_title = models.CharField(
+        _("title"),
+        max_length=200,
+        blank=True,
+        help_text=_("Used for Open Graph and other meta tags."),
+    )
+    meta_description = models.TextField(
+        _("description"),
+        blank=True,
+        help_text=_("Override the description for this page."),
+    )
+    meta_image = ImageField(
+        _("image"),
+        blank=True,
+        auto_add_fields=True,
+        upload_to="meta/%Y/%m",
+        help_text=_("Set the Open Graph image."),
+        formats={"recommended": ("default", ("crop", (1200, 630)))},
+    )
+    meta_canonical = models.URLField(
+        _("canonical URL"),
+        blank=True,
+        help_text=_("If you need this you probably know."),
+    )
+    meta_author = models.CharField(
+        _("author"),
+        max_length=200,
+        blank=True,
+        help_text=_("Override the author meta tag."),
+    )
+    meta_robots = models.CharField(
+        _("robots"),
+        max_length=200,
+        blank=True,
+        help_text=_("Override the robots meta tag."),
+    )
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def admin_fieldset(cls, **kwargs):
+        cfg = {
+            "fields": (
+                "meta_title",
+                "meta_description",
+                "meta_image",
+                "meta_image_ppoi",
+                "meta_canonical",
+                "meta_author",
+                "meta_robots",
+            ),
+            "classes": ("tabbed",),
+        }
+        cfg.update(kwargs)
+        return (_("Meta tags"), cfg)
+
+    def meta_dict(self):
+        ctx = {
+            "title": self.meta_title or getattr(self, "title", ""),
+            "description": self.meta_description,
+            "canonical": self.meta_canonical,
+            # Override URL if canonical is set to a non-empty value (the empty
+            # string will be skipped when merging this dictionary)
+            "url": self.meta_canonical,
+            "author": self.meta_author,
+            "robots": self.meta_robots,
+        }
+        ctx.update(self.meta_images_dict())
+        return ctx
+
+    def meta_images_dict(self):
+        if self.meta_image:
+            return {
+                "image": str(self.meta_image.recommended),
+                "image:width": 1200,
+                "image:height": 630,
+            }
+
+        elif getattr(self, "image", None):
+            return {"image": self.image.url}
+
+        return {"image": ""}
