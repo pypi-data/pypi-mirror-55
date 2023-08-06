5c4a64980ab4067b3367b5c3bc23dd3086fd55eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:13 +0000

    Add package ural/0.13.0/ural-0.13.0-py3-none-any.whl

---
+++ packages/ural/ural-0.13.0-py3-none-any.whl/ural/lru/__init__.py
@@ -0,0 +1,9 @@
+# =============================================================================
+# Ural LRU Module
+# =============================================================================
+#
+# Module gathering LRU-related functions.
+#
+from ural.lru.serialization import serialize_lru, unserialize_lru
+from ural.lru.stems import lru_stems, normalized_lru_stems
+from ural.lru.trie import NormalizedLRUTrie
