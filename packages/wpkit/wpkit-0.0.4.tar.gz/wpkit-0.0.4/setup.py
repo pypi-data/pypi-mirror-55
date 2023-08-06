823ee5a2771043e4d8466f2d367810bcfc7fabb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:43 +0000

    Add package wpkit/0.0.4/wpkit-0.0.4.tar.gz

---
+++ packages/wpkit/wpkit-0.0.4.tar.gz/wpkit-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wpkit", # Replace with your own username
+    version="0.0.4",
+    author="WangPei",
+    author_email="1535376447@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    package_dir={'wpkit':'wpkit'},
+    package_data={'wpkit':['data/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
