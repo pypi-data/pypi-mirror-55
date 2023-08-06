ba2a25bd0f0893983dc6d9303be4fd4e6ef5171b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:22 +0000

    Add package bravado-types/0.1.0/bravado-types-0.1.0.tar.gz

---
+++ packages/bravado-types/bravado-types-0.1.0.tar.gz/bravado-types-0.1.0/integration_test/tests/test_runtime.py
@@ -0,0 +1,53 @@
+import random
+import typing
+
+import pytest
+from bravado.exception import HTTPError
+
+import example
+from example import petstore
+
+
+@pytest.fixture(scope='module')
+def client():
+    return example.create_client()
+
+
+def test_model_init(client):
+    Pet = client.get_model('Pet')
+    pet = Pet(name='petName', photoUrls=[])
+    assert pet.name == 'petName'
+
+    with pytest.raises(RuntimeError):
+        petstore.Pet(name='petName', photoUrls=[])
+
+
+def test_model_isinstance(client):
+    Pet = client.get_model('Pet')
+    pet = Pet(name='petName', photoUrls=[])
+    assert isinstance(pet, Pet)
+
+    with pytest.raises(TypeError):
+        isinstance(pet, petstore.Pet)
+
+
+def test_placeholder_union():
+    typing.Optional[petstore.Pet]
+    typing.Union[petstore.User, petstore.Pet]
+
+
+def test_api_calls(client):
+    pet_id = random.randrange(2**32)
+
+    try:
+        example.create_pet(client, pet_id)
+    except HTTPError as e:
+        # XXX: petstore.swagger.io server returns an undocumented 200 response
+        # for the addPet operation, which causes a bravado HTTPError
+        if e.status_code == 200:
+            pass
+
+    pet = example.get_pet_by_id(client, pet_id)
+    assert pet is not None
+    assert isinstance(pet, client.get_model('Pet'))
+    assert pet.name is not None
