4998db65696816a5f95ddc5e8f5a398c47da99dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:25 +0000

    Add package commah/0.3/commah-0.3-py2.py3-none-any.whl

---
+++ packages/commah/commah-0.3-py2.py3-none-any.whl/commah-0.3.dist-info/AUTHORS.rst
@@ -0,0 +1,14 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Alan Duffy <mail@alanrduffy.com>
+* Camila Correa <correac@student.unimelb.edu.au>
+
+Contributors
+------------
+
+* Ellert vd Velden aka @1313e has written the entire package into python 3 (and improved 2.7 functionality)
---
+++ packages/commah/commah-0.3-py2.py3-none-any.whl/commah/tests/test_commah.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_commah
+----------------------------------
+
+Tests for `commah` module.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import commah
+
+
+class TestCommah(object):
+    def test_cosmologies(self):
+        cosmolist = ['WMAP1', 'WMAP3', 'WMAP5',
+                     'WMAP7', 'WMAP9',
+                     'Planck13', 'Planck15']
+        conclist = [8.84952005507872,
+                    6.570929526400634,
+                    7.663081978810533,
+                    7.906741464320479,
+                    8.883912548303279,
+                    9.250262507139139,
+                    9.044999285760362]
+        for ival, cosmo in enumerate(cosmolist):
+            output = commah.run(cosmo, Mi=[1e12], verbose=True)
+            assert(np.allclose(output['c'].flatten()[0],
+                   conclist[ival], rtol=1e-3))
+
+    def test_evolution(self):
+        zlist = np.array([0, 1, 2])
+        conclist = np.array([7.66308, 5.70009, 4.55295])
+        output = commah.run('WMAP5', zi=[0], Mi=[1e12], z=zlist)
+        assert(np.allclose(output['c'].flatten(), conclist, rtol=1e-3))
+
+    def test_startingz(self):
+        zlist = np.array([0, 1, 2])
+        conclist = np.array([4.55295, 4.43175, 4.26342])
+        output = commah.run('WMAP5', zi=zlist, Mi=[1e12], z=2)
+        assert(np.allclose(output['c'].flatten(), conclist, rtol=1e-3))
