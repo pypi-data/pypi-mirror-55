e2827f7aae588699a86a0b5e47d671e59bc4f271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:04 +0000

    Add package autofunccli/1.0.1/autofunccli-1.0.1.tar.gz

---
+++ packages/autofunccli/autofunccli-1.0.1.tar.gz/autofunccli-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autofunccli", # Replace with your own username,
+    packages = ['autofunccli'],
+    version="1.0.1",
+    author="Nicolas Cuadros",
+    author_email="cuadros.nicolas@orange.fr",
+    description="Transform your function into a command line utility.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CuadrosNicolas/Python-Command-Function",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
