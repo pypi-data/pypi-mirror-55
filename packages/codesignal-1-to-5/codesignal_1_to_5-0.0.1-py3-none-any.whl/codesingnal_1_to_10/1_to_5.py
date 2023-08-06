3ed7d617d3ce5c28b7c6cc0c1da65d24739be36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:17 +0000

    Add package codesignal-1-to-5/0.0.1/codesignal_1_to_5-0.0.1-py3-none-any.whl

---
+++ packages/codesignal-1-to-5/codesignal_1_to_5-0.0.1-py3-none-any.whl/codesingnal_1_to_10/1_to_5.py
@@ -0,0 +1,31 @@
+def isLucky(n):
+    m = str(n)
+    s = 0
+    t = 0
+
+    for i in range(int(len(m) / 2)):
+        s = s + int(m[i])
+        t = t + int(m[i + int(len(m) / 2)])
+
+    return s == t
+
+
+def sortByHeight(a):
+    l = sorted([i for i in a if i > 0])
+    for n, i in enumerate(a):
+        if i == -1:
+            l.insert(n, i)
+    return l
+
+
+def reverseInParentheses(gf):
+    while ')' in gf:
+        A = gf.find(')')
+        b = gf[:A].rfind('(')
+        c = gf[b + 1:A][::-1]
+        gf = gf[:b] + c + gf[A + 1:]
+    return gf
+
+
+def alternatingSums(a):
+    return [sum(a[::2]), sum(a[1::2])]
