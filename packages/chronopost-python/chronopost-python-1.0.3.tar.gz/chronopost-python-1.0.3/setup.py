50894fdd6ea99d976a26f4d3aec74d0785170eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:49 +0000

    Add package chronopost-python/1.0.3/chronopost-python-1.0.3.tar.gz

---
+++ packages/chronopost-python/chronopost-python-1.0.3.tar.gz/chronopost-python-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'chronopost-python',         # How you named your package folder (MyLib)
+  packages = ['chronopost'],   # Chose the same as "name"
+  version = '1.0.3',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Chronopost connector',   # Give a short description about your library
+  author = 'Andre Rosado',                   # Type in your name
+  author_email = 'rosado_andre@live.com.pt',      # Type in your E-Mail
+  url = 'https://github.com/BetacodeTech',   # Provide either the link to your github or to your website
+  keywords = ['Chronopost', 'Retail', 'Logistics'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+      ],
+  classifiers=[
+  ],
+)
