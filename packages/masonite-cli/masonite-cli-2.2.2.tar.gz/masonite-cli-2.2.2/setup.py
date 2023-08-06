44faa6e7b5382f4609f9761f0494a2d024c9042e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:46 +0000

    Add package masonite-cli/2.2.2/masonite-cli-2.2.2.tar.gz

---
+++ packages/masonite-cli/masonite-cli-2.2.2.tar.gz/masonite-cli-2.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="masonite-cli",
+    version='2.2.2',
+    packages=[
+        'masonite_cli',
+        'masonite_cli.commands',
+        'masonite_cli.helpers',
+    ],
+    py_modules=['masonite_cli'],
+    install_requires=[
+        'cleo>=0.6,<=0.6.99',
+        'cryptography',
+        'requests',
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'craft = masonite_cli.application:application.run',
+        ],
+    },
+)
