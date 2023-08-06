929ad9eacf64f7f0f2a42187c4a2cd2514af6a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:56 +0000

    Add package mhcflurry/1.4.3/mhcflurry-1.4.3-py3-none-any.whl

---
+++ packages/mhcflurry/mhcflurry-1.4.3-py3-none-any.whl/mhcflurry/__init__.py
@@ -0,0 +1,13 @@
+"""
+Class I MHC ligand prediction package
+"""
+
+from .class1_affinity_predictor import Class1AffinityPredictor
+from .class1_neural_network import Class1NeuralNetwork
+from .version import __version__
+
+__all__ = [
+    "__version__",
+    "Class1AffinityPredictor",
+    "Class1NeuralNetwork",
+]
