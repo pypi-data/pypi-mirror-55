9f8f3c8c4dfe16e197769fc29ecec081e583d0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:17 +0000

    Add package pyinterp/0.0.7/pyinterp-0.0.7.tar.gz

---
+++ packages/pyinterp/pyinterp-0.0.7.tar.gz/pyinterp-0.0.7/src/pyinterp/version.py
@@ -0,0 +1,14 @@
+"""
+Get software version information
+================================
+"""
+
+
+def release() -> str:
+    """Returns the software version number"""
+    return "0.0.7"
+
+
+def date() -> str:
+    """Returns the creation date of this release"""
+    return "13 November 2019"
