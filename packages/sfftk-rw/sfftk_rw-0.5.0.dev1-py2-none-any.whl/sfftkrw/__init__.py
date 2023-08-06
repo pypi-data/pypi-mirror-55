9459809cddf97c68ea5712fe169108903fb60e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:10 +0000

    Add package sfftk-rw/0.5.0.dev1/sfftk_rw-0.5.0.dev1-py2-none-any.whl

---
+++ packages/sfftk-rw/sfftk_rw-0.5.0.dev1-py2-none-any.whl/sfftkrw/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+VALID_EXTENSIONS = ['sff', 'hff', 'json']
+
+SFFTKRW_VERSION = 'v0.5.0.dev1'
+
+SFFTKRW_ENTRY_POINT = 'sff-rw'
