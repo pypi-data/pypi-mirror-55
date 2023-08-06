d232c82ed51fedc80947df4fc735719e969e2b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:16 +0000

    Add package spexxy/2.3/spexxy-2.3.tar.gz

---
+++ packages/spexxy/spexxy-2.3.tar.gz/spexxy-2.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+from spexxy.version import VERSION
+
+setup(
+    name='spexxy',
+    version=VERSION,
+    description='spexxy spectrum fitting package',
+    author='Tim-Oliver Husser',
+    author_email='thusser@uni-goettingen.de',
+    packages=find_packages(include=['spexxy', 'spexxy.*']),
+    scripts=['bin/spexxy', 'bin/spexxytools'],
+    include_package_data=True,
+    install_requires=[
+        'scipy',
+        'numpy',
+        'astropy',
+        'pandas',
+        'lmfit',
+        'pyyaml'
+    ]
+)
