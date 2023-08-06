519986cceada87fa393487b8653aa03293620e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:14 +0000

    Add package rp/0.1.241/rp-0.1.241.tar.gz

---
+++ packages/rp/rp-0.1.241.tar.gz/rp-0.1.241/rp/trans.py
@@ -0,0 +1,10 @@
+#Print a trans flag
+from rp import *
+colors    =pip_import('colors','ansicolors')
+color     =lambda n:(colors.color(' '*100,bg=n)+'\n')*7
+blue      =color(75) 
+white     =color(15)
+pink      =color(211)
+trans_flag=blue+pink+white+pink+blue
+print(trans_flag)
+assert False,'Not gay enough...try importing me again.'
