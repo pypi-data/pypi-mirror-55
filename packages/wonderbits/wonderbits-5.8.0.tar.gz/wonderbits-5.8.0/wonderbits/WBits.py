0acb8c206847c3dcd39b5fc7f04ab06034ad1c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:46 +0000

    Add package wonderbits/5.8.0/wonderbits-5.8.0.tar.gz

---
+++ packages/wonderbits/wonderbits-5.8.0.tar.gz/wonderbits-5.8.0/wonderbits/WBits.py
@@ -0,0 +1,67 @@
+from .MyCore import MyCore
+from .MyUtil import MyUtil
+import time
+import threading
+import types
+
+_lock = threading.Lock()
+
+
+class WBits(object):
+    '''
+    wonderbits parent class
+    '''
+    # singleton flag
+    __init_flag = False
+
+    _wb_serial = None
+
+    def __init__(self):
+        if not WBits.__init_flag:
+            MyUtil.wb_log('wonderbits初始化', '\r\n')
+            WBits.__init_flag = True
+            WBits._wb_serial = MyCore()
+
+    def _set_command(self, command):
+        '''
+        note: send command
+        param: command
+        return: return 'done' if send command successfully, else return error_msg
+        '''
+        _lock.acquire()
+        self._wb_serial.write_command(command)
+        self._timeout_get_command()
+        MyUtil.wb_log(MyCore.return_value, '\r\n')
+        _lock.release()
+        # return MyCore.return_value
+
+    def _get_command(self, command):
+        '''
+        note get command
+        params: command
+        return: return value if send command successfully, else return error_msg
+        '''
+        _lock.acquire()
+        cmd = 'print({})'.format(command)
+        self._wb_serial.write_command(cmd)
+        self._timeout_get_command()
+        MyUtil.wb_log(MyCore.return_value, '\r\n')
+        _lock.release()
+        return MyCore.return_value
+
+    @staticmethod
+    def _timeout_get_command(timeout=3):
+        '''
+        max time when execute command,
+        if exceed max time, ignore current command.
+        '''
+        MyCore.return_value = None
+        time_interval = 0.001
+        count = timeout // time_interval
+        while count > 0:
+            MyUtil.serial_error_check()
+            if MyCore.return_value:
+                break
+            time.sleep(time_interval)
+            count = count - 1
+            MyCore.can_send_data = True
