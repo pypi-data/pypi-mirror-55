f28edcd0ed130b3c41e1941d4df66963c933fa01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:35 +0000

    Add package atomman/1.3.1/atomman-1.3.1.tar.gz

---
+++ packages/atomman/atomman-1.3.1.tar.gz/atomman-1.3.1/atomman/__init__.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+"""
+Attributes
+----------
+rootdir : str
+    The absolute path to the iprPy package's root directory used to locate
+    contained data files.
+"""
+
+# Standard Python libraries
+import os
+
+# Define rootdir
+rootdir = os.path.dirname(os.path.abspath(__file__))
+
+# Read version from VERSION file
+with open(os.path.join(rootdir, 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
+
+# atomman imports
+from . import unitconvert
+from . import tools
+from . import region
+from . import lammps
+from .dump import *
+from .dump import __all__ as dump_all
+from .core import *
+from .core import __all__ as core_all
+from .load import *
+from .load import __all__ as load_all
+from . import plot
+from . import defect
+
+
+__all__ = ['__version__', 'rootdir'] + dump_all + core_all + load_all
+__all__.sort()
+
+# Define default working units
+unitconvert.reset_units(length = 'angstrom', mass = 'amu', energy='eV', charge='e')
