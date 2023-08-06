dcef25104533c57f70548ca707630dd3e63c29fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:28 +0000

    Add package calculator_test_leonardo/0.1/calculator_test_leonardo-0.1.tar.gz

---
+++ packages/calculator_test_leonardo/calculator_test_leonardo-0.1.tar.gz/calculator_test_leonardo-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='calculator_test_leonardo',
+      version='0.1',
+      description='Simple calculator with python',
+      url='https://github.com/leonardoishii07/calculator',
+      author='Leonardo Ishii',
+      author_email='leoishii@outlook.com',
+      license='USP',
+      packages=['calc'],
+      zip_safe=False)
