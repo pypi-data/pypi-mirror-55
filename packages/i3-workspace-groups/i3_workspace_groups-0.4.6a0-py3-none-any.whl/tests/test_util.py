8b01338dc3ba9a35cab2db2c322e74f65075c61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:32 +0000

    Add package i3-workspace-groups/0.4.6a0/i3_workspace_groups-0.4.6a0-py3-none-any.whl

---
+++ packages/i3-workspace-groups/i3_workspace_groups-0.4.6a0-py3-none-any.whl/tests/test_util.py
@@ -0,0 +1,16 @@
+import unittest.mock as mock
+
+import i3ipc
+
+from i3wsgroups import workspace_names
+
+
+def create_workspace(workspace_id: int,
+                     ws_metadata: workspace_names.WorkspaceGroupingMetadata
+                    ) -> i3ipc.Con:
+    workspace = mock.create_autospec(i3ipc.Con)
+    workspace.id = workspace_id
+    if ws_metadata.group is None:
+        ws_metadata.group = ''
+    workspace.name = workspace_names.create_name(ws_metadata)
+    return workspace
