0a15fef6553b772adeb72ccca1477670d55db3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:02 +0000

    Add package nanaimo/0.0.42/nanaimo-0.0.42-45-py3-none-any.whl

---
+++ packages/nanaimo/nanaimo-0.0.42-45-py3-none-any.whl/nanaimo/builtin/gtest_over_jlink.py
@@ -0,0 +1,83 @@
+#
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# This software is distributed under the terms of the MIT License.
+#
+#                                       (@@@@%%%%%%%%%&@@&.
+#                              /%&&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&@@(
+#                              *@&%%%%%%%%%&&%%%%%%%%%%%%%%%%%%&&&%%%%%%%
+#                               @   @@@(@@@@%%%%%%%%%%%%%%%%&@@&* @@@   .
+#                               ,   .        .  .@@@&                   /
+#                                .       .                              *
+#                               @@              .                       @
+#                              @&&&&&&@. .    .                     *@%&@
+#                              &&&&&&&&&&&&&&&&@@        *@@############@
+#                     *&/ @@ #&&&&&&&&&&&&&&&&&&&&@  ###################*
+#                              @&&&&&&&&&&&&&&&&&&##################@
+#                                 %@&&&&&&&&&&&&&&################@
+#                                        @&&&&&&&&&&%#######&@%
+#  nanaimo                                   (@&&&&####@@*
+#
+"""
+Built-in :class:`nanaimo.fixtures.Fixture` objects for common scenarios. See individual fixture documentation for use.
+"""
+import asyncio
+import pathlib
+import typing
+
+import pytest
+
+import nanaimo
+import nanaimo.connections
+import nanaimo.connections.uart
+import nanaimo.instruments
+import nanaimo.instruments.jlink
+import nanaimo.parsers
+import nanaimo.parsers.gtest
+import nanaimo.pytest.plugin
+import nanaimo.fixtures
+
+
+class Fixture(nanaimo.fixtures.Fixture):
+    """
+    Uploads a firmware using JLink and monitors a UART expected gtest output. Returns 0 if all
+    gtest tests pass else returns non-zero.
+    """
+    fixture_name = 'gtest_over_jlink'
+    argument_prefix = 'gtest'
+
+    @classmethod
+    def on_visit_test_arguments(cls, arguments: nanaimo.Arguments) -> None:
+        nanaimo.connections.uart.ConcurrentUart.on_visit_test_arguments(arguments)
+        nanaimo.instruments.jlink.ProgramUploaderJLink.on_visit_test_arguments(arguments)
+        arguments.add_argument('--gtest-timeout',
+                               default=30.0,
+                               type=float,
+                               help='Time to wait for the gtest to complete in seconds.')
+
+    async def on_gather(self, args: nanaimo.Namespace) -> nanaimo.Artifacts:
+
+        uploader = nanaimo.instruments.jlink.ProgramUploaderJLink()
+        jlink_scripts = pathlib.Path(args.base_path).glob(args.jlink_scripts)
+        parser = nanaimo.parsers.gtest.Parser(args.gtest_timeout)
+
+        result = 0
+        with nanaimo.connections.uart.ConcurrentUart.new_default(args.port, args.port_speed) as monitor:
+            for script in jlink_scripts:
+                if result != 0:
+                    break
+                result = await asyncio.wait_for(uploader.upload(script), timeout=args.upload_timeout_seconds)
+                if result != 0:
+                    break
+                result = await parser.read_test(monitor)
+
+        return nanaimo.Artifacts(result)
+
+
+@nanaimo.fixtures.PluggyFixtureManager.type_factory
+def get_fixture_type() -> typing.Type['Fixture']:
+    return Fixture
+
+
+@pytest.fixture
+def gtest_over_jlink(request: typing.Any) -> nanaimo.fixtures.Fixture:
+    return nanaimo.pytest.plugin.create_pytest_fixture(request, Fixture.get_canonical_name())
---
+++ packages/nanaimo/nanaimo-0.0.42-45-py3-none-any.whl/nanaimo/instruments/saleae/__init__.py
@@ -0,0 +1,77 @@
+#
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# This software is distributed under the terms of the MIT License.
+#
+#                                       (@@@@%%%%%%%%%&@@&.
+#                              /%&&%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&@@(
+#                              *@&%%%%%%%%%&&%%%%%%%%%%%%%%%%%%&&&%%%%%%%
+#                               @   @@@(@@@@%%%%%%%%%%%%%%%%&@@&* @@@   .
+#                               ,   .        .  .@@@&                   /
+#                                .       .                              *
+#                               @@              .                       @
+#                              @&&&&&&@. .    .                     *@%&@
+#                              &&&&&&&&&&&&&&&&@@        *@@############@
+#                     *&/ @@ #&&&&&&&&&&&&&&&&&&&&@  ###################*
+#                              @&&&&&&&&&&&&&&&&&&##################@
+#                                 %@&&&&&&&&&&&&&&################@
+#                                        @&&&&&&&&&&%#######&@%
+#  nanaimo                                   (@&&&&####@@*
+#
+"""
+TODO: See https://github.com/ppannuto/python-saleae/blob/master/saleae/saleae.py for the command
+strings. They don't seem to be documented anywhere else.
+"""
+
+import asyncio
+import typing
+
+import nanaimo
+import nanaimo.fixtures
+
+
+class Fixture(nanaimo.fixtures.Fixture):
+    """
+    .. Warning:: Stubbed-out implementation
+        This fixture doesn't do anything yet either then the most naive query possible of the Saleae.
+        This would be a great first contribution to the Nanaimo project.
+    """
+
+    fixture_name = 'saleae'
+
+    @classmethod
+    def on_visit_test_arguments(cls, arguments: nanaimo.Arguments) -> None:
+        arguments.add_argument('--saleae-port',
+                               enable_default_from_environ=True,
+                               default='10429',
+                               help='TCP port for the logic socket server.')
+        arguments.add_argument('--saleae-host',
+                               enable_default_from_environ=True,
+                               default='localhost',
+                               help='hostname for the logic socket server.')
+
+    async def on_gather(self, args: nanaimo.Namespace) -> nanaimo.Artifacts:
+        self.logger.info('about to connect to {}:{}'.format(args.saleae_host, args.saleae_port))
+        reader, writer = await asyncio.open_connection(host=args.saleae_host, port=args.saleae_port, loop=self.loop)
+
+        writer.write('GET_ALL_SAMPLE_RATES\0'.encode('utf-8'))
+
+        rx_buffer = ''
+        while 'ACK' not in rx_buffer:
+            rx_bytes = await reader.read(256)
+            if rx_bytes is not None:
+                rx_text = rx_bytes.decode('utf-8')
+                self.logger.debug('rx: %s', rx_text)
+                rx_buffer += rx_text
+                # this is kind of crap. do way better.
+                if 'NAK' == rx_buffer[0:3]:
+                    self.logger.error('NAK')
+                    break
+
+        writer.close()
+        await writer.wait_closed()
+        return nanaimo.Artifacts()
+
+
+@nanaimo.fixtures.PluggyFixtureManager.type_factory
+def get_fixture_type() -> typing.Type['Fixture']:
+    return Fixture
