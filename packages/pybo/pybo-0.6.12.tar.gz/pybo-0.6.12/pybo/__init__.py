629d8be00dd1c76c833fd925cb3b99cf03aaaba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:28 +0000

    Add package pybo/0.6.12/pybo-0.6.12.tar.gz

---
+++ packages/pybo/pybo-0.6.12.tar.gz/pybo-0.6.12/pybo/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+from .utils.regex_batch_apply import get_regex_pairs, batch_apply_regex
+from .utils.bo_sorted import bo_sorted
+from botok import *
+import pyewts
+
+__version__ = "0.6.12"
