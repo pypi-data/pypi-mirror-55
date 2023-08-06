b5977a0ae7a2ab6fb0fa2a235ed3c7b457e66a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:10 +0000

    Add package readthedocs-sphinx-ext/1.0.1/readthedocs-sphinx-ext-1.0.1.tar.gz

---
+++ packages/readthedocs-sphinx-ext/readthedocs-sphinx-ext-1.0.1.tar.gz/readthedocs-sphinx-ext-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import codecs
+try:
+    from setuptools import setup, find_packages
+    extra_setup = dict(
+        install_requires=['requests', 'Jinja2>=2.9'],
+    )
+except ImportError:
+    from distutils.core import setup
+    extra_setup = {}
+
+setup(
+    name='readthedocs-sphinx-ext',
+    version='1.0.1',
+    author='Read the Docs, Inc',
+    author_email='dev@readthedocs.com',
+    url='http://github.com/readthedocs/readthedocs-sphinx-ext',
+    license='MIT',
+    description='Sphinx extension for Read the Docs overrides',
+    package_dir={'': '.'},
+    packages=find_packages('.'),
+    long_description=codecs.open("README.rst", "r", "utf-8").read(),
+    # trying to add files...
+    include_package_data=True,
+    package_data={
+        '': ['_static/*.js', '_static/*.js_t', '_static/*.css', '_templates/*.tmpl'],
+    },
+    **extra_setup
+)
