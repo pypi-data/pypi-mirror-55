1609e9e7784a652d09712759299f7c9d0687ebb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:41 +0000

    Add package richenum/1.3.1/richenum-1.3.1.tar.gz

---
+++ packages/richenum/richenum-1.3.1.tar.gz/richenum-1.3.1/CHANGELOG.rst
@@ -0,0 +1,40 @@
+Changelog
+=========
+
+1.2.1 (2016-09-16)
+------------------
+    - ``EnumLookupError`` class now inherits from built-in ``LookupError``.
+
+1.2.0 (2016-04-15)
+------------------
+    - added simple ``LookupError`` members that are thrown when
+      ``RichEnum.lookup`` is called for a nonexistent attr/val pair.
+      Users can choose to catch either the specific ``LookupError`` or
+      continue to catch ``EnumLookupError``.
+
+1.1.0 (2014-04-17)
+------------------
+    - support for Python 3 and PyPy
+
+1.0.4 (2013-12-03)
+------------------
+    - Better unicode handling in ``__str__``, ``__unicode__``, and
+      ``__repr__`` magic methods.
+
+1.0.3 (2013-12-03)
+------------------
+    - Stop throwing warnings.
+
+1.0.2 (2013-11-05)
+------------------
+    - Suppress warnings from mismatched type comparisons when generated
+      in RichEnum.lookup.
+
+1.0.1 (2013-09-20)
+------------------
+    - Raise warnings when comparing enum values to other types, but not
+      when checking membership or comparing to None.
+
+1.0.0 (2013-08-16)
+------------------
+    - Initial public release.
---
+++ packages/richenum/richenum-1.3.1.tar.gz/richenum-1.3.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='richenum',
+    version='1.3.1',
+    description='Enum library for python.',
+    long_description=(
+        open('README.rst').read() + '\n\n' +
+        open('CHANGELOG.rst').read() + '\n\n' +
+        open('AUTHORS.rst').read()),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='python enum richenum',
+    url='https://github.com/hearsaycorp/richenum',
+    author='Hearsay Social',
+    author_email='opensource@hearsaysocial.com',
+    license='MIT',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    tests_require=['pytest'],
+    setup_requires=["pytest-runner"],
+    install_requires=['six'],
+    test_suite='tests'
+)
