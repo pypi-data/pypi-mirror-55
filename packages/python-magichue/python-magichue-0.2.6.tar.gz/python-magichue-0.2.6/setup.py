ae0f2d41c5a49b2e386b46d2b8a8381b9e222ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:19 +0000

    Add package python-magichue/0.2.6/python-magichue-0.2.6.tar.gz

---
+++ packages/python-magichue/python-magichue-0.2.6.tar.gz/python-magichue-0.2.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+import magichue
+
+
+setup(
+    name='python-magichue',
+    version=magichue.__version__,
+    url='https://github.com/namacha/python-magichue',
+    author=magichue.__author__,
+    author_email='mac.ayu15@gmail.com',
+    long_description_content_type='text/markdown',
+    description='A library to interface with Magichue(or Magichome)',
+    long_description=open('README.md').read(),
+    keywords='IoT SmartLight Led HomeAutomation',
+    license=magichue.__license__,
+    
+    packages=find_packages(),
+)
