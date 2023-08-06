c6e4bad0a94646c969d8d7d90efa087570f66648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:48 +0000

    Add package testpkgsam/0.1/testpkgsam-0.1.tar.gz

---
+++ packages/testpkgsam/testpkgsam-0.1.tar.gz/testpkgsam-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='testpkgsam',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Sam',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['testpkgsam'],
+      zip_safe=False)
