002668de092ee9b9918ffb39ef550d60f78d0fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:57 +0000

    Add package JACK-Client/0.5.1/JACK-Client-0.5.1.tar.gz

---
+++ packages/JACK-Client/JACK-Client-0.5.1.tar.gz/JACK-Client-0.5.1/doc/fake__jack.py
@@ -0,0 +1,35 @@
+"""Mock module for Sphinx autodoc."""
+
+import ctypes.util
+
+old_find_library = ctypes.util.find_library
+
+
+def new_find_library(name):
+    if 'jack' in name.lower():
+        return NotImplemented
+    return old_find_library(name)
+
+
+# Monkey-patch ctypes to disable searching for JACK
+ctypes.util.find_library = new_find_library
+
+
+class Fake(object):
+
+    NULL = NotImplemented
+
+    JackTransportStopped = 0
+    JackTransportRolling = 1
+    JackTransportStarting = 3
+    JackTransportNetStarting = 4
+
+    PropertyCreated = 0
+    PropertyChanged = 1
+    PropertyDeleted = 2
+
+    def dlopen(self, _):
+        return self
+
+
+ffi = Fake()
