8b06ca1ca68cf2f61666abb38b4ab22ca303bbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:36 +0000

    Add package firex-flame/2.0/firex_flame-2.0-py2.py3-none-any.whl

---
+++ packages/firex-flame/firex_flame-2.0-py2.py3-none-any.whl/firex_flame/__main__.py
@@ -0,0 +1,133 @@
+import argparse
+import eventlet
+import logging
+import os
+from pathlib import Path
+import signal
+from threading import Timer
+
+from firex_flame.main_app import run_flame
+from firex_flame.flame_helper import get_flame_debug_dir, get_flame_pid_file_path, DEFAULT_FLAME_TIMEOUT, \
+    stop_main_thread, get_flame_url
+from firex_flame.event_broker_processor import BrokerConsumerConfig
+
+logger = logging.getLogger(__name__)
+eventlet.monkey_patch()
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', help='Port for starting the web server on', type=int, default=8000)
+    parser.add_argument('--uid', help='Unique identifier for the represented FireX run.')
+    parser.add_argument('--logs_dir', help='Logs directory.', default=None, required=True)
+    parser.add_argument('--chain', help='Chain of the run.', default=None)
+    parser.add_argument('--recording', help='A file containing the recording of celery events.', default=None)
+    parser.add_argument('--central_server', help='A central web server from which the UI and logs can be served.',
+                        default=None)
+    parser.add_argument('--central_server_ui_path', help='Path part of the URL from which the central server serves'
+                                                         'the UI. Only has meaning when a central_server is supplied.',
+                        default=None)
+    parser.add_argument('--logs_server', help='A central web server from which logs can be fetched.',
+                        default=None)
+    parser.add_argument('--central_documentation_url', help='URL linking to main out-of-app docs.',
+                        default='http://www.firexapp.com/')
+    parser.add_argument('--firex_bin_path', help='Path to a firex executable.',
+                        default=None)
+    # TODO: could validate either rec file exists or broker is supplied.
+    parser.add_argument('--broker', help='Celery broker.', default=None)
+    parser.add_argument('--flame_timeout', help='Maximum lifetime of this service, in seconds', type=int,
+                        default=DEFAULT_FLAME_TIMEOUT)
+    parser.add_argument('--broker_recv_ready_file', help='File to create immediately before capturing celery events.',
+                        default=None)
+    parser.add_argument('--broker_max_retry_attempts',
+                        help='Number of retry attempts if connection with broker is lost. '
+                             'Retries are backed-off exponentially with base 2,'
+                             'so a value of 3 here with cause sleeps between retries of '
+                             '1 sec, 2 sec, 4 sec, before giving up on retries. Default waits at least a total of '
+                             '63 seconds before giving up on retries.',
+                        type=int,
+                        default=5)
+    parser.add_argument('--terminate_on_complete',
+                        help='Supply if the Flame server should terminate when the run completes. '
+                             'Causes the value of --flame_timeout to be ignored entirely.',
+                        type=bool,
+                        default=False)
+    return parser.parse_args()
+
+
+def _sigterm_handler(_, __):
+    stop_main_thread('SIGTERM detected')
+
+
+def _sigint_handler(_, __):
+    stop_main_thread('SIGINT detected')
+
+
+def _exit_on_timeout():
+    stop_main_thread('timeout exceeded')
+
+
+def _config_logging(root_logs_dir):
+    flame_logs_dir = get_flame_debug_dir(root_logs_dir)
+    os.makedirs(flame_logs_dir, exist_ok=True)
+
+    Path(get_flame_pid_file_path(root_logs_dir)).write_text(str(os.getpid()))
+
+    logging.basicConfig(
+        filename=os.path.join(flame_logs_dir, 'flame.log'),
+        format='[%(asctime)s][%(levelname)s][%(name)s]: %(message)s',
+        level=logging.DEBUG,
+    )
+    # This module is very noisy (logs all data sent), so turn up the level.
+    logging.getLogger('engineio.server').setLevel(logging.WARNING)
+
+
+def _create_run_metadata(cli_args):
+    return {
+        'uid': cli_args.uid,
+        'logs_dir': cli_args.logs_dir,
+        'central_server': cli_args.central_server,
+        'central_server_ui_path': cli_args.central_server_ui_path,
+        'chain': cli_args.chain,
+        'logs_server': cli_args.logs_server,
+        'central_documentation_url': cli_args.central_documentation_url,
+        'flame_url': get_flame_url(cli_args.port),
+        'firex_bin': cli_args.firex_bin_path,
+        'root_uuid': None,
+    }
+
+
+def create_broker_processor_config(args):
+    return BrokerConsumerConfig(args.broker,
+                                args.broker_max_retry_attempts,
+                                args.broker_recv_ready_file,
+                                args.terminate_on_complete)
+
+
+class NoopTimer:
+    def start(self):
+        pass
+
+    def cancel(self):
+        pass
+
+
+def main():
+    signal.signal(signal.SIGTERM, _sigterm_handler)
+    signal.signal(signal.SIGINT, _sigint_handler)
+
+    args = _parse_args()
+    _config_logging(args.logs_dir)
+    t = NoopTimer() if args.terminate_on_complete else Timer(args.flame_timeout, _exit_on_timeout)
+    try:
+        t.start()
+        logger.info('Starting Flame Server with args: %s' % args)
+        run_flame(create_broker_processor_config(args),
+                  args.port,
+                  _create_run_metadata(args),
+                  args.recording)
+        t.cancel()
+    except Exception as e:
+        logger.exception(e)
+    finally:
+        logger.info("Flame server finished.")
---
+++ packages/firex-flame/firex_flame-2.0-py2.py3-none-any.whl/firex_flame/api.py
@@ -0,0 +1,251 @@
+"""
+Flask API module for interacting with celery tasks.
+"""
+
+import logging
+
+from flask import jsonify
+
+from firex_flame.flame_helper import wait_until
+
+from firex_flame.event_aggregator import slim_tasks_by_uuid, INCOMPLETE_STATES
+
+from eventlet import spawn
+from eventlet.green import subprocess, select
+
+logger = logging.getLogger(__name__)
+
+subprocess_dict = {}
+
+
+def _run_metadata_to_api_model(run_metadata, root_uuid):
+    return {
+        'uid': run_metadata['uid'],
+        'logs_dir': run_metadata['logs_dir'],
+        'root_uuid': root_uuid,
+        'chain': run_metadata['chain'],
+        'logs_server': run_metadata['logs_server'],
+    }
+
+
+def _get_task_fields(tasks_by_uuid, fields):
+    return {uuid: {f: v for f, v in task.items() if f in fields}
+            for uuid, task in tasks_by_uuid.items()}
+
+
+def poll_fd_readable(fd, timeout=0):
+    readable, _, _ = select.select([fd], [], [], timeout)
+    return readable
+
+
+
+def monitor_file(sio_server, sid, host, filename):
+    # To avoid issues with huge log files, we only get the last 50000 lines
+    max_lines = 50000
+
+    # helper to send data
+    def emit_line_data(lines):
+        sio_server.emit('file-data', {'data': lines}, room=sid)
+
+    # spawn ssh to host to tail -f the file - output to be sent to requesting client
+    logger.info("Will start monitoring file %s on host %s" % (filename, host))
+    # noinspection PyBroadException
+    try:
+        # noinspection PyUnresolvedReferences
+        with subprocess.Popen(["/bin/ssh", "-C", "-t", "-t", host,
+                               """[ "$(/bin/find %s -perm -004)" ] && """
+                               """/usr/bin/tail -n %d --follow=name %s 2>/dev/null || """
+                               """echo "Access denied." """ % (filename, max_lines, filename)],
+                              bufsize=0, stdout=subprocess.PIPE) as p:
+            # Keep track of all spawned processes to be able to manage them later
+            subprocess_dict[sid] = p
+
+            # Wait up to 60s for data to start streaming in
+            poll_fd_readable(p.stdout, 60)
+
+            # Gather up the first bunch of log file data that comes in rapidly and it to the UI in one batch
+            lines = []
+            while poll_fd_readable(p.stdout, 5):
+                try:
+                    line = p.stdout.readline()
+                    if not line or line == '':
+                        break
+                    lines.append(line.decode('utf-8', 'ignore'))
+                except Exception:
+                    logger.warning("Exception raised while trying to monitor file:", exc_info=True)
+                    return
+            num_lines = len(lines)
+            if num_lines:
+                if num_lines >= max_lines:
+                    lines.insert(0, "[Showing only last %d lines]\n" % max_lines)
+                else:
+                    if num_lines == 1 and lines[0].startswith("Access denied."):
+                        emit_line_data(["File access permissions prevent viewing of file.\n"])
+                        return
+                    else:
+                        lines.insert(0, "[Beginning of file]\n")
+                emit_line_data(lines)
+
+            # Now keep up with the 'live' incoming trickle of data
+            done = False
+            while not done:
+                lines = []
+                try:
+                    # Looping check for data - might get multiple lines in a chunk
+                    while poll_fd_readable(p.stdout, 1):
+                        line = p.stdout.readline()
+                        if not line or line == '':
+                            done = True
+                            break
+                        lines.append(line.decode('utf-8', 'ignore'))
+                    # if we got lines, send them off to the ui
+                    if len(lines):
+                        emit_line_data(lines)
+                        num_lines += len(lines)
+                except Exception:
+                    logger.warning("Exception raised while trying to monitor file:", exc_info=True)
+                    return
+
+            if num_lines:
+                emit_line_data(['[end of file - program exited]\n'])
+            else:
+                emit_line_data(['[temporary file no longer exists because command has completed]\n'])
+
+    except Exception as e:
+        emit_line_data(["Spawned subprocess to monitor file failed:\n"])
+        emit_line_data([str(e)])
+        return
+
+
+def term_subproc(sid):
+    if sid not in subprocess_dict:
+        logger.warning("SID %s not in subprocess list" % sid)
+        return
+
+    subproc = subprocess_dict[sid]
+    subproc.terminate()
+    del subprocess_dict[sid]
+
+
+def term_all_subprocs():
+    for sid in subprocess_dict:
+        term_subproc(sid)
+
+
+def create_socketio_task_api(sio_server, event_aggregator, run_metadata):
+
+    @sio_server.on('send-graph-state')
+    def emit_frontend_tasks_by_uuid(sid):
+        """ Send 'slim' fields for all tasks. This allows visualization of the graph."""
+        sio_server.emit('graph-state', slim_tasks_by_uuid(event_aggregator.tasks_by_uuid), room=sid)
+
+    @sio_server.on('send-graph-fields')
+    def emit_task_fields_by_uuid(sid, fields):
+        """ Send the requested fields for all tasks."""
+        response = _get_task_fields(event_aggregator.tasks_by_uuid, fields)
+        sio_server.emit('graph-fields', response, room=sid)
+
+    @sio_server.on('send-run-metadata')
+    def emit_run_metadata(sid):
+        """ Get static run-level data."""
+        response = _run_metadata_to_api_model(run_metadata, event_aggregator.root_uuid)
+        sio_server.emit('run-metadata', response, room=sid)
+
+    @sio_server.on('send-task-details')
+    def emit_detailed_tasks(sid, uuids):
+        """ Get all fields for requested task UUIDs.
+        Arguments:
+            sid: The session ID to emit to.
+            uuids (str or list of str): The uuid of the desired task to get details for.
+        """
+        if isinstance(uuids, str):
+            uuid = uuids
+            response = event_aggregator.tasks_by_uuid.get(uuid, None)
+            sio_server.emit('task-details-' + uuid, response, room=sid)
+        else:
+            if not isinstance(uuids, list):
+                response = []
+            else:
+                response = [event_aggregator.tasks_by_uuid.get(u, None) for u in uuids]
+            sio_server.emit('task-details', response, room=sid)
+
+    @sio_server.on('start-listen-file')
+    def start_file_monitor(sid, args):
+        if 'host' not in args or 'filepath' not in args:
+            sio_server.emit('file-line',
+                            "File monitoring request is missing either host and/or filepath parameters: %s" % args,
+                            room=sid)
+        else:
+            spawn(monitor_file, sio_server=sio_server, sid=sid, host=args['host'], filename=args['filepath'])
+
+    @sio_server.on('stop-listen-file')
+    def stop_file_monitor(sid):
+        term_subproc(sid)
+
+
+def create_rest_task_api(web_app, event_aggregator, run_metadata):
+
+    @web_app.route('/api/tasks')
+    def get_all_tasks_by_uuid():
+        return jsonify(slim_tasks_by_uuid(event_aggregator.tasks_by_uuid))
+
+    # TODO: add /api/tasks?uuids=uuid1,uuid2, or POST with request body containing query by uuid
+    @web_app.route('/api/tasks/<uuid>')
+    def get_task_details(uuid):
+        if uuid not in event_aggregator.tasks_by_uuid:
+            return '', 404
+        # if 'fields' in request['query']['fields']:
+        #     return jsonify( _get_task_fields(event_aggregator.tasks_by_uuid, request['query']['fields']))
+        # No fields were requested, so send all fields.
+        return jsonify(event_aggregator.tasks_by_uuid[uuid])
+
+    @web_app.route('/api/run-metadata')
+    def get_run_metadata():
+        return jsonify(_run_metadata_to_api_model(run_metadata, event_aggregator.root_uuid))
+
+
+def create_revoke_api(sio_server, web_app, celery_app, tasks):
+
+    @sio_server.on('revoke-task')
+    def socketio_revoke_task(sid, uuid):
+        logger.info("Received SocketIO request to revoke %s" % uuid)
+        revoked = _revoke_task(uuid)
+        response_event = 'revoke-success' if revoked else 'revoke-failed'
+        sio_server.emit(response_event, room=sid)
+
+    @web_app.route('/api/revoke/<uuid>')
+    def rest_revoke_task(uuid):
+        logger.info("Received REST request to revoke %s" % uuid)
+        revoked = _revoke_task(uuid)
+        return '', 200 if revoked else 500
+
+    def _wait_until_task_complete(task, timeout, sleep_for=1):
+        wait_until(lambda t: t['state'] not in INCOMPLETE_STATES, timeout, sleep_for, task)
+
+    def _revoke_task(uuid):
+        if uuid not in tasks:
+            return False
+
+        # Get the task instance
+        task = tasks[uuid]
+
+        # Try to revoke the task
+        if task['state'] in INCOMPLETE_STATES:
+            celery_app.control.revoke(uuid, terminate=True)
+            logger.info("Submitted revoke to celery for: %s" % uuid)
+        else:
+            logger.info("Task %s already in terminal state %s" % (uuid, task['state']))
+
+        # Wait for the task to become revoked
+        revoke_timeout = 10
+        _wait_until_task_complete(task, timeout=revoke_timeout)
+
+        task_runstate = task['state']
+        revoked = task_runstate == 'task-revoked'
+        if not revoked:
+            logger.warning("Failed to revoke task: waited %s sec and runstate is currently %s"
+                           % (revoke_timeout, task_runstate))
+        else:
+            logger.debug("Successfully revoked task %s." % uuid)
+
+        return revoked  # If the task was successfully revoked, return true
