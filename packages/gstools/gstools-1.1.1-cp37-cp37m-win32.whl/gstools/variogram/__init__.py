a6a96419c43cf240e88d595d5cec3bed4a6acdda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:17 +0000

    Add package gstools/1.1.1/gstools-1.1.1-cp37-cp37m-win32.whl

---
+++ packages/gstools/gstools-1.1.1-cp37-cp37m-win32.whl/gstools/variogram/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+GStools subpackage providing tools for estimating and fitting variograms.
+
+.. currentmodule:: gstools.variogram
+
+Variogram estimation
+^^^^^^^^^^^^^^^^^^^^
+
+.. autosummary::
+   vario_estimate_unstructured
+   vario_estimate_structured
+
+----
+"""
+from __future__ import absolute_import
+
+from gstools.variogram.variogram import (
+    vario_estimate_structured,
+    vario_estimate_unstructured,
+)
+
+__all__ = ["vario_estimate_unstructured", "vario_estimate_structured"]
