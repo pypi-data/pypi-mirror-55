6b814230f874c5fcc5f36e5f0d5bb2c2b4b1a42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:03 +0000

    Add package docli/1.6/docli-1.6.tar.gz

---
+++ packages/docli/docli-1.6.tar.gz/docli-1.6/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='docli',
+    version='1.6',
+
+    description='Digital Ocean command line interface',
+    long_description=long_description,
+
+    author='Yogesh Panchal',
+    author_email='yspanchal@gmail.com',
+
+    url='https://github.com/yspanchal/docli',
+    download_url='https://github.com/yspanchal/docli/tarball/master',
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'requests',
+        'tabulate',
+    ],
+    entry_points='''
+        [console_scripts]
+        docli=docli.main:docli
+    ''',
+#    classifiers=[
+#        'License :: OSI Approved :: Apache Software License',
+#        'Programming Language :: Python',
+#    ],
+)
