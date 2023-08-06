9bdb7c2f288df3c6e48c264ceddac84934fc37f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:13 +0000

    Add package chapter6_hyman/1.0.1/chapter6_hyman-1.0.1.tar.gz

---
+++ packages/chapter6_hyman/chapter6_hyman-1.0.1.tar.gz/chapter6_hyman-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'chapter6_hyman',
+        version = '1.0.1',
+        py_modules = ['chapter6_hyman'],
+        author = 'hyman',
+        author_email = '578046099@qq.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of top3/add/extend lists',
+
+    )
