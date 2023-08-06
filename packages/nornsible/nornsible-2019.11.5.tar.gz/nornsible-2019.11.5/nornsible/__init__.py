5c7bb2d16fc2af74d99001bb45c54c500df7e914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:11 +0000

    Add package nornsible/2019.11.5/nornsible-2019.11.5.tar.gz

---
+++ packages/nornsible/nornsible-2019.11.5.tar.gz/nornsible-2019.11.5/nornsible/__init__.py
@@ -0,0 +1,32 @@
+"""nornir tag and host/group limit wrapper"""
+import logging
+from logging import NullHandler
+
+from nornsible.nornsible import (
+    InitNornsible,
+    nornsible_delegate,
+    nornsible_task,
+    nornsible_task_message,
+    patch_inventory,
+    parse_cli_args,
+    patch_config,
+    print_result,
+)
+
+
+__version__ = "2019.11.05"
+__all__ = (
+    "InitNornsible",
+    "nornsible_delegate",
+    "nornsible_task",
+    "nornsible_task_message",
+    "patch_inventory",
+    "parse_cli_args",
+    "patch_config",
+    "print_result",
+)
+
+
+# Setup logger
+session_log = logging.getLogger(__name__)
+logging.getLogger(__name__).addHandler(NullHandler())
