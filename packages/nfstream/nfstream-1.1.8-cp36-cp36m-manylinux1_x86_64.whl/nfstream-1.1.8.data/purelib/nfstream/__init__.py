705c2f7645f1116011b2a515b7b7d217c914e5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:20 +0000

    Add package nfstream/1.1.8/nfstream-1.1.8-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/nfstream/nfstream-1.1.8-cp36-cp36m-manylinux1_x86_64.whl/nfstream-1.1.8.data/purelib/nfstream/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from nfstream import streamer
+
+"""
+    streamer module is the core module of nfstream package.
+"""
+
+__author__ = """Zied Aouini"""
+__email__ = 'aouinizied@gmail.com'
+__version__ = '1.1.8'
